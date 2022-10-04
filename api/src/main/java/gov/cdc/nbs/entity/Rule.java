5134a8c68ca84172082136709851cae236534285
Michael Peels <michaelpeels@Michael-Peels.local>
2022-10-04 12:12:36 -0400

    WIP - set ddl-auto to validate

--- api/src/main/java/gov/cdc/nbs/entity/Rule.java
+++ api/src/main/java/gov/cdc/nbs/entity/Rule.java
@@ -1,19 +1,22 @@
 package gov.cdc.nbs.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
-import javax.persistence.Entity;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-@Table(name = "\"Rule\"")
 public class Rule {
     @Id
     @Column(name = "rule_uid", nullable = false)
