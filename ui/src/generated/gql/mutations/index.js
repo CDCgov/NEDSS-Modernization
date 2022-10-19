2ee73fd6baf77c8ea647bb7994e2ef7dfea1c327
Michael Peels <michaelpeels@Michael-Peels.local>
2022-10-19 11:00:00 -0400

    Re-generate GraphQL

--- ui/src/generated/gql/mutations/index.js
+++ ui/src/generated/gql/mutations/index.js
@@ -3,4 +3,3 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports.createPatient = fs.readFileSync(path.join(__dirname, 'createPatient.gql'), 'utf8');
-module.exports.deletePatient = fs.readFileSync(path.join(__dirname, 'deletePatient.gql'), 'utf8');
