763bbfff0d7ccc6343acc5d7875c98c0771bc9d3
Michael Peels <michaelpeels@Michael-Peels.local>
2022-10-25 11:45:56 -0400

    Generate REST login endpoint, add token to GraphQL codegen

--- ui/src/codegen.ts
+++ ui/src/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: 'http://localhost:8080/graphql',
+    schema: [{ 'http://localhost:8080/graphql': { headers: { Authorization: 'Bearer ' + process.env.API_TOKEN } } }],
     documents: './src/generated/**/*.{gql,graphql,graphqls}',
     generates: {
         './src/generated/graphql/schema.ts': {
---
+++ ui/src/generated/models/LoginResponse.ts
@@ -0,0 +1,9 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type LoginResponse = {
+    token?: string;
+    username?: string;
+};
+
