95252e2189d1524fecb4dcc0111f6b1b2f22950a
Michael Peels <michaelpeels@Michael-Peels.lan>
2022-12-06 15:57:27 -0500

    Fix missing pagination

--- patient-search/ui/src/pages/advancedSearch/SearchItems.tsx
+++ patient-search/ui/src/pages/advancedSearch/SearchItems.tsx
@@ -80,7 +80,7 @@ export const SearchItems = ({ data, initialSearch, totalResults }: SearchItemsPr
             case 'PST':
                 return `${locator.streetAddr1 ?? ''} ${locator.cityCd ?? ''} ${locator.stateCd ?? ''} ${
                     locator.zipCd ?? ''
-                } ${locator.cntryCd}`;
+                } ${locator.cntryCd ?? ''}`;
             default:
                 return '';
         }
@@ -102,14 +102,14 @@ export const SearchItems = ({ data, initialSearch, totalResults }: SearchItemsPr
 
     return (
         <div className="margin-x-4">
-            {initialSearch && data?.total > 0 && (
+            {initialSearch && totalResults && data?.length > 0 && (
                 <Grid row className="flex-align-center flex-justify">
                     <p className="margin-0 font-sans-3xs margin-top-05 text-normal text-base">
                         Showing {data.content?.length} of {data.total}
                     </p>
                     <Pagination
                         style={{ justifyContent: 'flex-end' }}
-                        totalPages={Math.ceil(data.content?.length / data.total)}
+                        totalPages={Math.ceil(totalResults / data?.length)}
                         currentPage={currentPage}
                         pathname={'/advanced-search'}
                         onClickNext={() => handleNext('next')}
@@ -125,89 +125,93 @@ export const SearchItems = ({ data, initialSearch, totalResults }: SearchItemsPr
                         <div
                             key={index}
                             className="padding-3 margin-bottom-3 bg-white border border-base-light radius-md">
-                            <Grid row gap={6}>
-                                <Grid col={3} className="margin-bottom-2 large-col">
-                                    <h5 className="margin-0 text-normal text-gray-50">LEGAL NAME</h5>
-                                    <p className="margin-0 font-sans-md margin-top-05 text-bold text-primary word-break">
-                                        {item.firstNm}, {item.lastNm}
-                                    </p>
-                                </Grid>
-                                <Grid col={3} className="margin-bottom-2 large-col">
-                                    <div className="grid-row flex-align-center">
-                                        <h5 className="margin-0 text-normal font-sans-1xs text-gray-50 margin-right-1">
-                                            DATE OF BIRTH
-                                        </h5>
-                                        <p className="margin-0 font-sans-1xs text-normal">
-                                            {new Date(item.birthTime).toLocaleDateString('en-US')}
-                                            <span className="font-sans-2xs">
-                                                {' '}
-                                                ({_calculateAge(new Date(item.birthTime))} years)
-                                            </span>
+                            <Grid row gap={3}>
+                                <Grid col={3}>
+                                    <Grid col={12} className="margin-bottom-2">
+                                        <h5 className="margin-0 text-normal text-gray-50">LEGAL NAME</h5>
+                                        <p className="margin-0 font-sans-md margin-top-05 text-bold text-primary word-break">
+                                            {item.firstNm}, {item.lastNm}
                                         </p>
-                                    </div>
-                                    <div className="grid-row flex-align-center">
-                                        <h5 className="margin-0 text-normal font-sans-1xs text-gray-50 margin-right-1">
-                                            SEX
-                                        </h5>
-                                        <p className="margin-0 font-sans-1xs text-normal">
-                                            {item.currSexCd === 'M'
-                                                ? 'Male'
-                                                : item.currSexCd === 'F'
-                                                ? 'Female'
-                                                : 'Unknown'}
-                                        </p>
-                                    </div>
-                                    <div className="grid-row flex-align-center">
-                                        <h5 className="margin-0 text-normal font-sans-1xs text-gray-50 margin-right-1">
-                                            PATIENT ID
-                                        </h5>
-                                        <p className="margin-0 font-sans-1xs text-normal">{item.localId}</p>
-                                    </div>
+                                    </Grid>
+                                    <Grid col={12} className="margin-bottom-2">
+                                        <div className="grid-row flex-align-center">
+                                            <h5 className="margin-0 text-normal font-sans-1xs text-gray-50 margin-right-1">
+                                                DATE OF BIRTH
+                                            </h5>
+                                            <p className="margin-0 font-sans-1xs text-normal">
+                                                {new Date(item.birthTime).toLocaleDateString('en-US')}
+                                                <span className="font-sans-2xs">
+                                                    {' '}
+                                                    ({_calculateAge(new Date(item.birthTime))} years)
+                                                </span>
+                                            </p>
+                                        </div>
+                                        <div className="grid-row flex-align-center">
+                                            <h5 className="margin-0 text-normal font-sans-1xs text-gray-50 margin-right-1">
+                                                SEX
+                                            </h5>
+                                            <p className="margin-0 font-sans-1xs text-normal">
+                                                {item.currSexCd === 'M'
+                                                    ? 'Male'
+                                                    : item.currSexCd === 'F'
+                                                    ? 'Female'
+                                                    : 'Unknown'}
+                                            </p>
+                                        </div>
+                                        <div className="grid-row flex-align-center">
+                                            <h5 className="margin-0 text-normal font-sans-1xs text-gray-50 margin-right-1">
+                                                PATIENT ID
+                                            </h5>
+                                            <p className="margin-0 font-sans-1xs text-normal">{item.localId}</p>
+                                        </div>
+                                    </Grid>
                                 </Grid>
-                                {/* Locator entries */}
-                                {item.NBSEntity.entityLocatorParticipations.map(
-                                    (locatorParticipation: LocatorParticipations, idIndex: number) => (
-                                        <Grid key={idIndex} col={3} className="margin-bottom-2">
-                                            <h5 className="margin-0 text-normal text-gray-50">
-                                                {getLocatorTypeDisplay(
-                                                    locatorParticipation.classCd,
-                                                    locatorParticipation.locator
-                                                )}
-                                            </h5>
-                                            <p className="margin-0 font-sans-1xs text-normal margin-top-05">
-                                                {/* {new Date(item.addTime).toLocaleDateString('en-US')} */}
-                                                {getLocatorDisplayValue(
-                                                    locatorParticipation.classCd,
-                                                    locatorParticipation.locator
-                                                )}
-                                            </p>
-                                        </Grid>
-                                    )
-                                )}
+                                <Grid row col={9}>
+                                    {/* Locator entries */}
+                                    {item.NBSEntity.entityLocatorParticipations.map(
+                                        (locatorParticipation: LocatorParticipations, idIndex: number) => (
+                                            <Grid key={idIndex} col={4} className="margin-bottom-2">
+                                                <h5 className="margin-0 text-normal text-gray-50">
+                                                    {getLocatorTypeDisplay(
+                                                        locatorParticipation.classCd,
+                                                        locatorParticipation.locator
+                                                    )}
+                                                </h5>
+                                                <p className="margin-0 font-sans-1xs text-normal margin-top-05">
+                                                    {/* {new Date(item.addTime).toLocaleDateString('en-US')} */}
+                                                    {getLocatorDisplayValue(
+                                                        locatorParticipation.classCd,
+                                                        locatorParticipation.locator
+                                                    )}
+                                                </p>
+                                            </Grid>
+                                        )
+                                    )}
 
-                                {/* Identifications */}
-                                {item.entityIds.map(
-                                    (
-                                        id: { typeDescTxt: String; rootExtensionTxt: String; typeCd: String },
-                                        idIndex: number
-                                    ) => (
-                                        <Grid key={idIndex} col={3} className="margin-bottom-2">
-                                            <h5 className="margin-0 text-normal text-gray-50">
-                                                {id.typeCd.replaceAll('_', ' ')}
-                                            </h5>
-                                            <p className="margin-0 font-sans-1xs text-normal margin-top-05">
-                                                {/* {new Date(item.addTime).toLocaleDateString('en-US')} */}
-                                                {id.rootExtensionTxt || '-'}
-                                            </p>
-                                        </Grid>
-                                    )
-                                )}
+                                    {/* Identifications */}
+                                    {item.entityIds.map(
+                                        (
+                                            id: { typeDescTxt: String; rootExtensionTxt: String; typeCd: String },
+                                            idIndex: number
+                                        ) => (
+                                            <Grid key={idIndex} col={3} className="margin-bottom-2">
+                                                <h5 className="margin-0 text-normal text-gray-50">
+                                                    {id.typeCd.replaceAll('_', ' ')}
+                                                </h5>
+                                                <p className="margin-0 font-sans-1xs text-normal margin-top-05">
+                                                    {/* {new Date(item.addTime).toLocaleDateString('en-US')} */}
+                                                    {id.rootExtensionTxt || '-'}
+                                                </p>
+                                            </Grid>
+                                        )
+                                    )}
 
-                                <Grid col={3} className="margin-bottom-2">
-                                    <h5 className="margin-0 text-normal text-gray-50">OTHER NAMES</h5>
-                                    <p className="margin-0 font-sans-1xs text-gray-50 text-normal margin-top-05">
-                                        {getOtherNames(item, item.names) || 'No data'}
-                                    </p>
+                                    <Grid col={3} className="margin-bottom-2">
+                                        <h5 className="margin-0 text-normal text-gray-50">OTHER NAMES</h5>
+                                        <p className="margin-0 font-sans-1xs text-gray-50 text-normal margin-top-05">
+                                            {getOtherNames(item, item.names) || 'No data'}
+                                        </p>
+                                    </Grid>
                                 </Grid>
                             </Grid>
                         </div>
