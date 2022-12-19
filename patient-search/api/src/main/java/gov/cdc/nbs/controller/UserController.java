eb5874b3894562c59f9e7e096d0dcd54386990c4
Michael Peels <michaelpeels@Michael-Peels.lan>
2022-12-19 15:38:00 -0500

    Add 'findAllUsers' endpoint. Update Organization endpoints to return Page

--- patient-search/api/src/main/java/gov/cdc/nbs/controller/UserController.java
+++ patient-search/api/src/main/java/gov/cdc/nbs/controller/UserController.java
@@ -3,22 +3,35 @@ package gov.cdc.nbs.controller;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import gov.cdc.nbs.config.security.SecurityProperties;
+import gov.cdc.nbs.entity.odse.AuthUser;
+import gov.cdc.nbs.graphql.GraphQLPage;
 import gov.cdc.nbs.model.LoginRequest;
 import gov.cdc.nbs.model.LoginResponse;
+import gov.cdc.nbs.repository.AuthUserRepository;
 import gov.cdc.nbs.service.UserService;
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 @RestController
 public class UserController {
-    private final UserService userService;
-    private final SecurityProperties securityProperties;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private SecurityProperties securityProperties;
+    @Autowired
+    private AuthUserRepository userRepository;
     private final String TOKEN_COOKIE_NAME = "nbs_token";
+
+    @Value("${nbs.max-page-size: 50}")
+    private Integer MAX_PAGE_SIZE;
 
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request, HttpServletResponse response) {
@@ -30,6 +43,11 @@ public class UserController {
         return new LoginResponse(userDetails.getUsername(),
                 userDetails.getFirstName() + " " + userDetails.getLastName(),
                 userDetails.getToken());
+    }
+
+    @QueryMapping
+    public Page<AuthUser> findAllUsers(@Argument GraphQLPage page) {
+        return userRepository.findAll(GraphQLPage.toPageable(page, MAX_PAGE_SIZE));
     }
 
 }
