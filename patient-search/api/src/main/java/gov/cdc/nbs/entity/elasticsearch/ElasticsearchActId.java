3ee557e2f903f1075ea99721f06b3363bf1468e7
Michael Peels <michaelpeels@Michael-Peels.lan>
2022-12-27 16:45:40 -0500

    update graphql's for Investigation

--- patient-search/api/src/main/java/gov/cdc/nbs/entity/elasticsearch/ElasticsearchActId.java
+++ patient-search/api/src/main/java/gov/cdc/nbs/entity/elasticsearch/ElasticsearchActId.java
@@ -4,8 +4,6 @@ import static gov.cdc.nbs.config.ElasticSearchConfig.DATE_PATTERN;
 
 import java.time.Instant;
 
-import javax.persistence.Id;
-
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.data.elasticsearch.annotations.ValueConverter;
