2a24450cde64514932d9f8f1ec48e208cede8f3e
Steve Urciuoli <gsa.steve.urciuoli@gmail.com>
2023-01-04 01:03:17 -0600

    CNPT-465 Add support for nested fields in Nifi/ElasticSearch/and API (#53)

---
+++ patient-search/api/src/main/java/gov/cdc/nbs/entity/elasticsearch/NestedEmail.java
@@ -0,0 +1,15 @@
+package gov.cdc.nbs.entity.elasticsearch;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class NestedEmail {
+    private String emailAddress;
+}
