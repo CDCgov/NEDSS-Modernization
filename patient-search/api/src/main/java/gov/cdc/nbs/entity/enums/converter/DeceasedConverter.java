650838de6feaa3853d59d849901546f9188bfa84
Michael Peels <michaelpeels@Michael-Peels.local>
2023-01-12 16:24:04 -0500

    DeceasedConverter

--- patient-search/api/src/main/java/gov/cdc/nbs/entity/enums/converter/DeceasedConverter.java
+++ patient-search/api/src/main/java/gov/cdc/nbs/entity/enums/converter/DeceasedConverter.java
@@ -25,8 +25,7 @@ public class DeceasedConverter implements PropertyValueConverter {
                     return Deceased.N;
                 case "UNK":
                     return Deceased.UNK;
-                case "FALSE":
-                case "false":
+                case "FALSE", "false":
                     return Deceased.FALSE;
                 default:
                     throw new IllegalArgumentException("Invalid Deceased string value: " + s);
