f9f7ee0d31a2a4c2f817af90065ea6ec6c223dd3
hclarkEnq <hclark@enquizit.com>
2023-01-18 05:53:14 -0800

    Adding reusable static table component

---
+++ patient-search/ui/src/components/TableComponent/TableComponent.tsx
@@ -0,0 +1,76 @@
+import { Button, Icon, Table, Pagination } from '@trussworks/react-uswds';
+
+type TableContentProps = {
+    tableHeader?: string;
+    tableHead: { name: string; sortable: boolean }[];
+    tableBody: any[];
+    isPagination?: boolean;
+    totalResults?: number;
+    currentPage?: number;
+    handleNext?: (page: number) => void;
+};
+
+export const TableComponent = ({
+    tableHeader,
+    tableHead,
+    tableBody,
+    isPagination = false,
+    totalResults = 20,
+    currentPage = 1,
+    handleNext
+}: TableContentProps) => {
+    return (
+        <div className="">
+            <div className="grid-row flex-align-center flex-justify padding-x-2 padding-y-3 border-bottom border-base-lighter">
+                <p className="font-sans-lg text-bold margin-0">{tableHeader}</p>
+            </div>
+            <Table bordered={false} fullWidth>
+                <thead>
+                    <tr>
+                        {tableHead.map((head: any, index) => (
+                            <th key={index} scope="col">
+                                <div style={{ display: 'flex', border: 'none', alignItems: 'center' }}>
+                                    {head.name}
+                                    {head.sortable && (
+                                        <Button className="usa-button--unstyled" type={'button'}>
+                                            <Icon.SortArrow color="black" />
+                                        </Button>
+                                    )}
+                                </div>
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableBody.map((item: any, index) => (
+                        <tr key={index}>
+                            {item.tableDetails.map((td: any, ind: number) =>
+                                td.title ? (
+                                    <td key={ind}>{td.title}</td>
+                                ) : (
+                                    <td
+                                        key={ind}
+                                        style={{ color: '#747474', fontStyle: 'italic' }}
+                                        className="font-sans-md">
+                                        No data
+                                    </td>
+                                )
+                            )}
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+            {isPagination && (
+                <Pagination
+                    style={{ justifyContent: 'flex-end' }}
+                    totalPages={Math.ceil(totalResults / 10)}
+                    currentPage={currentPage}
+                    pathname={'/patient-profile'}
+                    onClickNext={() => handleNext?.(currentPage + 1)}
+                    onClickPrevious={() => handleNext?.(currentPage - 1)}
+                    onClickPageNumber={(_, page) => handleNext?.(page)}
+                />
+            )}
+        </div>
+    );
+};
--- patient-search/ui/src/pages/patientProfile/index.tsx
+++ patient-search/ui/src/pages/patientProfile/index.tsx
@@ -6,10 +6,29 @@ import { useSearchParams } from 'react-router-dom';
 import { UserContext } from '../../providers/UserContext';
 import { FindPatientsByFilterQuery } from '../../generated/graphql/schema';
 import { calculateAge } from '../../utils/util';
+import { TableComponent } from '../../components/TableComponent/TableComponent';
+
+// To be added for future table list
+{
+    /* <div>
+    <strong>Reporting facility:</strong>
+    <br />
+    <span>Lab Corp</span>
+    <br />
+    <strong>Ordering facility:</strong>
+    <br />
+    <span>Dekalb General</span>
+    <br />
+    <strong>Ordering provider:</strong>
+    <br />
+    <span>Dr. Gene Davis SR</span>
+</div>; */
+}
 
 export const PatientProfile = () => {
     const { state } = useContext(UserContext);
     const [searchParams] = useSearchParams();
+    const [currentPage, setCurrentPage] = useState<number>(1);
     const [profileData, setProfileData] = useState<FindPatientsByFilterQuery['findPatientsByFilter']['content'][0]>();
 
     useEffect(() => {
@@ -19,6 +38,29 @@ export const PatientProfile = () => {
         }).then(async (data: any) => {
             setProfileData(data);
         });
+    }, []);
+
+    const [tableBody, setTableBody] = useState<any>([]);
+
+    useEffect(() => {
+        const tempArr = [];
+        for (let i = 0; i < 10; i++) {
+            tempArr.push({
+                id: i + 1,
+                checkbox: true,
+                tableDetails: [
+                    { id: 1, title: `12/${i + 1}/2021` },
+                    { id: 2, title: 'Acute flaccid myelitis' },
+                    { id: 3, title: i === 3 ? 'Confirmed' : i === 7 ? 'Not a case' : null },
+                    { id: 4, title: i === 1 ? 'Completed' : null },
+                    { id: 5, title: 'Cobb County' },
+                    { id: 6, title: i === 4 ? 'John Xerogeanes' : null },
+                    { id: 7, title: 'CAS10004022GA01' },
+                    { id: 8, title: i === 4 ? 'COIN1000XX01' : null }
+                ]
+            });
+        }
+        setTableBody(tempArr);
     }, []);
 
     return (
@@ -110,6 +152,26 @@ export const PatientProfile = () => {
                             </Grid>
                         </Grid>
                     </Grid>
+                </div>
+
+                <div className="margin-top-6 margin-bottom-2 flex-row common-card">
+                    <TableComponent
+                        isPagination={true}
+                        tableHeader={'Open investigations'}
+                        tableHead={[
+                            { name: 'Start Date', sortable: true },
+                            { name: 'Condition', sortable: true },
+                            { name: 'Case status', sortable: true },
+                            { name: 'Notification', sortable: true },
+                            { name: 'Jurisdiction', sortable: true },
+                            { name: 'Investigator', sortable: true },
+                            { name: 'Investigation #', sortable: false },
+                            { name: 'Co-infection #', sortable: false }
+                        ]}
+                        tableBody={tableBody}
+                        currentPage={currentPage}
+                        handleNext={(e) => setCurrentPage(e)}
+                    />
                 </div>
             </div>
         </div>
