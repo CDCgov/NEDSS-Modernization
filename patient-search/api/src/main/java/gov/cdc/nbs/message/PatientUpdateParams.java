eeb02322beceed111ee3bd013db919dce7ac076f
Ernst Melias <emelias@enquizit.com>
2023-01-19 09:46:23 -0500

    CNPT-661 finalize demographic api

--- patient-search/api/src/main/java/gov/cdc/nbs/message/PatientUpdateParams.java
+++ patient-search/api/src/main/java/gov/cdc/nbs/message/PatientUpdateParams.java
@@ -10,12 +10,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class PatientUpdateParams {
 	private Person updatePerson;
 	private List<TemplateInput> templateInputs;
