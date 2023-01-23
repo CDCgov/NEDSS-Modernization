5a7167b2df621c83ea637da57e282138f7c49016
hclarkEnq <hclark@enquizit.com>
2023-01-23 12:57:51 -0800

    adding option for click event from investigation

--- patient-search/ui/src/pages/advancedSearch/components/InvestigationResults.tsx
+++ patient-search/ui/src/pages/advancedSearch/components/InvestigationResults.tsx
@@ -93,8 +93,19 @@ export const InvestigationResults = ({
                 <Grid col={12} className="margin-bottom-2">
                     <h5 className="margin-0 text-normal text-gray-50">LEGAL NAME</h5>
                     <p
+                        onClick={async () => {
+                            const encryptedFilter = await EncryptionControllerService.encryptUsingPost({
+                                authorization: `Bearer ${state.getToken()}`,
+                                object: investigation
+                            });
+                            navigate(
+                                `/patient-profile/${investigation.localId}?data=${encodeURIComponent(
+                                    encryptedFilter.value
+                                )}`
+                            );
+                        }}
                         className="margin-0 font-sans-md margin-top-05 text-bold text-primary word-break"
-                        style={{ wordBreak: 'break-word' }}>
+                        style={{ wordBreak: 'break-word', cursor: 'pointer' }}>
                         {name}
                     </p>
                 </Grid>
