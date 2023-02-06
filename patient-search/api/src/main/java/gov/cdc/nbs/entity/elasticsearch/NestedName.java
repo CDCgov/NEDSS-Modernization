d115d0c4ed608378f097bc0e6ef45d704baaa270
Steven Urciuoli <steveu@Stevens-MacBook-Pro.local>
2023-02-06 01:24:09 -0600

    CNPT-642 Update ElasticSearch mappings/indexing to support phonetic analyzer/soundex

--- patient-search/api/src/main/java/gov/cdc/nbs/entity/elasticsearch/NestedName.java
+++ patient-search/api/src/main/java/gov/cdc/nbs/entity/elasticsearch/NestedName.java
@@ -11,7 +11,9 @@ import lombok.Setter;
 @Setter
 public class NestedName {
     private String firstNm;
+    private String firstNmSndx;
     private String lastNm;
+    private String lastNmSndx;
     private String middleNm;
     private String nmPrefix;
     private String nmSuffix;
