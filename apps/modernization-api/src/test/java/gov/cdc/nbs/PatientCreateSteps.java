d2ddc95f10b172241306e96221eecb5f2e86b0b9
Michael Peels <michaelpeels@Michael-Peels.local>
2023-02-09 20:13:48 -0500

    Add permission tests for create patient

--- apps/modernization-api/src/test/java/gov/cdc/nbs/PatientCreateSteps.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/PatientCreateSteps.java
@@ -1,5 +1,6 @@
 package gov.cdc.nbs;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -10,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -45,16 +47,29 @@ public class PatientCreateSteps {
 
     private String createPersonRequestId;
     private PatientInput input;
+    private AccessDeniedException accessDeniedException;
 
     @When("I send a create patient request")
     public void i_send_a_create_patient_request() {
         input = PersonUtil.convertToPatientInput(PersonMother.generateRandomPerson(1234));
-        createPersonRequestId = patientController.createPatient(input);
+        try {
+            createPersonRequestId = patientController.createPatient(input);
+        } catch (AccessDeniedException e) {
+            accessDeniedException = e;
+        }
+
+    }
+
+    @Then("I get an access denied exception")
+    public void i_get_an_access_denied_exception() {
+        assertNotNull(accessDeniedException);
+        assertNull(createPersonRequestId);
     }
 
     @Then("the patient create request is posted to kafka")
     public void the_patient_create_request_is_posted_to_kafka()
             throws InterruptedException, JsonMappingException, JsonProcessingException {
+        assertNull(accessDeniedException);
         assertNotNull(createPersonRequestId);
         boolean messageConsumed = consumer.getLatch().await(10, TimeUnit.SECONDS);
 
--- apps/modernization-api/src/test/resources/features/PatientCreate.feature
+++ apps/modernization-api/src/test/resources/features/PatientCreate.feature
@@ -1,9 +1,17 @@
 @patient_create
 Feature: Patient create
 
-  Background: 
-    Given I have the authorities: "FIND-PATIENT,ADD-PATIENT" for the jurisdiction: "ALL" and program area: "STD"
-
   Scenario: I can send a create patient request
+    Given I have the authorities: "FIND-PATIENT,ADD-PATIENT" for the jurisdiction: "ALL" and program area: "STD"
     When I send a create patient request
     Then the patient create request is posted to kafka
+
+  Scenario: I cannot send a create patient request without the FIND-PATIENT permission
+    Given I have the authorities: "ADD-PATIENT" for the jurisdiction: "ALL" and program area: "STD"
+    When I send a create patient request
+    Then I get an access denied exception
+
+  Scenario: I cannot send a create patient request without the ADD-PATIENT permission
+    Given I have the authorities: "FIND-PATIENT" for the jurisdiction: "ALL" and program area: "STD"
+    When I send a create patient request
+    Then I get an access denied exception
