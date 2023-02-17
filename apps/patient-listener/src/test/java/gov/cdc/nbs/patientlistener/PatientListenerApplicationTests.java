ad31bad9f19ae61db74ef3d64030f36fdc183ea9
Archit Dasika <115642773+adasika-cdc@users.noreply.github.com>
2023-02-17 05:33:33 -0800

    Moved test cases to patient-listener

--- apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/PatientListenerApplicationTests.java
+++ apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/PatientListenerApplicationTests.java
@@ -2,10 +2,14 @@ package gov.cdc.nbs.patientlistener;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.beans.Transient;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+
+import gov.cdc.nbs.entity.odse.Person;
 
 @SpringBootTest
 class PatientListenerApplicationTests {
@@ -16,6 +20,18 @@ class PatientListenerApplicationTests {
 	@Test
 	void contextLoads() {
 		assertNotNull(containerFactory);
+	}
+
+	@Test 
+	void findId() throws Exception {
+		Person person = containerFactory.findById(1000);
+		assertEquals(person.getNames());
+	}
+
+	@Test
+	void deletePatient() throws Exception {
+		Person person = containerFactory.delete(1001);
+		assertNull(containerFactory.findById(1001));
 	}
 
 }
