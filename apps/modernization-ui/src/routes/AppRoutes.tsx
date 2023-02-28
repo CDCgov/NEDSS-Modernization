7a0df6378ffef825e37fe31fe9aac2ea7ffb0218
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2023-02-28 12:50:15 -0800

    Patient profile testing (#124)
    * test cases for patient profile components
    
    * Adding remaining testing for patient profile
    
    * Fixing test case for patient profile
    
    ---------
    
    Co-authored-by: Michael Peels <109251240+mpeels@users.noreply.github.com>

--- apps/modernization-ui/src/routes/AppRoutes.tsx
+++ apps/modernization-ui/src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { AdvancedSearch } from '../pages/advancedSearch/AdvancedSearch';
 import { Login } from '../pages/login/Login';
 import { PatientProfile } from '../pages/patientProfile/PatientProfile';
+import AddPatient from '../pages/addPatient/AddPatient';
 
 export const AppRoutes = () => {
     return (
@@ -10,6 +11,7 @@ export const AppRoutes = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/advanced-search" element={<AdvancedSearch />} />
                 <Route path="/patient-profile/:id" element={<PatientProfile />} />
+                <Route path="/add-patient" element={<AddPatient />} />
                 <Route path="*" element={<Navigate to="/advanced-search" />} />
                 <Route path="/" element={<Navigate to="/advanced-search" />} />
             </Route>
