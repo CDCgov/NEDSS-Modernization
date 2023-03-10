5ba6c913cf0347fc39d99969558a1e0ffbe9d5ef
John Lee <jlee@enquizit.com>
2023-03-10 16:56:40 -0600

    CNFT1-666 Login page redirect

--- apps/modernization-ui/src/routes/AppRoutes.tsx
+++ apps/modernization-ui/src/routes/AppRoutes.tsx
@@ -3,18 +3,24 @@ import { AdvancedSearch } from '../pages/advancedSearch/AdvancedSearch';
 import { Login } from '../pages/login/Login';
 import { PatientProfile } from '../pages/patientProfile/PatientProfile';
 import AddPatient from '../pages/addPatient/AddPatient';
+import { useContext } from 'react';
+import { UserContext } from 'providers/UserContext';
 
 export const AppRoutes = () => {
+    const { state } = useContext(UserContext);
+
     return (
         <Routes>
-            <Route path="/">
-                <Route path="/login" element={<Login />} />
-                <Route path="/advanced-search/:searchType?" element={<AdvancedSearch />} />
-                <Route path="/patient-profile/:id" element={<PatientProfile />} />
-                <Route path="/add-patient" element={<AddPatient />} />
-                <Route path="*" element={<Navigate to="/advanced-search" />} />
-                <Route path="/" element={<Navigate to="/advanced-search" />} />
-            </Route>
+            {state.isLoggedIn && state.isLoginPending && (
+                <>
+                    <Route path="/advanced-search/:searchType?" element={<AdvancedSearch />} />
+                    <Route path="/patient-profile/:id" element={<PatientProfile />} />
+                    <Route path="/add-patient" element={<AddPatient />} />
+                    <Route path="*" element={<Navigate to="/advanced-search" />} />
+                    <Route path="/" element={<Navigate to="/advanced-search" />} />
+                </>
+            )}
+            <Route path="*" element={<Login />} />
         </Routes>
     );
 };
