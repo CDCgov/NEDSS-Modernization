0ebd96889a9c0ae600c845c3d9e657d65005999e
Adam Loup <aloup@enquizit.com>
2023-03-17 15:50:39 -0500

    removes unused imports

--- apps/patient-listener/src/main/java/gov/cdc/nbs/patientlistener/config/KafkaConfig.java
+++ apps/patient-listener/src/main/java/gov/cdc/nbs/patientlistener/config/KafkaConfig.java
@@ -8,7 +8,6 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
