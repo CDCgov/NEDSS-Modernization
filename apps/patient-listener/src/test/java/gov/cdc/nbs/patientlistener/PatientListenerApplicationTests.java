c28f8fac03813d3e88c34f03c670f9c364c4a023
Adam Loup <aloup@enquizit.com>
2023-03-17 16:02:07 -0500

    allows PatientListenerApplicationTests to resolve all Configurations

--- apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/PatientListenerApplicationTests.java
+++ apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/PatientListenerApplicationTests.java
@@ -1,7 +1,5 @@
 package gov.cdc.nbs.patientlistener;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,18 +7,18 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
-import gov.cdc.nbs.patientlistener.config.KafkaConfig;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest(classes = {KafkaConfig.class})
+@SpringBootTest
 @ActiveProfiles("test")
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class PatientListenerApplicationTests {
 
-    @Autowired
-    ConcurrentKafkaListenerContainerFactory<String, String> containerFactory;
+  @Autowired
+  ConcurrentKafkaListenerContainerFactory<String, String> containerFactory;
 
-    @Test
-    void contextLoads() {
-        assertNotNull(containerFactory);
-    }
+  @Test
+  void contextLoads() {
+    assertNotNull(containerFactory);
+  }
 }
