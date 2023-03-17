96b4f1dd54b1c32660b483796fafb5b2e4b98868
Adam Loup <aloup@enquizit.com>
2023-03-17 18:00:56 -0500

    fixes test ensuring that kafka beans are loaded

--- apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/KafkaConfigTest.java
+++ apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/KafkaConfigTest.java
@@ -1,7 +1,9 @@
 package gov.cdc.nbs.patientlistener;
 
+import gov.cdc.nbs.patientlistener.config.KafkaConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.test.context.EmbeddedKafka;
@@ -9,16 +11,18 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+
+@AutoConfigureJson
+@SpringBootTest(classes = KafkaConfig.class)
 @ActiveProfiles("test")
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
-class PatientListenerApplicationTests {
+class KafkaConfigTest {
 
   @Autowired
   ConcurrentKafkaListenerContainerFactory<String, String> containerFactory;
 
   @Test
-  void contextLoads() {
+  void should_resolve_beans_for_kafka_resources() {
     assertNotNull(containerFactory);
   }
 }
