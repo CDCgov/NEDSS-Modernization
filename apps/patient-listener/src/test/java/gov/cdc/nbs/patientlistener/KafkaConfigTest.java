0b783977ff2040a61973d471c0c6029fe37f2ad2
Michael Peels <michaelpeels@Michael-Peels.local>
2023-03-19 22:59:23 -0400

    Fix listener test failure

--- apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/KafkaConfigTest.java
+++ apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/KafkaConfigTest.java
@@ -3,13 +3,16 @@ package gov.cdc.nbs.patientlistener;
 import static org.junit.Assert.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
+import gov.cdc.nbs.patientlistener.config.KafkaConfig;
 
 
-@SpringBootTest()
+@AutoConfigureJson
+@SpringBootTest(classes = KafkaConfig.class)
 @ActiveProfiles("test")
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class KafkaConfigTest {
