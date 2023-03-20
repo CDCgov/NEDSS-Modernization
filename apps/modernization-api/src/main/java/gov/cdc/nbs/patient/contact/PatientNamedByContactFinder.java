2ddb6e63d30b3111763fcf0266a6b9c6c1727005
Adam Loup <aloup@enquizit.com>
2023-03-20 12:54:02 -0500

    adds associatedWith to contacts named by patient

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/contact/PatientNamedByContactFinder.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/contact/PatientNamedByContactFinder.java
@@ -18,114 +18,114 @@ import java.util.Objects;
 @Component
 class PatientNamedByContactFinder {
 
-    private static final QCtContact TRACING = new QCtContact("tracing");
-    private static final QPerson SUBJECT = new QPerson("subject");
-    private static final QPerson CONTACT = new QPerson("contact");
-    private static final QPublicHealthCase INVESTIGATION = new QPublicHealthCase("investigation");
+  private static final QCtContact TRACING = new QCtContact("tracing");
+  private static final QPerson SUBJECT = new QPerson("subject");
+  private static final QPerson CONTACT = new QPerson("contact");
+  private static final QPublicHealthCase INVESTIGATION = new QPublicHealthCase("investigation");
 
-    private static final QInterview INTERVIEW = new QInterview("interview");
+  private static final QInterview INTERVIEW = new QInterview("interview");
 
-    private static final Coalesce<Instant> NAMED_ON = new Coalesce<>(Instant.class)
-            .add(TRACING.namedOnDate)
-            .add(INTERVIEW.interviewDate)
-            .add(TRACING.addTime);
+  private static final Coalesce<Instant> NAMED_ON = new Coalesce<>(Instant.class)
+      .add(TRACING.namedOnDate)
+      .add(INTERVIEW.interviewDate)
+      .add(TRACING.addTime);
 
-    private final JPAQueryFactory factory;
+  private final JPAQueryFactory factory;
 
-    PatientNamedByContactFinder(final JPAQueryFactory factory) {
-        this.factory = factory;
-    }
+  PatientNamedByContactFinder(final JPAQueryFactory factory) {
+    this.factory = factory;
+  }
 
-    List<PatientContacts.NamedByContact> find(final long patient) {
-        return this.factory.selectDistinct(
-                        TRACING.id,
-                        TRACING.addTime,
-                        TRACING.localId,
-                        SUBJECT.id,
-                        SUBJECT.nmPrefix,
-                        SUBJECT.firstNm,
-                        SUBJECT.lastNm,
-                        SUBJECT.nmSuffix,
-                        INVESTIGATION.id,
-                        INVESTIGATION.localId,
-                        INVESTIGATION.cdDescTxt,
-                        NAMED_ON
-                ).from(TRACING)
-                .join(INVESTIGATION).on(
-                        INVESTIGATION.id.eq(TRACING.subjectEntityPhcUid.id)
-                )
-                .join(CONTACT).on(
-                        CONTACT.id.eq(TRACING.contactNBSEntityUid.id)
-                )
-                .join(SUBJECT).on(
-                        TRACING.subjectNBSEntityUid.id.eq(SUBJECT.id)
-                )
-                .leftJoin(INTERVIEW).on(
-                        INTERVIEW.id.eq(TRACING.namedDuringInterviewUid)
-                )
-                .where(TRACING.recordStatusCd.eq("ACTIVE"),
-                        CONTACT.personParentUid.id.eq(patient)
-                )
-                .fetch()
-                .stream()
-                .map(this::map)
-                .toList();
-    }
+  List<PatientContacts.NamedByContact> find(final long patient) {
+    return this.factory.selectDistinct(
+            TRACING.id,
+            TRACING.addTime,
+            TRACING.localId,
+            SUBJECT.id,
+            SUBJECT.nmPrefix,
+            SUBJECT.firstNm,
+            SUBJECT.lastNm,
+            SUBJECT.nmSuffix,
+            INVESTIGATION.id,
+            INVESTIGATION.localId,
+            INVESTIGATION.cdDescTxt,
+            NAMED_ON
+        ).from(TRACING)
+        .join(INVESTIGATION).on(
+            INVESTIGATION.id.eq(TRACING.subjectEntityPhcUid.id)
+        )
+        .join(CONTACT).on(
+            CONTACT.id.eq(TRACING.contactNBSEntityUid.id)
+        )
+        .join(SUBJECT).on(
+            TRACING.subjectNBSEntityUid.id.eq(SUBJECT.id)
+        )
+        .leftJoin(INTERVIEW).on(
+            INTERVIEW.id.eq(TRACING.namedDuringInterviewUid)
+        )
+        .where(TRACING.recordStatusCd.eq("ACTIVE"),
+            CONTACT.personParentUid.id.eq(patient)
+        )
+        .fetch()
+        .stream()
+        .map(this::map)
+        .toList();
+  }
 
-    private PatientContacts.NamedByContact map(final Tuple tuple) {
-        Long identifier = Objects.requireNonNull(tuple.get(TRACING.id));
-        Instant createdOn = tuple.get(TRACING.addTime);
-        String condition = tuple.get(INVESTIGATION.cdDescTxt);
+  private PatientContacts.NamedByContact map(final Tuple tuple) {
+    Long identifier = Objects.requireNonNull(tuple.get(TRACING.id));
+    Instant createdOn = tuple.get(TRACING.addTime);
+    String condition = tuple.get(INVESTIGATION.cdDescTxt);
 
-        PatientContacts.NamedContact contact = mapContact(tuple);
+    PatientContacts.NamedContact contact = mapContact(tuple);
 
-        Instant namedOn = tuple.get(NAMED_ON);
+    Instant namedOn = tuple.get(NAMED_ON);
 
-        String event = tuple.get(TRACING.localId);
+    String event = tuple.get(TRACING.localId);
 
-        PatientContacts.Investigation investigation = mapInvestigation(tuple);
+    PatientContacts.Investigation investigation = mapInvestigation(tuple);
 
-        return new PatientContacts.NamedByContact(
-                identifier,
-                createdOn,
-                condition,
-                contact,
-                namedOn,
-                event,
-                investigation
-        );
-    }
+    return new PatientContacts.NamedByContact(
+        identifier,
+        createdOn,
+        condition,
+        contact,
+        namedOn,
+        event,
+        investigation
+    );
+  }
 
-    private PatientContacts.NamedContact mapContact(final Tuple tuple) {
-        Long identifier = tuple.get(SUBJECT.id);
-        String prefix = tuple.get(SUBJECT.nmPrefix);
-        String first = tuple.get(SUBJECT.firstNm);
-        String last = tuple.get(SUBJECT.lastNm);
-        Suffix suffix = tuple.get(SUBJECT.nmSuffix);
+  private PatientContacts.NamedContact mapContact(final Tuple tuple) {
+    Long identifier = tuple.get(SUBJECT.id);
+    String prefix = tuple.get(SUBJECT.nmPrefix);
+    String first = tuple.get(SUBJECT.firstNm);
+    String last = tuple.get(SUBJECT.lastNm);
+    Suffix suffix = tuple.get(SUBJECT.nmSuffix);
 
-        String name = NameRenderer.render(
-                prefix,
-                first,
-                last,
-                suffix
-        );
+    String name = NameRenderer.render(
+        prefix,
+        first,
+        last,
+        suffix
+    );
 
-        return new PatientContacts.NamedContact(
-                Objects.requireNonNull(identifier, "A subject identifier is required."),
-                name
-        );
-    }
+    return new PatientContacts.NamedContact(
+        Objects.requireNonNull(identifier, "A subject identifier is required."),
+        name
+    );
+  }
 
-    private PatientContacts.Investigation mapInvestigation(final Tuple tuple) {
-        Long identifier =
-                Objects.requireNonNull(tuple.get(INVESTIGATION.id), "An investigation identifier is required.");
-        String local = tuple.get(INVESTIGATION.localId);
-        String condition = tuple.get(INVESTIGATION.cdDescTxt);
+  private PatientContacts.Investigation mapInvestigation(final Tuple tuple) {
+    Long identifier =
+        Objects.requireNonNull(tuple.get(INVESTIGATION.id), "An investigation identifier is required.");
+    String local = tuple.get(INVESTIGATION.localId);
+    String condition = tuple.get(INVESTIGATION.cdDescTxt);
 
-        return new PatientContacts.Investigation(
-                identifier,
-                local,
-                condition
-        );
-    }
+    return new PatientContacts.Investigation(
+        identifier,
+        local,
+        condition
+    );
+  }
 }
