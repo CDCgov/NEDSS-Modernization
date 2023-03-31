ee8099709aab038210b8730af3ff11080e5432e6
Archit Dasika <115642773+adasika-cdc@users.noreply.github.com>
2023-03-31 02:09:32 -0700

    Microservice for CRUD -questionbank

---
+++ apps/questionbank/src/main/java/gov/cdc/nbs/questionbank/QuestionBankApplication.java
@@ -0,0 +1,13 @@
+package gov.cdc.nbs.questionbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionBankApplication.class, args);
+	}
+
+}
---
+++ apps/questionbank/src/test/java/gov/cdc/nbs/questionbank/QuestionBankApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.cdc.nbs.questionbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
