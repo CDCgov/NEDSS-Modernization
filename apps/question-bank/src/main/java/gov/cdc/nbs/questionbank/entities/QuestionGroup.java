600b78849346889b1ee3f4525f5cbdc17ad0842f
Archit Dasika <115642773+adasika-cdc@users.noreply.github.com>
2023-04-14 09:41:51 -0700

    Cnft2 158/159 microservice database + flyway migration setup + jpa entities  (#179)
    * Microservice for CRUD -questionbank
    
    * Added JPA Entities + Flyway Config
    
    * Changes to question bank microservice
    
    * Trim down question_bank entities. Move entities into microservice. Add create question_bank databse script to db restore. Configure flyway
    
    * Create  schema
    
    * Add h2 database. Rename schema to 'MODERNIZATION'
    
    * Add question-bank to coverage report
    
    * use 'dbo' schema
    
    ---------
    
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels.local>

---
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/entities/QuestionGroup.java
@@ -0,0 +1,40 @@
+package gov.cdc.nbs.questionbank.entities;
+
+
+
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "question_group", catalog = "question_bank")
+public class QuestionGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "label")
+    private String label;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "question_group_questions",
+            joinColumns = @JoinColumn(name = "question_group_id"),
+            inverseJoinColumns = @JoinColumn(name = "question_id"))
+    private Set<Question> questions;
+
+}
---
+++ apps/question-bank/src/main/resources/db/migration/V0001__init.sql
@@ -0,0 +1,87 @@
+CREATE SEQUENCE hibernate_sequence
+    START WITH 1
+    INCREMENT BY 1;
+
+CREATE TABLE answer (
+    id bigint IDENTITY NOT NULL,
+    display varchar(255) NOT NULL,
+    val varchar(255) NOT NULL,
+    answer_set_id bigint NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE answer_set (
+    id bigint IDENTITY NOT NULL,
+    description varchar(200),
+    name varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE question (
+    id bigint IDENTITY NOT NULL,
+    allow_future_dates bit,
+    label varchar(300),
+    max_length int,
+    max_value int,
+    min_value int,
+    multiselect bit,
+    placeholder varchar(100),
+    required bit,
+    tooltip varchar(200),
+    units_set bigint,
+    default_answer_id bigint,
+    answer_set_id bigint,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE question_group (
+    id bigint IDENTITY NOT NULL,
+    label varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE question_group_questions (
+    question_group_id bigint NOT NULL,
+    question_id bigint NOT NULL,
+    PRIMARY KEY (question_group_id, question_id)
+);
+
+CREATE TABLE questionnaire (
+    id bigint NOT NULL,
+    condition_id bigint NOT NULL,
+    questionnaire_type varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE questionnaire_question_group (
+    questionnaire_id bigint NOT NULL,
+    question_group_id bigint NOT NULL,
+    PRIMARY KEY (questionnaire_id, question_group_id)
+);
+
+ALTER TABLE question_group_questions
+    ADD CONSTRAINT UK_by81ny1darfq17o2eea443ny0 UNIQUE (question_id);
+
+ALTER TABLE questionnaire_question_group
+    ADD CONSTRAINT UK_efuw6119pydmmj1as3bqos947 UNIQUE (question_group_id);
+
+ALTER TABLE answer
+    ADD CONSTRAINT FKpiyr5su8j772rgahh2m98cap FOREIGN KEY (answer_set_id) REFERENCES answer_set;
+
+ALTER TABLE question
+    ADD CONSTRAINT FKssg1rxfqipf5x1qk4ifa36t8x FOREIGN KEY (default_answer_id) REFERENCES answer;
+
+ALTER TABLE question
+    ADD CONSTRAINT FKjmqrcbi9bbs36d0v0nxbkbdo0 FOREIGN KEY (answer_set_id) REFERENCES answer_set;
+
+ALTER TABLE question_group_questions
+    ADD CONSTRAINT FKaxjg07cg3ax17o72tlvyx3isl FOREIGN KEY (question_id) REFERENCES question;
+
+ALTER TABLE question_group_questions
+    ADD CONSTRAINT FKa6xdvng7g1qxgb6b33kpmrs2a FOREIGN KEY (question_group_id) REFERENCES question_group;
+
+ALTER TABLE questionnaire_question_group
+    ADD CONSTRAINT FK35sl2r54gim617rn8ls0oa1mv FOREIGN KEY (question_group_id) REFERENCES question_group;
+
+ALTER TABLE questionnaire_question_group
+    ADD CONSTRAINT FK1i40d11ahid347m1nw5tefdq4 FOREIGN KEY (questionnaire_id) REFERENCES questionnaire;
