894fb7a64d4e2871b92a28a404e4fe332e5b88d1
Michael Peels <michaelpeels@Michael-Peels.local>
2023-04-20 11:24:02 -0400

    Make image configurable for test db. Update sonar.yaml to allow connecting to ecr

--- apps/modernization-api/src/test/java/gov/cdc/nbs/containers/database/NbsDatabaseContainer.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/containers/database/NbsDatabaseContainer.java
@@ -4,12 +4,14 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 class NbsDatabaseContainer extends GenericContainer<NbsDatabaseContainer> {
+    private static final String IMAGE =
+            "501715613725.dkr.ecr.us-east-1.amazonaws.com/cdc-nbs-modernization/modernization-test-db:1.0.7-SNAPSHOT.lb";
 
     private static final int DEFAULT_PORT = 1433;
 
     public NbsDatabaseContainer() {
-        super(DockerImageName.parse("cdc-sandbox-test-db"));
-
+        super(DockerImageName
+                .parse(IMAGE));
         addExposedPorts(DEFAULT_PORT);
     }
 
@@ -17,9 +19,8 @@ class NbsDatabaseContainer extends GenericContainer<NbsDatabaseContainer> {
         String server = getHost();
         Integer mappedPort = getMappedPort(DEFAULT_PORT);
         return String.format(
-            "jdbc:sqlserver://%s:%d;database=nbs_odse;encrypt=true;trustServerCertificate=true;",
-            server,
-            mappedPort
-        );
+                "jdbc:sqlserver://%s:%d;database=nbs_odse;encrypt=true;trustServerCertificate=true;",
+                server,
+                mappedPort);
     }
 }
