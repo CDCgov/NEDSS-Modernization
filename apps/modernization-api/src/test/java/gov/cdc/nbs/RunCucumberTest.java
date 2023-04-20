ba44794b8b52da2bb2965799072e43ba12a39bb0
Michael Peels <109251240+mpeels@users.noreply.github.com>
2023-04-20 11:22:48 -0400

    create test db (#201)
    * create test db
    
    * specify azure version
    
    * EmbeddedNbsDatabase
    
    * workflow update
    
    * change branch
    
    * run for new path
    
    * Suppress warning, update build yml to remove testing values.
    
    ---------
    
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels.local>
    Co-authored-by: Adam Loup <aloup@enquizit.com>

--- apps/modernization-api/src/test/java/gov/cdc/nbs/RunCucumberTest.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/RunCucumberTest.java
@@ -1,13 +1,10 @@
 package gov.cdc.nbs;
 
-import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import javax.transaction.Transactional;
-
+import gov.cdc.nbs.containers.NbsElasticsearchContainer;
+import gov.cdc.nbs.containers.database.EmbeddedNbsDatabase;
+import gov.cdc.nbs.patient.PatientController;
 import gov.cdc.nbs.patient.identifier.PatientLocalIdentifierGeneratorTestConfiguration;
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.Test;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
@@ -25,9 +22,12 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import gov.cdc.nbs.containers.NbsElasticsearchContainer;
-import gov.cdc.nbs.patient.PatientController;
-import io.cucumber.spring.CucumberContextConfiguration;
+import javax.transaction.Transactional;
+
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -44,6 +44,7 @@ import io.cucumber.spring.CucumberContextConfiguration;
 @Transactional
 @Rollback(false)
 @Testcontainers
+@EmbeddedNbsDatabase
 @EmbeddedKafka(
     partitions = 1,
     topics = {"patient"}
