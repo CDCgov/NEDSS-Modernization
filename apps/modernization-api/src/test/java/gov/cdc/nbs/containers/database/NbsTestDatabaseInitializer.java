ff1a7cc3cec8ebad19a03bae457c1134e76ee71c
Michael Peels <michaelpeels@Michael-Peels.local>
2023-04-21 16:16:58 -0400

    configurable database image

--- apps/modernization-api/src/test/java/gov/cdc/nbs/containers/database/NbsTestDatabaseInitializer.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/containers/database/NbsTestDatabaseInitializer.java
@@ -10,24 +10,22 @@ class NbsTestDatabaseInitializer implements ApplicationContextInitializer<Config
 
     @Override
     public void initialize(final ConfigurableApplicationContext context) {
-        NbsDatabaseContainer container = Holder.INSTANCE;
+        NbsDatabaseContainer container =
+                Holder.initialize(context.getEnvironment().getProperty("testing.database.image"));
 
         TestPropertyValues.of("spring.datasource.url=" + container.url())
-            .applyTo(context.getEnvironment());
+                .applyTo(context.getEnvironment());
 
     }
 
     private final static class Holder {
 
-        private static final NbsDatabaseContainer INSTANCE = initialize();
-
-        @SuppressWarnings({ "resource" })
-        private static NbsDatabaseContainer initialize() {
+        @SuppressWarnings({"resource"})
+        private static NbsDatabaseContainer initialize(final String image) {
             NbsDatabaseContainer container = new NbsDatabaseContainer()
-                .withImagePullPolicy(PullPolicy.defaultPolicy());
+                    .withImagePullPolicy(PullPolicy.defaultPolicy());
 
             container.start();
-
             return container;
         }
 
