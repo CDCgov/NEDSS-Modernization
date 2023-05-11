674137d70ebdfb966981af6a1ec39d157bdf9b3f
Steven Urciuoli <steveu@Stevens-MacBook-Pro.local>
2023-05-11 10:34:28 -0500

    more coverage

--- apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/request/update/PatientUpdateRequestHandlerTest.java
+++ apps/patient-listener/src/test/java/gov/cdc/nbs/patientlistener/request/update/PatientUpdateRequestHandlerTest.java
@@ -18,9 +18,15 @@ import org.mockito.MockitoAnnotations;
 import gov.cdc.nbs.entity.odse.Person;
 import gov.cdc.nbs.message.enums.Deceased;
 import gov.cdc.nbs.message.enums.Gender;
+import gov.cdc.nbs.message.enums.Suffix;
+import gov.cdc.nbs.message.patient.event.AddNameData;
+import gov.cdc.nbs.message.patient.event.DeleteNameData;
+import gov.cdc.nbs.message.patient.event.UpdateAdministrativeData;
 import gov.cdc.nbs.message.patient.event.UpdateGeneralInfoData;
 import gov.cdc.nbs.message.patient.event.UpdateMortalityData;
+import gov.cdc.nbs.message.patient.event.UpdateNameData;
 import gov.cdc.nbs.message.patient.event.UpdateSexAndBirthData;
+import gov.cdc.nbs.message.patient.input.PatientInput.NameUseCd;
 import gov.cdc.nbs.patientlistener.request.PatientNotFoundException;
 import gov.cdc.nbs.patientlistener.request.UserNotAuthorizedException;
 import gov.cdc.nbs.patientlistener.request.PatientRequestStatusProducer;
@@ -284,6 +290,118 @@ class PatientUpdateRequestHandlerTest {
                 "multiple birth",
                 "sex unknown",
                 "current age",
+                Instant.now());
+    }
+
+    @Test
+    void testAdministrativeUpdateSuccess() {
+        var data = getAdministrativeData();
+
+        // set valid mock returns
+        when(userService.isAuthorized(eq(321L), Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+        when(personRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(new Person(123L, "localId")));
+        when(patientUpdater.update(Mockito.any(), eq(data))).thenAnswer(i -> i.getArguments()[0]);
+
+        // call handle update gen info
+        updateHandler.handlePatientAdministrativeUpdate(data);
+
+        // verify save requests called, success status sent
+        verify(patientUpdater, times(1)).update(Mockito.any(), eq(data));
+        verify(elasticsearchPersonRepository, times(1)).save(Mockito.any());
+        verify(statusProducer, times(1)).successful(eq("RequestId"), Mockito.anyString(), eq(123L));
+    }
+
+    private UpdateAdministrativeData getAdministrativeData() {
+        return new UpdateAdministrativeData(
+                123L,
+                "RequestId",
+                321L,
+                Instant.now(),
+                "Administrative Data 1");
+    }
+
+    @Test
+    void testAddNameSuccess() {
+        var data = getAddNameData();
+
+        // set valid mock returns
+        when(userService.isAuthorized(eq(321L), Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+        when(personRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(new Person(123L, "localId")));
+        when(patientUpdater.update(Mockito.any(), eq(data))).thenAnswer(i -> i.getArguments()[0]);
+
+        // call handle update gen info
+        updateHandler.handlePatientNameAdd(data);
+
+        // verify save requests called, success status sent
+        verify(patientUpdater, times(1)).update(Mockito.any(), eq(data));
+        verify(elasticsearchPersonRepository, times(1)).save(Mockito.any());
+        verify(statusProducer, times(1)).successful(eq("RequestId"), Mockito.anyString(), eq(123L));
+    }
+
+    private AddNameData getAddNameData() {
+        return new AddNameData(123L,
+                "RequestId",
+                321L,
+                Instant.now(),
+                "First Name",
+                "Middle Name",
+                "Last Name",
+                Suffix.III,
+                NameUseCd.L);
+    }
+
+    @Test
+    void testUpdateNameSuccess() {
+        var data = getUpdateNameData();
+
+        // set valid mock returns
+        when(userService.isAuthorized(eq(321L), Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+        when(personRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(new Person(123L, "localId")));
+        when(patientUpdater.update(Mockito.any(), eq(data))).thenAnswer(i -> i.getArguments()[0]);
+
+        // call handle update gen info
+        updateHandler.handlePatientNameUpdate(data);
+
+        // verify save requests called, success status sent
+        verify(patientUpdater, times(1)).update(Mockito.any(), eq(data));
+        verify(elasticsearchPersonRepository, times(1)).save(Mockito.any());
+        verify(statusProducer, times(1)).successful(eq("RequestId"), Mockito.anyString(), eq(123L));
+    }
+
+    private UpdateNameData getUpdateNameData() {
+        return new UpdateNameData(123L,
+                (short) 1,
+                "RequestId",
+                321L,
+                Instant.now(),
+                "First Name",
+                "Middle Name",
+                "Last Name",
+                Suffix.III,
+                NameUseCd.L.toString());
+    }
+
+    @Test
+    void testDeleteNameSuccess() {
+        var data = getDeleteNameData();
+
+        // set valid mock returns
+        when(userService.isAuthorized(eq(321L), Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+        when(personRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(new Person(123L, "localId")));
+        when(patientUpdater.update(Mockito.any(), eq(data))).thenAnswer(i -> i.getArguments()[0]);
+
+        // call handle update gen info
+        updateHandler.handlePatientNameDelete(data.requestId(), data.patientId(), data.personNameSeq(), data.updatedBy());
+
+        // verify save requests called, success status sent
+        verify(statusProducer, times(1)).successful(eq("RequestId"), Mockito.anyString(), eq(321L));
+    }
+
+    private DeleteNameData getDeleteNameData() {
+        return new DeleteNameData(123L,
+                (short) 1,
+                "RequestId",
+                321L,
                 Instant.now());
     }
 }
