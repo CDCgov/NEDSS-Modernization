4a1001381ed77c8aaee83d676ee0ae0982abf620
Ernst Melias <emelias@enquizit.com>
2023-05-11 14:23:55 -0400

    CNFT2-407 Implement JWT authentication for question bank

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/QuestionBankApplication.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/QuestionBankApplication.java
@@ -3,7 +3,11 @@ package gov.cdc.nbs.questionbank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+
 @SpringBootApplication
+@EnableOpenApi
 public class QuestionBankApplication {
 
     public static void main(String[] args) {
