06b7e8af04b816fcc51862ed643379efa7af53ec
Adam Loup <aloup@enquizit.com>
2023-05-15 14:12:51 -0500

    Adds shortId to PersonParticipation

---
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/labreport/PersonParticipationPatientShortIdentifierResolver.java
@@ -0,0 +1,25 @@
+package gov.cdc.nbs.labreport;
+
+import gov.cdc.nbs.entity.elasticsearch.ElasticsearchPersonParticipation;
+import gov.cdc.nbs.patient.identifier.PatientShortIdentifierResolver;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+
+import java.util.OptionalLong;
+
+@Controller
+class PersonParticipationPatientShortIdentifierResolver {
+
+    private final PatientShortIdentifierResolver resolver;
+
+    PersonParticipationPatientShortIdentifierResolver(final PatientShortIdentifierResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    @SchemaMapping(typeName = "PersonParticipation",field = "shortId")
+    @PreAuthorize("hasAuthority('VIEWWORKUP-PATIENT')")
+    OptionalLong resolve(final ElasticsearchPersonParticipation participation) {
+        return this.resolver.resolve(participation.getLocalId());
+    }
+}
