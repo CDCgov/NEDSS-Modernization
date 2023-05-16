7b9770890a3cdafd521fdf6d8130614b4effcb98
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2023-05-16 11:10:22 -0500

    CNFT1-853/ Patient Profile Lab Report Navigation (#235)
    * Patient Profile Lab Report Navigation
    
    * sonar

---
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/patient/profile/report/lab/TestLabReports.java
@@ -0,0 +1,8 @@
+package gov.cdc.nbs.patient.profile.report.lab;
+
+import gov.cdc.nbs.support.TestAvailable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestLabReports extends TestAvailable<Long> {
+}
