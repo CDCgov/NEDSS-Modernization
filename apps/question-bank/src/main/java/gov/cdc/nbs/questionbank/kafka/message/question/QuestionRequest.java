9c5e6e3a68c7b0e622fb13818971875556965b69
Muhammad Assad <116684885+massad22@users.noreply.github.com>
2023-05-26 13:40:45 -0400

    Update QuestionRequest.java

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/kafka/message/question/QuestionRequest.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/kafka/message/question/QuestionRequest.java
@@ -7,12 +7,11 @@ public sealed interface QuestionRequest extends QuestionBankRequest {
     record CreateTextQuestionRequest(
             String requestId,
             long questionId,
-            long userId,
             String label,
             String tooltip,
-            Integer maxLength,
+            int maxLength,
             String placeholder) implements QuestionRequest {
-        
+
     }
 
     record UpdateTextQuestionRequest(
