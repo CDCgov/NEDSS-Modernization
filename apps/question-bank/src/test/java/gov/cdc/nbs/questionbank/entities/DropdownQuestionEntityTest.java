37a61c388a5a860e84046a034f1e15287827b290
Michael Peels <michaelpeels@Michael-Peels.local>
2023-05-26 11:03:53 -0400

    Fix capitalization

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/entities/DropdownQuestionEntityTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/entities/DropdownQuestionEntityTest.java
@@ -3,7 +3,7 @@ package gov.cdc.nbs.questionbank.entities;
 import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
-class DropDownQuestionEntityTest {
+class DropdownQuestionEntityTest {
 
     @Test
     void should_return_dropdown() {
