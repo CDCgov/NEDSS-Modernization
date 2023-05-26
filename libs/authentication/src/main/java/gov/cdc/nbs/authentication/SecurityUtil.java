92a345988f086ead7d8e7c3edb142cc4d2c53d64
Ernst Melias <emelias@enquizit.com>
2023-05-26 18:27:09 -0400

    CNFT2-468 API delete question

--- libs/authentication/src/main/java/gov/cdc/nbs/authentication/SecurityUtil.java
+++ libs/authentication/src/main/java/gov/cdc/nbs/authentication/SecurityUtil.java
@@ -11,29 +11,4 @@ public class SecurityUtil {
     public static NbsUserDetails getUserDetails() {
         return (NbsUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
-
-    public static class Operations {
-        private Operations() {
-        }
-
-        public static final String ADD = "ADD";
-        public static final String ANY = "ANY";
-        public static final String VIEW = "VIEW";
-        public static final String EDIT = "EDIT";
-        public static final String FIND = "FIND";
-        public static final String MERGE = "MERGE";
-        public static final String CREATE = "CREATE";
-        public static final String DELETE = "DELETE";
-        public static final String REVIEW = "REVIEW";
-        public static final String MANAGE = "MANAGE";
-    }
-
-
-    public static class BusinessObjects {
-        private BusinessObjects() {
-        }
-
-        public static final String QUESTION= "QUESTION";
-    }
-
 }
