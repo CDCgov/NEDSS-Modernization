d93b5eb9fe9de2638b9496f74c1b2fca764a9efa
Jim Barrows <JIm.Barrows@gmail.com>
2023-05-26 13:44:31 -0500

    Wrote the BDD tests

--- apps/modernization-api/src/test/java/gov/cdc/nbs/PatientDataEntryDropDownSteps.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/PatientDataEntryDropDownSteps.java
@@ -1,20 +1,98 @@
 package gov.cdc.nbs;
 
+import gov.cdc.nbs.controller.CodeValueGeneralController;
+import gov.cdc.nbs.entity.srte.CodeValueGeneral;
+import gov.cdc.nbs.graphql.GraphQLPage;
+import gov.cdc.nbs.patient.DropDownValuesController;
+import gov.cdc.nbs.patient.KeyValuePair;
+import gov.cdc.nbs.patient.KeyValuePairResults;
+import gov.cdc.nbs.repository.NaicsIndustryCodeRepository;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.springframework.test.context.ActiveProfiles;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 
-@ActiveProfiles("test")
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PatientDataEntryDropDownSteps {
+
+    @Mock
+    private NaicsIndustryCodeRepository naicsIndustryCodeRepository;
+    private final DropDownValuesController dropDownValuesController = new DropDownValuesController(naicsIndustryCodeRepository);
+    private KeyValuePairResults actualKeyValuePairResults;
+
+    @Autowired
+    private CodeValueGeneralController codeValueGeneralController;
+    private GraphQLPage page = new GraphQLPage(100, 0);
+    ;
 
     @When("I want to retrieve {string}")
     public void iWantToRetrieve(String fieldName) {
-        System.out.println("fieldName: " + fieldName);
+        switch (fieldName) {
+            case "Name Suffix" -> actualKeyValuePairResults = dropDownValuesController.findNameSuffixes();
+            case "Gender" -> actualKeyValuePairResults = dropDownValuesController.findGenders();
+            case "Yes/No/Unknown" -> actualKeyValuePairResults = dropDownValuesController.findYesNoUnk();
+            case "Name Type" -> actualKeyValuePairResults = dropDownValuesController.findNameType();
+            case "Name Prefix" -> {
+                Page<CodeValueGeneral> allNamePrefixes = codeValueGeneralController.findAllNamePrefixes(page);
+                actualKeyValuePairResults = KeyValuePairResults.builder()
+                        .total(allNamePrefixes.getNumberOfElements())
+                        .content(allNamePrefixes.map(codeValueGeneral ->
+                                        KeyValuePair.builder()
+                                                .key(codeValueGeneral.getId().getCode())
+                                                .value(codeValueGeneral.getCodeShortDescTxt())
+                                                .build())
+                                .toList())
+                        .build();
+            }
+            case "Degree" -> {
+                Page<CodeValueGeneral> allDegrees = codeValueGeneralController.findAllDegrees(page);
+                actualKeyValuePairResults = KeyValuePairResults.builder()
+                        .total(allDegrees.getNumberOfElements())
+                        .content(allDegrees.map(codeValueGeneral ->
+                                        KeyValuePair.builder()
+                                                .key(codeValueGeneral.getId().getCode())
+                                                .value(codeValueGeneral.getCodeShortDescTxt())
+                                                .build())
+                                .toList())
+                        .build();
+            }
+            case "Address Type" -> {
+                Page<CodeValueGeneral> allAddressTypes = codeValueGeneralController.findAllAddressTypes(page);
+                actualKeyValuePairResults = KeyValuePairResults.builder()
+                        .total(allAddressTypes.getNumberOfElements())
+                        .content(allAddressTypes.map(codeValueGeneral ->
+                                        KeyValuePair.builder()
+                                                .key(codeValueGeneral.getId().getCode())
+                                                .value(codeValueGeneral.getCodeShortDescTxt())
+                                                .build())
+                                .toList())
+                        .build();
+            }
+        }
     }
 
     @Then("I get these key-value pairs:")
-    public void iGetTheseKeyValuePairs(DataTable dataTable) {
-        dataTable.asList().forEach(x -> System.out.println("Key value pair: " + x));
+    public void iGetTheseKeyValuePairs(DataTable expectedDataTable) {
+        List<Map<String, String>> expectedDataTableMaps = expectedDataTable.asMaps();
+        List<KeyValuePair> expectedResults = expectedDataTableMaps.stream()
+                .map(row -> KeyValuePair.builder()
+                        .key(row.get("Key"))
+                        .value(row.get("Value"))
+                        .build())
+                .toList();
+
+        expectedResults.forEach(expectedKeyValue -> assertTrue(actualKeyValuePairResults
+                        .getContent()
+                        .stream()
+                        .anyMatch(expectedKeyValue::equals),
+                String.format("Expected: (%s, %s), Actual: %s",
+                        expectedKeyValue.getKey(),
+                        expectedKeyValue.getValue(), actualKeyValuePairResults.getContent())));
     }
 }
--- apps/modernization-api/src/test/resources/features/PatientDataEntryDropDown.feature
+++ apps/modernization-api/src/test/resources/features/PatientDataEntryDropDown.feature
@@ -1,4 +1,4 @@
-# Created by jamesbarrows at 5/19/23
+## Created by jamesbarrows at 5/19/23
 @patient_entry_drop_downs
 Feature: Dropdown element retrieval for patient data entry
   AS A Data Entry user
@@ -13,34 +13,34 @@ Feature: Dropdown element retrieval for patient data entry
   Scenario: I can retrieve Name Suffixes
     When I want to retrieve "Name Suffix"
     Then I get these key-value pairs:
-      | Key   | Value             |
-      | "esq" | "Esquire"         |
-      | "II"  | "II / The Second" |
-      | "III" | "III / The Third" |
-      | "IV"  | "IV / The Fourth" |
-      | "jr"  | "Jr."             |
-      | "sr"  | "Sr."             |
-      | "v"   | "V / the Fifth"   |
+      | Key | Value           |
+      | ESQ | Esquire         |
+      | II  | II / The Second |
+      | III | III / The Third |
+      | IV  | IV / The Fourth |
+      | JR  | Jr.             |
+      | SR  | Sr.             |
+      | V   | V / The Fifth   |
 
   Scenario: I can retrieve Genders
     When I want to retrieve "Gender"
-    Then I get these key/value pairs:
-      | Key | Value    |
-      | "m" | "Male"   |
-      | "f" | "Female" |
-      | "u" | "Unkown" |
+    Then I get these key-value pairs:
+      | Key | Value   |
+      | M   | Male    |
+      | F   | Female  |
+      | U   | Unknown |
 
   Scenario: I can retrieve Yes/No/Unknown
-    When I want to retrieve "Yes/No/Unkown"
-    Then I get these key/value pairs
-      | Key   | Value     |
-      | "Y"   | "Yes"     |
-      | "N"   | "No"      |
-      | "UNK" | "Unknown" |
+    When I want to retrieve "Yes/No/Unknown"
+    Then I get these key-value pairs:
+      | Key     | Value   |
+      | YES     | Yes     |
+      | NO      | No      |
+      | UNKNOWN | Unknown |
 
   Scenario: I can retrieve Name Type
     When I want to retrieve "Name Type"
-    Then I get these key/value pairs
+    Then I get these key-value pairs:
       | Key | Value                  |
       | A   | Artist/Stage Name      |
       | AD  | Adopted Name           |
@@ -56,8 +56,8 @@ Feature: Dropdown element retrieval for patient data entry
       | S   | Coded Pseudo           |
 
   Scenario: I can retrieve Name Prefix
-    When I want to retrieve "Name Type"
-    Then I get these key/value pairs
+    When I want to retrieve "Name Prefix"
+    Then I get these key-value pairs:
       | Key  | Value      |
       | BRO  | Brother    |
       | BSHP | Bishop     |
@@ -81,37 +81,37 @@ Feature: Dropdown element retrieval for patient data entry
 
   Scenario: I can retrieve Degree
     When I want to retrieve "Degree"
-    Then I get these key/value pairs
+    Then I get these key-value pairs:
       | Key | Value |
-      | DO  | DO
-      | DRN | DRN
-      | DVM | DVM
-      | JD  | JD
-      | LLB | LLB
-      | LPN | LPN
-      | MA  | MA
-      | MBA | MBA
-      | MD  | MD
-      | MED | MED
-      | MPH | MPH
-      | MS  | MS
-      | MSN | MSN
-      | NP  | NP
-      | PA  | PA
-      | PHD | PhD
-      | RN  | RN
+      | DO  | DO    |
+      | DRN | DRN   |
+      | DVM | DVM   |
+      | JD  | JD    |
+      | LLB | LLB   |
+      | LPN | LPN   |
+      | MA  | MA    |
+      | MBA | MBA   |
+      | MD  | MD    |
+      | MED | MED   |
+      | MPH | MPH   |
+      | MS  | MS    |
+      | MSN | MSN   |
+      | NP  | NP    |
+      | PA  | PA    |
+      | PHD | PhD   |
+      | RN  | RN    |
 
-  Scenario: I can retrieve Degree
+  Scenario: I can retrieve Address Types
     When I want to retrieve "Address Type"
-    Then I get these key/value pairs
+    Then I get these key-value pairs:
       | Key  | Value                                        |
-      | O    | Office
-      | PF   | Prison - Federal
-      | PLOC | Jail
-      | PNS  | Prison/Correctional Facility - not specified
-      | PS   | Prison - State
-      | RH   | Registry Home
-      | RTF  | Residential treatment facility
-      | SHLT | Shelter - unspecified
-      | T    | Transitional Facility
-      | U    | Unknown
+      | O    | Office                                       |
+      | PF   | Prison - Federal                             |
+      | PLOC | Jail                                         |
+      | PNS  | Prison/Correctional Facility - not specified |
+      | PS   | Prison - State                               |
+      | RH   | Registry Home                                |
+      | RTF  | Residential treatment facility               |
+      | SHLT | Shelter - unspecified                        |
+      | T    | Transitional Facility                        |
+      | U    | Unknown                                      |
