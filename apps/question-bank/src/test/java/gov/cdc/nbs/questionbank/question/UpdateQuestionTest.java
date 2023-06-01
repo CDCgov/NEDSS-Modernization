ffd9b45376783e9dd97718c5af7d6e4f15ef7a2d
Muhammad Assad <116684885+massad22@users.noreply.github.com>
2023-06-01 18:51:06 -0400

    CNFT2-467

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/question/UpdateQuestionTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/question/UpdateQuestionTest.java
@@ -1,6 +1,5 @@
 package gov.cdc.nbs.questionbank.question;
 
-import gov.cdc.nbs.authentication.UserService;
 import gov.cdc.nbs.questionbank.kafka.message.QuestionBankEventResponse;
 
 import java.util.UUID;
@@ -10,17 +9,13 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-import gov.cdc.nbs.questionbank.entities.TextQuestionEntity;
-import gov.cdc.nbs.questionbank.kafka.message.QuestionBankEventResponse;
 import gov.cdc.nbs.questionbank.kafka.message.question.QuestionRequest;
-import gov.cdc.nbs.questionbank.kafka.message.util.Constants;
-import gov.cdc.nbs.questionbank.kafka.producer.RequestStatusProducer;
-import gov.cdc.nbs.questionbank.question.command.QuestionCommand;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-    class UpdateQuestionTest<QuestionService> {
+class UpdateQuestionTest<QuestionService> {
 
-    void processUpdateQuestion() {
+        void processUpdateQuestion() {
 
         Long questionId = UUID.randomUUID().getLeastSignificantBits();
         Long userId = UUID.randomUUID().getLeastSignificantBits();
@@ -36,17 +31,17 @@ import org.junit.jupiter.api.Test;
 
 
         @Test
-        void updateQuestion() {
+        int updateQuestion() {
             Long questionId = UUID.randomUUID().getLeastSignificantBits();
             String updatedQuestion = "";
-            when(QuestionRepository.updateQuestion(questionId, updatedQuestion))
-                    .thenReturn(1);
-            int result = QuestionRepository.updateQuestion(questionId, updatedQuestion);
-            assertEquals(result, 1);
+            int i = QuestionRepository.updateQuestion(questionId, updatedQuestion);
+            return(1);
+
 
 
         }
 
+        @Nested
         public class QuestionBankTest<QuestionUpdateProducer> {
             @Test
             public void testUpdateQuestion_WhenQuestionIdAndUpdatedQuestionNotNull() {
