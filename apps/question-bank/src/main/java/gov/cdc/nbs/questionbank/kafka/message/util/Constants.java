014a026d8f62b0b5d6d358113485eda2156e8ce5
Muhammad Assad <116684885+massad22@users.noreply.github.com>
2023-06-01 17:29:38 -0400

    CNFT2-467

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/kafka/message/util/Constants.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/kafka/message/util/Constants.java
@@ -3,20 +3,9 @@ package gov.cdc.nbs.questionbank.kafka.message.util;
 public class Constants {
 	// Prefix Application ID associated with Kafka requestID
     public static final String APP_ID = "QUESTION-BANK";
-    
-	// question deleted aka marked as inactive
-	public static final String DELETE_SUCCESS_MESSAGE = "Question was successfully made inactive";
-
-	// was unable to delete aka mark a question as inactive
-	public static final String DELETE_FAILURE_MESSAGE = "Was not able to inactive question";
 
 	public static final String UPDATE_SUCCESS_MESSAGE = "Question was successfully updated";
 
 	public static final String UPDATE_FAILURE_MESSAGE = "Was not able to update question";
-
-
-
-
-
 
 }
