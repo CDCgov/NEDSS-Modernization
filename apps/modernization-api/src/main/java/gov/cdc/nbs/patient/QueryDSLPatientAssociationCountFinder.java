c34657e3defb7bb48652568b69f62f3a71a183ae
Adam Loup <aloup@enquizit.com>
2023-06-05 13:10:19 -0500

    changed access

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/QueryDSLPatientAssociationCountFinder.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/QueryDSLPatientAssociationCountFinder.java
@@ -6,12 +6,12 @@ import gov.cdc.nbs.entity.odse.QPerson;
 import org.springframework.stereotype.Component;
 
 @Component
-public class QueryDSLPatientAssociationCountFinder implements PatientAssociationCountFinder {
+class QueryDSLPatientAssociationCountFinder implements PatientAssociationCountFinder {
 
     private final JPAQueryFactory factory;
     private final QPerson revision;
 
-    public QueryDSLPatientAssociationCountFinder(final JPAQueryFactory factory) {
+    QueryDSLPatientAssociationCountFinder(final JPAQueryFactory factory) {
         this.factory = factory;
         this.revision = QPerson.person;
     }
