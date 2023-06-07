4a0e754e2970ce42f70040a9b6fe2fe677484294
Michael Peels <michaelpeels@Michael-Peels.local>
2023-06-07 13:48:43 -0400

    single controller

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/question/QuestionController.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/question/QuestionController.java
@@ -5,18 +5,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/questions/")
-@AllArgsConstructor
-public class DeleteQuestionController {
-
+public class QuestionController {
 
     @DeleteMapping("{id}")
     @PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
     public void deleteQuestion(@PathVariable Long id) {
 
     }
-
 }
