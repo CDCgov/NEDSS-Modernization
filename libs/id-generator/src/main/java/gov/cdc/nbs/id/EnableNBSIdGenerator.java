12b22b343e6d770209d308fd6d83a75192e3138d
Michael Peels <michaelpeels@Michael-Peels.local>
2023-06-09 17:46:29 -0400

    Create EnableNBSIdGenerator annotation

---
+++ libs/id-generator/src/main/java/gov/cdc/nbs/id/EnableNBSIdGenerator.java
@@ -0,0 +1,18 @@
+package gov.cdc.nbs.id;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Import;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import(IdGeneratorConfig.class)
+public @interface EnableNBSIdGenerator {
+
+}
