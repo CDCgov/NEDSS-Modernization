fac5214c426d2db4a79e83441519b67c334360da
Adam Loup <aloup@enquizit.com>
2023-06-14 09:49:50 -0500

    allows add, update, and delete of patient race

---
+++ apps/modernization-ui/src/pages/patient/profile/race/RaceDetailModal.tsx
@@ -0,0 +1,56 @@
+import { Grid, ModalRef } from '@trussworks/react-uswds';
+import { RefObject } from 'react';
+
+import { ModalComponent } from 'components/ModalComponent/ModalComponent';
+import { PatientRace } from './Race';
+import { internalizeDate } from 'date';
+import { maybeDescription, maybeDescriptions } from 'pages/patient/profile/coded';
+
+type Props = {
+    modal: RefObject<ModalRef>;
+    data: PatientRace | undefined;
+};
+
+const noData = <span className="no-data">No data</span>;
+
+const renderDetailed = (data: PatientRace) => {
+    const details = maybeDescriptions(data.detailed).join(' | ');
+
+    return details.length > 0 ? <>{details}</> : noData;
+};
+
+export const RaceDetailModal = ({ modal, data }: Props) => {
+    return (
+        <ModalComponent
+            modalRef={modal}
+            modalHeading="View details - Race"
+            modalBody={
+                (data && (
+                    <div className="modal-body">
+                        <Grid row>
+                            <Grid col={12} className="border-bottom border-base-lighter padding-bottom-2 padding-2">
+                                <Grid row>
+                                    <Grid col={6}>As of:</Grid>
+                                    <Grid col={6}>{internalizeDate(data.asOf)}</Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid col={12} className="border-bottom border-base-lighter padding-bottom-2 padding-2">
+                                <Grid row>
+                                    <Grid col={6}>Race:</Grid>
+                                    <Grid col={6}>{maybeDescription(data.category)}</Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid col={12} className="border-bottom border-base-lighter padding-bottom-2 padding-2">
+                                <Grid row>
+                                    <Grid col={6}>Detailed race:</Grid>
+                                    <Grid col={6}>{renderDetailed(data)}</Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </div>
+                )) ||
+                noData
+            }
+        />
+    );
+};
