707a7a0b6cec37d301afd4cd5326e97e2e089a47
Muhammad Assad <116684885+massad22@users.noreply.github.com>
2023-06-14 13:32:52 -0400

    Changed for creating API for rule creation

---
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/businessrules/BusinessRulesServiceImpl.java
@@ -0,0 +1,12 @@
+package gov.cdc.nbs.questionbank.businessrules;
+
+import gov.cdc.nbs.questionbank.model.CreateRuleRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BusinessRulesServiceImpl implements BusinessRuleService{
+
+    public String createBusinessRule(CreateRuleRequest createRuleRequest){
+
+    }
+}
