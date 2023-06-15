74b08044c7f1cdbda101d2daea24ed6b3598c641
Muhammad Assad <116684885+massad22@users.noreply.github.com>
2023-06-15 13:13:43 -0400

    Changes for Data entity and sending the request to kafka

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/businessrules/BusinessRulesServiceImpl.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/businessrules/BusinessRulesServiceImpl.java
@@ -1,12 +1,40 @@
 package gov.cdc.nbs.questionbank.businessrules;
 
+import gov.cdc.nbs.questionbank.businessrules.repository.WaRuleMetaDataRepository;
+import gov.cdc.nbs.questionbank.entity.businessRule.WaRuleMetadata;
+import gov.cdc.nbs.questionbank.kafka.message.rule.RuleCreatedEvent;
+import gov.cdc.nbs.questionbank.kafka.producer.RuleCreatedEventProducer;
 import gov.cdc.nbs.questionbank.model.CreateRuleRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.text.RuleBasedCollator;
 
 @Service
 public class BusinessRulesServiceImpl implements BusinessRuleService{
 
-    public String createBusinessRule(CreateRuleRequest createRuleRequest){
+    @Autowired
+    private WaRuleMetaDataRepository waRuleMetaDataRepository;
 
+    @Autowired
+    private RuleCreatedEventProducer ruleCreatedEventProducer;
+
+
+    public Long createBusinessRule(CreateRuleRequest createRuleRequest){
+
+       WaRuleMetadata ruleMetadata = new WaRuleMetadata();
+
+       ruleMetadata.setRuleCd(createRuleRequest.getRuleDescription().getRuleFunction());
+       ruleMetadata.setRuleDescText(createRuleRequest.getRuleDescription().getRuleDescription());
+       ruleMetadata.setLogic(createRuleRequest.getRuleDetails().getComparator());
+       waRuleMetaDataRepository.save(ruleMetadata);
+       sendRuleEvent(createRuleRequest);
+       return ruleMetadata.getWaTemplateUid();
+
+    }
+
+    private void sendRuleEvent(CreateRuleRequest createRuleRequest){
+        ruleCreatedEventProducer.send(new RuleCreatedEvent(createRuleRequest));
     }
 }
