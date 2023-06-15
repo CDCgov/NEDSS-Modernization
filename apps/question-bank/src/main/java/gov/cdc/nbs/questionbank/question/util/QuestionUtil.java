f18efca7f3644bdd65865e75894571d12fd77a8c
Michael Peels <michaelpeels@Michael-Peels.local>
2023-06-15 15:12:15 -0400

    Code smell

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/question/util/QuestionUtil.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/question/util/QuestionUtil.java
@@ -4,6 +4,10 @@ import gov.cdc.nbs.questionbank.question.exception.NullObjectException;
 
 public class QuestionUtil {
 
+    private QuestionUtil() {
+
+    }
+
     public static <T> T requireNonNull(T obj, String message) {
         if (obj == null) {
             throw new NullObjectException(message);
