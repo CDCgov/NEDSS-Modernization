294736146af8890d40ca22c032db4f02a3bbdfa3
Muhammad Assad <116684885+massad22@users.noreply.github.com>
2023-06-16 11:14:51 -0400

    changes for saving request in DB

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/businessrules/repository/WaRuleMetaDataRepository.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/businessrules/repository/WaRuleMetaDataRepository.java
@@ -2,7 +2,8 @@ package gov.cdc.nbs.questionbank.businessrules.repository;
 
 import gov.cdc.nbs.questionbank.entity.businessRule.WaRuleMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface WaRuleMetaDataRepository extends JpaRepository<WaRuleMetadata, String> {
 }
