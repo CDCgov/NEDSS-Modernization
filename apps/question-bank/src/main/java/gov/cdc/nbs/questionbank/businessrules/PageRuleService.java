3b943ab368a81fbcd2ce9d5d5e276b509610b382
Muhammad Assad <116684885+massad22@users.noreply.github.com>
2023-06-20 14:02:15 -0400

    Test case changes a changes as per review comments

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/businessrules/PageRuleService.java
+++
@@ -1,8 +0,0 @@
-package gov.cdc.nbs.questionbank.businessrules;
-
-import gov.cdc.nbs.questionbank.model.CreateRuleRequest;
-
-public interface PageRuleService {
-
-   Long createPageRule(CreateRuleRequest.ruleRequest request);
-}
