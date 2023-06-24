f63c84e0922a3e7f2ccc5270450f2db0bbb56dc6
Muhammad Assad <116684885+massad22@users.noreply.github.com>
2023-06-24 16:40:52 -0400

    Code changes based on more findings from Legacy and fixing discrepancies.

---
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/pagerules/TargetValueHelper.java
@@ -0,0 +1,8 @@
+package gov.cdc.nbs.questionbank.pagerules;
+
+import java.util.List;
+
+public record TargetValueHelper(List<String> identifiers, List<String> targetValues, String ruleExpression) {
+
+}
+
