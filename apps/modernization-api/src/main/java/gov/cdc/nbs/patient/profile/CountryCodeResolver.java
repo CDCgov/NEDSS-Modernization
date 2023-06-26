2e10e7fc293a89b55aa7d928d264aaf4cb6d2848
Steven Urciuoli <steveu@Stevens-MacBook-Pro.local>
2023-06-26 11:43:25 -0500

    fix

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/CountryCodeResolver.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/CountryCodeResolver.java
@@ -1,5 +1,6 @@
 package gov.cdc.nbs.patient.profile;
 
+import gov.cdc.nbs.entity.odse.Locator;
 import gov.cdc.nbs.entity.odse.PostalLocator;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,12 @@ class CountryCodeResolver {
     @Autowired
     private CountryCodeRepository countryCodeRepository;
 
-    @SchemaMapping("countryCode")
+    @SchemaMapping(typeName = "Locator", field = "countryCode")
     @PreAuthorize("hasAuthority('FIND-PATIENT')")
-    Optional<CountryCode> resolve(final PostalLocator locator) {
-        return countryCodeRepository.findById(locator.getCntryCd());
+    Optional<CountryCode> resolve(final Locator locator) {
+        if (!(locator instanceof PostalLocator)) {
+            return null;
+        }
+        return countryCodeRepository.findById(((PostalLocator) locator).getCntryCd());
     }
 }
