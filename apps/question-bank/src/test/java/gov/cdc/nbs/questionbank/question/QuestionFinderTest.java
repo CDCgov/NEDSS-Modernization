01ef7c66c68f2c10dcaac88ba3f2f246b64b70ae
Michael Peels <michaelpeels@Michael-Peels.local>
2023-06-27 13:28:21 -0400

    Additional testing

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/question/QuestionFinderTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/question/QuestionFinderTest.java
@@ -1,23 +1,30 @@
 package gov.cdc.nbs.questionbank.question;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import gov.cdc.nbs.questionbank.entity.WaUiMetadatum;
 import gov.cdc.nbs.questionbank.entity.repository.WaUiMetadatumRepository;
 import gov.cdc.nbs.questionbank.question.exception.QuestionNotFoundException;
 import gov.cdc.nbs.questionbank.question.repository.WaQuestionRepository;
+import gov.cdc.nbs.questionbank.question.request.FindQuestionRequest;
 import gov.cdc.nbs.questionbank.question.response.GetQuestionResponse;
 import gov.cdc.nbs.questionbank.support.QuestionEntityMother;
 
@@ -77,5 +84,39 @@ class QuestionFinderTest {
         // then a question is found
         assertNotNull(response);
         assertTrue(response.isInUse());
+    }
+
+    @Test
+    void should_try_search_id() {
+        // given a request that can be converted to an id
+        FindQuestionRequest request = new FindQuestionRequest("123");
+
+        // and a question exists
+        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+        when(questionRepository.findAllByNameOrIdentifier(eq("123"), captor.capture(), Mockito.any()))
+                .thenReturn(new PageImpl<>(new ArrayList<>()));
+
+        // when a query is run
+        finder.find(request, PageRequest.ofSize(10));
+
+        // then the Id is queried
+        assertEquals(123L, captor.getValue().longValue());
+    }
+
+    @Test
+    void should_try_not_fail_if_search_not_id() {
+        // given a request that can be converted to an id
+        FindQuestionRequest request = new FindQuestionRequest("abc");
+
+        // and a question exists
+        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+        when(questionRepository.findAllByNameOrIdentifier(eq("abc"), captor.capture(), Mockito.any()))
+                .thenReturn(new PageImpl<>(new ArrayList<>()));
+
+        // when a query is run
+        finder.find(request, PageRequest.ofSize(10));
+
+        // then the Id is queried
+        assertEquals(-1L, captor.getValue().longValue());
     }
 }
