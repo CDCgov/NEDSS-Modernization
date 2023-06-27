d2d701bd4acd1b8126cb3943b6af9117aa8e632a
Steven Urciuoli <steveu@Stevens-MacBook-Pro.local>
2023-06-27 09:58:33 -0500

    backout deletable

--- apps/modernization-api/src/test/java/gov/cdc/nbs/patient/profile/CountryCodeResolverTest.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/patient/profile/CountryCodeResolverTest.java
@@ -9,12 +9,4 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class CountryCodeResolverTest {
-    @Test
-    void should_resolve_state_code_for_postal_locator() {
-        CountryCodeResolver resolver = new CountryCodeResolver();
-        PostalLocator locator = mock(PostalLocator.class);
-        when(locator.getCntryCd()).thenReturn("840");
-        Optional<CountryCode> actual = resolver.resolve(locator);
-        assertThat(actual.get().getCodeDescTxt()).isEqualTo("United States");
-    }
 }
--- apps/modernization-api/src/test/java/gov/cdc/nbs/patient/profile/StateCodeResolverTest.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/patient/profile/StateCodeResolverTest.java
@@ -9,12 +9,4 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class StateCodeResolverTest {
-    @Test
-    void should_resolve_state_code_for_postal_locator() {
-        StateCodeResolver resolver = new StateCodeResolver();
-        PostalLocator locator = mock(PostalLocator.class);
-        when(locator.getStateCd()).thenReturn("01");
-        Optional<StateCode> actual = resolver.resolve(locator);
-        assertThat(actual.get().getStateNm()).isEqualTo("AL");
-    }
 }
