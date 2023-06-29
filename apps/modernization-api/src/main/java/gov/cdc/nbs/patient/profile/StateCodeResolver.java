c0072d664882f4fb5031414c295cb5345ce9e6b3
Adam Loup <aloup@enquizit.com>
2023-06-29 10:04:03 -0500

    resolves error from state and country lookups for Patient Search results

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/StateCodeResolver.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/StateCodeResolver.java
@@ -2,25 +2,30 @@ package gov.cdc.nbs.patient.profile;
 
 import gov.cdc.nbs.entity.odse.Locator;
 import gov.cdc.nbs.entity.odse.PostalLocator;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
+import gov.cdc.nbs.entity.srte.StateCode;
+import gov.cdc.nbs.repository.StateCodeRepository;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import gov.cdc.nbs.repository.StateCodeRepository;
-import gov.cdc.nbs.entity.srte.StateCode;
+
+import java.util.Optional;
 
 @Controller
 class StateCodeResolver {
-    @Autowired
-    private StateCodeRepository stateCodeRepository;
+
+    private final StateCodeRepository stateCodeRepository;
+
+    StateCodeResolver(final StateCodeRepository stateCodeRepository) {
+        this.stateCodeRepository = stateCodeRepository;
+    }
 
     @SchemaMapping(typeName = "Locator", field = "stateCode")
     @PreAuthorize("hasAuthority('FIND-PATIENT')")
     Optional<StateCode> resolve(final Locator locator) {
-        if (!(locator instanceof PostalLocator)) {
-            return Optional.empty();
+        if (locator instanceof PostalLocator address && address.getStateCd() != null) {
+
+            return stateCodeRepository.findById(address.getStateCd());
         }
-        return stateCodeRepository.findById(((PostalLocator) locator).getStateCd());
+        return Optional.empty();
     }
 }
