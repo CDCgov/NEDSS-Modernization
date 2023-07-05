2e4efae82dc0f0eaaf2fe2974448a1a137e60a2e
Michael Peels <michaelpeels@Michael-Peels.local>
2023-07-05 17:15:03 -0400

    Add API to return program area codes available

---
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/programarea/model/ProgramArea.java
@@ -0,0 +1,9 @@
+package gov.cdc.nbs.questionbank.programarea.model;
+
+public record ProgramArea(
+        String code,
+        String description,
+        Integer nbsId,
+        String status) {
+
+}
