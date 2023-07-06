d1bdd74ceddec99182c9a6647956a4ccc5ddc033
Steven Urciuoli <steveu@Stevens-MacBook-Pro.local>
2023-07-06 03:05:11 -0500

    CNFT1-1055 prevent redelete of deleted patients

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/PatientProfileDeletableResolver.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/PatientProfileDeletableResolver.java
@@ -1,22 +1,29 @@
 package gov.cdc.nbs.patient.profile;
 
+import gov.cdc.nbs.entity.enums.RecordStatus;
 import gov.cdc.nbs.patient.PatientAssociationCountFinder;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import gov.cdc.nbs.repository.PersonRepository;
 
 @Controller
 class PatientProfileDeletableResolver {
 
+    @Autowired
+    private final PersonRepository personRepository;
     private final PatientAssociationCountFinder finder;
 
-    PatientProfileDeletableResolver(final PatientAssociationCountFinder finder) {
+    PatientProfileDeletableResolver(final PatientAssociationCountFinder finder, final PersonRepository personRepository) {
         this.finder = finder;
+        this.personRepository = personRepository;
     }
 
     @SchemaMapping("deletable")
     boolean resolve(final PatientProfile profile) {
-        // PatientAssociationCountFinder only returns non deleted person association counts
-        return finder.count(profile.id()) == 0;
+        RecordStatus status = personRepository.findById(profile.id()).get().getRecordStatusCd();
+        return !status.equals(RecordStatus.LOG_DEL) && finder.count(profile.id()) == 0;
     }
 
 }
