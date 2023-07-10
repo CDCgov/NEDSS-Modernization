88dbc7b116246d8d4c472f3176999304356310a7
Lakshmi Narayanan Ramakrishnan <lramakrishnan@enquizit.com>
2023-07-10 10:18:36 -0700

    Update CreateCondition.tsx

--- apps/modernization-ui/src/apps/page-builder/components/CreateCondition/CreateCondition.tsx
+++ apps/modernization-ui/src/apps/page-builder/components/CreateCondition/CreateCondition.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ClassicButton } from '../../../../classic';
 import './CreateCondition.scss';
 import { ProgramAreaControllerService, ValueSetControllerService } from '../../generated';
