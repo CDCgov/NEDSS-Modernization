94405fa55a6ec4427b9d61b4093722f73eb9c1ed
Steven Urciuoli <steveu@Stevens-MacBook-Pro.local>
2023-07-17 10:53:20 -0500

    1164 - Remove any boost from soundex so it comes last in search results

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/PatientService.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/PatientService.java
@@ -57,10 +57,10 @@ import static gov.cdc.nbs.config.security.SecurityUtil.Operations.FINDINACTIVE;
 public class PatientService {
     private static final float FIRST_NAME_PRIMARY_BOOST = 2.0f;
     private static final float FIRST_NAME_NON_PRIMARY_BOOST = 1.0f;
-    private static final float FIRST_NAME_SOUNDEX_BOOST = 0.5f;
+    private static final float FIRST_NAME_SOUNDEX_BOOST = 0.0f;
     private static final float LAST_NAME_PRIMARY_BOOST = 2.0f;
     private static final float LAST_NAME_NON_PRIMARY_BOOST = 1.0f;
-    private static final float LAST_NAME_SOUNDEX_BOOST = 0.5f;
+    private static final float LAST_NAME_SOUNDEX_BOOST = 0.0f;
 
     @Value("${nbs.max-page-size: 50}")
     private Integer maxPageSize;
