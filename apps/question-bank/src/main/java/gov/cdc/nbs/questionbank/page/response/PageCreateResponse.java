4ad1fedfb6e894a51fab2f0c73badcb452208d14
Ernst Melias <emelias@enquizit.com>
2023-08-09 18:09:34 -0400

    CNFT2-478: Legacy Create Page

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/page/response/PageCreateResponse.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/page/response/PageCreateResponse.java
@@ -2,9 +2,6 @@ package gov.cdc.nbs.questionbank.page.response;
 
 import org.springframework.http.HttpStatus;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 public record PageCreateResponse (
 		 Long pageId,
