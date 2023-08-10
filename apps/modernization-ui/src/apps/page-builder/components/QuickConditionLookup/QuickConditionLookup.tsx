161db2b90b1f80e839c88275a596a867d2da39cc
Richard Krussel <RichardKrussel@Richard-Krussel.attlocal.net>
2023-08-10 11:38:56 -0500

    init

--- apps/modernization-ui/src/apps/page-builder/components/QuickConditionLookup/QuickConditionLookup.tsx
+++ apps/modernization-ui/src/apps/page-builder/components/QuickConditionLookup/QuickConditionLookup.tsx
@@ -1,5 +1,16 @@
-import { ButtonGroup, Modal, ModalFooter, ModalRef, ModalHeading, ModalToggleButton } from '@trussworks/react-uswds';
+import {
+    Button,
+    ButtonGroup,
+    Icon,
+    Modal,
+    ModalFooter,
+    ModalHeading,
+    ModalRef,
+    ModalToggleButton,
+    TextInput
+} from '@trussworks/react-uswds';
 import { RefObject } from 'react';
+import './QuickConditionLookup.scss';
 
 type Props = {
     modal: RefObject<ModalRef>;
@@ -20,12 +31,41 @@ export const QuickConditionLookup = ({ modal, onClose }: Props) => {
             <ModalHeading
                 id="incomplete-form-confirmation-modal-heading"
                 className="border-bottom border-base-lighter font-sans-lg padding-2">
-                Unverified Address
+                <div className="header">
+                    <div>Search and add condition(s)</div>
+                    <Button type="button" unstyled onClick={onClose} className="close-btn">
+                        <Icon.Close />
+                    </Button>
+                </div>
             </ModalHeading>
-            <h2>Quick condition lookup</h2>
-            <p>Body</p>
-            <p>Body</p>
-            <p>Body</p>
+            <div className="modal-body">
+                <p className="description">You can search for existing condition(s) or create a new one</p>
+                <div className="search-container">
+                    <div style={{ display: 'flex' }}>
+                        <TextInput
+                            inputSize="medium"
+                            placeholder="Type something here..."
+                            id={''}
+                            style={{ height: '41px', border: 'none' }}
+                            name={'condition-search'}
+                            type="search"
+                        />
+                        <Button type="button" style={{ height: '41px', borderRadius: 0 }}>
+                            <Icon.Search />
+                        </Button>
+                    </div>
+                    <Button type="button" outline style={{ height: '41px' }}>
+                        <Icon.FilterAlt />
+                        Filter
+                    </Button>
+                    <Button type="button" style={{ height: '41px' }}>
+                        Add new condition
+                    </Button>
+                </div>
+
+                <p>Body</p>
+                <p>Body</p>
+            </div>
             <ModalFooter className="padding-2 margin-left-auto">
                 <ButtonGroup className="flex-justify-end">
                     <ModalToggleButton modalRef={modal} closer onClick={onClose}>
