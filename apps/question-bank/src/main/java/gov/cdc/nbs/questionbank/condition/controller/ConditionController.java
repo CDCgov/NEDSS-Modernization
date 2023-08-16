dd000d8a61784b7e7fad013bb5ada19b4ea49431
Archit Dasika <115642773+adasika-cdc@users.noreply.github.com>
2023-08-16 04:37:07 -0700

    Update ConditionController.java

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/controller/ConditionController.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/controller/ConditionController.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.http.ResponseEntity;
 import gov.cdc.nbs.authentication.UserDetailsProvider;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
