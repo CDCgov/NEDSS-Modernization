742972f09d5915d399f8878b5bf0146d2adae4de
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2023-08-18 11:57:40 -0500

    NBS gateway (#462)
    * adds nbs-gateway project
    
    * excludes Application classes from the coverage report
    
    * changes local docker to communicate through reverse-proxy
    
    * workflow
    
    * configuires helm chart for nbs-gateway
    
    * documentation
    
    * trigger sonar on build
    
    * ignores gateway
    
    * alpine
    
    * removing sonar job trigger

---
+++ apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/patient/profile/events/report/lab/CancelledLabAddLocatorConfiguration.java
@@ -0,0 +1,49 @@
+package gov.cdc.nbs.gateway.patient.profile.events.report.lab;
+
+import gov.cdc.nbs.gateway.patient.profile.PatientProfileService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+/**
+ * Configures the Patient Profile routes to the {@code /nbs/redirect/patientProfile/events/return} path of the
+ * {@code nbs.gateway.patient.profile.service} when the {@code routes.patient.profile.enabled} property is {@code true}
+ * and the following criteria is satisfied;
+ *
+ * <ul>
+ *     <li>Path equal to {@code /nbs/AddObservationLab2.do}</li>
+ *     <li>Query Parameter {@code ContextAction} equal to {@code Cancel}</li>
+ * </ul>
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "nbs.gateway.patient.profile", name = "enabled", havingValue = "true")
+class CancelledLabAddLocatorConfiguration {
+
+    @Bean
+    RouteLocator cancelledLabAddPatientProfileReturn(
+        final RouteLocatorBuilder builder,
+        @Qualifier("default") final GatewayFilter globalFilter,
+        final PatientProfileService parameters
+    ) {
+        return builder.routes()
+            .route(
+                "cancelled-lab-add-patient-profile-return",
+                route -> route.order(Ordered.HIGHEST_PRECEDENCE)
+                    .path("/nbs/AddObservationLab2.do")
+                    .and()
+                    .query("ContextAction", "Cancel")
+                    .filters(
+                        filter -> filter.setPath("/nbs/redirect/patientProfile/events/return")
+                            .filter(globalFilter)
+                    )
+                    .uri(parameters.uri())
+            )
+            .build();
+    }
+
+}
---
+++ apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/patient/search/PatientSearchRouteLocatorConfiguration.java
@@ -0,0 +1,85 @@
+package gov.cdc.nbs.gateway.patient.search;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+/**
+ * Configures the NBS Home Page to route searches and the advanced search link to the patient-search service.  The
+ * routes are only enabled when the {@code routes.patient.search.enabled} property is {@code true} and any of the following criteria is satisfied.
+ *
+ * <ul>
+ *     <li>Path equal to {@code /nbs/MyTaskList1.do}</li>
+ *     <li>Query Parameter {@code ContextAction} equal to {@code GlobalPatient}</li>
+ * </ul>
+ *
+ * <ul>
+ *     <li>Path equal to {@code /nbs/MyTaskList1.do}</li>
+ *     <li>Query Parameter {@code ContextAction} equal to {@code GlobalMP_ManualSearch}</li>
+ *     <li>Query Parameter {@code Mode1} equal to {@code ManualMerge}</li>
+ * </ul>
+ *
+ * <ul>
+ *     <li>Path equal to {@code /nbs/HomePage.do}</li>
+ *     <li>Query Parameter {@code method} equal to {@code patientSearchSubmit}</li>
+ * </ul>
+ *
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "nbs.gateway.patient.search", name = "enabled", havingValue = "true")
+class PatientSearchRouteLocatorConfiguration {
+
+    @Bean
+    RouteLocator searchRouteLocator(
+        final RouteLocatorBuilder builder,
+        @Qualifier("default") final GatewayFilter globalFilter,
+        final PatientSearchService parameters
+    ) {
+        return builder.routes()
+            .route(
+                "advanced-search-link",
+                route -> route
+                    .order(Ordered.HIGHEST_PRECEDENCE)
+                    .path("/nbs/MyTaskList1.do")
+                    .and()
+                    .query("ContextAction", "GlobalPatient")
+                    .filters(
+                        filter -> filter.setPath("/nbs/redirect/advancedSearch")
+                            .filter(globalFilter)
+                    )
+                    .uri(parameters.uri())
+            )
+            .route(
+                "merge-patient-manual-search",
+                route -> route
+                    .order(Ordered.HIGHEST_PRECEDENCE)
+                    .path("/nbs/MyTaskList1.do")
+                    .and()
+                    .query("ContextAction", "GlobalMP_ManualSearch")
+                    .and()
+                    .query("Mode1", "ManualMerge")
+                    .filters(
+                        filter -> filter.setPath("/nbs/redirect/advancedSearch")
+                            .filter(globalFilter)
+                    )
+                    .uri(parameters.uri())
+            )
+            .route("home-page-search",
+                route -> route.order(Ordered.HIGHEST_PRECEDENCE)
+                    .path("/nbs/HomePage.do")
+                    .and()
+                    .query("method", "patientSearchSubmit")
+                    .filters(
+                        filter -> filter.setPath("/nbs/redirect/simpleSearch")
+                            .filter(globalFilter)
+                    )
+                    .uri(parameters.uri())
+            )
+            .build();
+    }
+}
