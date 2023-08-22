f3a83c78554f1cfbbe9d912d69c1b34fcb8e19d0
Archit Dasika <115642773+adasika-cdc@users.noreply.github.com>
2023-08-22 01:51:52 -0700

    Update ConditionController.java

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/ConditionController.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/ConditionController.java
@@ -1,17 +1,14 @@
 package gov.cdc.nbs.questionbank.condition;
 
 import gov.cdc.nbs.questionbank.condition.request.ReadConditionRequest;
+import gov.cdc.nbs.questionbank.condition.response.ConditionStatusResponse;
 import gov.cdc.nbs.questionbank.condition.response.ReadConditionResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import gov.cdc.nbs.authentication.UserDetailsProvider;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import gov.cdc.nbs.questionbank.condition.request.CreateConditionRequest;
 import gov.cdc.nbs.questionbank.condition.response.CreateConditionResponse;
 
@@ -24,6 +21,7 @@ public class ConditionController {
     private final ConditionCreator conditionCreator;
     private final ConditionReader conditionReader;
     private final UserDetailsProvider userDetailsProvider;
+    private final ConditionStatus conditionStatus;
 
     @PostMapping
     @PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
@@ -43,6 +41,20 @@ public class ConditionController {
     @PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
     public Page<ReadConditionResponse.GetCondition> searchConditions(@RequestBody ReadConditionRequest search, @PageableDefault(size = 20) Pageable pageable) {
         return conditionReader.searchCondition(search, pageable);
+    }
+
+    @PatchMapping("/{id}")
+    @PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
+    public ConditionStatusResponse activateCondition(@PathVariable String id) {
+        ConditionStatusResponse response = conditionStatus.activateCondition(id);
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
+    public ConditionStatusResponse inactivateCondition(@PathVariable String id) {
+        ConditionStatusResponse response = conditionStatus.inactivateCondition(id);
+        return response;
     }
 
 
