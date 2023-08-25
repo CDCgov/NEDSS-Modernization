9780112bed8a6b8109acd5838c240a284b18dd5b
hclarkEnq <hclark@enquizit.com>
2023-08-25 07:40:36 -0700

    - restructuring html for message modal with the same styles

--- apps/modernization-ui/src/messageModal/MessageModal.tsx
+++ apps/modernization-ui/src/messageModal/MessageModal.tsx
@@ -1,6 +1,7 @@
 import { RefObject } from 'react';
 import { Icon, Modal, ModalHeading, ModalRef } from '@trussworks/react-uswds';
 import { Note } from 'components/Note';
+import './MessageModal.scss';
 
 export type MessageModalContent = { message: string; detail?: string };
 
@@ -8,25 +9,30 @@ type Props = {
     modal: RefObject<ModalRef>;
     title: string;
     content: MessageModalContent;
+    ariaDescribedBy?: string;
 };
 
-export const MessageModal = ({ modal, title = '', content }: Props) => {
+export const MessageModal = ({ modal, title = '', content, ariaDescribedBy = 'message-description' }: Props) => {
     return (
         <Modal
             ref={modal}
             id="example-modal-1"
             aria-labelledby="modal-1-heading"
-            className="padding-0 modal-message padding-bottom-3"
+            className="padding-0 modal-message modal"
             aria-describedby="modal-1-description">
             <ModalHeading id="modal-1-heading" className="border-bottom border-base-lighter font-sans-lg padding-2">
                 {title}
             </ModalHeading>
-            <div className="margin-2 grid-row flex-no-wrap border-left-1 flex-align-start modal-body">
-                <Icon.Warning className="font-sans-2xl margin-x-2 warning-icon" />
-                <div className="width-full">
-                    <p id="modal-1-description" className="margin-top-0">
+
+            <div className="modal-content margin-2 padding-bottom-2">
+                <div className="warning">
+                    <Icon.Warning className="font-sans-2xl margin-x-2" />
+                </div>
+                <div className="modal-message">
+                    <p className="margin-top-0" id={ariaDescribedBy}>
                         {content.message}
                     </p>
+
                     {content.detail && (
                         <div>
                             <Note>{content.detail}</Note>
