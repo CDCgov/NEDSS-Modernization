4a96935ea737128b0e89e30bd1406a78aac53e68
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2023-08-25 09:31:39 -0700

    Cnft1 1071 message modal (#486)
    * adding modal for message if patient not deletable
    
    * changing the header description
    
    * Remove +1 from random causing test failure when max result returned (#485)
    
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels.local>
    
    * CNFT1-1243 patient address auto complete patient profile (#484)
    
    * Creates component for address suggestions
    
    * Note
    
    * address verification for add patient
    
    * feature toggle
    
    * changing design of the modal and adding note component
    
    * adding padding to note: title
    
    * changing the color for the warning and adding color-warning to main color template
    
    * - restructuring html for message modal with the same styles
    
    ---------
    
    Co-authored-by: Michael Peels <109251240+mpeels@users.noreply.github.com>
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels.local>
    Co-authored-by: Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>

--- apps/modernization-ui/src/pages/patient/profile/resolveDeleteMessage.ts
+++ apps/modernization-ui/src/pages/patient/profile/resolveDeleteMessage.ts
@@ -1,7 +1,9 @@
 import { DeletabilityResult } from './resolveDeletability';
 
-const HAS_ASSOCIATIONS_MESSAGE =
-    'This patient file has associated event records. The file cannot be deleted until all associated event records have been deleted. If you are unable to see the associated event records due to your user permission settings, please contact your system administrator.';
+const HAS_ASSOCIATIONS_MESSAGE = {
+    message: 'This patient file has associated event records.',
+    detail: 'The file cannot be deleted until all associated event records have been deleted. If you are unable to see the associated event records due to your user permission settings, please contact your system administrator.'
+};
 
 const IS_INACTIVE_MESSAGE = 'This patient file is inactive and cannot be deleted.';
 
