8ce4712298a3f722454711c091599239483ed8df
John Lee <jlee@enquizit.com>
2023-08-25 14:24:32 -0500

    CNFT2-923 CreateCondition refactor
    Controller inputs
    
    styling

--- apps/modernization-ui/src/apps/page-builder/services/conditionAPI.ts
+++ apps/modernization-ui/src/apps/page-builder/services/conditionAPI.ts
@@ -1,10 +1,19 @@
 /* eslint-disable camelcase */
-import { Condition, ConditionControllerService } from '../generated';
+import { Condition, ConditionControllerService, CreateConditionRequest } from '../generated';
 
 export const fetchConditions = (token: string): Promise<Array<Condition>> => {
     return ConditionControllerService.findAllConditionsUsingGet({
         authorization: token
     }).then((response) => {
+        return response;
+    });
+};
+
+export const createCondition = (token: string, request: CreateConditionRequest) => {
+    return ConditionControllerService.createConditionUsingPost({
+        authorization: token,
+        request: request
+    }).then((response: any) => {
         return response;
     });
 };
