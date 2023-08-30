e548821d1847b66bb1a7aadbc02c301bc1534d30
Archit Dasika <115642773+adasika-cdc@users.noreply.github.com>
2023-08-30 12:43:44 -0700

    Update ConditionStatusTest.java

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/condition/delete/ConditionStatusTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/condition/delete/ConditionStatusTest.java
@@ -1,8 +1,6 @@
 package gov.cdc.nbs.questionbank.condition.delete;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -10,13 +8,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
 
 import gov.cdc.nbs.questionbank.condition.ConditionStatus;
 import gov.cdc.nbs.questionbank.condition.repository.ConditionCodeRepository;
 import gov.cdc.nbs.questionbank.condition.response.ConditionStatusResponse;
 
-import java.util.Objects;
 
 class ConditionStatusTest {
     @Mock
