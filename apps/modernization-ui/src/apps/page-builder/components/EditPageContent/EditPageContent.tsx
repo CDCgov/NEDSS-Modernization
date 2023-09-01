42fef55352ec2da7041da172b12a4661b6c504d5
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2023-09-01 14:41:27 -0400

    CNFT2-606 Edit Page: Display Section (#508)
    Questions
    
    cleanup
    
    updates
    
    log

--- apps/modernization-ui/src/apps/page-builder/components/EditPageContent/EditPageContent.tsx
+++ apps/modernization-ui/src/apps/page-builder/components/EditPageContent/EditPageContent.tsx
@@ -1,9 +1,15 @@
+import { SectionComponent } from '../Section/Section';
 import './EditPageContent.scss';
+import { EditPageContent } from 'apps/page-builder/generated/models/EditPageContent';
 
-type Props = {
-    content: string;
-};
-
-export const EditPageContent = ({ content }: Props) => {
-    return <div className="edit-page-content">{content} Content</div>;
+export const EditPageContentComponent = ({ content }: { content: EditPageContent }) => {
+    return (
+        <div className="edit-page-content">
+            <div className="edit-page-content__sections">
+                {content.tabSections.map((section: any, i: number) => {
+                    return <SectionComponent key={i} section={section} />;
+                })}
+            </div>
+        </div>
+    );
 };
