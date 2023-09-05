5cd546290c63285dfe55cbe266dde9a5f028a46e
Michael Davis <michaeldavis@Michaels-MBP.fios-router.home>
2023-09-05 09:52:18 -0400

    removed unused variable

--- apps/modernization-ui/src/pages/patient/profile/summary/PatientProfileSummary.tsx
+++ apps/modernization-ui/src/pages/patient/profile/summary/PatientProfileSummary.tsx
@@ -17,7 +17,7 @@ const formattedPhones = (items: Phone[]) => display(items.map((items) => items.n
 
 const formattedEmails = (items: Email[]) => display(items.map((item) => item.address).join('\n'));
 
-const formattedAddress = ({ street, city, state, zipcode, country }: Address) => {
+const formattedAddress = ({ street, city, state, zipcode }: Address) => {
     const location = ((city && city + ' ') || '') + ((state && state + ' ') || '') + (zipcode ?? '');
     const address =
         ((street && street + '\n') || '') + ((location && location + '\n') || '');
