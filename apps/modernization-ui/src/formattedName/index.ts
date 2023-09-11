df8a353667d17c97ed054d8bcb47ecfeed9b839d
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2023-09-11 10:47:16 -0700

    first name and last name check for null to not appear in search results (#527)
    * first name and last name check for null to not appear in search results
    
    * changing formattedName into its own file, testing the function, and importing when needed
    
    * replacing the folder structure with formattedName

---
+++ apps/modernization-ui/src/formattedName/index.ts
@@ -0,0 +1,8 @@
+const formattedName = (lastNm: string | undefined | null, firstNm: string | undefined | null) => {
+    const format = [];
+    lastNm && format.push(lastNm);
+    firstNm && format.push(firstNm);
+    return (format.length > 1 ? format.join(', ') : format.join('')) || '--';
+};
+
+export default formattedName;
