c62f8bbcbf15efc68c5c0377b34a594a0b32b159
Michael Peels <michaelpeels@Michael-Peels.local>
2023-09-15 09:52:59 -0400

    Increase sonar loging. Remove exclusion that wasnt working

--- apps/modernization-api/src/test/java/gov/cdc/nbs/FindUsersSteps.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/FindUsersSteps.java
@@ -1,20 +1,7 @@
 package gov.cdc.nbs;
 
-import gov.cdc.nbs.authentication.entity.AuthAudit;
-import gov.cdc.nbs.authentication.entity.AuthPermSet;
-import gov.cdc.nbs.authentication.entity.AuthPermSetRepository;
-import gov.cdc.nbs.authentication.entity.AuthUser;
-import gov.cdc.nbs.authentication.entity.AuthUserRepository;
-import gov.cdc.nbs.authentication.entity.AuthUserRole;
-import gov.cdc.nbs.authentication.entity.AuthUserRoleRepository;
-import gov.cdc.nbs.authorization.TestAuthorizedUser;
-import gov.cdc.nbs.controller.UserController;
-import gov.cdc.nbs.support.PermissionMother;
-import gov.cdc.nbs.support.TestAvailable;
-import gov.cdc.nbs.support.UserMother;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Objects;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,11 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.time.Instant;
-import java.util.Objects;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import gov.cdc.nbs.authentication.entity.AuthUser;
+import gov.cdc.nbs.authorization.TestAuthorizedUser;
+import gov.cdc.nbs.controller.UserController;
+import gov.cdc.nbs.support.TestAvailable;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -35,13 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FindUsersSteps {
 
     @Autowired
-    private AuthPermSetRepository permSetRepository;
-    @Autowired
-    private AuthUserRoleRepository roleRepository;
-    @Autowired
     private UserController userController;
-    @Autowired
-    private AuthUserRepository userRepository;
 
     @Autowired
     TestAvailable<TestAuthorizedUser> availableUsers;
@@ -60,21 +42,21 @@ public class FindUsersSteps {
     public void a_user_is_returned(final String expected) {
 
         TestAuthorizedUser authorizedUser = this.availableUsers.all().filter(u -> Objects.equals(expected, u.name()))
-            .findFirst()
-            .orElseThrow();
+                .findFirst()
+                .orElseThrow();
 
         assertThat(users.getContent())
-            .anySatisfy(actual -> assertThat(actual).returns(authorizedUser.id(), AuthUser::getId));
+                .anySatisfy(actual -> assertThat(actual).returns(authorizedUser.id(), AuthUser::getId));
     }
 
     @Then("The {string} user is not returned")
     public void a_user_is_not_returned(final String expected) {
         TestAuthorizedUser authorizedUser = this.availableUsers.all().filter(u -> Objects.equals(expected, u.name()))
-            .findFirst()
-            .orElseThrow();
+                .findFirst()
+                .orElseThrow();
 
         assertThat(users.getContent())
-            .noneSatisfy(actual -> assertThat(actual).returns(authorizedUser.id(), AuthUser::getId));
+                .noneSatisfy(actual -> assertThat(actual).returns(authorizedUser.id(), AuthUser::getId));
     }
 
 }
