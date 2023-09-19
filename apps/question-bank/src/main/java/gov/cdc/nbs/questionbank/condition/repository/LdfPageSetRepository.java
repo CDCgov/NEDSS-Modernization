4fefc432a10d85623b9a75a0f38dab5a4cc16535
Archit Dasika <115642773+adasika-cdc@users.noreply.github.com>
2023-09-19 02:42:12 -0700

    Changed to update LDF accordingly as well

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/repository/LdfPageSetRepository.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/repository/LdfPageSetRepository.java
@@ -3,13 +3,22 @@ package gov.cdc.nbs.questionbank.condition.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import gov.cdc.nbs.questionbank.entity.condition.LdfPageSet;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface LdfPageSetRepository extends JpaRepository<LdfPageSet, String> {
     @Query("SELECT id FROM LdfPageSet")
     List<String> findAllIds();
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE LdfPageSet l " +
+            "SET l.statusCd = (SELECT c.statusCd FROM ConditionCode c WHERE c.id = l.conditionCd) " +
+            "WHERE EXISTS (SELECT 1 FROM ConditionCode c WHERE c.id = l.conditionCd)")
+    void updateStatusBasedOnConditionCode();
 
     @Query("SELECT MAX(displayRow) + 1 FROM LdfPageSet")
     Short nextDisplayRow();
