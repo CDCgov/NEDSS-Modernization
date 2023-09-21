701cb5b1d06dd50b90360a28bd3c4ff522cb124a
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2023-09-21 11:04:19 -0500

    [Docker] Containerization Updates (#567)
    * reduces copied files
    
    * move java applications to eclipse-temurin
    
    * removes geocoding-api
    
    * ui test issue
    
    * containerization of ui
    
    * removes patient-search folder
    
    * apply to all projects
    
    * merges docs into documentation

--- apps/modernization-ui/src/apps/page-builder/pages/EditPage/EditPage.spec.tsx
+++ apps/modernization-ui/src/apps/page-builder/pages/EditPage/EditPage.spec.tsx
@@ -1,9 +1,7 @@
-import jwt from 'jsonwebtoken';
-
+import { EditPage } from './EditPage';
 
 describe('when EditPage renders', () => {
     it('should fetch Page Details', () => {
-        jest.spyOn(global, 'fetch').mockImplementationOnce(
-            jest.fn(() => Promise.resolve()) as jest.Mock);
+        jest.spyOn(global, 'fetch').mockImplementationOnce(jest.fn(() => Promise.resolve()) as jest.Mock);
     });
 });
