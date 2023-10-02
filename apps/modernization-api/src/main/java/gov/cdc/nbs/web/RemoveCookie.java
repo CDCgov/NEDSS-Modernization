cd4f851d529d1bc5e0e7995a1d4820fe089a0b61
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2023-10-02 16:55:48 -0500

    removes max-age (#600) (#601)

--- apps/modernization-api/src/main/java/gov/cdc/nbs/web/RemoveCookie.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/web/RemoveCookie.java
@@ -1,13 +1,21 @@
 package gov.cdc.nbs.web;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 
 import java.util.function.Consumer;
 
 public class RemoveCookie {
 
     public static void from(final String name, final HttpHeaders headers) {
-        String cookie = name + "=; SameSite=Strict; HttpOnly; Secure; Max-Age=0";
+        String cookie = ResponseCookie.from(name,"")
+            .path("/nbs/")
+            .sameSite("Strict")
+            .httpOnly(true)
+            .secure(true)
+            .maxAge(0)
+            .build()
+            .toString();
         headers.add(HttpHeaders.SET_COOKIE, cookie);
     }
 
