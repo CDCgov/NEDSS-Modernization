7eb6dc79d44e6b222c4cf6577072d28df4d3a0f8
Michael Davis <michaeldavis@michaels-mbp.mynetworksettings.com>
2023-10-18 15:50:45 -0400

    Fixed Incorrect spelling Superceded with Superseded in all variations

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/PatientProfile.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/PatientProfile.java
@@ -5,7 +5,7 @@ import gov.cdc.nbs.entity.enums.RecordStatus;
 public record PatientProfile(long id, String local, short version, String status) {
 
     public PatientProfile(long id, String local, short version) {
-        this(id, local, version, RecordStatus.ACTIVE.name());
+        this(id, local, version, RecordStatus.ACTIVE.display());
     }
 
 }
