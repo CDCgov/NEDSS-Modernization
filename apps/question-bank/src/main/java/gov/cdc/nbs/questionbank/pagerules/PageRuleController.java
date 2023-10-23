5c891d7ed587a8a580cf5605d2e28b79ef6aac0b
Lakshmi Narayanan Ramakrishnan <132704359+lramakr@users.noreply.github.com>
2023-10-23 14:59:18 -0700

    [CNFT2-1040] [CNFT2-1267] - Develop get all business rules API and Search business rules API (#615)
    * [CNFT2-1040] - Develop get all business rules API, Cucumber Test cases, fixed Total page size, Added search business rule API
    
    * test fix
    
    * Fix Code Smell
    
    * Fix Code Smell
    
    * Fix code review comment
    
    * Code review comment fix

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/pagerules/PageRuleController.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/pagerules/PageRuleController.java
@@ -7,6 +7,9 @@ import gov.cdc.nbs.questionbank.pagerules.exceptions.RuleException;
 import gov.cdc.nbs.questionbank.pagerules.response.CreateRuleResponse;
 import gov.cdc.nbs.questionbank.model.CreateRuleRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -15,54 +18,65 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/")
+@PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
+@RequestMapping("/api/v1/pages/{page}/rules")
 public class PageRuleController {
 
     private final PageRuleService pageRuleService;
 
     private final UserDetailsProvider userDetailsProvider;
 
-    public PageRuleController(PageRuleService pageRuleService, UserDetailsProvider userDetailsProvider) {
+    private final PageRuleFinderService pageRuleFinderService;
+
+    public PageRuleController(PageRuleService pageRuleService, UserDetailsProvider userDetailsProvider,PageRuleFinderService pageRuleFinderService) {
         this.userDetailsProvider = userDetailsProvider;
         this.pageRuleService = pageRuleService;
+        this.pageRuleFinderService = pageRuleFinderService;
     }
 
-    @PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
-    @PostMapping("rule")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public CreateRuleResponse createBusinessRule(@RequestBody CreateRuleRequest request) {
+    public CreateRuleResponse createBusinessRule(@RequestBody CreateRuleRequest request,@PathVariable Long page) {
         log.info("Request for Business Rule Creation");
         Long userId = userDetailsProvider.getCurrentUserDetails().getId();
         try {
-            CreateRuleResponse ruleResponse = pageRuleService.createPageRule(userId, request);
+            CreateRuleResponse ruleResponse = pageRuleService.createPageRule(userId, request,page);
             log.debug("Successfully added business rule with Id: {}", ruleResponse.ruleId());
             return ruleResponse;
         } catch (RuleException e) {
-            return new CreateRuleResponse(null, "Error in Creating a Rule");
+            return new CreateRuleResponse(null, "Error in Creating a Rules");
         }
     }
 
-    @PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
-    @DeleteMapping("rule/{ruleId}")
+    @DeleteMapping("/{ruleId}")
     @ResponseBody
     public CreateRuleResponse deletePageRule(@PathVariable Long ruleId) {
         return pageRuleService.deletePageRule(ruleId);
     }
 
-    @PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
-    @PutMapping("rule/{ruleId}")
+    @PutMapping("/{ruleId}")
     @ResponseBody
     public CreateRuleResponse updatePageRule(@PathVariable Long ruleId,
-            @RequestBody CreateRuleRequest request) throws RuleException {
+                                             @RequestBody CreateRuleRequest request,@PathVariable Long page) throws RuleException {
         Long userId = userDetailsProvider.getCurrentUserDetails().getId();
-        return pageRuleService.updatePageRule(ruleId, request, userId);
+        return pageRuleService.updatePageRule(ruleId, request, userId,page);
     }
 
-    @PreAuthorize("hasAuthority('LDFADMINISTRATION-SYSTEM')")
-    @GetMapping("rule/{ruleId}")
+    @GetMapping("/{ruleId}")
     @ResponseBody
     public ViewRuleResponse viewRuleResponse(@PathVariable Long ruleId) {
-        return pageRuleService.getRuleResponse(ruleId);
+        return pageRuleFinderService.getRuleResponse(ruleId);
+    }
+    @GetMapping
+    @ResponseBody
+    public Page<ViewRuleResponse> getAllPageRule(@PageableDefault(size = 25) Pageable pageable,@PathVariable Long page){
+        return pageRuleFinderService.getAllPageRule(pageable,page);
+    }
+
+    @PostMapping("/search")
+    @ResponseBody
+    public Page<ViewRuleResponse> findPageRule(@RequestBody SearchPageRuleRequest request,@PageableDefault(size = 25) Pageable pageable){
+        return pageRuleFinderService.findPageRule(request,pageable);
     }
 }
---
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/PageRuleSteps.java
@@ -0,0 +1,165 @@
+package gov.cdc.nbs.questionbank.pagerules;
+
+import gov.cdc.nbs.questionbank.model.CreateRuleRequest;
+import gov.cdc.nbs.questionbank.model.ViewRuleResponse;
+import gov.cdc.nbs.questionbank.pagerules.response.CreateRuleResponse;
+import gov.cdc.nbs.questionbank.support.ExceptionHolder;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+public class PageRuleSteps {
+
+    @Autowired
+    private PageRuleController pageRuleController;
+
+    @Autowired
+    private ExceptionHolder exceptionHolder;
+
+    CreateRuleResponse ruleResponse;
+
+    Page<ViewRuleResponse> viewRuleResponses;
+
+    Long ruleId;
+
+    @Given("I am not logged in user")
+    public void i_am_not_logged_in() {
+        SecurityContextHolder.getContext().setAuthentication(null);
+    }
+
+    @When("I make a request to add a rule to a page")
+    public void i_make_a_request_to_add_a_rule_to_a_page() {
+
+        try {
+            ruleResponse = pageRuleController.createBusinessRule(ruleRequest(),123456L);
+        } catch (AccessDeniedException e) {
+            exceptionHolder.setException(e);
+        } catch (AuthenticationCredentialsNotFoundException e) {
+            exceptionHolder.setException(e);
+        }
+    }
+    @Then("A rule is created")
+    public void a_rule_is_created() {
+        assertNotNull(ruleResponse);
+    }
+
+    @Then("A no credentials found exception is thrown")
+    public void a_no_credentials_found_exception_is_thrown() {
+        assertNotNull(exceptionHolder.getException());
+        assertTrue(exceptionHolder.getException() instanceof AuthenticationCredentialsNotFoundException);
+    }
+
+
+    @When("I make a request to update a rule to a page")
+    public void i_make_a_request_to_update_a_rule_to_a_page() {
+        try{
+            ruleId = 6405L;
+            ruleResponse = pageRuleController.updatePageRule(ruleId,ruleRequest(),123456L);
+        } catch (AuthenticationCredentialsNotFoundException e) {
+            exceptionHolder.setException(e);
+        }
+    }
+
+    @Then("A rule is updated")
+    public void a_rule_is_updated() {
+        assertNotNull(ruleResponse);
+    }
+
+
+
+    @When("I make a request to delete a rule to a page")
+    public void i_make_a_request_to_delete_a_rule_to_a_page() {
+        try{
+            ruleResponse = pageRuleController.createBusinessRule(ruleRequest(),123456L);
+            ruleResponse = pageRuleController.deletePageRule(ruleResponse.ruleId());
+        } catch (AuthenticationCredentialsNotFoundException e) {
+            exceptionHolder.setException(e);
+        }
+    }
+
+    @Then("A rule is deleted")
+    public void a_rule_is_deleted() {
+        assertNotNull(ruleResponse);
+    }
+
+
+    private static CreateRuleRequest ruleRequest() {
+        List<String> targetValuesList = new ArrayList<>();
+        targetValuesList.add("string");
+        List<String> targetIdentifiers = new ArrayList<>();
+        targetIdentifiers.add("string");
+
+        List<CreateRuleRequest.SourceValues> sourceValues = new ArrayList<>();
+        List<String> sourceIds = new ArrayList<>();
+        sourceIds.add("string");
+
+        List<String> sourceValueText = new ArrayList<>();
+        sourceValueText.add("string");
+        CreateRuleRequest.SourceValues sourceValue = new CreateRuleRequest.SourceValues(sourceIds, sourceValueText);
+        sourceValues.add(sourceValue);
+        return new CreateRuleRequest(
+                "Enable",
+                "string",
+                "string",
+                "string",
+                sourceValues,
+                true,
+                "string",
+                "string",
+                targetValuesList,
+                targetIdentifiers);
+    }
+
+    @When("I search for page rule")
+    public void iSearchForPageRule() {
+        try{
+            SearchPageRuleRequest request = new SearchPageRuleRequest("Dengue virus");
+            viewRuleResponses = pageRuleController.findPageRule(request,null);
+        } catch (AuthenticationCredentialsNotFoundException e) {
+            exceptionHolder.setException(e);
+        }
+    }
+
+    @Then("an access denied exception is thrown")
+    public void a_access_denied_exception_is_thrown() {
+        assertNotNull(exceptionHolder.getException());
+        assertTrue(exceptionHolder.getException() instanceof AccessDeniedException);
+    }
+
+    @When("I get all page rules")
+    public void iGetAllPageRules() {
+        try{
+            int page = 0;
+            int size =1;
+            String sort ="id";
+            Pageable pageRequest = PageRequest.of(page,size,Sort.by(sort));
+            viewRuleResponses = pageRuleController.getAllPageRule(pageRequest,1000272L);
+        } catch (AuthenticationCredentialsNotFoundException e) {
+            exceptionHolder.setException(e);
+        }
+    }
+
+    @Then("page rules are returned")
+    public void pageRulesAreReturned() {
+        assertNotNull(viewRuleResponses);
+        if(viewRuleResponses.get().findFirst().isPresent()){
+            ViewRuleResponse resp =  viewRuleResponses.get().findFirst().get();
+            assertEquals("ARB001",resp.sourceIdentifier());
+            assertEquals("QUESTION",resp.targetType());
+        }
+    }
+}
