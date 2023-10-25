4164c76d419005477e90769b85d50a672a0b85fe
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2023-10-25 11:56:55 -0500

    UI/ Update (#651)
    * Update frontend dependencies
    
    * remove use of generated graphql types
    
    * update graphql codegen

--- apps/modernization-ui/src/pages/patient/profile/identification/identification.ts
+++ apps/modernization-ui/src/pages/patient/profile/identification/identification.ts
@@ -1,21 +1,8 @@
-import { Maybe, PatientIdentificationAuthority, PatientIdentificationType, Scalars } from 'generated/graphql/schema';
-
-export type Identification = {
-    __typename?: 'PatientIdentification';
-    asOf: Scalars['DateTime'];
-    authority?: Maybe<PatientIdentificationAuthority>;
-    patient: Scalars['Int'];
-    sequence: Scalars['Int'];
-    type?: PatientIdentificationType;
-    value?: Maybe<Scalars['String']>;
-    version: Scalars['Int'];
-};
-
 export type IdentificationEntry = {
     patient: number;
     asOf: string | null;
-    type: Maybe<Scalars['String']>;
-    value?: Maybe<Scalars['String']>;
+    type: string | null;
+    value: string | null;
     state: string | null;
-    sequence?: Scalars['Int'];
+    sequence?: number;
 };
