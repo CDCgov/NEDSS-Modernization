17e4cce6a2624fc8cd2006c2015f6ec69794939d
Adam Loup <aloup@enquizit.com>
2023-10-26 08:44:31 -0500

    ride ze shoopuf

--- libs/options-api/src/test/java/gov/cdc/nbs/option/RunCucumber.java
+++ libs/options-api/src/test/java/gov/cdc/nbs/option/RunCucumber.java
@@ -2,22 +2,22 @@ package gov.cdc.nbs.option;
 
 import gov.cdc.nbs.testing.database.EmbeddedNbsDatabase;
 import gov.cdc.nbs.testing.support.EnableSupport;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@CucumberOptions(
-    glue = "gov.cdc.nbs.concept",
-    features = "src/test/resources/features",
-    plugin = {"pretty", "html:build/reports/tests/test/cucumber-report.html"}
-)
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,html:build/reports/tests/test/cucumber-report.html")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "gov.cdc.nbs.option")
 @CucumberContextConfiguration
 @SpringBootTest(
     classes = OptionTestContext.class,
