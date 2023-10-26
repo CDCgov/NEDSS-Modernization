acdeb6642646f6d74e2d3bc7a9b1f5e621e3f0dc
Alaap Bharadwaj <abharadwaj@enquizit.com>
2023-10-26 16:06:40 -0400

    cucumber tests

---
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/page/content/staticelement/StaticElementSteps.java
@@ -0,0 +1,46 @@
+package gov.cdc.nbs.questionbank.page.content.staticelement;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import gov.cdc.nbs.questionbank.entity.WaTemplate;
+import gov.cdc.nbs.questionbank.entity.WaUiMetadata;
+import gov.cdc.nbs.questionbank.page.content.staticelement.request.AddStaticElementDefaultRequest;
+import gov.cdc.nbs.questionbank.page.content.tab.repository.WaUiMetaDataRepository;
+import gov.cdc.nbs.questionbank.support.PageMother;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StaticElementSteps {
+
+    @Autowired
+    private PageStaticController pageStaticController;
+
+    @Autowired
+    private WaUiMetaDataRepository waUiMetaDataRepository;
+
+    @Autowired
+    private PageMother mother;
+
+    private Long lineSeparatorId;
+
+    @When("I send an add line separator request")
+    public void i_send_an_add_tab_request() {
+        // create request
+        WaTemplate temp = mother.one();
+        WaUiMetadata subsection = temp.getUiMetadata().stream()
+                .filter(ui -> ui.getNbsUiComponentUid() == 1016L)
+                .findFirst()
+                .orElseThrow();
+
+        lineSeparatorId = pageStaticController.addStaticLineSeparator(
+                temp.getId(),
+                new AddStaticElementDefaultRequest(null, subsection.getId()))
+                .componentId();
+    }
+
+    @Then("a line separator is created")
+    public void a_line_separator_is_created() {
+        assertNotNull(lineSeparatorId);
+        WaUiMetadata lineSeparatorEnt = waUiMetaDataRepository.findById(lineSeparatorId).orElseThrow();
+    }
+}
---
+++ apps/question-bank/src/test/resources/features/page/staticelement/AddLineSeparatorToPage.feature
@@ -0,0 +1,14 @@
+Feature: Create Line Separator
+
+    Background:
+        Given I have a page
+        And the page has a tab
+        And the page has a section in the 1st tab
+        And the page has a sub-section named "sub-section" in the 1st section
+    
+    Scenario: I can create a line separator static element
+        Given I am an admin user
+        When I send an add line separator request
+        Then a line separator is created
+
+    
