8004aede27058a16138b86d1781864eb5ae68380
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2023-10-27 13:45:36 -0500

    enusres API returns the correct status display for the Patient Profile (#661)

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/PatientProfileDeletableResolver.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/PatientProfileDeletableResolver.java
@@ -2,22 +2,20 @@ package gov.cdc.nbs.patient.profile;
 
 import gov.cdc.nbs.entity.enums.RecordStatus;
 import gov.cdc.nbs.patient.PatientAssociationCountFinder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 class PatientProfileDeletableResolver {
 
-    @Autowired
-    private final PatientAssociationCountFinder finder;
+  private final PatientAssociationCountFinder finder;
 
-    PatientProfileDeletableResolver(final PatientAssociationCountFinder finder) {
-        this.finder = finder;
-    }
+  PatientProfileDeletableResolver(final PatientAssociationCountFinder finder) {
+    this.finder = finder;
+  }
 
-    @SchemaMapping("deletable")
-    boolean resolve(final PatientProfile profile) {
-        return !profile.status().equals(RecordStatus.LOG_DEL.toString()) && finder.count(profile.id()) == 0;
-    }
+  @SchemaMapping("deletable")
+  boolean resolve(final PatientProfile profile) {
+    return !profile.status().equals(RecordStatus.LOG_DEL.display()) && finder.count(profile.id()) == 0;
+  }
 }
