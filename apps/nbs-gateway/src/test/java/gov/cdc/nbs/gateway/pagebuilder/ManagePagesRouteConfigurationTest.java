112d27217c46703f10aaf1e33be31077d0e3471a
Michael Peels <michaelpeels@Michael-Peels.local>
2023-10-31 15:42:21 -0400

    Update test

--- apps/nbs-gateway/src/test/java/gov/cdc/nbs/gateway/pagebuilder/ManagePagesRouteConfigurationTest.java
+++ apps/nbs-gateway/src/test/java/gov/cdc/nbs/gateway/pagebuilder/ManagePagesRouteConfigurationTest.java
@@ -1,6 +1,7 @@
 package gov.cdc.nbs.gateway.pagebuilder;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.notFound;
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
@@ -12,62 +13,72 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 
 @SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = {
-                "nbs.gateway.pagebuilder.manage.service=localhost:10002",
-                "nbs.gateway.pagebuilder.manage.enabled=true"
-        })
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {
+        "nbs.gateway.classic=http://localhost:10000",
+        "nbs.gateway.pagebuilder.manage.service=localhost:10002",
+        "nbs.gateway.pagebuilder.manage.enabled=true"
+    })
 class ManagePagesRouteConfigurationTest {
 
-    @RegisterExtension
-    static WireMockExtension service = WireMockExtension.newInstance()
-            .options(wireMockConfig().port(10002))
-            .build();
+  @RegisterExtension
+  static WireMockExtension classic = WireMockExtension.newInstance()
+      .options(wireMockConfig().port(10000))
+      .build();
 
-    @Autowired
-    WebTestClient webClient;
+  @RegisterExtension
+  static WireMockExtension service = WireMockExtension.newInstance()
+      .options(wireMockConfig().port(10002))
+      .build();
+
+  @Autowired
+  WebTestClient webClient;
 
 
-    @Test
-    void should_route() {
+  @Test
+  void should_route_to_modernized() {
 
-        service.stubFor(get(urlPathMatching("/nbs/redirect/pagebuilder/manage/pages")).willReturn(ok()));
+    service.stubFor(get(urlPathMatching("/nbs/redirect/pagebuilder/manage/pages")).willReturn(ok()));
 
-        webClient
-                .get().uri(
-                        builder -> builder
-                                .path("/nbs/ManagePage.do")
-                                .queryParam("method", "list")
-                                .queryParam("initLoad", "true")
-                                .build())
-                .exchange()
-                .expectStatus()
-                .isOk();
-    }
+    webClient
+        .get().uri(
+            builder -> builder
+                .path("/nbs/ManagePage.do")
+                .queryParam("method", "list")
+                .queryParam("initLoad", "true")
+                .build())
+        .exchange()
+        .expectStatus()
+        .isOk();
+  }
 
-    @Test
-    void should_not_route_without_init_load_param() {
-        webClient
-                .get().uri(
-                        builder -> builder
-                                .path("/nbs/ManagePage.do")
-                                .queryParam("method", "list")
-                                .build())
-                .exchange()
-                .expectStatus()
-                .isFound();
-    }
+  @Test
+  void should_not_route_without_init_load_param() {
+    classic.stubFor(get(urlPathMatching("/nbs/ManagePage.do")).willReturn(notFound()));
 
-    @Test
-    void should_not_route_without_method_param() {
-        webClient
-                .get().uri(
-                        builder -> builder
-                                .path("/nbs/ManagePage.do")
-                                .queryParam("initLoad", "true")
-                                .build())
-                .exchange()
-                .expectStatus()
-                .isFound();
-    }
+    webClient
+        .get().uri(
+            builder -> builder
+                .path("/nbs/ManagePage.do")
+                .queryParam("method", "list")
+                .build())
+        .exchange()
+        .expectStatus()
+        .isNotFound();
+  }
+
+  @Test
+  void should_not_route_without_method_param() {
+    classic.stubFor(get(urlPathMatching("/nbs/ManagePage.do")).willReturn(notFound()));
+
+    webClient
+        .get().uri(
+            builder -> builder
+                .path("/nbs/ManagePage.do")
+                .queryParam("initLoad", "true")
+                .build())
+        .exchange()
+        .expectStatus()
+        .isNotFound();
+  }
 }
