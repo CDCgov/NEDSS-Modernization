4fab518ca83c604acdad9c32eba0e74e4ead9f86
Michael Davis <michaeldavis@michaels-mbp.mynetworksettings.com>
2023-11-06 10:09:44 -0500

    font weight bold for modal delete text and removed bold from action delete text

--- apps/modernization-ui/src/components/Table/Actions.tsx
+++ apps/modernization-ui/src/components/Table/Actions.tsx
@@ -39,7 +39,7 @@ export const Actions = ({ handleAction, handleOutsideClick, notDeletable }: any)
                     onClick={() => handleAction('delete')}
                     unstyled
                     type="button"
-                    className="text-bold text-base-dark display-block padding-1 border-bottom border-base-lighter text-no-underline width-full">
+                    className="text-base-dark display-block padding-1 border-bottom border-base-lighter text-no-underline width-full">
                     Delete
                 </Button>
             )}
