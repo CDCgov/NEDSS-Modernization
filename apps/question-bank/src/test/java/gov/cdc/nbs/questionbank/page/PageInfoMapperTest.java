74444102a33c6ef7004e99c2042c51a7c834dff4
Sheham Izat <sizat@enquizit.com>
2023-11-09 16:25:04 -0500

    Added test methods

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/page/PageInfoMapperTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/page/PageInfoMapperTest.java
@@ -4,10 +4,15 @@ import com.querydsl.core.Tuple;
 import gov.cdc.nbs.questionbank.entity.QCodeValueGeneral;
 import gov.cdc.nbs.questionbank.entity.QWaTemplate;
 import gov.cdc.nbs.questionbank.entity.condition.QConditionCode;
+import gov.cdc.nbs.questionbank.exception.QueryException;
 import gov.cdc.nbs.questionbank.page.model.PageInfo;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 class PageInfoMapperTest {
@@ -40,7 +45,39 @@ class PageInfoMapperTest {
     assertThat(actual.conditions().get(0).name()).isEqualTo("condition");
     assertThat(actual.messageMappingGuide().name()).isEqualTo("message mapping value");
     assertThat(actual.messageMappingGuide().value()).isEqualTo("message mapping name");
+  }
 
+  @Test
+  void mapPageInfoException() {
+    Tuple tuple = Mockito.mock(Tuple.class);
+    when(tuple.get(this.waTemplate.templateNm)).thenReturn("template name");
+    when(tuple.get(this.waTemplate.descTxt)).thenReturn("template description");
+    when(tuple.get(this.waTemplate.waTemplate.busObjType)).thenReturn("INV");
+    when(tuple.get(this.waTemplate.datamartNm)).thenReturn("data mart");
+    when(tuple.get(this.conditionCode.id)).thenReturn("1000");
+    when(tuple.get(this.conditionCode.conditionShortNm)).thenReturn("condition");
+    when(tuple.get(this.waTemplate.nndEntityIdentifier)).thenReturn("message mapping name");
+    when(tuple.get(this.codeValueGeneral.codeShortDescTxt)).thenReturn("message mapping value");
+    when(mapper.toPageInfo(tuple)).thenThrow(new ClassCastException("Error Converting Tuple to PageInfo"));
+    var exception = assertThrows(ClassCastException.class, () -> mapper.map(tuple));
+    assertTrue(exception.getMessage().contains("Error Converting Tuple to PageInfo"));
+
+  }
+
+  @Test
+  void getEventType() {
+    Map<String, String> eventTypeMap = Map.ofEntries(
+            Map.entry("INV", "Investigation"),
+            Map.entry("CON", "Contact"),
+            Map.entry("VAC", "Vaccination"),
+            Map.entry("IXS", "Interview"),
+            Map.entry("SUS", "Lab Susceptibility"),
+            Map.entry("LAB", "Lab Report"),
+            Map.entry("ISO", "Lab Isolate Tracking")
+    );
+    for(String value :  eventTypeMap.keySet()){
+      assertThat(mapper.getEventType(value).name()).isEqualTo(eventTypeMap.get(value));
+    }
   }
 
 
--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/page/download/PageControllerTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/page/download/PageControllerTest.java
@@ -5,7 +5,11 @@ import gov.cdc.nbs.questionbank.page.PageController;
 import gov.cdc.nbs.questionbank.page.PageCreator;
 import gov.cdc.nbs.questionbank.page.PageDownloader;
 import gov.cdc.nbs.questionbank.page.PageStateChanger;
+import gov.cdc.nbs.questionbank.page.model.PageInfo;
 import gov.cdc.nbs.questionbank.page.services.PageInfoFinder;
+import gov.cdc.nbs.questionbank.page.summary.search.PageSummary;
+import gov.cdc.nbs.questionbank.question.model.ConditionSummary;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -13,7 +17,12 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PageControllerTest {
@@ -29,16 +38,41 @@ class PageControllerTest {
     @Mock
     private PageInfoFinder pageInfoFinder;
 
+    private PageController pageController;
+    @BeforeEach
+    void setUp() {
+        pageController = new PageController(creator, stateChange, pageDownloader, userDetailsProvider, pageInfoFinder);
+    }
+
     @Test
     void downloadPageLibraryPDFTest() throws Exception {
-
-        PageController pageController = new PageController(creator, stateChange,
-                pageDownloader, userDetailsProvider,pageInfoFinder);
-
         byte[] resp = "pagedownloader".getBytes();
         Mockito.when(pageDownloader.downloadLibraryPDF())
                 .thenReturn(resp);
         ResponseEntity<byte[]> actual = pageController.downloadPageLibraryPDF();
         assertNotNull(actual);
     }
+
+    @Test
+    void getPageInfoDetails() throws Exception {
+        Long pageId = 1L;
+        PageInfo.EventType eventType = new PageInfo.EventType("value","name");
+        PageInfo.MessageMappingGuide messageMappingGuide = new PageInfo.MessageMappingGuide("100" ,"name");
+        List<ConditionSummary> conditions = Collections.EMPTY_LIST;
+
+        PageInfo expectedPageInfo = new PageInfo(eventType,"page name","description",
+                messageMappingGuide,conditions,"data mart");
+        Mockito.when(pageInfoFinder.find(pageId)).thenReturn(expectedPageInfo);
+        PageInfo result = pageController.getPageInfoDetails(pageId);
+        assertEquals(expectedPageInfo, result);
+    }
+
+    @Test
+    void getPageInfoDetailsException() throws Exception {
+        Long pageId = 1L;
+        when(pageInfoFinder.find(pageId)).thenThrow(new NullPointerException("No Page Info For That Id"));
+        var exception = assertThrows(NullPointerException.class, () -> pageController.getPageInfoDetails(pageId));
+        assertTrue(exception.getMessage().contains("No Page Info For That Id"));
+    }
+
 }
