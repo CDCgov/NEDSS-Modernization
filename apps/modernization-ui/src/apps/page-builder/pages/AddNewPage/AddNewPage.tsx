a1a501bf4d1cb04cf07c1138b3fb0dc3e9b6c283
Michael Peels <michaelpeels@Michael-Peels-4.local>
2023-11-09 18:03:56 -0500

    Add link to classic page creation for non Investigation types

--- apps/modernization-ui/src/apps/page-builder/pages/AddNewPage/AddNewPage.tsx
+++ apps/modernization-ui/src/apps/page-builder/pages/AddNewPage/AddNewPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Modal, ModalRef, ModalToggleButton } from '@trussworks/react-uswds';
+import { Button, Form, Modal, ModalRef } from '@trussworks/react-uswds';
 import { CreateCondition } from 'apps/page-builder/components/CreateCondition/CreateCondition';
 import { PagesBreadcrumb } from 'apps/page-builder/components/PagesBreadcrumb/PagesBreadcrumb';
 import { QuickConditionLookup } from 'apps/page-builder/components/QuickConditionLookup/QuickConditionLookup';
@@ -7,15 +7,13 @@ import { fetchConditions } from 'apps/page-builder/services/conditionAPI';
 import { createPage } from 'apps/page-builder/services/pagesAPI';
 import { fetchTemplates } from 'apps/page-builder/services/templatesAPI';
 import { fetchMMGOptions } from 'apps/page-builder/services/valueSetAPI';
-import { Input } from 'components/FormInputs/Input';
 import { SelectInput } from 'components/FormInputs/SelectInput';
-import { MultiSelectInput } from 'components/selection/multi';
 import { useContext, useEffect, useRef, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { Controller, FormProvider, useForm, useWatch } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import { UserContext } from 'user';
-import { PageBuilder } from '../PageBuilder/PageBuilder';
 import './AddNewPage.scss';
+import { AddNewPageFields } from './AddNewPageFields';
 
 type FormValues = {
     conditionIds: string[];
@@ -26,7 +24,7 @@ type FormValues = {
     pageDescription?: string;
     templateId: number;
 };
-type TEMPLATE = {
+type Template = {
     id: string;
     templateNm: string;
 };
@@ -42,15 +40,16 @@ const eventType = [
 ];
 
 export const AddNewPage = () => {
-    const modalQuick = useRef<ModalRef>(null);
-    const modalCreate = useRef<ModalRef>(null);
+    const conditionLookupModal = useRef<ModalRef>(null);
+    const createConditionModal = useRef<ModalRef>(null);
     const navigate = useNavigate();
     const { state } = useContext(UserContext);
     const token = `Bearer ${state.getToken()}`;
     const [conditions, setConditions] = useState<Condition[]>([]);
     const [mmgs, setMMGs] = useState<Concept[]>([]);
-    const [templates, setTemplates] = useState<TEMPLATE[]>([]);
-    const { handleSubmit, control, setValue, getValues } = useForm<FormValues, any>();
+    const [templates, setTemplates] = useState<Template[]>([]);
+    const form = useForm<FormValues>({ mode: 'onBlur' });
+    const watch = useWatch({ control: form.control });
 
     useEffect(() => {
         fetchMMGOptions(token)
@@ -69,10 +68,10 @@ export const AddNewPage = () => {
     }, [token]);
 
     const handleAddConditions = (conditions: string[]) => {
-        setValue('conditionIds', conditions.concat(getValues('conditionIds')));
+        form.setValue('conditionIds', conditions.concat(form.getValues('conditionIds')));
     };
 
-    const onSubmit = handleSubmit(async (data) => {
+    const onSubmit = form.handleSubmit(async (data) => {
         await createPage(
             token,
             data.conditionIds.filter(Boolean),
@@ -92,169 +91,97 @@ export const AddNewPage = () => {
         setConditions(conditions.concat([condition]));
 
         // select new condition
-        setValue('conditionIds', [condition.id].concat(getValues('conditionIds')));
+        form.setValue('conditionIds', [condition.id].concat(form.getValues('conditionIds')));
+    };
+
+    const eventTypeName = (value: string): string => {
+        for (let i = 0; i < eventType.length; i++) {
+            if (eventType[i].value === value) {
+                return eventType[i].name;
+            }
+        }
+        return '';
     };
 
     const handleCancel = () => {
         navigate(-1);
     };
 
+    const handleSubmit = () => {
+        if (watch.eventType !== undefined && watch.eventType != '' && watch.eventType !== 'INV') {
+            window.location.href = '/nbs/ManagePage.do?method=addPageLoad';
+        } else {
+            onSubmit();
+        }
+    };
+
     return (
-        <PageBuilder page="add-new-page">
-            <div className="add-new-page">
-                <div className="add-new-page__form">
-                    <Form onSubmit={onSubmit}>
-                        <PagesBreadcrumb />
-                        <div className="add-new-page__content">
-                            <h2>Add new page</h2>
-                            <h4>Let's fill out some information about your new page before creating it</h4>
-                            <p>All fields with * are required</p>
-                            <Controller
-                                control={control}
-                                name="conditionIds"
-                                render={({ field: { onChange, value } }) => (
-                                    <MultiSelectInput
-                                        onChange={onChange}
-                                        value={value}
-                                        options={conditions.map((m) => {
-                                            return {
-                                                name: m.conditionShortNm ?? '',
-                                                value: m.id
-                                            };
-                                        })}
-                                        label="Condition(s)"></MultiSelectInput>
-                                )}
-                            />
-                            <p>
-                                Can't find the condition you're looking for?
-                                <br />
-                                <ModalToggleButton modalRef={modalQuick} unstyled>
-                                    <p>Search and add condition(s)</p>
-                                </ModalToggleButton>
-                                &nbsp; or &nbsp;
-                                <ModalToggleButton modalRef={modalCreate} unstyled>
-                                    <p>create a new condition here</p>
-                                </ModalToggleButton>
-                            </p>
-                            <Controller
-                                control={control}
-                                name="name"
-                                rules={{ required: { value: true, message: 'Name is required.' } }}
-                                render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                    <Input
-                                        onChange={onChange}
-                                        defaultValue={value}
-                                        label="Page name"
-                                        type="text"
-                                        error={error?.message}
-                                        required
-                                    />
-                                )}
-                            />
-                            <Controller
-                                control={control}
-                                name="eventType"
-                                rules={{ required: { value: true, message: 'Event type is required.' } }}
-                                render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                    <SelectInput
-                                        label="Event type"
-                                        defaultValue={value}
-                                        onChange={onChange}
-                                        options={eventType}
-                                        error={error?.message}
-                                        required></SelectInput>
-                                )}
-                            />
-                            <Controller
-                                control={control}
-                                name="templateId"
-                                rules={{ required: { value: true, message: 'Template is required.' } }}
-                                render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                    <SelectInput
-                                        label="Templates"
-                                        defaultValue={value}
-                                        onChange={onChange}
-                                        options={templates.map((template) => {
-                                            return {
-                                                name: template.templateNm,
-                                                value: template.id
-                                            };
-                                        })}
-                                        error={error?.message}
-                                        required></SelectInput>
-                                )}
-                            />
-                            <p>
-                                Can't find the template you're looking for?
-                                <br />
-                                <NavLink to={'add/template'}>Import a new template here</NavLink>
-                            </p>
-                            <Controller
-                                control={control}
-                                name="messageMappingGuide"
-                                rules={{ required: { value: true, message: 'MMG is required.' } }}
-                                render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                    <SelectInput
-                                        label="MMG"
-                                        name="messageMappingGuide"
-                                        onChange={onChange}
-                                        defaultValue={value}
-                                        options={mmgs.map((m) => {
-                                            return {
-                                                name: m.display ?? '',
-                                                value: m.conceptCode ?? ''
-                                            };
-                                        })}
-                                        error={error?.message}
-                                        required></SelectInput>
-                                )}
-                            />
-                            <p>
-                                Would you like to add any additional information?
-                                <br />
-                                These fields are optional, you can make changes to this later.
-                            </p>
-                            <Controller
-                                control={control}
-                                name="pageDescription"
-                                render={({ field: { onChange, value } }) => (
-                                    <Input
-                                        onChange={(d: any) => {
-                                            onChange(d);
-                                        }}
-                                        label="Page description"
-                                        type="text"
-                                        multiline
-                                        defaultValue={value}
-                                    />
-                                )}
-                            />
-                            <Controller
-                                control={control}
-                                name="dataMartName"
-                                render={({ field: { onChange, value } }) => (
-                                    <Input
-                                        label="Data mart name"
-                                        type="text"
-                                        onChange={onChange}
-                                        defaultValue={value}
-                                    />
-                                )}
-                            />
-                        </div>
-                        <div className="add-new-page__buttons">
-                            <Button type="button" outline onClick={handleCancel}>
-                                Cancel
-                            </Button>
-                            <Button type="submit">Create page</Button>
-                        </div>
-                    </Form>
-                </div>
-                <Modal id="create-condition-modal" isLarge ref={modalCreate}>
-                    <CreateCondition conditionCreated={handleConditionCreated} modal={modalCreate} />
-                </Modal>
-                <QuickConditionLookup modal={modalQuick} addConditions={handleAddConditions} />
+        <div className="add-new-page">
+            <div className="breadcrumb-wrap">
+                <PagesBreadcrumb currentPage="Add new page" />
             </div>
-        </PageBuilder>
+            <Form onSubmit={onSubmit}>
+                <div className="add-new-page__form">
+                    <div className="add-new-page__content">
+                        <h2>Add new page</h2>
+                        <h4>Let's fill out some information about your new page before creating it</h4>
+                        <label className="fields-info">
+                            All fields with <span className="mandatory-indicator">*</span> are required
+                        </label>
+                        <Controller
+                            control={form.control}
+                            name="eventType"
+                            rules={{ required: { value: true, message: 'Event type is required.' } }}
+                            render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                <SelectInput
+                                    label="Event type"
+                                    value={value}
+                                    onChange={onChange}
+                                    options={eventType}
+                                    error={error?.message}
+                                    required
+                                />
+                            )}
+                        />
+                        {watch.eventType == undefined || watch.eventType == '' ? (
+                            <></>
+                        ) : (
+                            <>
+                                {watch.eventType == 'INV' ? (
+                                    <FormProvider {...form}>
+                                        <AddNewPageFields
+                                            conditions={conditions}
+                                            templates={templates}
+                                            mmgs={mmgs}
+                                            createConditionModal={createConditionModal}
+                                            conditionLookupModal={conditionLookupModal}
+                                        />
+                                    </FormProvider>
+                                ) : (
+                                    <div className="unsupported-event-type-message">
+                                        {eventTypeName(watch.eventType).toUpperCase()} event type is not supported by
+                                        the modern page design. Please click "Create page" to continue in classic design
+                                        mode.
+                                    </div>
+                                )}
+                            </>
+                        )}
+                    </div>
+                </div>
+                <div className="add-new-page__buttons">
+                    <Button type="button" outline onClick={handleCancel}>
+                        Cancel
+                    </Button>
+                    <Button type="button" onClick={handleSubmit}>
+                        Create page
+                    </Button>
+                </div>
+            </Form>
+
+            <Modal id="create-condition-modal" isLarge ref={createConditionModal}>
+                <CreateCondition conditionCreated={handleConditionCreated} modal={createConditionModal} />
+            </Modal>
+            <QuickConditionLookup modal={conditionLookupModal} addConditions={handleAddConditions} />
+        </div>
     );
 };
