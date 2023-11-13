f322e0fddae5754aba6c56b7076630f6c7680843
Michael Peels <michaelpeels@Michael-Peels-4.local>
2023-11-13 13:51:21 -0500

    Add test

---
+++ apps/modernization-ui/src/apps/page-builder/pages/PageBuilder/PageBuilder.spec.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter } from 'react-router-dom';
+import { PageBuilder } from './PageBuilder';
+import { render } from '@testing-library/react';
+
+describe('when rendered', () => {
+    it('should display side nav', async () => {
+        const { container } = render(
+            <BrowserRouter>
+                <PageBuilder nav>Child</PageBuilder>
+            </BrowserRouter>
+        );
+
+        const sideNav = container.getElementsByClassName('page-builder-side-nav')[0];
+        expect(sideNav).toBeInTheDocument();
+    });
+
+    it('should not side nav', async () => {
+        const { container } = render(
+            <BrowserRouter>
+                <PageBuilder>Child</PageBuilder>
+            </BrowserRouter>
+        );
+
+        const sideNav = container.getElementsByClassName('page-builder-side-nav')[0];
+        expect(sideNav).toBeUndefined();
+    });
+});
