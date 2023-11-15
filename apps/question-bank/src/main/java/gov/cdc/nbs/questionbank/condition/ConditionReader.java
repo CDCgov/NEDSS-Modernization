fe934b83bf9b4ebee6a9da1a739d838d8dbed462
Alaap Bharadwaj <abharadwaj@enquizit.com>
2023-11-15 15:08:32 -0500

    possible solution

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/ConditionReader.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/ConditionReader.java
@@ -11,6 +11,7 @@ import gov.cdc.nbs.questionbank.entity.condition.QConditionCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -33,6 +34,7 @@ public class ConditionReader {
 
 
     public Page<Condition> searchCondition(ReadConditionRequest request, Pageable pageable) {
+        Pageable test = PageRequest.of(pageable.getPageNumber()-1, pageable.getPageSize());
         if (request.getSearchText() != null && !request.getSearchText().trim().isEmpty()) {
             BooleanBuilder predicate = new BooleanBuilder();
             BooleanExpression searchPredicate = QConditionCode.conditionCode.id.eq(request.getSearchText())
