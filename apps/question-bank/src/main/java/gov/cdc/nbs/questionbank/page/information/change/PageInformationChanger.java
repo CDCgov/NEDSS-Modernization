5ed088ce158b960652cdfd41d18c6a5a015c02ac
Adam Loup <aloup@enquizit.com>
2023-11-15 10:12:27 -0600

    sonar

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/page/information/change/PageInformationChanger.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/page/information/change/PageInformationChanger.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Consumer;
 
 @Component
 class PageInformationChanger {
