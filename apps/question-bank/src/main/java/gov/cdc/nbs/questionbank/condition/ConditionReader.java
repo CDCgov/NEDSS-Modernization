6a74dacaebd3ee42c5586ddf30db51e7fe232894
Alaap Bharadwaj <abharadwaj@enquizit.com>
2023-11-16 10:03:21 -0500

    unused iimport

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/ConditionReader.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/ConditionReader.java
@@ -11,7 +11,6 @@ import gov.cdc.nbs.questionbank.entity.condition.QConditionCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
