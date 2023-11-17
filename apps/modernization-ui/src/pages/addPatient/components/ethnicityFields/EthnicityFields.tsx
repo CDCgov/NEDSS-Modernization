2ee8274360bb6e66f4329b6c18a91224c8148534
hclarkEnq <hclark@enquizit.com>
2023-11-17 08:47:07 -0800

    removed use of grid for race and ethnicity and added padding

--- apps/modernization-ui/src/pages/addPatient/components/ethnicityFields/EthnicityFields.tsx
+++ apps/modernization-ui/src/pages/addPatient/components/ethnicityFields/EthnicityFields.tsx
@@ -1,5 +1,5 @@
 import { Controller, useFormContext } from 'react-hook-form';
-import { Grid, Radio } from '@trussworks/react-uswds';
+import { Radio } from '@trussworks/react-uswds';
 import FormCard from 'components/FormCard/FormCard';
 import { CodedValue } from 'coded';
 
@@ -14,29 +14,25 @@ export default function EthnicityFields({ id, title, coded }: Props) {
 
     return (
         <FormCard id={id} title={title}>
-            <Grid col={12} className="padding-x-3 padding-bottom-3 padding-top-2">
-                <Grid row>
-                    <Grid col={12}>
-                        {coded.ethnicGroups.map((ethnicity, key) => (
-                            <Controller
-                                key={key}
-                                control={control}
-                                name="ethnicity"
-                                render={({ field: { onChange, name, value } }) => (
-                                    <Radio
-                                        onChange={onChange}
-                                        defaultChecked={value === ethnicity.value}
-                                        value={ethnicity.value}
-                                        id={ethnicity.value}
-                                        name={name}
-                                        label={ethnicity.name}
-                                    />
-                                )}
+            <div className="fields">
+                {coded.ethnicGroups.map((ethnicity, key) => (
+                    <Controller
+                        key={key}
+                        control={control}
+                        name="ethnicity"
+                        render={({ field: { onChange, name, value } }) => (
+                            <Radio
+                                onChange={onChange}
+                                defaultChecked={value === ethnicity.value}
+                                value={ethnicity.value}
+                                id={ethnicity.value}
+                                name={name}
+                                label={ethnicity.name}
                             />
-                        ))}
-                    </Grid>
-                </Grid>
-            </Grid>
+                        )}
+                    />
+                ))}
+            </div>
         </FormCard>
     );
 }
