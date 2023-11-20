41310ed765f99acb3355df83c4543f92d7a69124
Richard Krussel <139483220+rkrusselenq@users.noreply.github.com>
2023-11-20 17:07:55 -0600

    init (#737)

--- apps/modernization-ui/src/apps/page-builder/pages/AddNewPage/AddNewPage.tsx
+++ apps/modernization-ui/src/apps/page-builder/pages/AddNewPage/AddNewPage.tsx
@@ -184,7 +184,7 @@ export const AddNewPage = () => {
                 </div>
             </Form>
 
-            <Modal id="create-condition-modal" isLarge ref={createConditionModal}>
+            <Modal id="create-condition-modal" isLarge ref={createConditionModal} title="Create new condition">
                 <CreateCondition conditionCreated={handleConditionCreated} modal={createConditionModal} />
             </Modal>
             <Modal id="import-template-modal" isLarge ref={importTemplateModal}>
