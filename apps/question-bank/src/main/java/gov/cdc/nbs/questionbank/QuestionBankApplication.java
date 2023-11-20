b87bca15f56970f0191b2406b4a62eda6cc27d3e
Michael Peels <109251240+mpeels@users.noreply.github.com>
2023-11-20 15:21:17 -0500

    Redirection Lib (#739)
    * Add default redirect path
    
    * Update gateway to point to page-builder API for redirect
    
    * rename var
    
    * Nbs-gateway test
    
    * Add page-builder redirect to traefik
    
    * traefik route
    
    * Remove duplicate class
    
    * Add ApiIgnore
    
    * locally point to reverse proxy for routing
    
    * Remove Qualifer
    
    ---------
    
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels-4.local>

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/QuestionBankApplication.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/QuestionBankApplication.java
@@ -1,15 +1,17 @@
 package gov.cdc.nbs.questionbank;
 
-import gov.cdc.nbs.authentication.EnableNBSAuthentication;
-import gov.cdc.nbs.id.EnableNBSIdGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import gov.cdc.nbs.authentication.EnableNBSAuthentication;
+import gov.cdc.nbs.id.EnableNBSIdGenerator;
+import gov.cdc.nbs.redirect.EnableRedirection;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableNBSAuthentication
 @EnableNBSIdGenerator
 @EnableOpenApi
+@EnableRedirection
 public class QuestionBankApplication {
 
   public static void main(String[] args) {
