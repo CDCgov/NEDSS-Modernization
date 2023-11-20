1b16c206c37bcca075ec314a376f0a5efb955496
Michael Peels <michaelpeels@Michael-Peels-4.local>
2023-11-20 12:04:29 -0500

    Add ApiIgnore

--- libs/redirect/src/main/java/gov/cdc/nbs/redirect/RedirectController.java
+++ libs/redirect/src/main/java/gov/cdc/nbs/redirect/RedirectController.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+@ApiIgnore
 @RestController
 public class RedirectController {
 
