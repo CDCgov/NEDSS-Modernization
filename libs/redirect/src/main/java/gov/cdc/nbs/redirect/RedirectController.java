d16c6522ede06b5d3ee3ec493650bbb906e44565
Michael Peels <michaelpeels@Michael-Peels-4.local>
2023-11-20 10:21:19 -0500

    Add default redirect path

---
+++ libs/redirect/src/main/java/gov/cdc/nbs/redirect/RedirectController.java
@@ -0,0 +1,27 @@
+package gov.cdc.nbs.redirect;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedirectController {
+
+  private final DefaultRedirectionPath defaultRedirection;
+
+  RedirectController(final DefaultRedirectionPath defaultRedirection) {
+    this.defaultRedirection = defaultRedirection;
+  }
+
+  @GetMapping("/redirect")
+  public ResponseEntity<Void> redirect(HttpServletRequest request) {
+    String location = request.getHeader(HttpHeaders.LOCATION);
+    if (location == null) {
+      location = defaultRedirection.path();
+    }
+    return ResponseEntity.status(HttpStatus.FOUND).header(HttpHeaders.LOCATION, location).build();
+  }
+}
