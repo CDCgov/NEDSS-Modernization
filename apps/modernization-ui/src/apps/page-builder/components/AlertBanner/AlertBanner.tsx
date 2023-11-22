c8b2d702a5348a1d2e08469c4893199ea84c5267
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2023-11-22 12:11:46 -0600

    CNFT2-1468 Manage Tabs - Delete tab in modal
    Add $clear, styling
    
    cleanup

--- apps/modernization-ui/src/apps/page-builder/components/AlertBanner/AlertBanner.tsx
+++ apps/modernization-ui/src/apps/page-builder/components/AlertBanner/AlertBanner.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './AlertBanner.scss';
 import { Icon } from '@trussworks/react-uswds';
 
@@ -5,11 +6,22 @@ export type AlertBannerProps = {
     type?: string;
     children?: any;
     onClose?: () => void;
+    expiration?: number;
 };
 
-export const AlertBanner = ({ type, children, onClose }: AlertBannerProps) => {
+export const AlertBanner = ({ type, children, onClose, expiration }: AlertBannerProps) => {
+    const [hidden, setHidden] = useState(false);
+
+    useEffect(() => {
+        if (expiration) {
+            setTimeout(() => {
+                setHidden(true);
+            }, expiration);
+        }
+    }, []);
+
     return (
-        <div className={`alert-banner ${type}`}>
+        <div className={`alert-banner ${type} ${hidden ? 'hidden' : ''}`}>
             <div className="alert-banner__left">
                 {type === 'success' && <Icon.CheckCircle size={3} />}
                 {type === 'warning' && <Icon.Warning size={3} />}
@@ -17,7 +29,9 @@ export const AlertBanner = ({ type, children, onClose }: AlertBannerProps) => {
                 {type === 'info' && <Icon.Info size={3} />}
                 {type === 'error' && <Icon.Error size={5} />}
             </div>
-            <div className="alert-banner__right">{children}</div>
+            <div className="alert-banner__right">
+                <p>{children}</p>
+            </div>
             {onClose ? (
                 <div className="alert-banner__close" onClick={() => onClose()}>
                     <Icon.Close />
--- apps/modernization-ui/src/apps/page-builder/pages/EditPage/ManageTabs/ManageTabs.tsx
+++ apps/modernization-ui/src/apps/page-builder/pages/EditPage/ManageTabs/ManageTabs.tsx
@@ -1,28 +1,60 @@
-import React from 'react';
-import { PagesTab, Tab } from 'apps/page-builder/generated';
+// import { useState } from 'react';
+import { PagesTab } from 'apps/page-builder/generated';
 import './ManageTabs.scss';
 import { Icon } from '@trussworks/react-uswds';
 import { Icon as IconComponent } from 'components/Icon/Icon';
 
 type Props = {
-    tabs: Tab[];
+    tabs: PagesTab[];
     setSelectedEditTab: (tab: PagesTab, index: number) => void;
+    setDeleteTab: (tab: PagesTab) => void;
+    selectedForDelete: PagesTab | undefined;
+    setSelectedForDelete: (tab: PagesTab | undefined) => void;
+    reset: () => void;
 };
 
-const ManageTabs = ({ tabs, setSelectedEditTab }: Props) => {
+const ManageTabs = ({
+    tabs,
+    setSelectedEditTab,
+    setDeleteTab,
+    selectedForDelete,
+    setSelectedForDelete,
+    reset
+}: Props) => {
     return (
         <div className="manage-tabs">
             {tabs.map((tab, i) => {
                 return (
-                    <div key={i} className="manage-tabs__tile" style={{ listStyle: 'none' }}>
-                        <div className="manage-tabs__label">
+                    <div
+                        key={i}
+                        className={`manage-tabs__tile ${tab.id === selectedForDelete?.id ? 'delete' : ''}`}
+                        style={{ listStyle: 'none' }}>
+                        <div className="manage-tabs__handle">
                             <IconComponent name="drag" />
+                            <p>Delete this tab?</p>
+                        </div>
+                        <div className="manage-tabs__label">
                             <IconComponent name="folder" />
                             {tab.name}
                         </div>
-                        <div className="manage-tabs__buttons">
-                            <Icon.Edit onClick={() => setSelectedEditTab(tab, i)} size={3} />
-                            <Icon.Delete size={3} />
+                        <div className={`manage-tabs__buttons ${tab.sections?.length !== 0 ? 'locked' : ''}`}>
+                            <Icon.Edit
+                                onClick={() => {
+                                    reset();
+                                    setSelectedEditTab(tab, i);
+                                }}
+                                size={3}
+                            />
+                            <Icon.Delete
+                                onClick={() => {
+                                    tab.sections?.length === 0 ? (reset(), setSelectedForDelete(tab)) : null;
+                                }}
+                                size={3}
+                            />
+                            <div className="delete">
+                                <p onClick={() => setSelectedForDelete(undefined)}>Cancel</p>
+                                <p onClick={() => setDeleteTab(selectedForDelete!)}>Delete</p>
+                            </div>
                         </div>
                     </div>
                 );
