a225fc5beef88236e01d8670e046df1ada45dd80
Michael Davis <michaeldavis@michaels-mbp.mynetworksettings.com>
2023-11-27 12:12:30 -0500

    padding removed from parent editable card div to make borders full

--- apps/modernization-ui/src/components/EditableCard/EditableCard.tsx
+++ apps/modernization-ui/src/components/EditableCard/EditableCard.tsx
@@ -27,7 +27,7 @@ export const EditableCard = ({ title, data, children, editing = false, onEdit, r
                     </Button>
                 )}
             </div>
-            <div className="padding-2">
+            <div>
                 {!editing &&
                     data?.map((item, index) => (
                         <Grid row key={index} className="padding-x-2 padding-y-3 border-bottom border-base-lighter wall-design">
