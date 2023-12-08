b7b911a8fa5172e6c737c402edd59e7e24837484
Adam Loup <aloup@enquizit.com>
2023-12-08 12:47:17 -0600

    condition options api

--- libs/options-api/src/test/java/gov/cdc/nbs/option/concept/ConceptOptionsVerificationSteps.java
+++ libs/options-api/src/test/java/gov/cdc/nbs/option/concept/ConceptOptionsVerificationSteps.java
@@ -1,4 +1,4 @@
-package gov.cdc.nbs.option;
+package gov.cdc.nbs.option.concept;
 
 import gov.cdc.nbs.testing.support.Active;
 import io.cucumber.java.en.Then;
@@ -7,42 +7,42 @@ import org.springframework.test.web.servlet.ResultActions;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-public class OptionsVerificationSteps {
+public class ConceptOptionsVerificationSteps {
 
 
   private final Active<ResultActions> response;
 
-  OptionsVerificationSteps(final Active<ResultActions> response) {
+  ConceptOptionsVerificationSteps(final Active<ResultActions> response) {
     this.response = response;
   }
 
-  @Then("there are {int} option(s) included")
+  @Then("there are {int} concept(s) included")
   public void there_are_n_options(final int n) throws Exception {
     this.response.active()
-        .andExpect(jsonPath("$", hasSize(n)));
+        .andExpect(jsonPath("$.options", hasSize(n)));
   }
 
-  @Then("the option named {string} is included")
+  @Then("the concept named {string} is included")
   public void the_named_option_is_included(final String concept) throws Exception {
     this.response.active()
-        .andExpect(jsonPath("$.[*].name", hasItem(equalToIgnoringCase(concept))));
+        .andExpect(jsonPath("$.options[*].name", hasItem(equalToIgnoringCase(concept))));
   }
 
-  @Then("the option named {string} is not included")
+  @Then("the concept named {string} is not included")
   public void the_option_named_is_not_included(final String concept) throws Exception {
     this.response.active()
-        .andExpect(jsonPath("$.[*].name", hasItem(not(equalToIgnoringCase(concept)))));
+        .andExpect(jsonPath("$.options[*].name", hasItem(not(equalToIgnoringCase(concept)))));
   }
 
-  @Then("there are options available")
+  @Then("there are concepts available")
   public void there_are_options_available() throws Exception {
     this.response.active()
-        .andExpect(jsonPath("$", not(hasSize(0))));
+        .andExpect(jsonPath("$.options[*]", not(hasSize(0))));
   }
 
-  @Then("there aren't any options available")
+  @Then("there aren't any concepts available")
   public void there_are_not_any_options() throws Exception {
     this.response.active()
-        .andExpect(jsonPath("$", hasSize(0)));
+        .andExpect(jsonPath("$.options[*]", hasSize(0)));
   }
 }
