52e63e319c07802ade14099f79395dca49a7c8d7
Adam Loup <aloup@enquizit.com>
2023-12-08 13:06:25 -0600

    remove unused import

--- libs/options-api/src/main/java/gov/cdc/nbs/option/concept/ConceptOptionsResponse.java
+++ libs/options-api/src/main/java/gov/cdc/nbs/option/concept/ConceptOptionsResponse.java
@@ -1,7 +1,6 @@
 package gov.cdc.nbs.option.concept;
 
 import gov.cdc.nbs.option.Option;
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.Collection;
