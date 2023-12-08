e2ce0f662f1dd93fcf3275f7af93492c7d209787
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2023-12-08 13:34:51 -0600

    [CNFT2-1511] Filter page library by event names (#781)
    * corrected return type
    
    * event type
    
    * remove unused import

--- apps/modernization-ui/src/options/autocompete/useConceptOptionsAutocomplete.ts
+++ apps/modernization-ui/src/options/autocompete/useConceptOptionsAutocomplete.ts
@@ -1,81 +1,39 @@
-import { ConceptOptionsService, Option } from 'generated';
-import debounce from 'lodash.debounce';
-import { useCallback, useEffect, useReducer } from 'react';
-import { useUser } from 'user';
+import { authorization } from 'authorization';
+import {
+    AutocompleteOptionsResolver,
+    SelectableAutocompletion,
+    useSelectableAutocomplete
+} from './useSelectableAutocomplete';
+import { ConceptOptionsService } from 'generated';
 
-type State =
-    | { status: 'idle'; criteria?: string }
-    | { status: 'suggested'; criteria?: string; options: Option[] }
-    | { status: 'suggesting'; criteria: string; limit: number };
-
-type Action =
-    | { type: 'reset'; criteria?: string }
-    | { type: 'suggest'; criteria: string; limit: number }
-    | { type: 'suggested'; suggestions: Option[] };
-
-const reducer = (state: State, action: Action): State => {
-    switch (action.type) {
-        case 'suggest':
-            //  only suggest if the criteria has changed
-            return state.criteria === action.criteria
-                ? state
-                : { ...initial, status: 'suggesting', criteria: action.criteria, limit: action.limit };
-        case 'suggested':
-            return { ...state, status: 'suggested', options: action.suggestions };
-        case 'reset':
-            return { status: 'idle', criteria: action.criteria };
-        default:
-            return initial();
-    }
+type Settings = {
+    initialCriteria?: string;
+    limit?: number;
 };
 
-const initial = (criteria?: string): State => ({
-    status: 'idle',
-    criteria
-});
+const useConceptOptionsAutocomplete = (
+    valueSet: string,
+    settings: Settings = { initialCriteria: '' }
+): SelectableAutocompletion => {
+    const resolver: AutocompleteOptionsResolver = (criteria: string, limit?: number) =>
+        ConceptOptionsService.specificUsingGet({
+            authorization: authorization(),
+            name: valueSet,
+            criteria: criteria,
+            limit: limit
+        }).then((response) => response.options);
 
-type AutocompleteResolver = (criteria?: string, limit?: number) => void;
-
-type ConceptOptions = {
-    criteria?: string;
-    options: Option[];
-    autocomplete: AutocompleteResolver;
-    reset: (criteria?: string) => void;
-};
-
-type Options = {
-    valueSet: string;
-    criteria?: string;
-};
-
-const useConceptOptionsAutocomplete = ({ valueSet, criteria = '' }: Options): ConceptOptions => {
-    const {
-        state: { getToken }
-    } = useUser();
-
-    const [state, dispatch] = useReducer(reducer, criteria, initial);
-
-    useEffect(() => {
-        if (state.status === 'suggesting') {
-            ConceptOptionsService.specificUsingGet({
-                authorization: `Bearer ${getToken()}`,
-                name: valueSet,
-                criteria: state.criteria,
-                limit: state.limit
-            })
-                .then((response) => response.options ?? [])
-                .then((options) => dispatch({ type: 'suggested', suggestions: options }));
-        }
-    }, [state.status]);
-
-    const options = state.status === 'suggested' ? state.options : [];
-    const autocomplete = (criteria = '', limit = 15) => dispatch({ type: 'suggest', criteria, limit });
-    const reset = (criteria?: string) => dispatch({ type: 'reset', criteria });
+    const { criteria, options, suggest, complete, reset } = useSelectableAutocomplete({
+        resolver,
+        criteria: settings.initialCriteria,
+        limit: settings.limit
+    });
 
     return {
-        criteria: state.criteria,
+        criteria,
         options,
-        autocomplete: useCallback(debounce(autocomplete, 600), []),
+        suggest,
+        complete,
         reset
     };
 };
