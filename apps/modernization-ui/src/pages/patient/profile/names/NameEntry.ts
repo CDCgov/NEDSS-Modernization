dbbde172344836514755c8e5555a615baf38dd32
Michael Davis <125376056+mdavis-enquizit@users.noreply.github.com>
2023-12-12 14:56:41 -0500

    CNFT1-1715-Patient-name table consistency (#785)
    * Profile name table working
    
    * Test for nameTransformer
    
    * NameSorter Table Spec added
    
    * Sort UI changes to Names Table
    
    * Fix sort
    
    * Fetch after alert
    
    * function used for full name
    
    ---------
    
    Co-authored-by: Michael Davis <michaeldavis@michaels-mbp.mynetworksettings.com>

--- apps/modernization-ui/src/pages/patient/profile/names/NameEntry.ts
+++ apps/modernization-ui/src/pages/patient/profile/names/NameEntry.ts
@@ -1,3 +1,5 @@
+import { PatientNameDegree, PatientNamePrefix, PatientNameSuffix, PatientNameUse } from 'generated/graphql/schema';
+
 export type NameEntry = {
     patient: number;
     sequence: number | null;
@@ -11,4 +13,30 @@ export type NameEntry = {
     secondLast: string | null;
     suffix: string | null;
     degree: string | null;
+};
+
+export enum Column {
+    AsOf = 'As of',
+    Use = 'Type',
+    Prefix = 'Prefix',
+    Name = 'Name (last, first middle)',
+    Suffix = 'Suffix',
+    Degree = 'Degree',
+    Actions = 'Actions'
+}
+
+export type Name = {
+    asOf: Date;
+    use: PatientNameUse;
+    prefix?: PatientNamePrefix | null;
+    first?: string | null;
+    middle?: string | null;
+    secondMiddle?: string | null;
+    last?: string | null;
+    secondLast?: string | null;
+    suffix?: PatientNameSuffix | null;
+    degree?: PatientNameDegree | null;
+    patient: number;
+    sequence: number;
+    version: number | null;
 };
