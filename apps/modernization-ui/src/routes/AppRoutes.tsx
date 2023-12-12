97716f99aa972a0400950180fbc05718d51e5bc7
Michael Peels <michaelpeels@Michael-Peels-Work-6.local>
2023-12-12 16:24:50 -0500

    Fix import

--- apps/modernization-ui/src/routes/AppRoutes.tsx
+++ apps/modernization-ui/src/routes/AppRoutes.tsx
@@ -21,7 +21,7 @@ import { CreateQuestion } from 'apps/page-builder/components/CreateQuestion/Crea
 import { BusinessRulesLibrary } from 'apps/page-builder/pages/BusinessRulesLibrary/BusinessRulesLibrary';
 import { QuestionLibrary } from 'apps/page-builder/pages/QuestionLibrary/QuestionLibrary';
 import { ValuesetLibrary } from 'apps/page-builder/pages/ValuesetLibrary/ValuesetLibrary';
-import { Edit } from 'apps/page-builder/page/management/edit/Edit';
+import { Edit } from 'apps/page-builder/page/management/Edit/Edit';
 
 const ScrollToTop = ({ children }: { children: ReactNode }) => {
     const location = useLocation();
