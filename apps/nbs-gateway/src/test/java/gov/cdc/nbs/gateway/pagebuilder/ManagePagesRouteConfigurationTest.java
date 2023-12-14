587318a4b7ad4e55a30be09761d4aed23e1bbb8e
Michael Peels <109251240+mpeels@users.noreply.github.com>
2023-12-14 17:02:02 -0500

    Updates route for page library (#804)
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels-Work-6.local>

--- apps/nbs-gateway/src/test/java/gov/cdc/nbs/gateway/pagebuilder/ManagePagesRouteConfigurationTest.java
+++ apps/nbs-gateway/src/test/java/gov/cdc/nbs/gateway/pagebuilder/ManagePagesRouteConfigurationTest.java
@@ -56,7 +56,7 @@ class ManagePagesRouteConfigurationTest {
   void should_route_to_modernized() {
 
     pagebuilderApi.stubFor(get(urlPathMatching("/nbs/page-builder/redirect"))
-        .withHeader("Location", equalTo("/page-builder/manage/pages"))
+        .withHeader("Location", equalTo("/page-builder/pages"))
         .willReturn(ok()));
     webClient
         .get().uri(
@@ -73,7 +73,7 @@ class ManagePagesRouteConfigurationTest {
   void should_route_to_modernized_initLoad() {
 
     pagebuilderApi.stubFor(get(urlPathMatching("/nbs/page-builder/redirect"))
-        .withHeader("Location", equalTo("/page-builder/manage/pages"))
+        .withHeader("Location", equalTo("/page-builder/pages"))
         .willReturn(ok()));
     webClient
         .get().uri(
