06cea04f504991db6e2e4bb68ac169223eff3f51
Richard <rkrussel@enquizit.com>
2023-12-19 13:50:38 -0600

    fix tests

--- apps/modernization-ui/src/apps/page-builder/pages/BusinessRulesLibrary/BusinessRulesLibraryTable.spec.tsx
+++ apps/modernization-ui/src/apps/page-builder/pages/BusinessRulesLibrary/BusinessRulesLibraryTable.spec.tsx
@@ -1,47 +1,58 @@
 import { render } from '@testing-library/react';
 import { BusinessRulesLibraryTable } from './BusinessRulesLibraryTable';
+import { BrowserRouter } from 'react-router-dom';
 
-describe('when rendered', () => {
-    it('should display sentence cased headers', async () => {
-        const dataSummary: any = {};
-        const summaries = [dataSummary];
-        const { getAllByRole } = render(<BusinessRulesLibraryTable summaries={summaries} />);
+describe('BusinessRulesLibraryTable', () => {
+    describe('when rendered', () => {
+        it('should display sentence cased headers', async () => {
+            const dataSummary: any = {};
+            const summaries = [dataSummary];
+            const { getAllByRole } = render(
+                <BrowserRouter>
+                    <BusinessRulesLibraryTable summaries={summaries} />
+                </BrowserRouter>
+            );
 
-        const tableHeads = getAllByRole('columnheader');
+            const tableHeads = getAllByRole('columnheader');
 
-        expect(tableHeads[0]).toHaveTextContent('Source Fields');
-        expect(tableHeads[1]).toHaveTextContent('Logic');
-        expect(tableHeads[2]).toHaveTextContent('Values');
-        expect(tableHeads[3]).toHaveTextContent('Function');
-        expect(tableHeads[4]).toHaveTextContent('Target Fields');
-        expect(tableHeads[5]).toHaveTextContent('ID');
+            expect(tableHeads[0]).toHaveTextContent('Source Fields');
+            expect(tableHeads[1]).toHaveTextContent('Logic');
+            expect(tableHeads[2]).toHaveTextContent('Values');
+            expect(tableHeads[3]).toHaveTextContent('Function');
+            expect(tableHeads[4]).toHaveTextContent('Target Fields');
+            expect(tableHeads[5]).toHaveTextContent('ID');
+        });
     });
-});
 
-describe('when at least one summary is available', () => {
-    const rulesSummary: any = {
-        ruleId: 6376,
-        templateUid: 1000272,
-        ruleFunction: 'Enable',
-        ruleDescription: null,
-        sourceIdentifier: 'ARB001',
-        sourceValue: ['Dengue virus'],
-        comparator: '=',
-        targetType: 'QUESTION',
-        errorMsgText: 'Type of Arbovirus = must be ( Dengue virus ) Dengue (DENV) Serotype',
-        targetValueIdentifier: ['404400']
-    };
-    const summaries = [rulesSummary];
+    describe('when at least one summary is available', () => {
+        const rulesSummary: any = {
+            ruleId: 6376,
+            templateUid: 1000272,
+            ruleFunction: 'Enable',
+            ruleDescription: null,
+            sourceIdentifier: 'ARB001',
+            sourceValue: ['Dengue virus'],
+            comparator: '=',
+            targetType: 'QUESTION',
+            errorMsgText: 'Type of Arbovirus = must be ( Dengue virus ) Dengue (DENV) Serotype',
+            targetValueIdentifier: ['404400']
+        };
+        const summaries = [rulesSummary];
 
-    it('should display the Business rules summaries', async () => {
-        const { findAllByRole } = render(<BusinessRulesLibraryTable summaries={summaries} />);
+        it('should display the Business rules summaries', async () => {
+            const { findAllByRole } = render(
+                <BrowserRouter>
+                    <BusinessRulesLibraryTable summaries={summaries} />
+                </BrowserRouter>
+            );
 
-        const tableData = await findAllByRole('cell');
-        expect(tableData[0]).toHaveTextContent('ARB001');
-        expect(tableData[1]).toHaveTextContent('Equal to');
-        expect(tableData[2]).toHaveTextContent('Dengue virus');
-        expect(tableData[3]).toHaveTextContent('Enable');
-        expect(tableData[4]).toHaveTextContent('404400');
-        expect(tableData[5]).toHaveTextContent('6376');
+            const tableData = await findAllByRole('cell');
+            expect(tableData[0]).toHaveTextContent('ARB001');
+            expect(tableData[1]).toHaveTextContent('Equal to');
+            expect(tableData[2]).toHaveTextContent('Dengue virus');
+            expect(tableData[3]).toHaveTextContent('Enable');
+            expect(tableData[4]).toHaveTextContent('404400');
+            expect(tableData[5]).toHaveTextContent('6376');
+        });
     });
 });
