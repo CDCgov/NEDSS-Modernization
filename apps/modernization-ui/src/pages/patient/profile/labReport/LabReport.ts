94121bbcdc0f35ff131e6bbc7da00a88b260da75
Michael Davis <michaeldavis@michaels-mbp.mynetworksettings.com>
2023-12-19 08:06:14 -0500

    Module Sorter and Transfomer for LabReport

---
+++ apps/modernization-ui/src/pages/patient/profile/labReport/LabReport.ts
@@ -0,0 +1,36 @@
+type Maybe<T> = T | null;
+
+export enum Headers {
+    DateReceived = 'Date received',
+    FacilityProvider = 'Facility / provider',
+    DateCollected = 'Date collected',
+    TestResults = 'Test results',
+    AssociatedWith = 'Associated with',
+    ProgramArea = 'Program area',
+    Jurisdiction = 'Jurisdiction',
+    EventID = 'Event #'
+}
+
+export type LabReport = {
+    observationUid?: string | any;
+    lastChange?: Date | null;
+    classCd?: Maybe<string>;
+    moodCd?: Maybe<string> | undefined;
+    observationLastChgTime?: Maybe<string> | undefined;
+    cdDescTxt?: Maybe<string> | undefined;
+    recordStatusCd?: Maybe<string> | undefined;
+    programAreaCd?:  Maybe<string> | undefined;
+    jurisdictionCd?: Maybe<number> | undefined;
+    jurisdictionCodeDescTxt?: Maybe<string> | undefined;
+    pregnantIndCd?: Maybe<string> | undefined;
+    localId?: Maybe<string> | undefined;
+    activityToTime?: Maybe<string> | undefined;
+    effectiveFromTime?: Maybe<string> | undefined;
+    rptToStateTime?: Maybe<string> | undefined;
+    addTime?: Date | null;
+    electronicInd?: Maybe<string> | undefined;
+    versionCtrlNbr?: Maybe<number> | undefined;
+    addUserId?: Maybe<number> | undefined;
+    lastChgUserId?: Maybe<number> | undefined;
+    actIds?: Maybe<string> | undefined | any;
+};
---
+++ apps/modernization-ui/src/pages/patient/profile/labReport/LabReportTransformer.ts
@@ -0,0 +1,33 @@
+import { asLocalDate } from 'date';
+import { LabReport } from './LabReport';
+import { FindLabReportsForPatientQuery, PatientLabReport } from 'generated/graphql/schema';
+
+type Result = FindLabReportsForPatientQuery['findLabReportsForPatient'];
+
+type Content = PatientLabReport;
+
+const internalized = (content: Content): LabReport | null => {  
+    return content
+    return (
+        content && {
+            ...content,
+            addTime: content.addTime && asLocalDate(content.addTime)
+        }
+    );
+};
+
+export const transform = (result: Result): LabReport[] => {
+    if (result) {
+        return result.reduce((existing: LabReport[], next: Content | null) => {
+            if (next) {
+                const doc = internalized(next);
+                if (doc) {
+                    return [...existing, doc];
+                }
+            }
+
+            return existing;
+        }, []);
+    }
+    return [];
+};
