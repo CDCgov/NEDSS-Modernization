f973b7d17c4593a6a5f42ecd7baa5955d60eb56e
Adam Loup <aloup@enquizit.com>
2023-12-20 15:28:33 -0600

    ensures that page status is filtered using derived values

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/filter/querydsl/QueryDSLFilterApplier.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/filter/querydsl/QueryDSLFilterApplier.java
@@ -2,13 +2,7 @@ package gov.cdc.nbs.questionbank.filter.querydsl;
 
 import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.StringExpression;
-import com.querydsl.core.types.dsl.TemporalExpression;
-import gov.cdc.nbs.questionbank.filter.DateFilter;
-import gov.cdc.nbs.questionbank.filter.DateRangeFilter;
 import gov.cdc.nbs.questionbank.filter.Filter;
-import gov.cdc.nbs.questionbank.filter.MultiValueFilter;
-import gov.cdc.nbs.questionbank.filter.SingleValueFilter;
 
 import java.util.Collection;
 import java.util.function.Function;
@@ -17,8 +11,12 @@ import java.util.stream.Stream;
 public class QueryDSLFilterApplier {
 
   public interface ExpressionResolver {
-    @SuppressWarnings("java:S1452")
     Stream<Expression<?>> resolve(final String property);
+  }
+
+
+  public interface CriteriaResolver {
+    Stream<BooleanExpression> resolve(final Filter filter, final Expression<?> expression);
   }
 
   public static Stream<BooleanExpression> apply(
@@ -26,35 +24,29 @@ public class QueryDSLFilterApplier {
       final Collection<Filter> filters
   ) {
     return filters.stream()
-        .flatMap(applyFilter(expressionResolver))
+        .flatMap(applyFilter(expressionResolver, new DefaultQueryDSLCriteriaResolver()))
+        .reduce(BooleanExpression::and)
+        .stream();
+  }
+
+  public static Stream<BooleanExpression> apply(
+      final ExpressionResolver expressionResolver,
+      final CriteriaResolver criteriaResolver,
+      final Collection<Filter> filters
+  ) {
+    return filters.stream()
+        .flatMap(applyFilter(expressionResolver, criteriaResolver))
         .reduce(BooleanExpression::and)
         .stream();
   }
 
   private static Function<Filter, Stream<BooleanExpression>> applyFilter(
-      final ExpressionResolver expressionResolver) {
+      final ExpressionResolver expressionResolver,
+      final CriteriaResolver criteriaResolver
+  ) {
     return filter -> expressionResolver.resolve(filter.property())
-        .flatMap(asBooleanExpression(filter));
+        .flatMap(expression -> criteriaResolver.resolve(filter, expression));
   }
-
-  @SuppressWarnings("java:S1452")
-  private static Function<Expression<?>, Stream<BooleanExpression>> asBooleanExpression(final Filter filter) {
-    return expression -> {
-      if (filter instanceof SingleValueFilter single && expression instanceof StringExpression string) {
-        return Stream.of(QueryDSLSingleValueFilterApplier.apply(single, string));
-      } else if (filter instanceof MultiValueFilter multi && expression instanceof StringExpression string) {
-        return Stream.of(QueryDSLMultiValueFilterApplier.apply(multi, string));
-      } else if (filter instanceof DateFilter date && expression instanceof TemporalExpression<? extends Comparable> temporal) {
-        return Stream.of(QueryDSLDateFilterApplier.apply(date, temporal));
-      } else if (filter instanceof DateRangeFilter dateRange && expression instanceof TemporalExpression<? extends Comparable> temporal) {
-        return Stream.of(QueryDSLDateRangeFilterApplier.apply(dateRange, temporal));
-      } else {
-        return Stream.empty();
-      }
-    };
-  }
-
-
 
   private QueryDSLFilterApplier() {
   }
