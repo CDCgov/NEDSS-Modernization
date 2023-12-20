34c801b69027a7f1b4a6400cc31a5447433642a3
Adam Loup <aloup@enquizit.com>
2023-12-20 09:25:02 -0600

    transformer

---
+++ apps/modernization-ui/src/pages/patient/profile/labReport/PatientLabReport.ts
@@ -0,0 +1,26 @@
+type AssociatedWith = {
+    id: string;
+    condition: string;
+    local: string;
+};
+
+type TestResult = {
+    test: string;
+    result: string | null;
+};
+
+type PatientLabReport = {
+    report: string;
+    receivedOn: Date;
+    reportingFacility: string | null;
+    orderingProvider: string | null;
+    orderingFacility: string | null;
+    collectedOn: Date | null;
+    results: TestResult[];
+    associatedWith: AssociatedWith | null;
+    programArea: string;
+    jurisdiction: string;
+    event: string;
+};
+
+export type { PatientLabReport, AssociatedWith, TestResult };
---
+++ apps/modernization-ui/src/pages/patient/profile/labReport/PatientLabReportTransformer.ts
@@ -0,0 +1,62 @@
+import { asLocalDate } from 'date';
+import { FindLabReportsForPatientQuery, PatientLabReport as GraphQLPatientLabReport } from 'generated/graphql/schema';
+import { mapNonNull, orNull } from 'utils';
+import { AssociatedWith, PatientLabReport, TestResult } from './PatientLabReport';
+
+type Result = FindLabReportsForPatientQuery['findLabReportsForPatient'];
+
+const getReportingFacility = (content: GraphQLPatientLabReport): string | undefined | null => {
+    return content.organizationParticipations?.find((o) => o?.typeCd === 'AUT')?.name;
+};
+
+const getOrderingProviderName = (content: GraphQLPatientLabReport): string | null => {
+    const provider = content.personParticipations?.find((p) => p?.typeCd === 'ORD' && p?.personCd === 'PRV');
+    if (provider) {
+        return `${provider.firstName} ${provider.lastName}`;
+    } else {
+        return null;
+    }
+};
+
+const getOrderingFacility = (content: GraphQLPatientLabReport): string | undefined | null => {
+    return content.organizationParticipations?.find((o) => o?.typeCd === 'ORD')?.name;
+};
+
+const getAssociatedWith = (content: GraphQLPatientLabReport): AssociatedWith | null => {
+    if (content.associatedInvestigations.length > 0) {
+        const investigation = content.associatedInvestigations[0];
+        return {
+            id: String(investigation.publicHealthCaseUid),
+            local: investigation.localId,
+            condition: investigation.cdDescTxt
+        };
+    }
+    return null;
+};
+
+const getTestResults = (content: GraphQLPatientLabReport): TestResult[] => {
+    return content.observations
+        .filter((result) => result.domainCd === 'Result')
+        .map((result) => ({ test: result.cdDescTxt, result: orNull(result.displayName) }));
+};
+
+const internalized = (content: GraphQLPatientLabReport): PatientLabReport | null => {
+    if (content) {
+        return {
+            report: String(content.observationUid),
+            receivedOn: asLocalDate(content.addTime),
+            reportingFacility: orNull(getReportingFacility(content)),
+            orderingProvider: getOrderingProviderName(content),
+            orderingFacility: orNull(getOrderingFacility(content)),
+            collectedOn: asLocalDate(content.effectiveFromTime),
+            results: getTestResults(content),
+            associatedWith: getAssociatedWith(content),
+            programArea: content.programAreaCd,
+            jurisdiction: String(content.jurisdictionCodeDescTxt),
+            event: content.localId
+        };
+    }
+    return null;
+};
+
+export const transform = (result: Result): PatientLabReport[] => mapNonNull(internalized, result);
