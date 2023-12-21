42d99a699cb50225c177590e77d5c637e3da6102
Michael Peels <michaelpeels@Michael-Peels-Work-6.local>
2023-12-21 15:16:17 -0500

    Test displayComponent

--- apps/modernization-ui/src/apps/page-builder/page/management/edit/question/QuestionContent.tsx
+++ apps/modernization-ui/src/apps/page-builder/page/management/edit/question/QuestionContent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styles from './question-content.module.scss';
+import { AlertBanner } from 'apps/page-builder/components/AlertBanner/AlertBanner';
 
 type Props = {
     name: string;
@@ -10,6 +11,7 @@ type Props = {
 export const QuestionContent = ({ name, type, displayComponent }: Props) => {
     return (
         <div className={styles.question}>
+            <AlertBanner type="warning">Work in progress</AlertBanner>
             <div className={styles.name}>{name}</div>
             <div>Type: {type}</div>
             <div>Display component: {displayComponent}</div>
