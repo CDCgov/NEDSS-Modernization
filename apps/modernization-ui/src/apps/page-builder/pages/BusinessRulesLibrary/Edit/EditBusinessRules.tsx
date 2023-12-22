61500f41c3be7cea00f23cb1900bb08c9406771a
Richard <rkrussel@enquizit.com>
2023-12-22 13:24:16 -0600

    init

---
+++ apps/modernization-ui/src/apps/page-builder/pages/BusinessRulesLibrary/Edit/EditBusinessRules.tsx
@@ -0,0 +1,181 @@
+import { Button, ButtonGroup, Form, Grid, Icon } from '@trussworks/react-uswds';
+import { PagesBreadcrumb } from 'apps/page-builder/components/PagesBreadcrumb/PagesBreadcrumb';
+import React, { useContext, useEffect, useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { useNavigate, useParams } from 'react-router-dom';
+import { UserContext } from 'user';
+import './EditBusinessRules.scss';
+// import { EditBusinessRulesFields } from './EditBusinessRulesFields';
+import { PageRuleControllerService, ViewRuleResponse } from '../../../generated';
+
+export type FormValues = {
+    ruleFunction: string;
+    sourceIdentifier: string;
+    sourceText: string;
+    comparator?: string;
+    sourceValue: string[];
+    targetValueIdentifier?: string[];
+    targetType: string;
+    ruleDescription: string;
+    targetValueText?: string[];
+    anySourceValue?: boolean;
+};
+
+export const EditBusinessRules = () => {
+    const [selectedFieldType, setSelectedFieldType] = useState('');
+    const navigate = useNavigate();
+    const { state } = useContext(UserContext);
+    const { pageId, ruleId } = useParams();
+    const form = useForm<FormValues>({
+        defaultValues: { targetType: 'SUBSECTION', anySourceValue: false },
+        mode: 'onChange'
+    });
+    const token = `Bearer ${state.getToken()}`;
+
+    useEffect(() => {
+        if (!ruleId) return;
+        PageRuleControllerService.viewRuleResponseUsingGet({
+            authorization: token,
+            ruleId: Number(ruleId),
+            page: Number(pageId)
+        }).then((resp: ViewRuleResponse) => {
+            form.setValue('anySourceValue', resp?.anySourceValue!);
+            form.setValue('ruleDescription', resp?.ruleDescription!);
+            form.setValue('ruleFunction', resp?.ruleFunction!);
+            form.setValue('comparator', resp?.comparator!);
+            form.setValue('targetType', resp?.targetType!);
+            form.setValue('sourceIdentifier', resp?.sourceIdentifier!);
+            form.setValue('sourceText', resp?.sourceIdentifier!);
+            form.setValue('sourceValue', resp?.sourceValue!);
+            form.setValue('targetValueIdentifier', resp?.targetValueIdentifier!);
+            setSelectedFieldType(resp.ruleFunction!);
+        });
+    }, [ruleId]);
+
+    const onSubmit = form.handleSubmit(async (data) => {
+        const request = {
+            comparator: data.comparator,
+            ruleDescription: data.ruleDescription,
+            ruleFunction: selectedFieldType,
+            sourceIdentifier: data.sourceIdentifier,
+            sourceText: data.sourceText,
+            sourceValue: [
+                {
+                    sourceValueId: data.sourceValue,
+                    sourceValueText: data.sourceValue
+                }
+            ],
+            targetType: data.targetType,
+            targetValueIdentifier: data.targetValueIdentifier,
+            targetValueText: data.targetValueText
+        };
+        if (!ruleId) {
+            PageRuleControllerService.createBusinessRuleUsingPost({
+                authorization: token,
+                page: Number(pageId),
+                request
+            }).then((resp: any) => {
+                console.log(resp);
+            });
+        } else {
+            PageRuleControllerService.updatePageRuleUsingPut({
+                authorization: token,
+                page: Number(pageId),
+                ruleId: Number(ruleId),
+                request
+            }).then((resp: any) => {
+                // showAlert({ type: 'success', header: 'updated', message: resp.message });
+            });
+        }
+        handleCancel();
+    });
+
+    const handleCancel = () => {
+        navigate(-1);
+    };
+
+    const handleDeleteRule = () => {
+        PageRuleControllerService.deletePageRuleUsingDelete({
+            authorization: token,
+            ruleId: Number(ruleId)
+        }).then((resp: any) => {
+            handleCancel();
+            // showAlert({ type: 'success', header: 'Deleted', message: resp.message });
+        });
+    };
+
+    const fieldTypeTab = [
+        { name: 'Enable' },
+        { name: 'Date Compare' },
+        { name: 'Disable' },
+        { name: 'Hide' },
+        { name: 'Unhide' },
+        { name: 'Require If' }
+    ];
+
+    const title = !ruleId ? 'Add new' : 'Edit';
+    const ruleFunction = form.watch('ruleFunction');
+
+    return (
+        <div className="edit-rules">
+            <div className="breadcrumb-wrap">
+                <PagesBreadcrumb currentPage={`${title} business rules`} />
+            </div>
+            <Form onSubmit={onSubmit}>
+                <div className="edit-rules__form">
+                    <div className="edit-rules__content">
+                        <h2>{`${title} business rules`}</h2>
+                        <Grid row className="inline-field">
+                            <Grid col={3}>
+                                <label className="input-label">Function</label>
+                            </Grid>
+                            <Grid col={8}>
+                                {ruleId ? (
+                                    <label>{ruleFunction}</label>
+                                ) : (
+                                    <ButtonGroup type="segmented">
+                                        {fieldTypeTab.map((field, index) => (
+                                            <Button
+                                                key={index}
+                                                type="button"
+                                                outline={field.name !== selectedFieldType}
+                                                onClick={() => {
+                                                    setSelectedFieldType(field.name);
+                                                    form.setValue('ruleFunction', field.name);
+                                                }}>
+                                                {field.name}
+                                            </Button>
+                                        ))}
+                                    </ButtonGroup>
+                                )}
+                            </Grid>
+                        </Grid>
+                        {selectedFieldType == '' ? (
+                            <></>
+                        ) : (
+                            <FormProvider {...form}>{/* <EditBusinessRulesFields /> */}</FormProvider>
+                        )}
+                    </div>
+                </div>
+                <div className="edit-rules__buttons">
+                    {ruleId ? (
+                        <Button type="button" className="delete-btn" unstyled onClick={handleDeleteRule}>
+                            <Icon.Delete size={3} className="margin-right-2px" />
+                            <span> Delete</span>
+                        </Button>
+                    ) : (
+                        <div />
+                    )}
+                    <div>
+                        <Button type="button" outline onClick={handleCancel}>
+                            Cancel
+                        </Button>
+                        <Button type="submit" className="lbr" disabled={!form.formState.isValid}>
+                            Add to Library
+                        </Button>
+                    </div>
+                </div>
+            </Form>
+        </div>
+    );
+};
