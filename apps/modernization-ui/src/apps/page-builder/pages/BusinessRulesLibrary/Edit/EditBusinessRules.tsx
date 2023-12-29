2ee09b5bf2fc15649b73bdc3754f5ba4b4b6ebe1
Richard <rkrussel@enquizit.com>
2023-12-29 13:13:36 -0600

    remove the delete modal and use confirmation component

--- apps/modernization-ui/src/apps/page-builder/pages/BusinessRulesLibrary/Edit/EditBusinessRules.tsx
+++ apps/modernization-ui/src/apps/page-builder/pages/BusinessRulesLibrary/Edit/EditBusinessRules.tsx
@@ -3,9 +3,9 @@ import React, { useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import './EditBusinessRules.scss';
 import { PageRuleControllerService } from '../../../generated';
-import DeleteBusinessRuleWarningModal from './DeleteBusinessRuleWarningModal';
 import { useAlert } from 'alert';
 import { authorization } from 'authorization';
+import { ConfirmationModal } from 'confirmation';
 
 const EditBusinessRules = () => {
     const navigate = useNavigate();
@@ -36,9 +36,14 @@ const EditBusinessRules = () => {
 
     return (
         <>
-            <DeleteBusinessRuleWarningModal
-                deleteWarningModalModalRef={deleteWarningModalModal}
-                onDeleteRule={handleDeleteRule}
+            <ConfirmationModal
+                modal={deleteWarningModalModal}
+                title="Warning"
+                message="Are you sure you want to delete this business rule?"
+                detail="Once deleted, this business rule will be permanently removed from the system and will nolonger be associated with the page."
+                confirmText="Yes, delete"
+                onConfirm={handleDeleteRule}
+                onCancel={handleCancel}
             />
             <div className="edit-rules">
                 <div className="edit-rules__buttons">
@@ -50,7 +55,7 @@ const EditBusinessRules = () => {
                             className="delete-btn"
                             unstyled>
                             <Icon.Delete size={3} className="margin-right-2px" />
-                            <span> Delete</span>
+                            <span>Delete</span>
                         </ModalToggleButton>
                     )}
                     <div>
