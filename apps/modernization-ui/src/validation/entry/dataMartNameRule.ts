3b0c542eebc8110b8afa5bea0afbc77be6dace86
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2023-12-29 16:10:24 -0500

    CNFT2-1560 Datamart name: max length 21 char

--- apps/modernization-ui/src/validation/entry/dataMartNameRule.ts
+++ apps/modernization-ui/src/validation/entry/dataMartNameRule.ts
@@ -1,8 +1,11 @@
+import { maxLengthRule } from './maxLengthRule';
+
 const dataMartNameRule = {
     pattern: {
         value: /^[A-Za-z_-][A-Za-z0-9_-]*$/,
         message: 'Name cannot start with a number and can only contain alphanumeric and underscore characters.'
-    }
+    },
+    ...maxLengthRule(21)
 };
 
 export { dataMartNameRule };
