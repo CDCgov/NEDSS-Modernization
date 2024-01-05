7621c1c228da7b1fd850b81cc2aef194df0af46d
Lakshmi Narayanan Ramakrishnan <132704359+lramakr@users.noreply.github.com>
2024-01-05 11:50:07 -0700

    [CNFT2-1570] -  Create question (#794)
    * cnft2 1570 create question
    
    * Add question modal
    
    ---------
    
    Co-authored-by: John Lee <124739504+jlee-enquizit@users.noreply.github.com>

--- apps/modernization-ui/src/apps/page-builder/page/management/edit/content/PageContent.tsx
+++ apps/modernization-ui/src/apps/page-builder/page/management/edit/content/PageContent.tsx
@@ -2,11 +2,16 @@ import { PagesTab } from 'apps/page-builder/generated';
 import { Sections } from '../section/Sections';
 import { PageSideMenu } from './PageSideMenu';
 import styles from './page-content.module.scss';
+import { AddQuestionModal } from '../../../../components/Subsection/AddQuestionModal/AddQuestionModal';
+import { useRef, useState } from 'react';
+import { ModalRef } from '@trussworks/react-uswds';
 
 type Props = {
     tab: PagesTab;
 };
 export const PageContent = ({ tab }: Props) => {
+    const [subsectionId, setSubsectionId] = useState(0);
+    const addQuestionModalRef = useRef<ModalRef>(null);
     const handleAddSubsection = (section: number) => {
         console.log('add subsection not yet implemented', section);
     };
@@ -14,8 +19,14 @@ export const PageContent = ({ tab }: Props) => {
     return (
         <div className={styles.pageContent}>
             <div className={styles.invisible} />
-            <Sections sections={tab.sections ?? []} onAddSubsection={handleAddSubsection} />
+            <Sections
+                sections={tab.sections ?? []}
+                onAddSubsection={handleAddSubsection}
+                onAddQuestion={setSubsectionId}
+                addQuestionModalRef={addQuestionModalRef}
+            />
             <PageSideMenu />
+            <AddQuestionModal subsectionId={subsectionId} modalRef={addQuestionModalRef} />
         </div>
     );
 };
--- apps/modernization-ui/src/apps/page-builder/page/management/edit/section/Sections.tsx
+++ apps/modernization-ui/src/apps/page-builder/page/management/edit/section/Sections.tsx
@@ -1,18 +1,27 @@
 import { PagesSection } from 'apps/page-builder/generated';
-import React from 'react';
+import React, { RefObject } from 'react';
 
 import styles from './section.module.scss';
 import { Section } from './Section';
+import { ModalRef } from '@trussworks/react-uswds';
 
 type Props = {
     sections: PagesSection[];
+    onAddQuestion: (subsection: number) => void;
     onAddSubsection: (section: number) => void;
+    addQuestionModalRef: RefObject<ModalRef>;
 };
-export const Sections = ({ sections, onAddSubsection }: Props) => {
+export const Sections = ({ sections, onAddSubsection, onAddQuestion, addQuestionModalRef }: Props) => {
     return (
         <div className={styles.sections}>
             {sections.map((s, k) => (
-                <Section section={s} key={k} onAddSubsection={onAddSubsection} />
+                <Section
+                    section={s}
+                    key={k}
+                    onAddSubsection={onAddSubsection}
+                    onAddQuestion={onAddQuestion}
+                    addQuestionModalRef={addQuestionModalRef}
+                />
             ))}
         </div>
     );
