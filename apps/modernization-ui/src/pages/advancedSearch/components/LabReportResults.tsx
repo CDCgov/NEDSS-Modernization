a169034a7721b82bc417300cd16921975744f8f5
Michael Davis <michaeldavis@michaels-mbp.mynetworksettings.com>
2024-01-08 13:27:19 -0500

    CNFT1-1887 moved advanced search from pages to apps module

--- apps/modernization-ui/src/pages/advancedSearch/components/LabReportResults.tsx
+++ apps/modernization-ui/src/pages/advancedSearch/components/LabReportResults.tsx
@@ -1,8 +1,8 @@
 import { Grid, Pagination } from '@trussworks/react-uswds';
 import { useEffect, useRef } from 'react';
-import { LabReport, OrganizationParticipation, PersonParticipation } from '../../../generated/graphql/schema';
-import { calculateAge } from '../../..//utils/util';
-import '../AdvancedSearch.scss';
+import { LabReport, OrganizationParticipation, PersonParticipation } from 'generated/graphql/schema';
+import { calculateAge } from 'utils/util';
+import 'apps/search/advancedSearch/AdvancedSearch.scss';
 import { useNavigate } from 'react-router';
 import { ClassicLink } from 'classic';
 import { NoData } from 'components/NoData';
