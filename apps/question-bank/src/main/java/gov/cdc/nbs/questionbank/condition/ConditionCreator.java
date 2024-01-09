1eec0ca4518d3671d35b991e52043bee5dc415b0
Michael Peels <michaelpeels@Michael-Peels-Work-6.local>
2024-01-09 12:28:14 -0500

    Make create condition form perform validation on blur. set required for condition name

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/ConditionCreator.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/condition/ConditionCreator.java
@@ -1,8 +1,9 @@
 package gov.cdc.nbs.questionbank.condition;
 
+import static gov.cdc.nbs.questionbank.util.PageBuilderUtil.requireNotEmpty;
+import java.time.Clock;
 import java.time.LocalDate;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import gov.cdc.nbs.questionbank.condition.command.ConditionCommand;
 import gov.cdc.nbs.questionbank.condition.exception.ConditionCreateException;
@@ -12,95 +13,104 @@ import gov.cdc.nbs.questionbank.condition.repository.LdfPageSetRepository;
 import gov.cdc.nbs.questionbank.condition.request.CreateConditionRequest;
 import gov.cdc.nbs.questionbank.entity.condition.ConditionCode;
 import gov.cdc.nbs.questionbank.entity.condition.LdfPageSet;
-import lombok.RequiredArgsConstructor;
 
 
 @Service
-@RequiredArgsConstructor
 public class ConditionCreator {
 
-    @Autowired
-    private ConditionCodeRepository conditionCodeRepository;
+  private final ConditionCodeRepository conditionCodeRepository;
+  private final LdfPageSetRepository ldfPageSetRepository;
+  private final Clock clock;
 
-    @Autowired
-    private LdfPageSetRepository ldfPageSetRepository;
+  public ConditionCreator(
+      final ConditionCodeRepository conditionCodeRepository,
+      final LdfPageSetRepository ldfPageSetRepository,
+      final Clock clock) {
+    this.conditionCodeRepository = conditionCodeRepository;
+    this.ldfPageSetRepository = ldfPageSetRepository;
+    this.clock = clock;
+  }
 
-    public Condition createCondition(CreateConditionRequest request, long userId) {
-        String conditionNm = request.conditionShortNm();
-        long nbsUid = conditionCodeRepository.getNextNbsUid();
+  public Condition createCondition(CreateConditionRequest request, long userId) {
+    requireNotEmpty(request.conditionShortNm(), "Condition Name");
+    requireNotEmpty(request.codeSystemDescTxt(), "Coding System");
+    requireNotEmpty(request.code(), "Condition Code");
+    requireNotEmpty(request.progAreaCd(), "Program Area");
 
-        //check if id already exists
-        if (checkId(request.code())) {
-            throw new ConditionCreateException("Condition Id already exists");
-        }
+    long nbsUid = conditionCodeRepository.getNextNbsUid();
 
-        //check if conditionNm already exists
-        if (checkConditionNm(conditionNm)) {
-            throw new ConditionCreateException("Condition Name already exists");
-        }
-
-        try {
-            ConditionCode conditionCode = new ConditionCode(conditionAdd(request, userId, nbsUid));
-            conditionCode.getLdfPageSets().add(
-                    new LdfPageSet(
-                            conditionCode,
-                            getLdfId(),
-                            ldfPageSetRepository.nextDisplayRow(),
-                            ldfPageSetRepository.nextNbsUid()));
-            conditionCode = conditionCodeRepository.save(conditionCode);
-            return new Condition(
-                    conditionCode.getId(),
-                    conditionCode.getConditionShortNm(),
-                    conditionCode.getProgAreaCd(),
-                    conditionCode.getFamilyCd(),
-                    conditionCode.getCoinfectionGrpCd(),
-                    conditionCode.getNndInd(),
-                    conditionCode.getInvestigationFormCd(),
-                    conditionCode.getStatusCd());
-
-        } catch (Exception e) {
-            throw new ConditionCreateException("Failed to create condition");
-        }
+    //check if id already exists
+    if (checkId(request.code())) {
+      throw new ConditionCreateException("Condition Id already exists");
     }
 
-    public boolean checkId(String id) {
-        return (id != null && conditionCodeRepository.checkId(id) > 0);
+    //check if conditionNm already exists
+    if (checkConditionNm(request.conditionShortNm())) {
+      throw new ConditionCreateException("Condition Name already exists");
     }
 
-    public boolean checkConditionNm(String conditionNm) {
-        return (conditionNm != null && conditionCodeRepository.checkConditionName(conditionNm) > 0);
-    }
+    try {
+      ConditionCode conditionCode = new ConditionCode(conditionAdd(request, userId, nbsUid));
+      conditionCode.getLdfPageSets().add(
+          new LdfPageSet(
+              conditionCode,
+              getLdfId(),
+              ldfPageSetRepository.nextDisplayRow(),
+              ldfPageSetRepository.nextNbsUid()));
+      conditionCode = conditionCodeRepository.save(conditionCode);
+      return new Condition(
+          conditionCode.getId(),
+          conditionCode.getConditionShortNm(),
+          conditionCode.getProgAreaCd(),
+          conditionCode.getFamilyCd(),
+          conditionCode.getCoinfectionGrpCd(),
+          conditionCode.getNndInd(),
+          conditionCode.getInvestigationFormCd(),
+          conditionCode.getStatusCd());
 
-    public String getLdfId() {
-        List<String> allIds = ldfPageSetRepository.findAllIds();
-        String currentYear = String.valueOf(LocalDate.now().getYear());
-        int numericPart = 0;
-
-        if (!allIds.isEmpty()) {
-            numericPart = allIds.stream()
-                    .filter(id -> id.startsWith(currentYear))
-                    .mapToInt(id -> Integer.parseInt(id.substring(4)))
-                    .max()
-                    .orElse(0);
-        }
-        numericPart++;
-        return currentYear + String.format("%03d", numericPart);
+    } catch (Exception e) {
+      throw new ConditionCreateException("Failed to create condition");
     }
+  }
 
-    public ConditionCommand.AddCondition conditionAdd(final CreateConditionRequest request, long userId, long nbsUid) {
-        return new ConditionCommand.AddCondition(
-                request.code(),
-                nbsUid,
-                request.codeSystemDescTxt(),
-                request.conditionShortNm(),
-                request.nndInd(),
-                request.progAreaCd(),
-                request.reportableMorbidityInd(),
-                request.reportableSummaryInd(),
-                request.contactTracingEnableInd(),
-                request.familyCd(),
-                request.coinfectionGrpCd(),
-                userId);
+  public boolean checkId(String id) {
+    return (id != null && conditionCodeRepository.checkId(id) > 0);
+  }
+
+  public boolean checkConditionNm(String conditionNm) {
+    return (conditionNm != null && conditionCodeRepository.checkConditionName(conditionNm) > 0);
+  }
+
+  public String getLdfId() {
+    List<String> allIds = ldfPageSetRepository.findAllIds();
+    String currentYear = String.valueOf(LocalDate.now(clock).getYear());
+    int numericPart = 0;
+
+    if (!allIds.isEmpty()) {
+      numericPart = allIds.stream()
+          .filter(id -> id.startsWith(currentYear))
+          .mapToInt(id -> Integer.parseInt(id.substring(4)))
+          .max()
+          .orElse(0);
     }
+    numericPart++;
+    return currentYear + String.format("%03d", numericPart);
+  }
+
+  public ConditionCommand.AddCondition conditionAdd(final CreateConditionRequest request, long userId, long nbsUid) {
+    return new ConditionCommand.AddCondition(
+        request.code(),
+        nbsUid,
+        request.codeSystemDescTxt(),
+        request.conditionShortNm(),
+        request.nndInd(),
+        request.progAreaCd(),
+        request.reportableMorbidityInd(),
+        request.reportableSummaryInd(),
+        request.contactTracingEnableInd(),
+        request.familyCd(),
+        request.coinfectionGrpCd(),
+        userId);
+  }
 
 }
--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/condition/create/ConditionCreatorTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/condition/create/ConditionCreatorTest.java
@@ -6,7 +6,10 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +18,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import gov.cdc.nbs.questionbank.condition.ConditionCreator;
 import gov.cdc.nbs.questionbank.condition.exception.ConditionCreateException;
 import gov.cdc.nbs.questionbank.condition.model.Condition;
@@ -22,142 +26,219 @@ import gov.cdc.nbs.questionbank.condition.repository.ConditionCodeRepository;
 import gov.cdc.nbs.questionbank.condition.repository.LdfPageSetRepository;
 import gov.cdc.nbs.questionbank.condition.request.CreateConditionRequest;
 import gov.cdc.nbs.questionbank.entity.condition.ConditionCode;
+import gov.cdc.nbs.questionbank.exception.NullObjectException;
 
 
 
 class ConditionCreatorTest {
 
-    @Mock
-    ConditionCodeRepository conditionCodeRepository;
+  @Mock
+  ConditionCodeRepository conditionCodeRepository;
 
-    @Mock
-    LdfPageSetRepository ldfPageSetRepository;
+  @Mock
+  LdfPageSetRepository ldfPageSetRepository;
 
-    @InjectMocks
-    ConditionCreator conditionCreator;
+  @Spy
+  Clock clock = Clock.fixed(Instant.parse("2024-01-09T02:03:04Z"), ZoneId.of("UTC"));
 
-    private static final long userId = 1L;
+  @InjectMocks
+  ConditionCreator conditionCreator;
 
-    public ConditionCreatorTest() {
+  private static final long userId = 1L;
 
-        MockitoAnnotations.openMocks(this);
-    }
+  public ConditionCreatorTest() {
 
-    @Test
-    void createConditionTest() {
-        CreateConditionRequest request = getCreateConditionRequest();
-        ConditionCode conditionDb = new ConditionCode(conditionCreator.conditionAdd(request, userId, 123));
-        when(conditionCodeRepository.save(Mockito.any(ConditionCode.class))).thenReturn(conditionDb);
-        when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn(0L);
-        when(conditionCodeRepository.checkConditionName(Mockito.anyString())).thenReturn(0L);
-        Condition response = conditionCreator.createCondition(request, userId);
-        assertEquals(conditionDb.getId(), response.id());
-    }
+    MockitoAnnotations.openMocks(this);
+  }
 
-    @Test
-    void createIdExistsTest() {
-        CreateConditionRequest request =
-                new CreateConditionRequest(
-                        "1L",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null);
-        when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn((1L));
-        assertThrows(ConditionCreateException.class, () -> conditionCreator.createCondition(request, userId));
-    }
+  @Test
+  void createConditionTest() {
+    CreateConditionRequest request = getCreateConditionRequest();
+    ConditionCode conditionDb = new ConditionCode(conditionCreator.conditionAdd(request, userId, 123));
+    when(conditionCodeRepository.save(Mockito.any(ConditionCode.class))).thenReturn(conditionDb);
+    when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn(0L);
+    when(conditionCodeRepository.checkConditionName(Mockito.anyString())).thenReturn(0L);
+    Condition response = conditionCreator.createCondition(request, userId);
+    assertEquals(conditionDb.getId(), response.id());
+  }
 
-    @Test
-    void createConditionNameExistsTest() {
-        CreateConditionRequest request =
-                new CreateConditionRequest(
-                        null,
-                        null,
-                        "condition name",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null);
-        when(conditionCodeRepository.checkConditionName(Mockito.anyString())).thenReturn(1l);
-        assertThrows(ConditionCreateException.class, () -> conditionCreator.createCondition(request, userId));
-    }
+  @Test
+  void requireNonNullCode() {
+    CreateConditionRequest request =
+        new CreateConditionRequest(
+            null,
+            "null",
+            "null",
+            "null",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+    when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn((1L));
+    assertThrows(NullObjectException.class, () -> conditionCreator.createCondition(request, userId));
+  }
 
-    @Test
-    void checkIdExists() {
-        when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn(1L);
-        boolean val = conditionCreator.checkId("1L");
-        assertTrue(val);
-    }
+  @Test
+  void requireNonNullCodeSystem() {
+    CreateConditionRequest request =
+        new CreateConditionRequest(
+            "null",
+            null,
+            "null",
+            "null",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+    when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn((1L));
+    assertThrows(NullObjectException.class, () -> conditionCreator.createCondition(request, userId));
+  }
 
-    @Test
-    void checkIdExistsNullTest() {
-        boolean val = conditionCreator.checkId(null);
-        assertFalse(val);
-    }
+  @Test
+  void requireNonNullName() {
+    CreateConditionRequest request =
+        new CreateConditionRequest(
+            "null",
+            "null",
+            null,
+            "null",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+    when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn((1L));
+    assertThrows(NullObjectException.class, () -> conditionCreator.createCondition(request, userId));
+  }
 
-    @Test
-    void checkConditionNameExists() {
-        when(conditionCodeRepository.checkConditionName(Mockito.anyString())).thenReturn(1L);
-        boolean val = conditionCreator.checkConditionNm("condition name");
-        assertTrue(val);
-    }
+  @Test
+  void requireNonNullProgramArea() {
+    CreateConditionRequest request =
+        new CreateConditionRequest(
+            "null",
+            "null",
+            "null",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+    when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn((1L));
+    assertThrows(NullObjectException.class, () -> conditionCreator.createCondition(request, userId));
+  }
 
-    @Test
-    void checkConditionNameExistsNullTest() {
-        boolean val = conditionCreator.checkConditionNm(null);
-        assertFalse(val);
-    }
 
-    @Test
-    void testGenerateNewIdWithNoExistingIds() {
-        doReturn(new ArrayList<>()).when(ldfPageSetRepository).findAllIds();
-        String newId = conditionCreator.getLdfId();
-        String currentYear = String.valueOf(LocalDate.now().getYear());
-        String expected = currentYear + "001";
-        assertEquals(expected, newId);
-    }
+  @Test
+  void createIdExistsTest() {
+    CreateConditionRequest request =
+        new CreateConditionRequest(
+            "1L",
+            "null",
+            "null",
+            "null",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+    when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn((1L));
+    assertThrows(ConditionCreateException.class, () -> conditionCreator.createCondition(request, userId));
+  }
 
-    @Test
-    void testGenerateNewIdWithExistingIdsContainingCurrentYear() {
-        String currentYear = String.valueOf(LocalDate.now().getYear());
-        List<String> existingIds = Arrays.asList(currentYear +"001", currentYear +"002", currentYear+ "003");
-        doReturn(existingIds).when(ldfPageSetRepository).findAllIds();
-        String newId = conditionCreator.getLdfId();
-        String expected = currentYear + "004";
-        assertEquals(expected, newId);
-    }
+  @Test
+  void createConditionNameExistsTest() {
+    CreateConditionRequest request =
+        new CreateConditionRequest(
+            "null",
+            "null",
+            "condition name",
+            "null",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+    when(conditionCodeRepository.checkConditionName(Mockito.anyString())).thenReturn(1l);
+    assertThrows(ConditionCreateException.class, () -> conditionCreator.createCondition(request, userId));
+  }
 
-    @Test
-    void testGenerateNewIdWithExistingIdsNotContainingCurrentYear() {
-        List<String> existingIds = Arrays.asList("2022001", "98", "99");
-        doReturn(existingIds).when(ldfPageSetRepository).findAllIds();
-        String newId = conditionCreator.getLdfId();
-        String currentYear = String.valueOf(LocalDate.now().getYear());
-        String expected = currentYear + "001";
-        assertEquals(expected, newId);
-    }
+  @Test
+  void checkIdExists() {
+    when(conditionCodeRepository.checkId(Mockito.anyString())).thenReturn(1L);
+    boolean val = conditionCreator.checkId("1L");
+    assertTrue(val);
+  }
 
-    private CreateConditionRequest getCreateConditionRequest() {
-        CreateConditionRequest request = new CreateConditionRequest(
-                "1",
-                "code system",
-                "condition name",
-                "prog test",
-                'Y',
-                'Y',
-                'Y',
-                'Y',
-                "family test",
-                "co infection test");
-        return request;
-    }
+  @Test
+  void checkIdExistsNullTest() {
+    boolean val = conditionCreator.checkId(null);
+    assertFalse(val);
+  }
+
+  @Test
+  void checkConditionNameExists() {
+    when(conditionCodeRepository.checkConditionName(Mockito.anyString())).thenReturn(1L);
+    boolean val = conditionCreator.checkConditionNm("condition name");
+    assertTrue(val);
+  }
+
+  @Test
+  void checkConditionNameExistsNullTest() {
+    boolean val = conditionCreator.checkConditionNm(null);
+    assertFalse(val);
+  }
+
+  @Test
+  void testGenerateNewIdWithNoExistingIds() {
+    doReturn(new ArrayList<>()).when(ldfPageSetRepository).findAllIds();
+    String newId = conditionCreator.getLdfId();
+    String currentYear = String.valueOf(LocalDate.now().getYear());
+    String expected = currentYear + "001";
+    assertEquals(expected, newId);
+  }
+
+  @Test
+  void testGenerateNewIdWithExistingIdsContainingCurrentYear() {
+    String currentYear = String.valueOf(LocalDate.now().getYear());
+    List<String> existingIds = Arrays.asList(currentYear + "001", currentYear + "002", currentYear + "003");
+    doReturn(existingIds).when(ldfPageSetRepository).findAllIds();
+    String newId = conditionCreator.getLdfId();
+    String expected = currentYear + "004";
+    assertEquals(expected, newId);
+  }
+
+  @Test
+  void testGenerateNewIdWithExistingIdsNotContainingCurrentYear() {
+    List<String> existingIds = Arrays.asList("2022001", "98", "99");
+    doReturn(existingIds).when(ldfPageSetRepository).findAllIds();
+    String newId = conditionCreator.getLdfId();
+    String currentYear = String.valueOf(LocalDate.now().getYear());
+    String expected = currentYear + "001";
+    assertEquals(expected, newId);
+  }
+
+  private CreateConditionRequest getCreateConditionRequest() {
+    CreateConditionRequest request = new CreateConditionRequest(
+        "1",
+        "code system",
+        "condition name",
+        "prog test",
+        'Y',
+        'Y',
+        'Y',
+        'Y',
+        "family test",
+        "co infection test");
+    return request;
+  }
 
 }
