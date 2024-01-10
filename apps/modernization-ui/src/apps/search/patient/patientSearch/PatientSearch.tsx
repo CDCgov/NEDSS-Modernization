a6cb0a00ac6e5bdec8f417024ec4cd3797fafe32
Mounika Gangula <118309975+GangulaMounika@users.noreply.github.com>
2024-01-10 08:00:32 -0500

    Clear date of birth field and its message when there is an error (#867)
    * Clear date of birth field and its message when there is an error
    
    * CNFT1-1768 confined the fix to only when a control is passed since the issue also happens in the same case
    
    * CNFT1-1711 Code review comment
    
    ---------
    
    Co-authored-by: Mounika-Reddy <>

--- apps/modernization-ui/src/apps/search/patient/patientSearch/PatientSearch.tsx
+++ apps/modernization-ui/src/apps/search/patient/patientSearch/PatientSearch.tsx
@@ -286,8 +286,14 @@ export const PatientSearch = ({ handleSubmission, personFilter, clearAll }: Pati
                         className="width-full clear-btn"
                         type={'button'}
                         onClick={() => {
-                            form.reset({}, { keepDefaultValues: true });
-                            clearAll();
+                            // Because of the customized nature of Date picker errors, the date picker doesn't reset when the clear all is clicked.
+                            // So you need to set the error to false and then run the form reset and clearAll methods.
+                            // None instead of empty string because Trusswork's DatePicker doesn't update with empty string.
+                            form.setValue('dateOfBirth', 'none');
+                            setTimeout(() => {
+                                form.reset({}, { keepDefaultValues: true });
+                                clearAll();
+                            });
                         }}
                         outline>
                         Clear all
