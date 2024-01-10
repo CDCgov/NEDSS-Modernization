8c8904be6bb8e0bc194fcfb58ec7be66b23c04b6
Michael Peels <michaelpeels@Michael-Peels-Work-6.local>
2024-01-10 15:53:30 -0500

    Use page size for initial search

---
+++ apps/modernization-ui/src/apps/page-builder/condition/search/ConditionSearch.spec.tsx
@@ -0,0 +1,95 @@
+import { render } from '@testing-library/react';
+import { ConditionSearch } from './ConditionSearch';
+import { MemoryRouter } from 'react-router-dom';
+import { Condition } from 'apps/page-builder/generated';
+
+const mockCondition: Condition = {
+    coinfectionGroup: 'coinfection',
+    conditionFamily: 'conditionFamily',
+    id: 'id',
+    name: 'conditionName',
+    nndInd: 'T',
+    page: undefined,
+    programArea: 'program area',
+    status: 'A'
+};
+
+const search = jest.fn();
+const reset = jest.fn();
+
+const mockUsConditionSearch = {
+    search: search,
+    response: { content: [mockCondition] },
+    error: undefined,
+    isLoading: false,
+    reset
+};
+
+jest.mock('./useConditionSearch', () => ({
+    ...jest.requireActual('./useConditionSearch'),
+    useConditionSearch: () => mockUsConditionSearch
+}));
+
+describe('ConditionSearch', () => {
+    const onConditionSelect = jest.fn();
+    const onCancel = jest.fn();
+    const onCreateNew = jest.fn();
+
+    it('should have a modal heading', () => {
+        const { getByText } = render(
+            <MemoryRouter>
+                <ConditionSearch onConditionSelect={onConditionSelect} onCancel={onCancel} onCreateNew={onCreateNew} />
+            </MemoryRouter>
+        );
+        expect(getByText('Search and add condition(s)')).toBeInTheDocument();
+    });
+
+    it('should have a title', () => {
+        const { getByText } = render(
+            <MemoryRouter>
+                <ConditionSearch onConditionSelect={onConditionSelect} onCancel={onCancel} onCreateNew={onCreateNew} />
+            </MemoryRouter>
+        );
+        expect(getByText('You can search for existing condition(s) or create a new one.')).toBeInTheDocument();
+    });
+
+    it('should have expected buttons', () => {
+        const { getByText } = render(
+            <MemoryRouter>
+                <ConditionSearch onConditionSelect={onConditionSelect} onCancel={onCancel} onCreateNew={onCreateNew} />
+            </MemoryRouter>
+        );
+        const createBtn = getByText('Create new condition');
+        expect(createBtn).toBeInTheDocument();
+        expect(createBtn.nodeName).toBe('BUTTON');
+        expect(createBtn).not.toHaveAttribute('disabled');
+
+        const cancelBtn = getByText('Cancel');
+        expect(cancelBtn).toBeInTheDocument();
+        expect(cancelBtn.nodeName).toBe('BUTTON');
+        expect(cancelBtn).not.toHaveAttribute('disabled');
+
+        const addBtn = getByText('Add conditions');
+        expect(addBtn).toBeInTheDocument();
+        expect(addBtn.nodeName).toBe('BUTTON');
+        expect(addBtn).toHaveAttribute('disabled');
+    });
+
+    it('should pass data to the table', () => {
+        const { getAllByRole } = render(
+            <MemoryRouter>
+                <ConditionSearch onConditionSelect={onConditionSelect} onCancel={onCancel} onCreateNew={onCreateNew} />
+            </MemoryRouter>
+        );
+
+        const cells = getAllByRole('cell');
+        expect(cells[1]).toHaveTextContent('conditionName');
+        expect(cells[2]).toHaveTextContent('id');
+        expect(cells[3]).toHaveTextContent('program area');
+        expect(cells[4]).toHaveTextContent('conditionFamily');
+        expect(cells[5]).toHaveTextContent('coinfection');
+        expect(cells[6]).toHaveTextContent('T');
+        expect(cells[7]).toHaveTextContent('No Data');
+        expect(cells[8]).toHaveTextContent('Active');
+    });
+});
