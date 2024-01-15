7cafe3fd6d5736c4561ded79b0ea0d1208406cb3
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-01-15 12:05:20 -0600

    ModalToggleButton

--- apps/modernization-ui/src/apps/page-builder/page/management/edit/content/PageSideMenu.tsx
+++ apps/modernization-ui/src/apps/page-builder/page/management/edit/content/PageSideMenu.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 
 import styles from './page-content.module.scss';
-import { Icon } from '@trussworks/react-uswds';
+import { Icon, ModalRef, ModalToggleButton } from '@trussworks/react-uswds';
 import { Icon as EQIcon } from 'components/Icon/Icon';
 
 type PageSideMenuProps = {
     onAddSection: () => void;
+    addSectionModalRef: RefObject<ModalRef>;
 };
 
-export const PageSideMenu = ({ onAddSection }: PageSideMenuProps) => {
+export const PageSideMenu = ({ onAddSection, addSectionModalRef }: PageSideMenuProps) => {
     return (
         <div className={styles.sideMenu}>
             <ul className={styles.list}>
                 <li>
-                    <Icon.Add onClick={onAddSection} />
-                    Add section
+                    <ModalToggleButton type="button" modalRef={addSectionModalRef} unstyled>
+                        <Icon.Add
+                            size={4}
+                            onClick={() => {
+                                onAddSection();
+                                console.log(onAddSection);
+                            }}
+                        />
+                        Add section
+                    </ModalToggleButton>
                 </li>
                 <li>
                     <EQIcon name="reorder" />
