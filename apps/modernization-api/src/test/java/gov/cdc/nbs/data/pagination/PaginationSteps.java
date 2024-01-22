c7e592976cd7791436519c4bd4bf7072b40b2508
Adam Loup <aloup@enquizit.com>
2024-01-22 15:53:25 -0600

    ensure identification finder only returns identifications with values

---
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/data/pagination/PaginationSteps.java
@@ -0,0 +1,41 @@
+package gov.cdc.nbs.data.pagination;
+
+import gov.cdc.nbs.testing.support.Active;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Pageable;
+
+public class PaginationSteps {
+
+  private final int maxPageSize;
+  private final Active<Pageable> activePageable;
+
+  PaginationSteps(
+      @Value("${nbs.max-page-size}") final int maxPageSize,
+      final Active<Pageable> activePageable
+  ) {
+    this.maxPageSize = maxPageSize;
+    this.activePageable = activePageable;
+  }
+
+  @Before("@web-interaction")
+  public void reset() {
+    this.activePageable.reset();
+  }
+
+  @Given("I want at most {int} results")
+  public void i_want_at_least_x_results(final int x) {
+    this.activePageable.active(Pageable.ofSize(x));
+  }
+
+  @Given("I want the {nth} page")
+  public void i_want_the_nth_page(final int nth) {
+    this.activePageable.active(next -> next.withPage(nth), this::defaultPage);
+  }
+
+  private Pageable defaultPage() {
+    return Pageable.ofSize(this.maxPageSize);
+  }
+
+}
--- libs/testing/support/src/main/java/gov/cdc/nbs/testing/support/Active.java
+++ libs/testing/support/src/main/java/gov/cdc/nbs/testing/support/Active.java
@@ -3,34 +3,42 @@ package gov.cdc.nbs.testing.support;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 @Component
 public class Active<I> {
 
-    private I item;
+  private I item;
 
-    public void active(final I next) {
-        this.item = next;
-    }
+  public void active(final I next) {
+    this.item = next;
+  }
 
-    public void active(final UnaryOperator<I> next) {
-        if(this.item != null) {
-            this.item = next.apply(this.item);
-        }
+  public void active(final UnaryOperator<I> next, final Supplier<I> initializer) {
+    if (this.item != null) {
+      this.item = initializer.get();
     }
+    this.item = next.apply(this.item);
+  }
 
-    public void reset() {
-        this.item = null;
+  public void active(final UnaryOperator<I> next) {
+    if (this.item != null) {
+      this.item = next.apply(this.item);
     }
+  }
 
-    public Optional<I> maybeActive() {
-        return Optional.ofNullable(this.item);
-    }
+  public void reset() {
+    this.item = null;
+  }
 
-    public I active() {
-        return maybeActive()
-            .orElseThrow(() -> new IllegalStateException("there is nothing active"));
-    }
+  public Optional<I> maybeActive() {
+    return Optional.ofNullable(this.item);
+  }
+
+  public I active() {
+    return maybeActive()
+        .orElseThrow(() -> new IllegalStateException("there is nothing active"));
+  }
 
 }
