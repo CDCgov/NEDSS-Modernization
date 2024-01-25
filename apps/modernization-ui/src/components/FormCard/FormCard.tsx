2d597b5a95ae0d12235ac7a32aa00941a5f52a46
hclarkEnq <hclark@enquizit.com>
2024-01-25 09:40:55 -0800

    fixing test case for the form card

--- apps/modernization-ui/src/components/FormCard/FormCard.tsx
+++ apps/modernization-ui/src/components/FormCard/FormCard.tsx
@@ -4,11 +4,10 @@ export default function FormCard({ id, title, children }: { id?: string; title?:
     return (
         <section id={id}>
             <Grid row className="flex-align-center bg-white border radius-md border-base-lighter margin-bottom-4">
-                <Grid
-                    col={12}
-                    className="font-sans-lg text-bold padding-2 border-bottom border-base-lighter"
-                    data-testid="title">
-                    <h2 className="h2 margin-0">{title}</h2>
+                <Grid col={12} className="font-sans-lg text-bold padding-2 border-bottom border-base-lighter">
+                    <h2 data-testid="title" className="h2 margin-0">
+                        {title}
+                    </h2>
                 </Grid>
                 {children}
             </Grid>
