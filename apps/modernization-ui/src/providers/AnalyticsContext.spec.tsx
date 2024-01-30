9e3781b8c3a6e8c58688ed4d4012e8ba272e74df
hclarkEnq <hclark@enquizit.com>
2024-01-30 08:19:24 -0800

    removing mockedProvider

--- apps/modernization-ui/src/providers/AnalyticsContext.spec.tsx
+++ apps/modernization-ui/src/providers/AnalyticsContext.spec.tsx
@@ -1,15 +1,12 @@
 import { render } from '@testing-library/react';
-import { MockedProvider } from '@apollo/client/testing';
 import { AnalyticsProvider } from './AnalyticsContext';
 
 describe('AnalyticsWrapper', () => {
     it('renders children with ApolloProvider', async () => {
         const { getByText } = render(
-            <MockedProvider>
-                <AnalyticsProvider>
-                    <div>Hello World</div>
-                </AnalyticsProvider>
-            </MockedProvider>
+            <AnalyticsProvider>
+                <div>Hello World</div>
+            </AnalyticsProvider>
         );
 
         // Expect the "Hello World" text to be present in the rendered output
