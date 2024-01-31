4050520d8cda45646dbeb0c36db4c8583d85667f
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2024-01-31 13:26:24 -0600

    [CNFT1-2014] Configuration API includes Analytics (#964)
    * adds analyics to API
    
    * configuration tests
    
    * adjusts how the frontend obtains configuration from the API
    
    * default settings
    
    * made more apeeling

--- libs/configuration-api/src/main/java/gov/cdc/nbs/configuration/Configuration.java
+++ libs/configuration-api/src/main/java/gov/cdc/nbs/configuration/Configuration.java
@@ -1,7 +1,12 @@
 package gov.cdc.nbs.configuration;
 
 import gov.cdc.nbs.configuration.nbs.Properties;
+import gov.cdc.nbs.configuration.settings.Settings;
 
-public record Configuration(Features features, Properties properties) {
+public record Configuration(
+    Features features,
+    Settings settings,
+    Properties properties
+) {
 
 }
---
+++ libs/configuration-api/src/test/java/gov/cdc/nbs/configuration/nbs/PropertiesSteps.java
@@ -0,0 +1,41 @@
+package gov.cdc.nbs.configuration.nbs;
+
+import gov.cdc.nbs.testing.support.Active;
+import io.cucumber.java.ParameterType;
+import io.cucumber.java.en.Then;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class PropertiesSteps {
+
+  private final Active<ResultActions> response;
+
+  public PropertiesSteps(final Active<ResultActions> response) {
+    this.response = response;
+  }
+
+  @ParameterType(name = "property", value = ".*")
+  public String property(final String value) {
+    return switch (value.toLowerCase()) {
+      case "code base" -> "properties.entries.CODE_BASE";
+      case "std program areas", "stdprogramareas" -> "properties.stdProgramAreas";
+      case "hiv program areas", "hivprogramareas" -> "properties.hivProgramAreas";
+      default -> value;
+    };
+  }
+
+  @Then("the properties include a(n) {property} of {string}")
+  public void the_properties_include_a_property_of(final String path, final String value) throws Exception {
+    this.response.active()
+        .andExpect(jsonPath("$.%s", path).value(value));
+  }
+
+  @Then("the properties include a(n) {property} with {string}")
+  public void the_properties_include_a_property_with(final String path, final String value) throws Exception {
+    this.response.active()
+        .andExpect(jsonPath("$.%s", path).value(hasItem(equalTo(value))));
+  }
+}
