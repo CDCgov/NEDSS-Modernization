9883b7decbaee71c148702fb3b2a48d2adc98d56
Steven Urciuoli <steveu@stevens-mbp-2.lan>
2024-02-01 10:08:51 -0600

    CNFT1-1880 Provider Autocompletion- API development

---
+++ libs/options-api/src/test/java/gov/cdc/nbs/option/provider/autocomplete/ProviderAutocompleteSteps.java
@@ -0,0 +1,37 @@
+package gov.cdc.nbs.option.provider.autocomplete;
+
+import gov.cdc.nbs.testing.support.Active;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.When;
+import org.springframework.test.web.servlet.ResultActions;
+
+public class ProviderAutocompleteSteps {
+
+  private final ProviderAutocompleteRequester request;
+  private final Active<ResultActions> response;
+
+  ProviderAutocompleteSteps(
+      final ProviderAutocompleteRequester request,
+      final Active<ResultActions> response) {
+    this.request = request;
+    this.response = response;
+  }
+
+  @Before("@providers")
+  public void reset() {
+    response.reset();
+  }
+
+  @When("I am trying to find providers that start with {string}")
+  public void i_am_trying_to_find_providers_that_start_with(final String criteria) throws Exception {
+    response.active(request.complete(criteria));
+  }
+
+  @When("I am trying to find at most {int} providers(s) that start with {string}")
+  public void i_am_trying_to_find_n_providers_that_start_with(
+      final int limit,
+      final String criteria) throws Exception {
+    response.active(request.complete(criteria, limit));
+  }
+
+}
