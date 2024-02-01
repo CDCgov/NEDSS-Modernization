894ae95f445128f706bc4c1c5f79f1b604cef629
Alaap Bharadwaj <abharadwaj@enquizit.com>
2024-02-01 14:27:21 -0500

    radio btns

--- apps/modernization-ui/src/apps/page-builder/components/RadioButton/RadioButton.tsx
+++ apps/modernization-ui/src/apps/page-builder/components/RadioButton/RadioButton.tsx
@@ -1,20 +1,29 @@
 import styles from './radiobutton.module.scss';
 import { Selectable } from 'options';
 import { Radio } from '@trussworks/react-uswds';
+import { useState } from 'react';
 
 type Props = {
     options: Selectable[];
-    isChecked: boolean;
     onChange?: () => void;
 };
 
-export const RadioButtons = ({ options, isChecked }: Props) => {
+export const RadioButtons = ({ options }: Props) => {
+    const [currentSelection, setCurrentSelection] = useState<string>('');
+
     return (
         <div className={styles.radioBtns}>
             <>
                 {options.map((s, k) => (
                     <div className={styles.content} key={k}>
-                        <Radio label={s.name} id={'radio-btn'} name={'radio-btn'} checked={isChecked} />
+                        <Radio
+                            label={s.name}
+                            id={'radio-btn'}
+                            name={'radio-btn'}
+                            value={s.value}
+                            checked={s.value === currentSelection}
+                            onChange={() => setCurrentSelection(s.value)}
+                        />
                     </div>
                 ))}
             </>
