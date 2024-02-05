458cb5303ae0035a5fa34bc0dd8e4d778ba49562
Steven Urciuoli <steveu@stevens-mbp-2.lan>
2024-02-05 15:16:20 -0600

    Facility autocomplete

---
+++ libs/options-api/src/test/java/gov/cdc/nbs/option/facility/autocomplete/FacilityOptionMother.java
@@ -0,0 +1,90 @@
+package gov.cdc.nbs.option.facility.autocomplete;
+
+import gov.cdc.nbs.option.Option;
+import gov.cdc.nbs.testing.support.Available;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Component
+class FacilityOptionMother {
+
+  private static final String DELETE_IN = """
+      delete
+      from Organization
+      where organization_uid in (:identifiers);
+      delete from Entity where entity_uid in (:identifiers) and class_cd = 'ORG';
+      """;
+
+  private static final String CREATE = """
+      insert into Entity(entity_uid, class_cd) values (:identifier, 'ORG');
+      insert into Organization(organization_uid, display_nm, version_ctrl_nbr)
+      values (:identifier, :name, 1);
+      """;
+
+
+  private final NamedParameterJdbcTemplate template;
+  private final Available<Option> available;
+  private final AtomicLong identifier;
+
+  FacilityOptionMother(final JdbcTemplate template) {
+    this.template = new NamedParameterJdbcTemplate(template);
+    this.available = new Available<>();
+    this.identifier = new AtomicLong(Long.MIN_VALUE);
+  }
+
+  void reset() {
+
+    List<String> codes = this.available.all()
+        .map(Option::value)
+        .toList();
+
+    if (!codes.isEmpty()) {
+
+      Map<String, List<String>> parameters = Map.of("identifiers", codes);
+
+      template.execute(
+          DELETE_IN,
+          new MapSqlParameterSource(parameters),
+          PreparedStatement::executeUpdate);
+      this.available.reset();
+    }
+  }
+
+  private long nextIdentifier() {
+    return identifier.getAndIncrement();
+  }
+
+  void create(final String name) {
+
+    int order = this.available.all()
+        .map(Option::order)
+        .max(Comparator.naturalOrder())
+        .orElse(1);
+
+    long identifier = nextIdentifier();
+
+    Map<String, ? extends Serializable> parameters = Map.of(
+        "identifier", identifier,
+        "name", name);
+
+    template.execute(
+        CREATE,
+        new MapSqlParameterSource(parameters),
+        PreparedStatement::executeUpdate);
+
+    this.available.available(new Option(String.valueOf(identifier), name, name, order));
+
+  }
+
+}
