07d4fe9ad7d75e243dbf0954dead7d942ae736d9
Adam Loup <aloup@enquizit.com>
2024-02-11 16:38:32 -0600

    user information finder

--- libs/authentication/src/main/java/gov/cdc/nbs/authentication/NBSUserDetailsResolver.java
+++ libs/authentication/src/main/java/gov/cdc/nbs/authentication/NBSUserDetailsResolver.java
@@ -1,31 +1,28 @@
 package gov.cdc.nbs.authentication;
 
-import gov.cdc.nbs.authentication.entity.AuthProgAreaAdmin;
 import gov.cdc.nbs.authentication.entity.AuthUser;
 import gov.cdc.nbs.authentication.enums.AuthRecordStatus;
 import org.springframework.stereotype.Component;
 
-import java.util.stream.Collectors;
-
 @Component
 public class NBSUserDetailsResolver {
 
-    private final UserPermissionFinder finder;
+  private final UserPermissionFinder finder;
 
-    public NBSUserDetailsResolver(final UserPermissionFinder finder) {
-        this.finder = finder;
-    }
+  public NBSUserDetailsResolver(final UserPermissionFinder finder) {
+    this.finder = finder;
+  }
 
-    public NbsUserDetails resolve(final AuthUser authUser) {
-        return NbsUserDetails
-            .builder()
-            .id(authUser.getNedssEntryId())
-            .firstName(authUser.getUserFirstNm())
-            .lastName(authUser.getUserLastNm())
-            .username(authUser.getUserId())
-            .authorities(finder.getUserPermissions(authUser))
-            .isEnabled(authUser.getAudit().recordStatus().equals(AuthRecordStatus.ACTIVE))
-            .build();
-    }
+  public NbsUserDetails resolve(final AuthUser authUser) {
+    return NbsUserDetails
+        .builder()
+        .id(authUser.getNedssEntryId())
+        .firstName(authUser.getUserFirstNm())
+        .lastName(authUser.getUserLastNm())
+        .username(authUser.getUserId())
+        .authorities(finder.getUserPermissions(authUser))
+        .isEnabled(authUser.getAudit().recordStatus().equals(AuthRecordStatus.ACTIVE))
+        .build();
+  }
 
 }
--- libs/me-api/src/test/java/gov/cdc/nbs/me/MeControllerTest.java
+++ libs/me-api/src/test/java/gov/cdc/nbs/me/MeControllerTest.java
@@ -1,40 +1,32 @@
 package gov.cdc.nbs.me;
 
+import gov.cdc.nbs.authentication.NbsAuthority;
 import gov.cdc.nbs.authentication.NbsUserDetails;
-import gov.cdc.nbs.authorization.permission.Permission;
-import gov.cdc.nbs.authorization.permission.PermissionFinder;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class MeControllerTest {
 
     @Test
-    void should_resolve_permissions_from_finder() {
-
-
-
-        PermissionFinder finder = mock(PermissionFinder.class);
-
-        when(finder.find(anyLong())).thenReturn(
-            List.of(
-                new Permission("object-one", "operation-one"),
-                new Permission("object-two", "operation-two")
-            )
-        );
-
+    void should_resolve_permissions_from_authorities() {
         NbsUserDetails user = mock(NbsUserDetails.class);
 
         when(user.getId()).thenReturn(2371L);
         when(user.getFirstName()).thenReturn("first-name");
         when(user.getLastName()).thenReturn("last-name");
+        when(user.getAuthorities()).thenReturn(
+            Set.of(
+                new NbsAuthority("operation-one", "object-one"),
+                new NbsAuthority("operation-two", "object-two")
+            )
+        );
 
-        MeController controller = new MeController(finder);
+        MeController controller = new MeController();
 
         Me actual = controller.me(user);
 
@@ -44,7 +36,7 @@ class MeControllerTest {
             .returns("last-name", Me::lastName)
             .extracting(Me::permissions)
             .asList()
-            .contains("object-one-operation-one", "object-two-operation-two")
+            .contains("operation-one-object-one", "operation-two-object-two")
             ;
 
     }
