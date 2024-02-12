0078d99f472fce0f7130c5fc5f0792492a1afd23
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2024-02-12 07:58:45 -0800

    Cnft1 2016 UI code coverage (#1005)
    * adding installation set up for test coverage
    
    * fixing the coverage statements and removing unneeded configuration
    
    * adding collectCoverage for files in package json scripts
    
    * adding back jest/globals
    
    * - removing jest/globals
    
    * - adding coverage folder to the gitignore file
    
    * removing unneeded import
    
    ---------
    
    Co-authored-by: Henry Clark <HenryClark@Henry-Clark.attlocal.net>

--- apps/modernization-ui/src/apps/search/advancedSearch/AdvancedSearch.spec.tsx
+++ apps/modernization-ui/src/apps/search/advancedSearch/AdvancedSearch.spec.tsx
@@ -8,7 +8,6 @@ import { AdvancedSearch } from 'apps/search/advancedSearch/AdvancedSearch';
 import { fireEvent, render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
-import { expect } from '@jest/globals';
 import { SkipLinkProvider } from 'SkipLink/SkipLinkContext';
 
 describe('AdvancedSearch component tests', () => {
