a871e6940c4c0644eaaa16a97b95da2e31aed1b8
Nouman Javed <njaved@enquizit.com>
2024-02-14 16:04:45 -0500

    Added Default Release Tag and cleaned up variables

--- cdc-sandbox/nbs6-win-container/entrypoint.ps1
+++ cdc-sandbox/nbs6-win-container/entrypoint.ps1
@@ -44,55 +44,62 @@ $subsystems | % {
 # Save XML file after connection url replacement
 $xmlDoc.Save($xmlFileName)
 
-
-############# WIN TASK SCHEDULES #############
+############# Configure User Guide #############
+# NOTE: Verify NBS User Training Guide.pdf is located in release zip file
+# Fetch the latest release
+$latestRelease = Invoke-RestMethod -Uri $GITHUB_API_URL
+# Output the latest tag name
+$latestTag = $latestRelease.tag_name
+Write-Output "Latest Release Tag: $latestTag"
+# Set Zip File Name
+$zip_url = ($latestRelease.assets | Where-Object { $_.name -eq $env:GITHUB_ZIP_FILE_NAME }).browser_download_url
+# Download Zip File
+Invoke-WebRequest -Uri $zip_url -OutFile $env:GITHUB_ZIP_FILE_NAME
+Write-Output "Downloaded Zip File: $env:ZIP_FILE_NAME"
+# Extract ZIP to temporary directory
+Expand-Archive -LiteralPath "$env:GITHUB_ZIP_FILE_NAME" -Force
+Write-Output "$env:GITHUB_ZIP_FILE_NAME"        
+$zip_folder = $env:GITHUB_ZIP_FILE_NAME.Trim(".zip")
+# Move zip file the final destination
+$user_guide_path = Join-Path -Path $zip_folder -ChildPath "$zip_folder/$env:USER_GUIDE_NAME"
+Move-Item -Path $user_guide_path -Destination C:\nbs\wildfly-10.0.0.Final\nedssdomain\Nedss\UserGuide -Force
+Remove-Item $env:GITHUB_ZIP_FILE_NAME
+Rename-Item -Path C:\nbs\wildfly-10.0.0.Final\nedssdomain\Nedss\UserGuide\$env:USER_GUIDE_NAME -NewName $env:FINAL_NBS_USER_GUIDE_NAME
+#### END OF Configure User Guide ####
 
 #### DI app required task schedule ####
-
 # Following commands are required to run ELRImporter.bat in Docker Container Windows ServerCore.
 # This will add the variables in setenvJBOSS.cmd, allowing Instance Scheduler user to view them
-
 # Path to setenvJBOSS.cmd
 $setenvFilePath = "C:\nbs\wildfly-10.0.0.Final\nedssdomain\Nedss\BatchFiles\setenvJBOSS.cmd"
-
 # Read the current content
 $currentContent = Get-Content -Path $setenvFilePath
-
 # String to prepend
 $setEchoOff = "@echo off"
 $setJAVA_HOME = "set JAVA_HOME=$env:JAVA_HOME"
 $setJBOSS_HOME = "set JBOSS_HOME=$env:JBOSS_HOME"
 $setJAVA_TOOL_OPTIONS = "set JAVA_TOOL_OPTIONS=$env:JAVA_TOOL_OPTIONS"
-
 # Combine the new strings with the current content, keeping variables at the top of setenvJBOSS.cmd
 $newContent = $setEchoOff, $setJAVA_HOME, $setJBOSS_HOME, $setJAVA_TOOL_OPTIONS, $currentContent
-
 # Write the new content back to the file
 $newContent | Set-Content -Path $setenvFilePath
-
 $jobName = "ELRImporter Task"
 $repeat = (New-TimeSpan -Minutes 2)
 $currentDate= ([DateTime]::Now)
 $duration = $currentDate.AddYears(25) -$currentDate
-
 # Define the file path
 $scriptDirPath = "D:\wildfly-10.0.0.Final\nedssdomain\Nedss\BatchFiles"
 $scriptPath = ".\ELRImporter.bat"
 $argument = "> ElrImporter.output 2>&1"
 $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType S4U
-
 # Action to run the specified batch file
 $action = New-ScheduledTaskAction -Execute "$scriptPath" -Argument "$argument" -WorkingDirectory "$scriptDirPath"
-
 # Trigger for daily execution once, repeating every 2 minutes
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval $repeat -RepetitionDuration $duration
-
 # Create scheduled task
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd
-
 # Register the scheduled task
 Register-ScheduledTask -TaskName $jobName -Action $action -Trigger $trigger -Principal $principal -Settings $settings
-
 #### END OF TASK SCHEDULES ####
 
 
