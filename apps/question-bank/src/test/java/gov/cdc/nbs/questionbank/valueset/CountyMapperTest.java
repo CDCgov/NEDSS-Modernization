ce7c76757ffc9c339114d8e0e868dcd7b4c603e6
Sheham Izat <125893922+sizatenquizit@users.noreply.github.com>
2024-02-15 15:41:29 -0500

    Creating API to fetch the County options for a given State (#1036)

---
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/valueset/CountyMapperTest.java
@@ -0,0 +1,41 @@
+package gov.cdc.nbs.questionbank.valueset;
+
+
+import gov.cdc.nbs.questionbank.valueset.response.County;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CountyMapperTest {
+
+  CountyMapper mapper = new CountyMapper();
+
+  private static final int CODE = 1;
+  private static int SHORT_DESCRIPTION = 2;
+  private static int DESCRIPTION = 3;
+  private static int CODE_SET_NAME = 4;
+
+  @Test
+  void findByStateCodeTest() throws SQLException {
+    ResultSet rs = mock(ResultSet.class);
+    when(rs.getString(CODE)).thenReturn("55077");
+    when(rs.getString(SHORT_DESCRIPTION)).thenReturn("Wood,WV");
+    when(rs.getString(DESCRIPTION)).thenReturn("Wood County");
+    when(rs.getString(CODE_SET_NAME)).thenReturn("COUNTY_CCD");
+
+    County response = mapper.mapRow(rs, 1);
+    assertEquals("55077", response.code());
+    assertEquals("Wood,WV", response.shortName());
+    assertEquals("Wood County", response.longName());
+    assertEquals("COUNTY_CCD", response.codeSetName());
+  }
+
+}
