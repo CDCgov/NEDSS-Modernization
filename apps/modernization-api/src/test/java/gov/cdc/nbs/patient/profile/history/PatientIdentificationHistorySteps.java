068fd5e6f1e0bff614702a1392d4316ccb4a150a
Ragul Shanmugam <123001929+rshanmugam-enquizit@users.noreply.github.com>
2024-02-19 11:04:15 -0800

    [CNFT1-1965] Patient identification change history creation (#1048)
    * Partial identification history creator logic added.
    
    * Patient identification history creation logic with tests added
    
    * Import clean up and renaming variables
    
    * Sonar code smell fix
    
    * Sonar code smell fix - 2. Removed unused import (being lazy and not checking properly before pushing)

---
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/patient/profile/history/PatientIdentificationHistorySteps.java
@@ -0,0 +1,27 @@
+package gov.cdc.nbs.patient.profile.history;
+
+import gov.cdc.nbs.patient.identifier.PatientIdentifier;
+import gov.cdc.nbs.testing.support.Active;
+import io.cucumber.java.en.Then;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PatientIdentificationHistorySteps {
+    private final Active<PatientIdentifier> activePatient;
+    private final PatientIdentificationHistoryPreviousVersionVerifier versionVerifier;
+
+    public PatientIdentificationHistorySteps(Active<PatientIdentifier> activePatient, PatientIdentificationHistoryPreviousVersionVerifier versionVerifier) {
+        this.activePatient = activePatient;
+        this.versionVerifier = versionVerifier;
+    }
+
+    @Then("the patient identification history contains the previous version")
+    public void the_patient_name_history_contains_the_previous_version() {
+        boolean verified = this.activePatient.maybeActive()
+                .map(active -> this.versionVerifier.verify(active.id()))
+                .orElse(false);
+
+        assertThat(verified).isTrue();
+    }
+
+}
