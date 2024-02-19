3cb04bc797734b1ee8d7906b3a6a1e03647ffaed
Ragul Shanmugam <123001929+rshanmugam-enquizit@users.noreply.github.com>
2024-02-19 09:16:26 -0800

    [CNFT1-1962] Patient name change history creation (#1047)
    * Patient name histroy creator logic added.
    
    * Patient name history tests added
    
    * Sonar code smells fix
    
    * Fixed typo in feature file name

---
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/patient/profile/history/PatientNameHistoryPreviousVersionVerifier.java
@@ -0,0 +1,31 @@
+package gov.cdc.nbs.patient.profile.history;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PatientNameHistoryPreviousVersionVerifier {
+    private static final String QUERY = """
+      select 1 from Person_name [patient]
+          join Person_name_hist [history] on
+                  [history].[person_uid] = [patient].[person_uid]
+      where [patient].person_uid = ?
+      and [patient].person_name_seq = 1
+      """;
+    private static final int RESULT_COLUMN = 1;
+    private static final int PATIENT_PARAMETER = 1;
+
+    private final JdbcTemplate template;
+
+    PatientNameHistoryPreviousVersionVerifier(final JdbcTemplate template) {
+        this.template = template;
+    }
+
+    boolean verify(final long patient) {
+        return !this.template.query(
+                QUERY,
+                statement -> statement.setLong(PATIENT_PARAMETER, patient),
+                (resultSet, row) -> resultSet.getLong(RESULT_COLUMN)
+        ).isEmpty();
+    }
+}
