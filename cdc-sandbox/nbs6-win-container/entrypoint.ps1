ea38389ab592ffcddccb58d6aa99de93cd88cbac
njaved <46687381+njaved@users.noreply.github.com>
2024-02-20 14:05:06 -0500

    Dockerfile User Update (#1059)

--- cdc-sandbox/nbs6-win-container/entrypoint.ps1
+++ cdc-sandbox/nbs6-win-container/entrypoint.ps1
@@ -47,69 +47,46 @@ $xmlDoc.Save($xmlFileName)
 ############# Configure User Guide #############
 # NOTE: Verify NBS User Training Guide.pdf is located in release zip file
 
-if ($env:GITHUB_RELEASE_TAG -eq 'latest') {
-    $github_api_url = "https://api.github.com/repos/CDCgov/NEDSS-Modernization/releases/latest"
+# If $env:GITHUB_RELEASE_TAG null or latest, then choose ../releases/latest URL
+if (-not $env:GITHUB_RELEASE_TAG -or $env:GITHUB_RELEASE_TAG -eq "latest") {
+    Write-Output "Getting $env:GITHUB_RELEASE_TAG Release URL"
+    # Fetch the latest release
+    $githubRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/CDCgov/NEDSS-Modernization/releases/latest"
+    # Output the latest tag name
+    $releaseTag = $githubRelease.tag_name
   } else {
-    $github_api_url = "https://api.github.com/repos/CDCgov/NEDSS-Modernization/releases/tags/$env:GITHUB_RELEASE_TAG"
+    "Getting $env:GITHUB_RELEASE_TAG Release URL"
+    # Fetch the latest release
+    $githubRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/CDCgov/NEDSS-Modernization/releases/tags/$env:GITHUB_RELEASE_TAG"
+    # Output the latest tag name
+    $releaseTag = $githubRelease.tag_name
   }
 
-# Fetch the latest release
-$githubRelease = Invoke-RestMethod -Uri $github_api_url
-# Output the latest tag name
-$releaseTag = $githubRelease.tag_name
+
 Write-Output "Release Tag to Download Zip From: $releaseTag"
+
+# Update Variables
+$zip_file_name = $env:GITHUB_ZIP_FILE_NAME -replace "<version>", $releaseTag
+$user_guide_name_version = $releaseTag.Trim("v")
+$user_guide_name = $env:USER_GUIDE_DOC_NAME -replace "<version>", $user_guide_name_version
 # Set Zip File Name
-$zip_url = ($githubRelease.assets | Where-Object { $_.name -eq $env:GITHUB_ZIP_FILE_NAME }).browser_download_url
+$zip_url = ($githubRelease.assets | Where-Object { $_.name -like "$zip_file_name" }).browser_download_url
 # Download Zip File. System.Net.WebClient is faster then using Invoke-WebRequest
 $webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($zip_url, $env:GITHUB_ZIP_FILE_NAME)
-Write-Output "Downloaded Zip File: $env:GITHUB_ZIP_FILE_NAME"
+$webClient.DownloadFile($zip_url, $zip_file_name)
+Write-Output "Downloaded Zip File: $zip_file_name"
 # Extract ZIP to temporary directory
-Expand-Archive -LiteralPath "$env:GITHUB_ZIP_FILE_NAME" -Force
-$zip_folder = $env:GITHUB_ZIP_FILE_NAME.Trim(".zip")
+Expand-Archive -LiteralPath "$zip_file_name" -Force
+$zip_folder = $zip_file_name.Trim(".zip")
 # Move zip file the final destination
-$zip_user_guide_path = Join-Path -Path $zip_folder -ChildPath "$zip_folder\$env:USER_GUIDE_DOC_NAME"
+$zip_user_guide_path = Join-Path -Path $zip_folder -ChildPath "$zip_folder\$user_guide_name"
 $user_guide_directory = "C:\nbs\wildfly-10.0.0.Final\nedssdomain\Nedss\UserGuide\$env:FINAL_NBS_USER_GUIDE_NAME"
+Write-Output "Moving and Renaming User Guide '$user_guide_name' to '$env:FINAL_NBS_USER_GUIDE_NAME'"
 Copy-Item -Path "$zip_user_guide_path" -Destination "$user_guide_directory" -Force
 # Cleanup
-Remove-Item $env:GITHUB_ZIP_FILE_NAME
+Remove-Item $zip_file_name
 Remove-Item $zip_folder -Recurse -Force -Confirm:$false
 #### END OF Configure User Guide ####
-
-#### DI app required task schedule ####
-# Following commands are required to run ELRImporter.bat in Docker Container Windows ServerCore.
-# This will add the variables in setenvJBOSS.cmd, allowing Instance Scheduler user to view them
-# Path to setenvJBOSS.cmd
-$setenvFilePath = "C:\nbs\wildfly-10.0.0.Final\nedssdomain\Nedss\BatchFiles\setenvJBOSS.cmd"
-# Read the current content
-$currentContent = Get-Content -Path $setenvFilePath
-# String to prepend
-$setEchoOff = "@echo off"
-$setJAVA_HOME = "set JAVA_HOME=$env:JAVA_HOME"
-$setJBOSS_HOME = "set JBOSS_HOME=$env:JBOSS_HOME"
-$setJAVA_TOOL_OPTIONS = "set JAVA_TOOL_OPTIONS=$env:JAVA_TOOL_OPTIONS"
-# Combine the new strings with the current content, keeping variables at the top of setenvJBOSS.cmd
-$newContent = $setEchoOff, $setJAVA_HOME, $setJBOSS_HOME, $setJAVA_TOOL_OPTIONS, $currentContent
-# Write the new content back to the file
-$newContent | Set-Content -Path $setenvFilePath
-$jobName = "ELRImporter Task"
-$repeat = (New-TimeSpan -Minutes 2)
-$currentDate= ([DateTime]::Now)
-$duration = $currentDate.AddYears(25) -$currentDate
-# Define the file path
-$scriptDirPath = "D:\wildfly-10.0.0.Final\nedssdomain\Nedss\BatchFiles"
-$scriptPath = ".\ELRImporter.bat"
-$argument = "> ElrImporter.output 2>&1"
-$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType S4U
-# Action to run the specified batch file
-$action = New-ScheduledTaskAction -Execute "$scriptPath" -Argument "$argument" -WorkingDirectory "$scriptDirPath"
-# Trigger for daily execution once, repeating every 2 minutes
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval $repeat -RepetitionDuration $duration
-# Create scheduled task
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd
-# Register the scheduled task
-Register-ScheduledTask -TaskName $jobName -Action $action -Trigger $trigger -Principal $principal -Settings $settings
-#### END OF TASK SCHEDULES ####
 
 
 Start-Process "C:\\nbs\\wildfly-10.0.0.Final\\bin\\standalone.bat" -Wait -NoNewWindow -PassThru | Out-Host
