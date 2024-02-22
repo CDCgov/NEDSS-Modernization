6f744738b8d0a678a92d7891f443796be3951c9b
Sheham Izat <sizat@enquizit.com>
2024-02-22 17:34:45 -0500

    Update the page rule search API to match the UI needs(fixes)

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/PageRuleMapperTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/PageRuleMapperTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -18,10 +18,10 @@ class PageRuleMapperTest {
   @InjectMocks
   private PageRuleMapper pageRuleMapper;
 
+
   @Test
   void testMapRow() throws SQLException {
     ResultSet resultSet = mock(ResultSet.class);
-
     when(resultSet.getLong(1)).thenReturn(100l);
     when(resultSet.getLong(2)).thenReturn(123l);
     when(resultSet.getString(3)).thenReturn("Enable");
@@ -60,5 +60,32 @@ class PageRuleMapperTest {
     assertEquals("test456_label", response.targets().get(0).label());
   }
 
+
+  @Test
+  void testMapRow_null_values() throws SQLException {
+    ResultSet resultSet = mock(ResultSet.class);
+    when(resultSet.getLong(1)).thenReturn(100l);
+    when(resultSet.getLong(2)).thenReturn(123l);
+    when(resultSet.getString(3)).thenReturn("xx");
+    when(resultSet.getString(4)).thenReturn("description");
+    when(resultSet.getString(5)).thenReturn("test456");
+    when(resultSet.getString(6)).thenReturn("test( )test");
+    when(resultSet.getString(7)).thenReturn(null);
+    when(resultSet.getString(8)).thenReturn("xx");
+    when(resultSet.getString(9)).thenReturn("xx");
+    when(resultSet.getString(10)).thenReturn(null);
+    when(resultSet.getString(11)).thenReturn("label123");
+    when(resultSet.getString(12)).thenReturn("codeSetName");
+    when(resultSet.getLong(13)).thenReturn(1000l);
+    when(resultSet.getString(14)).thenReturn(null);
+    when(resultSet.getLong(15)).thenReturn(10l);
+    Rule actualResponse = pageRuleMapper.mapRow(resultSet, 1);
+
+    assertNull(actualResponse.sourceValues());
+    assertTrue(actualResponse.targets().isEmpty());
+    assertNull(actualResponse.ruleFunction());
+    assertNull(actualResponse.comparator());
+    assertNull(actualResponse.targetType());
+  }
 
 }
