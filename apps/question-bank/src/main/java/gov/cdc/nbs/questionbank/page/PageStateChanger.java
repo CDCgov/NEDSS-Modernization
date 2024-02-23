3ca765a38e049dda9d29a39cf0f0009f70cadae2
Sheham Izat <125893922+sizatenquizit@users.noreply.github.com>
2024-02-23 10:08:27 -0500

    Edit the Page Draft response to return the id of the Draft Page. (#1062)

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/page/PageStateChanger.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/page/PageStateChanger.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import java.util.Set;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import gov.cdc.nbs.questionbank.entity.PageCondMapping;
@@ -25,113 +24,113 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PageStateChanger {
 
-    @Autowired
-    private WaTemplateRepository templateRepository;
 
-    @Autowired
-    private WaUiMetadataRepository waUiMetadataRepository;
-    
-    @Autowired
-    private PageCondMappingRepository pageConMappingRepository;
-    
-    
+  private final WaTemplateRepository templateRepository;
 
-    public PageStateResponse savePageAsDraft(Long id) {
-        PageStateResponse response = new PageStateResponse();
-        try {
-            Optional<WaTemplate> result = templateRepository.findById(id);
-            if (result.isPresent()) {
-                WaTemplate page = result.get();
-                if(page.getTemplateType().equals(PageConstants.PUBLISHED_WITH_DRAFT))  {	
-                throw new PageUpdateException(PageConstants.SAVE_DRAFT_NOCHANGE);
-                }
-                WaTemplate draftPage = createDraftCopy(page);
-                page.setTemplateType(PageConstants.PUBLISHED_WITH_DRAFT);
-               
-                page = templateRepository.save(page);
-                draftPage = templateRepository.save(draftPage);
-                pageConMappingRepository.saveAll(draftPage.getConditionMappings());
-                
-                List<WaUiMetadata> draftMappings = copyWaTemplateUIMetaData(page, draftPage);
-                
-                waUiMetadataRepository.saveAll(draftMappings);
-                response.setMessage(PageConstants.SAVE_DRAFT_SUCCESS);
-                response.setTemplateId(page.getId());
-            } else {
-                throw new PageUpdateException(PageConstants.SAVE_DRAFT_FAIL);
-            }
-        } catch (PageUpdateException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new PageUpdateException(PageConstants.SAVE_DRAFT_FAIL);
+
+  private final WaUiMetadataRepository waUiMetadataRepository;
+
+
+  private final PageCondMappingRepository pageConMappingRepository;
+
+
+
+  public PageStateResponse savePageAsDraft(Long id) {
+    PageStateResponse response = new PageStateResponse();
+    try {
+      Optional<WaTemplate> result = templateRepository.findById(id);
+      if (result.isPresent()) {
+        WaTemplate page = result.get();
+        if (page.getTemplateType().equals(PageConstants.PUBLISHED_WITH_DRAFT)) {
+          throw new PageUpdateException(PageConstants.SAVE_DRAFT_NOCHANGE);
         }
-        return response;
+        WaTemplate draftPage = createDraftCopy(page);
+        page.setTemplateType(PageConstants.PUBLISHED_WITH_DRAFT);
+
+        page = templateRepository.save(page);
+        draftPage = templateRepository.save(draftPage);
+        pageConMappingRepository.saveAll(draftPage.getConditionMappings());
+
+        List<WaUiMetadata> draftMappings = copyWaTemplateUIMetaData(page, draftPage);
+
+        waUiMetadataRepository.saveAll(draftMappings);
+        response.setMessage(PageConstants.SAVE_DRAFT_SUCCESS);
+        response.setTemplateId(draftPage.getId());
+      } else {
+        throw new PageUpdateException(PageConstants.SAVE_DRAFT_FAIL);
+      }
+    } catch (PageUpdateException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new PageUpdateException(PageConstants.SAVE_DRAFT_FAIL);
     }
-    
-	
+    return response;
+  }
 
-    public List<WaUiMetadata> copyWaTemplateUIMetaData(WaTemplate page, WaTemplate clonePage) {
-        List<WaUiMetadata> draftMappings = new ArrayList<>();
-        List<WaUiMetadata> pages = waUiMetadataRepository.findAllByWaTemplateUid(page);
-        for (WaUiMetadata original : pages) {
-            WaUiMetadata clone = original;
-            clone.setWaTemplateUid(clonePage);
-            draftMappings.add(clone);
-        }
-        return draftMappings;
 
+
+  public List<WaUiMetadata> copyWaTemplateUIMetaData(WaTemplate page, WaTemplate clonePage) {
+    List<WaUiMetadata> draftMappings = new ArrayList<>();
+    List<WaUiMetadata> pages = waUiMetadataRepository.findAllByWaTemplateUid(page);
+    for (WaUiMetadata original : pages) {
+      WaUiMetadata clone = original;
+      clone.setWaTemplateUid(clonePage);
+      draftMappings.add(clone);
+    }
+    return draftMappings;
+
+  }
+
+  public WaTemplate createDraftCopy(WaTemplate oldPage) {
+    if (oldPage.getTemplateType().equals("Draft")) {
+      return oldPage;
     }
 
-    public WaTemplate createDraftCopy(WaTemplate oldPage) {
-        if (oldPage.getTemplateType().equals("Draft")) {
-            return oldPage;
-        }
-
-        WaTemplate draftCopy = new WaTemplate();
-        draftCopy.setTemplateNm(oldPage.getTemplateNm());
-        draftCopy.setTemplateType("Draft");
-        draftCopy.setPublishVersionNbr(0);
-        draftCopy.setPublishIndCd('F');
-        draftCopy.setAddTime(oldPage.getAddTime());
-        draftCopy.setAddUserId(oldPage.getAddUserId());
-        draftCopy.setBusObjType(oldPage.getBusObjType());
-        draftCopy.setConditionCd(oldPage.getConditionCd());
-        draftCopy.setDatamartNm(oldPage.getDatamartNm());
-        draftCopy.setDescTxt(oldPage.getDescTxt());
-        draftCopy.setFormCd(oldPage.getFormCd());
-        draftCopy.setLastChgTime(oldPage.getLastChgTime());
-        draftCopy.setLastChgUserId(oldPage.getLastChgUserId());
-        draftCopy.setLocalId(oldPage.getLocalId());
-        draftCopy.setNndEntityIdentifier(oldPage.getNndEntityIdentifier());
-        draftCopy.setParentTemplateUid(oldPage.getParentTemplateUid());
-        draftCopy.setRecordStatusCd(oldPage.getRecordStatusCd());
-        draftCopy.setRecordStatusTime(oldPage.getRecordStatusTime());
-        draftCopy.setSourceNm(oldPage.getSourceNm());
-        draftCopy.setVersionNote(oldPage.getVersionNote());
-        draftCopy.setXmlPayload(oldPage.getXmlPayload());
-        draftCopy.setConditionMappings(copyConditionMappings(oldPage.getConditionMappings(), draftCopy));
+    WaTemplate draftCopy = new WaTemplate();
+    draftCopy.setTemplateNm(oldPage.getTemplateNm());
+    draftCopy.setTemplateType("Draft");
+    draftCopy.setPublishVersionNbr(0);
+    draftCopy.setPublishIndCd('F');
+    draftCopy.setAddTime(oldPage.getAddTime());
+    draftCopy.setAddUserId(oldPage.getAddUserId());
+    draftCopy.setBusObjType(oldPage.getBusObjType());
+    draftCopy.setConditionCd(oldPage.getConditionCd());
+    draftCopy.setDatamartNm(oldPage.getDatamartNm());
+    draftCopy.setDescTxt(oldPage.getDescTxt());
+    draftCopy.setFormCd(oldPage.getFormCd());
+    draftCopy.setLastChgTime(oldPage.getLastChgTime());
+    draftCopy.setLastChgUserId(oldPage.getLastChgUserId());
+    draftCopy.setLocalId(oldPage.getLocalId());
+    draftCopy.setNndEntityIdentifier(oldPage.getNndEntityIdentifier());
+    draftCopy.setParentTemplateUid(oldPage.getParentTemplateUid());
+    draftCopy.setRecordStatusCd(oldPage.getRecordStatusCd());
+    draftCopy.setRecordStatusTime(oldPage.getRecordStatusTime());
+    draftCopy.setSourceNm(oldPage.getSourceNm());
+    draftCopy.setVersionNote(oldPage.getVersionNote());
+    draftCopy.setXmlPayload(oldPage.getXmlPayload());
+    draftCopy.setConditionMappings(copyConditionMappings(oldPage.getConditionMappings(), draftCopy));
 
 
-        return draftCopy;
+    return draftCopy;
+
+  }
+
+  private Set<PageCondMapping> copyConditionMappings(Set<PageCondMapping> original, WaTemplate page) {
+    if (original == null)
+      return original;
+    Set<PageCondMapping> copy = new HashSet<>();
+    for (PageCondMapping con : original) {
+      PageCondMapping aCopy = new PageCondMapping();
+      aCopy.setAddTime(con.getAddTime());
+      aCopy.setAddUserId(con.getAddUserId());
+      aCopy.setConditionCd(con.getConditionCd());
+      aCopy.setLastChgTime(con.getLastChgTime());
+      aCopy.setLastChgUserId(con.getLastChgUserId());
+      aCopy.setWaTemplateUid(page);
+      copy.add(aCopy);
 
     }
-    
-	private Set<PageCondMapping> copyConditionMappings(Set<PageCondMapping> original, WaTemplate page) {
-		if (original == null)
-			return original;
-		Set<PageCondMapping> copy = new HashSet<>();
-		for (PageCondMapping con : original) {
-			PageCondMapping aCopy = new PageCondMapping();
-			aCopy.setAddTime(con.getAddTime());
-			aCopy.setAddUserId(con.getAddUserId());
-			aCopy.setConditionCd(con.getConditionCd());
-			aCopy.setLastChgTime(con.getLastChgTime());
-			aCopy.setLastChgUserId(con.getLastChgUserId());
-			aCopy.setWaTemplateUid(page);
-			copy.add(aCopy);
-
-		}
-		return copy;
-	}
+    return copy;
+  }
 
 }
