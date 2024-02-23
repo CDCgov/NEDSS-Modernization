837fd4fae80e371fd10b2a7a1aef62100a9f1369
Sheham Izat <125893922+sizatenquizit@users.noreply.github.com>
2024-02-23 10:06:42 -0500

    Update the page rule search API to match the UI needs. (#1080)
    * Update the page rule search API to match the UI needs.
    
    * Update the page rule search API to match the UI needs(fixes)

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/PageRuleMapperTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/PageRuleMapperTest.java
@@ -1,31 +1,23 @@
 package gov.cdc.nbs.questionbank.pagerules;
 
-import gov.cdc.nbs.questionbank.question.repository.WaQuestionRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.Test;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PageRuleMapperTest {
 
-
-  @Mock
-  private WaQuestionRepository waQuestionRepository;
-
   @InjectMocks
   private PageRuleMapper pageRuleMapper;
+
 
   @Test
   void testMapRow() throws SQLException {
@@ -39,14 +31,12 @@ class PageRuleMapperTest {
     when(resultSet.getString(7)).thenReturn("test1,test2,test3");
     when(resultSet.getString(8)).thenReturn("=");
     when(resultSet.getString(9)).thenReturn("QUESTION");
-    when(resultSet.getString(10)).thenReturn("test456,test789");
+    when(resultSet.getString(10)).thenReturn("test456,test789,test123");
     when(resultSet.getString(11)).thenReturn("label123");
     when(resultSet.getString(12)).thenReturn("codeSetName");
     when(resultSet.getLong(13)).thenReturn(1000l);
-    when(resultSet.getLong(14)).thenReturn(10l);
-    List<Object[]> targetQuestions =
-        Arrays.asList(new Object[] {"test456", "label1"}, new Object[] {" test789", "label2"});
-    when(waQuestionRepository.findLabelsByIdentifiers(any())).thenReturn(targetQuestions);
+    when(resultSet.getString(14)).thenReturn("test456_label,test789_label");
+    when(resultSet.getLong(15)).thenReturn(10l);
     Rule actualResponse = pageRuleMapper.mapRow(resultSet, 1);
     validate(actualResponse);
     long rowsCount = pageRuleMapper.getTotalRowsCount();
@@ -65,9 +55,37 @@ class PageRuleMapperTest {
     assertEquals(3, response.sourceValues().size());
     assertEquals("EQUAL_TO", response.comparator().toString());
     assertEquals("QUESTION", response.targetType().toString());
-    assertEquals(2, response.targets().size());
     assertEquals(1000l, response.sourceQuestion().questionId());
+    assertEquals("test456", response.targets().get(0).targetIdentifier());
+    assertEquals("test456_label", response.targets().get(0).label());
   }
 
+
+  @Test
+  void testMapRow_null_values() throws SQLException {
+    ResultSet resultSet = mock(ResultSet.class);
+    when(resultSet.getLong(1)).thenReturn(100l);
+    when(resultSet.getLong(2)).thenReturn(123l);
+    when(resultSet.getString(3)).thenReturn("xx");
+    when(resultSet.getString(4)).thenReturn("description");
+    when(resultSet.getString(5)).thenReturn("test456");
+    when(resultSet.getString(6)).thenReturn("test( )test");
+    when(resultSet.getString(7)).thenReturn(null);
+    when(resultSet.getString(8)).thenReturn("xx");
+    when(resultSet.getString(9)).thenReturn("xx");
+    when(resultSet.getString(10)).thenReturn(null);
+    when(resultSet.getString(11)).thenReturn("label123");
+    when(resultSet.getString(12)).thenReturn("codeSetName");
+    when(resultSet.getLong(13)).thenReturn(1000l);
+    when(resultSet.getString(14)).thenReturn(null);
+    when(resultSet.getLong(15)).thenReturn(10l);
+    Rule actualResponse = pageRuleMapper.mapRow(resultSet, 1);
+
+    assertNull(actualResponse.sourceValues());
+    assertTrue(actualResponse.targets().isEmpty());
+    assertNull(actualResponse.ruleFunction());
+    assertNull(actualResponse.comparator());
+    assertNull(actualResponse.targetType());
+  }
 
 }
