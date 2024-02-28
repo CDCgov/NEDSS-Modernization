c10f7c8dc657d7ae5d3dab1b4bb769dd0c7c5ec6
Steven Urciuoli <steveu@stevens-mbp-2.lan>
2024-02-28 14:02:38 -0600

    code smell

--- libs/database-entities/src/main/java/gov/cdc/nbs/repository/ConditionCodeRepository.java
+++ libs/database-entities/src/main/java/gov/cdc/nbs/repository/ConditionCodeRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface ConditionCodeRepository
-    extends JpaRepository<ConditionCode, String>, QuerydslPredicateExecutor<ConditionCode> {
-
+                extends JpaRepository<ConditionCode, String>, QuerydslPredicateExecutor<ConditionCode> {
 }
