be572fba66f39f37d4ea7352b278b0f9b91d366d
Lakshmi Narayanan Ramakrishnan <132704359+lramakr@users.noreply.github.com>
2024-03-07 11:45:06 -0700

    CNFT2-2138 Preview Page Group Questions (#1146)

--- apps/modernization-ui/src/apps/page-builder/page/management/preview/tab/subsection/PreviewSubsection.tsx
+++ apps/modernization-ui/src/apps/page-builder/page/management/preview/tab/subsection/PreviewSubsection.tsx
@@ -3,6 +3,8 @@ import { PagesSubSection } from '../../../../../generated';
 import { PreviewQuestion } from '../question/PreviewQuestion';
 import { PreviewSubsectionHeader } from './PreviewSubsectionHeader';
 import { useState } from 'react';
+import { Heading } from '../../../../../../../components/heading';
+import { Button } from '@trussworks/react-uswds';
 
 type Props = {
     subsection: PagesSubSection;
@@ -25,10 +27,42 @@ export const PreviewSubsection = ({ subsection }: Props) => {
                         isExpanded={isExpanded}
                         onExpandedChange={handleExpandedChange}
                     />
-                    <div className={styles.questionWrapper}>
-                        {isExpanded &&
-                            subsection.questions.map((question, k) => <PreviewQuestion question={question} key={k} />)}
-                    </div>
+                    {isExpanded && (
+                        <div className={styles.questionWrapper}>
+                            {subsection.isGrouped ? (
+                                <div className={styles.grouped}>
+                                    <div className={styles.groupedForm}>
+                                        {subsection.questions.map((question, k) => (
+                                            <div className={styles.groupedQuestionName} key={k}>
+                                                <Heading level={3}>{question.name}</Heading>
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <p className={styles.groupedInfo}>No data has been entered.</p>
+                                    <div className={styles.groupedQuestionsSection}>
+                                        {subsection.questions.map((question, k) => (
+                                            <PreviewQuestion
+                                                question={question}
+                                                isGrouped={subsection.isGrouped}
+                                                key={k}
+                                            />
+                                        ))}
+                                    </div>
+                                    <div className={styles.footer}>
+                                        <Button type="button" disabled={true}>
+                                            Add
+                                        </Button>
+                                    </div>
+                                </div>
+                            ) : (
+                                <>
+                                    {subsection.questions.map((question, k) => (
+                                        <PreviewQuestion question={question} key={k} />
+                                    ))}
+                                </>
+                            )}
+                        </div>
+                    )}
                 </div>
             )}
         </>
