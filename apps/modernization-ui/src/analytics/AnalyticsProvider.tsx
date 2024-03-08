1fb22c6a5d1200f6b58a1cb7a8c13e916dd573ec
Adam Loup <aloup@enquizit.com>
2024-03-08 07:21:38 -0600

    adds pageview tracker

--- apps/modernization-ui/src/analytics/AnalyticsProvider.tsx
+++ apps/modernization-ui/src/analytics/AnalyticsProvider.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { AnalyticsSettings, useAnalyticsSettings } from './useAnalytics';
 import { PostHogProvider } from 'posthog-js/react';
+import { PageviewTracker } from './PageviewTracker';
 
 type AnalyticsProviderProps = {
     children: ReactNode;
@@ -13,6 +14,7 @@ const AnalyticsProvider = ({ children }: AnalyticsProviderProps) => {
 
 const withAnalytics = (settings: AnalyticsSettings, children: ReactNode) => (
     <PostHogProvider apiKey={settings.key} options={settings.options}>
+        <PageviewTracker />
         {children}
     </PostHogProvider>
 );
