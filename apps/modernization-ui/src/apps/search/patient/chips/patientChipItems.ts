e238ee0e998753b4754f8a260e76d71ce55f2c43
hclarkEnq <hclark@enquizit.com>
2024-03-11 11:08:46 -0700

    adding patientChipsItems destructuring filter object into an array to handle patient chips

--- apps/modernization-ui/src/apps/search/patient/chips/patientChipItems.ts
+++ apps/modernization-ui/src/apps/search/patient/chips/patientChipItems.ts
@@ -7,60 +7,55 @@ export interface ChipItem {
     source: string;
 }
 
-const getNameAndValue = (
-    key: string,
-    value: string | { identificationType?: string; identificationNumber?: string },
-    searchCriteria: SearchCriteria
-): { name: string; value: string } => {
-    const nameMap: { [key: string]: string } = {
-        lastName: 'LAST',
-        firstName: 'FIRST',
-        dateOfBirth: 'DOB'
-    };
-
-    const dynamicValueLookup: { [key: string]: () => string | undefined } = {
-        ethnicity: () => searchCriteria.ethnicities.find((e) => e.id.code === value)?.codeDescTxt,
-        race: () => searchCriteria.races.find((r) => r.id.code === value)?.codeDescTxt,
-        state: () => searchCriteria.states.find((s) => s.value === value)?.name
-    };
-
-    const dynamicValue = dynamicValueLookup[key] ? dynamicValueLookup[key]() : undefined;
-
-    return {
-        name: nameMap[key] || key.toUpperCase(),
-        value: dynamicValue || value.toString()
-    };
-};
-
 export const patientChipItems = (filter: PersonFilter, searchCriteria: SearchCriteria): ChipItem[] => {
     const items: ChipItem[] = [];
+    for (const key in filter) {
+        if (Object.prototype.hasOwnProperty.call(filter, key)) {
+            const value = filter[key as keyof PersonFilter];
+            if (value && key !== 'recordStatus') {
+                if (key === 'identification' && typeof value === 'object') {
+                    const { identificationType, identificationNumber } = value;
+                    if (identificationType) {
+                        items.push({
+                            name: 'ID TYPE',
+                            source: 'identification.identificationType',
+                            value:
+                                searchCriteria.identificationTypes.find((i) => i.id.code === identificationType)
+                                    ?.codeDescTxt ?? ''
+                        });
+                    }
+                    if (identificationNumber) {
+                        items.push({
+                            name: 'ID NUMBER',
+                            source: 'identification.identificationNumber',
+                            value: identificationNumber
+                        });
+                    }
+                } else {
+                    const nameMap: { [key: string]: string } = {
+                        lastName: 'LAST',
+                        firstName: 'FIRST',
+                        dateOfBirth: 'DOB',
+                        gender: 'SEX'
+                    };
 
-    Object.entries(filter).forEach(([key, originalValue]) => {
-        if (!originalValue || key === 'recordStatus') return;
+                    const dynamicValueLookup: { [key: string]: () => string | undefined } = {
+                        ethnicity: () => searchCriteria.ethnicities.find((e) => e.id.code === value)?.codeDescTxt,
+                        race: () => searchCriteria.races.find((r) => r.id.code === value)?.codeDescTxt,
+                        state: () => searchCriteria.states.find((s) => s.value === value)?.name
+                    };
 
-        if (key === 'identification' && typeof originalValue === 'object') {
-            const { identificationType, identificationNumber } = originalValue;
-            if (identificationType) {
-                items.push({
-                    name: 'ID TYPE',
-                    source: 'identification.identificationType',
-                    value:
-                        searchCriteria.identificationTypes.find((i) => i.id.code === identificationType)?.codeDescTxt ??
-                        ''
-                });
+                    const dynamicValue = dynamicValueLookup[key] ? dynamicValueLookup[key]() : undefined;
+
+                    items.push({
+                        name: nameMap[key] || key.toUpperCase(),
+                        value: dynamicValue || value.toString(),
+                        source: key
+                    });
+                }
             }
-            if (identificationNumber) {
-                items.push({
-                    name: 'ID NUMBER',
-                    source: 'identification.identificationNumber',
-                    value: identificationNumber
-                });
-            }
-        } else {
-            const { name, value } = getNameAndValue(key, originalValue as string, searchCriteria);
-            items.push({ name, value, source: key });
         }
-    });
+    }
 
     return items;
 };
