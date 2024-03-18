dcb375f7d4c89ba305ab7394febbec39bd015036
Michael Peels <michaelpeels@Michael-Peels-Work-6.local>
2024-03-18 13:44:30 -0400

    Filter out questions that do not appear in batch from preview

--- apps/modernization-ui/src/apps/page-builder/page/management/preview/tab/subsection/PreviewSubsection.tsx
+++ apps/modernization-ui/src/apps/page-builder/page/management/preview/tab/subsection/PreviewSubsection.tsx
@@ -44,13 +44,15 @@ export const PreviewSubsection = ({ subsection }: Props) => {
                                     </div>
                                     <p className={styles.groupedInfo}>No data has been entered.</p>
                                     <div className={styles.groupedQuestionsSection}>
-                                        {subsection.questions.map((question, k) => (
-                                            <PreviewQuestion
-                                                question={question}
-                                                isGrouped={subsection.isGrouped}
-                                                key={k}
-                                            />
-                                        ))}
+                                        {subsection.questions
+                                            .filter((q) => q.appearsInBatch)
+                                            .map((question, k) => (
+                                                <PreviewQuestion
+                                                    question={question}
+                                                    isGrouped={subsection.isGrouped}
+                                                    key={k}
+                                                />
+                                            ))}
                                     </div>
                                     <div className={styles.footer}>
                                         <Button type="button" disabled={true}>
