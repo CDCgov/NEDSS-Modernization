ff7adbca3d8d0708a841f3aee77ba5b420d4ae14
Michael Peels <michaelpeels@Michael-Peels-Work-6.local>
2024-03-19 15:10:41 -0400

    Controller test

---
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/PageRuleControllerTest.java
@@ -0,0 +1,59 @@
+package gov.cdc.nbs.questionbank.pagerules;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.ResponseEntity;
+import gov.cdc.nbs.questionbank.page.content.rule.PageRuleDeleter;
+
+@ExtendWith(MockitoExtension.class)
+class PageRuleControllerTest {
+
+  @Mock
+  private PageRuleDeleter pageRuleDeleter;
+  @Mock
+  private PageRuleCreator pageRuleCreator;
+  @Mock
+  private PageRuleUpdater pageRuleUpdater;
+  @Mock
+  private PageRuleFinder pageRuleFinder;
+  @Mock
+  private PdfCreator pdfCreator;
+  @Mock
+  private CsvCreator csvCreator;
+
+  @InjectMocks
+  private PageRuleController controller;
+
+
+  @Test
+  void createsPdf() {
+    SearchPageRuleRequest request = new SearchPageRuleRequest("");
+    when(pageRuleFinder.searchPageRule(1l, request, null)).thenReturn(new PageImpl<>(new ArrayList<>()));
+    ResponseEntity<byte[]> response = controller.downloadRulePdf(1l, request, null);
+    assertThat(response).isNotNull();
+
+    assertThat(response.getHeaders().get("Content-Type").get(0)).isEqualTo("application/pdf");
+    assertThat(response.getHeaders().get("Content-Disposition").get(0))
+        .isEqualTo("attachment; filename=\"ManageRulesLibrary.pdf\"");
+  }
+
+  @Test
+  void createsCsv() {
+    SearchPageRuleRequest request = new SearchPageRuleRequest("");
+    when(pageRuleFinder.searchPageRule(1l, request, null)).thenReturn(new PageImpl<>(new ArrayList<>()));
+    ResponseEntity<byte[]> response = controller.downloadRuleCsv(1l, request, null);
+    assertThat(response).isNotNull();
+
+    assertThat(response.getHeaders().get("Content-Type").get(0)).isEqualTo("text/plain");
+    assertThat(response.getHeaders().get("Content-Disposition").get(0))
+        .isEqualTo("attachment; filename=\"ManageRulesLibrary.csv\"");
+  }
+
+}
