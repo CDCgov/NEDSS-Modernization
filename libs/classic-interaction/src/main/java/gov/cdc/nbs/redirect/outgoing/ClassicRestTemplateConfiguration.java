3ad4bec951edec741a840232903e3a63506a9c2b
Michael Peels <michaelpeels@Michael-Peels-Work-6.local>
2024-03-21 16:28:57 -0400

    Remvoe PHIN client

--- libs/classic-interaction/src/main/java/gov/cdc/nbs/redirect/outgoing/ClassicRestTemplateConfiguration.java
+++ libs/classic-interaction/src/main/java/gov/cdc/nbs/redirect/outgoing/ClassicRestTemplateConfiguration.java
@@ -1,27 +1,23 @@
 package gov.cdc.nbs.redirect.outgoing;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import java.net.MalformedURLException;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.MalformedURLException;
-
 @Configuration
 class ClassicRestTemplateConfiguration {
 
-    @Bean
-    @Qualifier("classic")
-    RestTemplate classicRestTemplate(
-        final ClassicPathResolver resolver,
-        final ClassicOutgoingRequestInterceptor interceptor
-    ) throws MalformedURLException {
+  @Bean(name = "classic")
+  RestTemplate classic(
+      final ClassicPathResolver resolver,
+      final ClassicOutgoingRequestInterceptor interceptor) throws MalformedURLException {
 
-        return new RestTemplateBuilder()
-            .rootUri(resolver.base().toURL().toString())
-            .additionalInterceptors(new ClassicOutgoingLoggingHttpRequestInterceptor(), interceptor)
-            .build();
-    }
+    return new RestTemplateBuilder()
+        .rootUri(resolver.base().toURL().toString())
+        .additionalInterceptors(new ClassicOutgoingLoggingHttpRequestInterceptor(), interceptor)
+        .build();
+  }
 
 }
