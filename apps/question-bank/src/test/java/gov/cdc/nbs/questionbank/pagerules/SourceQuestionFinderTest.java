968bac6c58ec056966cdcbde173745bddb6f9fcb
Alaap Bharadwaj <abharadwaj@enquizit.com>
2024-03-22 10:52:49 -0400

    test coverage for source question

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/SourceQuestionFinderTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/SourceQuestionFinderTest.java
@@ -1,6 +1,7 @@
 package gov.cdc.nbs.questionbank.pagerules;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -51,9 +52,171 @@ class SourceQuestionFinderTest {
     assertNotNull(result);
   }
 
+  @Test
+  void testFilterOtherQuestionsWithoutPage() {
+    Long pageId = 1L;
+    when(resolver.resolve(pageId)).thenReturn(Optional.empty());
+
+    SourceQuestionRequest request = new SourceQuestionRequest(Rule.RuleFunction.ENABLE);
+    PagesResponse result = sourceQuestionFinder.filterQuestions(pageId, request);
+    assertNull(result);
+  }
+
+  @Test
+  void testFilterDateQuestionsWithoutPage() {
+    Long pageId = 1L;
+    when(resolver.resolve(pageId)).thenReturn(Optional.empty());
+
+    SourceQuestionRequest request = new SourceQuestionRequest(Rule.RuleFunction.DATE_COMPARE);
+    PagesResponse result = sourceQuestionFinder.filterQuestions(pageId, request);
+    assertNull(result);
+  }
+
+  @Test
+  void testFilterOtherQuestionsWithoutDataType() {
+    Long pageId = 1L;
+    Optional<PagesResponse> page = Optional.of(getPageNoDataType());
+    when(resolver.resolve(pageId)).thenReturn(page);
+
+    SourceQuestionRequest request = new SourceQuestionRequest(Rule.RuleFunction.ENABLE);
+    PagesResponse result = sourceQuestionFinder.filterQuestions(pageId, request);
+    assertNull(result);
+  }
+
+  @Test
+  void testFilterOtherQuestionsWithDatePage() {
+    Long pageId = 1L;
+    Optional<PagesResponse> page = Optional.of(getDatePage());
+    when(resolver.resolve(pageId)).thenReturn(page);
+
+    SourceQuestionRequest request = new SourceQuestionRequest(Rule.RuleFunction.ENABLE);
+    PagesResponse result = sourceQuestionFinder.filterQuestions(pageId, request);
+    assertNull(result);
+  }
+
+  @Test
+  void testFilerOtherQuestionsWithTrueStandardNnd() {
+    Long pageId = 1L;
+    Optional<PagesResponse> page = Optional.of(getPageWithTrueStandardNnd());
+    when(resolver.resolve(pageId)).thenReturn(page);
+
+    SourceQuestionRequest request = new SourceQuestionRequest(Rule.RuleFunction.ENABLE);
+    PagesResponse result = sourceQuestionFinder.filterQuestions(pageId, request);
+    assertNull(result);
+  }
+
+  @Test
+  void testFilterOtherQuestionsWithoutCompBehavior() {
+    Long pageId = 1L;
+    Optional<PagesResponse> page = Optional.of(getPageWithoutCompBehavior());
+    when(resolver.resolve(pageId)).thenReturn(page);
+
+    SourceQuestionRequest request = new SourceQuestionRequest(Rule.RuleFunction.ENABLE);
+    PagesResponse result = sourceQuestionFinder.filterQuestions(pageId, request);
+    assertNull(result);
+  }
+
+  @Test
+  void testFilterOtherQuestionsWithInv169() {
+    Long pageId = 1L;
+    Optional<PagesResponse> page = Optional.of(getPageWithInv169());
+    when(resolver.resolve(pageId)).thenReturn(page);
+
+    SourceQuestionRequest request = new SourceQuestionRequest(Rule.RuleFunction.ENABLE);
+    PagesResponse result = sourceQuestionFinder.filterQuestions(pageId, request);
+    assertNotNull(result);
+  }
+
+
+
   PagesResponse getPage() {
     PagesQuestion question = new PagesQuestion(1156355L, false, true, "SYS", "DEM107", "Suffix", 15, 0, "IPO",
         "The patient's name suffix", false, "CODED", null, false, "The patient's name suffix", true, true, false, null,
+        "P_NM_SFX", 1007, null, null, "D_PATIENT", "PATIENT_NAME_SUFFIX", "Patient Name Suffix", "PATIENT_NAME_SUFFIX",
+        false, null, 0, false, null, 0, "coded_data", "Single-Select (Drop down)", "code_value_general");
+    Collection<PagesQuestion> questions = new ArrayList<>();
+    questions.add(question);
+    PagesSubSection subsection =
+        new PagesSubSection(4L, "test subsection", 3, false, false, false, null, null, questions);
+    Collection<PagesSubSection> subsections = new ArrayList<>();
+    subsections.add(subsection);
+    PagesSection section = new PagesSection(3L, "test", 2, false, subsections);
+    Collection<PagesSection> sections = new ArrayList<>();
+    sections.add(section);
+    PagesTab tab = new PagesTab(2L, "testtab", 1, false, sections);
+    Collection<PagesTab> tabs = new ArrayList<>();
+    tabs.add(tab);
+    PagesResponse page = new PagesResponse(1L, "test", null, null, 0, tabs, null);
+    return page;
+  }
+
+  PagesResponse getPageWithInv169() {
+    PagesQuestion question = new PagesQuestion(1156355L, false, true, "SYS", "INV169", "Suffix", 15, 0, "IPO",
+        "The patient's name suffix", false, "CODED", null, false, "The patient's name suffix", true, true, false, null,
+        "P_NM_SFX", 1007, null, null, "D_PATIENT", "PATIENT_NAME_SUFFIX", "Patient Name Suffix", "PATIENT_NAME_SUFFIX",
+        false, null, 0, false, null, 0, "coded_data", "Single-Select (Drop down)", "something else");
+    Collection<PagesQuestion> questions = new ArrayList<>();
+    questions.add(question);
+    PagesSubSection subsection =
+        new PagesSubSection(4L, "test subsection", 3, false, false, false, null, null, questions);
+    Collection<PagesSubSection> subsections = new ArrayList<>();
+    subsections.add(subsection);
+    PagesSection section = new PagesSection(3L, "test", 2, false, subsections);
+    Collection<PagesSection> sections = new ArrayList<>();
+    sections.add(section);
+    PagesTab tab = new PagesTab(2L, "testtab", 1, false, sections);
+    Collection<PagesTab> tabs = new ArrayList<>();
+    tabs.add(tab);
+    PagesResponse page = new PagesResponse(1L, "test", null, null, 0, tabs, null);
+    return page;
+  }
+
+
+  PagesResponse getPageWithoutCompBehavior() {
+    PagesQuestion question = new PagesQuestion(1156355L, false, true, "SYS", "DEM107", "Suffix", 15, 0, "IPO",
+        "The patient's name suffix", false, "CODED", null, false, "The patient's name suffix", true, true, false, null,
+        "P_NM_SFX", 1007, null, null, "D_PATIENT", "PATIENT_NAME_SUFFIX", "Patient Name Suffix", "PATIENT_NAME_SUFFIX",
+        false, null, 0, false, null, 0, "something else", "Single-Select (Drop down)", "code_value_general");
+    Collection<PagesQuestion> questions = new ArrayList<>();
+    questions.add(question);
+    PagesSubSection subsection =
+        new PagesSubSection(4L, "test subsection", 3, false, false, false, null, null, questions);
+    Collection<PagesSubSection> subsections = new ArrayList<>();
+    subsections.add(subsection);
+    PagesSection section = new PagesSection(3L, "test", 2, false, subsections);
+    Collection<PagesSection> sections = new ArrayList<>();
+    sections.add(section);
+    PagesTab tab = new PagesTab(2L, "testtab", 1, false, sections);
+    Collection<PagesTab> tabs = new ArrayList<>();
+    tabs.add(tab);
+    PagesResponse page = new PagesResponse(1L, "test", null, null, 0, tabs, null);
+    return page;
+  }
+
+  PagesResponse getPageWithTrueStandardNnd() {
+    PagesQuestion question = new PagesQuestion(1156355L, true, true, "SYS", "DEM107", "Suffix", 15, 0, "IPO",
+        "The patient's name suffix", false, "CODED", null, false, "The patient's name suffix", true, true, false, null,
+        "P_NM_SFX", 1007, null, null, "D_PATIENT", "PATIENT_NAME_SUFFIX", "Patient Name Suffix", "PATIENT_NAME_SUFFIX",
+        false, null, 0, false, null, 0, "coded_data", "Single-Select (Drop down)", "code_value_general");
+    Collection<PagesQuestion> questions = new ArrayList<>();
+    questions.add(question);
+    PagesSubSection subsection =
+        new PagesSubSection(4L, "test subsection", 3, false, false, false, null, null, questions);
+    Collection<PagesSubSection> subsections = new ArrayList<>();
+    subsections.add(subsection);
+    PagesSection section = new PagesSection(3L, "test", 2, false, subsections);
+    Collection<PagesSection> sections = new ArrayList<>();
+    sections.add(section);
+    PagesTab tab = new PagesTab(2L, "testtab", 1, false, sections);
+    Collection<PagesTab> tabs = new ArrayList<>();
+    tabs.add(tab);
+    PagesResponse page = new PagesResponse(1L, "test", null, null, 0, tabs, null);
+    return page;
+  }
+
+  PagesResponse getPageNoDataType() {
+    PagesQuestion question = new PagesQuestion(1156355L, false, true, "SYS", "DEM107", "Suffix", 15, 0, "IPO",
+        "The patient's name suffix", false, null, null, false, "The patient's name suffix", true, true, false, null,
         "P_NM_SFX", 1007, null, null, "D_PATIENT", "PATIENT_NAME_SUFFIX", "Patient Name Suffix", "PATIENT_NAME_SUFFIX",
         false, null, 0, false, null, 0, "coded_data", "Single-Select (Drop down)", "code_value_general");
     Collection<PagesQuestion> questions = new ArrayList<>();
