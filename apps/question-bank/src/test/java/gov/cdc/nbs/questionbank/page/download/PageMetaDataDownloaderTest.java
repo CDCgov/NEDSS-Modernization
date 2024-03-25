9df1385082365e7f691f3e04c3fca563ae51efa0
Sheham Izat <sizat@enquizit.com>
2024-03-25 18:13:31 -0400

    Adding Missed Columns for Page Metadata.

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/page/download/PageMetaDataDownloaderTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/page/download/PageMetaDataDownloaderTest.java
@@ -19,10 +19,7 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
