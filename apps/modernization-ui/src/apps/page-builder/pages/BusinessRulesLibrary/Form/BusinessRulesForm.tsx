e03af87eddc28f87830e99c852e6b1ada6aeae2a
Richard Krussel <139483220+rkrusselenq@users.noreply.github.com>
2024-03-26 08:45:43 -0500

    View business rule page bugs (#1230)
    * init
    
    * fix tests

--- apps/modernization-ui/src/apps/page-builder/pages/BusinessRulesLibrary/Form/BusinessRulesForm.tsx
+++ apps/modernization-ui/src/apps/page-builder/pages/BusinessRulesLibrary/Form/BusinessRulesForm.tsx
@@ -15,6 +15,7 @@ import SubSectionsDropdown from '../SubSectionDropdown';
 import { useParams } from 'react-router-dom';
 import { mapLogicForDateCompare } from '../helpers/mapLogicForDateCompare';
 import './ModalWidth.scss';
+import { checkForSemicolon, removeNumericAndSymbols } from '../helpers/errorMessageUtils';
 
 type Props = {
     isEdit: boolean;
@@ -104,26 +105,6 @@ export const BusinessRulesForm = ({
             targetValueText: [],
             description: ''
         });
-    };
-
-    const removeNumericAndSymbols = (text: string | undefined) => {
-        const firstChar = text?.charAt(0);
-        if (firstChar && firstChar <= '9' && firstChar >= '0') {
-            return text?.replace(/\d+/, '').replace('. ', '');
-        }
-        return text;
-    };
-
-    const checkForSemicolon = (text: string | undefined) => {
-        const labelLength = text?.length;
-        if (labelLength) {
-            const lastChar = text.charAt(labelLength - 1);
-            if (lastChar === ':') {
-                return text.replace(':', '');
-            }
-            return text;
-        }
-        return text;
     };
 
     const handleOpenSourceQuestion = () => {
--- apps/modernization-ui/src/apps/page-builder/pages/BusinessRulesLibrary/ViewBusinessRule/ViewBusinessRule.spec.tsx
+++ apps/modernization-ui/src/apps/page-builder/pages/BusinessRulesLibrary/ViewBusinessRule/ViewBusinessRule.spec.tsx
@@ -4,40 +4,42 @@ import { BrowserRouter } from 'react-router-dom';
 import { Rule } from 'apps/page-builder/generated';
 import { PageProvider } from 'page';
 
-    describe('when ViewBusinessRule rendered', () => {
-        it('should display heading', () => {
-            const { container } = render(
-                <BrowserRouter>
-                    <PageProvider>
-                        <ViewBusinessRule />
-                    </PageProvider>
-                </BrowserRouter>
-            );
-            const heading = container.querySelector('h2');
-            expect(heading).toHaveTextContent('View business rule');
-        });
-        it('should display table headers', async () => {
-            const { findAllByRole } = render(
-                <BrowserRouter>
-                    <PageProvider>
-                        <ViewBusinessRule />
-                    </PageProvider>
-                </BrowserRouter>
-            );
-
-            const tableHeads = await findAllByRole('columnheader');
-            expect(tableHeads).toHaveLength(2);
-        });
-        it('should rule fields', async () => {
-            const { findAllByRole } = render(
-                <BrowserRouter>
-                    <PageProvider>
-                        <ViewBusinessRule />
-                    </PageProvider>
-                </BrowserRouter>
-            );
-
-            const tableData = await findAllByRole('cell');
-            expect(tableData).toHaveLength(16);
-        });
+describe('when ViewBusinessRule rendered', () => {
+    it('should display heading', () => {
+        const { container } = render(
+            <BrowserRouter>
+                <PageProvider>
+                    <ViewBusinessRule />
+                </PageProvider>
+            </BrowserRouter>
+        );
+        const heading = container.querySelector('h2');
+        expect(heading).toHaveTextContent('View business rule');
     });
+
+    it('should display table headers', async () => {
+        const { findAllByRole } = render(
+            <BrowserRouter>
+                <PageProvider>
+                    <ViewBusinessRule />
+                </PageProvider>
+            </BrowserRouter>
+        );
+
+        const tableHeads = await findAllByRole('columnheader');
+        expect(tableHeads).toHaveLength(2);
+    });
+
+    it('should rule fields', async () => {
+        const { findAllByRole } = render(
+            <BrowserRouter>
+                <PageProvider>
+                    <ViewBusinessRule />
+                </PageProvider>
+            </BrowserRouter>
+        );
+
+        const tableData = await findAllByRole('cell');
+        expect(tableData).toHaveLength(14);
+    });
+});
