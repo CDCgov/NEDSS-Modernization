1b50c1086da177dd70de8ea9d9347597b4f958e4
Adam Loup <aloup@enquizit.com>
2024-03-27 10:12:52 -0500

    smells

--- libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/Observation.java
+++ libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/Observation.java
@@ -1,9 +1,5 @@
 package gov.cdc.nbs.entity.odse;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,10 +8,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.Instant;
-import java.util.List;
 
 @NoArgsConstructor
 @Getter
