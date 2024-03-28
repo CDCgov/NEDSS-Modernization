a166b4471f37c7d234f10fa255429a4b3d8b74cd
Michael Davis <michaeldavis@michaels-mbp.mynetworksettings.com>
2024-03-28 14:21:55 -0400

    Screenshot on Failure for Cypress component specs

--- apps/modernization-ui/cypress.config.ts
+++ apps/modernization-ui/cypress.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+    screenshotOnRunFailure: true,
     component: {
         devServer: {
             framework: 'create-react-app',
             bundler: 'webpack'
-        }
+        },
+        experimentalInteractiveRunEvents: true
     }
 });
--- apps/modernization-ui/src/components/FormInputs/Input.cy.tsx
+++ apps/modernization-ui/src/components/FormInputs/Input.cy.tsx
@@ -2,8 +2,58 @@ import React from 'react'
 import { Input } from './Input'
 
 describe('<Input />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Input defaultValue={"test1234"} />)
+  it('renders input with correct value', () => {
+    const onChange = () => {};
+    cy.mount(
+      <Input
+          id="test-input-id"
+          name="test-input-name"
+          label="Test Input Label"
+          className="test-input-class-name"
+          htmlFor="test-input-id"
+          type="text"
+          onChange={onChange}
+          defaultValue="test-input-defaultValue"
+          error="invalid input"
+      />
+    )
+    cy.get('input').should('have.value', 'test-input-defaultValue')        
   })
+
+  it('renders input with correct name', () => {
+    const onChange = () => {};
+    cy.mount(
+      <Input
+          id="test-input-id"
+          name="test-input-name"
+          label="Test Input Label"
+          className="test-input-class-name"
+          htmlFor="test-input-id"
+          type="text"
+          onChange={onChange}
+          defaultValue="test-input-defaultValue"
+          error="invalid input"
+      />
+    )
+    cy.get('[name=test-input-name]').should('have.value', 'test-input-defaultValue')
+  })  
+
+  it('renders input that is visible', () => {
+    const onChange = () => {};
+    cy.mount(
+      <Input
+          id="test-input-id"
+          name="test-input-name"
+          label="Test Input Label"
+          className="test-input-class-name"
+          htmlFor="test-input-id"
+          type="text"
+          onChange={onChange}
+          defaultValue="test-input-defaultValue"
+          error="invalid input"
+      />
+    )    
+
+        cy.contains('input').should('be.visible')
+  })  
 })
