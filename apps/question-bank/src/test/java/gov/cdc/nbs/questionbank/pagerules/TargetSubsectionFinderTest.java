8507487abea3d549e5ae60255fb81f0905004a59
Alaap Bharadwaj <abharadwaj@enquizit.com>
2024-04-02 16:03:50 -0400

    check for test coverage

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/TargetSubsectionFinderTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/TargetSubsectionFinderTest.java
@@ -1,10 +1,15 @@
 package gov.cdc.nbs.questionbank.pagerules;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import org.apache.poi.hpsf.Array;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -15,9 +20,10 @@ import gov.cdc.nbs.questionbank.page.detail.PagesResponse.PagesQuestion;
 import gov.cdc.nbs.questionbank.page.detail.PagesResponse.PagesSection;
 import gov.cdc.nbs.questionbank.page.detail.PagesResponse.PagesSubSection;
 import gov.cdc.nbs.questionbank.page.detail.PagesResponse.PagesTab;
+import gov.cdc.nbs.questionbank.pagerules.Rule.Target;
 import gov.cdc.nbs.questionbank.pagerules.request.TargetSubsectionRequest;
 
-public class TargetSubsectionFinderTest {
+class TargetSubsectionFinderTest {
   @Mock
   PagesResolver resolver;
 
@@ -40,8 +46,90 @@ public class TargetSubsectionFinderTest {
 
     TargetSubsectionRequest request = new TargetSubsectionRequest(1, null);
     Collection<PagesSubSection> response = targetSubsectionFinder.filterSubsections(pageId, request);
-    assertNotNull(response);
+    assertEquals(1, response.size());
   }
+
+  @Test
+  void testFilterSubsectionsGreaterOrderNumber() {
+    Long pageId = 1L;
+    Optional<PagesResponse> page = Optional.of(getPage());
+
+    when(resolver.resolve(pageId)).thenReturn(page);
+
+    TargetSubsectionRequest request = new TargetSubsectionRequest(22, null);
+    Collection<PagesSubSection> response = targetSubsectionFinder.filterSubsections(pageId, request);
+    assertEquals(0, response.size());
+  }
+
+  @Test
+  void testFilterSubsectionsEmptySubsections() {
+    Long pageId = 1L;
+    Optional<PagesResponse> page = Optional.of(getPageWithoutQuestions());
+
+    when(resolver.resolve(pageId)).thenReturn(page);
+
+    TargetSubsectionRequest request = new TargetSubsectionRequest(1, null);
+    Collection<PagesSubSection> response = targetSubsectionFinder.filterSubsections(pageId, request);
+    assertEquals(0, response.size());
+  }
+
+  @Test
+  void testFilterSubsectionsWithTargetSubsections() {
+    Long pageId = 1L;
+    Optional<PagesResponse> page = Optional.of(getPage());
+
+    when(resolver.resolve(pageId)).thenReturn(page);
+
+    when(ruleFinder.getAllRules(pageId)).thenReturn(getRules());
+
+    Collection<String> targets = new ArrayList<>();
+    targets.add("questionIdent");
+
+    TargetSubsectionRequest request = new TargetSubsectionRequest(0, targets);
+    Collection<PagesSubSection> response = targetSubsectionFinder.filterSubsections(pageId, request);
+    assertEquals(1, response.size());
+  }
+
+  List<Rule> getRules() {
+    List<String> sourceValues = new ArrayList<>();
+    sourceValues.add("Yes");
+
+    List<Target> targets = new ArrayList<>();
+    targets.add(new Target("questionIdent", "test label"));
+
+    List<Rule> rules = new ArrayList<>();
+
+    Rule rule = new Rule(100,
+        200l,
+        Rule.RuleFunction.ENABLE,
+        "testDescription",
+        null,
+        true,
+        sourceValues,
+        Rule.Comparator.EQUAL_TO,
+        Rule.TargetType.QUESTION,
+        targets);
+    rules.add(rule);
+    return rules;
+  }
+
+
+  PagesResponse getPageWithoutQuestions() {
+
+    PagesSubSection subsection =
+        new PagesSubSection(4L, "test subsection", 3, false, false, false, null, null, new ArrayList<PagesQuestion>());
+    Collection<PagesSubSection> subsections = new ArrayList<>();
+    subsections.add(subsection);
+    PagesSection section = new PagesSection(3L, "test", 2, false, subsections);
+    Collection<PagesSection> sections = new ArrayList<>();
+    sections.add(section);
+    PagesTab tab = new PagesTab(2L, "testtab", 1, false, sections);
+    Collection<PagesTab> tabs = new ArrayList<>();
+    tabs.add(tab);
+    PagesResponse page = new PagesResponse(1L, "test", null, null, 0, tabs, null);
+    return page;
+  }
+
 
   PagesResponse getPage() {
     PagesQuestion question = new PagesQuestion(1156355L, false, true, "SYS", "DEM107", "Suffix", 15, 0, "IPO",
@@ -51,7 +139,7 @@ public class TargetSubsectionFinderTest {
     Collection<PagesQuestion> questions = new ArrayList<>();
     questions.add(question);
     PagesSubSection subsection =
-        new PagesSubSection(4L, "test subsection", 3, false, false, false, null, null, questions);
+        new PagesSubSection(4L, "test subsection", 3, false, false, false, "questionIdent", null, questions);
     Collection<PagesSubSection> subsections = new ArrayList<>();
     subsections.add(subsection);
     PagesSection section = new PagesSection(3L, "test", 2, false, subsections);
