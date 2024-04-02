01cacc44b88bd6da8502fd7ee7fe0a7564f75487
Sheham Izat <sizat@enquizit.com>
2024-04-02 12:17:44 -0400

    Target Question has to have Static elements also included(fix)

--- apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/PageRuleMapperTest.java
+++ apps/question-bank/src/test/java/gov/cdc/nbs/questionbank/pagerules/PageRuleMapperTest.java
@@ -87,4 +87,37 @@ class PageRuleMapperTest {
     assertNull(actualResponse.targetType());
   }
 
+  @Test
+  void testGetComponentType() throws SQLException {
+    String targetIdentifiers = "test1,test1,test3,test4,test5,test6,test7,test8,test9,test10,test11,test12," +
+        "test13,test14,test15,test16,test17,test18,test19,test20,test21,test22,test23,test24,test25,test26,test27";
+    String targetTypes =
+        "1003,1011,1012,1022,1023,1007,1013,1001,1006,1024,1025,1027,1028,1008,1026,1029,1014,1000,1009" +
+            ",1017,1019,1030,1032,1033,1034,1035,1036";
+
+    ResultSet resultSet = mock(ResultSet.class);
+    when(resultSet.getLong(1)).thenReturn(100l);
+    when(resultSet.getLong(2)).thenReturn(123l);
+    when(resultSet.getString(3)).thenReturn("Enable");
+    when(resultSet.getString(4)).thenReturn("description");
+    when(resultSet.getString(5)).thenReturn("test456");
+    when(resultSet.getString(6)).thenReturn("test(  )test");
+    when(resultSet.getString(7)).thenReturn("test1,test2,test3");
+    when(resultSet.getString(8)).thenReturn("=");
+    when(resultSet.getString(9)).thenReturn("QUESTION");
+    when(resultSet.getString(11)).thenReturn("label123");
+    when(resultSet.getString(12)).thenReturn("codeSetName");
+    when(resultSet.getString(13)).thenReturn("");
+    when(resultSet.getLong(15)).thenReturn(10l);
+    when(resultSet.getString(10)).thenReturn(targetIdentifiers);
+    when(resultSet.getString(14)).thenReturn(targetTypes);
+    when(resultSet.getLong(15)).thenReturn(10l);
+    Rule actualResponse = pageRuleMapper.mapRow(resultSet, 1);
+    for (Rule.Target target : actualResponse.targets()) {
+      assertNotNull(target.label());
+    }
+  }
+
+
+
 }
