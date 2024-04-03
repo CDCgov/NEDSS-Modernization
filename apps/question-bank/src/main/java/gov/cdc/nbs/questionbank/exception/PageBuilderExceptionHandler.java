45b0975be6adfc70dc99fcb3004a7ac12339a723
Michael Peels <michaelpeels@Michael-Peels-Work-6.local>
2024-04-03 17:10:39 -0400

    Set status to 403 for AccessDeniedException

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/exception/PageBuilderExceptionHandler.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/exception/PageBuilderExceptionHandler.java
@@ -2,6 +2,7 @@ package gov.cdc.nbs.questionbank.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,21 +14,27 @@ public class PageBuilderExceptionHandler {
     return new ResponseEntity<>(new ExceptionMessage(e.getMessage()), HttpStatus.BAD_REQUEST);
   }
 
-  @ExceptionHandler({
-      NotFoundException.class})
+  @ExceptionHandler({NotFoundException.class})
   public ResponseEntity<ExceptionMessage> handleNotFound(Exception e) {
     return new ResponseEntity<>(new ExceptionMessage(e.getMessage()), HttpStatus.NOT_FOUND);
   }
 
-  @ExceptionHandler({
-      InternalServerException.class})
+  @ExceptionHandler({InternalServerException.class})
   public ResponseEntity<ExceptionMessage> handleInternalException(Exception e) {
     return new ResponseEntity<>(new ExceptionMessage(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
+  @ExceptionHandler({AccessDeniedException.class})
+  public ResponseEntity<ExceptionMessage> handleAccessDenied(Exception e) {
+    return new ResponseEntity<>(
+        new ExceptionMessage("Access denied"),
+        HttpStatus.FORBIDDEN);
+  }
+
   @ExceptionHandler({RuntimeException.class})
   public ResponseEntity<ExceptionMessage> handleRuntimeException(Exception e) {
-    return new ResponseEntity<>(new ExceptionMessage("An unexpected error has occurred"),
+    return new ResponseEntity<>(
+        new ExceptionMessage("An unexpected error has occurred"),
         HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
