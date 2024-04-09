2ce782228af987870f8dd32bce8cca15f47cb319
Adam Loup <aloup@enquizit.com>
2024-04-09 15:23:56 -0500

    smells

--- apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/GatewayApplication.java
+++ apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/GatewayApplication.java
@@ -2,7 +2,6 @@ package gov.cdc.nbs.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 public class GatewayApplication {
