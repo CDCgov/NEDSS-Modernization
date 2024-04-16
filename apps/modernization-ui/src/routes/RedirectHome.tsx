ae2673b822e2b7acb19bca8f68e12bcf1cccf75b
Henry Clark <hclark@enquizit.com>
2024-04-16 11:38:16 -0700

    replacing patient with person on redirection for now

--- apps/modernization-ui/src/routes/RedirectHome.tsx
+++ apps/modernization-ui/src/routes/RedirectHome.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom';
 
-const RedirectHome = () => <Navigate to="/advanced-search/patient" />;
+const RedirectHome = () => <Navigate to="/advanced-search/person" />;
 
 export { RedirectHome };
