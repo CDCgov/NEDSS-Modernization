f73e210055e829581cd93cc874f1be1529cae259
Michael Peels <michaelpeels@Michael-Peels.local>
2024-04-18 10:57:28 -0400

    initial commit

---
+++ testing/deduplication/src/main/java/gov/cdc/nbs/deduplication/DeduplicationApplication.java
@@ -0,0 +1,13 @@
+package gov.cdc.nbs.deduplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeduplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeduplicationApplication.class, args);
+	}
+
+}
---
+++ testing/deduplication/src/test/java/gov/cdc/nbs/deduplication/DeduplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.cdc.nbs.deduplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeduplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
