05df6f99a5c67adb4c70f36eea2e439d652a8f1f
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-04-19 16:04:24 -0500

    CNFT1-2244 Login page: About (#1296)

---
+++ apps/modernization-ui/src/apps/landing/About/About.tsx
@@ -0,0 +1,55 @@
+export const About = () => {
+    return (
+        <>
+            <p>
+                We aim to keep the demo site up to date with the latest NBS 6.x.x and NBS 7.x.x versions. The current
+                demo site is updated with NBS 7.1.0 (which is integrated with NBS 6.0.15).
+            </p>
+            <h2>Documentation</h2>
+            <p>
+                Please see documentation associated with releases to see the updated features and content. You can find
+                the documentation (Release Notes, User Guide, System Admin Guide, etc) on Github and NBS Central:
+            </p>
+            <h3>GitHub</h3>
+            <p>
+                <a href="https://github.com/CDCgov/NEDSS-Modernization/releases">
+                    https://github.com/CDCgov/NEDSS-Modernization/releases
+                </a>
+            </p>
+            <h3>NBS Central</h3>
+            <p>
+                <a href="https://cdcnbscentral.com/projects/71release-materials/documents">
+                    https://cdcnbscentral.com/projects/71release-materials/documents
+                </a>
+            </p>
+            <p>
+                All the data that currently exists is synthetic data. We are working on improving our synthetic database
+                to be more representative of a production environment.
+            </p>
+            <h2>Feedback</h2>
+            <p>
+                Please provide feedback here:
+                <br />
+                <a href="https://cdcprime.gov1.qualtrics.com/jfe/form/SV_3NNZwwdTOlnIIxU">
+                    https://cdcprime.gov1.qualtrics.com/jfe/form/SV_3NNZwwdTOlnIIxU
+                </a>
+            </p>
+            <p>
+                If you cannot access the Qualtrics link, please reach out to us at nbs@cdc.gov, and we will work with
+                you on an alternate method for feedback.
+            </p>
+            <h2>Technical Support</h2>
+            <p>
+                Email
+                <br />
+                <a href="mailto:nbs@cdc.gov">nbs@cdc.gov</a>
+                <br />
+                Submit a ticket on NBS Central
+                <br />
+                <a href="https://cdcnbscentral.com/projects/70collaboration/issues">
+                    https://cdcnbscentral.com/projects/70collaboration/issues
+                </a>
+            </p>
+        </>
+    );
+};
