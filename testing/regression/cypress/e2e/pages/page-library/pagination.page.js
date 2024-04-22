6eb4c7223f96e8802f88341fe7e072026a36985b
Lakshmi Narayanan Ramakrishnan <132704359+lramakr@users.noreply.github.com>
2024-04-22 13:11:41 -0700

    CNFT2-976 E2E page library pagination 20 row (#1313)

--- testing/regression/cypress/e2e/pages/page-library/pagination.page.js
+++ testing/regression/cypress/e2e/pages/page-library/pagination.page.js
@@ -6,6 +6,45 @@ class PaginationPage {
     get table() {
         return "table[data-testid=table]";
     }
+
+    selectNumberOfRows (numberOfRows) {
+        cy.get('#range-toggle').select(numberOfRows).should('have.value', numberOfRows);
+    }
+
+    checkDisplayingNumberOfRowsSubsequently(rowsPerPage, onlyOnNext, pageNumber) {
+        this.totalRowsCountFromDOM().then((totalRowsCount) => {
+            const totalPages = Math.ceil(totalRowsCount / rowsPerPage);
+            const lastPageRowsCount = totalRowsCount % rowsPerPage || rowsPerPage;
+            const arrayMapFun = (_, i) => i === totalPages - 1 ? lastPageRowsCount : rowsPerPage;
+            const rowsCountPerPage = Array.from({length: totalPages}, arrayMapFun);
+
+            if (onlyOnNext) {
+                this.openInvestigationTable.find("tbody tr").should('have.length', rowsCountPerPage[pageNumber-1]);
+            } else {
+                rowsCountPerPage.forEach((countPerPage, i) => {
+                    if (i !== 0) {
+                        this.clickNextPage();
+                    }
+                    this.openInvestigationTable.find("tbody tr").should('have.length', countPerPage);
+                });
+            }
+        })
+    }
+
+    clickNextPage() {
+        cy.get('.usa-pagination__next-page').click();
+    }
+
+    get openInvestigationTable() {
+        return cy.get(this.table).eq(0);
+    }
+
+    totalRowsCountFromDOM(){
+        return cy.get('#totalRowCount').invoke('text')
+            .then(text => {
+                return parseInt(text, 10)
+            });
+    }
 }
 
 export const pageLibraryPaginationPage = new PaginationPage()
--- testing/regression/cypress/step_definitions/page-library/pagination.steps.js
+++ testing/regression/cypress/step_definitions/page-library/pagination.steps.js
@@ -4,3 +4,11 @@ import {pageLibraryPaginationPage} from "@pages/page-library/pagination.page";
 Then("User should see by default 10 rows of pages listed in the library", () => {
     pageLibraryPaginationPage.checkForDefaultRows();
 });
+
+Then("User select 20 left footer of the page to show the list of pages", () => {
+    pageLibraryPaginationPage.selectNumberOfRows('20');
+});
+
+Then("User should see only 20 rows in the library and for each subsequent list where applicable", () => {
+    pageLibraryPaginationPage.checkDisplayingNumberOfRowsSubsequently(20);
+});
