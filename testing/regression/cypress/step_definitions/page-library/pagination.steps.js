002c952167b162588b36336d3d8636b2a47067eb
Lakshmi Narayanan Ramakrishnan <lramakrishnan@enquizit.com>
2024-04-24 10:47:40 -0700

    CNFT2-1006 E2E page library pagination 50 row defaulted after redirects

--- testing/regression/cypress/step_definitions/page-library/pagination.steps.js
+++ testing/regression/cypress/step_definitions/page-library/pagination.steps.js
@@ -5,28 +5,14 @@ Then("User should see by default 10 rows of pages listed in the library", () => 
     pageLibraryPaginationPage.checkForDefaultRows();
 });
 
-Then("User select 20 left footer of the page to show the list of pages", () => {
-    pageLibraryPaginationPage.selectNumberOfRows('20');
+Then("User select {string} left footer of the page to show the list of pages", (string) => {
+    pageLibraryPaginationPage.selectNumberOfRows(string);
 });
 
-Then("User should see only 20 rows in the library and for each subsequent list where applicable", () => {
-    pageLibraryPaginationPage.checkDisplayingNumberOfRowsSubsequently(20);
+Then("User should see only {string} rows in the library and for each subsequent list where applicable", (string) => {
+    pageLibraryPaginationPage.checkDisplayingNumberOfRowsSubsequently(string);
 });
+
 Then("User navigates to the Create page", () => {
     pageLibraryPaginationPage.navigateToCreatePage();
-});
-Then("User should only see 10 rows in the library and for each subsequent list where applicable", () => {
-    pageLibraryPaginationPage.checkForDefaultRows();
-});
-Then("User select 30 left footer of the page to show the list of pages", () => {
-    pageLibraryPaginationPage.selectNumberOfRows('30');
-});
-Then("User should see only 30 rows in the library and for each subsequent list where applicable", () => {
-    pageLibraryPaginationPage.checkDisplayingNumberOfRowsSubsequently(30);
-});
-Then("User select 50 left footer of the page to show the list of pages", () => {
-    pageLibraryPaginationPage.selectNumberOfRows('50');
-});
-Then("User should see only 50 rows in the library and for each subsequent list where applicable", () => {
-    pageLibraryPaginationPage.checkDisplayingNumberOfRowsSubsequently(50);
 });
