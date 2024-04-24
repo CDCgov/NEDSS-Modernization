e283f4a94087dbfbacc3cddc4c79052ad68ea697
Henry Clark <hclark@enquizit.com>
2024-04-24 12:02:35 -0700

    updating welcome title and fixing birth order input field

--- apps/modernization-ui/src/welcomeLayout/WelcomeHeader/WelcomeHeader.tsx
+++ apps/modernization-ui/src/welcomeLayout/WelcomeHeader/WelcomeHeader.tsx
@@ -3,8 +3,8 @@ import style from './welcomeHeader.module.scss';
 export const WelcomeHeader = () => {
     return (
         <div className={style.header}>
-            <img src="/nedssLogo.jpeg" height={40} alt="" />
-            Welcome to the NBS demo site (Version 7.x)
+            <img src="/nbs-logo.png" height={40} alt="" />
+            Welcome to the NBS7 demo site
         </div>
     );
 };
