a3b603d46be9fe69197128e4888ae6220416fa37
Lakshmi Narayanan Ramakrishnan <lramakrishnan@enquizit.com>
2024-04-25 07:47:12 -0700

    CNFT2-1009 E2E page library pagination next page button click

--- testing/regression/cypress/step_definitions/page-library/pagination.steps.js
+++ testing/regression/cypress/step_definitions/page-library/pagination.steps.js
@@ -16,3 +16,11 @@ Then("User should see only {string} rows in the library and for each subsequent 
 Then("User navigates to the Create page", () => {
     pageLibraryPaginationPage.navigateToCreatePage();
 });
+
+Then("User click 2 in the pagination section bottom right", () => {
+    pageLibraryPaginationPage.clickByPageNumber(2);
+});
+
+Then("User should see the subsequent rows listed in the library for the number of rows selected and Same results when paginating pages 3, 4, and 5", () => {
+    pageLibraryPaginationPage.clickByPageNumber();
+});
