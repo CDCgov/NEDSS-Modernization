82603c0b3818549b49e481c41758b5fc5d4a0215
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-05-07 13:37:27 -0500

    CNFT1-2198 Segmented buttons part 2 (#1348)

--- apps/modernization-ui/src/components/SegmentedButtons/SegmentedButtons.tsx
+++ apps/modernization-ui/src/components/SegmentedButtons/SegmentedButtons.tsx
@@ -1,30 +1,40 @@
-import { Button, Icon } from '@trussworks/react-uswds';
+import { Button, Icon, Label } from '@trussworks/react-uswds';
 import styles from './segmentedButtons.module.scss';
 import { Option } from 'generated';
 type Props = {
+    title?: string;
     buttons: Option[];
     onBlur?: () => void;
     onChange?: () => void;
     onClick?: (button: any) => void;
     value: string | number;
+    required?: boolean;
 };
 
-export const SegmentedButtons = ({ buttons, value, onBlur, onChange, onClick }: Props) => {
+export const SegmentedButtons = ({ title, buttons, value, onBlur, onChange, onClick, required }: Props) => {
     return (
-        <div className={styles.segmentedButtons}>
-            {buttons.map((button: Option, key: number) => (
-                <Button
-                    type="button"
-                    key={key}
-                    onBlur={onBlur}
-                    onClick={onClick ? () => onClick(button) : undefined}
-                    onChange={onChange}
-                    outline={button.value !== value}
-                    className={button.value === value ? styles.active : ''}>
-                    {value === button.value && <Icon.Check />}
-                    {button.name}
-                </Button>
-            ))}
+        <div className={`${styles.segmentedButtons} ${required && styles.hidden}`}>
+            {title && (
+                <Label htmlFor="title">
+                    {title}
+                    {required && <span> *</span>}
+                </Label>
+            )}
+            <div className={styles.buttons}>
+                {buttons.map((button: Option, key: number) => (
+                    <Button
+                        type="button"
+                        key={key}
+                        onBlur={onBlur}
+                        onClick={onClick ? () => onClick(button) : undefined}
+                        onChange={onChange}
+                        outline={button.value !== value}
+                        className={button.value === value ? styles.active : ''}>
+                        {value === button.value && <Icon.Check />}
+                        {button.name}
+                    </Button>
+                ))}
+            </div>
         </div>
     );
 };
