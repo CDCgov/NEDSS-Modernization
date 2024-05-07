b9633a20affa15aa3549d5de878e8a3e5c122a62
Richard <rkrussel@enquizit.com>
2024-05-07 13:43:31 -0500

    init

---
+++ apps/modernization-ui/src/authorization/IdleTimer.tsx
@@ -0,0 +1,84 @@
+import { Button } from 'components/button';
+import React, { useState, useEffect } from 'react';
+
+interface IdleTimerProps {
+    timeout: number; // Timeout in milliseconds
+    onIdle: () => void; // Callback function to execute when idle
+}
+
+const IdleTimer: React.FC<IdleTimerProps> = ({ timeout, onIdle }) => {
+    const [idle, setIdle] = useState(false);
+    const [showWarningModal, setShowWarningModal] = useState(false);
+
+    useEffect(() => {
+        let idleTimer: number;
+        let warningTimer: number;
+
+        const resetIdleTimer = () => {
+            console.log('reset');
+            clearTimeout(idleTimer);
+            idleTimer = window.setTimeout(() => {
+                setIdle(true);
+                removeEventListeners();
+                startWarningTimer();
+            }, timeout);
+        };
+
+        const startWarningTimer = () => {
+            setShowWarningModal(true);
+            clearTimeout(warningTimer);
+            warningTimer = window.setTimeout(() => {
+                onIdle();
+                setShowWarningModal(false);
+            }, 120000); // 2 minutes?
+        };
+
+        const removeEventListeners = () => {
+            const events = ['mousemove', 'keydown', 'mousedown', 'touchstart'];
+            events.forEach((event) => {
+                document.removeEventListener(event, handleActivity);
+            });
+        };
+
+        const addEventListeners = () => {
+            const events = ['mousemove', 'keydown', 'mousedown', 'touchstart'];
+            events.forEach((event) => {
+                document.addEventListener(event, handleActivity);
+            });
+        };
+
+        const handleActivity = () => {
+            resetIdleTimer();
+        };
+
+        if (!idle) {
+            addEventListeners();
+            resetIdleTimer();
+        }
+
+        return () => {
+            clearTimeout(idleTimer);
+            clearTimeout(warningTimer);
+            removeEventListeners();
+        };
+    }, [timeout, onIdle, idle]);
+
+    return (
+        <>
+            {showWarningModal && (
+                <div className="warning-modal">
+                    <p>Warning: Only 2 minutes remaining of idle time!</p>
+                    <Button
+                        onClick={() => {
+                            setShowWarningModal(false);
+                            setIdle(false);
+                        }}>
+                        I'm still here
+                    </Button>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default IdleTimer;
--- apps/modernization-ui/src/authorization/ProtectedLayout.tsx
+++ apps/modernization-ui/src/authorization/ProtectedLayout.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { Await, Navigate, useLoaderData } from 'react-router-dom';
+import { Await, Navigate, redirect, useLoaderData } from 'react-router-dom';
 import { User, UserContextProvider } from 'providers/UserContext';
 
 import { Configuration, ConfigurationProvider } from 'configuration';
@@ -7,12 +7,40 @@ import { AnalyticsProvider } from 'analytics';
 import { Spinner } from 'components/Spinner';
 import { Layout } from '../layout/Layout';
 import { InitializationLoaderResult } from './initializationLoader';
+import IdleTimer from './IdleTimer';
 
 const ProtectedLayout = () => {
     const data = useLoaderData() as InitializationLoaderResult;
 
+    const handleIdle = () => {
+        redirect('/nbs/logout/');
+    };
+
+    // const timeout = 1000 * 60 * 15; // 15 minutes
+    const timeout = 3000;
+
+    const WithUser = (user: User) => {
+        const data = useLoaderData() as InitializationLoaderResult;
+        return (
+            <UserContextProvider initial={user}>
+                <Await resolve={data?.configuration}>{WithConfiguration}</Await>
+            </UserContextProvider>
+        );
+    };
+
+    const WithConfiguration = (configuration: Configuration) => {
+        return (
+            <ConfigurationProvider initial={configuration}>
+                <AnalyticsProvider>
+                    <Layout />
+                </AnalyticsProvider>
+            </ConfigurationProvider>
+        );
+    };
+
     return (
         <Suspense fallback={<Spinner />}>
+            <IdleTimer onIdle={handleIdle} timeout={timeout} />
             <Await resolve={data?.user} errorElement={<Navigate to={'/login'} />}>
                 {WithUser}
             </Await>
@@ -20,23 +48,23 @@ const ProtectedLayout = () => {
     );
 };
 
-const WithUser = (user: User) => {
-    const data = useLoaderData() as InitializationLoaderResult;
-    return (
-        <UserContextProvider initial={user}>
-            <Await resolve={data?.configuration}>{WithConfiguration}</Await>
-        </UserContextProvider>
-    );
-};
+// const WithUser = (user: User) => {
+//     const data = useLoaderData() as InitializationLoaderResult;
+//     return (
+//         <UserContextProvider initial={user}>
+//             <Await resolve={data?.configuration}>{WithConfiguration}</Await>
+//         </UserContextProvider>
+//     );
+// };
 
-const WithConfiguration = (configuration: Configuration) => {
-    return (
-        <ConfigurationProvider initial={configuration}>
-            <AnalyticsProvider>
-                <Layout />
-            </AnalyticsProvider>
-        </ConfigurationProvider>
-    );
-};
+// const WithConfiguration = (configuration: Configuration) => {
+//     return (
+//         <ConfigurationProvider initial={configuration}>
+//             <AnalyticsProvider>
+//                 <Layout />
+//             </AnalyticsProvider>
+//         </ConfigurationProvider>
+//     );
+// };
 
 export { ProtectedLayout };
