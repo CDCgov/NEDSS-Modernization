d9042f33e8aa759cc26a6730575690ceb0c7dc4b
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2024-05-10 08:05:06 -0700

    adding required header specificity for welcome page and skipLink styles (#1377)
    * adding required header specificity for welcome page and set skipLink styles and used in login pages
    
    * adding unit test fixes

--- apps/modernization-ui/src/apps/landing/Layout/WelcomeLayout.tsx
+++ apps/modernization-ui/src/apps/landing/Layout/WelcomeLayout.tsx
@@ -6,34 +6,36 @@ import { SignUp } from 'apps/landing/SignUp/SignUp';
 import { LoginWrapper } from './LoginWrapper';
 
 import style from './welcomeLayout.module.scss';
+import { SkipLinkProvider } from 'SkipLink/SkipLinkContext';
 
 const WelcomeLayout = () => {
     const [event, setEvent] = useState('login');
 
     return (
-        <LoginWrapper header={<h1>Welcome to the NBS 7 demo site</h1>}>
-            <div className={style.welcome}>
-                <main>
-                    <div className={style.information}>
-                        <TabNavigation className={style.tabs}>
-                            <TabNavigationEntry path={'about'}>About</TabNavigationEntry>
-                            <TabNavigationEntry path={'learn'}>Learn more</TabNavigationEntry>
-                        </TabNavigation>
-                        <div className={style['tab-content']}>
-                            <Outlet />
+        <SkipLinkProvider>
+            <LoginWrapper header={<h1>Welcome to the NBS 7 demo site</h1>}>
+                <div className={style.welcome}>
+                    <main>
+                        <div className={style.information}>
+                            <TabNavigation className={style.tabs}>
+                                <TabNavigationEntry path={'about'}>About</TabNavigationEntry>
+                                <TabNavigationEntry path={'learn'}>Learn more</TabNavigationEntry>
+                            </TabNavigation>
+                            <div className={style['tab-content']}>
+                                <Outlet />
+                            </div>
                         </div>
-                    </div>
-                </main>
-                <aside>
-                    {event === 'login' ? <SignIn handleWelcomeEvent={(event) => setEvent(event)} /> : <SignUp />}
-                    <div className={style.agency}>
-                        <img src="/cdc.svg" alt="" />
-                        <span>Centers for Disease Control and Prevention</span>
-                        <span>@ Copyright 2024 NEDSS Base System (NBS)</span>
-                    </div>
-                </aside>
-            </div>
-        </LoginWrapper>
+                    </main>
+                    <aside>
+                        {event === 'login' ? <SignIn handleWelcomeEvent={(event) => setEvent(event)} /> : <SignUp />}
+                        <div className={style.agency}>
+                            <img src="/cdc.svg" alt="" />
+                            <span>Centers for Disease Control and Prevention</span>
+                        </div>
+                    </aside>
+                </div>
+            </LoginWrapper>
+        </SkipLinkProvider>
     );
 };
 
