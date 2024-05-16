1b7408e6bac100cee98c6054a43e14b539f5e797
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2024-05-16 16:07:42 -0500

    Allows the welcome page to be visible without authentication (#1402)

--- apps/modernization-ui/src/pages/login/Login.tsx
+++ apps/modernization-ui/src/pages/login/Login.tsx
@@ -2,7 +2,7 @@ import { Config } from 'config';
 import { LoginForm } from './LoginForm';
 
 const Login = () => {
-    return Config.enableLogin ? <LoginForm /> : <>{(window.location.href = `/nbs/login`)}</>;
+    return Config.enableLogin ? <LoginForm /> : <>{(window.location.href = '/nbs/login')}</>;
 };
 
 export { Login };
