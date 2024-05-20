24061673b1b703af97d62c0decd4b945c06416bb
Steven Urciuoli <steveu@stevens-mbp-2.lan>
2024-05-20 14:05:38 -0500

    logout

--- apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/classic/NBSClassicServiceProvider.java
+++ apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/classic/NBSClassicServiceProvider.java
@@ -1,21 +1,33 @@
 package gov.cdc.nbs.gateway.classic;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import gov.cdc.nbs.gateway.ui.UIService;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
 class NBSClassicServiceProvider {
 
-    @Bean
-    NBSClassicService nbsClassicService(
-            @Value("${nbs.gateway.classic}") final String classic
-    ) throws URISyntaxException {
-        URI uri = new URI(classic);
-        return new NBSClassicService(uri);
-    }
+  @Bean
+  NBSClassicService nbsClassicService(
+      @Value("${nbs.gateway.classic}") final String classic) throws URISyntaxException {
+    URI uri = new URI(classic);
+    return new NBSClassicService(uri);
+  }
+
+  @Bean
+  RouteLocator logoutRouteLocator(
+      final RouteLocatorBuilder builder,
+      final UIService uiService) {
+    return builder.routes()
+        .route(
+            "classic-logout-redirect", route -> route.path("/nbs/logOut")
+                .filters(filters -> filters.redirect(302, uiService.path("/logout"))).uri("no://op"))
+        .build();
+  }
 
 }
