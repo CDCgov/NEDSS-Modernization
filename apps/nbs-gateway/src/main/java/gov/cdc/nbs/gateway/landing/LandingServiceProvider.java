1ec0bb93528a53ab8da7455ae3797ede84162185
Steven Urciuoli <steveu@stevens-mbp-2.lan>
2024-05-31 14:07:16 -0500

    simplify landingserviceprovider

--- apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/landing/LandingServiceProvider.java
+++ apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/landing/LandingServiceProvider.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import gov.cdc.nbs.gateway.ui.UIService;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -24,13 +23,11 @@ class LandingServiceProvider {
   @Bean
   RouteLocator landingRouteLocator(
       final RouteLocatorBuilder builder,
-      @Value("${nbs.gateway.landing.uri}") final String host,
-      @Value("${nbs.gateway.landing.base}") final String base,
-      final UIService uiService) {
+      final LandingService landingService) {
     return builder.routes()
         .route(
             "landing-service-redirect", route -> route.path("/")
-                .filters(filters -> filters.redirect(302, uiService.path(base))).uri("no://op"))
+                .filters(filters -> filters.redirect(302, landingService.base())).uri("no://op"))
         .build();
   }
 }
