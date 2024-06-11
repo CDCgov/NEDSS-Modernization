f17b83823fb1d3faf1ee200890c9b6b17abb91e3
Steven Urciuoli <steveu@stevens-mbp-2.lan>
2024-06-10 21:15:38 -0500

    CNFT1-2328 Resulted test auto-complete API

--- libs/options-api/src/main/java/gov/cdc/nbs/option/jdbc/autocomplete/SQLBasedOptionResolver.java
+++ libs/options-api/src/main/java/gov/cdc/nbs/option/jdbc/autocomplete/SQLBasedOptionResolver.java
@@ -5,18 +5,47 @@ import gov.cdc.nbs.option.jdbc.OptionRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 public class SQLBasedOptionResolver {
-
+  private static List<String> relatedClassCodes = Arrays.asList(
+      "ABXBACT",
+      "BC",
+      "CELLMARK",
+      "CHAL",
+      "CHALSKIN",
+      "CHEM",
+      "COAG",
+      "CYTO",
+      "DRUG",
+      "DRUG/TOX",
+      "HEM",
+      "HEM/BC",
+      "MICRO",
+      "MISC",
+      "PANEL.ABXBACT",
+      "PANEL.BC",
+      "PANEL.CHEM",
+      "PANEL.MICRO",
+      "PANEL.OBS",
+      "PANEL.SERO",
+      "PANEL.TOX",
+      "PANEL.UA",
+      "SERO",
+      "SPEC",
+      "TOX",
+      "UA",
+      "VACCIN");
 
   private static final String CRITERIA_PARAMETER = "criteria";
   private static final String PREFIX_CRITERIA_PARAMETER = "prefixCriteria";
   private static final String LIMIT_PARAMETER = "limit";
   private static final String SQL_WILDCARD = "%";
   private static final String QUICK_CODE = "quickCode";
+  private static final String RELATED_CLASS_CODES = "relatedClassCodes";
 
   private final String query;
   private final NamedParameterJdbcTemplate template;
@@ -34,6 +63,7 @@ public class SQLBasedOptionResolver {
         CRITERIA_PARAMETER, withWildcard(keyword),
         PREFIX_CRITERIA_PARAMETER, withPrefixWildcard(keyword),
         QUICK_CODE, keyword,
+        RELATED_CLASS_CODES, relatedClassCodes,
         LIMIT_PARAMETER, limit);
     return this.template.query(
         this.query,
---
+++ libs/options-api/src/main/java/gov/cdc/nbs/option/resultedtest/autocomplete/ResultedTestOptionResolver.java
@@ -0,0 +1,42 @@
+package gov.cdc.nbs.option.resultedtest.autocomplete;
+
+import gov.cdc.nbs.option.jdbc.autocomplete.SQLBasedOptionResolver;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResultedTestOptionResolver extends SQLBasedOptionResolver {
+  private static final String QUERY = """
+      with [resultedtest]([value], [name]) as (
+          SELECT DISTINCT
+            lab_test_cd, lab_test_desc_txt
+          FROM
+            NBS_SRTE.dbo.Lab_test
+          WHERE
+            test_type_cd = 'R'
+          UNION
+          SELECT DISTINCT
+            loinc_cd, component_name
+          FROM
+            NBS_SRTE.dbo.LOINC_code
+          WHERE
+            related_class_cd in(:relatedClassCodes)
+      )
+      select
+          [value],
+          [name],
+          row_number() over( order by [name])
+      from [resultedtest]
+      where [name] like :criteria or [name] like :prefixCriteria or[value] like :criteria
+
+      order by
+          [name]
+
+      offset 0 rows
+      fetch next :limit rows only
+      """;
+
+  public ResultedTestOptionResolver(final NamedParameterJdbcTemplate template) {
+    super(QUERY, template);
+  }
+}
