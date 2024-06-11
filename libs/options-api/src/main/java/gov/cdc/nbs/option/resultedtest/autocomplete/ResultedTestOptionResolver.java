3861c7e8bbef29bef3bc1c2b0bd39ca7e32f09d5
Steven Urciuoli <steveu@stevens-mbp-2.lan>
2024-06-11 13:25:50 -0500

    update query with in clause values

--- libs/options-api/src/main/java/gov/cdc/nbs/option/resultedtest/autocomplete/ResultedTestOptionResolver.java
+++ libs/options-api/src/main/java/gov/cdc/nbs/option/resultedtest/autocomplete/ResultedTestOptionResolver.java
@@ -6,35 +6,36 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ResultedTestOptionResolver extends SQLBasedOptionResolver {
-  private static final String QUERY = """
-      with [resultedtest]([value], [name]) as (
-          SELECT DISTINCT
-            lab_test_cd, lab_test_desc_txt
-          FROM
-            NBS_SRTE.dbo.Lab_test
-          WHERE
-            test_type_cd = 'R'
-          UNION
-          SELECT DISTINCT
-            loinc_cd, component_name
-          FROM
-            NBS_SRTE.dbo.LOINC_code
-          WHERE
-            related_class_cd in(:relatedClassCodes)
-      )
-      select
-          [value],
-          [name],
-          row_number() over( order by [name])
-      from [resultedtest]
-      where [name] like :criteria or [name] like :prefixCriteria or[value] like :criteria
+  private static final String QUERY =
+      """
+          with [resultedtest]([value], [name]) as (
+              SELECT DISTINCT
+                lab_test_cd, lab_test_desc_txt
+              FROM
+                NBS_SRTE.dbo.Lab_test
+              WHERE
+                test_type_cd = 'R'
+              UNION
+              SELECT DISTINCT
+                loinc_cd, component_name
+              FROM
+                NBS_SRTE.dbo.LOINC_code
+              WHERE
+                related_class_cd in('ABXBACT','BC','CELLMARK','CHAL','CHALSKIN','CHEM','COAG','CYTO','DRUG','DRUG/TOX','HEM','HEM/BC','MICRO','MISC','PANEL.ABXBACT','PANEL.BC','PANEL.CHEM','PANEL.MICRO','PANEL.OBS','PANEL.SERO','PANEL.TOX','PANEL.UA','SERO','SPEC','TOX','UA','VACCIN')
+          )
+          select
+              [value],
+              [name],
+              row_number() over( order by [name])
+          from [resultedtest]
+          where [name] like :criteria or [name] like :prefixCriteria or[value] like :criteria
 
-      order by
-          [name]
+          order by
+              [name]
 
-      offset 0 rows
-      fetch next :limit rows only
-      """;
+          offset 0 rows
+          fetch next :limit rows only
+          """;
 
   public ResultedTestOptionResolver(final NamedParameterJdbcTemplate template) {
     super(QUERY, template);
