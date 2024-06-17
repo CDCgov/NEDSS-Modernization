72417b88c24702bb3d8820b37993fcd6217ff3c4
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-06-17 11:19:43 -0500

    CNFT1-2427 Patient Search form
    transformer
    
    test
    
    cleanup

---
+++ apps/modernization-ui/src/apps/search/patient/PatientCriteria/transformer.spec.ts
@@ -0,0 +1,28 @@
+import { PatientCriteriaForm } from "./PatientCriteria"
+import { transformObject } from "./transformer";
+
+describe('when transformObject runs', () => {
+    it('should render Selectable objects', () => {
+        const input: PatientCriteriaForm = {
+            country: { value: 'USA', name: 'United States of America', label: 'United States'}
+        };
+        const expected = {
+            "assigningAuthority": undefined,
+            "country": "USA",
+            "deceased": undefined,
+            "ethnicity": undefined,
+            "gender": undefined,
+            "identification": undefined,
+            "identificationType": undefined,
+            "labTest": "default",
+            "mortalityStatus": undefined,
+            "race": undefined,
+            "recordStatus": [],
+            "state": undefined,
+            "status": undefined,
+            "zip": undefined,
+        };
+        const result = transformObject(input);
+        expect(result).toEqual(expected);
+    });
+});
