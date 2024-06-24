3aeb39fc1c8dbd13006af712be6039a5f444694d
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2024-06-24 09:57:19 -0700

    Design system/select (#1496)
    * selectable findbyvalue
    
    * expose Orientation
    
    * singe select
    
    * adding multiSelect using selectable[]
    
    * tests for multi and optional props
    
    * semicolumn
    
    * removing undesired change on keycloak login
    
    ---------
    
    Co-authored-by: Adam Loup <aloup@enquizit.com>

---
+++ apps/modernization-ui/src/design-system/select/multi/MultiSelect.spec.tsx
@@ -0,0 +1,46 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MultiSelect } from './MultiSelect';
+
+describe('MultiSelect', () => {
+    const options = [
+        { name: 'Option One', value: '1', label: 'Option One' },
+        { name: 'Option Two', value: '2', label: 'Option Two' },
+        { name: 'Option Three', value: '3', label: 'Option Three' }
+    ];
+
+    it('should display options when clicked', async () => {
+        const { getByText, getByRole } = render(
+            <MultiSelect id="test-multi-select" name="test-multi-select" label="Test Multi Select" options={options} />
+        );
+
+        const component = getByRole('combobox');
+
+        userEvent.click(component);
+
+        options.forEach((option) => {
+            expect(getByText(option.label)).toBeInTheDocument();
+        });
+    });
+
+    it('should allow selecting multiple options', async () => {
+        const onChange = jest.fn();
+        const { getByText, getByRole } = render(
+            <MultiSelect
+                id="test-multi-select"
+                name="test-multi-select"
+                label="Test Multi Select"
+                options={options}
+                onChange={onChange}
+            />
+        );
+
+        const component = getByRole('combobox');
+
+        userEvent.click(component);
+
+        userEvent.click(getByText('Option One'));
+
+        expect(onChange).toHaveBeenCalledWith([expect.objectContaining({ value: '1', label: 'Option One' })]);
+    });
+});
