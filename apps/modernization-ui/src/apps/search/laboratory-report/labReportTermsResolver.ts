aeeedb384e7abcd566feedca4b20f1a27fd045a2
Adam Loup <aloup@enquizit.com>
2024-06-25 13:12:53 -0500

    useLaboratoryReportSearch

---
+++ apps/modernization-ui/src/apps/search/laboratory-report/labReportTermsResolver.ts
@@ -0,0 +1,10 @@
+import { Term } from 'apps/search/useSearchResultDisplay';
+import { LabReportFilterEntry } from './labReportFormTypes';
+
+const labReportTermsResolver = (entry: LabReportFilterEntry): Term[] => {
+    const terms: Term[] = [];
+
+    return terms;
+};
+
+export { labReportTermsResolver };
---
+++ apps/modernization-ui/src/apps/search/laboratory-report/useLaboratoryReportSearch.ts
@@ -0,0 +1,32 @@
+import { LabReport, LabReportFilter, useFindLabReportsByFilterLazyQuery } from 'generated/graphql/schema';
+import { Interaction, ResultRequest, useSearch } from 'apps/search';
+import { LabReportFilterEntry } from './labReportFormTypes';
+import { transformObject } from './transformer';
+import { labReportTermsResolver } from './labReportTermsResolver';
+
+const useLaboratoryReportSearch = (): Interaction<LabReportFilterEntry, LabReport> => {
+    const [fetch] = useFindLabReportsByFilterLazyQuery();
+
+    const resultResolver = (request: ResultRequest<LabReportFilter>) =>
+        fetch({
+            variables: {
+                filter: request.parameters,
+                page: {
+                    pageNumber: request.page.number - 1,
+                    pageSize: request.page.size
+                }
+            },
+            notifyOnNetworkStatusChange: true
+        }).then((response) => {
+            if (response.error) {
+                throw new Error(response.error.message);
+            }
+            return response.data?.findLabReportsByFilter
+                ? { ...response.data.findLabReportsByFilter, page: request.page.number }
+                : undefined;
+        });
+
+    return useSearch({ transformer: transformObject, resultResolver, termResolver: labReportTermsResolver });
+};
+
+export { useLaboratoryReportSearch };
