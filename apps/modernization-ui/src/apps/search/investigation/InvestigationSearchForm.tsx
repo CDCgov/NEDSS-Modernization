55f9cab40eab823467ef166c09392ebeef5b8c7c
Richard <rkrussel@enquizit.com>
2024-07-05 15:50:44 -0500

    add accordions

--- apps/modernization-ui/src/apps/search/investigation/InvestigationSearchForm.tsx
+++ apps/modernization-ui/src/apps/search/investigation/InvestigationSearchForm.tsx
@@ -1,12 +1,17 @@
 import GeneralSearchFields from './GeneralSearchFields';
 import styles from './InvestigationSearchForm.module.scss';
 import CriteriaSearchFields from './CriteriaSearchFields';
+import { Accordion } from 'components/Accordion/Accordion';
 
 const InvestigationSearchForm = () => {
     return (
         <div className={styles.investigationSearchContainer}>
-            <GeneralSearchFields />
-            <CriteriaSearchFields />
+            <Accordion title="General search">
+                <GeneralSearchFields />
+            </Accordion>
+            <Accordion title="Investigation criteria">
+                <CriteriaSearchFields />
+            </Accordion>
         </div>
     );
 };
