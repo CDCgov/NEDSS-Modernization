5b08951aa8af0f762c8a41c37e62cdf2d8210f9e
Alaap Bharadwaj <abharadwaj@enquizit.com>
2024-07-10 14:43:40 -0400

    feature created

--- apps/modernization-ui/src/apps/search/layout/result/SearchResults.tsx
+++ apps/modernization-ui/src/apps/search/layout/result/SearchResults.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from 'react';
 import { useSearchResultDisplay } from 'apps/search';
 import { SearchResultsHeader } from './header/SearchResultsHeader';
-
 import styles from './search-results.module.scss';
 import { usePage } from 'page';
+import { AlertBanner } from 'alert';
+import { Link } from 'react-router-dom';
 
 type Props = {
     children: ReactNode;
@@ -19,7 +20,27 @@ const SearchResults = ({ children }: Props) => {
     return (
         <div className={styles.results}>
             <SearchResultsHeader className={styles.header} view={view} total={total} terms={terms} />
-            <main className={styles.content}>{children}</main>
+            <main className={styles.content}>
+                {total > 0 ? (
+                    children
+                ) : (
+                    <div className={styles.noResults}>
+                        <AlertBanner type="info" iconSize={4}>
+                            <div className={styles.noResultsContent}>
+                                <span className={styles.noResultsHeader}> No result found</span>
+                                <span className={styles.noResultsSubHeading}>
+                                    Try refining your search, or
+                                    <Link className={styles.link} to="/add-patient">
+                                        {' '}
+                                        add a new patient.
+                                    </Link>
+                                </span>
+                            </div>
+                        </AlertBanner>
+                    </div>
+                )}
+            </main>
+
             <div className={styles.pagingation}></div>
         </div>
     );
