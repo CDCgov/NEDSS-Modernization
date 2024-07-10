42c6ba5895e6b4f1ca0afc13a3ef74c9804a9b66
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2024-07-10 09:21:49 -0700

    adding sort ui and toggle view component (#1535)
    * adding sort ui and toggle view component
    
    * setting up ToggleView  with useSearchResultDisplay
    
    * using use sorting hook to use for the sort of the patient search
    
    * adding sorting hook for investigation and labreports
    
    * changes useSorting to contain the property and direction
    
    ---------
    
    Co-authored-by: Adam Loup <aloup@enquizit.com>

--- apps/modernization-ui/src/apps/search/layout/result/header/options/list/SearchResultsListOptions.tsx
+++ apps/modernization-ui/src/apps/search/layout/result/header/options/list/SearchResultsListOptions.tsx
@@ -1,14 +1,57 @@
+import { ButtonActionMenu } from 'components/ButtonActionMenu/ButtonActionMenu';
 import { Icon } from '@trussworks/react-uswds';
-import { Button } from 'components/button';
-
 import styles from './search-results-list-options.module.scss';
+import { Direction, useSorting } from 'sorting';
+import { SortField } from 'generated/graphql/schema';
 
 type Props = {
     disabled?: boolean;
 };
 
 const SearchResultsListOptions = ({ disabled = false }: Props) => {
-    return <Button className={styles.option} outline icon={<Icon.SortArrow />} disabled={disabled} />;
+    const { sortBy } = useSorting();
+
+    return (
+        <ButtonActionMenu
+            className={styles.option}
+            outline
+            icon={<Icon.SortArrow />}
+            disabled={disabled}
+            items={[
+                {
+                    label: 'Closest match',
+                    action: () => {
+                        sortBy(SortField.Relevance, Direction.Descending);
+                    }
+                },
+                {
+                    label: 'Patient name (A-Z)',
+                    action: () => {
+                        sortBy(SortField.LastNm, Direction.Ascending);
+                    }
+                },
+                {
+                    label: 'Patient name (Z-A)',
+                    action: () => {
+                        sortBy(SortField.LastNm, Direction.Descending);
+                    }
+                },
+                {
+                    label: 'Date of birth (Ascending)',
+                    action: () => {
+                        sortBy(SortField.BirthTime, Direction.Ascending);
+                    }
+                },
+                {
+                    label: 'Date of birth (Descending)',
+                    action: () => {
+                        sortBy(SortField.BirthTime, Direction.Descending);
+                    }
+                }
+            ]}
+            label={''}
+        />
+    );
 };
 
 export { SearchResultsListOptions };
