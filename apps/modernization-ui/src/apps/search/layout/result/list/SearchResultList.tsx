d5e01bfec59c553dc2592c45d996a45b3ea15531
Henry Clark <hclark@enquizit.com>
2024-07-11 11:15:35 -0700

    reverting uneeded changes

--- apps/modernization-ui/src/apps/search/layout/result/list/SearchResultList.tsx
+++ apps/modernization-ui/src/apps/search/layout/result/list/SearchResultList.tsx
@@ -8,18 +8,11 @@ type Props<T> = {
 };
 
 const SearchResultList = <T,>({ results, render }: Props<T>) => {
-    return results && results.length > 0 ? (
-        results.map((result, index) => (
-            <div className={styles.result} key={index}>
-                {render(result)}
-            </div>
-        ))
-    ) : (
-        <div className="text-center">
-            <p>No results found.</p>
-            <p>Try refining your search.</p>
+    return results.map((result, index) => (
+        <div className={styles.result} key={index}>
+            {render(result)}
         </div>
-    );
+    ));
 };
 
 export { SearchResultList };
