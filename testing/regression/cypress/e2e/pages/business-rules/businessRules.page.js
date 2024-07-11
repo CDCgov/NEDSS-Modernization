174f10c38dca139cddd8a7f88aa3bd9c4a58120e
Lakshmi Narayanan Ramakrishnan <lramakrishnan@enquizit.com>
2024-07-11 08:27:57 -0700

    CNT-43 Business Rules Library - function

--- testing/regression/cypress/e2e/pages/business-rules/businessRules.page.js
+++ testing/regression/cypress/e2e/pages/business-rules/businessRules.page.js
@@ -24,6 +24,46 @@ class BusinessRulesPage {
                 }
             })
     }
+
+    checkFunctionColumnPopulated() {
+        cy.get('.business-rules-library')
+            .invoke('text')
+            .then((text) => {
+                if (!text.includes("Disable")) {
+                    cy.contains("Disable");
+                } else if (!text.includes("Enable")) {
+                   cy.contains("Enable");
+               } else if (!text.includes("Hide")) {
+                    cy.contains("Hide");
+               } else if (!text.includes("Unhide")) {
+                    cy.contains("Unhide");
+               }
+            })
+    }
+
+    checkTargetColumnPopulated() {
+        cy.get("table[data-testid=table]").eq(0).find("tbody tr").each(($tr) => {
+            expect($tr.find("td").eq(4).text()).to.exist;
+        });
+    }
+
+    enterUniqueNameInSearchField() {
+        cy.get('#business-rules-search').type('date of birth');
+    }
+
+    clickBusinessRulesSearchBtn() {
+        cy.get('[data-testid="businessRulesSearchBtn"]').click();
+    }
+
+    checkBusinessRulesListDisplayed() {
+        cy.get("table[data-testid=table]").eq(0).find("tbody tr").each(($tr) => {
+            expect($tr.find("td").eq(0).text()).to.exist;
+        });
+    }
+
+    enterUniqueIdInSearchField() {
+        cy.get('#business-rules-search').type('dem115');
+    }
 }
 
 export const businessRulesPage = new BusinessRulesPage()
