88698496a905afa89467528e37952b2cc215bf2a
Adam Loup <aloup@enquizit.com>
2024-07-11 14:43:46 -0500

    data-table component

--- apps/modernization-ui/src/apps/search/patient/result/table/PatientSearchResultTable.tsx
+++ apps/modernization-ui/src/apps/search/patient/result/table/PatientSearchResultTable.tsx
@@ -1,12 +1,49 @@
 import { PatientSearchResult } from 'generated/graphql/schema';
-import { DataTable } from 'design-system/table';
+import { Column, DataTable } from 'design-system/table';
+import { displayName } from 'name';
+import { internalizeDate } from 'date';
+import { displayAddress } from 'address/display';
+
+const displayNames = (result: PatientSearchResult): string => {
+    const legalName = result.legalName;
+    return result.names
+        .filter((name) => name?.first != legalName?.first || name?.last != legalName?.last)
+        .map(displayName())
+        .join('\n');
+};
+
+const displayAddresses = (result: PatientSearchResult): string => result.addresses.map(displayAddress).join('\n');
+
+const displayIdentifications = (result: PatientSearchResult): string =>
+    result.identification.map((identification) => identification.type + '\n' + identification.value).join('\n');
+
+const displayPhones = (result: PatientSearchResult): string => result.phones.join('\n');
+const displayEmails = (result: PatientSearchResult): string => result.emails.join('\n');
 
 type Props = {
     results: PatientSearchResult[];
 };
 
+const columns: Column<PatientSearchResult>[] = [
+    {
+        id: 'lastNm',
+        name: 'Legal name',
+        fixed: true,
+        sortable: true,
+        render: (row) => row?.legalName && displayName()(row?.legalName)
+    },
+    { id: 'birthTime', name: 'Date of birth', sortable: true, render: (result) => internalizeDate(result.birthday) },
+    { id: 'sex', name: 'Sex', sortable: true, render: (result) => result.gender },
+    { id: 'id', name: 'Patient ID', render: (row) => row.shortId },
+    { id: 'address', name: 'Address', render: displayAddresses },
+    { id: 'phoneNumber', name: 'Phone', render: displayPhones },
+    { id: 'names', name: 'Other names', render: displayNames },
+    { id: 'identification', name: 'ID', render: displayIdentifications },
+    { id: 'email', name: 'Email', render: displayEmails }
+];
+
 const PatientSearchResultTable = ({ results }: Props) => {
-    return <DataTable<PatientSearchResult> data={results}></DataTable>;
+    return <DataTable<PatientSearchResult> id="patient-search-results" columns={columns} data={results}></DataTable>;
 };
 
 export { PatientSearchResultTable };
