70528e44347c6a2ce4962bad94f97fbc15a7209f
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-07-19 09:34:27 -0500

    CNFT1-2804 Refactor ButtonActionMenu (#1580)
    removing instances
    
    cleanup

--- apps/modernization-ui/src/components/ButtonActionMenu/ButtonActionMenu.spec.tsx
+++ apps/modernization-ui/src/components/ButtonActionMenu/ButtonActionMenu.spec.tsx
@@ -4,13 +4,12 @@ import { render } from "@testing-library/react";
 
 describe('When ButtonActionMenu renders', () => {
     const { container } = render(
-        <ButtonActionMenu
-            label="Add new"
-            items={[
-                { label: 'Add new patient', action: jest.fn() },
-                { label: 'Add new lab report', action: jest.fn() }
-            ]}
-        />
+        <ButtonActionMenu label="Add new">
+            <>
+                <Button type="button" onClick={() => jest.fn()}>Test this</Button>
+                <Button type="button" onClick={() =>jest.fn() }>Test that</Button>
+            </>
+        </ButtonActionMenu>
     );
     it('should not display menu', () => {
         expect(container.getElementsByClassName('menu')).toHaveLength(0);
