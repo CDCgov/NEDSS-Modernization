eb25da1640c4346cffdcb238713be0005000cc11
Steve Urciuoli <surciuoli@enquizit.com>
2024-07-23 09:06:40 -0500

    CNFT1-2555 API: Patient sorting for every field (#1554)

--- apps/modernization-api/src/test/java/gov/cdc/nbs/patient/PatientSteps.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/patient/PatientSteps.java
@@ -18,8 +18,7 @@ public class PatientSteps {
 
   PatientSteps(
       final Active<PatientIdentifier> patient,
-      final PatientMother mother
-  ) {
+      final PatientMother mother) {
     this.patient = patient;
     this.mother = mother;
   }
@@ -73,6 +72,14 @@ public class PatientSteps {
           identifier,
           LocalDate.parse(value));
 
+      case "sex" -> mother.withGender(
+          identifier,
+          value);
+
+      case "id" -> mother.withId(identifier, Long.parseLong(value));
+
+      case "local id" -> mother.withLocalId(identifier, value);
+
       case "phone number" -> mother.withPhone(
           identifier,
           value);
@@ -99,6 +106,13 @@ public class PatientSteps {
           null,
           null,
           null);
+
+      case "zip" -> mother.withAddress(
+          identifier,
+          null,
+          null,
+          null,
+          value);
 
       case "city" -> mother.withAddress(
           identifier,
