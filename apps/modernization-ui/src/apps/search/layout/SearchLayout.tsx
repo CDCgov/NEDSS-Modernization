f99d1b8d772043b271af3176b053413552862b12
Alaap Bharadwaj <abharadwaj@enquizit.com>
2024-07-24 10:15:32 -0400

    collapse button working now

--- apps/modernization-ui/src/apps/search/layout/SearchLayout.tsx
+++ apps/modernization-ui/src/apps/search/layout/SearchLayout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { Button } from 'components/button';
 import { Term, useSearchResultDisplay } from 'apps/search';
 import { SearchLanding } from './landing';
@@ -8,6 +8,7 @@ import styles from './search-layout.module.scss';
 import { Loading } from 'components/Spinner';
 import { SearchNavigation } from './navigation/SearchNavigation';
 import { usePage } from 'page';
+import { Icon } from '@trussworks/react-uswds';
 
 type Renderer = () => ReactNode;
 
@@ -36,6 +37,8 @@ const SearchLayout = ({
 }: Props) => {
     const { view, status } = useSearchResultDisplay();
 
+    const [collapse, setCollapse] = useState<boolean>();
+
     const {
         page: { total }
     } = usePage();
@@ -44,7 +47,7 @@ const SearchLayout = ({
         <section className={styles.search}>
             <SearchNavigation className={styles.navigation} actions={actions} />
             <div className={styles.content}>
-                <div className={styles.criteria}>
+                <div className={collapse ? styles.collapse : styles.criteria}>
                     <div className={styles.search}>{criteria()}</div>
                     <div className={styles.actions}>
                         <Button type="button" onClick={onSearch}>
@@ -55,19 +58,27 @@ const SearchLayout = ({
                         </Button>
                     </div>
                 </div>
+                <div className={styles.collapseButton}>
+                    <div className={collapse ? styles.collapseTrue : styles.content}>
+                        {!collapse && <Icon.ExpandLess onClick={() => setCollapse(true)} size={3} />}
+                        {collapse && <Icon.ExpandMore onClick={() => setCollapse(false)} size={3} />}
+                    </div>
+                </div>
                 <div className={styles.results}>
-                    {status === 'waiting' && <SearchLanding />}
-                    {status === 'searching' && <Loading className={styles.loading} />}
-                    {status === 'completed' && (
-                        <SearchResults onRemoveTerm={onRemoveTerm}>
-                            {total === 0 && noResults?.()}
-                            {view === 'list' && resultsAsList()}
-                            {view === 'table' && resultsAsTable()}
-                        </SearchResults>
-                    )}
-                    {status === 'noInput' && (
-                        <SearchResults onRemoveTerm={onRemoveTerm}>{noInputResults?.()}</SearchResults>
-                    )}
+                    <div className={styles.resultContent}>
+                        {status === 'waiting' && <SearchLanding />}
+                        {status === 'searching' && <Loading className={styles.loading} />}
+                        {status === 'completed' && (
+                            <SearchResults onRemoveTerm={onRemoveTerm}>
+                                {total === 0 && noResults?.()}
+                                {view === 'list' && resultsAsList()}
+                                {view === 'table' && resultsAsTable()}
+                            </SearchResults>
+                        )}
+                        {status === 'noInput' && (
+                            <SearchResults onRemoveTerm={onRemoveTerm}>{noInputResults?.()}</SearchResults>
+                        )}
+                    </div>
                 </div>
             </div>
         </section>
