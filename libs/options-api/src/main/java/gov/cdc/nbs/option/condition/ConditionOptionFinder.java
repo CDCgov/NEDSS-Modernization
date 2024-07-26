7095070f9cf0f2d6bd6556c0a0d4b69b810bcb4f
Adam Loup <aloup@enquizit.com>
2024-07-26 18:05:46 -0500

    standardize condition options

--- libs/options-api/src/main/java/gov/cdc/nbs/option/condition/ConditionOptionFinder.java
+++ libs/options-api/src/main/java/gov/cdc/nbs/option/condition/ConditionOptionFinder.java
@@ -8,15 +8,16 @@ import org.springframework.stereotype.Component;
 class ConditionOptionFinder extends SQLBasedOptionFinder {
 
   private static final String QUERY = """
-      select\s
+      select
           condition_cd        as [value],
           condition_short_nm  as [name],
           indent_level_nbr    as [order]
       from NBS_SRTE..Condition_code
           where status_cd = 'A'
       order by
-          indent_level_nbr
-            """;
+          indent_level_nbr,
+          condition_short_nm
+      """;
 
 
   ConditionOptionFinder(final JdbcTemplate template) {
