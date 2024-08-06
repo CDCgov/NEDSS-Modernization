e5dbbb7fb868f8216141d6c15aadcf679482b1ac
Adam Loup <aloup@enquizit.com>
2024-08-06 09:27:43 -0500

    multi-select

--- apps/modernization-ui/src/design-system/select/multi/CheckboxOption.tsx
+++ apps/modernization-ui/src/design-system/select/multi/CheckboxOption.tsx
@@ -2,8 +2,10 @@ import { OptionProps, components } from 'react-select';
 import { Checkbox } from 'design-system/checkbox';
 import { Selectable } from 'options';
 
+import styles from './checkbox-option.module.scss';
+
 const CheckboxOption = (props: OptionProps<Selectable, true>) => (
-    <components.Option {...props}>
+    <components.Option {...props} className={styles.option}>
         <Checkbox label={props.label} selected={props.isSelected} />
     </components.Option>
 );
