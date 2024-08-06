430d8e19cde8cd5dbf0289114ce3788dc5faa2de
Henry Clark <hclark@enquizit.com>
2024-08-06 07:39:13 -0700

    adding tooltip for gear icon

--- apps/modernization-ui/src/apps/search/layout/result/header/options/table/SearchResultsTableOptions.tsx
+++ apps/modernization-ui/src/apps/search/layout/result/header/options/table/SearchResultsTableOptions.tsx
@@ -17,6 +17,9 @@ const SearchResultsTableOptions = ({ disabled = false }: Props) => {
             position="right"
             toggle={({ toggle }) => (
                 <Button
+                    className={styles.tooltip}
+                    aria-label="Columns Setting"
+                    data-tooltip-position="top"
                     outline
                     disabled={disabled}
                     icon={
