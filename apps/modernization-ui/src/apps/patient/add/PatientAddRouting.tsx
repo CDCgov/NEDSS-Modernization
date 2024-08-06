de3e708f6ced657c3187bfd5e866c1f1085cc4fe
Richard Krussel <139483220+rkrusselenq@users.noreply.github.com>
2024-08-06 11:55:12 -0500

    Adds page for new patient extended (#1653)
    * init
    
    * remove unused vars

--- apps/modernization-ui/src/apps/patient/add/PatientAddRouting.tsx
+++ apps/modernization-ui/src/apps/patient/add/PatientAddRouting.tsx
@@ -1,9 +1,14 @@
 import { AddPatient } from './AddPatient';
+import { AddPatientExtended } from './extended/AddPatientExtended';
 
 const routing = [
     {
         path: '/add-patient',
         element: <AddPatient />
+    },
+    {
+        path: '/patient/add/extended',
+        element: <AddPatientExtended />
     }
 ];
 
