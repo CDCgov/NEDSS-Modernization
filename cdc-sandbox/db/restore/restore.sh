eff704327282c02d1839a7f5d23d430d436847aa
Adam Loup <aloup@enquizit.com>
2024-08-07 10:23:55 -0500

    initialization

--- cdc-sandbox/db/restore/restore.sh
+++ cdc-sandbox/db/restore/restore.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
-until /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -Q "select 1" &> /dev/null; do
-    sleep 1s
-done;
+set -e
+BASE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+initialization_check=$BASE/restore.done
+
+if [ ! -f $initialization_check ]; then
+
+    until /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -Q "select 1" &> /dev/null; do
+        sleep 1s
+    done;
+
+    echo "*************************************************************************"
+    echo "  Initializing NBS databases"
+    echo "*************************************************************************"
+
+    for sql in $(find "$INITIALIZE_SCRIPTS/restore.d" -iname "*.sql" | sort) ; 
+    do
+        echo "Executing: $sql"  
+        /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -i "$sql"
+
+        echo "Completed: $sql"
+    done
+
+    date > $initialization_check
+
+fi
 
 echo "*************************************************************************"
-echo "Initializing NBS databases"
+echo "  NBS databases ready"
 echo "*************************************************************************"
-
-for restore_file in $(find "$INITIALIZE_SCRIPTS/restore.d" -iname "*.sql" | sort) ; 
-do
-    echo "Executing: $restore_file"  
-    /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -i "$restore_file"
-
-    echo "Completed: $restore_file"
-done
