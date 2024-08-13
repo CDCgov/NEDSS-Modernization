bfdee0725714f69dfd895f0e584473d61c2a4922
Michael Peels <michaelpeels@Michael-Peels.local>
2024-08-13 11:21:04 -0400

    fix other references

--- cdc-sandbox/build_modernized.sh
+++ cdc-sandbox/build_modernized.sh
@@ -5,17 +5,17 @@ BASE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Start ES and the proxy
 echo "Starting elasticsearch reverse-proxy"
-docker-compose -f $BASE/docker-compose.yml up elasticsearch --build -d
+docker compose -f $BASE/docker-compose.yml up elasticsearch --build -d
 
 MODERNIZED_PATH=$BASE/../
 
 # Start of modernization-api initializes the elasticserach indices 
 echo "Starting modernized application services"
-docker-compose -f $MODERNIZED_PATH/docker-compose.yml  up --build -d
+docker compose -f $MODERNIZED_PATH/docker-compose.yml  up --build -d
 
 # NiFi handles synchronizing data between nbs-mssql and elasticsearch
 echo "Starting NiFi"
-docker-compose -f $BASE/docker-compose.yml up nifi --build -d
+docker compose -f $BASE/docker-compose.yml up nifi --build -d
 
 # Process complete
 echo "**** Modernized application startup complete ****"
