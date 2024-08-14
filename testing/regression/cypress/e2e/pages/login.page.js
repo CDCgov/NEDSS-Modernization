6a1f5f91d5e5982448b5971085d4c12c9e1d5847
Michael Peels <michaelpeels@Michael-Peels.local>
2024-08-14 11:30:11 -0400

    WIP

--- testing/regression/cypress/e2e/pages/login.page.js
+++ testing/regression/cypress/e2e/pages/login.page.js
@@ -1,42 +1,38 @@
-const user = Cypress.env()["env"].loginusername;
-const pass = Cypress.env()["env"].loginpassword;  
+const user = Cypress.env("LOGIN_USERNAME");
+const pass = Cypress.env("LOGIN_PASSWORD");
 
 class LoginPage {
-
-    navigateToHomepage() {
-        cy.visit('/nbs/login')
-    }
-
-    login() {      
-        cy.get('body').then((body) => {
-            if (body.find("input[id='id_UserName']").length > 0) {
-                cy.intercept("POST", "/graphql").as("loginRequest");
-                cy.get('#id_UserName').type(user);
-                if (pass != '') {
-                    cy.get('#id_Password').type(pass);
-                }
-                cy.get('#id_Submit_bottom_ToolbarButtonGraphic').click();
-                cy.get('#homePageAdvancedSearch').click();
-                cy.wait("@loginRequest");         
-            } else {
-                cy.intercept("POST", "/graphql").as("loginRequest");
-                cy.get('#username').type(user);
-                if (pass != '') {
-                    cy.get('#password').type(pass);
-                }
-                cy.get('#kc-login').click();
-                cy.get('#homePageAdvancedSearch').click();
-                cy.wait("@loginRequest");
-            }
-        });
-
-
-
-    }
-
-    verifyLoginPage() {
-        cy.get('.logo img').should('be.visible')
-    }
+  navigateToHomepage() {
+    cy.visit("/nbs/login");
   }
-  
-export const loginPage = new LoginPage(); 
+
+  login() {
+    cy.get("body").then((body) => {
+      if (body.find("input[id='id_UserName']").length > 0) {
+        cy.intercept("POST", "/graphql").as("loginRequest");
+        cy.get("#id_UserName").type(user);
+        if (pass != "") {
+          cy.get("#id_Password").type(pass);
+        }
+        cy.get("#id_Submit_bottom_ToolbarButtonGraphic").click();
+        cy.get("#homePageAdvancedSearch").click();
+        cy.wait("@loginRequest");
+      } else {
+        cy.intercept("POST", "/graphql").as("loginRequest");
+        cy.get("#username").type(user);
+        if (pass != "") {
+          cy.get("#password").type(pass);
+        }
+        cy.get("#kc-login").click();
+        cy.get("#homePageAdvancedSearch").click();
+        cy.wait("@loginRequest");
+      }
+    });
+  }
+
+  verifyLoginPage() {
+    cy.get(".logo img").should("be.visible");
+  }
+}
+
+export const loginPage = new LoginPage();
--- testing/regression/cypress/step_definitions/api/api.steps.js
+++ testing/regression/cypress/step_definitions/api/api.steps.js
@@ -1,21 +1,25 @@
-import { When, Then, attach, Given } from "@badeball/cypress-cucumber-preprocessor";
+import { attach, Given } from "@badeball/cypress-cucumber-preprocessor";
+const user = Cypress.env("LOGIN_USERNAME");
+const password = Cypress.env("LOGIN_PASSWORD");
+const baseUrl = Cypress.config().baseUrl;
 
 Given("I login to the generate token", () => {
   cy.request({
     method: "POST",
-    url: "https://app.test.nbspreview.com/login",
+    url: `${baseUrl}login`,
     headers: {
       "Content-Type": "application/json",
     },
     body: {
-      username: "superuser",
+      username: user,
+      password: password,
     },
   }).then((response) => {
     expect(response.status).to.eq(200);
     attach(`Response: ${JSON.stringify(response.body)}`);
     const authToken = response.body.token;
+    expect(authToken).to.not.equal(undefined);
     cy.wrap(authToken).as("authToken");
-    cy.log("Stored Token:", authToken);
     Cypress.env("authToken", authToken);
   });
 });
@@ -24,36 +28,10 @@ Given("I get user information", () => {
   const authToken = Cypress.env("authToken");
   cy.request({
     method: "GET",
-    url: "https://app.test.nbspreview.com/nbs/api/me",
+    url: `${baseUrl}nbs/api/me`,
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${authToken}`,
-    },
-  }).then((response) => {
-    expect(response.status).to.eq(200);
-    attach(`Response: ${JSON.stringify(response.body)}`);
-  });
-});
-
-Given("create a new patient", () => {
-  const authToken = Cypress.env("authToken");
-  cy.request({
-    method: "POST",
-    url: "https://app.test.nbspreview.com/graphql",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${authToken}`,
-    },
-    body: {
-      query:
-        "mutation create($patient: PersonInput!) {\n  createPatient(patient: $patient) {\n    id\n    shortId\n  }\n}\n",
-      variables: {
-        patient: {
-          comments: "Created for Development testing",
-          asOf: "2023-11-22T18:10:48.199Z",
-          names: [{ use: "L", first: "Aimee", last: "Schuppe" }],
-        },
-      },
     },
   }).then((response) => {
     expect(response.status).to.eq(200);
