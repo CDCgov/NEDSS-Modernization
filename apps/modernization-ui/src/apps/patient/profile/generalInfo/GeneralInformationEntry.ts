51b3e9b9b8ca97543415f2365a3df5e90a9ce8c0
Michael Peels <michaelpeels@Michael-Peels.local>
2024-08-20 13:08:42 -0400

    Separates out patient demographic forms into component. Fixes styling for HorizontalWrapper

---
+++ apps/modernization-ui/src/apps/patient/profile/generalInfo/GeneralInformationEntry.ts
@@ -0,0 +1,12 @@
+export type GeneralInformationEntry = {
+    asOf: string | null;
+    maritalStatus: string | null;
+    maternalMaidenName: string | null;
+    adultsInHouse: number | null;
+    childrenInHouse: number | null;
+    occupation: string | null;
+    educationLevel: string | null;
+    primaryLanguage: string | null;
+    speaksEnglish: string | null;
+    stateHIVCase: string | null;
+};
---
+++ apps/modernization-ui/src/apps/patient/profile/mortality/MortalityEntry.ts
@@ -0,0 +1,9 @@
+export type MortalityEntry = {
+    asOf: string | null;
+    deceased: string | null;
+    deceasedOn: string | null;
+    city: string | null;
+    state: string | null;
+    county: string | null;
+    country: string | null;
+};
