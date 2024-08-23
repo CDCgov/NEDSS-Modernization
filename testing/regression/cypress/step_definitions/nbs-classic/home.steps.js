faec641cd96c1c051682706b13666d45d06f1dcc
Lakshmi Narayanan Ramakrishnan <lramakrishnan@enquizit.com>
2024-08-23 12:13:56 -0700

    CNT-57 CNQAT-127 Classic NBS Merge Patients

--- testing/regression/cypress/step_definitions/nbs-classic/home.steps.js
+++ testing/regression/cypress/step_definitions/nbs-classic/home.steps.js
@@ -32,3 +32,36 @@ Then("Verify Add Lab Report page displayed", () => {
 Then("Click each of the following {string}", (queueName) => {
     classicHomePage.clickDefaultQueue(queueName)
 });
+
+Then("Create two users with same firstname and last name", () => {
+    classicHomePage.createTwoPatients()
+});
+
+Then("Click on Marge Patients tab on upper left side", () => {
+    classicHomePage.clickMergePatientTab()
+});
+
+Then("Click on Manual Search tab", () => {
+    classicHomePage.clickOnManualSearch()
+});
+
+Then("Verify user navigated to Find Patient page", () => {
+    classicHomePage.verifyFindPatientPage()
+});
+
+Then("Search user to manual merge", () => {
+    classicHomePage.searchUser()
+});
+
+Then("Select users and merge", () => {
+    classicHomePage.selectUsersToMerge()
+});
+
+Then("Click on System Identified tab", () => {
+    classicHomePage.clickSystemIdentifiedTab()
+});
+
+Then("Verify Merge Candidate List is displayed to user", () => {
+    classicHomePage.verifyMergeCandidateListDisplayed()
+});
+
