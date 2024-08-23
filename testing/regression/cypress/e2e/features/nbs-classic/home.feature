3c4fd359666d5d9822a1675934fee7895a211b24
Lakshmi Narayanan Ramakrishnan <132704359+lramakr@users.noreply.github.com>
2024-08-23 10:36:19 -0700

    CNT-57 CNQAT-121 Classic NBS Search Page Add new lab report (#1726)

--- testing/regression/cypress/e2e/features/nbs-classic/home.feature
+++ testing/regression/cypress/e2e/features/nbs-classic/home.feature
@@ -8,3 +8,11 @@ Feature: User can view data in classic NBS Home
     And Enter Last Name text box input "Smith"
     Then Click on Search in Patient Search pane
 
+  Scenario: Add new lab report
+    Then Navigate to Patient Search pane
+    And Enter First Name text box input "A"
+    Then Click on Search in Patient Search pane
+    And Click on Add New button in patient Search pane
+    Then Click on Add new lab report in patient Search pane
+    And Verify Add Lab Report page displayed
+
--- testing/regression/cypress/step_definitions/nbs-classic/home.steps.js
+++ testing/regression/cypress/step_definitions/nbs-classic/home.steps.js
@@ -13,3 +13,18 @@ Then("Click on Search in Patient Search pane", () => {
     classicHomePage.clickSearchBtnInPatientSearchPane()
 });
 
+Then("Enter First Name text box input {string}", (text) => {
+    classicHomePage.enterFirstName(text)
+});
+
+Then("Click on Add New button in patient Search pane", () => {
+    classicHomePage.clickAddNewBtnInPatientSearchPane()
+});
+
+Then("Click on Add new lab report in patient Search pane", () => {
+    classicHomePage.clickAddNewLabReportBtnInPatientSearchPane()
+});
+
+Then("Verify Add Lab Report page displayed", () => {
+    classicHomePage.verifyAddLabReport()
+});
