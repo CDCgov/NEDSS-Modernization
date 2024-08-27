d6765ac36ba31f7942e8f8aba1e813fbcd59ecfc
Sheham Izat <sizat@enquizit.com>
2024-08-27 17:37:25 -0400

    revert changes on build_classic.sh file

--- cdc-sandbox/build_classic.sh
+++ cdc-sandbox/build_classic.sh
@@ -3,20 +3,19 @@ set -e
 
 BASE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-#CLASSIC_PATH=$BASE/nbs-classic/builder/NEDSSDev
-CLASSIC_PATH=C:/dev/NEDSS/NEDSSDev-NBS_6.0.15
+CLASSIC_PATH=$BASE/nbs-classic/builder/NEDSSDev
 CLASSIC_VERSION=NBS_6.0.15
 
 # Clone NEDSSDev
-#rm -rf $CLASSIC_PATH
-#git clone -b $CLASSIC_VERSION git@github.com:cdcent/NEDSSDev.git $CLASSIC_PATH
+rm -rf $CLASSIC_PATH
+git clone -b $CLASSIC_VERSION git@github.com:cdcent/NEDSSDev.git $CLASSIC_PATH
 
 # Build and deploy database and wildfly containers
 echo "Building SQL Server database and WildFly"
 docker-compose -f $BASE/docker-compose.yml up nbs-mssql wildfly --build -d
 
-# Cleanup 
-#rm -rf $CLASSIC_PATH
+# Cleanup
+rm -rf $CLASSIC_PATH
 
 echo "**** Classic build complete ****"
 echo "http://localhost:7001/nbs/login"
