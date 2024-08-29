ec487f57576083dca4baf8d1a3544815ad692be0
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-08-29 13:43:14 -0500

    CNFT2-2763 Data elements table page setup

---
+++ apps/modernization-ui/src/apps/dedup-config/const/init.ts
@@ -0,0 +1,19 @@
+export const DataElements = {
+    "lastName": "",
+    "secondLastName": "",
+    "firstName": "",
+    "middleName": "",
+    "secondMiddleName": "",
+    "suffix": "",
+    "currentSex": "",
+    "dateOfBirth": "",
+    "ssn": "",
+    "idType": "",
+    "idAssigningAuthority": "",
+    "idValue": "",
+    "streetAddress1": "",
+    "city": "",
+    "state": "",
+    "zipCode": "",
+    "telephone": ""
+  };
