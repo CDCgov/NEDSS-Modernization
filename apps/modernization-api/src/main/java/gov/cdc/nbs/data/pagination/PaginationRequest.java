539bd536c97db9c37cf6cdd529c00d2f6db8038c
Adam Loup <aloup@enquizit.com>
2024-08-30 08:37:23 -0500

    sorting ui integraiton

--- apps/modernization-api/src/main/java/gov/cdc/nbs/data/pagination/PaginationRequest.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/data/pagination/PaginationRequest.java
@@ -1,10 +1,15 @@
 package gov.cdc.nbs.data.pagination;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.springframework.data.domain.Sort.Direction;
 
 public record PaginationRequest(Integer pageSize, Integer pageNumber, Sort sort) {
 
-  public record Sort (String property, Direction direction){}
+  public record Sort (
+      String property,
+      @JsonDeserialize(using = FlexibleDirectionJsonDeserializer.class)
+      Direction direction
+  ){}
 
 
 }
--- apps/modernization-ui/src/apps/search/investigation/result/table/InvestigationSearchResultsTable.tsx
+++ apps/modernization-ui/src/apps/search/investigation/result/table/InvestigationSearchResultsTable.tsx
@@ -18,11 +18,11 @@ const SEX = { id: 'sex', name: 'Sex' };
 const PATIENT_ID = { id: 'id', name: 'Patient ID' };
 const CONDITION = { id: 'condition', name: 'Condition' };
 const START_DATE = { id: 'startDate', name: 'Start date' };
-const JURSIDICTION = { id: 'jurisdition', name: 'Jurisdiction' };
+const JURSIDICTION = { id: 'jurisdiction', name: 'Jurisdiction' };
 const INVESTIGATOR = { id: 'investigator', name: 'Investigator' };
 const INVESTIGATION_ID = { id: 'investigationId', name: 'Investigation ID' };
 const STATUS = { id: 'status', name: 'Status' };
-const NOTIFICATION = { id: 'notificaiton', name: 'Notification' };
+const NOTIFICATION = { id: 'notification', name: 'Notification' };
 
 const columns = (notificationStatusResolver: SelectableResolver): Column<Investigation>[] => [
     {
