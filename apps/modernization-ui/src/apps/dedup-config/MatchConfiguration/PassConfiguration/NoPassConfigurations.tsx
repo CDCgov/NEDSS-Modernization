c3ae40c440ac8ef7d08a6cda8149a37450573d7b
Richard <rkrussel@enquizit.com>
2024-09-04 09:52:27 -0500

    remaining styling

--- apps/modernization-ui/src/apps/dedup-config/MatchConfiguration/PassConfiguration/NoPassConfigurations.tsx
+++ apps/modernization-ui/src/apps/dedup-config/MatchConfiguration/PassConfiguration/NoPassConfigurations.tsx
@@ -1,7 +1,14 @@
 import styles from './NoPassConfigurations.module.scss';
 
 const NoPassConfigurations = () => {
-    return <div className={styles.content}>No pass configurations have been created</div>;
+    return (
+        <div className={styles.content}>
+            <h3 className={styles.heading}>No pass configurations have been created</h3>
+            <p className={styles.text}>
+                To get started, select <span>"Add pass configuration"</span> from the left panel.
+            </p>
+        </div>
+    );
 };
 
 export default NoPassConfigurations;
