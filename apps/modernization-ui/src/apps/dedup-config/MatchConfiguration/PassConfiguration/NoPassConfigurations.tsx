6029d3ead9140dec40c28b4711bef13a0c9b96f1
Richard <rkrussel@enquizit.com>
2024-09-04 09:24:23 -0500

    init

---
+++ apps/modernization-ui/src/apps/dedup-config/MatchConfiguration/PassConfiguration/NoPassConfigurations.tsx
@@ -0,0 +1,7 @@
+import styles from './NoPassConfigurations.module.scss';
+
+const NoPassConfigurations = () => {
+    return <div className={styles.content}>No pass configurations have been created</div>;
+};
+
+export default NoPassConfigurations;
