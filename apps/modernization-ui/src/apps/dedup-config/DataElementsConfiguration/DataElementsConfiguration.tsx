91a55e4cf17497d68743424c19240ce900507acb
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-09-04 12:26:02 -0500

    styling (#1762)

--- apps/modernization-ui/src/apps/dedup-config/DataElementsConfiguration/DataElementsConfiguration.tsx
+++ apps/modernization-ui/src/apps/dedup-config/DataElementsConfiguration/DataElementsConfiguration.tsx
@@ -1,19 +1,92 @@
 import { Button, Icon } from '@trussworks/react-uswds';
 import { useDataElementsContext } from '../context/DataElementsContext';
 import { useDedupeContext } from '../context/DedupeContext';
+import styles from './data-elements-configuration.module.scss';
+import { Input } from 'components/FormInputs/Input';
 
 const DataElementsConfiguration = () => {
     const { dataElements } = useDataElementsContext();
     const { setMode } = useDedupeContext();
     console.log(dataElements);
     return (
-        <div>
-            <div>
+        <div className={styles.dataElements}>
+            <div className={styles.header}>
                 <h2>
                     <Icon.ArrowBack onClick={() => setMode('patient')} /> Data elements configuration
                 </h2>
-                <Button unstyled type="button" onClick={() => setMode('patient')}>
-                    <Icon.Settings /> Patient match configuration
+            </div>
+            <div className={styles.body}>
+                <div className={styles.sections}>
+                    <div className={styles.section}>
+                        <div className={styles.sectionHeader}>
+                            <h4>Global settings</h4>
+                        </div>
+                        <div className={styles.sectionBody}>
+                            <Input
+                                type="text"
+                                label="Belongingness ratio"
+                                defaultValue="0.5"
+                                orientation="horizontal"
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.section}>
+                        <div className={styles.table}>
+                            <div className={styles.sectionHeader}>
+                                <h4>Data elements</h4>
+                            </div>
+                            <div className={styles.sectionBody}>Table data</div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.sidebar}>
+                    <h3>How to </h3>
+                    <h4>Belongingness ratio</h4>
+                    <p>
+                        The belongingness ratio of a new record to an existing cluster of data is the percentage of
+                        records already belonging to that cluster that the new record is a match with.
+                    </p>
+                    <h4>Data elements</h4>
+                    <p>
+                        This table represents all the possible data elements that are available to the patient match
+                        algorithm.
+                    </p>
+                    <ol>
+                        <li>
+                            Checking a data element will make it available to be selected during pass configuration.
+                        </li>
+                        <li>
+                            Once checked, the data element requires the M, U and Threshold values to be set before
+                            saving.
+                        </li>
+                    </ol>
+                    <ul>
+                        <li>
+                            <span>M -</span> Probability that a given field is the same in a pair of matching records.
+                        </li>
+                        <li>
+                            <span>U -</span> Probability that a given field is the same in a pair of non-matching
+                            records
+                        </li>
+                        <li>
+                            <span>Threshold -</span> Values above which two strings are said to be “similar enough” that
+                            they’re probably the same thing. Values that are less than the threshold will be calculated
+                            as 0.
+                        </li>
+                    </ul>
+                    <h4>Log odds</h4>
+                    <p>
+                        A score of the likelihood that something observed in a data set is due to a statistically
+                        meaningful relationship or cause, rather than random chance. Calculated based on odds ratio.
+                    </p>
+                </div>
+            </div>
+            <div className={styles.footer}>
+                <Button type="button" outline onClick={() => setMode('patient')}>
+                    Cancel
+                </Button>
+                <Button type="button" onClick={() => console.log('save')} disabled>
+                    Save data elements configuration
                 </Button>
             </div>
         </div>
