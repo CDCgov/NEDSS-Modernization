6b3b56c6b90769901edfd02d86b8ee54bd219b65
Richard Krussel <139483220+rkrusselenq@users.noreply.github.com>
2024-09-05 10:35:57 -0500

    Empty layouts for dedup (#1767)
    * init
    
    * remaining styling
    
    * fix conflicts

--- apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration.tsx
+++ apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration.tsx
@@ -2,9 +2,17 @@ import styles from './PatientMatchConfig.module.scss';
 import { Button, Icon } from '@trussworks/react-uswds';
 import DataElementsConfiguration from './DataElementsConfiguration/DataElementsConfiguration';
 import { useDedupeContext } from './context/DedupeContext';
+import { useDataElementsContext } from './context/DataElementsContext';
+import NoDataElements from './NoDataElementsContent';
+import MatchConfiguration from './MatchConfiguration/MatchConfiguration';
 
 const PatientMatchConfiguration = () => {
     const { mode, setMode } = useDedupeContext();
+    const { dataElements } = useDataElementsContext();
+
+    const handleSetDataMode = () => {
+        setMode('data');
+    };
 
     return (
         <div>
@@ -14,15 +22,11 @@ const PatientMatchConfiguration = () => {
                 <>
                     <div className={styles.header}>
                         <h2>Patient match configuration</h2>
-                        <Button
-                            unstyled
-                            type="button"
-                            className={styles.dataConfigButton}
-                            onClick={() => setMode('data')}>
+                        <Button unstyled type="button" className={styles.dataConfigButton} onClick={handleSetDataMode}>
                             <Icon.Settings size={3} /> Data elements configuration
                         </Button>
                     </div>
-                    <div className={styles.content}>CONTENT</div>
+                    {!dataElements ? <NoDataElements onConfigClick={handleSetDataMode} /> : <MatchConfiguration />}
                 </>
             )}
         </div>
