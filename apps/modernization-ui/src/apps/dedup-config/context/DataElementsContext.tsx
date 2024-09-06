ff41fe6d8484dc3b448cf97ee5ba7dc7336d7fcd
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-09-06 13:40:08 -0500

    Data Elements Table rows (#1776)

--- apps/modernization-ui/src/apps/dedup-config/context/DataElementsContext.tsx
+++ apps/modernization-ui/src/apps/dedup-config/context/DataElementsContext.tsx
@@ -3,29 +3,97 @@ import React, { createContext, useContext, useState } from 'react';
 const DataElementsContext = createContext<DataElementsContextProps | undefined>(undefined);
 
 type DataElementsContextProps = {
-    dataElements: typeof DataElements | null;
-    setDataElements: (dataElements: typeof DataElements) => void;
+    dataElements: DataElement[];
+    setDataElements: (dataElements: DataElement[]) => void;
 };
 
-const DataElements = {
-    lastName: '',
-    secondLastName: '',
-    firstName: '',
-    middleName: '',
-    secondMiddleName: '',
-    suffix: '',
-    currentSex: '',
-    dateOfBirth: '',
-    ssn: '',
-    idType: '',
-    idAssigningAuthority: '',
-    idValue: '',
-    streetAddress1: '',
-    city: '',
-    state: '',
-    zipCode: '',
-    telephone: ''
+export type DataElement = {
+    name: string;
+    active: boolean;
+    m: number;
+    u: number;
+    threshold: number;
 };
+
+export const DataElements: DataElement[] = [
+    { name: 'lastName', active: false, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'secondLastName', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'firstName', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'middleName', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'secondMiddleName', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'suffix', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'currentSex', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    {
+        name: 'dateOfBirth',
+        active: true,
+        m: 0.5,
+        u: 0.5,
+        threshold: 0.5
+    },
+    {
+        name: 'ssn',
+        active: true,
+        m: 0.5,
+        u: 0.5,
+        threshold: 0.5
+    },
+    {
+        name: 'idType',
+        active: true,
+        m: 0.5,
+        u: 0.25,
+        threshold: 0.5
+    },
+    {
+        name: 'idAssigningAuthority',
+        active: true,
+        m: 0.5,
+        u: 0.25,
+        threshold: 0.5
+    },
+    {
+        name: 'idValue',
+        active: true,
+        m: 0.5,
+        u: 0.25,
+        threshold: 0.5
+    },
+    {
+        name: 'streetAddress1',
+        active: true,
+        m: 0.5,
+        u: 0.25,
+        threshold: 0.5
+    },
+    {
+        name: 'city',
+        active: true,
+        m: 0.5,
+        u: 0.25,
+        threshold: 0.5
+    },
+    {
+        name: 'state',
+        active: true,
+        m: 0.5,
+        u: 0.25,
+        threshold: 0.5
+    },
+    {
+        name: 'zip',
+        active: true,
+        m: 0.5,
+        u: 0.25,
+        threshold: 0.5
+    },
+    {
+        name: 'telephone',
+        active: true,
+        m: 0.5,
+        u: 0.1,
+        threshold: 0.5
+    }
+];
 
 const DataElementsContextProvider: React.FC<{
     children: React.ReactNode;
