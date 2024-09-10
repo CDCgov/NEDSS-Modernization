e8d5acdd6f10d0ca65dd9a7125ffea3e8c4da241
Richard <rkrussel@enquizit.com>
2024-09-10 11:46:58 -0500

    init

--- apps/modernization-ui/src/apps/dedup-config/MatchConfiguration/PassConfiguration/PassConfiguration.tsx
+++ apps/modernization-ui/src/apps/dedup-config/MatchConfiguration/PassConfiguration/PassConfiguration.tsx
@@ -1,11 +1,38 @@
+import { Button, Icon } from '@trussworks/react-uswds';
 import styles from './PassConfiguration.module.scss';
 
 const PassConfiguration = () => {
     return (
         <div className={styles.configurationDetails}>
-            <div className={styles.criteria}>Blocking criteria</div>
-            <div className={styles.criteria}>Matching criteria</div>
-            <div className={styles.matchingBounds}>Matching bounds</div>
+            <div className={styles.criteria}>
+                <div className={styles.criteriaHeadingContainer}>
+                    <h4>Blocking criteria</h4>
+                    <span>Include records that meet all these conditions</span>
+                </div>
+                <div className={styles.criteriaContentContainer}>
+                    <p className={styles.criteriaRequest}>Please add blocking criteria to get started</p>
+                    <Button unstyled type={'button'}>
+                        <Icon.Add />
+                        Add blocking criteria
+                    </Button>
+                </div>
+            </div>
+            <div className={`${styles.criteria} ${styles.disabled}`}>
+                <div className={styles.criteriaHeadingContainer}>
+                    <h4>Matching criteria</h4>
+                    <span>Include records that meet all these conditions</span>
+                </div>
+                <div className={styles.criteriaContentContainer}>
+                    <p className={styles.criteriaRequest}>Please add a matching attribute to continue</p>
+                    <Button unstyled type={'button'} disabled>
+                        <Icon.Add />
+                        Add matching criteria
+                    </Button>
+                </div>
+            </div>
+            <div className={styles.matchingBounds}>
+                <h4>Matching bounds</h4>
+            </div>
         </div>
     );
 };
