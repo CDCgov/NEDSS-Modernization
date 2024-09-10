9df9dbef9ced0485474c065a1fbdbba67328ca39
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-09-10 13:49:16 -0500

    CNFT2-2767 Error tooltips (#1786)

--- apps/modernization-ui/src/components/Entry/VerticalEntryWrapper.tsx
+++ apps/modernization-ui/src/components/Entry/VerticalEntryWrapper.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react';
 import classNames from 'classnames';
 import { ErrorMessage, Label } from '@trussworks/react-uswds';
+import { Tooltip, Icon } from '@trussworks/react-uswds';
+import styles from './vertical-entry-wrapper.module.scss';
 
 type Props = {
     className?: string;
@@ -8,17 +10,22 @@ type Props = {
     label: string;
     error?: string;
     required?: boolean;
+    tooltipDirection?: 'top' | 'left' | 'right' | 'bottom';
     children: ReactNode;
 };
-
-const VerticalEntryWrapper = ({ className, htmlFor, label, required, error, children }: Props) => (
-    <span className={className}>
+const VerticalEntryWrapper = ({ className, htmlFor, label, required, error, tooltipDirection, children }: Props) => (
+    <span className={classNames(styles.wrapper, className)}>
         {label && (
             <Label className={classNames({ required })} htmlFor={htmlFor}>
                 {label}
             </Label>
         )}
-        {error && <ErrorMessage id={`${error}-message`}>{error}</ErrorMessage>}
+        {tooltipDirection && error && (
+            <Tooltip id={htmlFor} label={error} className={classNames(styles.tooltip)} position={tooltipDirection}>
+                <Icon.ErrorOutline className={styles.tooltipIcon} />
+            </Tooltip>
+        )}
+        {!tooltipDirection && error && <>{error && <ErrorMessage id={`${error}-message`}>{error}</ErrorMessage>}</>}
         {children}
     </span>
 );
