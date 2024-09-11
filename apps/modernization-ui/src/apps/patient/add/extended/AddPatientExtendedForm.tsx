8cc7c0596f00fdcd1e1513ece17d4ca1745af9cf
Michael Peels <michaelpeels@Michael-Peels.local>
2024-09-11 16:13:04 -0400

    WIP

--- apps/modernization-ui/src/apps/patient/add/extended/AddPatientExtendedForm.tsx
+++ apps/modernization-ui/src/apps/patient/add/extended/AddPatientExtendedForm.tsx
@@ -4,23 +4,33 @@ import { PhoneAndEmailMultiEntry } from './inputs/PhoneAndEmailMultiEntry';
 import { PhoneEmailFields } from 'apps/patient/profile/phoneEmail/PhoneEmailEntry';
 import { useState } from 'react';
 import { AddPatientExtendedNav } from './nav/AddPatientExtendedNav';
+import { AddressMultiEntry } from './inputs/address/AddressMultiEntry';
+import { AddressFields } from 'apps/patient/profile/addresses/AddressEntry';
 
 type ExtendedPatientCreationForm = {
+    address: AddressFields[];
     phone: PhoneEmailFields[];
 };
 
 type DirtyState = {
+    address: boolean;
     phone: boolean;
 };
 export const AddPatientExtendedForm = () => {
     const form = useForm<ExtendedPatientCreationForm>({ defaultValues: { phone: [] } });
-    const [dirtyState, setDirtyState] = useState<DirtyState>({ phone: false });
+    const [dirtyState, setDirtyState] = useState<DirtyState>({ address: false, phone: false });
 
     return (
         <>
             <div className={styles.addPatientForm}>
                 <FormProvider {...form}>
                     <div className={styles.formContent}>
+                        <AddressMultiEntry
+                            isDirty={(isDirty) => setDirtyState({ ...dirtyState, address: isDirty })}
+                            onChange={(addressData) => {
+                                form.setValue('address', addressData);
+                            }}
+                        />
                         <PhoneAndEmailMultiEntry
                             isDirty={(isDirty) => setDirtyState({ ...dirtyState, phone: isDirty })}
                             onChange={(phoneEmailData) => {
