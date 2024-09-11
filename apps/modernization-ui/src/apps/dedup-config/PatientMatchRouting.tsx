90e225c6fda1b3e7a58be57c98c2cad83468b241
Richard <rkrussel@enquizit.com>
2024-09-11 14:11:20 -0500

    fix merge

--- apps/modernization-ui/src/apps/dedup-config/PatientMatchRouting.tsx
+++ apps/modernization-ui/src/apps/dedup-config/PatientMatchRouting.tsx
@@ -1,23 +1,13 @@
 import PatientMatchConfiguration from './PatientMatchConfiguration';
-<<<<<<< HEAD
-import PatientMatchContextProvider from './context/PatientMatchContext';
-=======
 import DedupeContextProvider from './context/DedupeContext';
->>>>>>> case-dedup-ui
 
 const routing = [
     {
         path: '/patient-match-config',
         element: (
-<<<<<<< HEAD
-            <PatientMatchContextProvider>
-                <PatientMatchConfiguration />
-            </PatientMatchContextProvider>
-=======
             <DedupeContextProvider>
                 <PatientMatchConfiguration />
             </DedupeContextProvider>
->>>>>>> case-dedup-ui
         )
     }
 ];
