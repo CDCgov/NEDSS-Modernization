25561576d3cffb12851f787fbab997a0343e6fab
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-09-12 13:10:17 -0500

    CNFT2-2771 Blocking criteria rows (#1810)

--- apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/BlockingCriteria/BlockingCriteriaRow.tsx
+++ apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/BlockingCriteria/BlockingCriteriaRow.tsx
@@ -1,42 +1,45 @@
-// import { BlockingCriteria, usePatientMatchContext } from "apps/dedup-config/context/PatientMatchContext";
-// import { SingleSelect } from "design-system/select";
-// import { Icon } from "@trussworks/react-uswds";
-// import { availableMethods } from "apps/dedup-config/context/PatientMatchContext";
-// import { Controller } from "react-hook-form";
-// import React from "react";
+import { BlockingCriteria } from 'apps/dedup-config/context/PatientMatchContext';
+import { SingleSelect } from 'design-system/select';
+import { Icon } from '@trussworks/react-uswds';
+import { availableMethods } from 'apps/dedup-config/context/PatientMatchContext';
+import { Controller, useFormContext } from 'react-hook-form';
+import styles from './blocking-criteria-row.module.scss';
+import { usePatientMatchContext } from 'apps/dedup-config/context/PatientMatchContext';
 
-// export const BlockingCriteriaRow = ({ criteria }: { criteria: BlockingCriteria }) => {
-//     const options = availableMethods.map((method, index) => ({
-//         name: method.name,
-//         label: method.name,
-//         value: method.value,
-//         id: index
-//     }));
-//     const {blockingCriteria, setBlockingCriteria} = usePatientMatchContext();
-//     return (
-//         <div>
-//             {criteria.field.label}
-//             <Controller
-//                 control={form.control}
-//                 name={`blockingCriteria.${criteria.field.name}.method`}
-//                 render={({ field: { onChange, value, name } }) => (
-//                     <SingleSelect
-//                         id={`method-select-${criteria.field.name}`}
-//                         label="Select method"
-//                         options={options}
-//                         value={options[0]}
-//                         onChange={(newMethod) => {
-//                             const updatedCriteria = blockingCriteria.map((item) =>
-//                                 item.field.name === criteria.field.name
-//                                     ? { ...item, method: newMethod }
-//                                     : item
-//                             );
-//                             setBlockingCriteria(updatedCriteria);
-//                         }}
-//                     />
-//                 )}
-//             />
-//             <Icon.Delete size={3} />
-//         </div>
-//     );
-// };
+export const BlockingCriteriaRow = ({ criteria }: { criteria: BlockingCriteria }) => {
+    const { removeBlockingCriteria } = usePatientMatchContext();
+    const form = useFormContext<any>();
+    const options = availableMethods.map((method, index) => ({
+        name: method.name,
+        label: method.name,
+        value: method.value,
+        id: index
+    }));
+    console.log(criteria);
+    return (
+        <div className={styles.row}>
+            <h5>{criteria.field.label}</h5>
+            <Controller
+                control={form.control}
+                defaultValue={criteria.method.value}
+                name={`blockingCriteria.${criteria.field.name}.method`}
+                render={({ field: { onChange, value, name } }) => (
+                    <SingleSelect
+                        id={`method-select-${criteria.field.name}`}
+                        name={name}
+                        label="Method"
+                        options={options}
+                        value={options.find((option) => option.value === value) || value}
+                        onChange={onChange}
+                        orientation="horizontal"
+                        size={1}
+                        sizing="compact"
+                    />
+                )}
+            />
+            <h5 className={styles.delete} onClick={() => removeBlockingCriteria(criteria.field.name)}>
+                <Icon.Delete size={3} /> Remove
+            </h5>
+        </div>
+    );
+};
--- apps/modernization-ui/src/apps/dedup-config/const/init.ts
+++ apps/modernization-ui/src/apps/dedup-config/const/init.ts
@@ -1,5 +1,6 @@
 export type DataElement = {
     name: string;
+    label: string;
     active: boolean;
     m: number;
     u: number;
@@ -7,81 +8,22 @@ export type DataElement = {
 };
 
 export const dataElements: DataElement[] = [
-    { name: 'lastName', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
-    { name: 'secondLastName', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
-    { name: 'firstName', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
-    { name: 'middleName', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
-    { name: 'secondMiddleName', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
-    { name: 'suffix', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
-    { name: 'currentSex', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
-    {
-        name: 'dateOfBirth',
-        active: true,
-        m: 0.5,
-        u: 0.5,
-        threshold: 0.5
-    },
-    {
-        name: 'ssn',
-        active: true,
-        m: 0.5,
-        u: 0.5,
-        threshold: 0.5
-    },
-    {
-        name: 'idType',
-        active: true,
-        m: 0.5,
-        u: 0.25,
-        threshold: 0.5
-    },
-    {
-        name: 'idAssigningAuthority',
-        active: true,
-        m: 0.5,
-        u: 0.25,
-        threshold: 0.5
-    },
-    {
-        name: 'idValue',
-        active: true,
-        m: 0.5,
-        u: 0.25,
-        threshold: 0.5
-    },
-    {
-        name: 'streetAddress1',
-        active: true,
-        m: 0.5,
-        u: 0.25,
-        threshold: 0.5
-    },
-    {
-        name: 'city',
-        active: true,
-        m: 0.5,
-        u: 0.25,
-        threshold: 0.5
-    },
-    {
-        name: 'state',
-        active: true,
-        m: 0.5,
-        u: 0.25,
-        threshold: 0.5
-    },
-    {
-        name: 'zip',
-        active: true,
-        m: 0.5,
-        u: 0.25,
-        threshold: 0.5
-    },
-    {
-        name: 'telephone',
-        active: true,
-        m: 0.5,
-        u: 0.1,
-        threshold: 0.5
-    }
+    { name: 'lastName', label: 'Last name', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'secondLastName', label: 'Second last name', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'firstName', label: 'First name', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'middleName', label: 'Middle name', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'secondMiddleName', label: 'Second middle name', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'suffix', label: 'Suffix', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'currentSex', label: 'Current sex', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'dateOfBirth', label: 'Date of birth', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'ssn', label: 'Social security number', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'idType', label: 'ID type', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'idAssigningAuthority', label: 'ID assigning authority', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'idValue', label: 'ID value', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'streetAddress1', label: 'Street address 1', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'streetAddress2', label: 'Street address 2', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'city', label: 'City', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'state', label: 'State', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'zip', label: 'Zip code', active: true, m: 0.5, u: 0.5, threshold: 0.5 },
+    { name: 'telephone', label: 'Telephone', active: true, m: 0.5, u: 0.1, threshold: 0.5 }
 ];
