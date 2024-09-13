bd108b3041626e986ceb8a8a781a8a500e429465
Richard <rkrussel@enquizit.com>
2024-09-13 10:57:45 -0500

    init

--- apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/PatientMatchConfigurationPage/PatientMatchConfigurationPage.tsx
+++ apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/PatientMatchConfigurationPage/PatientMatchConfigurationPage.tsx
@@ -2,33 +2,54 @@ import { Button, Icon } from '@trussworks/react-uswds';
 import styles from './patient-match-configuration-page.module.scss';
 import NoPassConfigurations from '../PassConfiguration/NoPassConfigurations';
 import PassConfigurationListItem from '../PassConfiguration/PassConfigurationListItem';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PatientMatchForm from './PatientMatchForm';
 import { PassConfiguration } from 'apps/dedup-config/types';
+import { usePatientMatchContext } from 'apps/dedup-config/context/PatientMatchContext';
 
 const PatientMatchConfigurationPage = () => {
     const [configurations, setConfigurations] = useState<PassConfiguration[]>([]);
     const [selectedConfigurationIndex, setSelectedConfigurationIndex] = useState<number | null>(null);
     const [isEditingConfiguration, setIsEditingConfiguration] = useState<boolean>(false);
+    const { setBlockingCriteria } = usePatientMatchContext();
 
     const handleAddConfiguration = () => {
         const configs = [...configurations];
         configs.push({
             name: `New configuration (${configurations.length})`,
             description: 'a description will go here',
-            active: false
+            active: false,
+            blockingCriteria: [],
+            matchingCriteria: []
         });
         setConfigurations(configs);
         setSelectedConfigurationIndex(configurations.length);
         setIsEditingConfiguration(true);
+        setBlockingCriteria([]);
+        localStorage.setItem('passConfigurations', JSON.stringify(configs));
     };
 
     const handleConfigListItemClick = (index: number) => {
         setSelectedConfigurationIndex(index);
         setIsEditingConfiguration(true);
+        const selectedConfig = configurations[index];
+        if (selectedConfig.blockingCriteria?.length) {
+            setBlockingCriteria(selectedConfig.blockingCriteria);
+        } else {
+            setBlockingCriteria([]);
+        }
     };
 
     const showConfiguration = isEditingConfiguration && configurations.length;
+
+    useEffect(() => {
+        const storedConfiguration = localStorage.getItem('passConfigurations');
+        if (storedConfiguration) {
+            setConfigurations(JSON.parse(storedConfiguration));
+        } else {
+            setConfigurations([]);
+        }
+    });
 
     return (
         <div className={styles.wrapper}>
--- apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/PatientMatchConfigurationPage/PatientMatchForm.tsx
+++ apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/PatientMatchConfigurationPage/PatientMatchForm.tsx
@@ -5,12 +5,17 @@ import { MatchingBounds } from '../MatchingBounds/MatchingBounds';
 import { useForm, FormProvider, Controller } from 'react-hook-form';
 import { Toggle } from 'design-system/toggle/Toggle';
 import { Button } from 'components/button';
+import { PassConfiguration } from 'apps/dedup-config/types';
 
-const PatientMatchForm = () => {
+type Props = {
+    passConfiguration: PassConfiguration;
+};
+
+const PatientMatchForm = ({ passConfiguration }: Props) => {
     const patientMatchForm = useForm({
         mode: 'onBlur',
         defaultValues: {
-            active: true
+            ...passConfiguration
         }
     });
 
