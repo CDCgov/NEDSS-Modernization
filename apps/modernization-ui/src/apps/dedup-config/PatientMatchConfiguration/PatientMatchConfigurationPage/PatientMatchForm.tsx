7aa1cc23ae2cc24047ed1272f9b170e80c76bd2d
Richard Krussel <139483220+rkrusselenq@users.noreply.github.com>
2024-09-13 11:07:47 -0500

    Configure pass config fetch (#1815)
    * init
    
    * add saving

--- apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/PatientMatchConfigurationPage/PatientMatchForm.tsx
+++ apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/PatientMatchConfigurationPage/PatientMatchForm.tsx
@@ -5,14 +5,23 @@ import { MatchingBounds } from '../MatchingBounds/MatchingBounds';
 import { useForm, FormProvider, Controller } from 'react-hook-form';
 import { Toggle } from 'design-system/toggle/Toggle';
 import { Button } from 'components/button';
+import { PassConfiguration } from 'apps/dedup-config/types';
 
-const PatientMatchForm = () => {
+type Props = {
+    passConfiguration: PassConfiguration;
+};
+
+const PatientMatchForm = ({ passConfiguration }: Props) => {
     const patientMatchForm = useForm({
         mode: 'onBlur',
         defaultValues: {
-            active: true
+            ...passConfiguration
         }
     });
+
+    const saveConfiguration = () => {
+        localStorage.setItem('passConfigurations', JSON.stringify(patientMatchForm.getValues()));
+    };
 
     return (
         <div className={styles.form}>
@@ -43,7 +52,10 @@ const PatientMatchForm = () => {
                         <Button type="reset" outline>
                             Cancel
                         </Button>
-                        <Button type="submit" disabled={!patientMatchForm.formState.isValid}>
+                        <Button
+                            type="submit"
+                            disabled={!patientMatchForm.formState.isValid}
+                            onClick={saveConfiguration}>
                             Save pass configuration
                         </Button>
                     </div>
