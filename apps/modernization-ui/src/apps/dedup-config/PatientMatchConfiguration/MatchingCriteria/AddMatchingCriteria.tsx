937fb928a861d1b2bd90dd299c1d9950cb7a52b7
John Lee <124739504+jlee-enquizit@users.noreply.github.com>
2024-09-16 13:12:53 -0500

    CNFT2-2772 Matching Criteria section

---
+++ apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/MatchingCriteria/AddMatchingCriteria.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+import { usePatientMatchContext } from '../../context/PatientMatchContext';
+import { useDataElementsContext } from '../../context/DataElementsContext';
+import { ModalToggleButton, Checkbox } from '@trussworks/react-uswds';
+import { DataElement } from '../../types';
+import { availableMethods } from '../../context/PatientMatchContext';
+import { ModalRef } from '@trussworks/react-uswds';
+import styles from './add-matching-criteria.module.scss';
+
+type Props = {
+    matchingModalRef: React.RefObject<ModalRef>;
+};
+
+export const AddMatchingCriteria = ({ matchingModalRef }: Props) => {
+    const { dataElements } = useDataElementsContext();
+    const { blockingCriteria, matchingCriteria, setMatchingCriteria } = usePatientMatchContext();
+
+    const [selectedFields, setSelectedFields] = useState<string[]>([]);
+
+    // Sync `selectedFields` with `matchingCriteria` when the component mounts or `matchingCriteria` changes
+    useEffect(() => {
+        const selected = matchingCriteria.map((criteria) => criteria.field.name);
+        setSelectedFields(selected);
+    }, [matchingCriteria]);
+
+    const handleCheckboxChange = (fieldName: string, checked: boolean) => {
+        setSelectedFields((prev) => {
+            if (checked) {
+                return [...prev, fieldName];
+            } else {
+                return prev.filter((name) => name !== fieldName);
+            }
+        });
+    };
+
+    const addMatchingCriteria = () => {
+        if (!dataElements) {
+            return;
+        }
+
+        const newCriteria = selectedFields
+            .filter((fieldName) => !matchingCriteria.some((criteria) => criteria.field.name === fieldName))
+            .map((fieldName) => {
+                const field = dataElements.find((element) => element.name === fieldName);
+                if (!field) {
+                    return null;
+                }
+                return {
+                    field: field,
+                    method: availableMethods[0]
+                };
+            })
+            .filter((criteria) => criteria !== null);
+
+        const updatedCriteria = matchingCriteria.filter((criteria) => selectedFields.includes(criteria.field.name));
+
+        setMatchingCriteria([...updatedCriteria, ...newCriteria]);
+
+        matchingModalRef.current?.toggleModal();
+    };
+
+    return (
+        <div className={styles.addMatchingCriteria}>
+            <div className={styles.addMatchingCriteriaContent}>
+                {dataElements &&
+                    dataElements.length > 0 &&
+                    dataElements.map((dataElement: DataElement, index: number) => (
+                        <Checkbox
+                            key={index}
+                            name={dataElement.name}
+                            label={dataElement.label}
+                            id={dataElement.name + index}
+                            disabled={
+                                !dataElement.active ||
+                                blockingCriteria.some((criteria) => criteria.field.name === dataElement.name)
+                            }
+                            tile
+                            checked={selectedFields.includes(dataElement.name)} // Locally track selected checkboxes
+                            onChange={(e) => handleCheckboxChange(dataElement.name, e.target.checked)} // Update local selectedFields state only
+                        />
+                    ))}
+            </div>
+            <div className={styles.addMatchingCriteriaFooter}>
+                <ModalToggleButton type="button" closer outline modalRef={matchingModalRef}>
+                    Cancel
+                </ModalToggleButton>
+                <ModalToggleButton type="button" onClick={addMatchingCriteria} closer modalRef={matchingModalRef}>
+                    Add matching criteria
+                </ModalToggleButton>
+            </div>
+        </div>
+    );
+};
---
+++ apps/modernization-ui/src/apps/dedup-config/PatientMatchConfiguration/MatchingCriteria/MatchingCriteriaSection.tsx
@@ -0,0 +1,59 @@
+import { ModalToggleButton, Icon } from '@trussworks/react-uswds';
+import styles from '../PatientMatchConfigurationPage/patient-match-form.module.scss';
+import { usePatientMatchContext } from '../../context/PatientMatchContext';
+import { ModalRef } from '@trussworks/react-uswds';
+import { useEffect, useRef, useState } from 'react';
+import { ModalComponent } from 'components/ModalComponent/ModalComponent';
+import { AddMatchingCriteria } from './AddMatchingCriteria';
+import { MatchingCriteriaRow } from './MatchingCriteriaRow';
+
+export const MatchingCriteriaSection = () => {
+    const matchingModalRef = useRef<ModalRef>(null);
+    const { blockingCriteria, matchingCriteria } = usePatientMatchContext();
+    const [logOddsSum, setLogOddsSum] = useState<number>(0);
+
+    useEffect(() => {
+        // Sum up the logOdds from each criteria.form.logOdds
+        const sum = matchingCriteria
+            .map((criteria) => criteria.field.logOdds || 0) // Ensure logOdds exists, fallback to 0 if not
+            .reduce((acc, logOdds) => acc + logOdds, 0); // Sum the logOdds
+
+        // Set the final log odds sum rounded to two decimal places
+        setLogOddsSum(sum); // Round final sum to 2 decimal places
+    }, [matchingCriteria]);
+
+    return (
+        <div className={`${styles.criteria} ${blockingCriteria.length < 1 ? styles.disabled : ''}`}>
+            <div className={styles.criteriaHeadingContainer}>
+                <h4>Matching criteria</h4>
+                <p>Include records that meet all these conditions</p>
+            </div>
+            <div className={styles.criteriaContentContainer}>
+                {matchingCriteria.length > 0 ? (
+                    matchingCriteria.map((criteria, index) => <MatchingCriteriaRow key={index} criteria={criteria} />)
+                ) : (
+                    <p className={styles.criteriaRequest}>Please add a matching attribute to continue</p>
+                )}
+                {matchingCriteria.length > 0 && (
+                    <div className={styles.totalOdds}>
+                        <p>Log odds total &nbsp; {logOddsSum}</p>
+                    </div>
+                )}
+                <ModalToggleButton
+                    unstyled
+                    type={'button'}
+                    modalRef={matchingModalRef}
+                    disabled={blockingCriteria.length < 1}>
+                    <Icon.Add />
+                    Add matching criteria
+                </ModalToggleButton>
+            </div>
+            <ModalComponent
+                modalRef={matchingModalRef}
+                size="medium"
+                modalHeading={'Add static element'}
+                modalBody={<AddMatchingCriteria matchingModalRef={matchingModalRef} />}
+            />
+        </div>
+    );
+};
