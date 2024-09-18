69d0f3bdd5d6af0999c460b8598813a936428524
Michael Peels <michaelpeels@Michael-Peels.local>
2024-09-18 15:32:34 -0400

    Testing

---
+++ apps/modernization-ui/src/apps/patient/add/extended/inputs/identification/IdentificationView.spec.tsx
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/react';
+import { IdentificationEntry } from 'apps/patient/data/entry';
+import { IdentificationView } from './IdentificationView';
+
+const entry: IdentificationEntry = {
+    asOf: '12/25/2020',
+    type: { name: 'Account number', value: 'AN' },
+    issuer: { name: 'Texas', value: 'TX' },
+    id: '1122'
+};
+
+describe('IdentificationView', () => {
+    it('should render label with values', () => {
+        const { getByText } = render(<IdentificationView entry={entry} />);
+        const asOf = getByText('Identification as of');
+        expect(asOf.parentElement?.children[1]).toHaveTextContent('12/25/2020');
+
+        const type = getByText('Type');
+        expect(type.parentElement?.children[1]).toHaveTextContent('Account number');
+
+        const issuer = getByText('Assigning authority');
+        expect(issuer.parentElement?.children[1]).toHaveTextContent('Texas');
+
+        const value = getByText('ID value');
+        expect(value.parentElement?.children[1]).toHaveTextContent('1122');
+    });
+
+    it('should display required for the proper inputs', () => {
+        const { getByText } = render(<IdentificationView entry={entry} />);
+        const asOf = getByText('Identification as of');
+        expect(asOf).toHaveClass('required');
+
+        const type = getByText('Type');
+        expect(type).toHaveClass('required');
+
+        const issuer = getByText('Assigning authority');
+        expect(issuer).not.toHaveClass('required');
+
+        const value = getByText('ID value');
+        expect(value).toHaveClass('required');
+    });
+});
--- apps/modernization-ui/src/apps/patient/add/extended/inputs/identification/IdentificationView.tsx
+++ apps/modernization-ui/src/apps/patient/add/extended/inputs/identification/IdentificationView.tsx
@@ -9,8 +9,8 @@ export const IdentificationView = ({ entry }: Props) => {
         <>
             <DataDisplay title="Identification as of" value={entry.asOf} required />
             <DataDisplay title="Type" value={entry.type.name} required />
-            <DataDisplay title="Assigning authority" value={entry.issuer?.name} required />
-            <DataDisplay title="ID value" value={entry.id} />
+            <DataDisplay title="Assigning authority" value={entry.issuer?.name} />
+            <DataDisplay title="ID value" value={entry.id} required />
         </>
     );
 };
