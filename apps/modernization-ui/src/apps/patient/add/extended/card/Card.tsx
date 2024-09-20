96ebb606b581705e07350cfd20052087961c5e9d
Michael Peels <109251240+mpeels@users.noreply.github.com>
2024-09-20 17:07:08 -0400

    Use real header elements. Fix No Data in table. Adjust card (#1840)
    * Use real header elements. Fix No Data in table. Adjust card
    
    * Change name
    
    * Fix case
    
    * Remove h6, format _headers.scss file
    
    ---------
    
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels.local>

--- apps/modernization-ui/src/apps/patient/add/extended/card/Card.tsx
+++ apps/modernization-ui/src/apps/patient/add/extended/card/Card.tsx
@@ -1,12 +1,18 @@
-import { Heading } from 'components/heading';
+import { Heading, HeadingLevel } from 'components/heading';
 import { ReactNode } from 'react';
-import styles from './Card.module.scss';
+import styles from './card.module.scss';
 
-export const Card = ({ id, title, children }: { id: string; title: string; children: ReactNode }) => {
+type Props = {
+    id: string;
+    title: ReactNode;
+    level?: HeadingLevel;
+    children: ReactNode;
+};
+export const Card = ({ id, title, children, level = 2 }: Props) => {
     return (
-        <section id={id} className={styles.input}>
+        <section id={id} className={styles.card}>
             <header>
-                <Heading level={2}>{title}</Heading>
+                <Heading level={level}>{title}</Heading>
             </header>
             {children}
         </section>
