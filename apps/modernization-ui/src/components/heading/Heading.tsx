4fa704df3d57f3966957af320716c95c0794d847
Michael Peels <michaelpeels@Michael-Peels.local>
2024-09-20 09:46:10 -0400

    Remove h6, format _headers.scss file

--- apps/modernization-ui/src/components/heading/Heading.tsx
+++ apps/modernization-ui/src/components/heading/Heading.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import styles from './heading.module.scss';
 import { ReactNode } from 'react';
 
-type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
+type HeadingLevel = 1 | 2 | 3 | 4 | 5;
 type HeadingProps = {
     level: HeadingLevel;
     className?: string;
@@ -21,8 +21,6 @@ const Heading = ({ level, className, children }: HeadingProps) => {
             return <h4 className={classNames(styles.heading, className)}>{children}</h4>;
         case 5:
             return <h5 className={classNames(styles.heading, className)}>{children}</h5>;
-        case 6:
-            return <h6 className={classNames(styles.heading, className)}>{children}</h6>;
     }
 };
 
