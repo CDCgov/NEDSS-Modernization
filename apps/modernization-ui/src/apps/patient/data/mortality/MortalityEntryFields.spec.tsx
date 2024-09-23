22761e3062e7e5f97b6334fd9799cb9e00f109ce
Michael Peels <michaelpeels@Michael-Peels.local>
2024-09-23 14:41:04 -0400

    testing

--- apps/modernization-ui/src/apps/patient/data/mortality/MortalityEntryFields.spec.tsx
+++ apps/modernization-ui/src/apps/patient/data/mortality/MortalityEntryFields.spec.tsx
@@ -1,3 +1,10 @@
+import { ExtendedNewPatientEntry } from 'apps/patient/add/extended';
+import { MortalityEntryFields } from './MortalityEntryFields';
+import { FormProvider, useForm } from 'react-hook-form';
+import { internalizeDate } from 'date';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 const mockLocationCodedValues = {
     states: {
         all: [{ name: 'StateName', value: '1' }]
@@ -11,3 +18,67 @@ const mockLocationCodedValues = {
 jest.mock('location/useLocationCodedValues', () => ({
     useLocationCodedValues: () => mockLocationCodedValues
 }));
+
+const Fixture = () => {
+    const form = useForm<ExtendedNewPatientEntry>({
+        mode: 'onBlur',
+        defaultValues: { mortality: { asOf: internalizeDate(new Date()) } }
+    });
+
+    return (
+        <FormProvider {...form}>
+            <MortalityEntryFields />
+        </FormProvider>
+    );
+};
+describe('MortalityEntryFields', () => {
+    it('should render the proper labels', () => {
+        const { getByLabelText, queryByLabelText } = render(<Fixture />);
+
+        expect(getByLabelText('Mortality information as of')).toBeInTheDocument();
+        expect(getByLabelText('Is the patient deceased')).toBeInTheDocument();
+        expect(queryByLabelText('Date of death')).not.toBeInTheDocument();
+        expect(queryByLabelText('Death city')).not.toBeInTheDocument();
+        expect(queryByLabelText('Death state')).not.toBeInTheDocument();
+        expect(queryByLabelText('Death county')).not.toBeInTheDocument();
+        expect(queryByLabelText('Death country')).not.toBeInTheDocument();
+    });
+
+    it('should render the death information when deceased is true', () => {
+        const { getByLabelText } = render(<Fixture />);
+
+        const deceased = getByLabelText('Is the patient deceased');
+
+        userEvent.selectOptions(deceased, 'Y');
+
+        expect(getByLabelText('Date of death')).toBeInTheDocument();
+        expect(getByLabelText('Death city')).toBeInTheDocument();
+        expect(getByLabelText('Death state')).toBeInTheDocument();
+        expect(getByLabelText('Death county')).toBeInTheDocument();
+        expect(getByLabelText('Death country')).toBeInTheDocument();
+    });
+
+    it('should reset the death information when deceased is reverted to false', () => {
+        const { getByLabelText } = render(<Fixture />);
+        // Set patient deceased Y
+        userEvent.selectOptions(getByLabelText('Is the patient deceased'), 'Y');
+
+        // Enter data in previously hidden fields
+        userEvent.paste(getByLabelText('Date of death'), '12/01/2020');
+        userEvent.type(getByLabelText('Death city'), 'City value');
+        userEvent.selectOptions(getByLabelText('Death state'), '1');
+        userEvent.selectOptions(getByLabelText('Death county'), '2');
+        userEvent.selectOptions(getByLabelText('Death country'), '3');
+
+        // Toggle deceased to N then back to Y
+        userEvent.selectOptions(getByLabelText('Is the patient deceased'), 'N');
+        userEvent.selectOptions(getByLabelText('Is the patient deceased'), 'Y');
+
+        // Verify previously input data is cleared
+        expect(getByLabelText('Date of death')).toHaveValue('');
+        expect(getByLabelText('Death city')).toHaveValue('');
+        expect(getByLabelText('Death state')).toHaveValue('');
+        expect(getByLabelText('Death county')).toHaveValue('');
+        expect(getByLabelText('Death country')).toHaveValue('');
+    });
+});
