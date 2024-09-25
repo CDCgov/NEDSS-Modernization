b482c6b8489712961b40d52bf4bd553726f1a0e3
Alaap Bharadwaj <abharadwaj@enquizit.com>
2024-09-25 14:46:18 -0400

    modal added

--- apps/modernization-ui/src/apps/patient/add/extended/AddPatientExtended.tsx
+++ apps/modernization-ui/src/apps/patient/add/extended/AddPatientExtended.tsx
@@ -2,8 +2,13 @@ import { Button } from 'components/button';
 import { DataEntrySideNav } from '../DataEntrySideNav/DataEntrySideNav';
 import { AddPatientExtendedForm } from './AddPatientExtendedForm';
 import styles from './add-patient-extended.module.scss';
+import { ConfirmationModal } from 'confirmation';
+import { useRef } from 'react';
+import { ModalRef } from '@trussworks/react-uswds';
 
 export const AddPatientExtended = () => {
+    const cancelPatientModal = useRef<ModalRef>(null);
+
     return (
         <div className={styles.addPatientExtended}>
             <DataEntrySideNav />
@@ -11,7 +16,9 @@ export const AddPatientExtended = () => {
                 <header>
                     <h1>New patient - extended</h1>
                     <div className={styles.buttonGroup}>
-                        <Button outline>Cancel</Button>
+                        <Button onClick={() => cancelPatientModal.current?.toggleModal()} outline>
+                            Cancel
+                        </Button>
                         <Button>Save</Button>
                     </div>
                 </header>
@@ -19,6 +26,19 @@ export const AddPatientExtended = () => {
                     <AddPatientExtendedForm />
                 </main>
             </div>
+            <ConfirmationModal
+                modal={cancelPatientModal}
+                title="Warning"
+                message="Canceling the form will result in the loss of all additional data entered.  Are you sure you want to cancel?"
+                confirmText="Yes, cancel"
+                cancelText="No, back to form"
+                onConfirm={() => {
+                    cancelPatientModal.current?.toggleModal();
+                }}
+                onCancel={() => {
+                    cancelPatientModal.current?.toggleModal();
+                }}
+            />
         </div>
     );
 };
