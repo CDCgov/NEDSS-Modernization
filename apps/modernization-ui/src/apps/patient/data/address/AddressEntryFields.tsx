37ef56cbf1a67812867d3de6110dbe51367c25fb
hclarkEnq <hclark@enquizit.com>
2024-10-02 10:38:09 -0700

    adding suggested value regex after testing

--- apps/modernization-ui/src/apps/patient/data/address/AddressEntryFields.tsx
+++ apps/modernization-ui/src/apps/patient/data/address/AddressEntryFields.tsx
@@ -208,7 +208,7 @@ export const AddressEntryFields = () => {
                 name="censusTract"
                 rules={{
                     pattern: {
-                        value: /^(0[0-9]{3}|[1-9][0-9]{3})(\.(0[1-9]|[1-8][0-9]|9[0-8]))?$/,
+                        value: /^(?!0000)(\d{4})(?:\.(?!00|99)\d{2})?$/,
                         message:
                             'Census Tract should be in numeric XXXX or XXXX.xx format where XXXX is the basic tract and xx is the suffix. XXXX ranges from 0001 to 9999. The suffix is limited to a range between .01 and .98.'
                     }
