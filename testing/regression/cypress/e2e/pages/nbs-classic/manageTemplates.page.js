9fb7f87a88dd212763506763b085e95273057ad3
Lakshmi Narayanan Ramakrishnan <132704359+lramakr@users.noreply.github.com>
2024-10-14 08:14:43 -0700

    CNT-70 1037 Import Template Library (#1926)

---
+++ testing/regression/cypress/e2e/pages/nbs-classic/manageTemplates.page.js
@@ -0,0 +1,42 @@
+class ClassicManageTemplatePage {
+
+  navigateToTemplateLibrary() {
+    cy.contains('System Management').click()
+    cy.get('#systemAdmin5 a').eq(0).click()
+    cy.contains('Manage Templates').click()
+  }
+
+  clickImportBtnTemplateLibrary() {
+    cy.get('input[type="button"][value="Import"]').eq(0).click()
+  }
+
+  clickChooseFileBtnTemplateLibrary() {
+    cy.get('input[type="file"][name="importFile"]').click()
+  }
+
+  clickFilterIconTemplateLibrary() {
+    cy.get('.multiSelect').eq(1).click()
+  }
+
+  enterFilterTextInFilterInbox() {
+    cy.get('#SearchText2').eq(0).type('template')
+  }
+
+  clickOKbtnTemplateLibrary() {
+    cy.get('#b1SearchText2').eq(0).click()
+  }
+
+  clickTemplateInTemplateList() {
+    cy.get('table#parent tbody tr td a').eq(1).click()
+  }
+
+  clickViewRulesBtnTemplateLibrary() {
+    cy.get('input[type="button"][name="View Rules"]').eq(0).click()
+  }
+
+  verifyRulesListedInResultsPage() {
+    cy.contains('Rules')
+  }
+}
+export default new ClassicManageTemplatePage();
+
