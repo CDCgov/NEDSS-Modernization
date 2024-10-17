bb9ea4ab417ac8a94f06083f4a3f8cbf42a93a67
Adam Loup <aloup@enquizit.com>
2024-10-17 18:06:41 -0500

    numeric input

---
+++ apps/modernization-ui/src/design-system/input/numeric/NumericInput.tsx
@@ -0,0 +1,94 @@
+import {
+    ChangeEvent as ReactChangeEvent,
+    KeyboardEvent as ReactKeyboardEvent,
+    useEffect,
+    useMemo,
+    useState
+} from 'react';
+import classNames from 'classnames';
+import { EntryWrapper, Orientation, Sizing } from 'components/Entry';
+
+type NumericOnChange = (value?: number) => void;
+
+const handleKeydown = (event: ReactKeyboardEvent<HTMLInputElement>) => {
+    if (!event.altKey && !event.ctrlKey && !event.metaKey && /Space|Key*/.test(event.code)) {
+        //  prevents spaces and letter characters from being entered but allows keyboard short cuts like (crtl-v)
+        event.preventDefault();
+    }
+};
+
+const asDisplay = (value?: string | number | null) => (value === undefined ? '' : `${value}`);
+
+type NumericInputProps = {
+    id: string;
+    label: string;
+    value?: number;
+    onChange?: NumericOnChange;
+    orientation?: Orientation;
+    sizing?: Sizing;
+    error?: string;
+    required?: boolean;
+} & Omit<JSX.IntrinsicElements['input'], 'defaultValue' | 'onChange' | 'value' | 'type'>;
+
+const NumericInput = ({
+    id,
+    name,
+    label,
+    value,
+    onChange,
+    orientation,
+    sizing,
+    error,
+    required,
+    className,
+    placeholder = 'No Data',
+    ...props
+}: NumericInputProps) => {
+    const [current, setCurrent] = useState<number | undefined>(value);
+
+    useEffect(() => {
+        onChange?.(current);
+    }, [current, onChange]);
+
+    const display = useMemo(() => asDisplay(current), [current]);
+
+    const handleChange = (event: ReactChangeEvent<HTMLInputElement>) => {
+        const next = event.target.value;
+
+        if (next === '') {
+            setCurrent(undefined);
+        } else if (Number.isNaN(next)) {
+            event.preventDefault();
+        } else {
+            const adjusted = Number(next);
+            if (!Number.isNaN(adjusted)) {
+                setCurrent(Number(next));
+            }
+        }
+    };
+
+    return (
+        <EntryWrapper
+            orientation={orientation}
+            sizing={sizing}
+            label={label}
+            htmlFor={id}
+            required={required}
+            error={error}>
+            <input
+                id={id}
+                name={name}
+                className={classNames('usa-input', className)}
+                type="number"
+                onChange={handleChange}
+                placeholder={placeholder}
+                value={display}
+                pattern="[0-9]*"
+                onKeyDown={handleKeydown}
+                {...props}
+            />
+        </EntryWrapper>
+    );
+};
+
+export { NumericInput };
