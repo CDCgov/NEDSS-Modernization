c415fefe1318049d34690a5f531fa4b9a973462b
hclarkEnq <hclark@enquizit.com>
2024-10-17 14:07:28 -0400

    adding a data provider wrapper including both add extended and basic patients

--- apps/modernization-ui/src/apps/patient/add/PatientAddRouting.tsx
+++ apps/modernization-ui/src/apps/patient/add/PatientAddRouting.tsx
@@ -2,25 +2,31 @@ import { FeatureGuard } from 'feature';
 import { AddPatient } from './AddPatient';
 import { AddPatientExtended } from './extended/AddPatientExtended';
 import { PatientDataProvider } from './usePatientData/usePatientData';
+import { Outlet } from 'react-router-dom';
+
+const PatientDataProviderWrapper = () => (
+    <PatientDataProvider>
+        <Outlet />
+    </PatientDataProvider>
+);
 
 const routing = [
     {
-        path: '/add-patient',
-        element: (
-            <PatientDataProvider>
-                <AddPatient />
-            </PatientDataProvider>
-        )
-    },
-    {
-        path: '/patient/add/extended',
-        element: (
-            <PatientDataProvider>
-                <FeatureGuard guard={(features) => features?.patient?.add?.extended?.enabled}>
-                    <AddPatientExtended />
-                </FeatureGuard>
-            </PatientDataProvider>
-        )
+        element: <PatientDataProviderWrapper />,
+        children: [
+            {
+                path: '/add-patient',
+                element: <AddPatient />
+            },
+            {
+                path: '/patient/add/extended',
+                element: (
+                    <FeatureGuard guard={(features) => features?.patient?.add?.extended?.enabled}>
+                        <AddPatientExtended />
+                    </FeatureGuard>
+                )
+            }
+        ]
     }
 ];
 
