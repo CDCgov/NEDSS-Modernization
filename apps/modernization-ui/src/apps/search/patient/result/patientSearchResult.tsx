90f8e6d8eb8feb739731bcccc7916ca4db0d1c87
benlambert_ignw <benjamin.lambert@cdw.com>
2024-10-21 08:01:20 -0400

    Implement use of ItemGroup design system through code reuse

--- apps/modernization-ui/src/apps/search/patient/result/patientSearchResult.tsx
+++ apps/modernization-ui/src/apps/search/patient/result/patientSearchResult.tsx
@@ -2,7 +2,6 @@ import { PatientSearchResult } from 'generated/graphql/schema';
 import { displayName, displayNameElement, matchesLegalName } from 'name';
 import { displayAddress } from 'address/display';
 import { Link } from 'react-router-dom';
-import { ItemGroup } from 'design-system/item';
 
 const displayProfileLink = (shortId: number, displayName?: string) => {
     return <Link to={`/patient-profile/${shortId}/summary`}>{displayName || shortId}</Link>;
@@ -16,7 +15,7 @@ const displayProfileLegalName = (result: PatientSearchResult) => {
 // Displays Other names, that are not the legal name
 const displayOtherNames = (result: PatientSearchResult, order: 'normal' | 'reverse' = 'normal'): JSX.Element => {
     const legalName = result.legalName;
-    const patientSearchResultNames = order === 'normal' ? result.names : result.names.reverse();
+    const patientSearchResultNames = order === 'normal' ? result.names : [...result.names].reverse();
     return (
         <div>
             {patientSearchResultNames
@@ -41,22 +40,8 @@ const displayPhones = (result: PatientSearchResult): string => result.phones.joi
 const displayEmails = (result: PatientSearchResult): string => result.emails.join('\n');
 const displayPatientName = (result: PatientSearchResult): JSX.Element => (
     <div>
-        <div>
-            <ItemGroup type="name" label={'Legal name'}>
-                {result.legalName && displayName('fullLastFirst')(result.legalName)}
-
-                {result.names
-                    .filter((v) => JSON.stringify(v) !== JSON.stringify(result.legalName))
-                    .reverse()
-                    .map((name, index) => (
-                        <div key={index}>
-                            <b>{name.type}</b>
-                            <br />
-                            {result.legalName && displayName('fullLastFirst')(name)}
-                        </div>
-                    ))}
-            </ItemGroup>
-        </div>
+        {result.legalName && displayNameElement(result.legalName)}
+        {displayOtherNames(result, 'reverse')}
     </div>
 );
 
