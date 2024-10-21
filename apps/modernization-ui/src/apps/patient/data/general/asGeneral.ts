7ad25e9f3e5b63969232d7c17240a44b136fc8b3
Adam Loup <aloup@enquizit.com>
2024-10-21 10:31:29 -0500

    removed console log

--- apps/modernization-ui/src/apps/patient/data/general/asGeneral.ts
+++ apps/modernization-ui/src/apps/patient/data/general/asGeneral.ts
@@ -3,17 +3,30 @@ import { GeneralInformation } from '../api';
 import { GeneralInformationEntry } from '../entry';
 
 const asGeneral = (entry: GeneralInformationEntry): GeneralInformation => {
-    const { maritalStatus, primaryOccupation, educationLevel, speaksEnglish, primaryLanguage, ...remaining } = entry;
-
-    console.log(remaining);
+    const {
+        asOf,
+        maternalMaidenName,
+        adultsInResidence,
+        childrenInResidence,
+        maritalStatus,
+        primaryOccupation,
+        educationLevel,
+        speaksEnglish,
+        primaryLanguage,
+        stateHIVCase
+    } = entry;
 
     return {
+        asOf,
+        maternalMaidenName,
+        adultsInResidence,
+        childrenInResidence,
         maritalStatus: asValue(maritalStatus),
         primaryOccupation: asValue(primaryOccupation),
         educationLevel: asValue(educationLevel),
         speaksEnglish: asValue(speaksEnglish),
         primaryLanguage: asValue(primaryLanguage),
-        ...remaining
+        stateHIVCase
     };
 };
 
