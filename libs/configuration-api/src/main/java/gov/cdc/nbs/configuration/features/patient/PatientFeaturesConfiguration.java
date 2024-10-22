9a17a2de64abff0ba9aa31cfa5896e7ebde603dd
Joe Agster <joe.agster@cdw.com>
2024-10-22 15:07:04 -0700

    Backend changes for add patient feature flag

--- libs/configuration-api/src/main/java/gov/cdc/nbs/configuration/features/patient/PatientFeaturesConfiguration.java
+++ libs/configuration-api/src/main/java/gov/cdc/nbs/configuration/features/patient/PatientFeaturesConfiguration.java
@@ -11,9 +11,10 @@ class PatientFeaturesConfiguration {
   @Bean
   @Scope("prototype")
   Patient patientFeatures(
-      @Value("${nbs.ui.features.patient.add.extended.enabled:false}") final boolean enabled
+      @Value("${nbs.ui.features.patient.add.enabled:false}") final boolean enabled,
+      @Value("${nbs.ui.features.patient.add.extended.enabled:false}") final boolean extendedEnabled
   ) {
-    return new Patient(new Patient.Add(new Patient.Add.Extended(enabled)));
+    return new Patient(new Patient.Add(enabled, new Patient.Add.Extended(extendedEnabled)));
   }
 
 }
