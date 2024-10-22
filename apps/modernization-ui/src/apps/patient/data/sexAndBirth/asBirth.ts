c4cc082e12adaf3003cb0de766ad6adbc995dab8
Adam Loup <aloup@enquizit.com>
2024-10-22 17:06:54 -0500

    sex and birth

--- apps/modernization-ui/src/apps/patient/data/sexAndBirth/asBirth.ts
+++ apps/modernization-ui/src/apps/patient/data/sexAndBirth/asBirth.ts
@@ -1,21 +1,26 @@
 import { asValue } from 'options';
 import { Birth } from '../api';
 import { BirthEntry } from '../entry';
+import { isEmpty, orUndefined } from 'utils';
 
-const asBirth = (entry: BirthEntry): Birth => {
-    const { asOf, bornOn, sex, multiple, order, city, county, state, country } = entry;
+const asBirth = (entry: BirthEntry): Birth | undefined => {
+    const { asOf, ...remaining } = entry;
 
-    return {
-        asOf,
-        bornOn,
-        sex: asValue(sex),
-        multiple: asValue(multiple),
-        order,
-        city,
-        county: asValue(county),
-        state: asValue(state),
-        country: asValue(country)
-    };
+    if (!isEmpty(remaining)) {
+        const { bornOn, sex, multiple, order, city, county, state, country } = remaining;
+
+        return {
+            asOf,
+            bornOn: orUndefined(bornOn),
+            sex: asValue(sex),
+            multiple: asValue(multiple),
+            order: orUndefined(order),
+            city: orUndefined(city),
+            county: asValue(county),
+            state: asValue(state),
+            country: asValue(country)
+        };
+    }
 };
 
 export { asBirth };
