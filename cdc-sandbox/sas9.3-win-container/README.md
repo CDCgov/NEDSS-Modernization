44e998c1754c4a10b64c3e72cdc4d1f77fe4da62
Nouman Javed <njaved@enquizit.com>
2024-11-08 14:16:15 -0500

    Adding SAS Files

---
+++ cdc-sandbox/sas9.3-win-container/README.md
@@ -0,0 +1,29 @@
+# Creating and Running NB6 Docker Image Manually
+
+## Description
+Below are manual NBS6 creation and run process steps. Foundation image used is Microsoft Server Core ltsc2019. Additional details, inclusive of licensing information, are available at https://hub.docker.com/_/microsoft-windows-servercore/. 
+
+## Files
+- Dockerfile - Configuration file to build docker image
+- entrypoint.ps1 - Docker entrypoint script to run each time a container runs
+- task-scheduler.ps1 - Script to configure windows scheduled jobs
+
+## Prerequisites
+- Docker engine installed locally or on an instance with Docker engine installed
+- Access to S3 bucket containing NBS6 installation package
+- Optional: Access to a Quay.io Docker Repository if pushing image
+
+## Steps
+- Build Docker Container
+  - Verify you're in cdc-sandbox -> nbs6-win-container directory
+  - Run:  ``` docker build --build-arg S3_ZIP_NAME=wildfly-10.0.0.Final-6.0.15.1 -t <CONTAINER-NAME>:<TAG> . ```
+    - S3_ZIP_NAME - Zip file name pulling NBS6 Wildfly installation zip from S3.
+  - Verify container was built successfully
+- Run NBS6 Docker Container
+  - Run:  ``` docker run -p 7001:7001 -e "GITHUB_RELEASE_TAG=latest" -e "DATABASE_ENDPOINT=<ENDPOINT>" -t <CONTAINER-NAME> ```
+    - GITHUB_RELEASE_TAG - Creates URL, downloads Release Package from GitHub Releases and configure User Guide. Default is always latest or Null
+    - DATABASE_ENDPOINT - Provides Database Endpoint
+- Optional: Push Image to Quay.io Repository
+  - Tag Image:  ``` docker tag <CONTAINER-NAME>:<TAG> <DOCKER-REPOSITORY-URL>/<CONTAINER-NAME>:<TAG> ``` 
+  - Authenticate to Quay.io:  ``` docker login -u=<USERNAME> -p=<PASSWORD> quay.io ```
+  - Push to Quay.io with latest Tag:  ``` docker push <DOCKER-REPOSITORY-URL>/<CONTAINER-NAME>:<TAG>  ```
---
+++ cdc-sandbox/sas9.3-win-container/entrypoint.ps1
@@ -0,0 +1,92 @@
+# ./entrypoint.ps1
+# Prepare NBS Configuration and Start NBS 6.0
+
+# Initialize hastable for data sources
+# NOTE: Provide DATABASE_ENDPOINT when running Container
+
+$connectionURLs = @{
+    "NedssDS" = "jdbc:sqlserver://DATABASE_ENDPOINT:1433;SelectMethod=direct;DatabaseName=nbs_odse";
+    "MsgOutDS" = "jdbc:sqlserver://DATABASE_ENDPOINT:1433;SelectMethod=direct;DatabaseName=nbs_msgoute";
+    "ElrXrefDS" = "jdbc:sqlserver://DATABASE_ENDPOINT:1433;SelectMethod=direct;DatabaseName=nbs_msgoute";
+    "RdbDS" = "jdbc:sqlserver://DATABASE_ENDPOINT:1433;SelectMethod=direct;DatabaseName=rdb";
+    "SrtDS" = "jdbc:sqlserver://DATABASE_ENDPOINT:1433;SelectMethod=direct;DatabaseName=nbs_srte"
+}
+
+
+$keys = $connectionURLs.Keys.Clone()
+
+foreach ($key in $keys) {
+    $connectionURLs[$key] = $connectionURLs[$key] -replace "DATABASE_ENDPOINT", $env:DATABASE_ENDPOINT
+}
+
+# Replace datasources in standalone.xml file
+$xmlFileName = "C:\wildfly-10.0.0.Final\nedssdomain\configuration\standalone.xml"
+
+# Create a XML document
+[xml]$xmlDoc = New-Object system.Xml.XmlDocument
+
+# Read the existing XML file
+[xml]$xmlDoc = Get-Content $xmlFileName
+
+# Search and replace db host name in connection URL
+$subsystems = $xmlDoc.server.profile.subsystem
+$subsystems | % {
+    if ($_.xmlns -eq "urn:jboss:domain:datasources:4.0") {
+        $datsources = $_.datasources.datasource
+        $datsources | % {
+            if ( $connectionURLs.ContainsKey($_.'pool-name')) {
+                $_.'connection-url' =  $connectionURLs[$_.'pool-name']
+            }
+        }
+    }
+}
+
+# Save XML file after connection url replacement
+$xmlDoc.Save($xmlFileName)
+
+############# Configure User Guide #############
+# NOTE: Verify NBS User Training Guide.pdf is located in release zip file
+
+# If $env:GITHUB_RELEASE_TAG null or latest, then choose ../releases/latest URL
+if (-not $env:GITHUB_RELEASE_TAG -or $env:GITHUB_RELEASE_TAG -eq "latest") {
+    Write-Output "Getting $env:GITHUB_RELEASE_TAG Release URL"
+    # Fetch the latest release
+    $githubRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/CDCgov/NEDSS-Modernization/releases/latest"
+    # Output the latest tag name
+    $releaseTag = $githubRelease.tag_name
+  } else {
+    "Getting $env:GITHUB_RELEASE_TAG Release URL"
+    # Fetch the latest release
+    $githubRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/CDCgov/NEDSS-Modernization/releases/tags/$env:GITHUB_RELEASE_TAG"
+    # Output the latest tag name
+    $releaseTag = $githubRelease.tag_name
+  }
+
+
+Write-Output "Release Tag to Download Zip From: $releaseTag"
+
+# Update Variables
+$zip_file_name = $env:GITHUB_ZIP_FILE_NAME -replace "<version>", $releaseTag
+# Set Zip File Name
+$zip_url = ($githubRelease.assets | Where-Object { $_.name -like "$zip_file_name" }).browser_download_url
+# Download Zip File. System.Net.WebClient is faster then using Invoke-WebRequest
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($zip_url, $zip_file_name)
+Write-Output "Downloaded Zip File: $zip_file_name"
+# Extract ZIP to temporary directory
+Expand-Archive -LiteralPath "$zip_file_name" -Force
+$zip_folder = $zip_file_name.Trim(".zip")
+# Get User Guide Name
+$user_guide_name = $(Get-ChildItem -Path $zip_folder\$zip_folder\*"User Guide.pdf" -Recurse).Name
+# Move zip file the final destination
+$zip_user_guide_path = Join-Path -Path $zip_folder -ChildPath "$zip_folder\$user_guide_name"
+$user_guide_directory = "C:\wildfly-10.0.0.Final\nedssdomain\Nedss\UserGuide\$env:FINAL_NBS_USER_GUIDE_NAME"
+Write-Output "Moving and Renaming User Guide '$user_guide_name' to '$env:FINAL_NBS_USER_GUIDE_NAME'"
+Copy-Item -Path "$zip_user_guide_path" -Destination "$user_guide_directory" -Force
+# Cleanup
+Remove-Item $zip_file_name
+Remove-Item $zip_folder -Recurse -Force -Confirm:$false
+#### END OF Configure User Guide ####
+
+
+Start-Process "C:\\wildfly-10.0.0.Final\\bin\\standalone.bat" -Wait -NoNewWindow -PassThru | Out-Host
