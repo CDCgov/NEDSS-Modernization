c70c533ec104f39b469f6142c5deebcfdeeff974
Michael Davis <125376056+mdavis-enquizit@users.noreply.github.com>
2024-11-12 14:28:37 -0500

    CNT-93 CNT-94 add and delete VACCINATION (#2009)
    * CNT-93 CNT9-4 add and delete VACCINATION
    
    * CNT-94 test for delete success message; proper format for url
    
    ---------
    
    Co-authored-by: Michael Davis <michaeldavis@Michaels-MacBook-Pro.local>

---
+++ testing/regression/cypress/step_definitions/nbs-classic/vaccination.steps.js
@@ -0,0 +1,16 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Then("user submits {string} vaccination with a popup and deletes it", (string) => {
+  const vaccinationUrl = Cypress.config().baseUrl + "nbs/PageAction.do?method=createGenericLoad&businessObjectType=VAC&Action=DSFilePath";
+  cy.visit(vaccinationUrl);
+  cy.wait(1000);
+  cy.get("td#tabs0head1").first().click({force: true});
+  cy.get("#VAC101").select(string, {force: true});
+  cy.get("input[name='Submit'").first().click();
+  cy.get("#successMessages").contains("Vaccination has been successfully saved in the system.");
+  cy.get("input[name='Delete'").first().click();
+  cy.on("window:confirm", () =>  {
+    return true;
+  });
+  cy.get("#successMessages").contains("Vaccination has been successfully deleted in the system.");
+});
