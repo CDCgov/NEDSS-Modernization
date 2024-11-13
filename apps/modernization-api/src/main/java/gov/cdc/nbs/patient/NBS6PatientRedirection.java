e98f1664e64d93da98ba742e0384d074afdc4122
Adam Loup <aloup@enquizit.com>
2024-11-13 14:22:07 -0600

    configurable feature flag for the modernized patient profile

---
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/NBS6PatientRedirection.java
@@ -0,0 +1,25 @@
+package gov.cdc.nbs.patient;
+
+import gov.cdc.nbs.patient.profile.redirect.outgoing.NBS6PatientFileRedirectResolver;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Hidden
+@RestController
+class NBS6PatientRedirection {
+
+  private final NBS6PatientFileRedirectResolver resolver;
+
+  NBS6PatientRedirection(final NBS6PatientFileRedirectResolver resolver) {
+    this.resolver = resolver;
+  }
+
+  @GetMapping("/nbs/api/patient/{patient}/file/redirect")
+  ResponseEntity<Void> profile(@PathVariable final long patient) {
+    return resolver.resolve(patient);
+  }
+
+}
