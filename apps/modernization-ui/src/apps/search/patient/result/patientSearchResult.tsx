9a9fd8bc5c2e7586521d3f1bf31cd707b0318007
Adam Loup <aloup@enquizit.com>
2024-11-13 15:14:56 -0600

    search result links that use the patient profile feature flag

--- apps/modernization-ui/src/apps/search/patient/result/patientSearchResult.tsx
+++ apps/modernization-ui/src/apps/search/patient/result/patientSearchResult.tsx
@@ -1,35 +1,8 @@
 import { PatientSearchResult } from 'generated/graphql/schema';
-import { displayName, displayNameElement, matchesLegalName } from 'name';
+import { displayNameElement, matchesLegalName } from 'name';
 import { displayAddress } from 'address/display';
-import { Link } from 'react-router-dom';
+
 import { ItemGroup } from 'design-system/item';
-import { internalizeDate } from 'date';
-import { displayAgeAsOfToday } from 'date/displayAge';
-import { FeatureToggle } from 'feature';
-
-const nbs6PatientProfile = (identifier: number, shortId: number, display?: string) => (
-    <a href={`/nbs/PatientSearchResults1.do?ContextAction=ViewFile&uid=${identifier}`}>{display || shortId}</a>
-);
-
-const modernizedPatientProfile = (shortId: number, display?: string) => (
-    <Link to={`/patient-profile/${shortId}/summary`}>{display || shortId}</Link>
-);
-
-const displayProfileLink = (identifier: number, shortId: number, display?: string) => {
-    return (
-        <FeatureToggle
-            guard={(features) => features?.patient?.profile.enabled}
-            fallback={nbs6PatientProfile(identifier, shortId, display)}>
-            {modernizedPatientProfile(shortId, display)}
-        </FeatureToggle>
-    );
-};
-
-const displayProfileLegalName = (result: PatientSearchResult) => {
-    const legalNameDisplay =
-        result.legalName?.first || result.legalName?.last ? displayName('fullLastFirst')(result.legalName) : 'No Data';
-    return displayProfileLink(result.patient, result.shortId, legalNameDisplay);
-};
 
 // Displays Other names, that are not the legal name
 const displayOtherNames = (result: PatientSearchResult, order: 'normal' | 'reverse' = 'normal'): JSX.Element => {
@@ -99,8 +72,6 @@ export {
     displayPatientName,
     displayPatientAge,
     displayOtherNames,
-    displayProfileLink,
-    displayProfileLegalName,
     displayPhones,
     displayAddresses,
     displayEmails,
