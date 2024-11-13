21d8875e178af8cb89edf4274e7e1a91ce0c6742
benlambert_ignw <benjamin.lambert@cdw.com>
2024-11-13 09:44:34 -0500

    modify calculateAge to support plural or singular units

--- apps/modernization-ui/src/date/calculateAge.ts
+++ apps/modernization-ui/src/date/calculateAge.ts
@@ -1,15 +1,18 @@
 import { differenceInDays, differenceInMonths, differenceInYears } from 'date-fns';
 
-const calculate = (from: Date, dateOfBirth: Date): { quantity: number; unit: 'years' | 'months' | 'days' } => {
+const calculate = (
+    from: Date,
+    dateOfBirth: Date
+): { quantity: number; unit: 'years' | 'year' | 'months' | 'month' | 'days' | 'day' } => {
     const years = differenceInYears(from, dateOfBirth);
     const months = differenceInMonths(from, dateOfBirth);
     const days = differenceInDays(from, dateOfBirth);
     if (years > 0) {
-        return { quantity: years, unit: 'years' };
+        return { quantity: years, unit: years === 1 ? 'year' : 'years' };
     } else if (months > 0) {
-        return { quantity: months, unit: 'months' };
+        return { quantity: months, unit: months === 1 ? 'month' : 'months' };
     } else if (days > 0) {
-        return { quantity: days, unit: 'days' };
+        return { quantity: days, unit: days === 1 ? 'day' : 'days' };
     }
     return { quantity: 0, unit: 'days' };
 };
@@ -17,7 +20,7 @@ const calculate = (from: Date, dateOfBirth: Date): { quantity: number; unit: 'ye
 const calculateAge = (
     dateOfBirth: string | Date | null | undefined,
     from: Date = new Date()
-): { quantity: number; unit: 'years' | 'months' | 'days' } | undefined => {
+): { quantity: number; unit: 'years' | 'year' | 'months' | 'month' | 'days' | 'day' } | undefined => {
     if (dateOfBirth) {
         if (typeof dateOfBirth === 'string') {
             return calculate(from, new Date(dateOfBirth));
