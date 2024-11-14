7e6d4e64ef5db86af3ef1c8052b76a1bc9507151
Michael Peels <michaelpeels@Michael-Peels.local>
2024-11-14 15:52:16 -0500

    Blocking criteria display

--- apps/modernization-ui/src/apps/deduplication/match-configuration/pass-form/blocking/BlockingCriteriaRow.tsx
+++ apps/modernization-ui/src/apps/deduplication/match-configuration/pass-form/blocking/BlockingCriteriaRow.tsx
@@ -3,6 +3,8 @@ import { Controller, useFormContext } from 'react-hook-form';
 import styles from './blocking-criteria-row.module.scss';
 import { MatchingConfiguration } from '../../model/Pass';
 import { BLOCKING_METHOD_OPTIONS } from '../../model/Blocking';
+import { Icon } from '@trussworks/react-uswds';
+import { Button } from 'components/button';
 
 type Props = {
     label: string;
@@ -14,28 +16,33 @@ export const BlockingCriteriaRow = ({ label, index, onRemove }: Props) => {
 
     return (
         <div className={styles.blockingCriteriaRow}>
-            <div>
-                <div>{label}</div>
-                <div>
-                    <Controller
-                        control={form.control}
-                        name={`passes.${1}.blockingCriteria.${index}.method`}
-                        render={({ field: { onBlur, onChange, value, name } }) => (
-                            <SingleSelect
-                                id={`blockingMethod-${index}`}
-                                orientation="horizontal"
-                                label="Method"
-                                onBlur={onBlur}
-                                onChange={onChange}
-                                name={name}
-                                value={value}
-                                options={BLOCKING_METHOD_OPTIONS}
-                            />
-                        )}
-                    />
+            <div className={styles.underlined}>
+                <div className={styles.information}>
+                    <div className={styles.label}>{label}</div>
+                    <div className={styles.input}>
+                        <Controller
+                            control={form.control}
+                            name={`passes.${1}.blockingCriteria.${index}.method`}
+                            render={({ field: { onBlur, onChange, value, name } }) => (
+                                <SingleSelect
+                                    id={`blockingMethod-${index}`}
+                                    orientation="horizontal"
+                                    label="Method"
+                                    onBlur={onBlur}
+                                    onChange={onChange}
+                                    name={name}
+                                    value={value}
+                                    options={BLOCKING_METHOD_OPTIONS}
+                                />
+                            )}
+                        />
+                    </div>
                 </div>
+                <Button unstyled onClick={onRemove}>
+                    <Icon.Delete size={3} />
+                    Remove
+                </Button>
             </div>
-            <div onClick={onRemove}>Remove</div>
         </div>
     );
 };
