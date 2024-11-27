44fa12263121a4206f63eeebbab8f632b6e646ca
Alaap Bharadwaj <137817788+alaapbharadwaj@users.noreply.github.com>
2024-11-27 16:01:41 -0500

    phone changes completed (#2058)

--- apps/modernization-ui/src/apps/patient/add/contactFields/ContactFields.tsx
+++ apps/modernization-ui/src/apps/patient/add/contactFields/ContactFields.tsx
@@ -1,12 +1,10 @@
-import { Button, Grid, Label } from '@trussworks/react-uswds';
-import { Controller, useFieldArray, useFormContext, useWatch } from 'react-hook-form';
+import { Grid } from '@trussworks/react-uswds';
+import { Controller, useFormContext, useWatch } from 'react-hook-form';
 import FormCard from 'components/FormCard/FormCard';
 import { validatePhoneNumber } from 'validation/phone';
 import { Input } from 'components/FormInputs/Input';
 import { PhoneNumberInput } from 'components/FormInputs/PhoneNumberInput/PhoneNumberInput';
 import { maxLengthRule } from 'validation/entry';
-import { SelectInput } from 'components/FormInputs/SelectInput';
-import styles from './contactFields.module.scss';
 import { useEffect } from 'react';
 
 type Props = {
@@ -14,29 +12,8 @@ type Props = {
     title: string;
 };
 
-type Type = {
-    name: string;
-    value: string;
-    type: string;
-};
-
-const classification: Type[] = [
-    { name: 'Home phone', value: 'H', type: 'PH' },
-    { name: 'Work phone', value: 'WP', type: 'PH' },
-    { name: 'Cell phone', value: 'MC', type: 'CP' }
-];
-
 export default function ContactFields({ id, title }: Props) {
     const { control, setValue } = useFormContext();
-
-    const { fields: phoneNumberFields, append: phoneNumberAppend } = useFieldArray({
-        control,
-        name: 'phoneNumbers'
-    });
-    const { fields: emailFields, append: emailFieldAppend } = useFieldArray({
-        control,
-        name: 'emailAddresses'
-    });
 
     const phoneFields = useWatch({ control: control, name: 'phoneNumbers' });
 
@@ -68,7 +45,6 @@ export default function ContactFields({ id, title }: Props) {
                             }}
                             render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
                                 <PhoneNumberInput
-                                    placeholder="333-444-555"
                                     onChange={onChange}
                                     onBlur={onBlur}
                                     label="Home phone"
@@ -95,7 +71,6 @@ export default function ContactFields({ id, title }: Props) {
                             }}
                             render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
                                 <PhoneNumberInput
-                                    placeholder="333-444-555"
                                     onChange={onChange}
                                     onBlur={onBlur}
                                     label="Work phone"
@@ -116,7 +91,6 @@ export default function ContactFields({ id, title }: Props) {
                             render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
                                 <Input
                                     inputMode="numeric"
-                                    placeholder="1234"
                                     onChange={onChange}
                                     onBlur={onBlur}
                                     type="tel"
@@ -143,7 +117,6 @@ export default function ContactFields({ id, title }: Props) {
                             }}
                             render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
                                 <PhoneNumberInput
-                                    placeholder="333-444-555"
                                     onChange={onChange}
                                     onBlur={onBlur}
                                     label="Cell phone"
@@ -155,123 +128,6 @@ export default function ContactFields({ id, title }: Props) {
                                 />
                             )}
                         />
-                    </Grid>
-                </Grid>
-                {phoneNumberFields.map((item: any, index: number) => (
-                    <Grid row gap={2} key={item.id}>
-                        <div className={styles.phone}>
-                            <Label htmlFor="Phone">Phone</Label>
-                            <div className={styles.fields}>
-                                <Grid col={5}>
-                                    <Controller
-                                        control={control}
-                                        name={`phoneNumbers[${index}].use`}
-                                        rules={{ required: { value: true, message: 'Type is required.' } }}
-                                        render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                            <SelectInput
-                                                defaultValue={value ? value : 'H'}
-                                                onChange={onChange}
-                                                htmlFor={'use'}
-                                                options={classification}
-                                                error={error?.message}
-                                                required
-                                            />
-                                        )}
-                                    />
-                                </Grid>
-                                <Grid col={7}>
-                                    <Controller
-                                        control={control}
-                                        name={`phoneNumbers[${index}].number`}
-                                        rules={{
-                                            validate: {
-                                                properNumber: validatePhoneNumber
-                                            },
-                                            ...maxLengthRule(20)
-                                        }}
-                                        render={({
-                                            field: { onChange, onBlur, value, name },
-                                            fieldState: { error }
-                                        }) => (
-                                            <PhoneNumberInput
-                                                placeholder="333-444-555"
-                                                onChange={onChange}
-                                                onBlur={onBlur}
-                                                defaultValue={value}
-                                                id={name}
-                                                error={error?.message}
-                                                mask="___-___-____"
-                                                pattern="\d{3}-\d{3}-\d{4}"
-                                            />
-                                        )}
-                                    />
-                                </Grid>
-                            </div>
-                        </div>
-                    </Grid>
-                ))}
-                <Grid row>
-                    <Grid col={12}>
-                        <Button
-                            type={'button'}
-                            onClick={() =>
-                                phoneNumberAppend({
-                                    number: null,
-                                    use: 'H',
-                                    type: 'PH'
-                                })
-                            }
-                            className="text-bold"
-                            unstyled
-                            style={{ margin: '1.125rem 0 0 0', padding: '0' }}>
-                            + Add another phone number
-                        </Button>
-                    </Grid>
-                </Grid>
-                {emailFields.map((item: any, index: number) => (
-                    <Grid row key={item.id}>
-                        <Grid col={6}>
-                            <Controller
-                                control={control}
-                                name={`emailAddresses[${index}].email`}
-                                rules={{
-                                    pattern: {
-                                        value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-                                        message: 'Please enter a valid email address (example: youremail@website.com)'
-                                    },
-                                    ...maxLengthRule(100)
-                                }}
-                                render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
-                                    <Input
-                                        placeholder="jdoe@gmail.com"
-                                        onChange={onChange}
-                                        onBlur={onBlur}
-                                        type="text"
-                                        label="Email"
-                                        defaultValue={value}
-                                        htmlFor={name}
-                                        id={name}
-                                        error={error?.message}
-                                    />
-                                )}
-                            />
-                        </Grid>
-                    </Grid>
-                ))}
-                <Grid row>
-                    <Grid col={12}>
-                        <Button
-                            type={'button'}
-                            onClick={() =>
-                                emailFieldAppend({
-                                    email: null
-                                })
-                            }
-                            className="text-bold"
-                            unstyled
-                            style={{ margin: '1.125rem 0 0 0', padding: '0' }}>
-                            + Add another email address
-                        </Button>
                     </Grid>
                 </Grid>
             </Grid>
