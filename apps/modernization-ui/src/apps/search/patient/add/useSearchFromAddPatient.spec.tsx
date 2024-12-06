2a0191293ff25ae18e4b226aad69a82d0986b745
Alaap Bharadwaj <abharadwaj@enquizit.com>
2024-12-06 09:45:14 -0500

    small changes to fix tests

---
+++ apps/modernization-ui/src/apps/search/patient/add/useSearchFromAddPatient.spec.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+import { useSearchFromAddPatient } from './useSearchFromAddPatient';
+
+const TestComponent = () => {
+    const { toSearch } = useSearchFromAddPatient();
+
+    return (
+        <div>
+            <button onClick={() => toSearch('test criteria')}>Cancel</button>
+            <div data-testid="patient-name">test</div>
+            <div data-testid="patient-age">test</div>
+        </div>
+    );
+};
+
+describe('useSearchFromAddPatient', () => {
+    it('should throw an error if outside of SearchFromAddPatientProvider', () => {
+      const consoleError = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+      expect(() => render(<TestComponent />)).toThrow(
+        'useSearchFromAddPatient must be used within a SearchFromAddPatientProvider'
+    );
+
+    consoleError.mockRestore();
+    });
+});
