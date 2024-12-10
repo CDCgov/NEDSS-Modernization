825d5a36265259452ef263209b91f7ee9af6ea04
hclarkEnq <hclark@enquizit.com>
2024-12-10 13:53:28 -0500

    adding phoneemailbasicfields and tests supporting

--- apps/modernization-ui/src/apps/patient/add/basic/AddPatientBasic.tsx
+++ apps/modernization-ui/src/apps/patient/add/basic/AddPatientBasic.tsx
@@ -3,21 +3,30 @@ import { AddPatientLayout, DataEntryLayout } from 'apps/patient/add/layout';
 import styles from './add-patient-basic.module.scss';
 import { sections } from './sections';
 import { AddPatientBasicForm } from './AddPatientBasicForm';
+import { FormProvider, useForm } from 'react-hook-form';
+import { BasicNewPatientEntry, initial } from './entry';
 
 export const AddPatientBasic = () => {
+    const form = useForm<BasicNewPatientEntry>({
+        defaultValues: initial(),
+        mode: 'onBlur'
+    });
+
     return (
         <DataEntryLayout>
-            <AddPatientLayout
-                headerTitle="New patient"
-                sections={sections}
-                headerActions={() => (
-                    <div className={styles.buttonGroup}>
-                        <Button outline>Cancel</Button>
-                        <Button>Save</Button>
-                    </div>
-                )}>
-                <AddPatientBasicForm />
-            </AddPatientLayout>
+            <FormProvider {...form}>
+                <AddPatientLayout
+                    headerTitle="New patient"
+                    sections={sections}
+                    headerActions={() => (
+                        <div className={styles.buttonGroup}>
+                            <Button outline>Cancel</Button>
+                            <Button type="submit">Save</Button>
+                        </div>
+                    )}>
+                    <AddPatientBasicForm />
+                </AddPatientLayout>
+            </FormProvider>
         </DataEntryLayout>
     );
 };
---
+++ apps/modernization-ui/src/apps/patient/add/basic/phoneEmail/BasicPhoneEmailFields.tsx
@@ -0,0 +1,121 @@
+import { Controller, useFormContext } from 'react-hook-form';
+import { EntryFieldsProps } from 'design-system/entry';
+import { validEmailRule } from 'validation/entry';
+import { PhoneNumberInputField, validPhoneNumberRule } from 'libs/demographics/contact';
+import { Input } from 'components/FormInputs/Input';
+import { BasicPhoneEmail } from '../entry';
+import { MaskedTextInputField } from 'design-system/input/text';
+
+const HOME_PHONE_LABEL = 'Home phone';
+const WORK_PHONE_LABEL = 'Work phone';
+const CELL_PHONE_LABEL = 'Cell phone';
+
+type BasicPhoneEmailFieldsProps = EntryFieldsProps;
+
+export const BasicPhoneEmailFields = ({ orientation = 'horizontal' }: BasicPhoneEmailFieldsProps) => {
+    const { control } = useFormContext<BasicPhoneEmail>();
+
+    return (
+        <section>
+            <Controller
+                control={control}
+                name="home"
+                rules={validPhoneNumberRule(HOME_PHONE_LABEL)}
+                render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
+                    <PhoneNumberInputField
+                        id={name}
+                        sizing="compact"
+                        label={HOME_PHONE_LABEL}
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        orientation={orientation}
+                        error={error?.message}
+                    />
+                )}
+            />
+            <Controller
+                control={control}
+                name="work.phone"
+                rules={validPhoneNumberRule(WORK_PHONE_LABEL)}
+                render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
+                    <PhoneNumberInputField
+                        id={name}
+                        sizing="compact"
+                        label={WORK_PHONE_LABEL}
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        orientation={orientation}
+                        error={error?.message}
+                    />
+                )}
+            />
+
+            <Controller
+                control={control}
+                name="work.extension"
+                rules={{
+                    pattern: {
+                        value: /^\+?\d{1,20}$/,
+                        message: 'An Extension should be 1 to 20 digits'
+                    }
+                }}
+                render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
+                    <MaskedTextInputField
+                        id={name}
+                        label="Work phone extension"
+                        mask="____________________"
+                        pattern="^\+?\d{1,20}$"
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        orientation={orientation}
+                        error={error?.message}
+                    />
+                )}
+            />
+
+            <Controller
+                control={control}
+                name="cell"
+                rules={validPhoneNumberRule(CELL_PHONE_LABEL)}
+                render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
+                    <PhoneNumberInputField
+                        id={name}
+                        sizing="compact"
+                        label={CELL_PHONE_LABEL}
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        orientation={orientation}
+                        error={error?.message}
+                    />
+                )}
+            />
+
+            <Controller
+                control={control}
+                name="email"
+                rules={{
+                    ...validEmailRule(100)
+                }}
+                render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
+                    <Input
+                        sizing="compact"
+                        label="Email"
+                        orientation={orientation}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        defaultValue={value}
+                        type="text"
+                        htmlFor={name}
+                        id={name}
+                        name={name}
+                        error={error?.message}
+                    />
+                )}
+            />
+        </section>
+    );
+};
