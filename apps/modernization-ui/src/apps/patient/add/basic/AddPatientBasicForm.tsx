1fa652140aa88c4ac7519d2596c378cb269e0619
Joe Agster <joe.agster@cdw.com>
2024-12-11 13:49:00 -0800

    Add identification section

--- apps/modernization-ui/src/apps/patient/add/basic/AddPatientBasicForm.tsx
+++ apps/modernization-ui/src/apps/patient/add/basic/AddPatientBasicForm.tsx
@@ -1,14 +1,30 @@
 import { Card } from 'design-system/card';
 import styles from './add-patient-basic-form.module.scss';
 import { BasicPhoneEmailFields } from './phoneEmail';
+import { BasicIdentificationRepeatingBlock } from './identification';
+import { Controller, useFormContext } from 'react-hook-form';
+import { BasicNewPatientEntry } from './entry';
 
 export const AddPatientBasicForm = () => {
+    const { control } = useFormContext<BasicNewPatientEntry>();
     return (
         <div className={styles.addPatientForm}>
             <div className={styles.formContent}>
                 <Card id="phoneEmail" title="Phone & Email">
                     <BasicPhoneEmailFields />
                 </Card>
+                <Controller
+                    control={control}
+                    name="identifications"
+                    render={({ field: { onChange, value, name } }) => (
+                        <BasicIdentificationRepeatingBlock
+                            id={name}
+                            values={value}
+                            onChange={onChange}
+                            isDirty={() => {}}
+                        />
+                    )}
+                />
             </div>
         </div>
     );
