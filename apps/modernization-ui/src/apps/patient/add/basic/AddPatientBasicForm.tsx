f2bee64c14ee3a6e0f384b1120333abcf8c916f8
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2024-12-11 09:49:45 -0500

    [Cnft1 3319]: phone email basic add section (#2087)
    * adding phoneemailbasicfields and tests supporting
    
    * moving card to design system folder and changing input type to textinput field
    
    * compact sizing for work phone extension

--- apps/modernization-ui/src/apps/patient/add/basic/AddPatientBasicForm.tsx
+++ apps/modernization-ui/src/apps/patient/add/basic/AddPatientBasicForm.tsx
@@ -1,5 +1,15 @@
-import styles from './add-patient-basic.module.scss';
+import { Card } from 'design-system/card';
+import styles from './add-patient-basic-form.module.scss';
+import { BasicPhoneEmailFields } from './phoneEmail';
 
 export const AddPatientBasicForm = () => {
-    return <div className={styles.formContent}>Basic form content</div>;
+    return (
+        <div className={styles.addPatientForm}>
+            <div className={styles.formContent}>
+                <Card id="phoneEmail" title="Phone & Email">
+                    <BasicPhoneEmailFields />
+                </Card>
+            </div>
+        </div>
+    );
 };
