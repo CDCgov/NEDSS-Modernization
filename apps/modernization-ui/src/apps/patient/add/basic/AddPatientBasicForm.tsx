82ce6f5cf83e4064ddf82a7a289026c2a26f2f38
Joe Agster <98052312+joeaxcdw@users.noreply.github.com>
2024-12-12 13:22:03 -0800

    CNFT1-3392: Identification section for basic patient form (#2097)
    * Components for identification section basic form
    
    * Add identification section

--- apps/modernization-ui/src/apps/patient/add/basic/AddPatientBasicForm.tsx
+++ apps/modernization-ui/src/apps/patient/add/basic/AddPatientBasicForm.tsx
@@ -1,9 +1,13 @@
 import { Card } from 'design-system/card';
 import styles from './add-patient-basic-form.module.scss';
 import { BasicPhoneEmailFields } from './phoneEmail';
+import { BasicIdentificationRepeatingBlock } from './identification';
+import { Controller, useFormContext } from 'react-hook-form';
+import { BasicNewPatientEntry } from './entry';
 import { AdministrativeEntryFields } from 'apps/patient/data/administrative/AdministrativeEntryFields';
 
 export const AddPatientBasicForm = () => {
+    const { control } = useFormContext<BasicNewPatientEntry>();
     return (
         <div className={styles.addPatientForm}>
             <div className={styles.formContent}>
@@ -20,6 +24,18 @@ export const AddPatientBasicForm = () => {
                 <Card id="phoneEmail" title="Phone & Email">
                     <BasicPhoneEmailFields />
                 </Card>
+                <Controller
+                    control={control}
+                    name="identifications"
+                    render={({ field: { onChange, value, name } }) => (
+                        <BasicIdentificationRepeatingBlock
+                            id={name}
+                            values={value}
+                            onChange={onChange}
+                            isDirty={() => {}}
+                        />
+                    )}
+                />
             </div>
         </div>
     );
