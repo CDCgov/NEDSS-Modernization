2dae250ab261202582e749fbf1a7578d35656f25
Joe Agster <98052312+joeaxcdw@users.noreply.github.com>
2024-12-18 07:42:37 -0800

    Fix section name and href for personal details (#2117)

--- apps/modernization-ui/src/apps/patient/add/basic/sections.ts
+++ apps/modernization-ui/src/apps/patient/add/basic/sections.ts
@@ -3,7 +3,7 @@ import { NavSection } from 'design-system/inPageNavigation/InPageNavigation';
 export const sections: NavSection[] = [
     { id: 'administrative', label: 'Administrative' },
     { id: 'name', label: 'Name' },
-    { id: 'otherInformation', label: 'Other information' },
+    { id: 'personalDetails', label: 'Personal details' },
     { id: 'addresses', label: 'Address' },
     { id: 'phoneEmail', label: 'Phone & email' },
     { id: 'raceEthnicity', label: 'Ethnicity and race' },
