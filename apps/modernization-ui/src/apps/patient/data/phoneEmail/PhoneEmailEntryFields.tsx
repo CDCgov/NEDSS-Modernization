9b019f6caf983802d11f11b435dcd9c40f797eaf
benlambert_ignw <benjamin.lambert@cdw.com>
2024-12-30 11:45:17 -0700

    change casing back

--- apps/modernization-ui/src/apps/patient/data/phoneEmail/PhoneEmailEntryFields.tsx
+++ apps/modernization-ui/src/apps/patient/data/phoneEmail/PhoneEmailEntryFields.tsx
@@ -91,7 +91,7 @@ export const PhoneEmailEntryFields = ({ orientation = 'horizontal' }: PhoneEmail
                 rules={{
                     pattern: {
                         value: /^\+?\d{1,20}$/,
-                        message: 'A country code should be 1 to 20 digits'
+                        message: 'A Country code should be 1 to 20 digits'
                     }
                 }}
                 render={({ field: { onChange, value, onBlur, name }, fieldState: { error } }) => (
