517151a15ca9caa29b364bb94f301696ab0c63f1
hclarkEnq <hclark@enquizit.com>
2024-12-31 12:44:35 -0500

    adding min width for right side menu to fix wrapping of text and fixing ids

--- apps/modernization-ui/src/apps/patient/add/basic/sections.ts
+++ apps/modernization-ui/src/apps/patient/add/basic/sections.ts
@@ -4,7 +4,7 @@ export const sections: NavSection[] = [
     { id: 'administrative', label: 'Administrative' },
     { id: 'name', label: 'Name' },
     { id: 'personalDetails', label: 'Personal details' },
-    { id: 'addresses', label: 'Address' },
+    { id: 'address', label: 'Address' },
     { id: 'phoneEmail', label: 'Phone & email' },
     { id: 'raceEthnicity', label: 'Ethnicity & race' },
     { id: 'identifications', label: 'Identification' }
