21fad4d67b112d8ca92bf8f3b8bd3a1faf7f32de
Joe Agster <joe.agster@cdw.com>
2025-01-06 12:12:05 -0800

    Clean comments

--- apps/modernization-api/src/main/java/gov/cdc/nbs/search/redirect/simple/SimplePatientSearchCriteria.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/search/redirect/simple/SimplePatientSearchCriteria.java
@@ -1,15 +1,10 @@
 package gov.cdc.nbs.search.redirect.simple;
 
-//import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import gov.cdc.nbs.option.Option;
 import gov.cdc.nbs.search.criteria.date.DateCriteria;
-//import gov.cdc.nbs.time.json.FormattedLocalDateJsonSerializer;
-
-//import java.time.LocalDate;
 
 record SimplePatientSearchCriteria(
         SimplePatientSearchNameCriteria name,
-        //@JsonSerialize(using = FormattedLocalDateJsonSerializer.class) LocalDate dateOfBirth,
         DateCriteria bornOn,
         Option gender,
         String id,
