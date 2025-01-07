22e6204ba22860fb20f8e56a409fd6e18dc6c806
Lakshmi Narayanan Ramakrishnan <132704359+lramakr@users.noreply.github.com>
2025-01-07 11:33:29 -0700

    CNT-119 CNT-120 Dedupe Add Organization and Add Provider (#2156)

--- testing/regression/cypress/e2e/pages/nbs-classic/organization.page.js
+++ testing/regression/cypress/e2e/pages/nbs-classic/organization.page.js
@@ -17,6 +17,25 @@ class ClassicPatientSearchPage {
     cy.contains('View').eq(0).click()
   }
 
+  navigateToAddOrganisation() {
+    this.navigateToClassicOrganizationSearchPane()
+    this.enterNameInClassicSearchOrganizationPage('test')
+    this.clickSearchBtnInClassicOrganizationSearchPane()
+  }
+
+  clickAddButtonOnAddOrganisation() {
+    cy.get('#Add').eq(0).click()
+  }
+
+  enterQuickCode() {
+    const newName = Math.random().toString(36).substring(2, 8)
+    cy.get('input[name="quickCodeIdDT.rootExtensionTxt"]').eq(0).type(newName)
+  }
+
+  clickSubmitBtnOnOrganisation() {
+    cy.get('input[name="Submit"][id="Submit"]').eq(0).click()
+  }
+
 }
 
 export default new ClassicPatientSearchPage();
