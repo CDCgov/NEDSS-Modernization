1f9fbb2e53c99899fd41d7eee4fda6e6d1b1c284
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2025-01-07 15:17:02 -0600

    [CNFT1-3656] Fixes direct patient routing rules (#2154)
    * fixes direct patient routing rules
    
    * version

--- apps/nbs-gateway/src/test/java/gov/cdc/nbs/gateway/patient/profile/PatientProfileRouteLocatorConfigurationTest.java
+++ apps/nbs-gateway/src/test/java/gov/cdc/nbs/gateway/patient/profile/PatientProfileRouteLocatorConfigurationTest.java
@@ -1,11 +1,16 @@
 package gov.cdc.nbs.gateway.patient.profile;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.stream.Stream;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
@@ -25,53 +30,42 @@ class PatientProfileRouteLocatorConfigurationTest {
       .options(wireMockConfig().port(10001))
       .build();
 
+  @RegisterExtension
+  static WireMockExtension nbs6 = WireMockExtension.newInstance()
+      .options(wireMockConfig().port(10000))
+      .build();
+
   @Autowired
   WebTestClient webClient;
 
-  @Test
-  void should_route_to_service_when_ContextAction_is_ViewFile() {
+  @ParameterizedTest
+  @ValueSource(strings = {"ViewFile", "FileSummary"})
+  void should_route_to(final String action) {
     service.stubFor(get(urlPathMatching("/nbs/redirect/patientProfile/summary/return\\\\?.*")).willReturn(ok()));
 
     webClient
         .get().uri(
             builder -> builder
-                .path("/nbs/any")
-                .queryParam("ContextAction", "ViewFile")
+                .path("/nbs/any/path")
+                .queryParam("ContextAction", action)
                 .build()
         )
         .exchange()
         .expectStatus()
         .isOk();
-
   }
 
-  @Test
-  void should_route_to_service_when_ContextAction_is_FileSummary() {
-    service.stubFor(get(urlPathMatching("/nbs/redirect/patientProfile/summary/return\\\\?.*")).willReturn(ok()));
-
-    webClient
-        .get().uri(
-            builder -> builder
-                .path("/nbs/any")
-                .queryParam("ContextAction", "FileSummary")
-                .build()
-        )
-        .exchange()
-        .expectStatus()
-        .isOk();
-
-  }
-
-  @Test
-  void should_route_to_patient_profile_from_from_patient_name_in_an_event_search_result() {
+  @ParameterizedTest
+  @MethodSource("directParameters")
+  void should_route_directly_to_the_patient_profile(final String action, final String type) {
     service.stubFor(get(urlPathMatching("/nbs/redirect/patient/profile")).willReturn(ok()));
 
     webClient
         .get().uri(
             builder -> builder
-                .path("/nbs/PatientSearchResults1.do")
-                .queryParam("ContextAction", "ViewFile")
-                .queryParam("uid", "1051")
+                .path("/nbs/any/path")
+                .queryParam("ContextAction", action)
+                .queryParam(type, "1051")
                 .build()
         )
         .exchange()
@@ -79,16 +73,26 @@ class PatientProfileRouteLocatorConfigurationTest {
         .isOk();
   }
 
-  @Test
-  void should_route_to_patient_profile_service_from_patient_name_in_documents_requiring_review_queue() {
-    service.stubFor(get(urlPathMatching("/nbs/redirect/patient/profile")).willReturn(ok()));
+  static Stream<Arguments> directParameters() {
+    return Stream.of(
+        Arguments.arguments("ViewFile", "uid"),
+        Arguments.arguments("ViewFile", "MPRUid"),
+        Arguments.arguments("FileSummary", "uid"),
+        Arguments.arguments("FileSummary", "MPRUid")
+    );
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"uid", "MPRUid"})
+  void should_only_route_to_the_patient_profile_with_the_known_context_action(final String type) {
+    nbs6.stubFor(get(urlPathMatching("/nbs/any/path")).willReturn(ok()));
 
     webClient
         .get().uri(
             builder -> builder
-                .path("/nbs/NewLabReview1.do")
-                .queryParam("ContextAction", "ViewFile")
-                .queryParam("uid", "1051")
+                .path("/nbs/any/path")
+                .queryParam("ContextAction", "Other")
+                .queryParam(type, "1051")
                 .build()
         )
         .exchange()
