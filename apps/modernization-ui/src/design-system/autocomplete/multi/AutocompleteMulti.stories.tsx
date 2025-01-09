8159b7a8b6ce5b398ad78aebfff49f4162b8baaa
Joe Agster <joe.agster@cdw.com>
2025-01-09 11:22:15 -0800

    New story

---
+++ apps/modernization-ui/src/design-system/autocomplete/multi/AutocompleteMulti.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { AutocompleteMulti } from './AutocompleteMulti';
+import { asSelectable, Selectable } from 'options';
+
+const meta = {
+    title: 'Design System/AutocompleteMulti',
+    component: AutocompleteMulti
+} satisfies Meta<typeof AutocompleteMulti>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const options: Selectable[] = [
+    asSelectable('apple', 'Apple'),
+    asSelectable('banana', 'Banana'),
+    asSelectable('mango', 'Mango'),
+    asSelectable('orange', 'Orange'),
+    asSelectable('watermelon', 'Watermelon')
+];
+const [, banana, mango] = options;
+
+const resolver = (searchText: string) => {
+    return Promise.resolve(options.filter((option) => option.name.toLowerCase().includes(searchText.toLowerCase())));
+};
+
+export const Default: Story = {
+    args: {
+        id: 'autocomplete-default',
+        name: 'autocomplete-default',
+        label: 'Default',
+        options: [...options],
+        resolver: resolver,
+        placeholder: 'Select options'
+    }
+};
+
+export const WithPreselectedOptions: Story = {
+    args: {
+        id: 'autocomplete-default',
+        name: 'autocomplete-default',
+        label: 'Default',
+        options: [...options],
+        value: [banana, mango],
+        resolver: resolver,
+        placeholder: 'Select options'
+    }
+};
