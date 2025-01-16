dd7f60664571ccaf7503b5a58a39f1d94b52d8f2
Joe Agster <joe.agster@cdw.com>
2025-01-16 13:30:37 -0800

    Delete and move to repeating block

--- apps/modernization-ui/src/apps/patient/add/extended/inputs/race/DetailedRaceDisplay.tsx
+++
@@ -1,8 +0,0 @@
-import { RaceEntry } from 'apps/patient/data/race';
-import { NoData } from 'components/NoData';
-
-type Props = {
-    entry: RaceEntry;
-};
-export const DetailedRaceDisplay = ({ entry }: Props) =>
-    entry.detailed?.map((v) => v.name).join(', ') || <NoData display="dashes" />;
