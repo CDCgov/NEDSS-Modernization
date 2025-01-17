34ae6ffd3199e21d0bafc782b272673bbca3bc2c
Joe Agster <joe.agster@cdw.com>
2025-01-17 10:10:02 -0800

    clean up

--- apps/modernization-ui/src/authorization/IdleTimer.tsx
+++ apps/modernization-ui/src/authorization/IdleTimer.tsx
@@ -10,21 +10,16 @@ interface IdleTimerProps {
 
 const IdleTimer: React.FC<IdleTimerProps> = ({ timeout, warningTimeout, onIdle }) => {
     const [idle, setIdle] = useState(false);
-    // const [showWarningModal, setShowWarningModal] = useState(false);
     const [idleTimer, setIdleTimer] = useState<number | undefined>();
     const [warningTimer, setWarningTimer] = useState<number | undefined>();
     const warningMins = Math.ceil(warningTimeout / 60000);
 
     // this starts the warning timer and shows the warning modal
     const startWarningTimer = useCallback(() => {
-        console.log('idle', 'start warning');
         setIdle(true);
         clearTimeout(warningTimer);
         setWarningTimer(
             window.setTimeout(() => {
-                console.log('idle', 'warning timeout');
-                // setShowWarningModal(false);
-                // setIdle(false);
                 onIdle();
             }, warningTimeout)
         );
@@ -32,24 +27,19 @@ const IdleTimer: React.FC<IdleTimerProps> = ({ timeout, warningTimeout, onIdle }
 
     // this resets the idle timer, when there is mouse activity or the warning modal is dismissed
     const resetIdleTimer = useCallback(() => {
-        console.log('idle', 'reset idle', idleTimer, warningTimer);
         clearTimeout(idleTimer);
         clearTimeout(warningTimer);
         setIdle(false);
         setIdleTimer(
             window.setTimeout(() => {
-                // setIdle(true);
-                // removeEventListeners();
                 startWarningTimer();
             }, timeout)
         );
         setWarningTimer(undefined);
     }, [idleTimer, warningTimer, startWarningTimer]);
-
     const debouncedResetIdleTimer = useCallback(debounce(resetIdleTimer, 100), [resetIdleTimer]);
 
     const handleActivity = useCallback(() => {
-        console.log('idle', 'handleActivity');
         if (!idle) {
             debouncedResetIdleTimer();
         }
@@ -57,11 +47,13 @@ const IdleTimer: React.FC<IdleTimerProps> = ({ timeout, warningTimeout, onIdle }
     const handleActivityRef = useRef(handleActivity);
 
     useEffect(() => {
+        // set up ref to the handleActivity function
+        // we do this so we can use the same function in the event listeners
         handleActivityRef.current = handleActivity;
     }, [handleActivity]);
 
     useEffect(() => {
-        console.log('idle', 'start timer');
+        // first call to start the idle timer
         debouncedResetIdleTimer();
         return () => {
             clearTimeout(idleTimer);
@@ -69,27 +61,8 @@ const IdleTimer: React.FC<IdleTimerProps> = ({ timeout, warningTimeout, onIdle }
             debouncedResetIdleTimer.cancel();
         };
     }, []);
-    // const clearIdleTimer = () => {
-    //     clearTimeout(idleTimer);
-    //     setIdleTimer(undefined);
-    // };
-
-    // const clearWarningTimer = () => {
-    //     clearTimeout(warningTimer);
-    //     setWarningTimer(undefined);
-    // };
-
-    // const [, handleActivity] = useReducer((state) => {
-    //     console.log('idle', 'handleActivity');
-    //     if (!idle) {
-    //         debouncedResetIdleTimer();
-    //     }
-    //     return state;
-    // }, null);
 
     useEffect(() => {
-        console.log('idle', 'init events');
-
         const handleEvent = () => {
             handleActivityRef.current();
         };
@@ -111,39 +84,16 @@ const IdleTimer: React.FC<IdleTimerProps> = ({ timeout, warningTimeout, onIdle }
         addEventListeners();
 
         return () => {
-            // clearTimeout(idleTimer);
-            // clearTimeout(warningTimer);
-            // debouncedResetIdleTimer.cancel();
             removeEventListeners();
-            // debouncedResetIdleTimer.cancel();
         };
     }, [timeout, onIdle]);
 
-    // useEffect(() => {
-    //     console.log('idle', 'idle changed', idle);
-
-    //     if (!idle && !showWarningModal) {
-    //         debouncedResetIdleTimer();
-    //     }
-    // }, [idle, showWarningModal, debouncedResetIdleTimer]);
-
-    // const handleActivity = useCallback(() => {
-    //     console.log('idle', 'handleActivity');
-    //     if (!showWarningModal) {
-    //         debouncedResetIdleTimer();
-    //     }
-    // }, [showWarningModal, debouncedResetIdleTimer]);
-
     const handleContinue = () => {
-        console.log('idle', 'continue');
-        // setShowWarningModal(false);
         setIdle(false);
         debouncedResetIdleTimer();
     };
 
     const handleLogout = () => {
-        console.log('idle', 'logout');
-        // setIdle(false);
         onIdle();
     };
 
