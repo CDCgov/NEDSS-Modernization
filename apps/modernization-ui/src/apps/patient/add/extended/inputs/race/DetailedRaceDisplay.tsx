b747e62cde5a9ceec1a1bc51a650f5849436a42e
Joe Agster <98052312+joeaxcdw@users.noreply.github.com>
2025-01-17 09:01:20 -0800

    CNFT1-3678: New patient extended - Race repeating block blank value to dashes (#2181)
    * Display dashes when field is empty
    
    * Delete and move to repeating block
    
    * Implement detailed race render as string

--- apps/modernization-ui/src/apps/patient/add/extended/inputs/race/DetailedRaceDisplay.tsx
+++
@@ -1,6 +0,0 @@
-import { RaceEntry } from 'apps/patient/data/race';
-
-type Props = {
-    entry: RaceEntry;
-};
-export const DetailedRaceDisplay = ({ entry }: Props) => entry.detailed?.map((v) => v.name).join(', ');
