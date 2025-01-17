127026f831b632002e2a3798136d60644b7405bd
Joe Agster <joe.agster@cdw.com>
2025-01-17 10:00:48 -0800

    rewrite of idle logic with unit tests

--- apps/modernization-ui/src/authorization/IdleTimer.tsx
+++ apps/modernization-ui/src/authorization/IdleTimer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import debounce from 'lodash.debounce';
 import { Confirmation } from 'design-system/modal';
 
@@ -10,105 +10,155 @@ interface IdleTimerProps {
 
 const IdleTimer: React.FC<IdleTimerProps> = ({ timeout, warningTimeout, onIdle }) => {
     const [idle, setIdle] = useState(false);
-    const [showWarningModal, setShowWarningModal] = useState(false);
+    // const [showWarningModal, setShowWarningModal] = useState(false);
     const [idleTimer, setIdleTimer] = useState<number | undefined>();
     const [warningTimer, setWarningTimer] = useState<number | undefined>();
     const warningMins = Math.ceil(warningTimeout / 60000);
 
-    useEffect(() => {
-        //let idleTimer: number | undefined = undefined;
-        //let warningTimer: number | undefined = undefined;
-        console.log('idle', 'init', timeout, warningTimeout, idleTimer, warningTimer, idle);
+    // this starts the warning timer and shows the warning modal
+    const startWarningTimer = useCallback(() => {
+        console.log('idle', 'start warning');
+        setIdle(true);
+        clearTimeout(warningTimer);
+        setWarningTimer(
+            window.setTimeout(() => {
+                console.log('idle', 'warning timeout');
+                // setShowWarningModal(false);
+                // setIdle(false);
+                onIdle();
+            }, warningTimeout)
+        );
+    }, [onIdle, warningTimeout, warningTimer]);
 
-        const resetIdleTimer = () => {
-            console.log('idle', 'reset idle', idleTimer, warningTimer);
+    // this resets the idle timer, when there is mouse activity or the warning modal is dismissed
+    const resetIdleTimer = useCallback(() => {
+        console.log('idle', 'reset idle', idleTimer, warningTimer);
+        clearTimeout(idleTimer);
+        clearTimeout(warningTimer);
+        setIdle(false);
+        setIdleTimer(
+            window.setTimeout(() => {
+                // setIdle(true);
+                // removeEventListeners();
+                startWarningTimer();
+            }, timeout)
+        );
+        setWarningTimer(undefined);
+    }, [idleTimer, warningTimer, startWarningTimer]);
+
+    const debouncedResetIdleTimer = useCallback(debounce(resetIdleTimer, 100), [resetIdleTimer]);
+
+    const handleActivity = useCallback(() => {
+        console.log('idle', 'handleActivity');
+        if (!idle) {
+            debouncedResetIdleTimer();
+        }
+    }, [idle, debouncedResetIdleTimer]);
+    const handleActivityRef = useRef(handleActivity);
+
+    useEffect(() => {
+        handleActivityRef.current = handleActivity;
+    }, [handleActivity]);
+
+    useEffect(() => {
+        console.log('idle', 'start timer');
+        debouncedResetIdleTimer();
+        return () => {
             clearTimeout(idleTimer);
             clearTimeout(warningTimer);
-            setIdleTimer(
-                window.setTimeout(() => {
-                    setIdle(true);
-                    removeEventListeners();
-                    startWarningTimer();
-                }, timeout)
-            );
-            setWarningTimer(undefined);
+            debouncedResetIdleTimer.cancel();
         };
-        const debouncedResetIdleTimer = debounce(resetIdleTimer, 100);
+    }, []);
+    // const clearIdleTimer = () => {
+    //     clearTimeout(idleTimer);
+    //     setIdleTimer(undefined);
+    // };
 
-        const startWarningTimer = () => {
-            console.log('idle', 'start warning');
-            setShowWarningModal(true);
-            clearTimeout(warningTimer);
-            setWarningTimer(
-                window.setTimeout(() => {
-                    console.log('idle', 'warning timeout');
-                    onIdle();
-                    setShowWarningModal(false);
-                }, warningTimeout)
-            );
-        };
+    // const clearWarningTimer = () => {
+    //     clearTimeout(warningTimer);
+    //     setWarningTimer(undefined);
+    // };
 
-        const removeEventListeners = () => {
-            const events = ['mousemove', 'keydown', 'mousedown', 'touchstart'];
-            events.forEach((event) => {
-                document.removeEventListener(event, handleActivity);
-            });
+    // const [, handleActivity] = useReducer((state) => {
+    //     console.log('idle', 'handleActivity');
+    //     if (!idle) {
+    //         debouncedResetIdleTimer();
+    //     }
+    //     return state;
+    // }, null);
+
+    useEffect(() => {
+        console.log('idle', 'init events');
+
+        const handleEvent = () => {
+            handleActivityRef.current();
         };
 
         const addEventListeners = () => {
             const events = ['mousemove', 'keydown', 'mousedown', 'touchstart'];
             events.forEach((event) => {
-                document.addEventListener(event, handleActivity);
+                document.addEventListener(event, handleEvent);
             });
         };
 
-        const handleActivity = () => {
-            // if (!showWarningModal) {
-            //     // once warning modal is displayed, user must interact with continue or logout buttons
-            //     resetIdleTimer();
-            // }
-            // resetIdleTimer();
-            debouncedResetIdleTimer();
+        const removeEventListeners = () => {
+            const events = ['mousemove', 'keydown', 'mousedown', 'touchstart'];
+            events.forEach((event) => {
+                document.removeEventListener(event, handleEvent);
+            });
         };
 
-        if (!idle) {
-            addEventListeners();
-            resetIdleTimer();
-        }
+        addEventListeners();
 
         return () => {
-            clearTimeout(idleTimer);
-            clearTimeout(warningTimer);
+            // clearTimeout(idleTimer);
+            // clearTimeout(warningTimer);
+            // debouncedResetIdleTimer.cancel();
             removeEventListeners();
+            // debouncedResetIdleTimer.cancel();
         };
-    }, [timeout, onIdle, idle]);
+    }, [timeout, onIdle]);
+
+    // useEffect(() => {
+    //     console.log('idle', 'idle changed', idle);
+
+    //     if (!idle && !showWarningModal) {
+    //         debouncedResetIdleTimer();
+    //     }
+    // }, [idle, showWarningModal, debouncedResetIdleTimer]);
+
+    // const handleActivity = useCallback(() => {
+    //     console.log('idle', 'handleActivity');
+    //     if (!showWarningModal) {
+    //         debouncedResetIdleTimer();
+    //     }
+    // }, [showWarningModal, debouncedResetIdleTimer]);
 
     const handleContinue = () => {
         console.log('idle', 'continue');
-        setShowWarningModal(false);
+        // setShowWarningModal(false);
         setIdle(false);
+        debouncedResetIdleTimer();
     };
 
     const handleLogout = () => {
         console.log('idle', 'logout');
-        setShowWarningModal(false);
+        // setIdle(false);
         onIdle();
     };
 
     return (
-        <>
-            {showWarningModal && (
-                <Confirmation
-                    title="Timeout"
-                    confirmText="Continue"
-                    cancelText="Logout"
-                    forceAction={true}
-                    onConfirm={handleContinue}
-                    onCancel={handleLogout}>
-                    Your session will timeout in {warningMins} minutes. Would you like to continue your session in NBS?
-                </Confirmation>
-            )}
-        </>
+        idle && (
+            <Confirmation
+                title="Timeout"
+                confirmText="Continue"
+                cancelText="Logout"
+                forceAction={true}
+                onConfirm={handleContinue}
+                onCancel={handleLogout}>
+                Your session will timeout in {warningMins} minutes. Would you like to continue your session in NBS?
+            </Confirmation>
+        )
     );
 };
 
