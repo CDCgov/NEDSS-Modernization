f81b100388b3224de4fa8adc4813700dc721a54c
Joe Agster <98052312+joeaxcdw@users.noreply.github.com>
2025-01-20 08:46:01 -0800

    CNFT1-3685: Idle timeout logout modal (#2184)
    * add modal to idle timer WIP
    
    * rewrite of idle logic with unit tests
    
    * clean up
    
    * Revert

--- apps/modernization-ui/src/authorization/IdleTimer.tsx
+++ apps/modernization-ui/src/authorization/IdleTimer.tsx
@@ -1,5 +1,6 @@
-import { Button } from 'components/button';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import debounce from 'lodash.debounce';
+import { Confirmation } from 'design-system/modal';
 
 interface IdleTimerProps {
     timeout: number; // Timeout in milliseconds
@@ -9,75 +10,105 @@ interface IdleTimerProps {
 
 const IdleTimer: React.FC<IdleTimerProps> = ({ timeout, warningTimeout, onIdle }) => {
     const [idle, setIdle] = useState(false);
-    const [showWarningModal, setShowWarningModal] = useState(false);
+    const [idleTimer, setIdleTimer] = useState<number | undefined>();
+    const [warningTimer, setWarningTimer] = useState<number | undefined>();
+    const warningMins = Math.ceil(warningTimeout / 60000);
+
+    // this starts the warning timer and shows the warning modal
+    const startWarningTimer = useCallback(() => {
+        setIdle(true);
+        clearTimeout(warningTimer);
+        setWarningTimer(
+            window.setTimeout(() => {
+                onIdle();
+            }, warningTimeout)
+        );
+    }, [onIdle, warningTimeout, warningTimer]);
+
+    // this resets the idle timer, when there is mouse activity or the warning modal is dismissed
+    const resetIdleTimer = useCallback(() => {
+        clearTimeout(idleTimer);
+        clearTimeout(warningTimer);
+        setIdle(false);
+        setIdleTimer(
+            window.setTimeout(() => {
+                startWarningTimer();
+            }, timeout)
+        );
+        setWarningTimer(undefined);
+    }, [idleTimer, warningTimer, startWarningTimer]);
+    const debouncedResetIdleTimer = useCallback(debounce(resetIdleTimer, 100), [resetIdleTimer]);
+
+    const handleActivity = useCallback(() => {
+        if (!idle) {
+            debouncedResetIdleTimer();
+        }
+    }, [idle, debouncedResetIdleTimer]);
+    const handleActivityRef = useRef(handleActivity);
 
     useEffect(() => {
-        let idleTimer: number;
-        let warningTimer: number;
+        // set up ref to the handleActivity function
+        // we do this so we can use the same function in the event listeners
+        handleActivityRef.current = handleActivity;
+    }, [handleActivity]);
 
-        const resetIdleTimer = () => {
+    useEffect(() => {
+        // first call to start the idle timer
+        debouncedResetIdleTimer();
+        return () => {
             clearTimeout(idleTimer);
-            idleTimer = window.setTimeout(() => {
-                setIdle(true);
-                removeEventListeners();
-                startWarningTimer();
-            }, timeout);
-        };
-
-        const startWarningTimer = () => {
-            setShowWarningModal(true);
             clearTimeout(warningTimer);
-            warningTimer = window.setTimeout(() => {
-                onIdle();
-                setShowWarningModal(false);
-            }, warningTimeout);
+            debouncedResetIdleTimer.cancel();
         };
+    }, []);
 
-        const removeEventListeners = () => {
-            const events = ['mousemove', 'keydown', 'mousedown', 'touchstart'];
-            events.forEach((event) => {
-                document.removeEventListener(event, handleActivity);
-            });
+    useEffect(() => {
+        const handleEvent = () => {
+            handleActivityRef.current();
         };
 
         const addEventListeners = () => {
             const events = ['mousemove', 'keydown', 'mousedown', 'touchstart'];
             events.forEach((event) => {
-                document.addEventListener(event, handleActivity);
+                document.addEventListener(event, handleEvent);
             });
         };
 
-        const handleActivity = () => {
-            resetIdleTimer();
+        const removeEventListeners = () => {
+            const events = ['mousemove', 'keydown', 'mousedown', 'touchstart'];
+            events.forEach((event) => {
+                document.removeEventListener(event, handleEvent);
+            });
         };
 
-        if (!idle) {
-            addEventListeners();
-            resetIdleTimer();
-        }
+        addEventListeners();
 
         return () => {
-            clearTimeout(idleTimer);
-            clearTimeout(warningTimer);
             removeEventListeners();
         };
-    }, [timeout, onIdle, idle]);
+    }, [timeout, onIdle]);
+
+    const handleContinue = () => {
+        setIdle(false);
+        debouncedResetIdleTimer();
+    };
+
+    const handleLogout = () => {
+        onIdle();
+    };
 
     return (
-        <>
-            {showWarningModal && (
-                <div className="warning-modal">
-                    <p>Warning: Only 2 minutes remaining of idle time!</p>
-                    <Button
-                        onClick={() => {
-                            setShowWarningModal(false);
-                            setIdle(false);
-                        }}>
-                        I'm still here
-                    </Button>
-                </div>
-            )}
-        </>
+        idle && (
+            <Confirmation
+                title="Timeout"
+                confirmText="Continue"
+                cancelText="Logout"
+                forceAction={true}
+                onConfirm={handleContinue}
+                onCancel={handleLogout}>
+                Your session will timeout in {warningMins} minutes. Would you like to continue your session in NBS?
+            </Confirmation>
+        )
     );
 };
 
