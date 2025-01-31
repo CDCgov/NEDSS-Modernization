48b76ace30e5ffdb3a1dc8741b057fbb03ff1d68
Joe Agster <joe.agster@cdw.com>
2025-01-31 09:32:02 -0800

    Sort storybook components

--- apps/modernization-ui/.storybook/preview.ts
+++ apps/modernization-ui/.storybook/preview.ts
@@ -1,6 +1,13 @@
 import type { Preview } from '@storybook/react';
 import 'styles/global.scss';
 
+// v7-style sort
+function storySort(a, b) {
+    return a.title === b.title
+      ? 0
+      : a.id.localeCompare(b.id, undefined, { numeric: true });
+}
+
 const preview: Preview = {
     parameters: {
         controls: {
@@ -8,6 +15,9 @@ const preview: Preview = {
                 color: /(background|color)$/i,
                 date: /Date$/i
             }
+        },
+        options: {
+            storySort: (a, b) => a.title === b.title ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true })
         }
     }
 };
