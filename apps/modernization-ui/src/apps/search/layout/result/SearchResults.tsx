76e8b7b897523fb40df6f532616285018074cbdc
Adam Loup <aloup@enquizit.com>
2025-02-03 17:44:00 -0600

    loader

--- apps/modernization-ui/src/apps/search/layout/result/SearchResults.tsx
+++ apps/modernization-ui/src/apps/search/layout/result/SearchResults.tsx
@@ -1,10 +1,11 @@
-import { ReactNode, useEffect, useRef, useState } from 'react';
+import { ReactNode, useLayoutEffect, useRef, useState } from 'react';
 import { Pagination } from 'design-system/Pagination';
+import { LoadingPanel } from 'design-system/loading';
 import { View } from 'apps/search';
 import { SearchResultsHeader } from './header/SearchResultsHeader';
 import { Term } from 'apps/search/terms';
+
 import styles from './search-results.module.scss';
-import { LoadingPanel } from 'design-system/loading';
 
 type Props = {
     children: ReactNode;
@@ -15,39 +16,43 @@ type Props = {
 };
 
 const SearchResults = ({ children, total, view, terms, loading = false }: Props) => {
+    const [contentHeight, setContentHeight] = useState<string>('auto');
     const headerRef = useRef<HTMLDivElement>(null);
     const paginationRef = useRef<HTMLDivElement>(null);
-    const [contentHeight, setContentHeight] = useState<string>('auto');
 
-    const updateContentHeight = () => {
-        requestAnimationFrame(() => {
-            const headerHeight = headerRef.current?.offsetHeight || 0;
-            const paginationHeight = paginationRef.current?.offsetHeight || 0;
-            setContentHeight(`calc(100% - ${headerHeight}px - ${paginationHeight}px)`);
-        });
+    const computeContentHeight = () => {
+        const headerHeight = headerRef.current?.offsetHeight || 0;
+        const paginationHeight = paginationRef.current?.offsetHeight || 0;
+        const offset = headerHeight + paginationHeight;
+
+        return offset > 0 ? `calc(100% - ${offset}px)` : 'auto';
     };
 
-    useEffect(() => {
-        updateContentHeight();
-        window.addEventListener('resize', updateContentHeight);
+    const handleResize = () => {
+        setContentHeight(computeContentHeight());
+    };
 
-        return () => window.removeEventListener('resize', updateContentHeight);
+    useLayoutEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
     }, []);
 
     return (
-        <LoadingPanel loading={loading}>
-            <div className={styles.results}>
-                <div ref={headerRef}>
-                    <SearchResultsHeader className={styles.header} view={view} total={total} terms={terms} />
-                </div>
-                <main className={styles.content} style={{ height: contentHeight }}>
-                    {children}
-                </main>
-                <div ref={paginationRef} className={styles.pagination}>
-                    <Pagination />
-                </div>
+        <div className={styles.results}>
+            <div ref={headerRef}>
+                <SearchResultsHeader className={styles.header} view={view} total={total} terms={terms} />
             </div>
-        </LoadingPanel>
+            <main style={{ height: contentHeight }}>
+                <LoadingPanel loading={loading} className={styles.loader}>
+                    {children}
+                </LoadingPanel>
+            </main>
+            <div ref={paginationRef} className={styles.pagination}>
+                <Pagination />
+            </div>
+        </div>
     );
 };
 
--- apps/modernization-ui/src/design-system/loading/LoadingPanel.tsx
+++ apps/modernization-ui/src/design-system/loading/LoadingPanel.tsx
@@ -3,18 +3,20 @@ import { Loading } from 'components/Spinner';
 
 import styles from './loading-panel.module.scss';
 import { Shown } from 'conditional-render';
+import classNames from 'classnames';
 
 type LoadingPanelProps = {
+    className?: string;
     loading: boolean;
     children: ReactNode | ReactNode[];
 };
 
-const LoadingPanel = ({ loading = false, children }: LoadingPanelProps) => {
+const LoadingPanel = ({ loading = false, children, className }: LoadingPanelProps) => {
     return (
         <>
             <Shown when={loading}>
                 <span className={styles.panel}>
-                    <span className={styles.loader}>
+                    <span className={classNames(styles.loader, className)}>
                         <Loading center />
                     </span>
                 </span>
