d20de962ad2d92d99485125f54368734249b2ba9
Joe Agster <joe.agster@cdw.com>
2025-02-04 08:50:02 -0800

    Story for DataTable

--- apps/modernization-ui/src/design-system/table/DataTable.stories.tsx
+++ apps/modernization-ui/src/design-system/table/DataTable.stories.tsx
@@ -1,6 +1,12 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Column, DataTable } from './DataTable';
+import { MemoryRouter } from 'react-router-dom';
+import { SortingProvider, useSorting } from 'sorting';
+import { ComponentType } from 'react';
+import { Checkbox } from 'design-system/checkbox';
 
+type AnyObject = { [key: string]: any };
+type AnyObjectWithData = AnyObject & { data?: AnyObject[] };
 type Person = {
     id: string;
     name: string;
@@ -42,6 +48,20 @@ const columns: Column<Person>[] = [
         render: (value: Person) => value.email
     }
 ];
+const checkboxColumns: Column<Person>[] = [
+    {
+        id: 'select',
+        name: 'X',
+        render: (value: Person) => (
+            <Checkbox
+                id={value.id}
+                label=" "
+                onChange={(checked) => console.log('Checkbox changed', checked, value.id)}
+            />
+        )
+    } as Column<Person>,
+    ...columns
+];
 
 const data: Person[] = [
     {
@@ -61,10 +81,64 @@ const data: Person[] = [
     }
 ];
 
+function sortData<V extends AnyObject>(data: V[], property?: string, direction?: string) {
+    return data.sort((a: V, b: V) => {
+        if (property) {
+            if (a[property] < b[property]) {
+                return direction === 'desc' ? 1 : -1;
+            }
+            if (a[property] > b[property]) {
+                return direction === 'desc' ? -1 : 1;
+            }
+        }
+        return 0;
+    });
+}
+// , W extends DataTableProps<V>
+const withSortable = <W extends AnyObjectWithData>(Component: ComponentType<W>) => {
+    const SortableComponent = (props: W) => {
+        const { property, direction } = useSorting();
+        console.log('SortableComponent', property, direction, props);
+        const sortedData = sortData(props.data!, property, direction);
+        return <Component {...props} data={sortedData} />;
+    };
+    SortableComponent.displayName = 'SortableComponent';
+    return SortableComponent;
+};
+// const SortableDataTable = withSortable(DataTable);
+
 export const Default: Story = {
     args: {
         id: 'people',
         columns: columns as Column<Person>[],
+        data: data as Person[]
+    }
+};
+
+export const Sortable: Story = {
+    decorators: [
+        (Story) => {
+            const SortableDataTable = withSortable(Story);
+            return (
+                <MemoryRouter>
+                    <SortingProvider>
+                        <SortableDataTable data={data} />
+                    </SortingProvider>
+                </MemoryRouter>
+            );
+        }
+    ],
+    args: {
+        id: 'people',
+        columns: columns.map((column) => ({ ...column, sortable: true })) as Column<Person>[], // make all columns sortable
+        data: data as Person[]
+    }
+};
+
+export const Checkboxes: Story = {
+    args: {
+        id: 'people',
+        columns: checkboxColumns as Column<Person>[],
         data: data as Person[]
     }
 };
