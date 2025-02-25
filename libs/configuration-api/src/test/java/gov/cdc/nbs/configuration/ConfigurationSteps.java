7163ddbee41c7b40dd9d4e3ff849120a8fb2f8d3
Adam Loup <adam.loup@cdwg.com>
2025-02-25 08:27:52 -0600

    smells

--- libs/configuration-api/src/test/java/gov/cdc/nbs/configuration/ConfigurationSteps.java
+++ libs/configuration-api/src/test/java/gov/cdc/nbs/configuration/ConfigurationSteps.java
@@ -1,7 +1,6 @@
 package gov.cdc.nbs.configuration;
 
 import gov.cdc.nbs.testing.support.Active;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.When;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -17,11 +16,6 @@ public class ConfigurationSteps {
     this.requester = requester;
     this.response = response;
   }
-
-//  @Before
-//  public void reset() {
-//    response.reset();
-//  }
 
   @When("I request the frontend configuration")
   public void i_request_the_frontend_configuration() throws Exception {
