2b5d28f841c4aeea3e5b53ba6c281a3447a75637
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-02-25 14:50:29 -0500

    testing and implementation is complete

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/search/phone/PatientSearchResultDetailedPhoneFinder.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/search/phone/PatientSearchResultDetailedPhoneFinder.java
@@ -8,54 +8,58 @@ import java.util.Collection;
 @Component
 class PatientSearchResultDetailedPhoneFinder {
 
-  private static final String QUERY = """
-      select distinct
-        coalesce(
-          [type].code_short_desc_txt,
-          IsNull([locators].cd, '')
-        )                               as [type],
-        coalesce(
-            [use].code_short_desc_txt,
-            IsNull([locators].[use_cd],'')
-        )                               as [use],
-        IsNull([locators].cd, '')       as [type_cd],
-        [locators].use_cd               as [use_cd],
-        [phone_number].phone_nbr_txt    as [phone_number]
-      from Entity_locator_participation [locators]
+	private static final String QUERY = """
+			select distinct
+			  coalesce(
+			    [type].code_short_desc_txt,
+			    IsNull([locators].cd, '')
+			  )                               as [type],
+			  coalesce(
+			      [use].code_short_desc_txt,
+			      IsNull([locators].[use_cd],'')
+			  )                               as [use],
+			  IsNull([locators].cd, '')       as [type_cd],
+			  [locators].use_cd               as [use_cd],
+			  [phone_number].phone_nbr_txt    as [phone_number],
+			  [locators].as_of_date           as [as_of]
+			from Entity_locator_participation [locators]
+			
+			    join Tele_locator [phone_number] on
+			            [phone_number].[tele_locator_uid] = [locators].[locator_uid]
+			        and [phone_number].record_status_cd   = [locators].[record_status_cd]
+			        and [phone_number].phone_nbr_txt is not null
+			
+			    left join  NBS_SRTE..Code_value_general [type] on
+			            [type].code_set_nm = 'EL_TYPE_TELE_PAT'
+			        and [type].code = [locators].cd
+			
+			    left join NBS_SRTE..Code_value_general [use] on
+			            [use].code_set_nm = 'EL_USE_TELE_PAT'
+			        and [use].code = [locators].[use_cd]
+			
+			
+			where   [locators].entity_uid = ?
+			    and [locators].[class_cd] = 'TELE'
+			    and ([locators].cd is null or [locators].cd <> 'NET')
+			    and [locators].record_status_cd = 'ACTIVE'
+			
+			order by
+			    [locators].as_of_date desc    
+			""";
+	private static final int PATIENT_PARAMETER = 1;
 
-          join Tele_locator [phone_number] on
-                  [phone_number].[tele_locator_uid] = [locators].[locator_uid]
-              and [phone_number].record_status_cd   = [locators].[record_status_cd]
-              and [phone_number].phone_nbr_txt is not null
+	private final JdbcTemplate template;
+	private final PatientSearchResultPhoneMapper mapper;
 
-          left join  NBS_SRTE..Code_value_general [type] on
-                  [type].code_set_nm = 'EL_TYPE_TELE_PAT'
-              and [type].code = [locators].cd
+	PatientSearchResultDetailedPhoneFinder(final JdbcTemplate template) {
+		this.template = template;
+		this.mapper = new PatientSearchResultPhoneMapper();
+	}
 
-          left join NBS_SRTE..Code_value_general [use] on
-                  [use].code_set_nm = 'EL_USE_TELE_PAT'
-              and [use].code = [locators].[use_cd]
-
-
-      where   [locators].entity_uid = ?
-          and [locators].[class_cd] = 'TELE'
-          and ([locators].cd is null or [locators].cd <> 'NET')
-          and [locators].record_status_cd = 'ACTIVE'
-      """;
-  private static final int PATIENT_PARAMETER = 1;
-
-  private final JdbcTemplate template;
-  private final PatientSearchResultPhoneMapper mapper;
-
-  PatientSearchResultDetailedPhoneFinder(final JdbcTemplate template) {
-    this.template = template;
-    this.mapper = new PatientSearchResultPhoneMapper();
-  }
-
-  Collection<PatientSearchResultPhone> find(final long patient) {
-    return this.template.query(
-        QUERY,
-        statement -> statement.setLong(PATIENT_PARAMETER, patient),
-        mapper);
-  }
+	Collection<PatientSearchResultPhone> find(final long patient) {
+		return this.template.query(
+				QUERY,
+				statement -> statement.setLong(PATIENT_PARAMETER, patient),
+				mapper);
+	}
 }
---
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/patient/profile/phone/PatientPhoneDemographicSteps.java
@@ -0,0 +1,44 @@
+package gov.cdc.nbs.patient.profile.phone;
+
+import gov.cdc.nbs.patient.PatientMother;
+import gov.cdc.nbs.patient.identifier.PatientIdentifier;
+import gov.cdc.nbs.testing.support.Active;
+import io.cucumber.java.en.Given;
+
+import java.time.LocalDate;
+
+public class PatientPhoneDemographicSteps {
+
+	private final PatientMother mother;
+	private final Active<PatientIdentifier> patient;
+
+	PatientPhoneDemographicSteps(
+			final PatientMother mother,
+			final Active<PatientIdentifier> patient
+	) {
+		this.mother = mother;
+		this.patient = patient;
+	}
+
+	@Given("the patient has the {phoneType} - {phoneUse} phone number of {string} {string} - {string} as of {localDate}")
+	public void the_patient_has_phone_number(
+			final String type,
+			final String use,
+			final String countryCode,
+			final String phone,
+			final String extension,
+			final LocalDate localDate
+	) {
+		this.patient.maybeActive().ifPresent(
+				identifier -> this.mother.withPhone(
+						identifier,
+						type,
+						use,
+						countryCode,
+						phone,
+						extension,
+						localDate
+				)
+		);
+	}
+}
