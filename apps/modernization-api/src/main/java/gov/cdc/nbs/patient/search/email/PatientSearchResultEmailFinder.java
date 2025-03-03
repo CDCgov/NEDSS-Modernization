19f5bd02c85f9f8b54032ea12aa5e1ddf107827b
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-03-03 15:10:46 -0500

    sonar issue

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/search/email/PatientSearchResultEmailFinder.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/search/email/PatientSearchResultEmailFinder.java
@@ -10,22 +10,22 @@ class PatientSearchResultEmailFinder {
 
   private static final String QUERY = """
       select distinct
-                [locator].as_of_date            as [as_of],
-                [email_address].email_address   as [email],
-                [locator].locator_uid			as [locator_uid]
-            from Entity_locator_participation [locator]
+      [locator].as_of_date            as [as_of],
+      [email_address].email_address   as [email],
+      [locator].locator_uid			as [locator_uid]
+      from Entity_locator_participation [locator]
       
-                join Tele_locator [email_address] on
-                        [email_address].[tele_locator_uid] = [locator].[locator_uid]
-                    and [email_address].record_status_cd = 'ACTIVE'
-                    and [email_address].email_address is not null
+      join Tele_locator [email_address] on
+      [email_address].[tele_locator_uid] = [locator].[locator_uid]
+      and [email_address].record_status_cd = 'ACTIVE'
+      and [email_address].email_address is not null
       
       
-            where   [locator].entity_uid = ?
-                and [locator].[class_cd] = 'TELE'
-                and [locator].record_status_cd = 'ACTIVE'
-            order by
-                [locator].as_of_date desc, [locator].locator_uid desc""";
+      where   [locator].entity_uid = ?
+      and [locator].[class_cd] = 'TELE'
+      and [locator].record_status_cd = 'ACTIVE'
+      order by
+      [locator].as_of_date desc, [locator].locator_uid desc""";
 
   private static final int PATIENT_PARAMETER = 1;
   private static final int EMAIL_COLUMN = 2;
