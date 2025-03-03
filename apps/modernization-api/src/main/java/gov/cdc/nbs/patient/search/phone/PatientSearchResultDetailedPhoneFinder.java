5abe0d9727433b5cabc8bbd19b6e1e79aac27f8d
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-03-03 16:04:56 -0500

    maybe might fix

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/search/phone/PatientSearchResultDetailedPhoneFinder.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/search/phone/PatientSearchResultDetailedPhoneFinder.java
@@ -7,13 +7,11 @@ import java.util.Collection;
 
 @Component
 class PatientSearchResultDetailedPhoneFinder {
-
   private static final String QUERY = """
       select distinct
-        coalesce(
+      coalesce(
           [type].code_short_desc_txt,
-          IsNull([locators].cd, '')
-        )                               as [type],
+          IsNull([locators].cd, ''))  as [type],
         coalesce(
             [use].code_short_desc_txt,
             IsNull([locators].[use_cd],'')
@@ -24,12 +22,10 @@ class PatientSearchResultDetailedPhoneFinder {
         [locators].as_of_date           as [as_of],
       	[locators].locator_uid         as [locator_uid]
       from Entity_locator_participation [locators]
-      
           join Tele_locator [phone_number] on
                   [phone_number].[tele_locator_uid] = [locators].[locator_uid]
               and [phone_number].record_status_cd   = [locators].[record_status_cd]
               and [phone_number].phone_nbr_txt is not null
-      
           left join  NBS_SRTE..Code_value_general [type] on
                   [type].code_set_nm = 'EL_TYPE_TELE_PAT'
               and [type].code = [locators].cd
@@ -37,13 +33,10 @@ class PatientSearchResultDetailedPhoneFinder {
           left join NBS_SRTE..Code_value_general [use] on
                   [use].code_set_nm = 'EL_USE_TELE_PAT'
               and [use].code = [locators].[use_cd]
-      
-      
       where   [locators].entity_uid = ?
           and [locators].[class_cd] = 'TELE'
           and ([locators].cd is null or [locators].cd <> 'NET')
           and [locators].record_status_cd = 'ACTIVE'
-      
       order by
           [locators].as_of_date desc, [locators].locator_uid desc""";
   private static final int PATIENT_PARAMETER = 1;
