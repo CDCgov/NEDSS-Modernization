63959f4c2e1985ac2a0fa29d344127270f3fad39
Serban Zamfir <78826267+szamfir@users.noreply.github.com>
2025-03-07 15:15:35 -0500

    adding env variable for scheduled tasks (#2339)

--- cdc-sandbox/nbs6-win-container/entrypoint.ps1
+++ cdc-sandbox/nbs6-win-container/entrypoint.ps1
@@ -58,6 +58,12 @@ $env:JAVA_OPTS="$env:JAVA_OPTS -Djboss.modules.system.pkgs=org.jboss.byteman"
 [Environment]::SetEnvironmentVariable("JBOSS_HOME", $env:JBOSS_HOME, "Machine")
 [Environment]::SetEnvironmentVariable("JAVA_TOOL_OPTIONS", $env:JAVA_TOOL_OPTIONS, "Machine")
 [Environment]::SetEnvironmentVariable("DATABASE_ENDPOINT", $env:DATABASE_ENDPOINT, "Machine")
+[Environment]::SetEnvironmentVariable("odse_user", $env:odse_user, "Machine")
+[Environment]::SetEnvironmentVariable("odse_pass", $env:odse_pass, "Machine")
+[Environment]::SetEnvironmentVariable("rdb_user", $env:rdb_user, "Machine")
+[Environment]::SetEnvironmentVariable("rdb_pass", $env:rdb_pass, "Machine")
+[Environment]::SetEnvironmentVariable("srte_user", $env:srte_user, "Machine")
+[Environment]::SetEnvironmentVariable("srte_pass", $env:srte_pass, "Machine")
 [Environment]::SetEnvironmentVariable("JAVA_OPTS", $env:JAVA_OPTS, "Machine")
 [Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\executables\sqlcmd", "Machine")
 
