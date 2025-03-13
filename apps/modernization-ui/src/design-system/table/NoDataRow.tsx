387183b89cc6ff7f522022b690d290158f0e221e
Henry Clark <hclark@enquizit.com>
2025-03-13 14:19:48 -0400

    adding period to end of sentence

--- apps/modernization-ui/src/design-system/table/NoDataRow.tsx
+++ apps/modernization-ui/src/design-system/table/NoDataRow.tsx
@@ -8,7 +8,7 @@ export const NoDataRow = ({ colSpan }: Props) => {
     return (
         <tr>
             <td colSpan={colSpan} className={styles.noData}>
-                No data has been added
+                No data has been added.
             </td>
         </tr>
     );
