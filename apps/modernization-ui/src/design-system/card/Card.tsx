84776a489c0315ae1a90b7c746ebf9122aef6d6d
snehaasjournal <uob6@cdc.gov>
2025-03-13 17:05:21 -0500

    Add SubText to Card Component

--- apps/modernization-ui/src/design-system/card/Card.tsx
+++ apps/modernization-ui/src/design-system/card/Card.tsx
@@ -6,15 +6,19 @@ type Props = {
     id: string;
     title: ReactNode;
     info?: ReactNode;
+    subtext?: string;
     level?: HeadingLevel;
     children: ReactNode;
 };
-export const Card = ({ id, title, info, children, level = 2 }: Props) => {
+export const Card = ({ id, title, info, subtext, children, level = 2 }: Props) => {
+    const cardClassName = subtext ? `${styles.card} ${styles.withSubtext}` : styles.card;
+
     return (
-        <section id={id} className={styles.card}>
+        <section id={id} className={cardClassName}>
             <header>
                 <Heading level={level}>{title}</Heading>
                 {info}
+                {subtext && <div className={styles.subtext}>{subtext}</div>}
             </header>
             {children}
         </section>
