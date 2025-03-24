1cf8670394dcdabad2c8f8d10e550e93047a53d6
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-03-24 12:12:31 -0400

    design changes made, waiting for design review

---
+++ apps/modernization-ui/src/design-system/select/multi/DropdownIndicator.tsx
@@ -0,0 +1,7 @@
+import { Icon } from 'design-system/icon';
+
+import styles from './dropdown-indicator.module.scss';
+
+const DropdownIndicator = () => <Icon name={'expand_more'} className={styles.indicator} />;
+
+export { DropdownIndicator };
--- apps/modernization-ui/src/design-system/select/multi/design.ts
+++ apps/modernization-ui/src/design-system/select/multi/design.ts
@@ -7,14 +7,9 @@ const theme = (existing: Theme) => ({
     colors: { ...existing.colors, primary: '#d9e8f6' }
 });
 
-const fontSizeWithinChips = (standardRemFontSize: number) => {
-    return standardRemFontSize * (1 + 3 / 17);
-};
-
 const styles = {
     control: (baseStyles: CSSObjectWithLabel) => ({
         ...baseStyles,
-        fontSize: `${fontSizeWithinChips(0.75)}rem`,
         borderStyle: 'none',
         boxShadow: 'none',
         minHeight: 'unset',
