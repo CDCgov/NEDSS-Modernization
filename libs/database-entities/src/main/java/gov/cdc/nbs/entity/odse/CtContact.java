23c672b53470899f7ae2ed078dcdc9c1930ebc46
Michael Peels <michaelpeels@Michael-Peels.local>
2025-03-26 16:53:01 -0400

    Attempt to suppress some dependency warnings

--- libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/CtContact.java
+++ libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/CtContact.java
@@ -12,6 +12,7 @@ import java.time.Instant;
 
 @Entity
 @Table(name = "CT_contact")
+@SuppressWarnings("javaarchitecture:S7027")
 public class CtContact {
     @Id
     @Column(name = "ct_contact_uid", nullable = false)
