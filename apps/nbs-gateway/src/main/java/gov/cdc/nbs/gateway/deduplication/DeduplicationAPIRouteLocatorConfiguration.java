b87ad463822846003badeea4ef2e2796110173ac
Michael Peels <michaelpeels@Michael-Peels.local>
2025-03-31 12:56:16 -0400

    deduplication-api routing

---
+++ apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/deduplication/DeduplicationAPIRouteLocatorConfiguration.java
@@ -0,0 +1,31 @@
+package gov.cdc.nbs.gateway.deduplication;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+@Configuration
+@ConditionalOnExpression("${nbs.gateway.deduplication.enabled}")
+public class DeduplicationAPIRouteLocatorConfiguration {
+
+  @Bean
+  RouteLocator deduplicationAPIRouteLocator(
+      final RouteLocatorBuilder builder,
+      @Qualifier("defaults") final List<GatewayFilter> defaults,
+      final DeduplicationService service) {
+    return builder.routes()
+        .route(
+            "deduplication-api",
+            route -> route.order(Ordered.HIGHEST_PRECEDENCE)
+                .path("/nbs/deduplication/**")
+                .filters(filter -> filter.filters(defaults))
+                .uri(service.uri()))
+        .build();
+  }
+}
---
+++ apps/nbs-gateway/src/test/java/gov/cdc/nbs/gateway/deduplication/DeduplicationAPIRouteLocatorConfigurationEnabledTest.java
@@ -0,0 +1,61 @@
+package gov.cdc.nbs.gateway.deduplication;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+
+
+
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {
+        "nbs.gateway.deduplication.service=localhost:10002",
+        "nbs.gateway.deduplication.enabled=true"
+    })
+class DeduplicationAPIRouteLocatorConfigurationEnabledTest {
+
+  @RegisterExtension
+  static WireMockExtension deduplication = WireMockExtension.newInstance()
+      .options(wireMockConfig().port(10002))
+      .build();
+
+  @Autowired
+  WebTestClient webClient;
+
+  @Autowired
+  RouteLocator deduplicationConfig;
+
+  @Autowired
+  DeduplicationService service;
+
+
+  @Test
+  void should_autowire() {
+    assertThat(service.base()).isEqualTo("/");
+    assertThat(service.uri().getHost()).isEqualTo("localhost");
+    assertThat(service.uri().getPort()).isEqualTo(10002);
+  }
+
+  @Test
+  void should_route() {
+    deduplication.stubFor(get(urlPathMatching("/nbs/deduplication/")).willReturn(ok()));
+
+    webClient
+        .get().uri(
+            builder -> builder
+                .path("/nbs/deduplication/")
+                .build())
+        .exchange()
+        .expectStatus()
+        .isOk();
+  }
+}
