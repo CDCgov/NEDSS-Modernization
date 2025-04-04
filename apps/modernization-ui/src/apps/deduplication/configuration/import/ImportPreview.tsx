1eaa64374b8df8680d9a4e004a80f90f085c0f18
Michael Peels <michaelpeels@Michael-Peels.local>
2025-04-04 14:01:57 -0400

    style heading for empty preview

--- apps/modernization-ui/src/apps/deduplication/configuration/import/ImportPreview.tsx
+++ apps/modernization-ui/src/apps/deduplication/configuration/import/ImportPreview.tsx
@@ -1,13 +1,13 @@
 import { Heading } from 'components/heading';
 import { Button } from 'design-system/button';
-import React from 'react';
+import styles from './import-preview.module.scss';
 
 type Props = {
     onCancel: () => void;
 };
 export const ImportPreview = ({ onCancel }: Props) => {
     return (
-        <header>
+        <header className={styles.importPreviewHeading}>
             <Heading level={1}>Preview configuration</Heading>
             <div>
                 <Button secondary onClick={onCancel}>
