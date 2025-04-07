fd27495d377c8a2eb3f4db15835bf2bb9817baa2
Steve Urciuoli <surciuoli@enquizit.com>
2025-04-07 08:51:01 -0500

    CFNT1-3787,3788 New patient: Replace the list of API which uses Graph.ql (#2412)

--- libs/options-api/src/main/java/gov/cdc/nbs/option/countries/list/CountriesListController.java
+++ libs/options-api/src/main/java/gov/cdc/nbs/option/countries/list/CountriesListController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import gov.cdc.nbs.option.Option;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 public class CountriesListController {
@@ -14,6 +15,8 @@ public class CountriesListController {
     this.finder = finder;
   }
 
+  @Operation(operationId = "countries", summary = "Countries Option", description = "Provides all Country options.",
+      tags = "CountryOptions")
   @GetMapping("/nbs/api/options/countries")
   public Collection<Option> addressTypes() {
     return finder.find();
