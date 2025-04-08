1222085322b7668e850084dbb0431dc2c21bbd83
Adam Loup <adam.loup@cdwg.com>
2025-04-08 13:04:53 -0500

    corrected urls

--- apps/modernization-ui/src/classic/ClassicButton.tsx
+++ apps/modernization-ui/src/classic/ClassicButton.tsx
@@ -6,9 +6,22 @@ type Props = {
     url: string;
     className?: string;
     outline?: boolean;
+    /**
+     * The destination for the resolved location
+     */
     destination?: Destination;
 } & JSX.IntrinsicElements['button'];
 
+/**
+ * A USWDS styled button that interacts with specialized API endpoints which prepare the NBS6 session and then provides
+ *  a url to redirect to within the {@code Location} header of the response.  This interaction pattern has since been
+ * replaced by a standard redirect issued from the response.
+ *
+ * This component exists to maintain compatibility with API's that provide this type of interaction and should only be
+ * used with those API endpoints.
+ *
+ * @param {Props} props
+ */
 export const ClassicButton = ({
     url,
     className,
