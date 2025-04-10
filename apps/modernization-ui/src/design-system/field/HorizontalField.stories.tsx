42eb4ba95113b115beab209a3d00836a66304843
Adam Loup <adam.loup@cdwg.com>
2025-04-10 13:17:27 -0500

    top / down (vertical) styles

---
+++ apps/modernization-ui/src/design-system/field/HorizontalField.stories.tsx
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Field } from './Field';
+
+const meta = {
+    title: 'Design System/Field/Left Right',
+    component: Field,
+    argTypes: {
+        label: { required: true }
+    }
+} satisfies Meta<typeof Field>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        label: 'Label',
+        children: <span>Field</span>,
+        orientation: 'horizontal',
+        htmlFor: 'story-horizontal-field'
+    }
+};
+
+export const Required: Story = {
+    args: {
+        ...Default.args,
+        required: true
+    }
+};
+
+export const HelperText: Story = {
+    args: {
+        ...Default.args,
+        helperText: 'Helper text'
+    }
+};
+
+export const Error: Story = {
+    args: {
+        ...Default.args,
+        error: 'Helpful error message'
+    }
+};
+
+export const Warning: Story = {
+    args: {
+        ...Default.args,
+        warning: 'Helpful warning message'
+    }
+};
