c08e0e0ca8ea51efd3fb350d666902574016de41
Michael Peels <michaelpeels@Michael-Peels.local>
2025-04-15 11:11:33 -0400

    Fix padding

--- apps/modernization-ui/src/apps/deduplication/data-elements/DataElementConfig.tsx
+++ apps/modernization-ui/src/apps/deduplication/data-elements/DataElementConfig.tsx
@@ -94,20 +94,21 @@ export const DataElementConfig = () => {
             <div className={styles.heading}>
                 <Heading level={1}>Data elements configuration for match criteria</Heading>
             </div>
-            <Shown when={validationError !== undefined}>
-                <AlertMessage title="Data element currently in use" type="error">
-                    <span>
-                        The request to remove the{' '}
-                        <span style={{ fontWeight: '700' }}>{validationError?.fields.join(', ')}</span> data element
-                        {(validationError?.fields.length ?? 0) > 1 ? 's' : ''} is not possible because{' '}
-                        {(validationError?.fields.length ?? 0) > 1 ? 'they are' : 'it is'} currently being used in a
-                        pass configuration. If you would like to remove this data element, first remove it from the
-                        following pass configurations:
-                    </span>
-                    <ul>{validationError?.passes.map((p, k) => <li key={k}>{p}</li>)}</ul>
-                </AlertMessage>
-            </Shown>
+
             <div className={styles.content}>
+                <Shown when={validationError !== undefined}>
+                    <AlertMessage title="Data element currently in use" type="error">
+                        <span>
+                            The request to remove the{' '}
+                            <span style={{ fontWeight: '700' }}>{validationError?.fields.join(', ')}</span> data element
+                            {(validationError?.fields.length ?? 0) > 1 ? 's' : ''} is not possible because{' '}
+                            {(validationError?.fields.length ?? 0) > 1 ? 'they are' : 'it is'} currently being used in a
+                            pass configuration. If you would like to remove this data element, first remove it from the
+                            following pass configurations:
+                        </span>
+                        <ul>{validationError?.passes.map((p, k) => <li key={k}>{p}</li>)}</ul>
+                    </AlertMessage>
+                </Shown>
                 <main>
                     <Shown when={!loading} fallback={<Loading center />}>
                         <FormProvider {...form}>
