160d425e4d3ca3860e34b9a869371f69e175b3b2
Michael Peels <michaelpeels@Michael-Peels.local>
2025-04-15 11:32:00 -0400

    strong

--- apps/modernization-ui/src/apps/deduplication/data-elements/validation/DataElementValidationError.tsx
+++ apps/modernization-ui/src/apps/deduplication/data-elements/validation/DataElementValidationError.tsx
@@ -12,8 +12,7 @@ export const DataElementValidationError = ({ validationError }: Props) => {
         <Shown when={validationError !== undefined}>
             <AlertMessage title="Data element currently in use" type="error">
                 <span>
-                    The request to remove the{' '}
-                    <span style={{ fontWeight: '700' }}>{validationError?.fields.join(', ')}</span> data element
+                    The request to remove the <strong>{validationError?.fields.join(', ')}</strong> data element
                     {(validationError?.fields.length ?? 0) > 1 ? 's' : ''} is not possible because{' '}
                     {(validationError?.fields.length ?? 0) > 1 ? 'they are' : 'it is'} currently being used in a pass
                     configuration. If you would like to remove this data element, first remove it from the following
