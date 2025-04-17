4a94074579a80b6f4efdc46161ff52a7c746c4e8
Joe Agster <joe.agster@cdw.com>
2025-04-17 09:58:08 -0700

    Refactor to use BaseCard

--- apps/modernization-ui/src/design-system/card/Card.tsx
+++ apps/modernization-ui/src/design-system/card/Card.tsx
@@ -1,7 +1,7 @@
 import { Heading, HeadingLevel } from 'components/heading';
 import { ReactNode } from 'react';
 import styles from './card.module.scss';
-import classNames from 'classnames';
+import { BaseCard } from './base/BaseCard';
 
 type Props = {
     id: string;
@@ -14,15 +14,19 @@ type Props = {
 };
 export const Card = ({ id, title, info, subtext, children, className, level = 2 }: Props) => {
     return (
-        <section id={id} className={classNames(styles.card, className)}>
-            <header>
-                <div className={styles.titleBlock}>
-                    <Heading level={level}>{title}</Heading>
-                    {subtext && <div className={styles.subtext}>{subtext}</div>}
-                </div>
-                {info && <div className={styles.info}>{info}</div>}
-            </header>
+        <BaseCard
+            id={id}
+            className={className}
+            header={
+                <>
+                    <div className={styles.titleBlock}>
+                        <Heading level={level}>{title}</Heading>
+                        {subtext && <div className={styles.subtext}>{subtext}</div>}
+                    </div>
+                    {info && <div className={styles.info}>{info}</div>}
+                </>
+            }>
             {children}
-        </section>
+        </BaseCard>
     );
 };
