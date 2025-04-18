bed39f2752b863c8b4458916701e96ac4519ba7f
Henry Clark <hclark@enquizit.com>
2025-04-18 15:57:10 -0400

    adding unit test for resetting title

--- apps/modernization-ui/src/page/PageContext.spec.tsx
+++ apps/modernization-ui/src/page/PageContext.spec.tsx
@@ -1,20 +1,22 @@
-import { render, renderHook } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
 import { PageProvider, usePage } from '.';
 import { act } from 'react';
 
+const setup = () => {
+    return renderHook(() => usePage(), {
+        wrapper: PageProvider
+    })
+}
+
 describe('PageContext', () => {
     it('should provide a default page title', () => {
-        const { result } = renderHook(() => usePage(), {
-            wrapper: PageProvider
-        });
+        const { result } = setup();
 
         expect(result.current.title).toBeUndefined();
     });
 
     it('should update the page title', () => {
-        const { result } = renderHook(() => usePage(), {
-            wrapper: PageProvider
-        });
+        const { result } = setup();
 
         act(() => {
             result.current.setTitle('New Title');
@@ -22,6 +24,22 @@ describe('PageContext', () => {
 
         expect(result.current.title).toBe('New Title');
     });
+
+    it('should reset the page title', () => {
+        const { result } = setup();
+
+        act(() => {
+            result.current.setTitle('Temporary Title');
+        });
+
+        expect(result.current.title).toBe('Temporary Title');
+
+        act(() => {
+            result.current.resetTitle();
+        });
+        expect(result.current.title).toBeUndefined();
+    });
+
 
     it('should throw an error if usePage is used outside of PageProvider', () => {
         expect(() => renderHook(() => usePage())).toThrow('usePage must be used within a PageProvider');
