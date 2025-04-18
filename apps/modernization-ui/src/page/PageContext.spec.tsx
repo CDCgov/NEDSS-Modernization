cee7571742df47345de72a5ae8d5de39f4aa14fd
hclarkEnq <hclark@enquizit.com>
2025-04-18 15:07:59 -0400

    title expected to be undefined not empty string

--- apps/modernization-ui/src/page/PageContext.spec.tsx
+++ apps/modernization-ui/src/page/PageContext.spec.tsx
@@ -1,51 +1,33 @@
-import { render } from '@testing-library/react';
+import { render, renderHook } from '@testing-library/react';
 import { PageProvider, usePage } from '.';
 import { act } from 'react';
 
-const TestComponent = () => {
-    const { title, setTitle } = usePage();
-    return (
-        <div>
-            <h1>{title}</h1>
-            <button onClick={() => setTitle('New Title')}>Set Title</button>
-        </div>
-    );
-};
-
 describe('PageContext', () => {
     it('should provide a default page title', () => {
-        const { getByRole } = render(
-            <PageProvider>
-                <TestComponent />
-            </PageProvider>
-        );
+        const { result } = renderHook(() => usePage(), {
+            wrapper: PageProvider
+        });
 
-        const titleElement = getByRole('heading', { level: 1 });
-        expect(titleElement.textContent).toBe('');
+        expect(result.current.title).toBeUndefined();
     });
 
     it('should update the page title', () => {
-        const { getByRole, getByText } = render(
-            <PageProvider>
-                <TestComponent />
-            </PageProvider>
-        );
-
-        const titleElement = getByRole('heading', { level: 1 });
-        const button = getByText('Set Title');
-
-        act(() => {
-            button.click();
+        const { result } = renderHook(() => usePage(), {
+            wrapper: PageProvider
         });
 
-        expect(titleElement.textContent).toBe('New Title');
+        act(() => {
+            result.current.setTitle('New Title');
+        });
+
+        expect(result.current.title).toBe('New Title');
     });
 
     it('should throw an error if usePage is used outside of PageProvider', () => {
         const consoleError = console.error;
         console.error = jest.fn();
 
-        expect(() => render(<TestComponent />)).toThrow('usePage must be used within a PageProvider');
+        expect(() => renderHook(() => usePage())).toThrow('usePage must be used within a PageProvider');
 
         console.error = consoleError;
     });
