dfc56bbce6b8c40e64a2a7d22b10c624326622b8
Henry Clark <hclark@enquizit.com>
2025-04-21 13:31:45 -0400

    adding unit tests count fixes for expected behavior on getAllByRole

--- apps/modernization-ui/src/design-system/entry/multi-value/RepeatingBlock.spec.tsx
+++ apps/modernization-ui/src/design-system/entry/multi-value/RepeatingBlock.spec.tsx
@@ -358,10 +358,10 @@ describe('RepeatingBlock', () => {
         expect(getByText('Render view second value: second-value')).toBeInTheDocument();
 
         const buttons = getAllByRole('button');
-        expect(buttons).toHaveLength(1);
-        expect(buttons[0]).toHaveTextContent('Add test title');
-        expect(buttons[0].innerHTML).toContain('svg');
-        expect(buttons[0]).toHaveAttribute('aria-description');
+        expect(buttons).toHaveLength(4);
+        expect(buttons[3]).toHaveTextContent('Add test title');
+        expect(buttons[3].innerHTML).toContain('svg');
+        expect(buttons[3]).toHaveAttribute('aria-description');
     });
 
     it('should render edit when edit icon clicked', async () => {
@@ -386,12 +386,12 @@ describe('RepeatingBlock', () => {
         expect(getByLabelText('Second Input')).toBeInTheDocument();
 
         const buttons = getAllByRole('button');
-        expect(buttons).toHaveLength(2);
-        expect(buttons[0]).toHaveTextContent('Update test title');
-        expect(buttons[0]).toHaveAttribute('aria-description');
-        expect(buttons[0].innerHTML).not.toContain('svg');
-        expect(buttons[1]).toHaveTextContent('Cancel');
-        expect(buttons[1]).toHaveAttribute('aria-description');
+        expect(buttons).toHaveLength(5);
+        expect(buttons[3]).toHaveTextContent('Update test title');
+        expect(buttons[3]).toHaveAttribute('aria-description');
+        expect(buttons[3].innerHTML).not.toContain('svg');
+        expect(buttons[4]).toHaveTextContent('Cancel');
+        expect(buttons[4]).toHaveAttribute('aria-description');
     });
 
     it('should delete row when delete icon clicked', async () => {
