d6d7a61e73063f33eb91e5e6c52f3ad0823f1740
Joe Agster <joe.agster@cdw.com>
2025-04-25 13:53:46 -0700

    Add column preferences panel to settings in header

--- apps/modernization-ui/src/design-system/card/table/TableCard.tsx
+++ apps/modernization-ui/src/design-system/card/table/TableCard.tsx
@@ -1,7 +1,9 @@
 import { CollapsibleCard } from '../collapsible';
 import { Sizing } from 'design-system/field';
-import { DataTable, DataTableProps } from 'design-system/table';
+import { Column, DataTable, DataTableProps } from 'design-system/table';
 import { TableCardAction, TableCardHeader } from './TableCardHeader';
+import { ColumnPreference, useMaybeColumnPreferences, withColumnPreferences } from 'design-system/table/preferences';
+import { ComponentType, FC, useMemo } from 'react';
 
 export type TableCardProps<V> = {
     id: string;
@@ -12,6 +14,8 @@ export type TableCardProps<V> = {
     sizing?: Sizing;
     tableClassName?: string;
     actions?: TableCardAction[];
+    columnPreferencesKey?: string;
+    defaultColumnPreferences?: ColumnPreference[];
 } & Omit<DataTableProps<V>, 'id' | 'className'>;
 
 export const TableCard = <V,>({
@@ -21,16 +25,46 @@ export const TableCard = <V,>({
     title,
     tableClassName,
     actions,
+    columns,
+    columnPreferencesKey,
+    defaultColumnPreferences,
     ...props
 }: TableCardProps<V>) => {
+    const columnPreferences = useMemo(
+        () =>
+            defaultColumnPreferences ??
+            columns.map((column) => ({
+                id: column.id,
+                name: column.name,
+                moveable: true,
+                toggleable: true
+            })),
+        [defaultColumnPreferences, columns]
+    );
+    const ColumnPreferencesCard = withColumnPreferences(CollapsibleCard, {
+        storageKey: columnPreferencesKey,
+        defaults: columnPreferences
+    });
+    const ManagedDataTable = withMaybeColumnPreferencesDataTable(DataTable<V>);
     return (
-        <CollapsibleCard
+        <ColumnPreferencesCard
             id={id}
             className={className}
             collapsible={collapsible}
-            header={<TableCardHeader title={title} actions={actions} />}
+            header={<TableCardHeader title={title} actions={actions} showSettings={columnPreferencesKey != null} />}
             showCollapseSeparator={true}>
-            <DataTable id={`${id}-table`} className={tableClassName} {...props} />
-        </CollapsibleCard>
+            <ManagedDataTable id={`${id}-table`} className={tableClassName} columns={columns} {...props} />
+        </ColumnPreferencesCard>
     );
+};
+
+const withMaybeColumnPreferencesDataTable = <V,>(WrappedComponent: ComponentType<DataTableProps<V>>) => {
+    const EnhancedComponent: FC<DataTableProps<V>> = (props) => {
+        const interaction = useMaybeColumnPreferences();
+        const apply = interaction?.apply ?? ((columns: Array<Column<V>>) => columns);
+        const columns = apply(props.columns);
+        return <WrappedComponent {...props} columns={columns} />;
+    };
+
+    return EnhancedComponent;
 };
--- apps/modernization-ui/src/design-system/card/table/TableCardHeader.tsx
+++ apps/modernization-ui/src/design-system/card/table/TableCardHeader.tsx
@@ -2,6 +2,9 @@ import { Heading, HeadingLevel } from 'components/heading';
 import { Button, ButtonProps } from 'design-system/button';
 import { Icon } from 'design-system/icon';
 import styles from './table-card-header.module.scss';
+import { OverlayPanel } from 'overlay';
+import { ColumnPreferencesPanel } from 'design-system/table/preferences';
+import { Shown } from 'conditional-render';
 
 export type TableCardAction = ButtonProps;
 
@@ -27,19 +30,26 @@ export const TableCardHeader = ({
                 <Heading level={headingLevel}>{title}</Heading>
                 {subtext && <div className={styles.subtext}>{subtext}</div>}
             </div>
-            {showSettings && (
-                <div className={styles.actions}>
-                    {actions?.map((action, index) => <Button key={index} {...action} />)}
-                    <Button
-                        aria-label="Settings"
-                        data-tooltip-position="top"
-                        data-tooltip-offset="center"
-                        secondary
-                        sizing="small"
-                        icon={<Icon name="settings" />}
-                    />
-                </div>
-            )}
+            <div className={styles.actions}>
+                {actions?.map((action, index) => <Button key={index} {...action} />)}
+                <Shown when={showSettings}>
+                    <OverlayPanel
+                        className={styles.overlay}
+                        position="right"
+                        toggle={({ toggle }) => (
+                            <Button
+                                aria-label="Settings"
+                                data-tooltip-position="top"
+                                data-tooltip-offset="center"
+                                secondary
+                                icon={<Icon name="settings" />}
+                                onClick={toggle}
+                                sizing="small"
+                            />
+                        )}
+                        render={(close) => <ColumnPreferencesPanel close={close} />}></OverlayPanel>
+                </Shown>
+            </div>
         </>
     );
 };
