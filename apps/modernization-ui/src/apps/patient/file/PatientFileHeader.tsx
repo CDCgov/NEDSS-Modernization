22360f18abd9421df570988cb49857924be41fb0
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-04-28 09:16:24 -0700

    changed up css to match with new tab navigation designs

--- apps/modernization-ui/src/apps/patient/file/PatientFileHeader.tsx
+++ apps/modernization-ui/src/apps/patient/file/PatientFileHeader.tsx
@@ -1,4 +1,6 @@
 import { Heading } from 'components/heading';
+import { TabNavigation, TabNavigationEntry } from 'components/TabNavigation/TabNavigation';
+import styles from './patient-file.module.scss';
 
 interface PatientFileHeaderProps {
     id: string;
@@ -6,8 +8,17 @@ interface PatientFileHeaderProps {
 
 export const PatientFileHeader = ({ id }: PatientFileHeaderProps) => {
     return (
-        <header>
-            <Heading level={1}>Patient ID: {id}</Heading>
-        </header>
+        <div className={styles.header}>
+            <div className={styles.headerContent}>
+                <Heading level={1}>Patient ID: {id}</Heading>
+            </div>
+            <div className={styles.tabNavigation}>
+                <TabNavigation className="grid-row flex-align-center" newTab>
+                    <TabNavigationEntry path={`/patient/${id}/summary`}>Summary</TabNavigationEntry>
+                    <TabNavigationEntry path={`/patient/${id}/events`}>Events</TabNavigationEntry>
+                    <TabNavigationEntry path={`/patient/${id}/demographics`}>Demographics</TabNavigationEntry>
+                </TabNavigation>
+            </div>
+        </div>
     );
 };
