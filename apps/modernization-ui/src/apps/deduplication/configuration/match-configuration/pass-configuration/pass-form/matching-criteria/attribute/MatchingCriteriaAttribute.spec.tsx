2b3a1d3743efcfb241cbc05f9d590e5c34504963
Michael Peels <michaelpeels@Michael-Peels.local>
2025-04-29 11:44:38 -0400

    Add threshold to matching criteria.

--- apps/modernization-ui/src/apps/deduplication/configuration/match-configuration/pass-configuration/pass-form/matching-criteria/attribute/MatchingCriteriaAttribute.spec.tsx
+++ apps/modernization-ui/src/apps/deduplication/configuration/match-configuration/pass-configuration/pass-form/matching-criteria/attribute/MatchingCriteriaAttribute.spec.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { FormProvider, useForm } from 'react-hook-form';
-import { BlockingAttribute, MatchingAttribute, Pass } from 'apps/deduplication/api/model/Pass';
+import { BlockingAttribute, MatchingAttribute, MatchMethod, Pass } from 'apps/deduplication/api/model/Pass';
 import { MatchingCriteriaAttribute } from './MatchingCriteriaAttribute';
 
 const onRemove = jest.fn();
@@ -11,7 +11,9 @@ const Fixture = () => {
             name: 'Pass name',
             description: 'This is my description for this pass',
             blockingCriteria: [BlockingAttribute.LAST_NAME],
-            matchingCriteria: [{ attribute: MatchingAttribute.FIRST_NAME }],
+            matchingCriteria: [
+                { attribute: MatchingAttribute.FIRST_NAME, method: MatchMethod.JAROWINKLER, threshold: 0.5 }
+            ],
             active: true
         }
     });
@@ -37,6 +39,51 @@ describe('MatchingCriteriaAttribute', () => {
         const { getByText } = render(<Fixture />);
 
         expect(getByText('First name')).toBeInTheDocument();
+    });
+
+    it('should display method label', () => {
+        const { getByLabelText } = render(<Fixture />);
+
+        expect(getByLabelText('Method')).toBeInTheDocument();
+        expect(getByLabelText('Method')).toHaveValue('JAROWINKLER');
+    });
+
+    it('should display threshold label', () => {
+        const { getByLabelText } = render(<Fixture />);
+
+        expect(getByLabelText('Threshold')).toBeInTheDocument();
+    });
+
+    it('should display threshold value', () => {
+        const { getByLabelText } = render(<Fixture />);
+
+        expect(getByLabelText('Threshold')).toBeInTheDocument();
+        expect(getByLabelText('Threshold')).toHaveValue(0.5);
+    });
+
+    it('should set threshold value for exact match', async () => {
+        const user = userEvent.setup();
+        const { getByLabelText } = render(<Fixture />);
+
+        await user.selectOptions(getByLabelText('Method'), 'EXACT');
+
+        expect(getByLabelText('Threshold')).toHaveValue(1);
+        expect(getByLabelText('Threshold')).toBeDisabled();
+    });
+
+    it('should display threshold tooltip', async () => {
+        const user = userEvent.setup();
+
+        const { container, findByText } = render(<Fixture />);
+
+        const icon = container.querySelector('svg');
+        await user.hover(icon!);
+
+        expect(
+            await findByText(
+                'Values between 0 and 1, above which two strings are said to be "similar enough" that they are probably the same thing. Values that are less than the threshold will be calculated as 0. Attributes that use “Exact Match” will automatically have a threshold of 1 and cannot be adjusted.'
+            )
+        ).toBeInTheDocument();
     });
 
     it('should not display criteria that is not selected', () => {
