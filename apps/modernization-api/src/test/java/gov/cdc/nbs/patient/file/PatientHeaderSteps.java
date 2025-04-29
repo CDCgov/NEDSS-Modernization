46f803f14e570d313308438f896524306852ee4f
Steven Urciuoli <steveu@mac.lan>
2025-04-28 21:01:08 -0500

    coverage

--- apps/modernization-api/src/test/java/gov/cdc/nbs/patient/file/PatientHeaderSteps.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/patient/file/PatientHeaderSteps.java
@@ -1,30 +1,33 @@
 package gov.cdc.nbs.patient.file;
 
+import gov.cdc.nbs.patient.identifier.PatientIdentifier;
 import gov.cdc.nbs.testing.support.Active;
 import io.cucumber.java.en.Then;
 import org.springframework.test.web.servlet.ResultActions;
 
 public class PatientHeaderSteps {
 
+  private final Active<PatientIdentifier> activePatient;
   private final PatientHeaderRequester requester;
   private final Active<ResultActions> response;
   Exception exception;
 
   PatientHeaderSteps(
+      final Active<PatientIdentifier> activePatient,
       final PatientHeaderRequester requester,
       final Active<ResultActions> response) {
+    this.activePatient = activePatient;
     this.requester = requester;
     this.response = response;
   }
 
   @Then("I view the Patient File Header")
-  public void i_view_the_patient_file_header(long patientId) {
+  public void i_view_the_patient_file_header() {
     try {
       this.response.active(
-          this.requester.request(patientId));
+          this.requester.request(activePatient.active().shortId()));
     } catch (Exception thrown) {
       this.exception = thrown;
     }
-
   }
 }
---
+++ apps/modernization-api/src/test/resources/features/patient/file/PatientFileHeader.feature
@@ -0,0 +1,8 @@
+@patient-file-header
+Feature: Patient File Header
+
+  Background:
+    Given I have a patient
+
+  Scenario: I can retrieve patient file header for a patient
+    I view the Patient File Header
