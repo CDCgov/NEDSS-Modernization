38c9b49b43b73e32bbcb5fca0211cb0fd479339f
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2025-05-01 16:44:52 -0400

    [CNFT1-4090]: Adding action Icon components with the corresponding tests (#2510)
    * adding action Icon components tests and fixing other unit tests plus removed file
    
    * removing export
    
    * adding role button and onAction also moved to design system

--- apps/modernization-ui/src/design-system/icon/index.ts
+++ apps/modernization-ui/src/design-system/icon/index.ts
@@ -1,3 +1,5 @@
 export { Icon } from './Icon';
+export type { IconProps } from './Icon';
 export { IconsArray } from './types';
 export type { Icons } from './types';
+export { ActionIcon } from './actionIcon/ActionIcon';
