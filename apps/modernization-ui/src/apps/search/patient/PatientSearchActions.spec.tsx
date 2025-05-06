f66417886fcdf37804944fe58cd0e02cebe4e6df
Henry Clark <hclark@enquizit.com>
2025-05-06 13:42:15 -0400

    more tests supporting PatientSearchActions functionality

--- apps/modernization-ui/src/apps/search/patient/PatientSearchActions.spec.tsx
+++ apps/modernization-ui/src/apps/search/patient/PatientSearchActions.spec.tsx
@@ -2,7 +2,7 @@ import { permissions } from "libs/permission";
 import { useAddPatientFromSearch } from "./add"
 import { useFocusAddNewPatientButton } from "./useFocusAddNewPatientButton";
 import { PatientSearchActions } from "./PatientSearchActions";
-import { render } from "@testing-library/react";
+import { getByText, render } from "@testing-library/react";
 import { axe } from "jest-axe";
 
 jest.mock('./add/useAddPatientFromSearch', () => ({
@@ -38,4 +38,33 @@ describe('PatinetSearchActions', () => {
         const { container } = render(<PatientSearchActions disabled={false} />);
         expect(await axe(container)).toHaveNoViolations();
     });
+
+    it('should render the button in disabled state when disabled prop is true', () => {
+        const { getByText } = render(<PatientSearchActions disabled={true} />);
+
+        const button = getByText('Add new patient').closest('button');
+        button?.click();
+    });
+
+    it('should call the add function when the button is clicked', () => {
+        const { getByText } = render(<PatientSearchActions disabled={false} />);
+
+        const button = getByText('Add new patient').closest('button');
+        button?.click();
+        expect(mockAdd).toHaveBeenCalledTimes(1);
+    });
+
+    it('should wrap the button with the Permitted component with correct permission', () => {
+        const { container } = render(<PatientSearchActions disabled={false} />);
+
+        const permittedWrapper = container.querySelector('.permitted-wrapper');
+        expect(permittedWrapper).toHaveAttribute('data-permission', permissions.patient.add);
+    });
+
+    it('should initialize the focus hook', () => {
+        render(<PatientSearchActions disabled={false} />);
+
+        expect(useFocusAddNewPatientButton).toHaveBeenCalledTimes(1);
+    })
+
 });    
