6363a4284a9898e4ed4174457441b3db4b4ceaa0
Steven Urciuoli <steveu@mac.lan>
2025-05-09 13:29:00 -0500

    smell

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/investigation/PatientInvestigationsFinder.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/investigation/PatientInvestigationsFinder.java
@@ -2,7 +2,6 @@ package gov.cdc.nbs.patient.investigation;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
