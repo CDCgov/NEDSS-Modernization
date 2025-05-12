4b0e009cc10291d31792ffe2cedda955d9bc468c
Joe Agster <98052312+joeaxcdw@users.noreply.github.com>
2025-05-12 09:09:48 -0700

    CNFT1-4033: Delete functionality for Patient File (#2520)
    * Extend functionality to enable supplying our own target element
    
    * Create a context to use through component tree
    
    * Delete functionality
    
    * Integrate delete functionality
    
    * Update Confirmation modal to allow destructive button
    
    * Clean up
    
    * Fix UI issues and add unit tests
    
    * Simpler name
    
    * Fix confirmation
    
    * Integrate delete service
    
    * Remove close button per design
    
    * Integrate recent changes for routing and layout
    
    * Rename delete hook
    
    * jsdoc fix
    
    * Fix unit test

---
+++ apps/modernization-ui/src/apps/patient/file/delete/DeleteAction.spec.tsx
@@ -0,0 +1,163 @@
+import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { DeleteAction } from './DeleteAction';
+import { Patient } from '../patient';
+import { Permitted } from 'libs/permission';
+import { useDeletePatient } from './useDeletePatient';
+import { usePatient } from '../usePatient';
+
+const mockPatient: Partial<Patient> = {
+    id: 10056284,
+    local: 'PSN10091000GA01',
+    patientId: 91000,
+    deletability: 'Deletable',
+    status: 'ACTIVE'
+};
+
+const mockShowSuccess = jest.fn();
+const mockShowError = jest.fn();
+const mockGo = jest.fn();
+
+jest.mock('alert', () => ({
+    useAlert: () => ({
+        showSuccess: mockShowSuccess,
+        showError: mockShowError
+    })
+}));
+
+jest.mock('apps/search', () => ({
+    useSearchNavigation: () => ({
+        go: mockGo
+    })
+}));
+
+jest.mock('./useDeletePatient', () => ({
+    useDeletePatient: jest.fn()
+}));
+
+jest.mock('../usePatient', () => ({
+    usePatient: jest.fn(() => mockPatient)
+}));
+
+jest.mock('libs/permission', () => ({
+    Permitted: jest.fn(({ children }: { children: ReactNode }) => <>{children}</>),
+    permissions: {
+        patient: {
+            delete: 'DELETE-PATIENT'
+        }
+    }
+}));
+
+describe('DeleteAction', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        (usePatient as jest.Mock).mockReturnValue(mockPatient);
+        (Permitted as jest.Mock).mockImplementation(({ children }: { children: ReactNode }) => <>{children}</>);
+    });
+
+    it('should handle the deletion flow', async () => {
+        const user = userEvent.setup();
+        // (resolveDeletability as jest.Mock).mockReturnValue(DeletabilityResult.Deletable);
+        const { getByRole, getByText } = render(<DeleteAction />);
+
+        const deleteButton = getByRole('button');
+
+        await user.click(deleteButton);
+
+        expect(getByText('Delete patient file')).toBeInTheDocument();
+        expect(getByText('Delete', { selector: 'button' })).toBeInTheDocument();
+    });
+
+    it('should not show the delete button when user does not have permissions', () => {
+        (Permitted as jest.Mock).mockImplementation(() => <></>);
+        const { queryByRole } = render(<DeleteAction />);
+        expect(queryByRole('button')).not.toBeInTheDocument();
+    });
+
+    it('should show warning if patient has associations', async () => {
+        const user = userEvent.setup();
+        // (resolveDeletability as jest.Mock).mockReturnValue(DeletabilityResult.Has_Associations);
+        (usePatient as jest.Mock).mockReturnValue({
+            ...mockPatient,
+            deletability: 'Has_Associations'
+        });
+        const { getByRole, getByText } = render(<DeleteAction />);
+
+        const deleteButton = getByRole('button');
+        await user.hover(deleteButton);
+
+        expect(
+            getByText('The file cannot be deleted until all associated event records have been deleted.', {
+                exact: false
+            })
+        ).toBeInTheDocument();
+    });
+
+    it('should show warning if patient is inactive', async () => {
+        const user = userEvent.setup();
+        // (resolveDeletability as jest.Mock).mockReturnValue(DeletabilityResult.Is_Inactive);
+        (usePatient as jest.Mock).mockReturnValue({
+            ...mockPatient,
+            deletability: 'Is_Inactive'
+        });
+        const { getByRole, getByText } = render(<DeleteAction />);
+
+        const deleteButton = getByRole('button');
+        await user.hover(deleteButton);
+
+        expect(getByText('This patient file is inactive and cannot be deleted.')).toBeInTheDocument();
+    });
+
+    it('should call deletePatient function when deleted confirmation button is clicked', async () => {
+        const mockDeletePatient = jest.fn(() => Promise.resolve({ success: true }));
+        (useDeletePatient as jest.Mock).mockReturnValue(mockDeletePatient);
+        const user = userEvent.setup();
+        // (resolveDeletability as jest.Mock).mockReturnValue(DeletabilityResult.Deletable);
+        const { getByRole, getByText } = render(<DeleteAction />);
+
+        const deleteButton = getByRole('button');
+
+        await user.click(deleteButton);
+
+        expect(getByText('Delete patient file')).toBeInTheDocument();
+        const confirmButton = getByText('Delete', { selector: 'button' });
+
+        await user.click(confirmButton);
+        expect(mockDeletePatient).toHaveBeenCalledWith(mockPatient.id);
+    });
+
+    it('should show success message when patient successfully deleted', async () => {
+        (useDeletePatient as jest.Mock).mockImplementation((onComplete) => () => onComplete({ success: true }));
+        // (resolveDeletability as jest.Mock).mockReturnValue(DeletabilityResult.Deletable);
+        const user = userEvent.setup();
+        const { getByRole, getByText } = render(<DeleteAction />);
+
+        const deleteButton = getByRole('button');
+        await user.click(deleteButton);
+
+        const confirmButton = getByText('Delete', { selector: 'button' });
+        await user.click(confirmButton);
+
+        expect(mockShowSuccess).toHaveBeenCalledWith('Deleted patient: 10056284');
+        expect(mockGo).toHaveBeenCalled();
+    });
+
+    it('should show error message when patient failed to delete', async () => {
+        (useDeletePatient as jest.Mock).mockImplementation(
+            (onComplete) => () => onComplete({ success: false, message: 'Error in delete' })
+        );
+        // (resolveDeletability as jest.Mock).mockReturnValue(DeletabilityResult.Deletable);
+        const user = userEvent.setup();
+        const { getByRole, getByText } = render(<DeleteAction />);
+
+        const deleteButton = getByRole('button');
+        await user.click(deleteButton);
+
+        const confirmButton = getByText('Delete', { selector: 'button' });
+        await user.click(confirmButton);
+
+        expect(mockShowSuccess).not.toHaveBeenCalled();
+        expect(mockShowError).toHaveBeenCalledWith('Delete failed. Please try again later.');
+    });
+});
