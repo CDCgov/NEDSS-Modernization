a31be9c252854a44c7fdb46b72dfd5ae8542e813
hclarkEnq <hclark@enquizit.com>
2025-05-12 14:16:39 -0400

    keyboard navigation accessibilty improvement

--- apps/modernization-ui/src/layout/ScrollToTop.tsx
+++ apps/modernization-ui/src/layout/ScrollToTop.tsx
@@ -1,11 +1,41 @@
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 
 const ScrollToTop = ({ children }: { children: ReactNode }) => {
     const location = useLocation();
+    const [tabPressed, setTabPressed] = useState(false);
+    const [prevPathname, setPrevPathname] = useState(location.pathname);
+
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, [location]);
+
+        if (location.pathname !== prevPathname) {
+            setTabPressed(false);
+            setPrevPathname(location.pathname);
+        }
+
+        const handleFirstTab = (event: KeyboardEvent) => {
+            const activeElement = document.activeElement;
+            const isBodyOrNonFocusable = activeElement === document.body || activeElement === null;
+
+            if (event.code === 'Tab' && !tabPressed && isBodyOrNonFocusable) {
+                setTabPressed(true);
+                const skipLinkElement: HTMLElement | null = document.querySelector('.usa-skipnav');
+                if (skipLinkElement) {
+                    requestAnimationFrame(() => {
+                        skipLinkElement.focus();
+                    });
+                }
+                window.removeEventListener('keydown', handleFirstTab);
+            }
+        };
+
+        window.addEventListener('keydown', handleFirstTab);
+
+        return () => {
+            window.removeEventListener('keydown', handleFirstTab);
+        };
+    }, [location.pathname, tabPressed, prevPathname]);
 
     return <>{children}</>;
 };
