46e6109a06415f029ecd05f0e6503d1b86666b8d
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2025-05-13 15:15:45 -0400

    keyboard navigation accessibilty improvement (#2536)
    * keyboard navigation accessibilty improvement
    
    * prettier
    
    * adding use ref to target focused ref
    
    * modifying the state name and removing the listener event on pressing tab

--- apps/modernization-ui/src/layout/ScrollToTop.tsx
+++ apps/modernization-ui/src/layout/ScrollToTop.tsx
@@ -1,13 +1,55 @@
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useState, useRef } from 'react';
 import { useLocation } from 'react-router';
 
 const ScrollToTop = ({ children }: { children: ReactNode }) => {
     const location = useLocation();
+    const [initialTabPress, setInitialTabPress] = useState(false);
+    const [prevPathname, setPrevPathname] = useState(location.pathname);
+    const focusRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, [location]);
 
-    return <>{children}</>;
+        if (location.pathname !== prevPathname) {
+            setInitialTabPress(false);
+            setPrevPathname(location.pathname);
+
+            if (focusRef.current) {
+                focusRef.current.focus();
+            }
+        }
+
+        const handleFirstTab = (event: KeyboardEvent) => {
+            const activeElement = document.activeElement;
+            const isBodyOrNonFocusable = activeElement === document.body || activeElement === null;
+
+            if (event.code === 'Tab') {
+                if (!initialTabPress && isBodyOrNonFocusable) {
+                    setInitialTabPress(true);
+                    const skipLinkElement: HTMLElement | null = document.querySelector('.usa-skipnav');
+                    if (skipLinkElement) {
+                        requestAnimationFrame(() => {
+                            skipLinkElement.focus();
+                        });
+                    }
+                }
+                window.removeEventListener('keydown', handleFirstTab);
+            }
+        };
+
+        window.addEventListener('keydown', handleFirstTab);
+
+        return () => {
+            window.removeEventListener('keydown', handleFirstTab);
+        };
+    }, [location.pathname, initialTabPress, prevPathname]);
+
+    return (
+        <>
+            <div ref={focusRef} tabIndex={-1} style={{ outline: 'none' }} aria-hidden="true" />
+            {children}
+        </>
+    );
 };
 
 export { ScrollToTop };
