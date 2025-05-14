60e2d94f744de4abd2a43c00f6710d5c5191a46f
Joe Agster <joe.agster@cdw.com>
2025-05-14 15:35:40 -0700

    Fix jsdoc

--- apps/modernization-ui/src/design-system/table/SortableDataTable.tsx
+++ apps/modernization-ui/src/design-system/table/SortableDataTable.tsx
@@ -5,7 +5,6 @@ import { DataTable, DataTableProps } from './DataTable';
 export const withSortable = <T,>(Component: ComponentType<DataTableProps<T>>) => {
     const SortableComponent = (props: DataTableProps<T>) => {
         const { property, direction } = useSorting();
-        console.log('SortableDataTable', property, direction, props);
         const column = props.columns.find((column) => column.id === property);
         const comparator = column?.comparator;
         const sortedData = maybeSortData(props.data, comparator, property as keyof T, direction);
@@ -20,6 +19,8 @@ export const withSortable = <T,>(Component: ComponentType<DataTableProps<T>>) =>
  * Wraps the DataTable component with SortingProvider and uses the useSorting hook to manage
  * sorting state. Uses the comparator on each column (or default one) to determine the sorting logic.
  * Note: This requires React Router context to be available in the component tree.
+ * @param {DataTableProps<T>} props - The props for the DataTable component.
+ * @return {JSX.Element} - The wrapped DataTable component with sorting functionality.
  */
 export const SortableDataTable = <T,>(props: DataTableProps<T>) => {
     const WrappedDataTable = withSortable<T>(DataTable);
