30629282b2ef24f0d8e0261ff1b5dfa3efc2f2db
Ben Lambert <benjamin.lambert@cdw.com>
2025-05-14 10:57:15 -0700

    Fix remaining merge conflicts

--- apps/modernization-ui/src/apps/patient/file/patientSummary/PatientSummaryCard.tsx
+++
@@ -1,9 +0,0 @@
-const PatientSummaryCard = () => {
-    return (
-        <div style={{ marginTop: '1rem', marginBottom: '3rem' }}>
-            <span>Patient Summary Card</span>
-        </div>
-    );
-};
-
-export default PatientSummaryCard;
