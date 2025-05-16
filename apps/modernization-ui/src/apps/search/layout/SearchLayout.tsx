31909c7bb68ec14330e824700a58232671bbaa48
Henry Clark <hclark@enquizit.com>
2025-05-16 14:24:48 -0400

    fixing prop names to match their actions purpose

--- apps/modernization-ui/src/apps/search/layout/SearchLayout.tsx
+++ apps/modernization-ui/src/apps/search/layout/SearchLayout.tsx
@@ -69,7 +69,7 @@ const SearchLayout = <R,>({
                     contentClassName={styles.criteria}
                     ariaLabel="Search criteria">
                     <div className={styles.inputs}>{criteria()}</div>
-                    <div className={styles.actions}>
+                    <div className={styles.criteriaActions}>
                         <Button type="button" onClick={onSearch} disabled={!searchEnabled} sizing={sizing}>
                             Search
                         </Button>
