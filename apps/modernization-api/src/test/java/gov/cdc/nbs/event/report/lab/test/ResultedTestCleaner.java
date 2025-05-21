7e5b4c91a91dc31dc196e64ac81c4ec2b7fb77b4
Adam Loup <adam.loup@cdwg.com>
2025-05-21 08:33:53 -0500

    ensures that Lab Reports and Lab Tests are cleaned correctly between scenarios

--- apps/modernization-api/src/test/java/gov/cdc/nbs/event/report/lab/test/ResultedTestCleaner.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/event/report/lab/test/ResultedTestCleaner.java
@@ -1,4 +1,4 @@
-package gov.cdc.nbs.event.report.lab;
+package gov.cdc.nbs.event.report.lab.test;
 
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Component;
@@ -6,22 +6,29 @@ import org.springframework.stereotype.Component;
 import java.util.Collection;
 
 @Component
-class TestLabReportCleaner {
+ class ResultedTestCleaner {
 
   private final static String DELETE_IN = """
       delete from Participation where act_class_cd = 'OBS' and and act_uid in (:identifiers);
       
+      delete from Obs_value_coded where observation_uid in (:identifiers);
+      
       delete from Observation 
       where   ctrl_cd_display_form = 'LabReport' 
-          and obs_domain_cd_st_1 = 'Order'
+          and obs_domain_cd_st_1 = 'Result'
           and observation_uid in (:identifiers);
+      
+      delete from Act_relationship
+      where   source_act_uid in (:identifiers);
+          and source_class_cd = 'OBS'
+          and type_cd = 'COMP'
       
       delete from Act where class_cd = 'OBS' and act_uid in (:identifiers);
       """;
 
   private final JdbcClient client;
 
-  TestLabReportCleaner(final JdbcClient client) {
+  ResultedTestCleaner(final JdbcClient client) {
     this.client = client;
   }
 
