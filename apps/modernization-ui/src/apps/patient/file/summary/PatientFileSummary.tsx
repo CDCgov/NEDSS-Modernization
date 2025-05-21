5b67def5491cc6cfb29df7fc4c453d9c4c529f99
Ben Lambert <benjamin.lambert@cdw.com>
2025-05-20 17:21:43 -0700

    remove br and replace with proper summary-layout styles

--- apps/modernization-ui/src/apps/patient/file/summary/PatientFileSummary.tsx
+++ apps/modernization-ui/src/apps/patient/file/summary/PatientFileSummary.tsx
@@ -2,15 +2,15 @@ import { usePatient } from '../usePatient';
 import { usePatientFileSummary } from './usePatientFileSummary';
 import { PatientSummaryContent } from './PatientSummaryContent';
 import OpenInvestigationsCard from './openInvestigations/OpenInvestigationsCard';
+import styles from './patient-file-summary.module.scss';
 
 export const PatientFileSummary = () => {
     const { id } = usePatient();
 
     const { summary } = usePatientFileSummary(id);
     return (
-        <div>
+        <div className={styles['summary-layout']}>
             <PatientSummaryContent summary={summary} />
-            <br />
             <OpenInvestigationsCard />
         </div>
     );
