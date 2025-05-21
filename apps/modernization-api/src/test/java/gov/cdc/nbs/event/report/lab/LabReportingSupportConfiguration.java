6506a6b7758b9efb84fb641b0019762f930cc1fc
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2025-05-21 09:12:36 -0500

    [Feature Tests] Resulted test clean up (#2569)

--- apps/modernization-api/src/test/java/gov/cdc/nbs/event/report/lab/LabReportingSupportConfiguration.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/event/report/lab/LabReportingSupportConfiguration.java
@@ -2,6 +2,7 @@ package gov.cdc.nbs.event.report.lab;
 
 import gov.cdc.nbs.testing.support.Active;
 import gov.cdc.nbs.testing.support.Available;
+import io.cucumber.spring.ScenarioScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,16 +10,19 @@ import org.springframework.context.annotation.Configuration;
 class LabReportingSupportConfiguration {
 
   @Bean
+  @ScenarioScope
   Active<LabReportIdentifier> activeLabReport() {
     return new Active<>();
   }
 
   @Bean
+  @ScenarioScope
   Available<LabReportIdentifier> availableLabReport() {
     return new Available<>();
   }
 
   @Bean
+  @ScenarioScope
   Active<AccessionIdentifier> activeAccessionIdentifier() {
     return new Active<>();
   }
