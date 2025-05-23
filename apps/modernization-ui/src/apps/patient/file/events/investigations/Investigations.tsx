ae48dbf11af61efa4658c4877117bfbcfc70a669
hclarkEnq <hclark@enquizit.com>
2025-05-23 12:53:17 -0400

    setting up the event folders for the investigation table card

---
+++ apps/modernization-ui/src/apps/patient/file/events/investigations/Investigations.tsx
@@ -0,0 +1,111 @@
+import { TableCard } from 'design-system/card/table/TableCard';
+import { Headers } from './PatientInvestigation';
+import { Icon } from 'design-system/icon';
+import { Column } from 'design-system/table';
+import { format } from 'date-fns';
+import { displayName } from 'name';
+import { usePatientInvestigations } from './usePatientInvestigations';
+import { usePatient } from '../../usePatient';
+import { PatientInvestigation } from 'generated';
+import { Tag } from 'design-system/tag';
+import { TagVariant } from 'design-system/tag/Tag';
+
+const displayTag = (value: string | undefined, status: boolean, variant: TagVariant) => {
+    if (!value) return '';
+    return status ? <Tag variant={variant}>{value}</Tag> : value;
+};
+
+const columns: Column<PatientInvestigation>[] = [
+    {
+        id: Headers.Investigation,
+        name: Headers.Investigation,
+        sortable: true,
+        render: (value) => value.investigationId
+    },
+    {
+        id: Headers.StartDate,
+        name: Headers.StartDate,
+        sortable: true,
+        render: (value) => value?.startedOn && format(value.startedOn, 'MM/dd/yyyy'),
+        sortIconType: 'numeric'
+    },
+    {
+        id: Headers.Condition,
+        name: Headers.Condition,
+        sortable: true,
+        render: (value) => value.condition,
+        sortIconType: 'alpha'
+    },
+    {
+        id: Headers.Status,
+        name: Headers.Status,
+        sortable: true,
+        render: (value) => displayTag(value.status, value.status === 'Open', 'success'),
+        sortIconType: 'alpha'
+    },
+    {
+        id: Headers.CaseStatus,
+        name: Headers.CaseStatus,
+        sortable: true,
+        render: (value) => value.caseStatus,
+        sortIconType: 'alpha'
+    },
+    {
+        id: Headers.Notification,
+        name: Headers.Notification,
+        sortable: true,
+        render: (value) => displayTag(value.notification, value.notification === 'Rejected', 'error'),
+        sortIconType: 'alpha'
+    },
+    {
+        id: Headers.Jurisdiction,
+        name: Headers.Jurisdiction,
+        sortable: true,
+        render: (value) => value.jurisdiction,
+        sortIconType: 'alpha'
+    },
+    {
+        id: Headers.Investigator,
+        name: Headers.Investigator,
+        sortable: true,
+        render: (value) => (value.investigatorName ? displayName('short')(value.investigatorName) : ''),
+        sortIconType: 'alpha'
+    },
+    {
+        id: Headers.CoInfection,
+        name: Headers.CoInfection,
+        sortable: true,
+        render: (value) => value.coInfection,
+        sortIconType: 'numeric'
+    }
+];
+
+export const Investigations = () => {
+    const { id } = usePatient();
+    const { data } = usePatientInvestigations(id);
+
+    return (
+        <TableCard
+            title="Investigations"
+            id={'investigations'}
+            columnPreferencesKey={'investigations'}
+            columns={columns}
+            actions={[
+                {
+                    sizing: 'small',
+                    secondary: true,
+                    children: 'Add investigation',
+                    icon: <Icon name="add_circle" />,
+                    labelPosition: 'right',
+                    onClick: () => console.log('Add Person clicked')
+                },
+                {
+                    sizing: 'small',
+                    secondary: true,
+                    children: 'Compare investigations'
+                }
+            ]}
+            data={data ?? []}
+        />
+    );
+};
---
+++ apps/modernization-ui/src/apps/patient/file/events/investigations/PatientInvestigation.ts
@@ -0,0 +1,11 @@
+export enum Headers {
+    Investigation = 'Investigation #',
+    StartDate = 'Start date',
+    Condition = 'Condition',
+    Status = 'Status',
+    CaseStatus = 'Case status',
+    Notification = 'Notification',
+    Jurisdiction = 'Jurisdiction',
+    Investigator = 'Investigator',
+    CoInfection = 'Co-infection #'
+}
