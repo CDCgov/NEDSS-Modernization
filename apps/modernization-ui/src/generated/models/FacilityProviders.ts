651444de15ad6b9d121b4a7180838547a97be637
Steve Urciuoli <surciuoli@enquizit.com>
2025-05-23 17:17:47 -0500

    CNFT1-4128 API: Lab reports: List all the lab reports for the patient file (#2568)

---
+++ apps/modernization-ui/src/generated/models/FacilityProviders.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { OrderingProvider } from './OrderingProvider';
+export type FacilityProviders = {
+    reportingFacility?: string;
+    orderingProvider?: OrderingProvider;
+    sendingFacility?: string;
+};
+
---
+++ apps/modernization-ui/src/generated/models/OrderingProvider.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type OrderingProvider = {
+    prefix?: string;
+    first?: string;
+    last?: string;
+    suffix?: string;
+};
+
