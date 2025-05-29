e49e78659c0b383ffb55bf64f16287abdbec0f36
Steven Urciuoli <steveu@mac.lan>
2025-05-29 09:51:39 -0500

    307

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/investigation/AddInvestigationRedirector.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/investigation/AddInvestigationRedirector.java
@@ -2,10 +2,12 @@ package gov.cdc.nbs.patient.profile.investigation;
 
 import gov.cdc.nbs.patient.profile.redirect.outgoing.ClassicPatientProfileRedirector;
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import java.net.URISyntaxException;
 
@@ -25,6 +27,7 @@ class AddInvestigationRedirector {
 
   @PreAuthorize("hasAuthority('ADD-INVESTIGATION')")
   @GetMapping("/nbs/api/profile/{patient}/investigation")
+  @ResponseStatus(value = HttpStatus.TEMPORARY_REDIRECT)
   ResponseEntity<Void> add(@PathVariable final long patient) throws URISyntaxException {
     URI uri = new URI(LOCATION);
 
