3975540fc015e809e27d1865549d2058a6064b3e
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2025-05-29 18:48:37 -0400

    Cnft1 4107 investigation table events (#2581)
    * adding fixes to the investigation table
    
    * adding bold to the conditions columns value
    
    * adding size small fixing fontsize and size issues
    
    * adding size small to table
    
    * adding details for the lineheight and weight bold
    
    * adding permissions and noDataFallback
    
    * spelling fix
    
    * small change
    
    * removing unneeded permission from client side and using internalize date function
    
    * adding sorting using new sorting structure

--- apps/modernization-ui/src/apps/patient/file/events/investigations/Investigations.tsx
+++ apps/modernization-ui/src/apps/patient/file/events/investigations/Investigations.tsx
@@ -2,92 +2,128 @@ import { TableCard } from 'design-system/card/table/TableCard';
 import { Headers, Investigation } from './PatientInvestigation';
 import { Icon } from 'design-system/icon';
 import { Column } from 'design-system/table';
-import { format } from 'date-fns';
 import { displayName } from 'name';
 import { usePatientInvestigations } from './usePatientInvestigations';
 import { usePatient } from '../../usePatient';
 import { Tag } from 'design-system/tag';
 import { TagVariant } from 'design-system/tag/Tag';
+import { ColumnPreference } from 'design-system/table/preferences';
+import { ClassicLink } from 'classic';
+import { internalizeDate } from 'date';
+import { useMemo } from 'react';
+import { mapOr } from 'utils/mapping';
 
 const displayTag = (value: string | undefined, status: boolean, variant: TagVariant) => {
     if (!value) return '';
-    return status ? <Tag variant={variant}>{value}</Tag> : value;
+    return status ? (
+        <Tag size="small" weight="bold" variant={variant}>
+            {value}
+        </Tag>
+    ) : (
+        value
+    );
 };
 
-const columns: Column<Investigation>[] = [
+const INVESTIGATION_ID = { id: 'investigationId', name: Headers.Investigation };
+const START_DATE = { id: 'startedOn', name: Headers.StartDate };
+const STATUS = { id: 'status', name: Headers.Status };
+const CONDITION = { id: 'condition', name: Headers.Condition };
+const CASE_STATUS = { id: 'caseStatus', name: Headers.CaseStatus };
+const NOTIFICATION = { id: 'notification', name: Headers.Notification };
+const JURISDICTION = { id: 'jurisdiction', name: Headers.Jurisdiction };
+const INVESTIGATOR = { id: 'investigatorName', name: Headers.Investigator };
+const CO_INFECTION = { id: 'coInfection', name: Headers.CoInfection };
+
+const maybeDisplayName = mapOr(displayName('short'), undefined);
+
+const createColumns = (id: number): Column<Investigation>[] => [
     {
-        id: Headers.Investigation,
-        name: Headers.Investigation,
+        ...INVESTIGATION_ID,
         sortable: true,
-        render: (value) => value.investigationId
+        value: (row) => row.investigationId,
+        render: (value) => (
+            <ClassicLink url={`/patient/${id}/investigation/${value.investigationId}`}>
+                {value.investigationId}
+            </ClassicLink>
+        )
     },
     {
-        id: Headers.StartDate,
-        name: Headers.StartDate,
+        ...START_DATE,
         sortable: true,
-        render: (value) => value?.startedOn && format(value.startedOn, 'MM/dd/yyyy'),
+        value: (row) => row.startedOn,
+        render: (value) => internalizeDate(value.startedOn),
         sortIconType: 'numeric'
     },
     {
-        id: Headers.Condition,
-        name: Headers.Condition,
+        ...STATUS,
         sortable: true,
-        render: (value) => value.condition,
-        sortIconType: 'alpha'
-    },
-    {
-        id: Headers.Status,
-        name: Headers.Status,
-        sortable: true,
+        value: (row) => row.status,
         render: (value) => displayTag(value.status, value.status === 'Open', 'success'),
         sortIconType: 'alpha'
     },
     {
-        id: Headers.CaseStatus,
-        name: Headers.CaseStatus,
+        ...CONDITION,
         sortable: true,
-        render: (value) => value.caseStatus,
+        value: (row) => row.condition,
+        render: (value) => <b>{value.condition}</b>
+    },
+    {
+        ...CASE_STATUS,
+        sortable: true,
+        value: (row) => row.caseStatus,
+        render: (value) => displayTag(value.caseStatus, value.caseStatus === 'Open', 'success'),
         sortIconType: 'alpha'
     },
     {
-        id: Headers.Notification,
-        name: Headers.Notification,
+        ...NOTIFICATION,
         sortable: true,
+        value: (row) => row.notification,
         render: (value) => displayTag(value.notification, value.notification === 'Rejected', 'error'),
         sortIconType: 'alpha'
     },
     {
-        id: Headers.Jurisdiction,
-        name: Headers.Jurisdiction,
+        ...JURISDICTION,
         sortable: true,
-        render: (value) => value.jurisdiction,
-        sortIconType: 'alpha'
+        value: (row) => row.jurisdiction
     },
     {
-        id: Headers.Investigator,
-        name: Headers.Investigator,
+        ...INVESTIGATOR,
         sortable: true,
-        render: (value) => (value.investigatorName ? displayName('short')(value.investigatorName) : ''),
-        sortIconType: 'alpha'
+        value: (row) => maybeDisplayName(row.investigatorName),
+        render: (value) => (value.investigatorName ? displayName('short')(value.investigatorName) : '')
     },
     {
-        id: Headers.CoInfection,
-        name: Headers.CoInfection,
+        ...CO_INFECTION,
         sortable: true,
-        render: (value) => value.coInfection,
-        sortIconType: 'numeric'
+        value: (row) => row.coInfection
     }
+];
+
+const columnPreferences: ColumnPreference[] = [
+    { ...INVESTIGATION_ID },
+    { ...START_DATE, moveable: true, toggleable: true },
+    { ...STATUS, moveable: true, toggleable: true },
+    { ...CONDITION, moveable: true, toggleable: true },
+    { ...CASE_STATUS, moveable: true, toggleable: true },
+    { ...NOTIFICATION, moveable: true, toggleable: true },
+    { ...JURISDICTION, moveable: true, toggleable: true },
+    { ...INVESTIGATOR, moveable: true, toggleable: true },
+    { ...CO_INFECTION, moveable: true, toggleable: true }
 ];
 
 export const Investigations = () => {
     const { id } = usePatient();
     const { data } = usePatientInvestigations(id);
 
+    const columns = useMemo(() => createColumns(id), [id]);
+
     return (
         <TableCard
+            sizing="small"
             title="Investigations"
             id={'investigations'}
-            columnPreferencesKey={'investigations'}
+            columnPreferencesKey={'patient.file.open-investigations.preferences.${column.id}'}
+            defaultCollapsed={!data?.length}
             columns={columns}
             actions={[
                 {
@@ -104,7 +140,9 @@ export const Investigations = () => {
                     children: 'Compare investigations'
                 }
             ]}
+            noDataFallback
             data={data ?? []}
+            defaultColumnPreferences={columnPreferences}
         />
     );
 };
