08e29a85b31eac3ced4d5e38d66e3fff46855a9e
Steven Urciuoli <steveu@mac.lan>
2025-05-29 09:26:35 -0500

    CNFT1-4070 API: Add Investigations API

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/investigation/AddInvestigationRedirector.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/profile/investigation/AddInvestigationRedirector.java
@@ -7,7 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URISyntaxException;
 
 import java.net.URI;
 
@@ -15,25 +15,22 @@ import java.net.URI;
 @RestController
 class AddInvestigationRedirector {
 
-    private static final String LOCATION = "/nbs/LoadSelectCondition1.do";
+  private static final String LOCATION = "/nbs/LoadSelectCondition1.do?ContextAction=AddInvestigation";
 
-    private final ClassicPatientProfileRedirector redirector;
+  private final ClassicPatientProfileRedirector redirector;
 
-    AddInvestigationRedirector(final ClassicPatientProfileRedirector redirector) {
-        this.redirector = redirector;
-    }
+  AddInvestigationRedirector(final ClassicPatientProfileRedirector redirector) {
+    this.redirector = redirector;
+  }
 
-    @PreAuthorize("hasAuthority('ADD-INVESTIGATION')")
-    @GetMapping("/nbs/api/profile/{patient}/investigation")
-    ResponseEntity<Void> add(@PathVariable final long patient) {
+  @PreAuthorize("hasAuthority('ADD-INVESTIGATION')")
+  @GetMapping("/nbs/api/profile/{patient}/investigation")
+  ResponseEntity<Void> add(@PathVariable final long patient) throws URISyntaxException {
+    URI uri = new URI(LOCATION);
 
-        URI location = UriComponentsBuilder.fromPath(LOCATION)
-            .queryParam("ContextAction", "AddInvestigation")
-            .build()
-            .toUri();
-
-        return redirector.preparedRedirect(patient, location);
-    }
+    return redirector.preparedRedirect(patient, uri);
+  }
 
 
 }
+//
