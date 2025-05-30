75dae58210a31673b1b5bf421c3fcb44443f25a4
Michael Peels <m.peels.dev@gmail.com>
2025-05-30 17:12:21 -0400

    Testing

--- apps/modernization-ui/src/apps/deduplication/patient-merge/details/merge-review/MergeReview.spec.tsx
+++ apps/modernization-ui/src/apps/deduplication/patient-merge/details/merge-review/MergeReview.spec.tsx
@@ -11,9 +11,9 @@ const onRemove = jest.fn();
 const Fixture = () => {
     const form = useForm<PatientMergeForm>();
     const data: Partial<MergePatient>[] = [
-        { personUid: '100', adminComments: { date: '2025-05-01T00:00', comment: 'First comment' } },
-        { personUid: '200', adminComments: { date: '2005-01-21T00:00', comment: 'Second comment' } },
-        { personUid: '300', adminComments: { date: '1995-04-23T00:00', comment: 'Third comment' } }
+        { personUid: '100', adminComments: { date: '2025-05-01T00:00', comment: 'First comment' }, ethnicity: {} },
+        { personUid: '200', adminComments: { date: '2005-01-21T00:00', comment: 'Second comment' }, ethnicity: {} },
+        { personUid: '300', adminComments: { date: '1995-04-23T00:00', comment: 'Third comment' }, ethnicity: {} }
     ];
     return (
         <MemoryRouter>
