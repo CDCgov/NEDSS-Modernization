ae4e1e0c0af37c45b50e66b9e1f040f971ff68ea
Henry Clark <hclark@enquizit.com>
2025-06-02 17:32:55 -0400

    prettier fix

--- apps/modernization-ui/src/apps/search/layout/result/NoInput.tsx
+++ apps/modernization-ui/src/apps/search/layout/result/NoInput.tsx
@@ -5,7 +5,7 @@ const NoInput = () => {
     return (
         <div className={styles.noResults}>
             <AlertMessage type="warning">
-                <div className={styles.noResultsContent} tabIndex={0} >
+                <div className={styles.noResultsContent} tabIndex={0}>
                     <span className={styles.noResultsHeader}>No results found</span>
                     <span className={styles.noResultsSubHeading}>You must enter at least one item to search.</span>
                 </div>
