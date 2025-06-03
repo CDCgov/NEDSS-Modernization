35703b7fc22092b87faf65ac65d5197d4a268e9a
Ben Lambert <benjamin.lambert@cdw.com>
2025-06-03 11:09:26 -0700

    Cnft1 4129 UI lab reports list all the lab reports for the patient file (#2596)
    * add usePatientLabReports hook
    
    * set up lab report boilerplate
    
    * use some mock patient lab report data to debug
    
    * add date received formatting
    
    * format FacilityProvider column
    
    * only show reporting, sending and ordering when present
    
    * display collectedDate as locale date collected
    
    * add multiple test results
    
    * use displayTestResults
    
    * update mock test
    
    * add associated investigation render info
    
    * add Add lab report button
    
    * add style={{ paddingBottom: '0.5rem' }}> to slightly visually seperate test results
    
    * Update LabReports.tsx
    
    * use line breaks
    
    * remove mocks and use real data from import { usePatientLabReports } from './usePatientLabReports';
    
    * Reuse renderFacilityProvider
    
    * use mocks
    
    * add sortable value to columns
    
    * move constants, remove div
    
    * use events-layout and create patient file events stylesheet
    
    * remove mocks
    
    * shorten ternary

--- apps/modernization-ui/src/apps/patient/file/events/PatientFileEvents.tsx
+++ apps/modernization-ui/src/apps/patient/file/events/PatientFileEvents.tsx
@@ -1,9 +1,12 @@
 import { Investigations } from './investigations/Investigations';
+import LabReports from './labReports/LabReports';
+import styles from './patient-file-events.module.scss';
 
 export const PatientFileEvents = () => {
     return (
-        <div>
+        <div className={styles['events-layout']}>
             <Investigations />
+            <LabReports />
         </div>
     );
 };
