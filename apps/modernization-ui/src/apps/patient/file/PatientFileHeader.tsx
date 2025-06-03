c39d245b4d528372df558eac239d4bb879b81539
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2025-06-03 10:48:32 -0500

    [CNFT1-4245] Makes Patient file header sticky (#2603)

--- apps/modernization-ui/src/apps/patient/file/PatientFileHeader.tsx
+++ apps/modernization-ui/src/apps/patient/file/PatientFileHeader.tsx
@@ -11,9 +11,9 @@ type PatientFileHeaderProps = {
 
 export const PatientFileHeader = ({ patient, actions }: PatientFileHeaderProps) => {
     return (
-        <header className={styles.header}>
+        <div className={styles.header}>
             <PatientDescriptor headingLevel={1} patient={patient} />
             <div className={styles.actions}>{actions}</div>
-        </header>
+        </div>
     );
 };
