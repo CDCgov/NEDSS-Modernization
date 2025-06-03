bb7673ace0db4d34c2922d76d78d9aaea6d356bf
Ben Lambert <benjamin.lambert@cdw.com>
2025-06-03 07:45:31 -0700

    use events-layout and create patient file events stylesheet

--- apps/modernization-ui/src/apps/patient/file/events/PatientFileEvents.tsx
+++ apps/modernization-ui/src/apps/patient/file/events/PatientFileEvents.tsx
@@ -1,11 +1,11 @@
 import { Investigations } from './investigations/Investigations';
 import LabReports from './labReports/LabReports';
+import styles from './patient-file-events.module.scss';
 
 export const PatientFileEvents = () => {
     return (
-        <div>
+        <div className={styles['events-layout']}>
             <Investigations />
-            <br />
             <LabReports />
         </div>
     );
