593f38ac240a1e5a19959486f3b89cf1d5983fcf
Adam Loup <adam.loup@cdwg.com>
2025-06-04 09:40:40 -0500

    sonar

--- libs/authentication/src/test/java/gov/cdc/nbs/authentication/IgnoredPathsTest.java
+++ libs/authentication/src/test/java/gov/cdc/nbs/authentication/IgnoredPathsTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class IgnoredPathsTest {
 
