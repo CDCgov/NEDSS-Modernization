4da18910981ed4a845248400aaa8addb3b7d5869
Steven Urciuoli <steveu@mac.lan>
2025-06-05 14:57:27 -0500

    update

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/names/PatientNamesFinder.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/names/PatientNamesFinder.java
@@ -6,40 +6,39 @@ import java.util.List;
 
 @Component
 class PatientNamesFinder {
-  private String query() {
-    return """
-          select
-            [name].person_uid,
-            [name].as_of_date,
-            [name].nm_use_cd,
-            [use].[code_short_desc_txt],
-            [name].nm_prefix,
-            [prefix].[code_short_desc_txt],
-            [name].first_nm,
-            [name].middle_nm,
-            [name].last_nm,
-            [name].nm_suffix,
-            [name].nm_suffix,
-            [name].nm_degree,
-            [degree].[code_short_desc_txt]
-        from person_name [name]
-        join NBS_SRTE..Code_value_general [use] on
-                   [use].[code_set_nm] = 'P_NM_USE'
-               and [use].[code] = [name].nm_use_cd
-        left join NBS_SRTE..Code_value_general [suffix] on
-                    [suffix].[code_set_nm] = 'P_NM_SFX'
-                and [suffix].[code] = [name].nm_suffix
-        left join NBS_SRTE..Code_value_general [degree] on
-                    [suffix].[code_set_nm] = 'P_NM_DEG'
-                and [suffix].[code] = [name].nm_degree
-        left join NBS_SRTE..Code_value_general [prefix] on
-                    [suffix].[code_set_nm] = 'P_NM_PFX'
-                and [suffix].[code] = [name].nm_prefix
-        where
-               [name].record_status_cd = 'ACTIVE' and
-               [name].person_uid = ?
-        """;
-  }
+  private String SELECT_NAMES_SQL =
+      """
+            select
+              [name].person_uid,
+              [name].as_of_date,
+              [name].nm_use_cd,
+              [use].[code_short_desc_txt],
+              [name].nm_prefix,
+              [prefix].[code_short_desc_txt],
+              [name].first_nm,
+              [name].middle_nm,
+              [name].last_nm,
+              [name].nm_suffix,
+              [name].nm_suffix,
+              [name].nm_degree,
+              [degree].[code_short_desc_txt]
+          from person_name [name]
+          join NBS_SRTE..Code_value_general [use] on
+                     [use].[code_set_nm] = 'P_NM_USE'
+                 and [use].[code] = [name].nm_use_cd
+          left join NBS_SRTE..Code_value_general [suffix] on
+                      [suffix].[code_set_nm] = 'P_NM_SFX'
+                  and [suffix].[code] = [name].nm_suffix
+          left join NBS_SRTE..Code_value_general [degree] on
+                      [suffix].[code_set_nm] = 'P_NM_DEG'
+                  and [suffix].[code] = [name].nm_degree
+          left join NBS_SRTE..Code_value_general [prefix] on
+                      [suffix].[code_set_nm] = 'P_NM_PFX'
+                  and [suffix].[code] = [name].nm_prefix
+          where
+                 [name].record_status_cd = 'ACTIVE' and
+                 [name].person_uid = ?
+          """;
 
   private static final int PERSON_UID_PARAMETER = 1;
 
@@ -54,7 +53,7 @@ class PatientNamesFinder {
 
   List<PatientName> find(final long personUid) {
     return this.template.query(
-        query(),
+        SELECT_NAMES_SQL,
         statement -> statement.setLong(PERSON_UID_PARAMETER, personUid),
         this.mapper);
   }
--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/names/PatientNamesResolver.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/names/PatientNamesResolver.java
@@ -1,8 +1,5 @@
 package gov.cdc.nbs.patient.names;
 
-import gov.cdc.nbs.authorization.permission.Permission;
-import gov.cdc.nbs.authorization.permission.scope.PermissionScope;
-import gov.cdc.nbs.authorization.permission.scope.PermissionScopeResolver;
 import org.springframework.stereotype.Component;
 import java.util.List;
 
@@ -11,13 +8,11 @@ class PatientNamesResolver {
   private final PatientNamesFinder patientNamesFinder;
 
   PatientNamesResolver(
-      final PatientNamesFinder patientNamesFinder,
-      final PermissionScopeResolver scopeResolver) {
+      final PatientNamesFinder patientNamesFinder) {
     this.patientNamesFinder = patientNamesFinder;
   }
 
   public List<PatientName> resolve(final long patientId) {
-    List<PatientName> labReportResults = patientNamesFinder.find(patientId);
-    return labReportResults;
+    return patientNamesFinder.find(patientId);
   }
 }
