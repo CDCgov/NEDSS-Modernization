6bcab23bd38ccc619223c32d0406a55bc747f055
Henry Clark <111446161+hclarkEnq@users.noreply.github.com>
2025-06-09 15:07:37 -0400

    [Cnft1 4117]: overlay panel setfocus (#2624)
    * passing ref to components
    
    * passing ref to components again and fixing some styles because of button use
    
    * focus management more predictable now as its contained within the OverlayPanel component
    
    * adding tertiary and size medium
    
    * moving x icon to last element on tree and reusing closable panel, also moving state to only live in overlay
    
    * unused imports
    
    * removing unneeded ref
    
    * addressing some comments and refactoring

--- apps/modernization-ui/src/design-system/sorting/preferences/SortPreference.tsx
+++ apps/modernization-ui/src/design-system/sorting/preferences/SortPreference.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import { SortingSelectable } from './selectable';
 
 import styles from './sort-preference.module.scss';
+import { Button } from 'design-system/button';
 
 type Props = {
     className?: string;
@@ -12,11 +13,15 @@ type Props = {
 };
 
 const SortPreference = ({ className, active, selectable, onSelect }: Props) => (
-    <span className={classNames(styles.preference, className)} onClick={() => onSelect(selectable)}>
+    <Button
+        className={classNames(styles.preference, className)}
+        onClick={() => onSelect(selectable)}
+        aria-label={`Sort by ${selectable.name}`}
+        tertiary>
         {active && <Icon className={styles.indicator} name="check" />}
         {!active && <span className={styles.placeholder} />}
         {selectable.name}
-    </span>
+    </Button>
 );
 
 export { SortPreference };
