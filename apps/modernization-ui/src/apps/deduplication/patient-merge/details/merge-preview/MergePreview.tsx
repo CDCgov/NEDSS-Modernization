4ffadce842e209fb1e3314dd8db7d212d7b5fd7c
snehaasjournal <uob6@cdc.gov>
2025-06-09 14:55:23 -0700

    CND-321: Preview-Summary & AdminComments

--- apps/modernization-ui/src/apps/deduplication/patient-merge/details/merge-preview/MergePreview.tsx
+++ apps/modernization-ui/src/apps/deduplication/patient-merge/details/merge-preview/MergePreview.tsx
@@ -1,14 +1,10 @@
 import { Heading } from 'components/heading';
 import { Button } from 'design-system/button';
 import styles from './merge-preview.module.scss';
-import { PatientSummary } from './components/patient-summary/PatientSummary';
 import { PatientMergeForm } from '../merge-review/model/PatientMergeForm';
 import { MergeCandidate } from '../../../api/model/MergeCandidate';
+import { PatientSummary } from './components/patient-summary/PatientSummary';
 import { AdministrativeComments } from './components/administrative-comments/AdministrativeComments';
-import { PreviewName } from './components/name/PreviewName';
-import { PreviewAddress } from './components/address/PreviewAddress';
-import { PreviewPhoneAndEmail } from './components/phone-and-email/PreviewPhoneAndEmail';
-import { PreviewIdentification } from './components/identification/PreviewIdentification';
 
 type MergePreviewProps = {
     onBack: () => void;
@@ -35,32 +31,6 @@ export const MergePreview = ({ onBack, mergeFormData, mergeCandidates }: MergePr
             <section className={styles.summaryCardSection}>
                 <PatientSummary mergeFormData={mergeFormData} mergeCandidates={mergeCandidates} />
                 <AdministrativeComments mergeFormData={mergeFormData} mergeCandidates={mergeCandidates} />
-                <PreviewName
-                    selectedNames={mergeFormData.names.map(({ personUid, sequence }) => ({
-                        personUid,
-                        sequence
-                    }))}
-                    mergeCandidates={mergeCandidates}
-                />
-                <PreviewAddress
-                    selectedAddresses={mergeFormData.addresses.map(({ locatorId }) => ({
-                        locatorId
-                    }))}
-                    mergeCandidates={mergeCandidates}
-                />
-                <PreviewPhoneAndEmail
-                    selectedPhoneEmails={mergeFormData.phoneEmails.map(({ locatorId }) => ({
-                        locatorId
-                    }))}
-                    mergeCandidates={mergeCandidates}
-                />
-                <PreviewIdentification
-                    selectedIdentifications={mergeFormData.identifications.map(({ personUid, sequence }) => ({
-                        personUid,
-                        sequence
-                    }))}
-                    mergeCandidates={mergeCandidates}
-                />
             </section>
         </div>
     );
