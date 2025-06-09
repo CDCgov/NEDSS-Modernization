12fcbda0b65956f81e87d8dd967eab0bc5d5a2bd
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-06-09 10:41:53 -0400

    sonar fix

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/labreport/PatientLabReport.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/labreport/PatientLabReport.java
@@ -2,7 +2,6 @@ package gov.cdc.nbs.patient.labreport;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import gov.cdc.nbs.patient.events.tests.ResultedTest;
 
