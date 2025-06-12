571dd837865d860f22e93bce37c8536b8e6a6c50
Joe Agster <98052312+joeaxcdw@users.noreply.github.com>
2025-06-12 08:49:34 -0700

    NIS-48: Upgrade Storybook and fix any stories (React 19) (#2649)
    * Squash merge from branch CNFT1-4254-react19-unit-tests
    
    * Migrate Storybook
    
    * Merge from latest

--- apps/modernization-ui/src/design-system/table/DataTable.stories.tsx
+++ apps/modernization-ui/src/design-system/table/DataTable.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react-vite';
 import { Column, DataTable } from './DataTable';
 import { Checkbox } from 'design-system/checkbox';
 
