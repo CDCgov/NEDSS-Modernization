e4eb93cd36d07eb254e52f71ff80b0bf9e280565
Ben Lambert <benjamin.lambert@cdw.com>
2025-06-16 12:39:46 -0700

    Update preference page size into SearchPageProvider

--- apps/modernization-ui/src/apps/search/SearchPage.tsx
+++ apps/modernization-ui/src/apps/search/SearchPage.tsx
@@ -5,25 +5,36 @@ import { SortingProvider, SortingSettings } from 'libs/sorting';
 import { SearchResultDisplayProvider } from './useSearchResultDisplay';
 import { ComponentSizingProvider } from 'design-system/sizing';
 import { FilterProvider } from 'design-system/filter';
+import { useLocalStorage } from 'storage';
+// const PAGE_SIZE_PREFERENCE_KEY = 'patient-search-page-size';
 
 const SEARCH_PAGE_SIZE = 20;
 
 type SearchPageProviderProps = { sorting?: SortingSettings; paging?: PaginationSettings; children: ReactNode };
 
-const SearchPageProvider = ({ sorting, paging, children }: SearchPageProviderProps) => (
-    <ComponentSizingProvider>
-        <SortingProvider {...sorting} appendToUrl={sorting?.appendToUrl === undefined ? false : sorting.appendToUrl}>
-            <PaginationProvider
-                {...paging}
-                pageSize={paging?.pageSize || SEARCH_PAGE_SIZE}
-                appendToUrl={paging?.appendToUrl === undefined ? false : paging.appendToUrl}>
-                <FilterProvider>
-                    <SearchResultDisplayProvider>{children}</SearchResultDisplayProvider>
-                </FilterProvider>
-            </PaginationProvider>
-        </SortingProvider>
-    </ComponentSizingProvider>
-);
+const SearchPageProvider = ({ sorting, paging, children }: SearchPageProviderProps) => {
+    const { value: preferencePageSize } = useLocalStorage<number>({
+        key: PAGE_SIZE_PREFERENCE_KEY,
+        initial: SEARCH_PAGE_SIZE
+    });
+
+    return (
+        <ComponentSizingProvider>
+            <SortingProvider
+                {...sorting}
+                appendToUrl={sorting?.appendToUrl === undefined ? false : sorting.appendToUrl}>
+                <PaginationProvider
+                    {...paging}
+                    pageSize={preferencePageSize}
+                    appendToUrl={paging?.appendToUrl === undefined ? false : paging.appendToUrl}>
+                    <FilterProvider>
+                        <SearchResultDisplayProvider>{children}</SearchResultDisplayProvider>
+                    </FilterProvider>
+                </PaginationProvider>
+            </SortingProvider>
+        </ComponentSizingProvider>
+    );
+};
 
 const SearchPage = (props: Omit<SearchPageProviderProps, 'children'>) => (
     <SearchPageProvider {...props}>
