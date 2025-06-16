0fb2ff3a7bfc92e2bca2a3fe4a919249430e002d
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2025-06-16 12:49:28 -0500

    [CNFT1-4207] Patient file ethnicity demographics (#2654)
    * adds patient ethnicity endpoint
    
    * generate client
    
    * postman
    
    * sonar

--- apps/modernization-api/src/test/java/gov/cdc/nbs/patient/demographics/ethnicity/EthnicityDemographicSupportConfiguration.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/patient/demographics/ethnicity/EthnicityDemographicSupportConfiguration.java
@@ -1,19 +1,17 @@
-package gov.cdc.nbs.patient.profile.ethnicity;
+package gov.cdc.nbs.patient.demographics.ethnicity;
 
 import gov.cdc.nbs.testing.support.Active;
 import io.cucumber.spring.ScenarioScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.Clock;
-import java.time.LocalDate;
-
 @Configuration
 class EthnicityDemographicSupportConfiguration {
 
   @Bean
   @ScenarioScope
-  Active<EthnicityDemographic> activeEthnicityDemographic(final Clock clock) {
-    return new Active<>(() -> new EthnicityDemographic(LocalDate.now(clock)));
+  Active<EthnicityDemographic> activeEthnicityDemographic() {
+    return new Active<>();
   }
+
 }
