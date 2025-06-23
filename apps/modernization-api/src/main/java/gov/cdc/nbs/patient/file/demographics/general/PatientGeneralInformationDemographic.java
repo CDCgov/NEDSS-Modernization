c6932590e1eeca056bea99f67b168d2f1123d0e3
Adam Loup <adam.loup@cdwg.com>
2025-06-23 09:59:02 -0500

    postman

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/file/demographics/general/PatientGeneralInformationDemographic.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/file/demographics/general/PatientGeneralInformationDemographic.java
@@ -1,16 +1,13 @@
 package gov.cdc.nbs.patient.file.demographics.general;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import gov.cdc.nbs.data.selectable.Selectable;
 import gov.cdc.nbs.data.sensitive.SensitiveValue;
-import gov.cdc.nbs.time.json.FormattedLocalDateJsonDeserializer;
 
 import java.time.LocalDate;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 record PatientGeneralInformationDemographic(
-    @JsonDeserialize(using = FormattedLocalDateJsonDeserializer.class)
     LocalDate asOf,
     Selectable maritalStatus,
     String maternalMaidenName,
