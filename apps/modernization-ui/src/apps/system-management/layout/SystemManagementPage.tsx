ec9cca428f19bae5ddd6c36c61b44a7615d7d7b2
Snehaa Shri Hari <125155334+snehaasjournal@users.noreply.github.com>
2025-06-24 08:16:56 -0700

    Sys Management: Case Report and Laboratory (#2687)

--- apps/modernization-ui/src/apps/system-management/layout/SystemManagementPage.tsx
+++ apps/modernization-ui/src/apps/system-management/layout/SystemManagementPage.tsx
@@ -1,12 +1,35 @@
 import { Heading } from 'components/heading';
 import { SearchBar } from 'design-system/search/SearchBar';
 import styles from './SystemManagementPage.module.scss';
+import { CaseReportLaboratorySection } from '../components/case-report-and-lab/CaseReportLaboratorySection';
+import { useState } from 'react';
+import { AlertMessage } from '../../../design-system/alert/AlertMessage';
+import { links as caseReportLinks } from '../shared/caseLinks';
 
 const SystemManagementPage = () => {
+    const [filter, setFilter] = useState('');
+    const [alert, setAlert] = useState<null | { type: 'success' | 'error'; message: string }>(null);
     return (
-        <div className={styles.page}>
-            <Heading level={1}>System Management </Heading>
-            <SearchBar placeholder={'Filter by keyword'} aria-label="Search" />
+        <div className={styles.systemManagement}>
+            <header>
+                <div className={styles.titleBar}>
+                    <Heading level={1}>System Management </Heading>
+                    <SearchBar
+                        placeholder={'Filter by keyword'}
+                        aria-label="Search"
+                        value={filter}
+                        onChange={setFilter}
+                    />
+                </div>
+            </header>
+            {alert && (
+                <div className={styles.alertWrapper}>
+                    <AlertMessage type={alert.type} slim={true} onClose={() => setAlert(null)}>
+                        {alert.message}
+                    </AlertMessage>
+                </div>
+            )}
+            <CaseReportLaboratorySection filter={filter} setAlert={setAlert} links={caseReportLinks} />
         </div>
     );
 };
