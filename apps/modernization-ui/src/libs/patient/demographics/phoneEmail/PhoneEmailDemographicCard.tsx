5aa6ca31fe3efc18d69afa5e49559a04272a8f6a
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-07-01 12:39:57 -0400

    small changes

---
+++ apps/modernization-ui/src/libs/patient/demographics/phoneEmail/PhoneEmailDemographicCard.tsx
@@ -0,0 +1,97 @@
+import { RepeatingBlock, RepeatingBlockProps } from 'design-system/entry/multi-value';
+import { initial, PhoneEmailDemographic } from './phoneEmails';
+import { SortHandler, SortingProvider } from 'libs/sorting';
+import { Column, columnSortResolver } from 'design-system/table';
+import { internalizeDate } from 'date';
+import styles from './phone-email-demographic-card.module.scss';
+import { PhoneEmailDemographicView } from './PhoneEmailDemographicView';
+
+const columns: Column<PhoneEmailDemographic>[] = [
+    {
+        id: 'phone-email-as-of',
+        name: 'As of',
+        className: styles['date-header'],
+        sortable: true,
+        sortIconType: 'numeric',
+        value: (v) => v.asOf,
+        render: (v) => internalizeDate(v.asOf)
+    },
+    {
+        id: 'phone-email-type',
+        name: 'Type',
+        className: styles['coded-header'],
+        sortable: true,
+        sortIconType: 'alpha',
+        value: (v) => v.type?.name
+    },
+    {
+        id: 'phone-email-number',
+        name: 'Phone number',
+        className: styles['text-header'],
+        sortable: true,
+        sortIconType: 'numeric',
+        value: (v) => v.phoneNumber
+    },
+    {
+        id: 'phone-email-email-address',
+        name: 'Email address',
+        className: styles['text-header'],
+        sortable: true,
+        sortIconType: 'alpha',
+        value: (v) => v.email
+    },
+    {
+        id: 'phone-email-comments',
+        name: 'Comments',
+        className: styles['text-header'],
+        sortable: true,
+        sortIconType: 'alpha',
+        value: (v) => v.type?.name
+    }
+];
+
+type PhoneEmailDemographicCardProps = {
+    title?: string;
+} & Omit<
+    RepeatingBlockProps<PhoneEmailDemographic>,
+    'columns' | 'formRenderer' | 'viewRenderer' | 'defaultValues' | 'title'
+>;
+
+const defaultValue = initial();
+
+const sortResolver = columnSortResolver(columns);
+
+const PhoneEmailDemographicCard = ({
+    title = 'Phone & email',
+    sizing,
+    data = [],
+    ...remaining
+}: PhoneEmailDemographicCardProps) => {
+    const renderView = (entry: PhoneEmailDemographic) => <PhoneEmailDemographicView entry={entry} />;
+    const renderForm = () => <PhoneEmailDemographicFields />;
+
+    <SortingProvider appendToUrl={false}>
+        {(sorting) => (
+            <SortHandler sorting={sorting} resolver={sortResolver} data={data}>
+                {({ sorting, sorted }) => (
+                    <RepeatingBlock<PhoneEmailDemographic>
+                        {...remaining}
+                        title={title}
+                        sizing={sizing}
+                        columns={columns}
+                        data={sorted}
+                        features={{ sorting }}
+                        defaultValues={defaultValue}
+                        editable={false}
+                        viewable={false}
+                        viewRenderer={renderView}
+                        formRenderer={renderForm}
+                        collapsible
+                    />
+                )}
+            </SortHandler>
+        )}
+    </SortingProvider>;
+};
+
+export { PhoneEmailDemographicCard };
---
+++ apps/modernization-ui/src/libs/patient/demographics/phoneEmail/PhoneEmailDemographicFields.tsx
@@ -0,0 +1,134 @@
+import { EntryFieldsProps } from 'design-system/entry';
+import { Controller, useFormContext } from 'react-hook-form';
+import { validDateRule, DatePickerInput } from 'design-system/date';
+import { validateExtendedNameRule, validateRequiredRule } from 'validation/entry';
+import { SingleSelect } from 'design-system/select';
+import { TextInputField } from 'design-system/input';
+import { PhoneEmailDemographic } from './phoneEmails';
+import { usePhoneEmailCodedValues } from './usePhoneEmailCodedValues';
+
+type PhoneEmailDemographicFieldsProps = {} & EntryFieldsProps;
+
+const AS_OF_DATE_LABEL = 'Phone & email as of';
+const TYPE_LABEL = 'Type';
+const USE_LABEL = 'Use';
+const COUNTRY_CODE_LABEL = 'Country label';
+const PHONE_NUMBER_LABEL = 'Phone number';
+const EXTENSION_LABEL = 'Extension';
+const EMAIL_LABEL = 'Email';
+const URL_LABEL = 'URL';
+const COMMENTS_LABEL = 'Phone & email comments';
+
+const PhoneEmailDemographicFields = ({
+    orientation = 'horizontal',
+    sizing = 'medium'
+}: PhoneEmailDemographicFieldsProps) => {
+    const { control } = useFormContext<PhoneEmailDemographic>();
+
+    const coded = usePhoneEmailCodedValues();
+
+    return (
+        <section>
+            <Controller
+                control={control}
+                name="asOf"
+                rules={{ ...validDateRule(AS_OF_DATE_LABEL), ...validateRequiredRule(AS_OF_DATE_LABEL) }}
+                render={({ field: { onBlur, onChange, value, name }, fieldState: { error } }) => (
+                    <DatePickerInput
+                        id={name}
+                        label={AS_OF_DATE_LABEL}
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        name={name}
+                        orientation={orientation}
+                        error={error?.message}
+                        required
+                        sizing={sizing}
+                    />
+                )}
+            />
+
+            <Controller
+                control={control}
+                name="type"
+                rules={{ ...validateRequiredRule(TYPE_LABEL) }}
+                render={({ field: { onBlur, onChange, value, name }, fieldState: { error } }) => (
+                    <SingleSelect
+                        label={TYPE_LABEL}
+                        orientation={orientation}
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        id={`name-${name}`}
+                        name={`name-${name}`}
+                        options={coded.types}
+                        error={error?.message}
+                        required
+                        sizing={sizing}
+                    />
+                )}
+            />
+            <Controller
+                control={control}
+                name="use"
+                rules={{ ...validateRequiredRule(TYPE_LABEL) }}
+                render={({ field: { onBlur, onChange, value, name }, fieldState: { error } }) => (
+                    <SingleSelect
+                        label={USE_LABEL}
+                        orientation={orientation}
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        id={`name-${name}`}
+                        name={`name-${name}`}
+                        options={coded.uses}
+                        error={error?.message}
+                        required
+                        sizing={sizing}
+                    />
+                )}
+            />
+            <Controller
+                control={control}
+                name="issuer"
+                render={({ field: { onBlur, onChange, value, name }, fieldState: { error } }) => (
+                    <SingleSelect
+                        label={ASSIGNING_AUTHORITY_LABEL}
+                        orientation={orientation}
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        id={`name-${name}`}
+                        name={`name-${name}`}
+                        options={coded.authorities}
+                        error={error?.message}
+                        required
+                        sizing={sizing}
+                    />
+                )}
+            />
+            <Controller
+                control={control}
+                name="value"
+                rules={{ ...validateExtendedNameRule(VALUE_LABEL) }}
+                render={({ field: { onBlur, onChange, value, name }, fieldState: { error } }) => (
+                    <TextInputField
+                        label={VALUE_LABEL}
+                        orientation={orientation}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        value={value}
+                        type="text"
+                        name={name}
+                        id={name}
+                        error={error?.message}
+                        sizing={sizing}
+                    />
+                )}
+            />
+        </section>
+    );
+};
+
+export { PhoneEmailDemographicFields };
