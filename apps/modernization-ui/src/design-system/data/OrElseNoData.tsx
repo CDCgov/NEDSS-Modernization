39b403396cc298ee2805f6e029f0fcc395275a4e
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2025-07-08 06:57:24 -0500

    [CNFT1-4314] OrElseNoData now uses exists (#2740)
    * OrElseNoData now uses exists
    
    * documentation

--- apps/modernization-ui/src/design-system/data/OrElseNoData.tsx
+++ apps/modernization-ui/src/design-system/data/OrElseNoData.tsx
@@ -1,10 +1,17 @@
 import { ReactNode } from 'react';
+import { exists } from 'utils/exists';
 import { NoData } from './NoData';
 
 type OrElseNoDataProps = {
     children: ReactNode;
 };
 
-const OrElseNoData = ({ children }: OrElseNoDataProps) => (children ? children : <NoData />);
+/**
+ * Renders the children component or the "no data" placeholder of "---" if it does not {@link exists}.
+ *
+ * @param {OrElseNoDataProps} props
+ * @return {ReactNode}
+ */
+const OrElseNoData = ({ children }: OrElseNoDataProps) => (exists(children) ? children : <NoData />);
 
 export { OrElseNoData };
