2c776746d66ede29a1691bfc3da767f906e3c080
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2025-07-08 17:00:53 -0500

    [CNFT1-4324] Handling of API response for Sex & Birth (#2744)

--- apps/modernization-ui/src/apps/patient/file/demographics/sex-birth/patientSexBirth.ts
+++ apps/modernization-ui/src/apps/patient/file/demographics/sex-birth/patientSexBirth.ts
@@ -1,11 +1,10 @@
 import { PatientFileService } from 'generated';
 import { asOfAgeResolver } from 'date';
 import { PatientFileSexBirthDemographic } from './PatientFileSexBirthDemographic';
-import { SexBirthDemographic } from 'libs/patient/demographics/sex-birth';
 
 const patientSexBirth = (patient: number): Promise<PatientFileSexBirthDemographic> =>
     PatientFileService.sexBirthDemographics({ patient }).then((response) => ({
-        demographic: response as SexBirthDemographic,
-        ageResolver: asOfAgeResolver(response.deceasedOn)
+        demographic: response,
+        ageResolver: asOfAgeResolver(response?.deceasedOn)
     }));
 export { patientSexBirth };
