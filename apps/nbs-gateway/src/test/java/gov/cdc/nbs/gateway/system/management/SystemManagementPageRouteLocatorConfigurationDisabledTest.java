5be70a7ebbc66ec2e573057cca9b173c24ca8336
Snehaa Shri Hari <125155334+snehaasjournal@users.noreply.github.com>
2025-07-09 07:14:04 -0700

    FeatureFlag: System Management Routing (#2743)

---
+++ apps/nbs-gateway/src/test/java/gov/cdc/nbs/gateway/system/management/SystemManagementPageRouteLocatorConfigurationDisabledTest.java
@@ -0,0 +1,41 @@
+package gov.cdc.nbs.gateway.system.management;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+        "nbs.gateway.system.management.enabled=false",
+        "nbs.gateway.classic=http://localhost:10000"
+})
+
+class SystemManagementPageRouteLocatorConfigurationDisabledTest {
+    @RegisterExtension
+    static WireMockExtension classic = WireMockExtension.newInstance()
+            .options(wireMockConfig().port(10000))
+            .build();
+
+    @Autowired
+    WebTestClient webClient;
+
+    @Test
+    void should_not_route_to_system_management_page_when_disabled() {
+        classic.stubFor(
+                get(urlEqualTo("/nbs/SystemAdmin.do")).willReturn(ok()));
+
+        webClient
+                .get().uri(
+                        builder -> builder
+                                .path("/nbs/SystemAdmin.do")
+                                .build())
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+}
