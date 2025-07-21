740ee4df066df25c117249ec1cc0956e520e6514
Adam Loup <adam.loup@cdwg.com>
2025-07-21 11:30:16 -0500

    TreatmentList

--- apps/modernization-ui/src/libs/events/reports/morbidity/TreatmentList.tsx
+++ apps/modernization-ui/src/libs/events/reports/morbidity/TreatmentList.tsx
@@ -1,13 +1,12 @@
 import { Shown } from 'conditional-render';
 import styles from './treatment-list.module.scss';
-import { exists } from 'utils/exists';
 
 type TreatmentListProps = {
     children?: string[];
 };
 
 const TreatmentList = ({ children }: TreatmentListProps) => (
-    <Shown when={exists(children)}>
+    <Shown when={Boolean(children?.length)}>
         <ul className={styles.treatments}>{children?.map((treatment, index) => <li key={index}>{treatment}</li>)}</ul>
     </Shown>
 );
