53a0857d97477db842475a8d66854e789dcf716d
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2025-07-21 14:30:17 -0500

    [DEVEX] Nbs/6.0.17.1 (#2755)
    * removes traefik
    
    * nbs6 builder
    
    * configurable wildfly
    
    * 6.0.17.1
    
    * database utilities script updates

--- cdc-sandbox/db/backup.sh
+++ cdc-sandbox/db/backup.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/env bash
+set -e
+
+BASE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 CONTAINER_NAME=nbs-mssql
 DATABASE_NAME=nbs_odse
-BACKUP_PATH=./.backups
+BACKUP_PATH=${BASE}/.backups
 
-while getopts ":c:d:p:" opt; do
+while getopts ":c:d:p:f:" opt; do
   case $opt in
     c)
       CONTAINER_NAME=$OPTARG
@@ -13,6 +17,9 @@ while getopts ":c:d:p:" opt; do
       ;;
     p)
       BACKUP_PATH=$OPTARG
+      ;;
+    f)
+      FILE_NAME=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -37,7 +44,11 @@ set -e
 set -o pipefail
 
 # Create a file name for the backup based on the current date and time
-FILE_NAME=$(date +%Y-%m-%d_.$DATABASE_NAME.bak)
+if [ -z $FILE_NAME ] 
+then 
+  FILE_NAME=$(date +%Y-%m-%d.$DATABASE_NAME.bak)
+fi
+
 FILE_LOCATION="$BACKUP_PATH/$FILE_NAME"
 
 # Make sure the backups folder exists on the host file system
@@ -46,7 +57,7 @@ mkdir -p "$BACKUP_PATH"
 echo "Backing up database [$DATABASE_NAME] from container $CONTAINER_NAME"
 
 # Create a database backup with sqlcmd
-docker exec -it "$CONTAINER_NAME" /opt/mssql-tools18/bin/sqlcmd -b -V16 -C -S localhost -U SA -Q "BACKUP DATABASE [$DATABASE_NAME] TO DISK = N'/var/opt/mssql/backups/$FILE_NAME' with NOFORMAT, NOINIT, NAME = '$DATABASE_NAME-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
+docker exec -it "$CONTAINER_NAME" /opt/mssql-tools18/bin/sqlcmd -b -V16 -C -S localhost -U SA -Q "BACKUP DATABASE [$DATABASE_NAME] TO DISK = N'/var/opt/mssql/backups/$FILE_NAME' with COMPRESSION, NOFORMAT, NOINIT, NAME = '$DATABASE_NAME-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
 
 echo ""
 echo "Exporting file from container"
