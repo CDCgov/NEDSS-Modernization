118e0182b9f7489f899fdc3c4ac26ad12449186f
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-07-28 15:10:56 -0400

    fixing test

--- apps/modernization-ui/src/classic/ClassicModalButton.spec.tsx
+++ apps/modernization-ui/src/classic/ClassicModalButton.spec.tsx
@@ -1,38 +1,27 @@
 import { ClassicModalButton } from './ClassicModalButton';
 import { render, waitFor } from '@testing-library/react';
 import { ClassicModalProvider } from './ClassicModalContext';
-
-import { useRedirect } from './useRedirect';
 import { Status, useClassicModal } from 'classic';
 import userEvent from '@testing-library/user-event';
-
-jest.mock('./useRedirect');
-
-const mockUseRedirect = useRedirect as jest.MockedFunction<typeof useRedirect>;
 
 jest.mock('classic');
 const mockUseClassicModal = useClassicModal as jest.MockedFunction<typeof useClassicModal>;
 
+const open = jest.fn();
+
 describe('A ClassicModalButton component', () => {
-    it('should redirect when clicked', async () => {
-        const redirect = jest.fn();
-
-        mockUseRedirect.mockImplementation(() => ({
-            redirecting: false,
-            location: 'location-value',
-            redirect,
-            reset: jest.fn()
-        }));
-
+    it('should open url when clicked', async () => {
         mockUseClassicModal.mockImplementation(() => ({
             state: { status: Status.Idle },
-            open: jest.fn(),
+            open: open,
             reset: jest.fn()
         }));
 
         const { findByText } = render(
             <ClassicModalProvider>
-                <ClassicModalButton url="redirect-url">Button text</ClassicModalButton>
+                <ClassicModalButton url="url" secondary sizing={'small'}>
+                    Button text
+                </ClassicModalButton>
             </ClassicModalProvider>
         );
 
@@ -41,31 +30,7 @@ describe('A ClassicModalButton component', () => {
         userEvent.click(button);
 
         await waitFor(() => {
-            expect(redirect).toHaveBeenCalledWith('redirect-url');
+            expect(open).toHaveBeenCalledWith('url');
         });
-    });
-
-    it('should open a modal when the location is present', () => {
-        mockUseRedirect.mockImplementation(() => ({
-            redirecting: false,
-            location: 'location-value',
-            redirect: jest.fn(),
-            reset: jest.fn()
-        }));
-
-        const open = jest.fn();
-        mockUseClassicModal.mockImplementation(() => ({
-            state: { status: Status.Idle },
-            open: open,
-            reset: jest.fn()
-        }));
-
-        render(
-            <ClassicModalProvider>
-                <ClassicModalButton url="redirect-url">Button text</ClassicModalButton>
-            </ClassicModalProvider>
-        );
-
-        expect(open).toHaveBeenCalledWith('location-value');
     });
 });
