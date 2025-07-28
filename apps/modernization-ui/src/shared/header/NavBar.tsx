c56da5552068c8a6eedc0cbe9c870e6139ac5bcd
hclarkEnq <hclark@enquizit.com>
2025-07-28 13:15:00 -0400

    updating button for logout back to anchor tag and using the href plus preventing default to redirect correctly

--- apps/modernization-ui/src/shared/header/NavBar.tsx
+++ apps/modernization-ui/src/shared/header/NavBar.tsx
@@ -91,9 +91,14 @@ export const NavBar = () => {
                                             <span> | </span>
                                         </td>
                                         <td className={styles.navLink}>
-                                            <button onClick={logout} tabIndex={0} className={styles.logoutButton}>
+                                            <a
+                                                href="#"
+                                                onClick={(e) => {
+                                                    e.preventDefault();
+                                                    logout();
+                                                }}>
                                                 Logout
-                                            </button>
+                                            </a>
                                         </td>
                                     </tr>
                                 </tbody>
