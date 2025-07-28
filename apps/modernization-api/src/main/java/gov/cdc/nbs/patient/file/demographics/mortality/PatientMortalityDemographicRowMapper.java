99940fd26bc3723186324daf58ae7afeb7ef6528
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-07-28 09:36:01 -0400

    both completed

--- apps/modernization-api/src/main/java/gov/cdc/nbs/patient/file/demographics/mortality/PatientMortalityDemographicRowMapper.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/patient/file/demographics/mortality/PatientMortalityDemographicRowMapper.java
@@ -58,6 +58,9 @@ class PatientMortalityDemographicRowMapper implements RowMapper<PatientMortality
     LocalDate asOf = LocalDateColumnMapper.map(resultSet, columns.asOf());
     Selectable deceased = deceasedMapper.mapRow(resultSet, rowNum);
     LocalDate deceasedOn = LocalDateColumnMapper.map(resultSet, columns.deceasedOn());
+    if (!deceased.value().equalsIgnoreCase("Y")) {
+      deceasedOn = null;
+    }
 
     String city = resultSet.getString(columns.city());
     Selectable state = stateMapper.mapRow(resultSet, rowNum);
