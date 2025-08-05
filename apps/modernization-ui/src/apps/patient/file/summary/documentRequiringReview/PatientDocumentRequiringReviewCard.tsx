81973833b9d6d9ac16098d7b95ac4a44df810b63
Alaap Bharadwaj <137817788+alaapbharadwaj@users.noreply.github.com>
2025-08-05 10:53:34 -0400

    DRR enhancements (#2848)
    * completed
    
    * small changes

--- apps/modernization-ui/src/apps/patient/file/summary/documentRequiringReview/PatientDocumentRequiringReviewCard.tsx
+++ apps/modernization-ui/src/apps/patient/file/summary/documentRequiringReview/PatientDocumentRequiringReviewCard.tsx
@@ -51,13 +51,19 @@ const resolveUrl = (value: PatientFileDocumentRequiringReview) => {
 };
 
 const renderEventDate = (value?: PatientFileDocumentRequiringReview) => {
-    if (value?.type === 'Morbidity Report') {
+    if (value?.type === 'Morbidity Report' || value?.type === 'Case Report') {
         return (
             <>
-                <LabeledValue label="Report Date" orientation="vertical">
+                <LabeledValue label="Report date" orientation="vertical">
                     {internalizeDate(value.eventDate)}
                 </LabeledValue>
             </>
+        );
+    } else if (value?.type === 'Laboratory Report') {
+        return (
+            <LabeledValue label="Date collected" orientation="vertical">
+                {internalizeDate(value.eventDate)}
+            </LabeledValue>
         );
     }
     return internalizeDate(value?.eventDate);
@@ -110,7 +116,7 @@ const columns: Column<PatientFileDocumentRequiringReview>[] = [
     },
     {
         ...EVENT_DATE,
-        className: styles['date-header'],
+        className: styles['date-time-header'],
         sortable: true,
         sortIconType: 'numeric',
         value: (value) => value.eventDate,
