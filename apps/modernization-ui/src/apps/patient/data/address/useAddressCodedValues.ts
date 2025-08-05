84d59638b8d6f7ccb57ee55b9864fa0b938b1bf1
Ben Lambert <benjamin.lambert@cdw.com>
2025-08-05 11:31:45 -0700

    Filter Birth Place and Death Place from only the Patient Data Address entry use dropdown

--- apps/modernization-ui/src/apps/patient/data/address/useAddressCodedValues.ts
+++ apps/modernization-ui/src/apps/patient/data/address/useAddressCodedValues.ts
@@ -1,5 +1,6 @@
 import { Selectable } from 'options';
 import { useConceptOptions } from 'options/concepts';
+import { useMemo } from 'react';
 
 type AddressCodedValues = {
     types: Selectable[];
@@ -10,9 +11,13 @@ const useAddressCodedValues = (): AddressCodedValues => {
     const types = useConceptOptions('EL_TYPE_PST_PAT', { lazy: false });
     const uses = useConceptOptions('EL_USE_PST_PAT', { lazy: false });
 
+    const filteredUses = useMemo(() => {
+        return uses.options.filter((option) => option.value !== 'BIR' && option.value !== 'DTH');
+    }, [uses.options]);
+
     return {
         types: types.options,
-        uses: uses.options
+        uses: filteredUses
     };
 };
 
