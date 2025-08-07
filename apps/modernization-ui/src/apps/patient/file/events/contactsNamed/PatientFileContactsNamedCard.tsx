200208ac858d21fa84f2e301460f6f91e0329d71
Alaap Bharadwaj <abharadwaj@enquizit.com>
2025-08-07 14:34:04 -0400

    small change

--- apps/modernization-ui/src/apps/patient/file/events/contactsNamed/PatientFileContactsNamedCard.tsx
+++ apps/modernization-ui/src/apps/patient/file/events/contactsNamed/PatientFileContactsNamedCard.tsx
@@ -1,15 +1,14 @@
-import { NoData } from 'components/NoData';
+import { displayNoData } from 'design-system/data';
 import { ContactsCard, ContactsCardProps } from 'libs/events/contacts/ContactsCard';
 import { PatientFileContacts } from 'libs/events/contacts/contactsNamed';
 import { DisplayableName, displayName } from 'name';
-import { mapOr } from 'utils/mapping';
 
 type PatientFileContactsNamedProps = Omit<ContactsCardProps, 'title' | 'titleResolver'>;
 
 const titleResolver = (patient?: DisplayableName, contact?: PatientFileContacts): string => {
-    const name = mapOr(displayName('short'), <NoData />);
+    const name = patient ? displayName('short')(patient) : displayNoData();
 
-    return `The following contacts were named by ${patient && name}'s investigation of ${contact?.condition}`;
+    return `The following contacts were named by ${name}'s investigation of ${contact?.condition}`;
 };
 
 const PatientFileContactsNamedCard = ({ ...remaining }: PatientFileContactsNamedProps) => {
--- apps/modernization-ui/src/apps/patient/file/events/patientsNamed/PatientFilePatientsNamedCard.tsx
+++ apps/modernization-ui/src/apps/patient/file/events/patientsNamed/PatientFilePatientsNamedCard.tsx
@@ -1,13 +1,12 @@
-import { NoData } from 'components/NoData';
+import { displayNoData } from 'design-system/data';
 import { ContactsCard, ContactsCardProps } from 'libs/events/contacts/ContactsCard';
 import { PatientFileContacts } from 'libs/events/contacts/contactsNamed';
 import { DisplayableName, displayName } from 'name';
-import { mapOr } from 'utils/mapping';
 
 type PatientFilePatientsNamedCardProps = Omit<ContactsCardProps, 'title' | 'titleResolver'>;
 
 const titleResolver = (patient?: DisplayableName, contact?: PatientFileContacts): string => {
-    const name = mapOr(displayName('short'), <NoData />);
+    const name = patient ? displayName('short')(patient) : displayNoData();
 
     return `${patient && name} was named as a contact in the following ${contact?.condition}`;
 };
