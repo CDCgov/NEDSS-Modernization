0627abf264e912abd1f5fb32d3d76de30a580db2
Adam Loup <adam.loup@cdwg.com>
2025-08-19 13:10:02 -0500

    default to

--- apps/modernization-ui/src/apps/search/patient/result/displayProfileLegalName.tsx
+++ apps/modernization-ui/src/apps/search/patient/result/displayProfileLegalName.tsx
@@ -1,13 +1,12 @@
 import { PatientSearchResult } from 'generated/graphql/schema';
 import { displayName } from 'name';
-import { maybeMap } from 'utils/mapping';
+import { mapOr } from 'utils/mapping';
 import { PatientFileLink } from './PatientFileLink';
-import { defaultTo } from 'libs/supplying';
 
-const maybeDisplayName = maybeMap(displayName('fullLastFirst'));
+const maybeDisplayName = mapOr(displayName('fullLastFirst'), 'No Data');
 
 const displayProfileLegalName = (result: PatientSearchResult) => {
-    const display = defaultTo('No Data', maybeDisplayName(result.legalName));
+    const display = maybeDisplayName(result.legalName);
 
     return (
         <PatientFileLink identifier={result.patient} patientId={result.shortId}>
--- apps/modernization-ui/src/libs/supplying/defaultTo.ts
+++ apps/modernization-ui/src/libs/supplying/defaultTo.ts
@@ -1,10 +1,12 @@
 import { Supplier } from './supplier';
 
-const defaultTo = <T>(fallback?: T | Supplier<T>, value?: T) => {
-    if (value) {
-        return value;
-    }
-    return fallback instanceof Function ? fallback() : fallback;
-};
+const defaultTo =
+    <T>(fallback: T | Supplier<T>) =>
+    (value?: T) => {
+        if (value) {
+            return value;
+        }
+        return fallback instanceof Function ? fallback() : fallback;
+    };
 
 export { defaultTo };
