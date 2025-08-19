0196163728f644f35d648c501552f848f8a7016d
Snehaa Shri Hari <125155334+snehaasjournal@users.noreply.github.com>
2025-08-19 10:19:09 -0500

    Move merge status hook (#2912)

--- apps/modernization-ui/src/apps/patient/file/summary/PatientFileSummary.tsx
+++ apps/modernization-ui/src/apps/patient/file/summary/PatientFileSummary.tsx
@@ -6,25 +6,15 @@ import { PatientFileOpenInvestigationsCard } from './openInvestigations';
 import { PatientDocumentRequiringReviewCard } from './documentRequiringReview';
 import { PatientMergeHistoryCard } from './mergeHistory/PatientMergeHistoryCard';
 import { FeatureToggle } from 'feature';
-import { usePatientMergeQueueStatus } from './mergeHistory/api/usePatientMergeQueueStatus';
 import { MergeAlert } from './mergeHistory/MergeAlert';
 
 const PatientFileSummary = () => {
     const { summary, demographics, patient } = usePatientFileData();
     const sizing = useComponentSizing();
-    const {
-        inMergeQueue,
-        mergeGroup,
-        loading: mergeQueueLoading
-    } = usePatientMergeQueueStatus(patient?.id?.toString());
 
     return (
         <PatientFileView patient={patient} sizing={sizing}>
-            <FeatureToggle guard={(features) => features.patient.file.mergeHistory?.enabled}>
-                {inMergeQueue && !mergeQueueLoading && (
-                    <MergeAlert mergeGroup={mergeGroup} patientId={patient?.patientId?.toString()} />
-                )}
-            </FeatureToggle>
+            {!!patient?.patientId && <MergeAlert patientId={patient.patientId} />}
             <PatientFileDemographicsSummaryCard
                 id="demographics-summary"
                 provider={demographics.get().summary}
