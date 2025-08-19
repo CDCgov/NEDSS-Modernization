cdff9a47b8ab63e9d1d6a7265ea31adff96674b9
Snehaa Shri Hari <79590798+snehaashrihari@users.noreply.github.com>
2025-08-19 07:22:34 -0700

    Move hook call to MerAlert

--- apps/modernization-ui/src/apps/patient/file/summary/mergeHistory/MergeAlert.tsx
+++ apps/modernization-ui/src/apps/patient/file/summary/mergeHistory/MergeAlert.tsx
@@ -1,30 +1,36 @@
 import { AlertMessage } from 'design-system/message';
 import { useNavigate } from 'react-router';
 import { permissions, Permitted } from 'libs/permission';
+import { usePatientMergeQueueStatus } from './api/usePatientMergeQueueStatus';
+import { FeatureToggle } from 'feature';
 
 type MergeAlertProps = {
-    mergeGroup: number | null;
     patientId: string;
 };
 
-export const MergeAlert = ({ mergeGroup, patientId }: MergeAlertProps) => {
+export const MergeAlert = ({ patientId }: MergeAlertProps) => {
     const nav = useNavigate();
+    const { inMergeQueue, mergeGroup, loading: mergeQueueLoading } = usePatientMergeQueueStatus(patientId);
 
     return (
-        <Permitted permission={permissions.patient.merge}>
-            <AlertMessage type="warning" slim>
-                We found potential duplicates for this patient in system-identified matches.&nbsp;&nbsp;&nbsp;
-                <a
-                    href={`/deduplication/merge/${mergeGroup}`}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        nav(`/deduplication/merge/${mergeGroup}`, {
-                            state: { fromPatientFileSummary: true, patientId }
-                        });
-                    }}>
-                    Review Matches
-                </a>
-            </AlertMessage>
-        </Permitted>
+        <FeatureToggle guard={(features) => features.patient.file.mergeHistory?.enabled}>
+            {inMergeQueue && !mergeQueueLoading && (
+                <Permitted permission={permissions.patient.merge}>
+                    <AlertMessage type="warning" slim>
+                        We found potential duplicates for this patient in system-identified matches.&nbsp;&nbsp;&nbsp;
+                        <a
+                            href={`/deduplication/merge/${mergeGroup}`}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                nav(`/deduplication/merge/${mergeGroup}`, {
+                                    state: { fromPatientFileSummary: true, patientId }
+                                });
+                            }}>
+                            Review Matches
+                        </a>
+                    </AlertMessage>
+                </Permitted>
+            )}
+        </FeatureToggle>
     );
 };
