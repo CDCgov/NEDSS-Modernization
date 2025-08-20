cf3db59c8aaeca66be49c0c47e1f0b00e7a76157
Adam Loup <adam.loup@cdwg.com>
2025-08-20 12:02:20 -0500

    prevent error when evaluating administrative

---
+++ apps/modernization-ui/src/apps/patient/file/edit/evaluated.spec.ts
@@ -0,0 +1,73 @@
+import {
+    evaluateAdministrative,
+    evaluateEthnicity,
+    evaluateGeneralInformation,
+    evaluateMortality,
+    evaluateSexBirth
+} from './evaluated';
+
+jest.mock('design-system/date/clock', () => ({
+    now: () => '2013-09-17T00:00:00'
+}));
+
+describe('when evaluating PatientDemographicEntry', () => {
+    it('should initialize administrative when not present', () => {
+        const actual = evaluateAdministrative(undefined);
+
+        expect(actual).toEqual(expect.objectContaining({ administrative: { asOf: '09/17/2013' } }));
+    });
+
+    it('should use values from administrative', () => {
+        const actual = evaluateAdministrative({ asOf: '11/19/2023' });
+
+        expect(actual).toEqual(expect.objectContaining({ administrative: { asOf: '11/19/2023' } }));
+    });
+
+    it('should initialize ethnicity when not present', () => {
+        const actual = evaluateEthnicity(undefined);
+
+        expect(actual).toEqual(expect.objectContaining({ ethnicity: expect.objectContaining({ asOf: '09/17/2013' }) }));
+    });
+
+    it('should use values from ethnicity', () => {
+        const actual = evaluateEthnicity({ asOf: '11/19/2023' });
+
+        expect(actual).toEqual(expect.objectContaining({ ethnicity: expect.objectContaining({ asOf: '11/19/2023' }) }));
+    });
+
+    it('should initialize sex and birth when not present', () => {
+        const actual = evaluateSexBirth(undefined);
+
+        expect(actual).toEqual(expect.objectContaining({ sexBirth: expect.objectContaining({ asOf: '09/17/2013' }) }));
+    });
+
+    it('should use values from sex and birth', () => {
+        const actual = evaluateSexBirth({ asOf: '11/19/2023' });
+
+        expect(actual).toEqual(expect.objectContaining({ sexBirth: expect.objectContaining({ asOf: '11/19/2023' }) }));
+    });
+
+    it('should initialize mortality when not present', () => {
+        const actual = evaluateMortality(undefined);
+
+        expect(actual).toEqual(expect.objectContaining({ mortality: expect.objectContaining({ asOf: '09/17/2013' }) }));
+    });
+
+    it('should use values from mortality', () => {
+        const actual = evaluateMortality({ asOf: '11/19/2023' });
+
+        expect(actual).toEqual(expect.objectContaining({ mortality: expect.objectContaining({ asOf: '11/19/2023' }) }));
+    });
+
+    it('should initialize general when not present', () => {
+        const actual = evaluateGeneralInformation(undefined);
+
+        expect(actual).toEqual(expect.objectContaining({ general: expect.objectContaining({ asOf: '09/17/2013' }) }));
+    });
+
+    it('should use values from general', () => {
+        const actual = evaluateGeneralInformation({ asOf: '11/19/2023' });
+
+        expect(actual).toEqual(expect.objectContaining({ general: expect.objectContaining({ asOf: '11/19/2023' }) }));
+    });
+});
