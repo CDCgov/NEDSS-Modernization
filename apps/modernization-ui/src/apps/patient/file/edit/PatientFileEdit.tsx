6655f29840cba037ca59c768fb50d09cd8c4ca93
Adam Loup <adam.loup@cdwg.com>
2025-08-21 15:11:14 -0500

    remove log

--- apps/modernization-ui/src/apps/patient/file/edit/PatientFileEdit.tsx
+++ apps/modernization-ui/src/apps/patient/file/edit/PatientFileEdit.tsx
@@ -101,7 +101,6 @@ const ReadyPatientFileEdit = ({ patient, entry }: ReadyPatientFileEditProps) => 
 
     useEffect(() => {
         if (interaction.status === 'error') {
-            console.log('error', interaction.reason);
             handleError(interaction.reason);
         } else if (interaction.status === 'completed') {
             handleSuccess();
