01992b21215f258b63163b25cc421dc29f342afc
Adam Loup <124325935+adamloup-enquizit@users.noreply.github.com>
2025-08-25 16:10:19 -0500

    [CNFT1-4418] Patient file edit perpetual loader (#2936)
    * removes Birth Place and Death Place from address use list
    
    * versions
    
    * sonar

--- libs/options-api/src/main/java/gov/cdc/nbs/option/concept/ConceptOptionModuleConfiguration.java
+++ libs/options-api/src/main/java/gov/cdc/nbs/option/concept/ConceptOptionModuleConfiguration.java
@@ -2,14 +2,14 @@ package gov.cdc.nbs.option.concept;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
 
 @Configuration
 class ConceptOptionModuleConfiguration {
 
   @Bean
-  ConceptOptionFinder conceptFinder(final JdbcTemplate template) {
-    return new CodeValueGeneralConceptOptionFinder(template);
+  ConceptOptionFinder conceptFinder(final JdbcClient client) {
+    return new CodeValueGeneralConceptOptionFinder(client);
   }
 
 }
