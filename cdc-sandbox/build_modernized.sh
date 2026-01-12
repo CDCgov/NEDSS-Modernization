0966404802d87098a641583978b2a9c25a449bff
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-01-12 14:04:14 -0500

    build: use NEDSSDev for containerization (#2975)
    ## Description
    
    Now that containerization has been incorporated directly into the
    NEDSSDev repo, we want to remove it from mod repos to avoid maintaining
    duplicates and generally keep concerns separated. This PR changes the
    docker compose to instead look to a sibling instantiation of NEDSSDev
    (though this is configurable) and to use that repo's docker-compose to
    build containers as needed
    
    Notes:
    * I updated/simplified the env instantiation to have reasonable
    defaults. This does include hardcoding some key-like tokens - *are we
    okay with this?* (For context, figuring out that these needed to have
    these formats was a several hour head-against-wall endeavor to figure
    out why patient search was redirecting to the timeout page)
    * removed the `nbs` network as we don't need it anymore with the
    included docker-compose approach
    * We probably could have gone all in on just `docker compose up` and
    ditched the build scripts, but some of the ordering I wasn't sure about
    and it does some env checks that are nominally helpful (but I can be
    swayed the getting rid of them is worth it)
    
    
    ## Checklist before requesting a review
    - [ ] PR focuses on a single story
    - [ ] Code has been fully tested to meet acceptance criteria
    - [ ] PR is reasonably small and reviewable (Generally less than 10
    files and 500 changed lines)
    - [ ] All new functions/classes/components reasonably small
    - [ ] Functions/classes/components focused on one responsibility
    - [ ] Code easy to understand and modify (clarity over concise/clever)
    - [ ] PRs containing TypeScript follow the [Do's and
    Don'ts](https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html)
    - [ ] PR does not contain hardcoded values (Uses constants)
    - [ ] All code is covered by unit or feature tests

--- cdc-sandbox/build_modernized.sh
+++ cdc-sandbox/build_modernized.sh
@@ -4,15 +4,13 @@ BASE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 source $BASE/check_env.sh
 
-if [ -z "$DATABASE_PASSWORD" ] || [ -z "$NIFI_PASSWORD" ] || [ -z "$KEYCLOAK_ADMIN_PASSWORD" ] || [ -z "$TOKEN_SECRET" ] || [ -z "$PARAMETER_SECRET" ] 
+if [ -z "$DATABASE_PASSWORD" ] || [ -z "$TOKEN_SECRET" ] || [ -z "$PARAMETER_SECRET" ] 
 then
-    echo "DATABASE_PASSWORD, NIFI_PASSWORD, KEYCLOAK_ADMIN_PASSWORD, TOKEN_SECRET are required"
+    echo "DATABASE_PASSWORD,TOKEN_SECRET, PARAMETER_SECRET are required"
     exit 1
 else
     echo "Building modernized services with:"
     echo "DATABASE_PASSWORD: $DATABASE_PASSWORD"
-    echo "NIFI_PASSWORD: $NIFI_PASSWORD"
-    echo "KEYCLOAK_ADMIN_PASSWORD: $KEYCLOAK_ADMIN_PASSWORD"
     echo "PARAMETER_SECRET: $PARAMETER_SECRET"
     echo "TOKEN_SECRET: $TOKEN_SECRET"
 fi
@@ -22,11 +20,9 @@ fi
 echo "Starting elasticsearch reverse-proxy"
 docker compose -f $BASE/docker-compose.yml up elasticsearch --build -d
 
-MODERNIZED_PATH=$BASE/../
-
-# Start of modernization-api initializes the elasticserach indices 
+# Start of modernization-api initializes the elasticsearch indices 
 echo "Starting modernized application services"
-docker compose -f $MODERNIZED_PATH/docker-compose.yml  up --build -d
+docker compose -f $BASE/docker-compose.yml  up --build -d
 
 # NiFi handles synchronizing data between nbs-mssql and elasticsearch
 echo "Starting NiFi"
@@ -34,7 +30,7 @@ docker compose -f $BASE/docker-compose.yml up nifi --build -d
 
 # Process complete
 echo "**** Modernized application startup complete ****"
-echo "http://localhost:8080/nbs/login"
+echo "http://localhost:8000/nbs/login"
 echo ""
 echo "**** Available users ****"
 echo "*\tmsa"
