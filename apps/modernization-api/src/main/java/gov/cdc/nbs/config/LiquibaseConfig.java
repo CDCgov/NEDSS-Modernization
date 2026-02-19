eaeeb5a40b09baf91dfc8b8c6509b902a77e0998
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-02-19 12:09:05 -0500

    style: formatting

--- apps/modernization-api/src/main/java/gov/cdc/nbs/config/LiquibaseConfig.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/config/LiquibaseConfig.java
@@ -1,5 +1,6 @@
 package gov.cdc.nbs.config;
 
+import javax.sql.DataSource;
 import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,75 +10,75 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class LiquibaseConfig {
 
-    @Value("${spring.liquibase.driver-class-name}")
-    private String driverClassName;
+  @Value("${spring.liquibase.driver-class-name}")
+  private String driverClassName;
 
-    @Value("${spring.liquibase.user}")
-    private String dbUserName;
+  @Value("${spring.liquibase.user}")
+  private String dbUserName;
 
-    @Value("${spring.liquibase.password}")
-    private String dbUserPassword;
+  @Value("${spring.liquibase.password}")
+  private String dbUserPassword;
 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.liquibase.dataingest")
-    public LiquibaseProperties dataingestLiquibaseProperties() {
-        return new LiquibaseProperties();
+  @Bean
+  @ConfigurationProperties(prefix = "spring.liquibase.dataingest")
+  public LiquibaseProperties dataingestLiquibaseProperties() {
+    return new LiquibaseProperties();
+  }
+
+  @Bean
+  public DataSource dataingestDataSource(
+      @Qualifier("dataingestLiquibaseProperties") LiquibaseProperties props) {
+    return DataSourceBuilder.create()
+        .url(props.getUrl())
+        .username(dbUserName)
+        .password(dbUserPassword)
+        .driverClassName(driverClassName)
+        .build();
+  }
+
+  @Bean(name = "dataingestLiquibase")
+  public SpringLiquibase dataingestLiquibase(
+      @Qualifier("dataingestDataSource") DataSource dataSource,
+      @Qualifier("dataingestLiquibaseProperties") LiquibaseProperties props) {
+    SpringLiquibase liquibase = new SpringLiquibase();
+    liquibase.setDataSource(dataSource);
+    liquibase.setChangeLog(props.getChangeLog());
+    if (liquibase.getContexts() != null) {
+      liquibase.setContexts(String.join(",", liquibase.getContexts()));
     }
+    return liquibase;
+  }
 
-    @Bean
-    public DataSource dataingestDataSource(@Qualifier("dataingestLiquibaseProperties") LiquibaseProperties props) {
-        return DataSourceBuilder.create()
-                .url(props.getUrl())
-                .username(dbUserName)
-                .password(dbUserPassword)
-                .driverClassName(driverClassName)
-                .build();
-    }
+  @Bean
+  @ConfigurationProperties(prefix = "spring.liquibase.msgoute")
+  public LiquibaseProperties msgouteLiquibaseProperties() {
+    return new LiquibaseProperties();
+  }
 
-    @Bean(name = "dataingestLiquibase")
-    public SpringLiquibase dataingestLiquibase(
-            @Qualifier("dataingestDataSource") DataSource dataSource,
-            @Qualifier("dataingestLiquibaseProperties") LiquibaseProperties props) {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(dataSource);
-        liquibase.setChangeLog(props.getChangeLog());
-        if (liquibase.getContexts() != null) {
-            liquibase.setContexts(String.join(",", liquibase.getContexts()));
-        }
-        return liquibase;
-    }
+  @Bean
+  public DataSource msgouteDataSource(
+      @Qualifier("msgouteLiquibaseProperties") LiquibaseProperties props) {
+    return DataSourceBuilder.create()
+        .url(props.getUrl())
+        .username(dbUserName)
+        .password(dbUserPassword)
+        .driverClassName(driverClassName)
+        .build();
+  }
 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.liquibase.msgoute")
-    public LiquibaseProperties msgouteLiquibaseProperties() {
-        return new LiquibaseProperties();
+  @Bean(name = "msgouteLiquibase")
+  public SpringLiquibase msgouteLiquibase(
+      @Qualifier("msgouteDataSource") DataSource dataSource,
+      @Qualifier("msgouteLiquibaseProperties") LiquibaseProperties props) {
+    SpringLiquibase liquibase = new SpringLiquibase();
+    liquibase.setDataSource(dataSource);
+    liquibase.setChangeLog(props.getChangeLog());
+    if (liquibase.getContexts() != null) {
+      liquibase.setContexts(String.join(",", liquibase.getContexts()));
     }
-
-    @Bean
-    public DataSource msgouteDataSource(@Qualifier("msgouteLiquibaseProperties") LiquibaseProperties props) {
-        return DataSourceBuilder.create()
-                .url(props.getUrl())
-                .username(dbUserName)
-                .password(dbUserPassword)
-                .driverClassName(driverClassName)
-                .build();
-    }
-
-    @Bean(name = "msgouteLiquibase")
-    public SpringLiquibase msgouteLiquibase(
-            @Qualifier("msgouteDataSource") DataSource dataSource,
-            @Qualifier("msgouteLiquibaseProperties") LiquibaseProperties props) {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(dataSource);
-        liquibase.setChangeLog(props.getChangeLog());
-        if (liquibase.getContexts() != null) {
-            liquibase.setContexts(String.join(",", liquibase.getContexts()));
-        }
-        return liquibase;
-    }
+    return liquibase;
+  }
 }
