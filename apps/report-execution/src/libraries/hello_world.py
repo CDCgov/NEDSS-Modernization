db9399583f7c7838e47c7ad3466c67e0a1e4e909
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-03-03 12:23:59 -0500

    feat: add library metadata to the library itself

--- apps/report-execution/src/libraries/hello_world.py
+++ apps/report-execution/src/libraries/hello_world.py
@@ -1,7 +1,14 @@
 """This is a fake/stub library just to start to get the interface/pipes hooked up"""
 
 from ..db_transaction import Transaction
-from ..models import ReportResult, TimeRange
+from ..models import ReportResult, TimeRange, LibraryMetadata
+
+library_metadata = LibraryMetadata(
+    version = 1,
+    name = "hello_world",
+    description = """Basic tabular report. Executes the described SQL by the data 
+    source and filters and returns the table.""",
+)
 
 
 def execute(
