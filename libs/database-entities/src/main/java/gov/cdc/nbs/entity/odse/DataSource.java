c0a2b85b72b0d9eeccb43e0a1d5aa19b755b185a
elisa lee <emyl3@users.noreply.github.com>
2026-03-03 10:34:03 -0600

    Add DataSource

---
+++ libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/DataSource.java
@@ -0,0 +1,65 @@
+package gov.cdc.nbs.entity.odse;
+
+import gov.cdc.nbs.audit.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class DataSource {
+
+  @Id
+  @Column(name = "data_source_uid", nullable = false, updatable = false)
+  private Long id;
+
+  @Column(name = "column_max_len")
+  private Integer columnMaxLen;
+
+  //  TODO: add a converter?
+  @Column(name = "condition_security", length = 1)
+  private Character conditionSecurity;
+
+  //  TODO: add a converter?
+  @Column(name = "jurisdiction_security", length = 1)
+  private Character jurisdictionSecurity;
+
+  //  TODO: add a converter?
+  @Column(name = "reporting_facility_security", length = 1)
+  private Character reportingFacilitySecurity;
+
+  @Column(name = "data_source_name", length = 50)
+  private String dataSourceName;
+
+  @Column(name = "data_source_title", length = 50)
+  private String dataSourceTitle;
+
+  @Column(name = "data_source_type_code", length = 20)
+  private String dataSourceTypeCode;
+
+  @Column(name = "desc_txt", length = 300)
+  private String descTxt;
+
+  @Column(name = "effective_from_time")
+  private LocalDate effectiveFromTime;
+
+  @Column(name = "effective_to_time")
+  private LocalDate effectiveToTime;
+
+  @Column(name = "org_access_permis", length = 2000)
+  private String orgAccessPermission;
+
+  @Column(name = "prog_area_access_permis", length = 2000)
+  private String progAreaAccessPermission;
+
+  @Embedded private Status status;
+}
