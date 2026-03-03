ef07efbccfe5939edeab11c40df5c3b6c78835d9
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-03-03 18:58:35 -0500

    style: linting

--- apps/report-execution/src/errors.py
+++ apps/report-execution/src/errors.py
@@ -1,5 +1,8 @@
+import logging
+
+
 class BaseReportExecutionError(Exception):
-    """Base exception for report execution errors. Builds in the correct HTTP code for the API"""
+    """Base exception for report execution errors. Builds in the correct HTTP code."""
 
     def __init__(self, message: str, http_code: int):
         self.message = message
@@ -8,8 +11,18 @@ class BaseReportExecutionError(Exception):
 
 
 class MissingLibraryError(BaseReportExecutionError):
-    """The requested library is missing"""
+    """The requested library is missing."""
 
     def __init__(self, library_name: str, is_builtin: bool):
         message = f'Library `{library_name}` (is_builtin: {is_builtin}) not found'
         super().__init__(message, 422)
+
+
+class ToDoError(BaseReportExecutionError):
+    """An error for a feature that hasn't been implemented yet."""
+
+    def __init__(self, message, orig_exc=None):
+        todo_message = f'TODO: {message}'
+        if orig_exc is not None:
+            logging.error(orig_exc)
+        super().__init__(todo_message, 502)
--- apps/report-execution/src/execute_report.py
+++ apps/report-execution/src/execute_report.py
@@ -5,25 +5,25 @@ from . import errors, models
 
 
 async def execute_report(report_spec: models.ReportSpec):
-    """Execute a report spec by validating inputs, loading library, handling DB connection
-    and transaction,and validating/processing results
+    """Execute a report spec by validating inputs, loading library, handling DB
+    connection and transaction,and validating/processing results.
     """
     if not is_valid_spec(report_spec):
-        raise 'TODO: validation handling'
+        raise errors.ToDoError('validation handling')
 
     # get the library defined in the spec as a python module
     library = get_library(report_spec.library_name, report_spec.is_builtin)
     if not is_valid_library(library):
-        raise 'TODO: validation handling'
+        raise errors.ToDoError('validation handling')
 
-    # TODO: set up database connection as read only and start a transaction
+    # TODO: set up db connection as read only and start a transaction  # noqa: FIX002
     async with db_transaction() as trx:
         result = await library.execute(
             trx, report_spec.data_source_name, report_spec.time_range
         )
 
     if not is_valid_result(result):
-        raise 'TODO: validation handling'
+        raise errors.ToDoError('validation handling')
 
     return result
 
@@ -33,7 +33,7 @@ def is_valid_spec(report_spec: models.ReportSpec):
     return True
 
 
-# TODO: what is the type that should go here? Part of spike
+# TODO: what is the type that should go here? Part of spike  # noqa: FIX002
 def is_valid_library(library):
     """Check if the library is valid."""
     return True
@@ -50,12 +50,12 @@ def get_library(library_name: str, is_builtin: bool):
         if is_builtin:
             return import_module(f'src.libraries.{library_name}')
         else:
-            raise 'TODO: support custom libraries'
+            raise errors.ToDoError('support custom libraries')
     except ModuleNotFoundError:
-        raise errors.MissingLibraryError(library_name, is_builtin)
+        # Initial error not helpful for debugging
+        raise errors.MissingLibraryError(library_name, is_builtin) from None
 
 
-# TODO: make this actually async?
 @asynccontextmanager
 async def db_transaction():
     """Set up a read only database transaction."""
