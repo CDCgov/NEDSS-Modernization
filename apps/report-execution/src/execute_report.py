211df8061f79e7fb3871759826b6fdc565a48931
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-03-03 11:30:07 -0500

    fix: cleanup

--- apps/report-execution/src/execute_report.py
+++ apps/report-execution/src/execute_report.py
@@ -54,6 +54,7 @@ def get_library(library_name: str, is_builtin: bool):
         if is_builtin:
             return import_module(f"src.libraries.{library_name}")
         else:
-            raise errors.ToDoError("upport custom libraries")
-    except ModuleNotFoundError as exc:
-        raise errors.MissingLibraryError(library_name, is_builtin) from exc
+            raise errors.ToDoError("support custom libraries")
+    except ModuleNotFoundError:
+        # cause isn't relevant for debugging this excepption
+        raise errors.MissingLibraryError(library_name, is_builtin) from None
