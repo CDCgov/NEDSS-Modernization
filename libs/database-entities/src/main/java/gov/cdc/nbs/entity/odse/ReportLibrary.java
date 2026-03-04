1bfd1b6db507658f6dfeac86e7abbf0937650a63
elisa lee <emyl3@users.noreply.github.com>
2026-03-04 11:13:49 -0600

    Add ReportLibraryTest

--- libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/ReportLibrary.java
+++ libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/ReportLibrary.java
@@ -5,43 +5,45 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
 @Table(name = "Report_Library", catalog = "NBS_ODSE")
 public class ReportLibrary {
+  // TODO: Add @NonNull here?
   @Id
   @Column(name = "library_uid")
   private Long id;
 
-  @Column(name = "library_name", length = 50, nullable = false)
+  @NonNull @Column(name = "library_name", length = 50, nullable = false)
   private String libraryName;
 
-  @Column(name = "desc_txt", length = 300, nullable = false)
+  @NonNull @Column(name = "desc_txt", length = 300, nullable = false)
   private String descTxt;
 
-  @Column(name = "runner", length = 10, nullable = false)
+  @NonNull @Column(name = "runner", length = 10, nullable = false)
   private String runner;
 
-  @Column(name = "is_builtin_ind", length = 1, nullable = false)
+  @NonNull @Column(name = "is_builtin_ind", length = 1, nullable = false)
   private Character isBuiltinIndex;
 
-  @Column(name = "add_time", nullable = false)
+  @NonNull @Column(name = "add_time", nullable = false)
   private LocalDateTime addTime;
 
-  @Column(name = "add_user_id", nullable = false)
+  @NonNull @Column(name = "add_user_id", nullable = false)
   private Long addUserId;
 
-  @Column(name = "last_chg_time", nullable = false)
+  @NonNull @Column(name = "last_chg_time", nullable = false)
   private LocalDateTime lastChgTime;
 
-  @Column(name = "last_chg_user_id", nullable = false)
+  @NonNull @Column(name = "last_chg_user_id", nullable = false)
   private Long lastChgUserId;
 }
