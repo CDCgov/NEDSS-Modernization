f25fff1970a8aa12802cc26f79142c0a0f2925d8
brick-green <brick.green1@gmail.com>
2026-03-04 12:52:43 -0600

    feature: add Filter_code entity

---
+++ libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/FilterCode.java
@@ -0,0 +1,51 @@
+package gov.cdc.nbs.entity.odse;
+
+import gov.cdc.nbs.audit.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "Filter_code", catalog = "NBS_ODSE")
+public class FilterCode {
+  @Id
+  @Column(name = "filter_uid", nullable = false)
+  private Long id;
+
+  @Column(name = "code_table", length = 50)
+  private String codeTable;
+
+  @Column(name = "desc_txt", length = 300)
+  private String descTxt;
+
+  @Column(name = "effective_from_time")
+  private LocalDateTime effectiveFromTime;
+
+  @Column(name = "effective_to_time")
+  private LocalDateTime effectiveToTime;
+
+  @Column(name = "filter_code", length = 20)
+  private String filterCode;
+
+  @Column(name = "filter_code_set_nm", length = 256)
+  private String filterCodeSentNumber;
+
+  @Column(name = "filter_type", length = 20)
+  private String filterType;
+
+  @Column(name = "filter_name", length = 50)
+  private String filterName;
+
+  @Embedded
+  private Status status;
+}
