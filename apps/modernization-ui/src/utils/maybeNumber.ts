f59c5d8f4376d40792289bba627ec50a24a6f9e2
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-03-09 11:34:29 -0400

    fix: typo

--- apps/modernization-ui/src/utils/maybeNumber.ts
+++ apps/modernization-ui/src/utils/maybeNumber.ts
@@ -1,7 +1,7 @@
 /**
  ** Returns a number if the value is truthy, otherwise returns null.
  * @param {number | string | null | undefined} value The value to convert to a number.
- * @returnsssss {number | null} The converted number or null if the value is falsy.
+ * @returns {number | null} The converted number or null if the value is falsy.
  */
 const maybeNumber = (value: number | string | null | undefined): number | null => (value ? +value : null);
 
