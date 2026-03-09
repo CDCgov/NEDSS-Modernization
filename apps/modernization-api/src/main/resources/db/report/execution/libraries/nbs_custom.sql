98e1f372890afbabbb66c601324efa4f442ae911
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-03-09 11:31:54 -0400

    fix: add insert if no row to update

--- apps/modernization-api/src/main/resources/db/report/execution/libraries/nbs_custom.sql
+++ apps/modernization-api/src/main/resources/db/report/execution/libraries/nbs_custom.sql
@@ -2,11 +2,42 @@
 
 USE [NBS_ODSE]
 
-UPDATE [dbo].[Report_Library]
-SET
-    library_name = 'nbs_custom',
-    runner = 'python',
-    last_chg_time = CURRENT_TIMESTAMP,
-    last_chg_user_id = 99999999
-WHERE
-    UPPER(library_name) = 'NBSCUSTOM.SAS';
+DECLARE @pyLib VARCHAR(50) = 'nbs_custom'
+DECLARE @sasLib VARCHAR(50) = 'NBSCUSTOM.SAS'
+DECLARE @desc VARCHAR(300) = 'Basic tabular report. Executes the query described by the data source and filters and returns the table'
+
+IF EXISTS (SELECT * FROM [dbo].[Report_Library] WHERE UPPER(library_name) = @sasLib)
+BEGIN
+    UPDATE [dbo].[Report_Library]
+    SET
+        library_name = @pyLib,
+        runner = 'python',
+        desc_txt = @desc,
+        last_chg_time = CURRENT_TIMESTAMP,
+        last_chg_user_id = 99999999
+    WHERE
+        UPPER(library_name) = @sasLib;
+END
+ELSE
+BEGIN
+    -- Create a row for this library
+    INSERT INTO [dbo].[Report_Library] (
+        library_name,
+        desc_txt,
+        runner,
+        is_builtin_ind,
+        add_time,
+        add_user_id,
+        last_chg_time,
+        last_chg_user_id
+    ) VALUES (
+        @pyLib,
+        @desc,
+        'python',
+        'Y',
+        CURRENT_TIMESTAMP,
+        99999999,
+        CURRENT_TIMESTAMP,
+        99999999
+    );
+END
