8fe408878e70ebd9b65c2ea9ed9da4bc136c1177
elisa lee <emyl3@users.noreply.github.com>
2026-03-11 11:46:23 -0500

    feat: return report id and runner

--- apps/modernization-api/src/main/java/gov/cdc/nbs/model/ReportModel.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/model/ReportModel.java
@@ -1,22 +1,8 @@
 package gov.cdc.nbs.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.HashMap;
 
 public record ReportModel(
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) ReportIdResponse id,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String runner) {
-
-  private static class ReportIdResponse {
-    Long reportId;
-    Long dataSourceId;
-
-    public ReportIdResponse(Long reportId, Long dataSourceId) {
-      this.reportId = reportId;
-      this.dataSourceId = dataSourceId;
-    }
-  }
-
-  public ReportModel(Long reportId, Long dataSourceId, String runner) {
-    this(new ReportIdResponse(reportId, dataSourceId), runner);
-  }
-}
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) HashMap<String, Long> id,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String runner) {}
--- apps/modernization-api/src/main/java/gov/cdc/nbs/service/ReportService.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/service/ReportService.java
@@ -5,6 +5,7 @@ import gov.cdc.nbs.entity.odse.ReportId;
 import gov.cdc.nbs.exception.NotFoundException;
 import gov.cdc.nbs.model.ReportModel;
 import gov.cdc.nbs.repository.ReportRepository;
+import java.util.HashMap;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 
@@ -22,8 +23,10 @@ public class ReportService {
 
     Optional<Report> optionalReport = reportRepository.findById(id);
     Report fetchedReport;
+    ReportId fetchedReportId;
     if (optionalReport.isPresent()) {
       fetchedReport = optionalReport.get();
+      fetchedReportId = fetchedReport.getId();
     } else {
       throw new NotFoundException(
           String.format(
@@ -32,8 +35,14 @@ public class ReportService {
     }
 
     return new ReportModel(
-        fetchedReport.getId().getReportUid(),
-        fetchedReport.getId().getDataSourceUid(),
+        createReportId(fetchedReportId.getReportUid(), fetchedReportId.getDataSourceUid()),
         fetchedReport.getReportLibrary().getRunner());
+  }
+
+  private HashMap<String, Long> createReportId(Long reportId, Long dataSourceId) {
+    HashMap<String, Long> idMap = new HashMap<>();
+    idMap.put("reportId", reportId);
+    idMap.put("dataSourceId", dataSourceId);
+    return idMap;
   }
 }
