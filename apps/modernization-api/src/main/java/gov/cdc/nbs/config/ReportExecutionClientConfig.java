7a954578ca8b1cc39d1b15c5d99f372d1f887f73
elisa lee <emyl3@users.noreply.github.com>
2026-03-12 17:42:55 -0500

    fix: use report execution url var

--- apps/modernization-api/src/main/java/gov/cdc/nbs/config/ReportExecutionClientConfig.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/config/ReportExecutionClientConfig.java
@@ -1,6 +1,7 @@
 package gov.cdc.nbs.config;
 
 import java.net.http.HttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
@@ -10,13 +11,11 @@ import org.springframework.web.client.RestClient;
 public class ReportExecutionClientConfig {
 
   @Bean
-  public RestClient reportExecutionClient(RestClient.Builder builder) {
+  public RestClient reportExecutionClient(
+      @Value("${nbs.report.execution.url}") final String url, RestClient.Builder builder) {
     HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
     JdkClientHttpRequestFactory jdkClientHttpRequestFactory =
         new JdkClientHttpRequestFactory(httpClient);
-    return builder
-        .baseUrl("http://report-execution:8001")
-        .requestFactory(jdkClientHttpRequestFactory)
-        .build();
+    return builder.baseUrl(url).requestFactory(jdkClientHttpRequestFactory).build();
   }
 }
