3ac4fefb3ce1ba819b44d5816bc88786a7a73c0b
elisa lee <emyl3@users.noreply.github.com>
2026-03-18 13:01:50 -0500

    run codegen

---
+++ apps/modernization-ui/src/generated/models/ReportExecutionRequest.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { AdvancedFilter } from './AdvancedFilter';
+import type { BasicFilter } from './BasicFilter';
+export type ReportExecutionRequest = {
+    reportUid: number;
+    dataSourceUid: number;
+    isExport: boolean;
+    columns: Array<string>;
+    filters?: Array<(AdvancedFilter | BasicFilter)>;
+};
+
