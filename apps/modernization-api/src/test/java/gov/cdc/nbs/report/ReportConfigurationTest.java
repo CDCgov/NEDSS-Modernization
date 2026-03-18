98abeecdcd302c3aa3b91ec9d54623f324212ebe
elisa lee <emyl3@users.noreply.github.com>
2026-03-18 10:44:03 -0500

    add reportConfigurationTest

---
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/report/ReportConfigurationTest.java
@@ -0,0 +1,21 @@
+package gov.cdc.nbs.report;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ReportConfigurationTest {
+  @Test
+  void should_create_report_configuration_with_idMap() {
+    Long reportUid = 1L;
+    Long dataSourceUid = 2L;
+    String runner = "python";
+
+    ReportConfiguration reportConfig = new ReportConfiguration(reportUid, dataSourceUid, runner);
+
+    assertThat(reportConfig.runner()).isEqualTo("python");
+    assertThat(reportConfig.id()).containsEntry("reportUid", 1L);
+    assertThat(reportConfig.id()).containsEntry("dataSourceUid", 2L);
+    assertThat(reportConfig.id()).hasSize(2);
+  }
+}
