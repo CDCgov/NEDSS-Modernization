04fe055bd202ba90697cac8c05fd5eed7f7b3108
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-03-24 15:56:26 -0400

    fix: get things working

--- apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/report/ReportExecutionRouteLocatorConfiguration.java
+++ apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/report/ReportExecutionRouteLocatorConfiguration.java
@@ -1,6 +1,7 @@
 package gov.cdc.nbs.gateway.report;
 
-import gov.cdc.nbs.gateway.ui.UIService;
+import com.fasterxml.jackson.databind.JsonNode;
+import gov.cdc.nbs.gateway.modernization.ModernizationService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.function.Predicate;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.handler.AsyncPredicate;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
@@ -17,14 +19,16 @@ import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 /**
- * Configures the NBS Report Runner to route run and execute to the report-execution service. The
+ * Configures the NBS Report Runner to route run and execute to the modernized UI service. The
  * routes are only enabled when the {@code routes.report.execute.enabled} property is {@code true}
  * and any of the following criteria is satisfied.
  *
@@ -32,7 +36,8 @@ import reactor.core.publisher.Mono;
  *   <li>Request is a POST
  *   <li>Path equal to {@code /nbs/nfc}
  *   <li>Request body has ObjectType="7"
- *   <li>Request body has OperationType="138" or "124"
+ *   <li>Request body has OperationType="117"
+ *   <li>The referenced report has been modernized
  * </ul>
  */
 @Configuration
@@ -49,11 +54,11 @@ class ReportExecutionRouteLocatorConfiguration {
   RouteLocator reportExecuteRouteLocator(
       final RouteLocatorBuilder builder,
       @Qualifier("defaults") final List<GatewayFilter> defaults,
-      final UIService uiService) {
+      final ModernizationService modService) {
     return builder
         .routes()
         .route(
-            "report-run-export-submit",
+            "report-run",
             route ->
                 route
                     .order(Ordered.HIGHEST_PRECEDENCE)
@@ -62,20 +67,93 @@ class ReportExecutionRouteLocatorConfiguration {
                     .path("/nbs/nfc")
                     .and()
                     .readBody(LinkedMultiValueMap.class, bodyPredicate())
-                    .filters(
-                        filter ->
-                            filter
-                                .filter(this::redirectToMod)
-                                .redirect(
-                                    302, uiService.path("/report/{reportUid}/{dataSourceUid}/run"))
-                                .filters(defaults))
+                    .and()
+                    .asyncPredicate(isModPredicate(modService))
+                    .filters(filter -> filter.filter(this::redirectToMod))
                     .uri("no://op"))
         .build();
   }
 
-  @SuppressWarnings({"unchecked"})
+  /** Check the /nbs/nfc post matches the object and operation for running a report */
+  @SuppressWarnings({"unchecked", "rawtypes"})
+  private Predicate<LinkedMultiValueMap> bodyPredicate() {
+    return body ->
+        REPORT_OBJECT_TYPE.equals(body.getFirst("ObjectType"))
+            && REPORT_OPERATION_TYPE.equals(body.getFirst("OperationType"));
+  }
+
+  /** Query the mod API to determine if this report should be run via NBS 7 or NBS 6 */
+  private AsyncPredicate<ServerWebExchange> isModPredicate(final ModernizationService modService) {
+    return exchange -> {
+      String path =
+          UriComponentsBuilder.fromPath("/nbs/api/report/configuration/{reportUid}/{dataSourceUid}")
+              .uri(modService.uri())
+              .build()
+              .expand(getParamsFromBody(exchange))
+              .toUriString();
+
+      return WebClient.create()
+          .get()
+          .uri(path)
+          .accept(MediaType.APPLICATION_JSON)
+          .cookies(
+              // Copy the cookies from the original request onto the new request to make sure auth
+              // works
+              newCookies -> {
+                exchange
+                    .getRequest()
+                    .getCookies()
+                    .forEach(
+                        (name, cookies) ->
+                            cookies.forEach(cookie -> newCookies.add(name, cookie.getValue())));
+              })
+          .retrieve()
+          .bodyToMono(JsonNode.class)
+          .flatMap(
+              b -> {
+                JsonNode runnerNode = b.get("runner");
+                if (runnerNode == null) return Mono.just(false);
+                String runner = runnerNode.textValue();
+                return Mono.just("python".equals(runner));
+              });
+    };
+  }
+
+  /**
+   * Redirect this request to the NBS 7 report UI
+   *
+   * <p>This is modeled after the `.redirect` method implementation, but because of the reading the
+   * params from the body and interpolating into the location, it couldn't be used directly
+   */
   private Mono<Void> redirectToMod(
       final ServerWebExchange exchange, final GatewayFilterChain chain) {
+    ServerWebExchangeUtils.setResponseStatus(exchange, HttpStatus.FOUND);
+
+    HashMap<String, String> params = getParamsFromBody(exchange);
+    String location =
+        // Make sure the location matches the service's original request (the gateway) so the
+        // cookies will be included on the new request to the location indicated hre
+        UriComponentsBuilder.fromUri(exchange.getRequest().getURI())
+            .replacePath("/report/{reportUid}/{dataSourceUid}/run")
+            .build()
+            .expand(params)
+            .toUriString();
+
+    ServerHttpResponse response = exchange.getResponse();
+    response.getHeaders().set(HttpHeaders.LOCATION, location);
+    return response.setComplete();
+  }
+
+  /**
+   * Get the report UID and DataSource UID from the requests body and put in a hashmap.
+   *
+   * <p>MUST BE RUN AFTER `readBody` PREDICATE so the parsed body is cached
+   */
+  @SuppressWarnings({"unchecked"})
+  private HashMap<String, String> getParamsFromBody(ServerWebExchange exchange) {
+    // UPGRADE TRIPPING HAZARD: This key is a private part of the `ReadBodyRoutePredicateFactory`
+    // implementation. It saves us needing to re-parse the cached body bytes into the map (and
+    // deal with Mono's to do so...)
     LinkedMultiValueMap<String, String> body =
         (LinkedMultiValueMap<String, String>)
             exchange.getRequest().getAttributes().get("cachedRequestBodyObject");
@@ -83,24 +161,6 @@ class ReportExecutionRouteLocatorConfiguration {
     HashMap<String, String> params = new HashMap<>();
     params.put("reportUid", body.getFirst("ReportUID"));
     params.put("dataSourceUid", body.getFirst("DataSourceUID"));
-
-    ServerWebExchangeUtils.setResponseStatus(exchange, HttpStatus.FOUND);
-
-    String location =
-        UriComponentsBuilder.fromPath("/report/{reportUid}/{dataSourceUid}/run")
-            .build()
-            .expand(params)
-            .getPath();
-
-    ServerHttpResponse response = exchange.getResponse();
-    response.getHeaders().set(HttpHeaders.LOCATION, location);
-    return response.setComplete();
-  }
-
-  @SuppressWarnings({"unchecked", "rawtypes"})
-  private Predicate<LinkedMultiValueMap> bodyPredicate() {
-    return r ->
-        REPORT_OBJECT_TYPE.equals(r.getFirst("ObjectType"))
-            && REPORT_OPERATION_TYPE.equals(r.getFirst("OperationType"));
+    return params;
   }
 }
