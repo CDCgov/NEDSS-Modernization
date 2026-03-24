65da1d9a46aab886dfac6c342ddb748327b1bf91
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-03-24 09:56:48 -0700

    Update ReportConfiguration.java

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/ReportConfiguration.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/ReportConfiguration.java
@@ -1,7 +1,6 @@
 package gov.cdc.nbs.report.models;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.Objects;
 
 public record ReportConfiguration(
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String runner) {
