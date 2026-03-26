8f0e2453b9cc39e292925e4d062f519eafd61021
elisa lee <emyl3@users.noreply.github.com>
2026-03-26 13:00:57 -0500

    fix: add setter

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/utils/DataSourceNames.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/utils/DataSourceNames.java
@@ -2,11 +2,13 @@ package gov.cdc.nbs.report.utils;
 
 import java.util.Map;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
+@Setter
 @ConfigurationProperties(prefix = "nbs.report.datasource")
 public class DataSourceNames {
 
