38a0161d20c1e4a71b711ea3787f91ef27b218ed
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-03-30 10:19:48 -0400

    feat: wip - open report results in new tab

--- apps/modernization-ui/src/apps/report/run/ReportRun.tsx
+++ apps/modernization-ui/src/apps/report/run/ReportRun.tsx
@@ -2,32 +2,43 @@ import { Button } from 'design-system/button';
 import { ReportConfiguration, ReportControllerService } from 'generated';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { useNewTab } from './useNewTab';
 
 const ReportRun = () => {
     const params = useParams();
     const reportUid = parseInt(params.reportUid ?? '0');
     const dataSourceUid = parseInt(params.dataSourceUid ?? '0');
     const [config, setConfig] = useState<ReportConfiguration | null>(null);
-    const [result, setResult] = useState<string>('');
+    const [hasResult, setHasResult] = useState<boolean>(false);
+    const { openNewTab } = useNewTab();
 
     useEffect(() => {
         ReportControllerService.getReportConfiguration({ reportUid, dataSourceUid }).then((value) => setConfig(value));
     }, []);
 
     const handleSubmit = (isExport: boolean) => {
-        ReportControllerService.executeReport({ requestBody: { isExport, reportUid, dataSourceUid } }).then((res) =>
-            setResult(res)
-        );
+        ReportControllerService.executeReport({ requestBody: { isExport, reportUid, dataSourceUid } })
+            .then((res) => {
+                setHasResult(true);
+                openNewTab(<div>{JSON.stringify(res)}</div>);
+            })
+            // TODO: in-component handling
+            // eslint-disable-next-line no-console
+            .catch((err) => console.error(err));
     };
 
     return (
         <section>
             <Button onClick={() => handleSubmit(false)}>Run</Button>
             <Button onClick={() => handleSubmit(true)}>Export</Button>
-            <p>Config:</p>
-            <p>{config ? JSON.stringify(config) : 'loading'}</p>
-            <p>Result:</p>
-            <p>{result ? JSON.stringify(result) : 'no result yet'}</p>
+            {hasResult ? (
+                <div>Result opened in new tab</div>
+            ) : (
+                <div>
+                    <p>Config:</p>
+                    <p>{config ? JSON.stringify(config) : 'loading'}</p>
+                </div>
+            )}
         </section>
     );
 };
---
+++ apps/modernization-ui/src/apps/report/run/useNewTab.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+/** 
+ * Open the provided content in a new tab.
+ * 
+ * The styles are copied over and the title is set.
+*/
+export const useNewTab = () => {
+    const openNewTab = (content: ReactNode) => {
+        let newWindow = window.open('', '_blank', '');
+
+        const div = document.createElement('div');
+        const root = createRoot(div);
+        root.render(content);
+        if (newWindow) {
+            newWindow.document.body.appendChild(div);
+            newWindow.document.title = 'NBS Report';
+            copyStyles(document, newWindow.document);
+        }
+    };
+
+    return {
+        openNewTab,
+    };
+};
+
+const copyStyles = (sourceDoc: Document, targetDoc: Document) => {
+    for (const stylesheet of sourceDoc.styleSheets) {
+        // For <link> elements (external stylesheets)
+        if (stylesheet.href) {
+            const el = targetDoc.createElement('link');
+            el.rel = 'stylesheet';
+            el.href = stylesheet.href;
+            targetDoc.head.appendChild(el);
+        }
+
+        // For <style> elements (inline or generated styles)
+        else if (stylesheet.ownerNode && stylesheet.ownerNode.textContent) {
+            const el = targetDoc.createElement('style');
+            el.textContent = stylesheet.ownerNode.textContent;
+            targetDoc.head.appendChild(el);
+        }
+    }
+};
