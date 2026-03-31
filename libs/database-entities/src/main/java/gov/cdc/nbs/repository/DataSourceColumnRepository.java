c3901e517f04ed2ece9b0deebba6198d97798620
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-03-31 15:20:08 -0700

    Delete DataSourceColumnRepository.java

--- libs/database-entities/src/main/java/gov/cdc/nbs/repository/DataSourceColumnRepository.java
+++
@@ -1,6 +0,0 @@
-package gov.cdc.nbs.repository;
-
-import gov.cdc.nbs.entity.odse.DataSourceColumn;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataSourceColumnRepository extends JpaRepository<DataSourceColumn, Long> {}
