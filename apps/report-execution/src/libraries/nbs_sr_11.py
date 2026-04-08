4ebe254b98cc3c84faf5b7379a8f1c4266b02e99
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-04-08 01:46:26 -0700

    Update nbs_sr_11.py

--- apps/report-execution/src/libraries/nbs_sr_11.py
+++ apps/report-execution/src/libraries/nbs_sr_11.py
@@ -18,8 +18,7 @@ def execute(
     content = trx.query(
         f"""
         WITH subset as ({subset_query})
-        SELECT COALESCE(state_cd, "N/A") as "State Code", 
-        COALESCE(state, "N/A") as State, COALESCE(county, "N/A") as County,
+        SELECT state_cd as "State Code", state as State, county as County,
         phc_code_short_desc as Condition, datepart(year, event_date) as Year,
         sum(group_case_cnt) as Cases
         FROM subset
