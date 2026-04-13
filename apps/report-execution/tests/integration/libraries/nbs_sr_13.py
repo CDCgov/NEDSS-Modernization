3c6269a81f1e5a7ebf83a163fb235d4b70997b01
Brick Green <86254221+brick-green@users.noreply.github.com>
2026-04-13 15:23:19 +0000

    Convert SAS library NBSSR00017.SAS (#3130)
    ## Description
    
    Please include a summary of the changes and any key information a
    reviewer may need.
    
    ## Tickets
    
    * [Jira
    Ticket](https://cdc-nbs.atlassian.net/jira/software/c/projects/APP/boards/626?selectedIssue=APP-446)
    
    ## Checklist before requesting a review
    Checklist for adding a library:
    - [X] Copy this checklist into your (draft) PR description
    - [X] Check the existing description for the library in
    `apps/modernization-api/src/main/resources/db/changelog/report/execution/03_ODSE_Data_Report_Library_Init.sql`
    - [X] If the report execution service is not yet in use by any STLT
    (even beta), you should update this file
      - [ ] Update the description in the library-specific migration
    - [X] Add a migration to
    `apps/modernization-api/src/main/resources/db/report/execution/libraries`
    named `<your library name>.sql`
    - [X] Copy another migration and update the variables at the top of the
    file
    - [X] Add the migration to
    `apps/modernization-api/src/main/resources/db/report-execution-changelog.yml`.
    It should be added to the latest `changeSet` since the last release -
    this could require a new `changeSet` if there isn't one since last
    release
    - [X] Check the migration works by building/running the containerized
    dev setup
    - [X] Add a python library file to `apps/report-execution/src/libraries`
    named in lowercase, but generally following the naming convention of SAS
    (needs to be human recognizable as the same library)
    - [X] Follow the pattern established in existing libraries to create
    your library. The `execute` function is the required method and its
    signature will (someday) be checked for validity
    - [X] Make sure pertinent text embedded in the reports is included in
    the description of the report
      - [X] Note in the doc string any deviation from the SAS
    - [X] Keep an eye out for things in the sas library that are/could be
    centralized to pre- or post-processing or a util would be helpful for
    (e.g. common formatting needs)
    - [X] Add integration tests in a
    `apps/report-execution/tests/libraries/<your_library_here>py` file
    following the conventions established for other libraries
    - [X] The `subset_sql` can be assumed to be well tested by the
    modernization-api, so focus on any logic and additional
    joins/queries/analysis that is added in the library
    - [X] The database used in integration testing is our standard golden db
    + seed used in other testing - if additional data is needed to test the
    report, add it to the seeding process
    - [ ] Add e2e test coverage for the report
    - [x] If this step is not set up, create a ticket to track this as
    follow on work needed for this report
    - [ ] Check that the report has parity with the NBS 6 version using
    (process TBD)
    - [x] If this step is not set up, create a ticket to track this as
    follow on work needed for this report
    - [ ] Keep an eye out for opportunities to:
      - [ ] Consolidate reports by using parameters (parameter design tbd)
      - [ ] Improve the report spec or result expectations
    - [x] Update the [SAS to Python tracking
    spreadsheet](https://docs.google.com/spreadsheets/d/1xpl-znNQIql_tii39i5WYPIrO6vsLRqLBG1dbtuq8-4/edit?usp=sharing)
    with information about the newly converted library
    - [ ] Update/clarify these instructions in the epic based on your
    experience

---
+++ apps/report-execution/tests/integration/libraries/nbs_sr_13.py
@@ -0,0 +1,104 @@
+from decimal import Decimal
+
+import pytest
+import yaml
+
+from src.execute_report import execute_report
+from src.models import ReportSpec
+
+db_table = '[NBS_ODSE].[dbo].[PublicHealthCaseFact]'
+db_fk_tables = ['[NBS_ODSE].[dbo].[SubjectRaceInfo]']
+faker_schema = 'phc_demographic.yaml'
+
+
+@pytest.mark.usefixtures('setup_containers', 'fake_db_table')
+@pytest.mark.integration
+class TestIntegrationNbsSr13Library:
+    """Integration tests for the nbs_custom library."""
+
+    def test_execute_report_check_data(self, snapshot):
+        report_spec = ReportSpec.model_validate(
+            {
+                'version': 1,
+                'is_export': True,
+                'is_builtin': True,
+                'report_title': 'SR 13',
+                'library_name': 'nbs_sr_13',
+                'data_source_name': '[NBS_ODSE].[dbo].[PHCDemographic]',
+                'subset_query': 'SELECT * FROM [NBS_ODSE].[dbo].[PHCDemographic]',
+                'time_range': {'start': '2020-01-01', 'end': '2024-12-31'},
+            }
+        )
+
+        result = execute_report(report_spec)
+        assert result.content_type == 'table'
+
+        data = result.content.data
+        assert len(data) == 6  # two combinations with no data, zeros not filled
+        assert len(data[0]) == 3
+        assert len(data[0]) == len(result.content.columns)
+
+        snapshot.assert_match(yaml.dump(data), 'snapshot.yml')
+
+        # Sanity check the result's shape beyond the snapshot
+        record = None
+        for row in result.content.data:
+            if (
+                row[0] == Decimal('4877.00000')
+                and row[1] == 'Measles'
+                and row[2] == 'Confirmed'
+            ):
+                record = row
+                break
+
+        assert record is not None
+
+    def test_execute_report_no_data(self, snapshot):
+        report_spec = ReportSpec.model_validate(
+            {
+                'version': 1,
+                'is_export': True,
+                'is_builtin': True,
+                'report_title': 'SR 3',
+                'library_name': 'nbs_sr_13',
+                'data_source_name': '[NBS_ODSE].[dbo].[PHCDemographic]',
+                'subset_query': (
+                    'SELECT * FROM [NBS_ODSE].[dbo].[PHCDemographic]'
+                    "WHERE state = 'Rhode Island'"
+                ),
+                'time_range': {'start': '2020-01-01', 'end': '2024-12-31'},
+            }
+        )
+
+        result = execute_report(report_spec)
+        assert result.content_type == 'table'
+
+        data = result.content.data
+        assert len(data) == 0
+        assert len(result.content.columns) == 3
+
+    def test_execute_report_check_metadata(self):
+        """Check the metadata and column names are correct."""
+        report_spec = ReportSpec.model_validate(
+            {
+                'version': 1,
+                'is_export': True,
+                'is_builtin': True,
+                'report_title': 'SR 13',
+                'library_name': 'nbs_sr_13',
+                'data_source_name': '[NBS_ODSE].[dbo].[PHCDemographic]',
+                'subset_query': (
+                    'SELECT * FROM [NBS_ODSE].[dbo].[PHCDemographic] '
+                    " WHERE state_cd = '13'"
+                ),
+            }
+        )
+
+        result = execute_report(report_spec)
+        assert result.header == 'SR13: Counts of Selected Diseases By Case Status'
+        assert len(result.description) > 100
+        assert result.content_type == 'table'
+
+        assert result.content.columns[0] == 'Case Count'
+        assert result.content.columns[1] == 'Condition'
+        assert result.content.columns[2] == 'Case Status'
