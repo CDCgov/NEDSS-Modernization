e5218cb80126e82d6424c47e09bfd27fc6eadda0
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-04-13 19:41:41 -0400

    fix: naming, docs

--- apps/report-execution/scripts/restore_original_data.py
+++ apps/report-execution/scripts/restore_original_data.py
@@ -7,7 +7,11 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.utils import get_env_or_error
 from tests.conftest import get_tables_from_faker, restore_original_data
 
-faker_schema = sys.argv[1]
+try:
+    faker_schema = sys.argv[1]
+except IndexError:
+    print("Must pass table faker schema file name as argument")  # noqa: T201
+    exit(1)
 
 (db_tables, fk_tables) = get_tables_from_faker(faker_schema)
 
