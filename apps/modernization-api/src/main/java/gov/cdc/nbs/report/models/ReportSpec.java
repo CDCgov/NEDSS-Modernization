ca7cdc4f0dc0d88febf3e73f388eae132ba76eee
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-04-15 11:16:56 -0700

    Update ReportSpec.java

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/ReportSpec.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/ReportSpec.java
@@ -3,7 +3,6 @@ package gov.cdc.nbs.report.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import java.time.LocalDate;
 import java.util.Map;
 
