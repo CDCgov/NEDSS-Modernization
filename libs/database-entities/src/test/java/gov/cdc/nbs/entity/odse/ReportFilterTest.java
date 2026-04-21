aae05b92a4c093f9e727411715599c276fb0d784
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-04-20 20:03:37 -0400

    test: and test the validation

--- libs/database-entities/src/test/java/gov/cdc/nbs/entity/odse/ReportFilterTest.java
+++ libs/database-entities/src/test/java/gov/cdc/nbs/entity/odse/ReportFilterTest.java
@@ -45,6 +45,7 @@ class ReportFilterTest {
         .satisfies(rf -> assertEquals(filterCode, rf.getFilterCode()))
         .satisfies(rf -> assertEquals(dataSourceColumn, rf.getDataSourceColumn()))
         .satisfies(rf -> assertEquals(filterValues, rf.getFilterValues()))
+        .satisfies(rf -> assertEquals(filterValidation, rf.getFilterValidation()))
         .satisfies(rf -> assertEquals(statusCd, rf.getStatusCd()))
         .satisfies(rf -> assertEquals(maxValueCnt, rf.getMaxValueCnt()))
         .satisfies(rf -> assertEquals(minValueCnt, rf.getMinValueCnt()));
