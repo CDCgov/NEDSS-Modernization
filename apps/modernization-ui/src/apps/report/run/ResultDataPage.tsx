b67183170d1feffc79f099c99117ffa92dda2f1e
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-04-21 19:23:01 -0400

    feat: display report results in a table

--- apps/modernization-ui/src/apps/report/run/ResultDataPage.tsx
+++ apps/modernization-ui/src/apps/report/run/ResultDataPage.tsx
@@ -1,12 +1,36 @@
+import { AlertBanner } from 'apps/page-builder/components/AlertBanner/AlertBanner';
+import { DataTable } from 'design-system/table';
 import { ReportResult } from 'generated';
+import Papa from 'papaparse';
+import { useId } from 'react';
 
-const ResultDataPage = ({ result }: { result: ReportResult }) => {
+import styles from './result-data-table.module.scss';
+import classNames from 'classnames';
+
+const ResultDataPage = ({ result: { header, subheader, description, content } }: { result: ReportResult }) => {
+    const id = useId();
+    const { data, errors, meta } = Papa.parse<Record<string, string>>(content, { header: true, skipEmptyLines: true });
+    console.log({ styles });
+
     return (
         <main>
-            <h1>{result.header}</h1>
-            <h2>{result.subheader}</h2>
-            <section>{result.description}</section>
-            {result.content}
+            {(errors?.length ?? 0) > 0 && <AlertBanner type="error">{JSON.stringify(errors)}</AlertBanner>}
+            <h1>{header}</h1>
+            <h2>{subheader}</h2>
+            <section>{description}</section>
+            {meta.fields && (
+                <section className={classNames('overflow-auto', styles.table)}>
+                    <DataTable
+                        id={id}
+                        columns={meta.fields.map((c) => ({
+                            id: c,
+                            name: c,
+                            value: (row: Record<string, string>) => row[c],
+                        }))}
+                        data={data}
+                    />
+                </section>
+            )}
         </main>
     );
 };
