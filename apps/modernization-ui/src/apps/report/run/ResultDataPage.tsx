d03b4764b10001c2427b52bfe581c7fd3c2f302b
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-04-22 08:02:57 -0400

    Apply suggestions from code review
    Co-authored-by: Mary McGrath <m.c.mcgrath13@gmail.com>

--- apps/modernization-ui/src/apps/report/run/ResultDataPage.tsx
+++ apps/modernization-ui/src/apps/report/run/ResultDataPage.tsx
@@ -10,7 +10,6 @@ import classNames from 'classnames';
 const ResultDataPage = ({ result: { header, subheader, description, content } }: { result: ReportResult }) => {
     const id = useId();
     const { data, errors, meta } = Papa.parse<Record<string, string>>(content, { header: true, skipEmptyLines: true });
-    console.log({ styles });
 
     return (
         <main>
