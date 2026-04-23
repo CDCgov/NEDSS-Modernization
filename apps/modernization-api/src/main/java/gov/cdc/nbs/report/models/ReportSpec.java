07b4d8b5c7c607e5b0e07bcc87b8778ace73bbc6
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-04-22 21:52:26 -0700

    Update ReportSpec.java

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/ReportSpec.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/ReportSpec.java
@@ -3,9 +3,9 @@ package gov.cdc.nbs.report.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record ReportSpec(
-        @JsonProperty(value = "is_export", required = true) boolean isExport,
-        @JsonProperty(value = "is_builtin", required = true) boolean isBuiltin,
-        @JsonProperty(value = "report_title", required = true) String reportTitle,
-        @JsonProperty(value = "library_name", required = true) String libraryName,
-        @JsonProperty(value = "data_source_name", required = true) String dataSourceName,
-        @JsonProperty(value = "subset_query", required = true) String subsetQuery) {}
+    @JsonProperty(value = "is_export", required = true) boolean isExport,
+    @JsonProperty(value = "is_builtin", required = true) boolean isBuiltin,
+    @JsonProperty(value = "report_title", required = true) String reportTitle,
+    @JsonProperty(value = "library_name", required = true) String libraryName,
+    @JsonProperty(value = "data_source_name", required = true) String dataSourceName,
+    @JsonProperty(value = "subset_query", required = true) String subsetQuery) {}
