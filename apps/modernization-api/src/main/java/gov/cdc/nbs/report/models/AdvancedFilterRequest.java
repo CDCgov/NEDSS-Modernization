12aec3ae6bb61baf4e7518ff6bc76801c7c0b5e8
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-04-23 11:22:50 -0700

    bump

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdvancedFilterRequest.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdvancedFilterRequest.java
@@ -1,7 +1,9 @@
 package gov.cdc.nbs.report.models;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 public record AdvancedFilterRequest(
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Long reportFilterUid,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Expr logic) {}
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull Long reportFilterUid,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull @Valid Expr logic) {}
