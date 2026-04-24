377eb7448ff1a10e819f949395023df63cdd8e11
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-04-24 10:33:35 -0700

    Update ReportService.java

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/ReportService.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/ReportService.java
@@ -24,6 +24,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
 
 @Service
