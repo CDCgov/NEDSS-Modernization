a3778571e977feea2e799178d20fb571cb5a7bcb
elisa lee <ttu8@cdc.gov>
2026-05-05 10:07:44 -0500

    [APP-488] feat: Convert NBSSR00008 and functional testing updates of nbs_sr_02 and nbs_sr_08 (#3193)
    Checklist for adding a library:
    - [X] Copy this checklist into your (draft) PR description
    - [X] Check the existing description for the library in
    `apps/modernization-api/src/main/resources/db/changelog/report/execution/03_ODSE_Data_Report_Library_Init.sql`
    - [X] If the report execution service is not yet in use by any STLT
    (even beta), you should update this file
      - [] Update the description in the library-specific migration
    - [X] Add a migration to
    `apps/modernization-api/src/main/resources/db/report/execution/libraries`
    named `<your library name>.sql`
    - [X] Copy another migration and update the variables at the top of the
    file
    - [X] Add the migration to
    `apps/modernization-api/src/main/resources/db/report-execution-changelog.yml`.
    It should be added to the latest `changeSet` since the last release -
    this could require a new `changeSet` if there isn't one since last
    release
    - [X] Check the migration works by building/running the containerized
    dev setup
    - [X] Add a python library file to `apps/report-execution/src/libraries`
    named in lowercase, but generally following the naming convention of SAS
    (needs to be human recognizable as the same library)
    - [X] Follow the pattern established in existing libraries to create
    your library. The `execute` function is the required method and its
    signature will (someday) be checked for validity
    - [X] Make sure pertinent text embedded in the reports is included in
    the description of the report
      - [X] Note in the doc string any deviation from the SAS
    - [X] Keep an eye out for things in the sas library that are/could be
    centralized to pre- or post-processing or a util would be helpful for
    (e.g. common formatting needs)
    - [X] Add integration tests in a
    `apps/report-execution/tests/libraries/<your_library_here>py` file
    following the conventions established for other libraries
    - [X] The `subset_sql` can be assumed to be well tested by the
    modernization-api, so focus on any logic and additional
    joins/queries/analysis that is added in the library
    - [ ] The database used in integration testing is our standard golden db
    + seed used in other testing - if additional data is needed to test the
    report, add it to the seeding process
    - [ ] Add e2e test coverage for the report
    - [X] If this step is not set up, create a ticket to track this as
    follow on work needed for this report
    ([ticket](https://cdc-nbs.atlassian.net/browse/APP-547))
    - [X] Check that the report has parity with the NBS 6 version using
    (https://cdc-nbs.atlassian.net/wiki/spaces/NE/pages/2372272139/Comparison+Testing+Libraries)
    - [ ] Keep an eye out for opportunities to:
      - [x] Consolidate reports by using parameters (parameter design tbd)
      - [ ] Improve the report spec or result expectations
    - [x] Update the [SAS to Python tracking
    spreadsheet](https://docs.google.com/spreadsheets/d/1xpl-znNQIql_tii39i5WYPIrO6vsLRqLBG1dbtuq8-4/edit?usp=sharing)
    with information about the newly converted library
    - [ ] Update/clarify these instructions in the epic based on your
    experience
    
    ---------
    
    Co-authored-by: elisa lee <emyl3@users.noreply.github.com>

--- apps/report-execution/src/libraries/nbs_sr_08.py
+++ apps/report-execution/src/libraries/nbs_sr_08.py
@@ -9,29 +9,32 @@ def execute(
     data_source_name: str,
     **kwargs,
 ):
-    """Standard Report 02: Cases of Reportable Diseases by County for Selected Time
-    Frame.
+    """Standard Report 08: Report of Disease Cases Over Selected Time Period.
 
     Conversion notes:
-    * Matched export format without pivot
+    * Matched export format
+    * Removed references to 'State Map' or 'choropleth map'
+    * Removed 'State and county boundaries are based on the
+    * SAS maps library uscounty dataset' in description
     * Removed calculations section of descriptions as those were run-format specific
-    * Capitalized the `F` in `Time Frame`
     """
     content = trx.query(
         f"""
         WITH subset AS ({subset_query})
         SELECT 
+            state_cd AS [State Code],
             state AS [State],
             county AS [County],
             phc_code_short_desc AS [Condition],
+            UPPER(FORMAT(event_date, 'ddMMMyyyy:HH:mm:ss.fff')) AS [Event Date],
+            cnty_cd AS [County Code],
             sum(group_case_cnt) AS [Cases]
         FROM subset
         WHERE event_date IS NOT NULL
-        GROUP BY state, county, phc_code_short_desc
-        ORDER BY state, county, phc_code_short_desc;
+        GROUP BY state_cd, state, county, phc_code_short_desc, event_date, cnty_cd
+        ORDER BY state_cd, state, county, phc_code_short_desc, event_date, cnty_cd;
         """
     )
-
     # Get the unique state(s) in the data set for subheader display
     state_list = content.get_unique_column('State')
     subheader = gen_subheader(states=state_list)
@@ -44,12 +47,12 @@ def execute(
         'Case Status.\n'
         'Output:\n'
         '* Does not include Investigation(s) that have been logically deleted\n'
-        '* Is filtered based on the state, county(s), disease(s), time frame and '
+        '* Is filtered based on the state, disease, time frame and '
         'advanced criteria selected by user\n'
         '* Will not include Investigation(s) that do not have a value for the State '
         'selected by the user\n'
         '* Will not include Investigation(s) that do not have a value for the '
-        'County(s) selected by the user\n'
+        'County (even though a value for State may exist)\n'
         '* Is based on the calculated Event Date\n'
     )
 
