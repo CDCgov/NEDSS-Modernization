721f41342d59f4607b8f5921871adf419bd4631d
Ada Ball <ada@adamball.org>
2026-05-13 10:03:07 -0500

    Move ES index initialization code to lib/es-indexing

--- libs/es-indexing/src/main/java/gov/cdc/nbs/search/initialize/ElasticsearchIndexInitialization.java
+++ libs/es-indexing/src/main/java/gov/cdc/nbs/search/initialize/ElasticsearchIndexInitialization.java
@@ -3,20 +3,25 @@ package gov.cdc.nbs.search.initialize;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import gov.cdc.nbs.search.SimpleIndex;
 import java.util.Collection;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 class ElasticsearchIndexInitialization {
 
   @Bean
   InitializingBean ensureIndexes(
       final Collection<SimpleIndex> indices, final ElasticsearchClient client) {
-    return () ->
-        new ElasticsearchIndexEnsurer(
-                new ElasticsearchIndexExistenceVerifier(client),
-                new ElasticsearchIndexCreator(client))
-            .ensure(indices);
+    return () -> {
+      log.info("Ensuring indicies: {}", indices);
+
+      new ElasticsearchIndexEnsurer(
+              new ElasticsearchIndexExistenceVerifier(client),
+              new ElasticsearchIndexCreator(client))
+          .ensure(indices);
+    };
   }
 }
