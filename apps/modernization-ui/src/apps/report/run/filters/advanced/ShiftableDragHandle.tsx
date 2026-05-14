056f06bca795b9f3cc438e82bde9081e869c440d
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-05-14 19:45:33 -0400

    fix: make interaction match other dnd

--- apps/modernization-ui/src/apps/report/run/filters/advanced/ShiftableDragHandle.tsx
+++ apps/modernization-ui/src/apps/report/run/filters/advanced/ShiftableDragHandle.tsx
@@ -2,6 +2,8 @@ import { forwardRef, ForwardRefExoticComponent, KeyboardEventHandler, RefAttribu
 import { DragHandleProps } from 'react-querybuilder';
 import { useKeyboardDnd } from './useKeyboardDnd';
 import { Icon } from 'design-system/icon';
+import * as keyCodes from './keyCodes';
+import supportedPageVisibilityEventName from './supportedPageVisibilityEventName';
 
 // custom drag handle to add shifting action on keyboard up down when space enabled
 const ShiftableDragHandle: ForwardRefExoticComponent<DragHandleProps & RefAttributes<HTMLSpanElement>> = forwardRef<
@@ -22,6 +24,11 @@ const ShiftableDragHandle: ForwardRefExoticComponent<DragHandleProps & RefAttrib
         }
     }, []);
 
+    const cancel = () => {
+        setIsActive(false);
+        reset(getQuery(), dispatchQuery);
+    };
+
     const handleKeyDown: KeyboardEventHandler = (event) => {
         // space toggles, escape will turn off activity if active
         if (event.code === 'Space') {
@@ -35,36 +42,33 @@ const ShiftableDragHandle: ForwardRefExoticComponent<DragHandleProps & RefAttrib
             event.preventDefault();
             return;
         } else if (isActive && event.code === 'Escape') {
-            setIsActive(false);
-            reset(getQuery(), dispatchQuery);
             // restore move focus to the drag handle that was moved back
+            // but otherwise let the general handler deal with logic
             setTimeout(() => {
                 const thisEl = document.querySelector<HTMLSpanElement>(`#drag-handle-${id}`);
                 thisEl?.focus();
             }, 50);
-            event.preventDefault();
             return;
         }
-
-        if (!isActive) return;
-
-        let dir: 'up' | 'down' | undefined = undefined;
-        if (event.code === 'ArrowUp') {
-            dir = 'up';
-        } else if (event.code === 'ArrowDown') {
-            dir = 'down';
-        } else {
-            // only allow stated actions while activated
-            event.preventDefault();
-            event.stopPropagation();
-        }
-
-        if (!dir) return;
-
-        // move the rule and update the query
-        drag(getQuery(), dispatchQuery, dir);
-        event.preventDefault();
     };
+
+    // once the drag handle is active, move to window level events as we're in a
+    // "captive" flow and want to make sure things are always handled.
+    // This is adapted from pragmatic-dnd that's used elsewhere in the app
+    useEffect(() => {
+        if (isActive) {
+            const bindings = getDraggingBindings((dir) => drag(getQuery(), dispatchQuery, dir), cancel);
+
+            bindings.forEach(({ eventName, fn, options = {} }) =>
+                window.addEventListener(eventName, fn, { capture: true, passive: false, ...options })
+            );
+
+            return () =>
+                bindings.forEach(({ eventName, fn, options = {} }) =>
+                    window.removeEventListener(eventName, fn, { capture: true, passive: false, ...options })
+                );
+        }
+    }, [isActive]);
 
     return (
         <span
@@ -81,5 +85,95 @@ const ShiftableDragHandle: ForwardRefExoticComponent<DragHandleProps & RefAttrib
         </span>
     );
 });
+
+// Adapted from https://github.com/hello-pangea/dnd/blob/main/src/view/use-sensor-marshal/sensors/use-keyboard-sensor.ts#L23-L139
+// to match behavior of other dnd in the app
+interface KeyMap {
+    [key: number]: true;
+}
+const preventedKeys: KeyMap = {
+    // submission
+    [keyCodes.enter]: true,
+    // tabbing
+    [keyCodes.tab]: true,
+    // scrolling
+    [keyCodes.arrowRight]: true,
+    [keyCodes.arrowLeft]: true,
+    [keyCodes.pageDown]: true,
+    [keyCodes.pageUp]: true,
+    [keyCodes.home]: true,
+    [keyCodes.end]: true,
+};
+
+function getDraggingBindings(move: (dir: 'up' | 'down') => void, cancel: () => void) {
+    return [
+        {
+            eventName: 'keydown',
+            fn: (event: KeyboardEvent) => {
+                if (event.keyCode === keyCodes.escape) {
+                    event.preventDefault();
+                    cancel();
+                    return;
+                }
+
+                // Movement
+
+                if (event.keyCode === keyCodes.arrowDown) {
+                    event.preventDefault();
+                    move('down');
+                    return;
+                }
+
+                if (event.keyCode === keyCodes.arrowUp) {
+                    event.preventDefault();
+                    move('up');
+                    return;
+                }
+
+                if (preventedKeys[event.keyCode]) {
+                    event.preventDefault();
+                    return;
+                }
+            },
+        },
+        // any mouse actions kills a drag
+        {
+            eventName: 'mousedown',
+            fn: cancel,
+        },
+        {
+            eventName: 'mouseup',
+            fn: cancel,
+        },
+        {
+            eventName: 'click',
+            fn: cancel,
+        },
+        {
+            eventName: 'touchstart',
+            fn: cancel,
+        },
+        // resizing the browser kills a drag
+        {
+            eventName: 'resize',
+            fn: cancel,
+        },
+        // kill if the user is using the mouse wheel
+        // We are not supporting wheel / trackpad scrolling with keyboard dragging
+        {
+            eventName: 'wheel',
+            fn: cancel,
+            // chrome says it is a violation for this to not be passive
+            // it is fine for it to be passive as we just cancel as soon as we get
+            // any event
+            options: { passive: true },
+        },
+        // Cancel on page visibility change
+        {
+            eventName: supportedPageVisibilityEventName,
+            fn: cancel,
+        },
+    ];
+}
 
 export { ShiftableDragHandle };
---
+++ apps/modernization-ui/src/apps/report/run/filters/advanced/keyCodes.ts
@@ -0,0 +1,13 @@
+// From https://github.com/hello-pangea/dnd/blob/28c47a7a0139a09b1bf0a77a010e7121c39246d4/src/view/key-codes.ts#L4
+export const tab = 9;
+export const enter = 13;
+export const escape = 27;
+export const space = 32;
+export const pageUp = 33;
+export const pageDown = 34;
+export const end = 35;
+export const home = 36;
+export const arrowLeft = 37;
+export const arrowUp = 38;
+export const arrowRight = 39;
+export const arrowDown = 40;
