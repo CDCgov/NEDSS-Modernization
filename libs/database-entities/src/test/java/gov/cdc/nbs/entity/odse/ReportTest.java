463bc47230e863cc60706e58c82651d8fde02ae9
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-05-20 15:10:17 -0700

    Update ReportTest.java

--- libs/database-entities/src/test/java/gov/cdc/nbs/entity/odse/ReportTest.java
+++ libs/database-entities/src/test/java/gov/cdc/nbs/entity/odse/ReportTest.java
@@ -1,7 +1,6 @@
 package gov.cdc.nbs.entity.odse;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
