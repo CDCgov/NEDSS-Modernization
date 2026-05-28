52a624d466b7527ef54f5bdf49fea386a9e24df5
Ada Ball <ada@adamball.org>
2026-05-28 12:53:01 -0500

    linting

--- apps/report-execution/scripts/uniq_line_csvs.py
+++ apps/report-execution/scripts/uniq_line_csvs.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
-"""
-Check the lines in two CSV files, output lines that differ in each. DOES NOT
+"""Check the lines in two CSV files, output lines that differ in each. DOES NOT
 care about ordering, file lines are put in a set and compared to one another.
 
     ./uniq_line_csvs.py <file_one> <file_two>
 """
 
+import logging
 from sys import argv
+
+logging.basicConfig(level='INFO')
 
 
 def csv_to_line_set(fp):
-    with open(fp, 'r') as fd:
+    """Given a file path of a CSV file, put all of its lines into a set."""
+    with open(fp) as fd:
         lines = fd.readlines()
-        lines = [l.strip() for l in lines]
+        lines = [line.strip() for line in lines]
 
     return set(lines)
 
 
 def get_csv_line_sets(first_csv, second_csv):
-
+    """Given two CSV file paths, create sets of each of their lines."""
     first_set = csv_to_line_set(first_csv)
     second_set = csv_to_line_set(second_csv)
 
@@ -27,7 +30,7 @@ def get_csv_line_sets(first_csv, second_csv):
 
 if __name__ == '__main__':
     if len(argv) != 3:
-        print(f'{argv[0]} <first_csv> <second_csv>')
+        logging.info(f'{argv[0]} <first_csv> <second_csv>')
         exit()
 
     fp1 = argv[1]
@@ -35,32 +38,32 @@ if __name__ == '__main__':
 
     first_set, second_set = get_csv_line_sets(fp1, fp2)
 
-    print(32 * '*')
+    logging.info(32 * '*')
 
-    print(f'* stats for [{fp1}]')
-    print(f'* line count: {len(first_set)}')
+    logging.info(f'* stats for [{fp1}]')
+    logging.info(f'* line count: {len(first_set)}')
 
     diff = first_set.difference(second_set)
 
     if not diff:
-        print(f'* all lines in [{fp1}] are found in [{fp2}]')
+        logging.info(f'* all lines in [{fp1}] are found in [{fp2}]')
     else:
-        print(f'* lines found in [{fp1}] that are NOT in [{fp2}]:')
+        logging.info(f'* lines found in [{fp1}] that are NOT in [{fp2}]:')
         for line in diff:
-            print(f'{line}')
+            logging.info(f'{line}')
 
-    print(32 * '*')
+    logging.info(32 * '*')
 
-    print(f'* stats for [{fp2}]')
-    print(f'* line count: {len(second_set)}')
+    logging.info(f'* stats for [{fp2}]')
+    logging.info(f'* line count: {len(second_set)}')
 
     diff = second_set.difference(first_set)
 
     if not diff:
-        print(f'* all lines in [{fp2}] are found in [{fp1}]')
+        logging.info(f'* all lines in [{fp2}] are found in [{fp1}]')
     else:
-        print(f'* lines found in [{fp2}] that are NOT in [{fp1}]:')
+        logging.info(f'* lines found in [{fp2}] that are NOT in [{fp1}]:')
         for line in diff:
-            print(f'{line}')
+            logging.info(f'{line}')
 
-    print(32 * '*')
+    logging.info(32 * '*')
