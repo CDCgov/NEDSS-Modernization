df7a0a2cd43a93136daf169a81cadfcfcd696779
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-05-27 19:45:52 -0700

    Update AdminReportRequest.java

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdminReportRequest.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdminReportRequest.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-
 import java.util.List;
 
 public record AdminReportRequest(
