19002af7fa8c3c86d2a05ce2ac82f3f5430721ac
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-05-27 19:45:18 -0700

    Update AdminReportRequest.java

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdminReportRequest.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdminReportRequest.java
@@ -2,14 +2,18 @@ package gov.cdc.nbs.report.models;
 
 import gov.cdc.nbs.report.ReportConstants;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.util.List;
 
 public record AdminReportRequest(
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Long dataSourceId,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Long libraryId,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String reportTitle,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String sectionCode,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Long ownerId,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) ReportConstants.ReportGroup group,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) List<CreateFilterRequest> filterRequests,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull @Positive Long dataSourceId,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull @Positive Long libraryId,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull @NotBlank String reportTitle,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull @NotBlank String sectionCode,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull Long ownerId,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull ReportConstants.ReportGroup group,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull List<CreateFilterRequest> filterRequests,
     String description) {}
