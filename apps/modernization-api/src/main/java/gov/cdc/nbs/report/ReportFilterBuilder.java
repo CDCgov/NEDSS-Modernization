d9644b15c77f98377807d9dc12e43f4ff3bc864a
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-05-29 12:02:49 -0700

    bump

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/ReportFilterBuilder.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/ReportFilterBuilder.java
@@ -3,6 +3,7 @@ package gov.cdc.nbs.report;
 import gov.cdc.nbs.audit.Status;
 import gov.cdc.nbs.entity.odse.*;
 import gov.cdc.nbs.report.models.UpsertFilterRequest;
+import gov.cdc.nbs.report.utils.ValueCountCalculator;
 import gov.cdc.nbs.repository.DataSourceColumnRepository;
 import gov.cdc.nbs.repository.FilterCodeRepository;
 import java.time.LocalDateTime;
@@ -46,59 +47,13 @@ public class ReportFilterBuilder {
                     new IllegalArgumentException(
                         "Filter code not found for UID: " + filter.filterCodeUid()));
 
-    Integer minValueCount = null;
-    Integer maxValueCount = null;
-
-    // Note that this logic mapping filterCodeUids to corresponding min value counts and
-    // max value counts was pulled **verbatim** from NBS 6, to maintain parity with 6 as
-    // closely as possible.
-    switch (filter.filterCodeUid().intValue()) {
-      case 7 // Where Clause Builder
-          -> {
-        minValueCount = 0;
-        maxValueCount = -1;
-      }
-      case 5, // Time Range
-          6, // Time Period
-          12, // Time Range (Including NULLS)
-          13, // Time Period (Including NULLS)
-          14, // Month Year Range
-          15, // Month Year Range (Including NULLS)
-          17, // Time Range Custom
-          18 // Basic Text Filter
-          -> {
-        minValueCount = 1;
-        maxValueCount = 2;
-      }
-      case 1, // Diseases
-          2, // States
-          3, // Counties
-          8, // Diseases (Including NULLS)
-          9, // States (Including NULLS)
-          10, // Counties (Including NULLS)
-          16, // Case Diagnosis
-          19, // STD Case Diagnosis
-          20, // HIV Case Diagnosis
-          21 // STD HIV Workers
-          -> {
-        switch (filter.selectType()) {
-          case ReportConstants.SelectType.MULTI -> {
-            minValueCount = 1;
-            maxValueCount = -1;
-          }
-          case ReportConstants.SelectType.SINGLE -> {
-            minValueCount = 1;
-            maxValueCount = 1;
-          }
-        }
-      }
-    }
-
     ReportFilter.ReportFilterBuilder filterBuilder =
         ReportFilter.builder().report(report).filterCode(filterCode).statusCd(Status.ACTIVE_CODE);
 
-    filterBuilder.minValueCnt(minValueCount);
-    filterBuilder.maxValueCnt(maxValueCount);
+      ValueCountCalculator.ReportValueCounts valueCounts =
+          ValueCountCalculator.fromFilterRequest(filter);
+    filterBuilder.minValueCnt(valueCounts.minValueCount());
+    filterBuilder.maxValueCnt(valueCounts.maxValueCount());
 
     if (dataSourceColumn != null) {
       filterBuilder.dataSourceColumn(dataSourceColumn);
---
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/utils/ValueCountCalculator.java
@@ -0,0 +1,62 @@
+package gov.cdc.nbs.report.utils;
+
+import gov.cdc.nbs.report.ReportConstants;
+import gov.cdc.nbs.report.models.UpsertFilterRequest;
+
+public class ValueCountCalculator {
+    private ValueCountCalculator() {}
+
+    public record ReportValueCounts(Integer minValueCount, Integer maxValueCount) {}
+
+    public static ReportValueCounts fromFilterRequest(UpsertFilterRequest filterRequest) {
+        Integer minValueCount = null;
+        Integer maxValueCount = null;
+
+        // Note that this logic mapping filterCodeUids to corresponding min value counts and
+        // max value counts was pulled **verbatim** from NBS 6, to maintain parity with 6 as
+        // closely as possible.
+        switch (filterRequest.filterCodeUid().intValue()) {
+            case 7 // Where Clause Builder
+                    -> {
+                minValueCount = 0;
+                maxValueCount = -1;
+            }
+            case 5, // Time Range
+                 6, // Time Period
+                 12, // Time Range (Including NULLS)
+                 13, // Time Period (Including NULLS)
+                 14, // Month Year Range
+                 15, // Month Year Range (Including NULLS)
+                 17, // Time Range Custom
+                 18 // Basic Text Filter
+                    -> {
+                minValueCount = 1;
+                maxValueCount = 2;
+            }
+            case 1, // Diseases
+                 2, // States
+                 3, // Counties
+                 8, // Diseases (Including NULLS)
+                 9, // States (Including NULLS)
+                 10, // Counties (Including NULLS)
+                 16, // Case Diagnosis
+                 19, // STD Case Diagnosis
+                 20, // HIV Case Diagnosis
+                 21 // STD HIV Workers
+                    -> {
+                switch (filterRequest.selectType()) {
+                    case ReportConstants.SelectType.MULTI -> {
+                        minValueCount = 1;
+                        maxValueCount = -1;
+                    }
+                    case ReportConstants.SelectType.SINGLE -> {
+                        minValueCount = 1;
+                        maxValueCount = 1;
+                    }
+                }
+            }
+        }
+
+        return new ReportValueCounts(minValueCount, maxValueCount);
+    }
+}
