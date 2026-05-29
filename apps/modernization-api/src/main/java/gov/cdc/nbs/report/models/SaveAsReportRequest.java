dc519a5ba5befa70d4eafd8de63091bbb7cc8c64
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-05-29 11:35:20 -0700

    Update SaveAsReportRequest.java

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/SaveAsReportRequest.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/SaveAsReportRequest.java
@@ -2,10 +2,13 @@ package gov.cdc.nbs.report.models;
 
 import gov.cdc.nbs.report.ReportConstants;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record SaveAsReportRequest(
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String reportTitle,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String sectionCode,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) ReportConstants.ReportGroup group,
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) ReportExecutionRequest executionRequest,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull @NotBlank String reportTitle,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull @NotBlank String sectionCode,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull @NotBlank ReportConstants.ReportGroup group,
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @Valid ReportExecutionRequest executionRequest,
     String description) {}
