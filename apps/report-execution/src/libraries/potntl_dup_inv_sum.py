d8cd978037d1a83bdfdc67be7234297bd9ea44b7
Krista Chan <kristachan@Kristas-MacBook-Pro.local>
2026-06-03 12:51:46 -0700

    chore: raise error if not column map

--- apps/report-execution/src/libraries/potntl_dup_inv_sum.py
+++ apps/report-execution/src/libraries/potntl_dup_inv_sum.py
@@ -22,6 +22,10 @@ def execute(
     Event Date to ensure consistent results regardless of encoding.
     * Dates defer to default formatting
     """
+    if not column_map:
+        raise MissingColumnError(
+            ['PATIENT_LOCAL_ID', 'DISEASE_CD', 'EVENT_DATE']
+        )
     # for easier lookups when order doesn't matter
     col_dict = {m[0]: m[1] for m in column_map}
 
