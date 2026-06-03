dc09e8ddc3e1bc35c1a88faf5d745f3f86c5464a
Ada Ball <ada@adamball.org>
2026-06-03 10:11:31 -0500

    Simplify SR18 library to only follow the SAS export format, not the SAS report format

--- apps/report-execution/src/libraries/nbs_sr_18.py
+++ apps/report-execution/src/libraries/nbs_sr_18.py
@@ -14,125 +14,12 @@ def execute(
     query = f"""
         WITH subset AS (
             {subset_query}
-        ),
-
-        -- ensure we have all case verification values even if there's no matching data
-        static_cv AS (
-            SELECT '0 - Not a Verified Case' AS 'CASE_VERIFICATION'
-            UNION
-            SELECT '1 - Positive Culture' AS 'CASE_VERIFICATION'
-            UNION
-            SELECT '1A - Positive NAA' AS 'CASE_VERIFICATION'
-            UNION
-            SELECT '2 - Positive Smear/Tissue' AS 'CASE_VERIFICATION'
-            UNION
-            SELECT '3 - Clinical Case Definition' AS 'CASE_VERIFICATION'
-            UNION
-            SELECT '4 - Verified by Provider Diagnosis' AS 'CASE_VERIFICATION'
-            UNION
-            SELECT '5 - Suspect' AS 'CASE_VERIFICATION'
-        ),
-
-        -- count all of the calc disease sites for each case verification
-        counts AS (
-            SELECT CASE_VERIFICATION,
-                SUM(CASE
-                        WHEN CALC_DISEASE_SITE = 'Pulmonary'
-                            THEN 1
-                        ELSE 0
-                        END) AS [Pulmonary Count],
-                SUM(CASE
-                        WHEN CALC_DISEASE_SITE = 'Extra Pulmonary'
-                            THEN 1
-                        ELSE 0
-                        END) AS [Extrapulmonary Count],
-                SUM(CASE
-                        WHEN CALC_DISEASE_SITE = 'Both'
-                            THEN 1
-                        ELSE 0
-                        END) AS [Both Count],
-                SUM(CASE
-                        WHEN CALC_DISEASE_SITE = 'Unknown'
-                            THEN 1
-                        ELSE 0
-                        END) AS [Unknown Count],
-                COUNT(*) AS [All Cases Count]
-            FROM subset
-            WHERE CASE_VERIFICATION IS NOT NULL
-                AND CALC_DISEASE_SITE IS NOT NULL
-            GROUP BY CASE_VERIFICATION
         )
 
-        -- display the final counts, substituting 0 where no data was found
-        SELECT sc.CASE_VERIFICATION AS [Case Verification],
-            CASE
-                WHEN c.[Pulmonary Count] IS NULL
-                    THEN 0
-                ELSE c.[Pulmonary Count]
-                END AS [Pulmonary Count],
-            CASE
-                WHEN c.[Extrapulmonary Count] IS NULL
-                    THEN 0
-                ELSE c.[Extrapulmonary Count]
-                END AS [Extrapulmonary Count],
-            CASE
-                WHEN c.[Both Count] IS NULL
-                    THEN 0
-                ELSE c.[Both Count]
-                END AS [Both Count],
-            CASE
-                WHEN c.[Unknown Count] IS NULL
-                    THEN 0
-                ELSE c.[Unknown Count]
-                END AS [Unknown Count],
-            CASE
-                WHEN c.[All Cases Count] IS NULL
-                    THEN 0
-                ELSE c.[All Cases Count]
-                END AS [All Cases Count]
-        FROM counts c
-        RIGHT OUTER JOIN static_cv sc ON sc.CASE_VERIFICATION = c.CASE_VERIFICATION
-        ORDER BY sc.CASE_VERIFICATION;
+        SELECT CASE_VERIFICATION, CALC_DISEASE_SITE
+        FROM subset;
     """
 
     content = trx.query(query)
-    table = _perform_calculations(content)
 
-    return ReportResult(content_type='table', content=table)
-
-
-def _perform_calculations(table: Table) -> Table:
-    """Take the raw query result data and calculate the data needed
-    to recreate the SAS report.
-    """
-    df = pd.DataFrame.from_records(table.data, columns=table.columns)
-
-    # order the columns to match the SAS report
-    cols = ['Pulmonary', 'Extrapulmonary', 'Both', 'Unknown', 'All Cases']
-    ordered_cols = ['Case Verification']
-    for col in cols:
-        ordered_cols.extend([f'{col} Count', f'{col} %'])
-
-    # short-circuit for no data
-    if not table.data:
-        return Table(columns=ordered_cols, data=table.data)
-
-    # calculate percentage of total for each value in each "Count" column
-    for col in cols:
-        df[f'{col} %'] = (df[f'{col} Count'] / df[f'{col} Count'].sum() * 100).round(1)
-
-    # re-write the case verification strings to match SAS report
-    df['Case Verification'] = [
-        '0 - Not a Verified Case',
-        '1 - Positive Culture',
-        '1A - Positive NAA',
-        '2 - Positive Smear',
-        '3 - Clinical Case Definition',
-        '4 - Provider Diagnosis',
-        '5 - Suspect Case',
-    ]
-
-    # ensure column ordering matches the SAS report
-    df = df[ordered_cols]
-
-    return Table(data=df.values.tolist(), columns=df.columns.to_list())
+    return ReportResult(content_type='table', content=content)
--- apps/report-execution/tests/integration/libraries/nbs_sr_18.py
+++ apps/report-execution/tests/integration/libraries/nbs_sr_18.py
@@ -35,41 +35,14 @@ class TestIntegrationNbsSr18Library:
 
         # ensure the columns are correct
         assert result.content.columns == [
-            'Case Verification',
-            'Pulmonary Count',
-            'Pulmonary %',
-            'Extrapulmonary Count',
-            'Extrapulmonary %',
-            'Both Count',
-            'Both %',
-            'Unknown Count',
-            'Unknown %',
-            'All Cases Count',
-            'All Cases %',
+            'CASE_VERIFICATION',
+            'CALC_DISEASE_SITE',
         ]
 
         # ensure proper data types for each column
         for d in data:
             assert isinstance(d[0], str)
-            assert isinstance(d[1], int)
-            assert isinstance(d[2], float)
-            assert isinstance(d[3], int)
-            assert isinstance(d[4], float)
-            assert isinstance(d[5], int)
-            assert isinstance(d[6], float)
-            assert isinstance(d[7], int)
-            assert isinstance(d[8], float)
-            assert isinstance(d[9], int)
-            assert isinstance(d[10], float)
-
-        # ensure the Case Verification strings match the SAS report
-        assert data[0][0] == '0 - Not a Verified Case'
-        assert data[1][0] == '1 - Positive Culture'
-        assert data[2][0] == '1A - Positive NAA'
-        assert data[3][0] == '2 - Positive Smear'
-        assert data[4][0] == '3 - Clinical Case Definition'
-        assert data[5][0] == '4 - Provider Diagnosis'
-        assert data[6][0] == '5 - Suspect Case'
+            assert isinstance(d[1], str)
 
     def test_execute_report_no_data(self, snapshot):
         report_spec = ReportSpec.model_validate(
@@ -88,85 +61,10 @@ class TestIntegrationNbsSr18Library:
         # should still have 7 rows of data and all columns
         assert len(result.content.data) == 7
         assert result.content.columns == [
-            'Case Verification',
-            'Pulmonary Count',
-            'Pulmonary %',
-            'Extrapulmonary Count',
-            'Extrapulmonary %',
-            'Both Count',
-            'Both %',
-            'Unknown Count',
-            'Unknown %',
-            'All Cases Count',
-            'All Cases %',
+            'CASE_VERIFICATION',
+            'CALC_DISEASE_SITE',
         ]
 
         for data in result.content.data:
             assert isinstance(data[0], str)
-            assert isinstance(data[1], int)
-            assert isinstance(data[2], float)
-            assert isinstance(data[3], int)
-            assert isinstance(data[4], float)
-            assert isinstance(data[5], int)
-            assert isinstance(data[6], float)
-            assert isinstance(data[7], int)
-            assert isinstance(data[8], float)
-            assert isinstance(data[9], int)
-            assert isinstance(data[10], float)
-
-    def test_execute_report_spotty_data(self, snapshot):
-        case_verifications = [
-            '0 - Not a Verified Case',
-            '1 - Positive Culture',
-            '1A - Positive NAA',
-            '2 - Positive Smear/Tissue',
-            '3 - Clinical Case Definition',
-            '4 - Verified by Provider Diagnosis',
-            '5 - Suspect',
-        ]
-
-        for cv in case_verifications:
-            subset_query = 'SELECT * FROM [RDB].[dbo].[TB_DATAMART]'
-            subset_query += f"WHERE CASE_VERIFICATION = '{cv}'"
-
-            report_spec = ReportSpec.model_validate(
-                {
-                    'is_export': True,
-                    'is_builtin': True,
-                    'report_title': 'SR 18',
-                    'library_name': 'nbs_sr_18',
-                    'data_source_name': '[RDB].[dbo].[TB_DATAMART]',
-                    'subset_query': subset_query,
-                }
-            )
-
-            result = execute_report(report_spec)
-
-            # should still have 7 rows of data and all columns
-            assert len(result.content.data) == 7
-            assert result.content.columns == [
-                'Case Verification',
-                'Pulmonary Count',
-                'Pulmonary %',
-                'Extrapulmonary Count',
-                'Extrapulmonary %',
-                'Both Count',
-                'Both %',
-                'Unknown Count',
-                'Unknown %',
-                'All Cases Count',
-                'All Cases %',
-            ]
-
-            for data in result.content.data:
-                assert isinstance(data[0], str)
-                assert isinstance(data[1], int)
-                assert isinstance(data[2], float)
-                assert isinstance(data[3], int)
-                assert isinstance(data[4], float)
-                assert isinstance(data[5], int)
-                assert isinstance(data[6], float)
-                assert isinstance(data[7], int)
-                assert isinstance(data[8], float)
-                assert isinstance(data[9], int)
-                assert isinstance(data[10], float)
+            assert isinstance(data[1], str)
