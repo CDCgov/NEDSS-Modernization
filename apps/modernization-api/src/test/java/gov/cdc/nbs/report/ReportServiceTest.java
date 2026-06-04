daef6c9b35a3f36fedbe2f3cfb841ddec2ede9d5
Krista Chan <kristachan@Mac.lan>
2026-06-04 10:45:03 -0700

    chore: more linting

--- apps/modernization-api/src/test/java/gov/cdc/nbs/report/ReportServiceTest.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/report/ReportServiceTest.java
@@ -562,7 +562,8 @@ class ReportServiceTest {
               "nbs_custom",
               "[NBS_ODSE].[dbo].[PHCDemographic]",
               "SELECT * FROM [NBS_ODSE].[dbo].[PHCDemographic]",
-              null, null);
+              null,
+              null);
       try (MockedConstruction<ReportSpecBuilder> specBuilderMock =
           mockConstruction(
               ReportSpecBuilder.class,
