f45f9f91b246442cf653304f0473a01b04d48e67
Ada Ball <ada@adamball.org>
2026-06-05 10:16:01 -0500

    update tests for nbs_custom library, including snapshot testing

--- apps/report-execution/tests/integration/libraries/nbs_custom.py
+++ apps/report-execution/tests/integration/libraries/nbs_custom.py
@@ -1,34 +1,73 @@
 import pytest
+import yaml
 
 from src.execute_report import execute_report
 from src.models import ReportSpec
 
+faker_schema = 'phc_demographic.yaml'
 
-@pytest.mark.usefixtures('setup_containers')
+
+@pytest.mark.usefixtures('setup_containers', 'fake_db_table')
 @pytest.mark.integration
 class TestIntegrationNbsCustomLibrary:
     """Integration tests for the nbs_custom library."""
 
-    def test_execute_report(self):
+    def test_execute_report_check_data(self, snapshot):
         report_spec = ReportSpec.model_validate(
             {
                 'is_export': True,
                 'is_builtin': True,
                 'report_title': 'NBS Custom',
                 'library_name': 'nbs_custom',
-                # Filter operator is used here as it is a stable, small table
-                'data_source_name': '[NBS_ODSE].[dbo].[Filter_operator]',
-                'subset_query': 'SELECT * FROM [NBS_ODSE].[dbo].[Filter_operator]',
+                'data_source_name': '[NBS_ODSE].[dbo].[PHCDemographic]',
+                'subset_query': """
+                        SELECT PHC_code_short_desc,
+                               state,
+                               county
+                        FROM NBS_ODSE.dbo.PHCDemographic
+                        ORDER BY PHC_code_short_desc, state, county
+                """,
             }
         )
 
         result = execute_report(report_spec)
-        assert result.header == (
-            'Custom Report For Table: [NBS_ODSE].[dbo].[Filter_operator]'
-        )
-        assert result.subheader is None
-        assert result.description is None
         assert result.content_type == 'table'
 
-        assert len(result.content.data) == 11
-        assert len(result.content.data[0]) == len(result.content.columns)
+        data = result.content.data
+        assert len(data) == 1000
+        assert len(data[0]) == len(result.content.columns)
+        assert result.content.columns == [
+            'PHC_code_short_desc',
+            'state',
+            'county'
+        ]
+
+        snapshot.assert_match(yaml.dump(data), 'snapshot.yml')
+
+    def test_execute_report_no_data(self, snapshot):
+        report_spec = ReportSpec.model_validate(
+            {
+                'is_export': True,
+                'is_builtin': True,
+                'report_title': 'NBS Custom',
+                'library_name': 'nbs_custom',
+                'data_source_name': '[NBS_ODSE].[dbo].[PHCDemographic]',
+                'subset_query': """
+                        SELECT PHC_code_short_desc,
+                               state,
+                               county
+                        FROM NBS_ODSE.dbo.PHCDemographic
+                        WHERE 1 = 2
+                """,
+            }
+        )
+
+        result = execute_report(report_spec)
+        assert result.content_type == 'table'
+
+        assert len(result.content.data) == 0
+        assert result.content.columns == [
+            'PHC_code_short_desc',
+            'state',
+            'county'
+        ]
