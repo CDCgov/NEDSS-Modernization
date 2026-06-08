a188d072a3b5b1c542a8936dc1f30cbc6d01c8d2
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-06-08 13:26:41 -0400

    feat: add more options endpoints

--- apps/modernization-ui/src/options/report/index.ts
+++ apps/modernization-ui/src/options/report/index.ts
@@ -1,3 +1,4 @@
 export * from './useReportDataSources';
+export * from './useReportFilters';
 export * from './useReportLibraries';
 export * from './useReportSections';
--- libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/ReportLibrary.java
+++ libs/database-entities/src/main/java/gov/cdc/nbs/entity/odse/ReportLibrary.java
@@ -38,9 +38,6 @@ public class ReportLibrary {
   @NonNull @Column(name = "is_builtin_ind", length = 1, nullable = false)
   private Character isBuiltinInd;
 
-  @Column(name = "library_params", length = 300)
-  private String libraryParams;
-
   @NonNull @Column(name = "add_time", nullable = false)
   private LocalDateTime addTime;
 
