7c86ee1fe9558c3fde69f297c4ab56bdcad9456d
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-06-12 15:33:48 -0700

    Update AdvancedFilterConfigurationTest.java

--- apps/modernization-api/src/test/java/gov/cdc/nbs/report/models/AdvancedFilterConfigurationTest.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/report/models/AdvancedFilterConfigurationTest.java
@@ -1,8 +1,8 @@
 package gov.cdc.nbs.report.models;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 class AdvancedFilterConfigurationTest {
   @Test
