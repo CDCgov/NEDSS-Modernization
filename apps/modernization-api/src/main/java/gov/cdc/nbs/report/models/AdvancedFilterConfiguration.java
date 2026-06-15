414f1ed607e074681f72b78def88cdee80124611
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-06-15 10:31:53 -0700

    bump

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdvancedFilterConfiguration.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdvancedFilterConfiguration.java
@@ -6,11 +6,6 @@ public record AdvancedFilterConfiguration(
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED) Long reportFilterUid,
     AdvancedQuery.RuleGroup defaultValue,
     //  The resulting SQL query portion extracted during construction of the RuleGroup.
-    //  Included for debugging purposes if the RuleGroup cannot be constructed.
+    //  Included for debugging purposes.
     String query) {
-  public AdvancedFilterConfiguration {
-    if (defaultValue == null && (query == null || query.isBlank())) {
-      throw new IllegalArgumentException("Either 'defaultValue' or 'query' must be provided");
-    }
-  }
 }
