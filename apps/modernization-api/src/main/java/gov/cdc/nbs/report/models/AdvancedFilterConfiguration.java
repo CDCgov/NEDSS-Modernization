16fa6d9a67ef0b44de58063def8ed495e4319f56
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-06-15 12:17:28 -0700

    Update AdvancedFilterConfiguration.java

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdvancedFilterConfiguration.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/AdvancedFilterConfiguration.java
@@ -1,6 +1,5 @@
 package gov.cdc.nbs.report.models;
 
-import gov.cdc.nbs.report.AdvancedQueryException;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record AdvancedFilterConfiguration(
