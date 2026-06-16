36e7067c8238c27f900d9322ee04ea14bc6b0813
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-06-16 11:03:41 -0400

    fix: copy

--- apps/modernization-ui/src/apps/report/run/columns/SortSelector.tsx
+++ apps/modernization-ui/src/apps/report/run/columns/SortSelector.tsx
@@ -26,7 +26,7 @@ const SortSelector = ({ columns, defaultSort }: { columns: ReportColumn[]; defau
             label="Report data sorting"
             htmlFor={groupId}
             orientation="horizontal"
-            helperText="Sort data by one of the selected columns">
+            helperText="Sort data by a selected column">
             <div role="group" id={groupId} className={styles.layout}>
                 <Controller
                     name="sort.column"
