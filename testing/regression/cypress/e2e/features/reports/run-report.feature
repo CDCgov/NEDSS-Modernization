2b12799cdb15add626566c5d837d74e46d04c3f2
brick-green <brick.green1@gmail.com>
2026-06-17 15:03:27 -0500

    add tests for nbs sr 8, nbs sr 11, and nbs custom

--- testing/regression/cypress/e2e/features/reports/run-report.feature
+++ testing/regression/cypress/e2e/features/reports/run-report.feature
@@ -5,6 +5,25 @@ Feature: Run reports
     Given I navigate to reports
 
 
-  Scenario: I can run a report
-    When I navigate to report with reportUid: 6 and dataSourceUid: 1
-    Then I navigate to report with reportUid: 6 and dataSourceUid: 1
+  Scenario: I can run a report for library nbs_sr_08
+    When I navigate to report with reportUid: "4" and dataSourceUid: "1"
+    Then I enter "04/28/2025" to the From date
+    Then I enter "04/28/2026" to the To date
+    Then I select "AIDS" from the "Condition" dropdown menu
+    Then I click on the "Run" button
+    Then I see confirmation the report has run
+
+  Scenario: I can run a report for library nbs_sr_11
+    When I navigate to report with reportUid: "8" and dataSourceUid: "1"
+    Then I select "AIDS" from the "Condition" dropdown menu
+    Then I select "Appling County" from the "County" dropdown menu
+    Then I click on the "Run" button
+    Then I see confirmation the report has run
+
+  Scenario: I can run a report for library nbs custom
+    When I navigate to report with reportUid: "10066768" and dataSourceUid: "30"
+    Then I enter "04/28/2025" to the From date
+    Then I enter "04/28/2026" to the To date
+    Then I select the column "AST Result"
+    Then I click on the "Run" button
+    Then I see confirmation the report has run
--- testing/regression/cypress/step_definitions/report/run-report.steps.js
+++ testing/regression/cypress/step_definitions/report/run-report.steps.js
@@ -1,22 +1,49 @@
 import { When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
+// Navigation steps
+
 When('I navigate to reports', () => {
     cy.visit('/nbs/ManageReports.do');
     cy.contains('Private Reports').should('be.visible');
     cy.contains('Public Reports').should('be.visible');
     cy.contains('Template Reports').should('be.visible');
-    cy.contains('Report Facility Reports').should('be.visible');
+    cy.contains('Reporting Facility Reports').should('be.visible');
 });
 
 Then('I navigate to report with reportUid: {string} and dataSourceUid: {string}', (reportUid, dataSourceUid) => {
     cy.visit(`report/${reportUid}/${dataSourceUid}/run`);
 });
 
-Then('I select {string} from the Condition Code dropdown', (condition) => {
-    cy.get('input[name="basicFilter.id_14.value"]').type(condition).type('{enter}');
+// Component input steps
+
+Then('I enter {string} to the From date', (date) => {
+    cy.get('input[id=":r3:-from"]').type(date);
+});
+
+Then('I enter {string} to the To date', (date) => {
+    cy.get('input[id=":r3:-to"]').type(date);
+});
+
+Then('I select {string} from the {string} dropdown menu', (value, label) => {
+    cy.selectByLabel(label, value);
+});
+
+Then('I select the column {string}', (columnName) => {
+    cy.get('input[name="column-search"]').clear().type(columnName);
+    
+    cy.contains('label', columnName)
+        .find('input[type="checkbox"]')
+        .check({ force: true });
 });
 
 Then("I click on the {string} button", (buttonText) => {
     cy.contains('button', buttonText).click();
     cy.wait(1000);
 });
+
+// Confirmation steps
+
+Then("I see confirmation the report has run", () => {
+    cy.contains('main', 'Your report has run').should('be.visible');
+});
+
