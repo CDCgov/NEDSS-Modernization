24b2d284c67dea2799e6e78b2fa1ba8412d4ef0f
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-06-17 12:48:20 -0400

    [APP-703] feat: add delete report to sys admin ui (#3337)
    ## Description
    
    Fill in delete functionality on the view report sys admin page
    
    <img width="1337" height="1118" alt="image"
    src="https://github.com/user-attachments/assets/34b6aadb-5a46-41b3-91c3-670578efcd25"
    />
    
    Notes:
    * Added disabled state to confirmation modal so the buttons are not
    interactive while it's processing the request
    
    ## Tickets
    
    * https://cdc-nbs.atlassian.net/browse/APP-703
    
    ## Checklist before requesting a review
    - [ ] PR focuses on a single story
    - [ ] Code has been fully tested to meet acceptance criteria
    - [ ] PR is reasonably small and reviewable (Generally less than 10
    files and 500 changed lines)
    - [ ] All new functions/classes/components reasonably small
    - [ ] Functions/classes/components focused on one responsibility
    - [ ] Code easy to understand and modify (clarity over concise/clever)
    - [ ] PRs containing TypeScript follow the [Do's and
    Don'ts](https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html)
    - [ ] PR does not contain hardcoded values (Uses constants)
    - [ ] All code is covered by unit or feature tests

--- apps/modernization-ui/src/apps/report/management/configuration/ViewReportConfiguration.tsx
+++ apps/modernization-ui/src/apps/report/management/configuration/ViewReportConfiguration.tsx
@@ -1,17 +1,24 @@
 import { ReportLayout } from 'apps/report/layout/ReportLayout';
 import { Button, LinkButton } from 'design-system/button';
+import { useRef, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { ReportConfigurationContent } from './ReportConfigurationContent';
 
 import styles from 'apps/report/layout/layout.module.scss';
 import { NBS_LIST_REPORT_CONFIG_PAGE } from './constants';
-import { ReportConfiguration } from 'generated';
+import { ModalRef } from '@trussworks/react-uswds';
+import { ConfirmationModal } from 'confirmation';
+import { ReportConfiguration, ReportControllerService } from 'generated';
 import { LoadingBlock } from 'libs/loading/block';
+import { AlertMessage } from 'design-system/message';
 
 const ViewReportConfiguration = () => {
     const params = useParams();
     const reportUid = parseInt(params.reportUid ?? '0');
     const dataSourceUid = parseInt(params.dataSourceUid ?? '0');
+    const confirmDeleteRef = useRef<ModalRef>(null);
+    const [deleting, setDeleting] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
     const config = useLoaderData<ReportConfiguration>();
 
     return !config ? (
@@ -23,7 +30,7 @@ const ViewReportConfiguration = () => {
             startPage="Manage Reports"
             actions={
                 <>
-                    <Button secondary={true} onClick={() => window.alert('to do')}>
+                    <Button secondary={true} onClick={() => confirmDeleteRef.current?.toggleModal()}>
                         Delete
                     </Button>
                     <LinkButton href={`/report/management/configuration/${reportUid}/${dataSourceUid}/edit`}>
@@ -32,9 +39,33 @@ const ViewReportConfiguration = () => {
                 </>
             }
         >
+            {error && <AlertMessage type="error">{error}</AlertMessage>}
             <div className={styles.columnContent}>
                 <ReportConfigurationContent isEditable={false} config={config} />
             </div>
+            <ConfirmationModal
+                modal={confirmDeleteRef}
+                title={`Delete report: ${config?.title}`}
+                message={<>This action is permanent and cannot be undone.</>}
+                confirmText="Yes, delete"
+                cancelText="No, cancel"
+                disabled={deleting}
+                onConfirm={() => {
+                    setDeleting(true);
+                    ReportControllerService.deleteReport({ reportUid, dataSourceUid })
+                        .then(() => {
+                            window.location.href = NBS_LIST_REPORT_CONFIG_PAGE;
+                        })
+                        .catch((err) => {
+                            setError(JSON.stringify(err));
+                            confirmDeleteRef.current?.toggleModal();
+                        })
+                        .finally(() => setDeleting(false));
+                }}
+                onCancel={() => {
+                    confirmDeleteRef.current?.toggleModal();
+                }}
+            />
         </ReportLayout>
     );
 };
