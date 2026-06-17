9e6853d67eeae7b8496aac8ae551db3307240397
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-06-17 16:28:27 -0400

    [APP-666] feat: add column sort to ui (#3334)
    ## Description
    
    Add column sort to column selection card
    
    <img width="1153" height="821" alt="image"
    src="https://github.com/user-attachments/assets/d8e9c950-d392-41b4-9ffd-38254cea6773"
    />
    
    Notes:
    * Also added max width styling to the column selection widget, per
    design
    * Moved out the utils to the root of project as they were more useful in
    more places
    * Fixed a twitchy whitespace issue on horizontal field
    
    ## Tickets
    
    * https://cdc-nbs.atlassian.net/browse/APP-666
    
    ## Checklist before requesting a review
    - [ ] PR focuses on a single story
    - [ ] Code has been fully tested to meet acceptance criteria
    - [ ] PR is reasonably small and reviewable (Generally less than 10
    files and 500 changed lines)
    - [ ] All new functions/classes/components reasonably small
    - [ ] Functions/classes/components focused on one responsibility
    - [ ] Code easy to understand and modify (clarity over concise/clever)
    - [ ] PRs containing TypeScript follow the [Do's and
    Don'ts](https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html)
    - [ ] PR does not contain hardcoded values (Uses constants)
    - [ ] All code is covered by unit or feature tests

---
+++ apps/modernization-ui/src/apps/report/run/columns/SortSelector.tsx
@@ -0,0 +1,84 @@
+import { ReportColumn, SortSpec } from 'generated';
+import { ReportExecuteForm } from '../ReportRunPage';
+import { Controller, useController, useWatch } from 'react-hook-form';
+import { useEffect, useId } from 'react';
+import { SingleSelect } from 'design-system/select';
+import { toSelectable } from './utils';
+import { EnumSelectable } from 'apps/report/utils';
+import { EntryWrapper } from 'components/Entry';
+
+import styles from './sort-selector.module.scss';
+
+const DIRECTION_OPTIONS: EnumSelectable<SortSpec.direction>[] = [
+    { value: SortSpec.direction.ASC, name: 'Ascending' },
+    { value: SortSpec.direction.DESC, name: 'Descending' },
+];
+
+const SortSelector = ({
+    columns,
+    defaultSort,
+    defaultColumns,
+}: {
+    columns: ReportColumn[];
+    defaultSort?: SortSpec;
+    defaultColumns?: number[];
+}) => {
+    const groupId = useId();
+    const {
+        field: { onChange, value },
+    } = useController<ReportExecuteForm, 'sort.column'>({
+        name: 'sort.column',
+        defaultValue: defaultSort?.columnUid.toString(),
+    });
+    const selectedColumns =
+        useWatch<ReportExecuteForm, 'columns'>({
+            name: 'columns',
+            defaultValue: defaultColumns?.map((id) => id.toString()) ?? [],
+        }) ?? [];
+    const columnOptions = toSelectable(columns.filter(({ id }) => selectedColumns.includes(id.toString())));
+
+    // make sure the sort column data resets if column un-selected
+    useEffect(() => {
+        if (!selectedColumns.includes(value)) {
+            onChange(undefined);
+        }
+    }, [selectedColumns, value]);
+
+    return (
+        <EntryWrapper
+            sizing="medium"
+            label="Report data sorting"
+            htmlFor={groupId}
+            orientation="horizontal"
+            helperText="Sort data by a selected column"
+        >
+            <div role="group" id={groupId} className={styles.layout}>
+                <SingleSelect
+                    label="Sort by"
+                    id={`${groupId}-col`}
+                    orientation="vertical"
+                    options={columnOptions}
+                    value={columnOptions.find((option) => option.value === value)}
+                    onChange={(option) => onChange(option?.value)}
+                />
+                <Controller
+                    name="sort.direction"
+                    defaultValue={defaultSort?.direction ?? DIRECTION_OPTIONS[0].value}
+                    render={({ field: { value, onChange } }) => (
+                        <SingleSelect
+                            label="Sort order"
+                            id={`${groupId}-dir`}
+                            orientation="vertical"
+                            placeholder="" // force a choice
+                            options={DIRECTION_OPTIONS}
+                            value={DIRECTION_OPTIONS.find((option) => option.value === value)}
+                            onChange={(option) => onChange(option?.value)}
+                        />
+                    )}
+                />
+            </div>
+        </EntryWrapper>
+    );
+};
+
+export { SortSelector };
