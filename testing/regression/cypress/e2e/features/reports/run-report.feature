a1e403a502c14d9f0a2ef665913c8592a6f85d6a
brick-green <brick.green1@gmail.com>
2026-06-17 16:52:27 -0500

    remove unneeded navigation steps

--- testing/regression/cypress/e2e/features/reports/run-report.feature
+++ testing/regression/cypress/e2e/features/reports/run-report.feature
@@ -2,28 +2,26 @@ Feature: Run reports
 
   Background:
     Given I am logged in as secure user
-    Given I navigate to reports
-
 
   Scenario: I can run a report for library nbs_sr_08
     When I navigate to report with reportUid: "4" and dataSourceUid: "1"
-    Then I enter "04/28/2025" to the From date
-    Then I enter "04/28/2026" to the To date
-    Then I select "AIDS" from the "Condition" dropdown menu
-    Then I click on the "Run" button
+    And I enter "04/28/2025" to the From date
+    And I enter "04/28/2026" to the To date
+    And I select "AIDS" from the "Condition" dropdown menu
+    And I click on the "Run" button
     Then I see confirmation the report has run
 
   Scenario: I can run a report for library nbs_sr_11
     When I navigate to report with reportUid: "8" and dataSourceUid: "1"
-    Then I select "AIDS" from the "Condition" dropdown menu
-    Then I select "Appling County" from the "County" dropdown menu
-    Then I click on the "Run" button
+    And I select "AIDS" from the "Condition" dropdown menu
+    And I select "Appling County" from the "County" dropdown menu
+    And I click on the "Run" button
     Then I see confirmation the report has run
 
   Scenario: I can run a report for library nbs_custom
     When I navigate to report with reportUid: "10066768" and dataSourceUid: "30"
-    Then I enter "04/28/2025" to the From date
-    Then I enter "04/28/2026" to the To date
-    Then I select the column "AST Result"
-    Then I click on the "Run" button
+    And I enter "04/28/2025" to the From date
+    And I enter "04/28/2026" to the To date
+    And I select the column "AST Result"
+    And I click on the "Run" button
     Then I see confirmation the report has run
--- testing/regression/cypress/step_definitions/report/run-report.steps.js
+++ testing/regression/cypress/step_definitions/report/run-report.steps.js
@@ -2,48 +2,39 @@ import { When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 // Navigation steps
 
-When('I navigate to reports', () => {
-    cy.visit('/nbs/ManageReports.do');
-    cy.contains('Private Reports').should('be.visible');
-    cy.contains('Public Reports').should('be.visible');
-    cy.contains('Template Reports').should('be.visible');
-    cy.contains('Reporting Facility Reports').should('be.visible');
-});
-
-Then('I navigate to report with reportUid: {string} and dataSourceUid: {string}', (reportUid, dataSourceUid) => {
+When('I navigate to report with reportUid: {string} and dataSourceUid: {string}', (reportUid, dataSourceUid) => {
     cy.visit(`report/${reportUid}/${dataSourceUid}/run`);
 });
 
 // Component input steps
 
-Then('I enter {string} to the From date', (date) => {
-    cy.get('input[id=":r3:-from"]').type(date);
+When('I enter {string} to the From date', (date) => {
+    // Matches any ID ending with "-from", bypassing the React dynamic prefix
+    cy.get('input[id$="-from"]').type(date);
 });
 
-Then('I enter {string} to the To date', (date) => {
-    cy.get('input[id=":r3:-to"]').type(date);
+When('I enter {string} to the To date', (date) => {
+    // Matches any ID ending with "-to", bypassing the React dynamic prefix
+    cy.get('input[id$="-to"]').type(date);
 });
 
-Then('I select {string} from the {string} dropdown menu', (value, label) => {
+When('I select {string} from the {string} dropdown menu', (value, label) => {
     cy.selectByLabel(label, value);
 });
 
-Then('I select the column {string}', (columnName) => {
-    cy.get('input[name="column-search"]').clear().type(columnName);
-    
+When('I select the column {string}', (columnName) => {
+    cy.get('input[name="column-search"]').type(columnName);
     cy.contains('label', columnName)
         .find('input[type="checkbox"]')
         .check({ force: true });
 });
 
-Then("I click on the {string} button", (buttonText) => {
+When('I click on the {string} button', (buttonText) => {
     cy.contains('button', buttonText).click();
-    cy.wait(1000);
 });
 
 // Confirmation steps
 
-Then("I see confirmation the report has run", () => {
+Then('I see confirmation the report has run', () => {
     cy.contains('main', 'Your report has run').should('be.visible');
 });
-
