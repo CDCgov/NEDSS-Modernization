605fd523998615b219c36366e688b53340f5a1b3
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-06-18 11:05:16 -0400

    docs: hopefully helpful comment

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/ReportExecutionResult.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/ReportExecutionResult.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
+/** Model returned to the user/UI when they execute a report */
 public record ReportExecutionResult(
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull LibraryExecutionResult result,
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED) @NotNull String query,
