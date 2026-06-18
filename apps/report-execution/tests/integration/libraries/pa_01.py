49f82293c771cf1f8d32fc8d509a610b5b31e597
Ada Ball <ada@adamball.org>
2026-06-18 10:18:02 -0500

    add test for pa_01, start work on pa_01 library on individual stats

---
+++ apps/report-execution/tests/integration/libraries/pa_01.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.execute_report import execute_report
+from src.models import ReportSpec
+
+faker_schema = 'pa_01.yaml'
+
+
+#@pytest.mark.usefixtures('setup_containers', 'fake_db_table')
+@pytest.mark.integration
+class TestIntegrationPa01Library:
+    """Integration tests for the pa_01 library."""
+
+    def test_execute_report_check_data(self, snapshot):
+        report_spec = ReportSpec.model_validate(
+            {
+                'is_export': True,
+                'is_builtin': True,
+                'report_title': (
+                    'PA01 Case Management Report (Interview Assign Date) - STD'
+                ),
+                'library_name': 'pa_01',
+                'data_source_name': '[RDB].[dbo].[STD_HIV_DATAMART]',
+                'subset_query': 'SELECT * FROM [RDB].[dbo].[STD_HIV_DATAMART]',
+            }
+        )
+
+        result = execute_report(report_spec)
