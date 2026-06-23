f29bed1531d5e42662e5dd615f95df54ec3fce13
brick-green <brick.green1@gmail.com>
2026-06-23 09:28:12 -0500

    remove commented out code

--- testing/regression/cypress/support/commands.js
+++ testing/regression/cypress/support/commands.js
@@ -25,20 +25,6 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import '@testing-library/cypress/add-commands'
 
-// Cypress.Commands.add('selectByLabel', (labelText, value) => {
-//     // 1. Testing Library immediately finds the correct element tied to the label text
-//     cy.findByLabelText(labelText).then(($el) => {
-//         // 2. Determine if it's a standard select dropdown or a React custom input
-//         if ($el.is('select')) {
-//             // Standard HTML Select
-//             cy.wrap($el).select(value);
-//         } else {
-//             // React Select (where the label points directly to the text input)
-//             cy.wrap($el).click({ force: true }).type(`${value}{enter}`);
-//         }
-//     });
-// });
-
 Cypress.Commands.add('selectDropdownByLabel', (labelText, value) => {
     // Get the ID string instead of holding a live element reference
     cy.findByLabelText(labelText)
