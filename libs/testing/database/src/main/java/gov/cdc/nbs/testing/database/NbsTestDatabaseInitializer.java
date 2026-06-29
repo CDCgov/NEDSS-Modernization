60d01bfdd8384fed41cd7d55e5d740f4214962d3
elisa lee <emyl3@users.noreply.github.com>
2026-06-29 16:37:09 -0500

    test

--- libs/testing/database/src/main/java/gov/cdc/nbs/testing/database/NbsTestDatabaseInitializer.java
+++ libs/testing/database/src/main/java/gov/cdc/nbs/testing/database/NbsTestDatabaseInitializer.java
@@ -28,6 +28,7 @@ class NbsTestDatabaseInitializer
         new NbsDatabaseContainer<>(image)
             .withUsername(username)
             .withPassword(credential)
+            .withEnv("DB_VERSION", "6.0.19.1")
             .withImagePullPolicy(PullPolicy.alwaysPull());
 
     container.start();
