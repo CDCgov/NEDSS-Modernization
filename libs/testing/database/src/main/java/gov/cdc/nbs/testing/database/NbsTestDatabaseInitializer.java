54e9ee5702d6220a01ce0217ffab82e9c893ba14
brick-green <brick.green1@gmail.com>
2026-06-29 16:50:02 -0400

    run migrations in test container

--- libs/testing/database/src/main/java/gov/cdc/nbs/testing/database/NbsTestDatabaseInitializer.java
+++ libs/testing/database/src/main/java/gov/cdc/nbs/testing/database/NbsTestDatabaseInitializer.java
@@ -1,19 +1,21 @@
 package gov.cdc.nbs.testing.database;
 
+import java.lang.System.Logger.Level;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.Container.ExecResult;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.images.PullPolicy;
 
 class NbsTestDatabaseInitializer
     implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
+  private static final System.Logger LOGGER =
+      System.getLogger(NbsTestDatabaseInitializer.class.getCanonicalName());
+
   @Override
-  @SuppressWarnings({
-    // We don't want to close the container until after tests have completed
-    "resource"
-  })
+  @SuppressWarnings({"resource"})
   public void initialize(final ConfigurableApplicationContext context) {
     String image =
         context
@@ -26,16 +28,45 @@ class NbsTestDatabaseInitializer
         new NbsDatabaseContainer<>(image)
             .withUsername(username)
             .withPassword(credential)
-            .withEnv("DB_VERSION", "6.0.19.1")
             .withImagePullPolicy(PullPolicy.alwaysPull());
 
     container.start();
 
+    runMigrations(container, credential);
+
     String url = container.getJdbcUrl();
 
-    System.getLogger(NbsTestDatabaseInitializer.class.getCanonicalName())
-        .log(System.Logger.Level.INFO, () -> "[url]: %s".formatted(url));
+    LOGGER.log(System.Logger.Level.INFO, () -> "[url]: %s".formatted(url));
 
     TestPropertyValues.of("spring.datasource.url=" + url).applyTo(context.getEnvironment());
+  }
+
+  /** Execute the repository migrations within the running container to execute the repository . */
+  private void runMigrations(final JdbcDatabaseContainer<?> container, final String credential) {
+    try {
+      LOGGER.log(Level.INFO, "Database container live. Running repository migrations...");
+
+      var command =
+          """
+          export PATH="$PATH:/opt/mssql/bin:/opt/mssql-tools18/bin" && \
+          export SQLCMDPASSWORD='%s' && \
+          export MIGRATIONS_DIR='migrations' && \
+          export SEED_DATA_DIR='seed_data' && \
+          /var/data/run_migrations.sh %s
+          """
+              .formatted(credential, "6.0.19.1");
+
+      ExecResult result = container.execInContainer("/bin/bash", "-c", command);
+
+      if (result.getExitCode() != 0) {
+        throw new IllegalStateException(
+            "Migrations failed (%d): %s".formatted(result.getExitCode(), result.getStdout()));
+      }
+
+      LOGGER.log(Level.INFO, "Migrations completed successfully:\n{0}", result.getStdout());
+
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to run migrations on the container database", e);
+    }
   }
 }
