e458abe316550662bca6a3f2f67e9765a765734d
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-07-10 11:51:08 -0700

    Update ReportRepository.java

--- libs/database-entities/src/main/java/gov/cdc/nbs/repository/ReportRepository.java
+++ libs/database-entities/src/main/java/gov/cdc/nbs/repository/ReportRepository.java
@@ -3,13 +3,12 @@ package gov.cdc.nbs.repository;
 import gov.cdc.nbs.entity.odse.Report;
 import gov.cdc.nbs.entity.odse.ReportId;
 import gov.cdc.nbs.repository.detach.DetachableRepository;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ReportRepository
     extends DetachableRepository<Report>, JpaRepository<Report, ReportId> {
-    @EntityGraph(value = "graph.Report.saveAs", type = EntityGraph.EntityGraphType.FETCH)
-    Optional<Report> findWithGraphById(ReportId id);
+  @EntityGraph(value = "graph.Report.saveAs", type = EntityGraph.EntityGraphType.FETCH)
+  Optional<Report> findWithGraphById(ReportId id);
 }
