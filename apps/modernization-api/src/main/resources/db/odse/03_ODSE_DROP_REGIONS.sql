1915dfe40ed5b2b8320bf9e10a22115d3f87e26b
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-07-14 13:21:11 -0700

    Update 03_ODSE_DROP_REGIONS.sql

--- apps/modernization-api/src/main/resources/db/odse/03_ODSE_DROP_REGIONS.sql
+++ apps/modernization-api/src/main/resources/db/odse/03_ODSE_DROP_REGIONS.sql
@@ -1,14 +1,14 @@
 USE [NBS_ODSE]
 
 -- Drop report filters for "Regions" code
-DECLARE @Region_Filter_Code_Id INT;
+DECLARE @Region_Filter_Code_Id BIGINT;
 SET @Region_Filter_Code_Id = (SELECT filter_uid FROM dbo.Filter_code WHERE filter_code = 'J_R01');
 
 DELETE FROM dbo.Report_Filter WHERE filter_uid IN
                                     (SELECT report_filter_uid from dbo.Report_Filter where filter_uid = @Region_Filter_Code_Id);
 
 -- Drop report filters for "Regions (Including Nulls)"
-DECLARE @Region_Filter_Code_Nulls_Id INT;
+DECLARE @Region_Filter_Code_Nulls_Id BIGINT;
 SET @Region_Filter_Code_Nulls_Id = (SELECT filter_uid FROM dbo.Filter_code WHERE filter_code = 'J_R01_N');
 
 DELETE FROM dbo.Report_Filter WHERE filter_uid IN
