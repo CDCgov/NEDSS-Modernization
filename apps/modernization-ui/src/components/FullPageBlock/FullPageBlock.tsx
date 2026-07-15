1c87c297d0208b823f2fc3e8f8e4f735820a046f
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-07-15 16:35:34 -0400

    Apply suggestion from @mcmcgrath13

--- apps/modernization-ui/src/components/FullPageBlock/FullPageBlock.tsx
+++ apps/modernization-ui/src/components/FullPageBlock/FullPageBlock.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import classNames from 'classnames';
 import styles from './full-page-block.module.scss';
 
-// Needs to be inside a space that will grow to space, otherwise defaults to minimum height
+// Will not take up full page unless inside an element that will grow to the full space, otherwise defaults to minimum height
 const FullPageBlock = ({ className, children }: { className?: string; children?: ReactNode }) => {
     return <div className={classNames(className, styles.layout)}>{children}</div>;
 };
