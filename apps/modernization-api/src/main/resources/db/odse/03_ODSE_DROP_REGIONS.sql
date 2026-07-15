6c482996e2dd106557b7f087d8cecf4a5f918a62
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-07-15 09:55:21 -0700

    bump

--- apps/modernization-api/src/main/resources/db/odse/03_ODSE_DROP_REGIONS.sql
+++
@@ -1,9 +0,0 @@
-USE [NBS_ODSE]
-
--- Drop all report filters with `Regions`/`Regions (Including NULLS)` filter codes
-DELETE rf FROM dbo.Report_Filter rf
-    LEFT JOIN Filter_code fc ON rf.filter_uid = fc.filter_uid
-         WHERE fc.filter_code IN ('J_R01', 'J_R01_N');
-
--- Drop `Regions`/`Regions (Including NULLS)` filter codes
-DELETE FROM dbo.Filter_code WHERE filter_code IN ('J_R01', 'J_R01_N');
