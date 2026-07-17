d26b61088342d62e947e14e2d3992bd3b07464a3
Ada Ball <ada@adamball.org>
2026-07-17 10:27:32 -0500

    add test to exercise when metadat column names are not found

--- apps/report-execution/tests/integration/libraries/tb_case_ver.py
+++ apps/report-execution/tests/integration/libraries/tb_case_ver.py
@@ -1,15 +1,68 @@
 import pytest
 
 from src.execute_report import execute_report
-from src.models import ReportSpec
+from src.libraries import tb_case_ver
+from src.models import ReportSpec, Table
 
 faker_schema = 'dm_inv_tb.yaml'
 
 
-#@pytest.mark.usefixtures('setup_containers', 'fake_db_table')
+# @pytest.mark.usefixtures('setup_containers', 'fake_db_table')
 @pytest.mark.integration
 class TestIntegrationTbCaseVerLibrary:
     """Integration tests for the tb_case_ver library."""
+
+    def get_metadata_table(self, data):
+        return Table(
+            columns=[
+                'QUESTION_IDENTIFIER',
+                'RDB_COLUMN_NM',
+                'RDB_TABLE_NM',
+                'USER_DEFINED_COLUMN_NM',
+                'DATAMART_NM',
+            ],
+            data=data,
+        )
+
+    def test_missing_metadata_raises_valueerror(self, mocker):
+        trx = mocker.Mock()
+        mocker.patch.object(tb_case_ver, '_metadata_query', return_value='metadata SQL')
+
+        # INV111 missing
+        metadata = self.get_metadata_table(
+            [
+                ('INV1133', None, None, 'DISEASE_SITE', None),
+                ('INV1115', None, None, 'CASE_VERIFCTON_CAT', None),
+            ]
+        )
+        trx.query.return_value = metadata
+
+        with pytest.raises(ValueError, match='Column name metadata missing'):
+            tb_case_ver.execute(trx, 'subset query SQL')
+
+        # INV1115 missing
+        metadata = self.get_metadata_table(
+            [
+                ('INV1133', None, None, 'DISEASE_SITE', None),
+                ('INV111', None, None, 'INV_RPT_DT', None),
+            ]
+        )
+        trx.query.return_value = metadata
+
+        with pytest.raises(ValueError, match='Column name metadata missing'):
+            tb_case_ver.execute(trx, 'subset query SQL')
+
+        # INV1133 missing
+        metadata = self.get_metadata_table(
+            [
+                ('INV1115', None, None, 'CASE_VERIFCTON_CAT', None),
+                ('INV111', None, None, 'INV_RPT_DT', None),
+            ]
+        )
+        trx.query.return_value = metadata
+
+        with pytest.raises(ValueError, match='Column name metadata missing'):
+            tb_case_ver.execute(trx, 'subset query SQL')
 
     def test_execute_report_check_data(self, snapshot):
         report_spec = ReportSpec.model_validate(
