69f42e7851a24c41f249691d3ae82f81730b5dcb
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-07-28 11:52:50 -0700

    bump

--- apps/modernization-api/src/main/java/gov/cdc/nbs/config/AccessLogFilter.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/config/AccessLogFilter.java
@@ -11,10 +11,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AccessLogFilter implements Filter {
+  private static final String NBS_BASE_PATH = "/nbs/api";
+
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
-    if (request instanceof HttpServletRequest req && req.getRequestURI().startsWith("/nbs/api")) {
+    if (request instanceof HttpServletRequest req
+        && req.getRequestURI().startsWith(NBS_BASE_PATH)) {
       request.setAttribute("NBS_REQUEST", "true");
     }
     chain.doFilter(request, response);
