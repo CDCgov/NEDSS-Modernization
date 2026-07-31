0e0dc077c2338084d45bdb0b0caa3f25dd918810
Jordan Guinn <the.jordan.guinn@gmail.com>
2026-07-31 10:43:44 -0700

    Update errors.py

--- apps/report-execution/src/errors.py
+++ apps/report-execution/src/errors.py
@@ -57,10 +57,11 @@ class InternalServerError(BaseReportExecutionError):
     """
 
     def __init__(self, message, orig_exc=None):
-        logging.error(message)
-        if orig_exc is not None:
-            logging.error(orig_exc)
         super().__init__('Internal Server Error', 500)
+        
+        logging.error(f'Internal Server Error (ID: {self.id}) - {message}')
+        if orig_exc is not None:
+            logging.error(f'Original exception (ID: {self.id}): {orig_exc}')
 
 
 class InvalidReportSpecError(BaseReportExecutionError):
