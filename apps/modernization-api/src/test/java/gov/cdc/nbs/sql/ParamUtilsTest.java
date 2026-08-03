b0dacf9b01afb88354ab60c5510204eb76b9c6dc
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-08-03 16:52:22 -0400

    Apply suggestion from @mcmcgrath13

--- apps/modernization-api/src/test/java/gov/cdc/nbs/sql/ParamUtilsTest.java
+++ apps/modernization-api/src/test/java/gov/cdc/nbs/sql/ParamUtilsTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-public class ParamUtilsTest {
+class ParamUtilsTest {
 
   @Test
   void replaceListParam_replaces_handles_null_collection() {
