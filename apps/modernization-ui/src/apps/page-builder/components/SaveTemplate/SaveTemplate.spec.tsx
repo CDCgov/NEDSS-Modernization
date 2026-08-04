664d2ce2db96aac525fd9f2832b260cb1f4bd925
brick-green <brick.green1@gmail.com>
2026-08-04 11:21:08 -0400

    mock the modalRef

--- apps/modernization-ui/src/apps/page-builder/components/SaveTemplate/SaveTemplate.spec.tsx
+++ apps/modernization-ui/src/apps/page-builder/components/SaveTemplate/SaveTemplate.spec.tsx
@@ -1,15 +1,18 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router';
+import { createRef } from 'react';
 import { SaveTemplates } from './SaveTemplate';
 
 describe('When SaveTemplates component loads', () => {
     it('Save button should be disabled', () => {
+        const mockModalRef = createRef<any>();
+
         const { container } = render(
             <BrowserRouter>
-                <SaveTemplates />
+                <SaveTemplates modalRef={mockModalRef} />
             </BrowserRouter>
         );
         const btn = container.getElementsByClassName('usa-button')[0];
-        expect(btn.hasAttribute('disabled'));
+        expect(btn).toBeDisabled();
     });
 });
