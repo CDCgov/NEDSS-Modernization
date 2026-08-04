513ad5935528b4568e453eb223016e728b4602b3
brick-green <brick.green1@gmail.com>
2026-08-04 09:58:04 -0400

    add type for callback on handleExternalOnBlur

--- apps/modernization-ui/src/design-system/date/picker/DatePicker.tsx
+++ apps/modernization-ui/src/design-system/date/picker/DatePicker.tsx
@@ -63,7 +63,7 @@ const DatePicker = ({
         }
     }, [current, externalInputRef.current]);
 
-    const handleExternalOnBlur = useCallback((e) => onBlur?.(e), [onBlur]);
+    const handleExternalOnBlur = useCallback((e: Event) => onBlur?.(), [onBlur]);
 
     const handleExternalOnChange = useCallback(
         (event: Event) => {
