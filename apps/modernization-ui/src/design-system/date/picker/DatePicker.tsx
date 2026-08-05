23a5cfe00657dfd338ba87c48f2968def2a6bce5
brick-green <brick.green1@gmail.com>
2026-08-05 10:06:36 -0400

    fix remaining lint and typecheck

--- apps/modernization-ui/src/design-system/date/picker/DatePicker.tsx
+++ apps/modernization-ui/src/design-system/date/picker/DatePicker.tsx
@@ -65,7 +65,7 @@ const DatePicker = ({
         }
     }, [current, externalInputRef.current]);
 
-    const handleExternalOnBlur = useCallback((_e: Event) => onBlur?.(), [onBlur]);
+    const handleExternalOnBlur = useCallback(() => onBlur?.(), [onBlur]);
 
     const handleExternalOnChange = useCallback(
         (event: Event) => {
