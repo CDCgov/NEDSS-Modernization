4b77ac8ceb7cffef8bbf5b3545d8bcb5c87f61a3
Michael Peels <m.peels.dev@gmail.com>
2026-08-07 11:23:36 -0400

    Encode disallowed characters

--- apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/classic/IllegalURICharacterChannelHandler.java
+++ apps/nbs-gateway/src/main/java/gov/cdc/nbs/gateway/classic/IllegalURICharacterChannelHandler.java
@@ -1,26 +1,46 @@
 package gov.cdc.nbs.gateway.classic;
 
+import java.util.Map;
+import static java.util.Map.entry;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.DefaultHttpRequest;
 
 /**
- * A {@link io.netty.channel.ChannelHandler} that will encode the pipe ({@code |}) character to the
- * safer {@code %7C} value on the uri of the request. The pipe character is considered unsafe to be
- * included in URIs.
+ * A {@link io.netty.channel.ChannelHandler} that will encode special characters to the
+ * safe values on the uri of the request. 
  */
 class IllegalURICharacterChannelHandler extends ChannelInboundHandlerAdapter {
 
-  private static final String PIPE = "|";
-  private static final String PIPE_ENCODED = "%7C";
+  private static final Map<String, String> ENCODING_MAP = Map.ofEntries(
+    entry(" ", "%20"),
+    entry("{", "%7B"),
+    entry("}", "%7D"),
+    entry("[", "%5B"),
+    entry("]", "%5D"),
+    entry("|", "%7C"),
+    entry("\\", "%5C"),
+    entry("^", "%5E"),
+    entry("`", "%60"),
+    entry("<", "%3C"),
+    entry(">", "%3E"),
+    entry("\"", "%22")
+  );
 
   @Override
   public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception {
     if (msg instanceof DefaultHttpRequest request) {
-      //  alter the uri to encode pipe characters.
-      request.setUri(request.uri().replace(PIPE, PIPE_ENCODED));
+      // alter the uri to encode special characters sent by NBS 6.
+      request.setUri(encodeUri(request.uri()));
     }
 
     super.channelRead(ctx, msg);
+  }
+
+  String encodeUri(String uri) {
+    for (Map.Entry<String, String> entry : ENCODING_MAP.entrySet()) {
+      uri = uri.replace(entry.getKey(), entry.getValue());
+    }
+    return uri;
   }
 }
