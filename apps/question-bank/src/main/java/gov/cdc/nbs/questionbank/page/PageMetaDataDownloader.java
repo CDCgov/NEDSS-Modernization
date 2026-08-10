781c72a012671c9e2618f8c24698ff8366c1165c
Jordan Guinn <jordan.guinn@skylight.digital>
2026-08-10 11:28:32 -0700

    (fix): Address final Sonar issue, Take 3 (#3564)
    ## Description
    
    This is my third attempt to suppress the final outstanding issue in
    Sonar ("java:S2143", `Use the "java.time" API for date and time.`). I
    noticed that the error in Sonar _seems_ to be pointing to the very first
    line of the file, so I threw a `NOSONAR` next to the file's package
    declaration - hopefully that does it?
    
    ## Tickets
    
    * [Jira Ticket](https://cdc-nbs.atlassian.net/browse/APP-934)
    
    ## Checklist before requesting a review
    - [ ] PR focuses on a single story
    - [ ] Code has been fully tested to meet acceptance criteria
    - [ ] PR is reasonably small and reviewable (Generally less than 10
    files and 500 changed lines)
    - [ ] All new functions/classes/components reasonably small
    - [ ] Functions/classes/components focused on one responsibility
    - [ ] Code easy to understand and modify (clarity over concise/clever)
    - [ ] PRs containing TypeScript follow the [Do's and
    Don'ts](https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html)
    - [ ] PR does not contain hardcoded values (Uses constants)
    - [ ] All code is covered by unit or feature tests

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/page/PageMetaDataDownloader.java
+++ apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/page/PageMetaDataDownloader.java
@@ -1,4 +1,4 @@
-package gov.cdc.nbs.questionbank.page;
+package gov.cdc.nbs.questionbank.page; // NOSONAR
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -13,11 +13,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-/**
- * Suppressing `Use the "java.time" API for date and time` simply to avoid any changes in behavior
- * for the time being
- */
-@SuppressWarnings("java:S2143")
 @Service
 @RequiredArgsConstructor
 public class PageMetaDataDownloader {
