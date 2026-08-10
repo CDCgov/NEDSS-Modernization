079c902e8ed3f094975cb63ee509cc40608c36ba
brick-green <brick.green1@gmail.com>
2026-08-10 11:41:46 -0400

    test id on ManageSection.spec.tsx

--- apps/modernization-ui/src/apps/page-builder/page/management/edit/section/manage/ManageSection.tsx
+++ apps/modernization-ui/src/apps/page-builder/page/management/edit/section/manage/ManageSection.tsx
@@ -1,4 +1,4 @@
-import { useId, useState } from 'react';
+import { useState } from 'react';
 
 import { DragDropContext, Droppable } from '@hello-pangea/dnd';
 import { Button, Icon } from '@trussworks/react-uswds';
@@ -13,7 +13,7 @@ import { ManageSectionTile } from './ManageSectionTile/ManageSectionTile';
 import styles from './managesection.module.scss';
 
 type ManageSectionProps = {
-    id?: string;
+    id: string;
     pageId: number;
     tab?: PagesTab;
     onCancel?: () => void;
@@ -41,9 +41,6 @@ export const ManageSection = ({
     onHiddenSection,
     onUnhiddenSection,
 }: ManageSectionProps) => {
-    const generatedId = useId();
-    const activeId = id ?? generatedId;
-
     const [sectionState, setSectionState] = useState<'manage' | 'add' | 'edit'>('manage');
 
     const [confirmDelete, setConfirmDelete] = useState<PagesSection | undefined>(undefined);
@@ -86,7 +83,7 @@ export const ManageSection = ({
         <>
             {sectionState === 'add' && (
                 <AddSection
-                    id={activeId}
+                    id={id}
                     pageId={pageId}
                     onSectionTouched={() => {
                         onContentChange?.();
@@ -100,7 +97,7 @@ export const ManageSection = ({
             )}
             {sectionState === 'edit' && (
                 <AddSection
-                    id={activeId}
+                    id={id}
                     pageId={pageId}
                     onSectionTouched={() => {
                         onContentChange?.();
