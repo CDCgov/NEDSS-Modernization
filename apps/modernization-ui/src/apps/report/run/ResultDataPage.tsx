0dbd1b4d048342148e191e1c3a744af819e1a288
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-08-13 08:45:48 -0400

    fix: wip - result page no data iterating

--- apps/modernization-ui/src/apps/report/run/ResultDataPage.tsx
+++ apps/modernization-ui/src/apps/report/run/ResultDataPage.tsx
@@ -141,7 +141,7 @@ const ResultDataPage = () => {
                     )}
                 </Card>
 
-                <Card id="report-criteria" title="Report criteria">
+                <Card id="report-criteria" title="Report criteria" collapsible={true} open={false}>
                     <ValueField sizing={SIZING} label="Base SQL query">
                         {/* The uswds text-pre-line forces a sans font instead of respecting mono */}
                         <span style={{ whiteSpace: 'pre-line' }} className="font-mono-xs">
