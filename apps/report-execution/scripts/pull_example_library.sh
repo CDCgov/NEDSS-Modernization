33cbab96f5966b2b20a036e20c042b658c4a3683
Ada Ball <ada@adamball.org>
2026-08-17 12:32:55 -0500

    make pull_example_library script more verbose

--- apps/report-execution/scripts/pull_example_library.sh
+++ apps/report-execution/scripts/pull_example_library.sh
@@ -8,4 +8,4 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)";
 GH_URL="https://raw.githubusercontent.com/CDCgov/NEDSS-Custom-Library-Example/refs/heads/ab/initial/example_library.py";
 FILE_NAME="custom_lib_repo_example.py"
 
-curl --fail -s "${GH_URL}" -o "${SCRIPT_DIR}/../tests/integration/assets/${FILE_NAME}";
+curl --fail "${GH_URL}" -o "${SCRIPT_DIR}/../tests/integration/assets/${FILE_NAME}";
