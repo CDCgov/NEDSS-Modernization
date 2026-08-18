b94f8c8803bbd76ec4e2a9f274fbbf2e8974c114
elisa lee <emyl3@users.noreply.github.com>
2026-08-18 12:53:18 -0500

    Add e2e formatting

--- testing/regression/cypress/e2e/pages/advanced-condition-search/createNewCondition.page.js
+++ testing/regression/cypress/e2e/pages/advanced-condition-search/createNewCondition.page.js
@@ -1,6 +1,6 @@
 class CreateNewConditionPage {
-    navigateToCreateNewPage () {
-        cy.visit('/page-builder/pages/add')
+    navigateToCreateNewPage() {
+        cy.visit('/page-builder/pages/add');
     }
 
     selectEventTypeInvestigation() {
@@ -10,7 +10,7 @@ class CreateNewConditionPage {
     }
 
     additionalFieldsDisplayed() {
-        cy.contains("Create a new condition here");
+        cy.contains('Create a new condition here');
     }
 
     clickCreateNewConditionBtn() {
@@ -18,7 +18,7 @@ class CreateNewConditionPage {
     }
 
     createNewConditionWindowDisplayed(show = true) {
-        if(show) {
+        if (show) {
             cy.contains('Create new condition');
         } else {
             cy.contains('Create new page');
@@ -37,27 +37,33 @@ class CreateNewConditionPage {
     }
 
     verifyConditionFieldHasValue() {
-        cy.contains("Condition");
+        cy.contains('Condition');
     }
 
     completeRemainingFormFields() {
         const num = Math.random().toString(36).substring(2, 8);
         cy.get('#name').type('New Test Page ' + num);
-        cy.get('#templateId').find('option').eq(1).then((option) => {
-            cy.get('#templateId').select(option.attr('value'))
-            cy.get('#messageMappingGuide').find('option').eq(1).then((option) => {
-                cy.get('#messageMappingGuide').select(option.attr('value'))
+        cy.get('#templateId')
+            .find('option')
+            .eq(1)
+            .then((option) => {
+                cy.get('#templateId').select(option.attr('value'));
+                cy.get('#messageMappingGuide')
+                    .find('option')
+                    .eq(1)
+                    .then((option) => {
+                        cy.get('#messageMappingGuide').select(option.attr('value'));
+                    });
             });
-        });
     }
 
     clickCreatePageButton() {
-        cy.get('.createPage').eq(0).click()
+        cy.get('.createPage').eq(0).click();
     }
 
-    navigateToClassicDesign () {
+    navigateToClassicDesign() {
         cy.contains('New Test Page', { timeout: 5000 }).should('be.visible');
     }
 }
 
-export const createNewConditionPage = new CreateNewConditionPage()
+export const createNewConditionPage = new CreateNewConditionPage();
--- testing/regression/cypress/e2e/pages/edit-page/grouping/groupAndUngroupQuestions.page.js
+++ testing/regression/cypress/e2e/pages/edit-page/grouping/groupAndUngroupQuestions.page.js
@@ -1,15 +1,14 @@
 class GroupAndUngroupQuestions {
-
-    navigateEditPage () {
-        this.navigateToPreviewPageWithStatusInitialDraft()
-        cy.get("body").then($body => {
-            if ($body.find("#create-new-draft-button").length > 0) {
-                cy.get("create-new-draft-button").then($button => {
-                    if ($button.is(':visible')){
-                        $button.click()
+    navigateEditPage() {
+        this.navigateToPreviewPageWithStatusInitialDraft();
+        cy.get('body').then(($body) => {
+            if ($body.find('#create-new-draft-button').length > 0) {
+                cy.get('create-new-draft-button').then(($button) => {
+                    if ($button.is(':visible')) {
+                        $button.click();
                         cy.get('.editDraftBtn').eq(0).click();
                     }
-                })
+                });
             } else {
                 cy.get('.editDraftBtn').eq(0).click();
             }
@@ -18,75 +17,80 @@ class GroupAndUngroupQuestions {
 
     checkForGroupedQuestions() {
         cy.get('body').then((ele) => {
-            if(ele.find('[data-testid="questionGroupIndicator"]').length > 0) {
+            if (ele.find('[data-testid="questionGroupIndicator"]').length > 0) {
                 cy.get('[data-testid="questionGroupIndicator"]').eq(0);
             } else {
                 // section
                 cy.get('.manageSections').eq(0).click();
                 cy.get('.addNewSectionBtn').eq(0).click({ force: true });
-                cy.get('.sectionName').eq(0).type("test new section");
+                cy.get('.sectionName').eq(0).type('test new section');
                 cy.get('.addSectionBtn').eq(0).click();
-                cy.wait(2000)
+                cy.wait(2000);
                 cy.get('.manageSectionsCloseBtn').eq(0).click();
-                cy.wait(2000)
+                cy.wait(2000);
 
                 // subsection
                 cy.get('[data-testid="addNewSubsection"]').eq(0).click();
                 const newSubsectionName = Math.random().toString(36).substring(2, 8);
                 cy.get('[data-testid="subsectionName"]').eq(0).type(`New subsection name ${newSubsectionName}`);
                 cy.get('[data-testid="addOrEditSubsectionBtn"]').eq(0).click();
-                cy.wait(2000)
+                cy.wait(2000);
 
                 // questions
                 cy.get('.subsectionHeader').eq(0).get('.addQuestionBtn').eq(0).click();
-                cy.wait(2000)
+                cy.wait(2000);
                 cy.get('label[for="selection-0"]').click({ force: true });
-                cy.wait(1000)
+                cy.wait(1000);
                 cy.contains('Apply to page').click();
-                cy.wait(2000)
+                cy.wait(2000);
 
                 cy.get('.subsectionHeader').eq(0).get('.addQuestionBtn').eq(0).click();
-                cy.wait(2000)
+                cy.wait(2000);
                 cy.get('label[for="selection-0"]').click({ force: true });
-                cy.wait(1000)
+                cy.wait(1000);
                 cy.contains('Apply to page').click();
-                cy.wait(2000)
+                cy.wait(2000);
 
                 // grouping
-                cy.get(".subsectionOptionsWithGrouped-").eq(0).click();
-                cy.get('.subsectionOptionsWithGrouped-').eq(0)
-                    .get('[data-testid="groupQuestionsOption"]').eq(0).click();
-                this.renameBlockName()
-                this.fillTableColumnWidths()
-                cy.get('[data-testid="group-questions-submit-btn"]')
-                            .filter(':visible').eq(0).click();
-                cy.wait(2000)
+                cy.get('.subsectionOptionsWithGrouped-').eq(0).click();
+                cy.get('.subsectionOptionsWithGrouped-')
+                    .eq(0)
+                    .get('[data-testid="groupQuestionsOption"]')
+                    .eq(0)
+                    .click();
+                this.renameBlockName();
+                this.fillTableColumnWidths();
+                cy.get('[data-testid="group-questions-submit-btn"]').filter(':visible').eq(0).click();
+                cy.wait(2000);
                 cy.get('[data-testid="questionGroupIndicator"]').eq(0);
             }
-        })
+        });
     }
 
     clickMenuIcon(toGroup) {
-        if(toGroup) {
-            cy.get(".subsectionOptionsWithGrouped-").eq(0).click();
+        if (toGroup) {
+            cy.get('.subsectionOptionsWithGrouped-').eq(0).click();
         } else {
-            cy.get(".subsectionOptionsWithGrouped-grouped").eq(0).click();
+            cy.get('.subsectionOptionsWithGrouped-grouped').eq(0).click();
         }
     }
 
     shouldSee(text1, text2, delay) {
-        if(delay) {
+        if (delay) {
             cy.wait(4000);
         }
         cy.contains(text1);
-        if(text2) {
+        if (text2) {
             cy.contains(text2);
         }
     }
 
     clickUngroupQuestionOption() {
-        cy.get('.subsectionOptionsWithGrouped-grouped').eq(0)
-        .get('[data-testid="ungroupQuestionsOption"]').eq(0).click();
+        cy.get('.subsectionOptionsWithGrouped-grouped')
+            .eq(0)
+            .get('[data-testid="ungroupQuestionsOption"]')
+            .eq(0)
+            .click();
     }
 
     clickUngroupBtn() {
@@ -98,8 +102,7 @@ class GroupAndUngroupQuestions {
     }
 
     clickEditSubsectionOption() {
-        cy.get('.subsectionOptionsWithGrouped-grouped').eq(0)
-            .get('[data-testid="editSubsectionOption"]').eq(0).click();
+        cy.get('.subsectionOptionsWithGrouped-grouped').eq(0).get('[data-testid="editSubsectionOption"]').eq(0).click();
     }
 
     verifyEditSubsectionPrefilled() {
@@ -107,7 +110,7 @@ class GroupAndUngroupQuestions {
     }
 
     renameSubsection() {
-        cy.enterInput('[data-testid="editSubsectionModalSubsectionName"]',`Edited section name ${this.newName()}`);
+        cy.enterInput('[data-testid="editSubsectionModalSubsectionName"]', `Edited section name ${this.newName()}`);
     }
 
     renameBlockName(newName) {
@@ -120,14 +123,20 @@ class GroupAndUngroupQuestions {
     }
 
     updateAppearsInTableValueToNo() {
-        cy.get('[name="batches.0.width"]').eq(0).invoke('val').then(value => {
-            const columnWidth = value;
-            cy.get('[name="batches.0.appearsInTable"]').eq(0).select('N');
-            cy.get('[name="batches.1.width"]').eq(0).invoke('val').then(value2 => {
-                const updatedWidth = parseInt(value2) + parseInt(columnWidth)
-                cy.enterInput('[name="batches.1.width"]', updatedWidth);
+        cy.get('[name="batches.0.width"]')
+            .eq(0)
+            .invoke('val')
+            .then((value) => {
+                const columnWidth = value;
+                cy.get('[name="batches.0.appearsInTable"]').eq(0).select('N');
+                cy.get('[name="batches.1.width"]')
+                    .eq(0)
+                    .invoke('val')
+                    .then((value2) => {
+                        const updatedWidth = parseInt(value2) + parseInt(columnWidth);
+                        cy.enterInput('[name="batches.1.width"]', updatedWidth);
+                    });
             });
-        });
     }
 
     checkTableColumnPercentage() {
@@ -138,13 +147,12 @@ class GroupAndUngroupQuestions {
         return Math.random().toString(36).substring(2, 12);
     }
 
-     verifyGroupedSubmitButtonEnabled() {
+    verifyGroupedSubmitButtonEnabled() {
         cy.get('[data-testid="subsection-submit-btn"]');
     }
 
     clickGroupedSubmitBtn() {
-        cy.get('[data-testid="subsection-submit-btn"]')
-            .filter(':visible').eq(0).click();
+        cy.get('[data-testid="subsection-submit-btn"]').filter(':visible').eq(0).click();
     }
 
     verifyUngroupedSubmitButtonEnabled() {
@@ -152,19 +160,20 @@ class GroupAndUngroupQuestions {
     }
 
     clickUngroupedSubmitBtn() {
-        cy.get('[data-testid="group-questions-submit-btn"]')
-            .filter(':visible').eq(0).click();
+        cy.get('[data-testid="group-questions-submit-btn"]').filter(':visible').eq(0).click();
     }
 
     clickGroupQuestionOption() {
-        cy.wait(2000)
-        cy.get('.subsectionOptionsWithGrouped-ungrouped').eq(1)
-        .get('[data-testid="groupQuestionsOption"]').eq(0).click();
+        cy.wait(2000);
+        cy.get('.subsectionOptionsWithGrouped-ungrouped')
+            .eq(1)
+            .get('[data-testid="groupQuestionsOption"]')
+            .eq(0)
+            .click();
     }
 
     verifySubsectionNamePrefilled() {
-        cy.get('[data-testid="editSubsectionModalSubsectionName"]').eq(0)
-            .invoke('val').should('not.be.empty');
+        cy.get('[data-testid="editSubsectionModalSubsectionName"]').eq(0).invoke('val').should('not.be.empty');
     }
 
     verifySubsectionVisible(visible) {
@@ -180,23 +189,19 @@ class GroupAndUngroupQuestions {
             .its('length')
             .then((length) => {
                 const distributeRandomly = (total, numberOfRows) => {
-                    const initialCommonValue = Math.floor(total/numberOfRows);
-                    const initialCommonValuesTotal = initialCommonValue * numberOfRows
-                    const leftOver = total - initialCommonValuesTotal
-                    const lastValue = initialCommonValue + leftOver
-                    return [initialCommonValue, lastValue]
+                    const initialCommonValue = Math.floor(total / numberOfRows);
+                    const initialCommonValuesTotal = initialCommonValue * numberOfRows;
+                    const leftOver = total - initialCommonValuesTotal;
+                    const lastValue = initialCommonValue + leftOver;
+                    return [initialCommonValue, lastValue];
                 };
                 const distribution = distributeRandomly(100, length);
-                cy.get('tbody[data-testid="group-questions-tbody"] tr')
-                    .each(($row, rowIndex) => {
-                        cy.get($row)
-                            .get('td:last-child [data-testid="group-questions-width"]')
-                            .eq(rowIndex)
-                            .clear();
-                        cy.get($row)
-                            .get('td:last-child [data-testid="group-questions-width"]')
-                            .eq(rowIndex)
-                            .type(rowIndex === length-1 ? distribution[1]: distribution[0]);
+                cy.get('tbody[data-testid="group-questions-tbody"] tr').each(($row, rowIndex) => {
+                    cy.get($row).get('td:last-child [data-testid="group-questions-width"]').eq(rowIndex).clear();
+                    cy.get($row)
+                        .get('td:last-child [data-testid="group-questions-width"]')
+                        .eq(rowIndex)
+                        .type(rowIndex === length - 1 ? distribution[1] : distribution[0]);
                 });
             });
     }
@@ -204,16 +209,18 @@ class GroupAndUngroupQuestions {
     navigateToPreviewPageWithStatusInitialDraft() {
         cy.visit('/page-builder/pages');
         cy.wait(2000);
-        cy.get('#range-toggle').select('100')
+        cy.get('#range-toggle').select('100');
         cy.wait(2000);
-        cy.get("table[data-testid=table]").eq(0).find("tbody tr").each(($tr, index) => {
-            if($tr.find("td").eq(3).text() === "Initial Draft") {
-                cy.get('table.pageLibraryTable tbody tr td a').eq(index).click();
-                return false
-            }
-        });
+        cy.get('table[data-testid=table]')
+            .eq(0)
+            .find('tbody tr')
+            .each(($tr, index) => {
+                if ($tr.find('td').eq(3).text() === 'Initial Draft') {
+                    cy.get('table.pageLibraryTable tbody tr td a').eq(index).click();
+                    return false;
+                }
+            });
     }
-
 }
 
-export const groupAndUngroupQuestions = new GroupAndUngroupQuestions()
+export const groupAndUngroupQuestions = new GroupAndUngroupQuestions();
