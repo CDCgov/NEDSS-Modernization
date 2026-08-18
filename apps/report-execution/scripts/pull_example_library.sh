5ef3405c8fb135e94c081d8ba767e38697dbf64c
Ada Ball <ada@adamball.org>
2026-08-18 09:33:06 -0500

    add TODO

--- apps/report-execution/scripts/pull_example_library.sh
+++ apps/report-execution/scripts/pull_example_library.sh
@@ -5,6 +5,7 @@
 set -e
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)";
+# TODO: change to `main` branch reference once https://github.com/CDCgov/NEDSS-Custom-Library-Example/pull/1 is merged
 GH_URL="https://raw.githubusercontent.com/CDCgov/NEDSS-Custom-Library-Example/refs/heads/ab/initial/example_library.py";
 FILE_NAME="custom_lib_repo_example.py"
 
