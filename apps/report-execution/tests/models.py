a11b21a94c0ad30cb24e9e8603ad1cdb5ed4d807
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-08-19 14:24:50 -0400

    fix: performance - less iteration when csv-ifying

--- apps/report-execution/tests/models.py
+++ apps/report-execution/tests/models.py
@@ -68,51 +68,71 @@ class TestModels:
             assert d == '13-04-1985'
             assert dt == '13-04-1985 04:15'
 
-    def test_number_formatting(self):
-        columns = [
-            'example_small_float',
-            'example_large_float',
-            'example_small_int',
-            'example_large_int',
-        ]
-        data = [(0.00000001, 123456789.23456789, 1, 1000000000000001)]
-
-        t = models.Table(data=data, columns=columns)
-        csv_str = models.serialize_table(t)
-
-        data_line = csv_str.split('\r\n')[1]
-
-        (
-            example_small_float,
-            example_large_float,
-            example_small_int,
-            example_large_int,
-        ) = data_line.split(',')
-
-        assert example_small_float == '0'
-        assert example_large_float == '123456789.23'
-        assert example_small_int == '1'
-        assert example_large_int == '1000000000000001'
-
-    def test_newline_comma_formatting(self):
-        data = [
-            (
-                'a comment with\na new line',
-                'a comment with\r\na carriage return',
-                'a comment with " a quote',
-                'a comment with , a comma',
-                'a comment with\r\nall, the " things',
+    def test_number_formatting(self, monkeypatch):
+        with monkeypatch.context() as m:
+            m.setitem(config._CONFIG_CACHE, 'REPORT_EXPORT_DATE_FORMAT', '%d-%m-%Y')
+            m.setitem(
+                config._CONFIG_CACHE, 'REPORT_EXPORT_DATETIME_FORMAT', '%d-%m-%Y %H:%M'
             )
-        ]
-        columns = ['new line', 'carriage return', 'quote', 'comma', 'all the things']
 
-        t = models.Table(data=data, columns=columns)
-        csv_str = models.serialize_table(t)
-        str_io = io.StringIO(csv_str)
-        df = pd.read_csv(str_io)
+            columns = [
+                'example_small_float',
+                'example_large_float',
+                'example_small_int',
+                'example_large_int',
+            ]
+            data = [(0.00000001, 123456789.23456789, 1, 1000000000000001)]
 
-        assert df['new line'][0] == 'a comment with\na new line'
-        assert df['carriage return'][0] == 'a comment with\r\na carriage return'
-        assert df['quote'][0] == 'a comment with " a quote'
-        assert df['comma'][0] == 'a comment with , a comma'
-        assert df['all the things'][0] == 'a comment with\r\nall, the " things'
+            t = models.Table(data=data, columns=columns)
+            csv_str = models.serialize_table(t)
+
+            data_line = csv_str.split('\r\n')[1]
+
+            (
+                example_small_float,
+                example_large_float,
+                example_small_int,
+                example_large_int,
+            ) = data_line.split(',')
+
+            assert example_small_float == '0'
+            assert example_large_float == '123456789.23'
+            assert example_small_int == '1'
+            assert example_large_int == '1000000000000001'
+
+    def test_newline_comma_formatting(self, monkeypatch):
+        with monkeypatch.context() as m:
+            m.setitem(config._CONFIG_CACHE, 'REPORT_EXPORT_DATE_FORMAT', '%m/%d/%Y')
+            m.setitem(
+                config._CONFIG_CACHE,
+                'REPORT_EXPORT_DATETIME_FORMAT',
+                '%m/%d/%Y %H:%M:%S',
+            )
+
+            data = [
+                (
+                    'a comment with\na new line',
+                    'a comment with\r\na carriage return',
+                    'a comment with " a quote',
+                    'a comment with , a comma',
+                    'a comment with\r\nall, the " things',
+                )
+            ]
+            columns = [
+                'new line',
+                'carriage return',
+                'quote',
+                'comma',
+                'all the things',
+            ]
+
+            t = models.Table(data=data, columns=columns)
+            csv_str = models.serialize_table(t)
+            str_io = io.StringIO(csv_str)
+            df = pd.read_csv(str_io)
+
+            assert df['new line'][0] == 'a comment with\na new line'
+            assert df['carriage return'][0] == 'a comment with\r\na carriage return'
+            assert df['quote'][0] == 'a comment with " a quote'
+            assert df['comma'][0] == 'a comment with , a comma'
+            assert df['all the things'][0] == 'a comment with\r\nall, the " things'
