d201d172ed19f95960a38022d5fafde8c1acbfa3
elisa lee <ttu8@cdc.gov>
2026-08-20 16:58:55 -0500

    [chore] Update 7.13 release branch with latest changes (#3620)
    ## Description
    
    [chore] Update 7.13 release branch with latest changes
    
    ## Tickets
    
    * [Jira Ticket](url)
    
    ## Checklist before requesting a review
    - [ ] PR focuses on a single story
    - [ ] Code has been fully tested to meet acceptance criteria
    - [ ] PR is reasonably small and reviewable (Generally less than 10
    files and 500 changed lines)
    - [ ] All new functions/classes/components reasonably small
    - [ ] Functions/classes/components focused on one responsibility
    - [ ] Code easy to understand and modify (clarity over concise/clever)
    - [ ] PRs containing TypeScript follow the [Do's and
    Don'ts](https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html)
    - [ ] PR does not contain hardcoded values (Uses constants)
    - [ ] All code is covered by unit or feature tests
    
    ---------
    
    Co-authored-by: Mary McGrath <m.c.mcgrath13@gmail.com>
    Co-authored-by: Brick Green <86254221+brick-green@users.noreply.github.com>

--- apps/modernization-ui/src/apps/report/utils/openNewTab.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Open a new tab with the provided content available.
- *
- * The path should point to nbs, content will be placed in local storage at the provided key
- */
-export const openNewTab = (path: string, content?: unknown, storageKey?: string) => {
-    if (content && storageKey) localStorage.setItem(storageKey, JSON.stringify(content));
-    window.open(path, '_blank', 'noopener,noreferrer');
-};
