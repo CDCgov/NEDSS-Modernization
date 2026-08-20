9521dc991e359195fc693e37b4495065353743dd
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-08-20 14:09:41 -0400

    [APP-1006] fix: convert api => ui to streaming csv (#3614)
    ## Description
    
    Pull forward the same streaming csv content changes through to the api
    => ui exchange
    
    ---------
    
    Co-authored-by: Jordan Guinn <the.jordan.guinn@gmail.com>
    Co-authored-by: Jordan Guinn <jordan.guinn@skylight.digital>
    Co-authored-by: Brick Green <86254221+brick-green@users.noreply.github.com>

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/ReportExecutionServiceClient.java
+++ apps/modernization-api/src/main/java/gov/cdc/nbs/report/ReportExecutionServiceClient.java
@@ -2,16 +2,11 @@ package gov.cdc.nbs.report;
 
 import gov.cdc.nbs.datasource.utils.DataSourceNameUtils;
 import gov.cdc.nbs.exception.UnprocessableEntityException;
-import gov.cdc.nbs.report.models.LibraryExecutionResult;
 import gov.cdc.nbs.report.models.ReportConfiguration;
 import gov.cdc.nbs.report.models.ReportExecutionRequest;
-import gov.cdc.nbs.report.models.ReportExecutionResult;
 import gov.cdc.nbs.report.models.ReportSpec;
-import java.nio.charset.StandardCharsets;
-import java.time.Clock;
-import java.time.LocalDateTime;
+import java.util.function.BiConsumer;
 import org.apache.commons.lang3.NotImplementedException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -23,27 +18,26 @@ public class ReportExecutionServiceClient {
   private static final System.Logger LOGGER =
       System.getLogger(ReportExecutionServiceClient.class.getName());
 
-  private final Clock clock;
-
   private final RestClient restClient;
   private final DataSourceNameUtils dataSourceNameUtils;
   private final WhereClauseService whereClauseService;
   private final ReportFetcher reportFetcher;
 
   public ReportExecutionServiceClient(
-      final Clock clock,
       final DataSourceNameUtils dataSourceNameUtils,
       RestClient restClient,
       WhereClauseService whereClauseService,
       ReportFetcher reportFetcher) {
-    this.clock = clock;
     this.restClient = restClient;
     this.dataSourceNameUtils = dataSourceNameUtils;
     this.whereClauseService = whereClauseService;
     this.reportFetcher = reportFetcher;
   }
 
-  public ReportExecutionResult executeReport(ReportExecutionRequest request) {
+  public void executeReport(
+      ReportExecutionRequest request,
+      BiConsumer<RestClient.RequestHeadersSpec.ConvertibleClientHttpResponse, ReportSpec>
+          responseHandler) {
     Long reportUid = request.reportUid();
     Long dataSourceUid = request.dataSourceUid();
     ReportConfiguration reportConfigResponse = reportFetcher.getReport(reportUid, dataSourceUid);
@@ -53,46 +47,31 @@ public class ReportExecutionServiceClient {
     LOGGER.log(
         System.Logger.Level.DEBUG, "POSTing report execution request for report " + reportUid);
 
-    LibraryExecutionResult result =
-        restClient
-            .post()
-            .uri("/report/execute")
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.TEXT_PLAIN, MediaType.parseMediaType("text/csv"))
-            .body(reportSpec)
-            .exchange(
-                (req, resp) -> {
-                  if (resp.getStatusCode().isError()) {
-                    throw new RestClientResponseException(
-                        "Error response from the report-execution service",
-                        resp.getStatusCode(),
-                        resp.getStatusText(),
-                        resp.getHeaders(),
-                        resp.getBody().readAllBytes(),
-                        null);
-                  }
+    restClient
+        .post()
+        .uri("/report/execute")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.TEXT_PLAIN, MediaType.parseMediaType("text/csv"))
+        .body(reportSpec)
+        .exchange(
+            (req, resp) -> {
+              if (resp.getStatusCode().isError()) {
+                throw new RestClientResponseException(
+                    "Error response from the report-execution service",
+                    resp.getStatusCode(),
+                    resp.getStatusText(),
+                    resp.getHeaders(),
+                    resp.getBody().readAllBytes(),
+                    null);
+              }
 
-                  HttpHeaders headers = resp.getHeaders();
-
-                  String contextHeader = headers.getFirst("X-Report-Context-Header");
-                  String description = headers.getFirst("X-Report-Description");
-
-                  String csv = new String(resp.getBody().readAllBytes(), StandardCharsets.UTF_8);
-
-                  return new LibraryExecutionResult(csv, contextHeader, description);
-                });
-
-    if (result == null) {
-      throw new IllegalStateException(
-          "No error response and no body parsed from report execution service");
-    }
+              responseHandler.accept(resp, reportSpec);
+              return null;
+            });
 
     LOGGER.log(
         System.Logger.Level.DEBUG,
         "Report execution POST request succeeded for report " + reportUid);
-
-    return new ReportExecutionResult(
-        result, reportSpec.subsetQuery(), LocalDateTime.now(this.clock));
   }
 
   private ReportSpec buildReportSpec(
