4e575bcb0a0de52aba17e1c6c4c466dc18775b84
Mary McGrath <m.c.mcgrath13@gmail.com>
2026-08-20 07:20:44 -0400

    fix: rm dead code

--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/LibraryExecutionResult.java
+++
@@ -1,7 +0,0 @@
-package gov.cdc.nbs.report.models;
-
-import java.io.InputStream;
-
-/** Model returned from the report-execution service when a library is invoked */
-public record LibraryExecutionResult(
-    InputStream content, String contextHeader, String description) {}
--- apps/modernization-api/src/main/java/gov/cdc/nbs/report/models/ReportExecutionResult.java
+++
@@ -1,7 +0,0 @@
-package gov.cdc.nbs.report.models;
-
-import java.time.LocalDateTime;
-
-/** Model returned to the user/UI when they execute a report */
-public record ReportExecutionResult(
-    LibraryExecutionResult result, String query, LocalDateTime timestamp) {}
