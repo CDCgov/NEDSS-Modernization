80f1c7a683483e63b543aa84ed0f5ab65eddef23
Ada Ball <ada@adamball.org>
2026-08-20 12:52:31 -0500

    Restore some cucumber support files from f95ebc3b8d6f54c7e589a78c4eab2991223c9510

---
+++ libs/configuration-api/src/test/java/gov/cdc/nbs/configuration/features/FeaturesSteps.java
@@ -0,0 +1,26 @@
+package gov.cdc.nbs.configuration.features;
+
+import io.cucumber.java.ParameterType;
+
+public class FeaturesSteps {
+
+  @ParameterType(name = "feature", value = ".*")
+  public String feature(final String value) {
+    return switch (value.toLowerCase()) {
+      case "nbs6 event search" -> "features.search.events.enabled";
+      case "investigation search" -> "features.search.investigations.enabled";
+      case "laboratory report search" -> "features.search.laboratoryReports.enabled";
+      case "search view" -> "features.search.view.enabled";
+      case "tabular search results" -> "features.search.view.table.enabled";
+      case "modernized patient profile" -> "features.patient.profile.enabled";
+      case "patient file" -> "features.patient.file.enabled";
+      case "patient file merge history" -> "features.patient.file.mergeHistory.enabled";
+      case "patient search filters" -> "features.patient.search.filters.enabled";
+      case "deduplication" -> "features.deduplication.enabled";
+      case "deduplication merge" -> "features.deduplication.merge.enabled";
+      case "system management" -> "features.system.management.enabled";
+      case "report execution" -> "features.report.execution.enabled";
+      default -> value;
+    };
+  }
+}
