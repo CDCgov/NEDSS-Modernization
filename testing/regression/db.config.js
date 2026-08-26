81b783ffe05f30896284c7d22e9e9935f71d2545
elisa lee <ttu8@cdc.gov>
2026-08-26 18:25:55 -0500

    test: add e2e tests for patient file lab reports (#3622)
    ## Description
    
    Adds e2e tests for [rows
    123-129](https://docs.google.com/spreadsheets/d/1qFEchotWjNCke6Yhm6vNPFxYoYF0c39d4vCB1l_icHM/edit?pli=1&gid=0#gid=0&range=123:129)
    
    ## Tickets
    
    * [Jira Ticket](https://cdc-nbs.atlassian.net/browse/APP-205)
    
    ## Checklist before requesting a review
    - [ ] PR focuses on a single story
    - [ ] Code has been fully tested to meet acceptance criteria
    - [ ] PR is reasonably small and reviewable (Generally less than 10
    files and 500 changed lines)
    - [ ] All new functions/classes/components reasonably small
    - [ ] Functions/classes/components focused on one responsibility
    - [ ] Code easy to understand and modify (clarity over concise/clever)
    - [ ] PRs containing TypeScript follow the [Do's and
    Don'ts](https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html)
    - [ ] PR does not contain hardcoded values (Uses constants)
    - [ ] All code is covered by unit or feature tests
    
    ---------
    
    Co-authored-by: elisa lee <emyl3@users.noreply.github.com>

---
+++ testing/regression/db.config.js
@@ -0,0 +1,13 @@
+export const dbConfig = {
+    user: 'sa',
+    password: 'PizzaIsGood33!',
+    server: 'localhost',
+    port: 1433,
+    database: 'NBS_ODSE',
+    options: {
+        encrypt: false,
+        trustServerCertificate: true,
+    },
+};
+
+export default dbConfig;
