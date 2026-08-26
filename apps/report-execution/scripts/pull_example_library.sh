fdb2a24ea3c0572bc6516bb036e742f36bd08664
Ada Ball <ada@adamball.org>
2026-08-26 08:38:41 -0500

    update download url in pull_example_library.sh

--- apps/report-execution/scripts/pull_example_library.sh
+++ apps/report-execution/scripts/pull_example_library.sh
@@ -5,8 +5,7 @@
 set -e
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)";
-# TODO: change to `main` branch reference once https://github.com/CDCgov/NEDSS-Custom-Library-Example/pull/1 is merged
-GH_URL="https://raw.githubusercontent.com/CDCgov/NEDSS-Custom-Library-Example/refs/heads/ab/initial/example_library.py";
+GH_URL="https://raw.githubusercontent.com/CDCgov/NEDSS-Custom-Library-Example/refs/heads/main/example_library.py";
 FILE_NAME="custom_lib_repo_example.py"
 
 curl --fail "${GH_URL}" -o "${SCRIPT_DIR}/../tests/integration/assets/${FILE_NAME}";
