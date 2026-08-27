24e583500d9fcb2d554a89a5fb19dcd16461c0f0
Ada Ball <etc@adamball.org>
2026-08-27 09:13:29 -0500

    [APP-976] Remove unused Java files in mod api (#3613)
    ## Description
    
    Removing Java files that are not used anywhere.
    
    ## Tickets
    
    * [Jira Ticket](https://cdc-nbs.atlassian.net/browse/APP-976)
    
    ---------
    
    Co-authored-by: Ada Ball <ada@adamball.org>

--- apps/question-bank/src/main/java/gov/cdc/nbs/questionbank/entity/repository/UserProfileRepository.java
+++
@@ -1,6 +0,0 @@
-package gov.cdc.nbs.questionbank.entity.repository;
-
-import gov.cdc.nbs.questionbank.entity.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {}
